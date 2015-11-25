c1c66714a2fd84a010a8b249f388a6b43e21cd3f
Hixie <github.com@spam.hixie.ch>
2015-11-25 12:47:27 -0800

    Have Travis check the build for Dart errors.

--- travis/build.sh
+++ travis/build.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Smoke test sky_shell.
-./out/sky_shell --help
-
-# Run the tests.
-pushd sky/unit
-SKY_SHELL=../../out/sky_shell ../../third_party/dart-sdk/dart-sdk/bin/pub run sky_tools:sky_test -j 1
-popd
-
-# Analyze the code.
-pushd sky/packages/workbench
-../../../third_party/dart-sdk/dart-sdk/bin/pub get
-popd
-pushd sky/packages/sky
-../../tools/skyanalyzer --congratulate
-popd
-
-# Generate docs.
-./sky/tools/skydoc.py
+sky/tools/gn --debug
+ninja -C out/Debug generate_dart_ui
+travis/analyze.sh
