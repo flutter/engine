8aa655cc076024ebcfa8dd48f1fad3146099bf8b
Matt Perry <mpcomplete@chromium.org>
2016-05-06 17:33:30 -0400

    format.arg

--- sky/services/editing/src/org/domokit/editing/ClipboardImpl.java
+++ sky/services/editing/src/org/domokit/editing/ClipboardImpl.java
@@ -5,12 +5,13 @@
 package org.domokit.editing;
 
 import android.content.ClipboardManager;
+import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Context;
 import android.view.inputmethod.InputMethodManager;
 
 import org.chromium.mojo.system.MojoException;
-import org.chromium.mojom.editing.ClipData;
+import org.chromium.mojom.editing.ClipboardData;
 import org.chromium.mojom.editing.Clipboard;
 
 /**
@@ -18,6 +19,7 @@ import org.chromium.mojom.editing.Clipboard;
  */
 public class ClipboardImpl implements Clipboard {
     private Context mContext;
+    private static final String kTextPlainFormat = "text/plain";
 
     public ClipboardImpl(Context context) {
         mContext = context;
@@ -31,28 +33,32 @@ public class ClipboardImpl implements Clipboard {
     public void onConnectionError(MojoException e) {}
 
     @Override
-    public void setClipData(ClipData incomingClip) {
+    public void setClipboardData(ClipboardData incomingClip) {
         ClipboardManager clipboard =
             (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
-        android.content.ClipData clip =
-            android.content.ClipData.newPlainText("text label?", incomingClip.text);
+        ClipData clip = ClipData.newPlainText("text label?", incomingClip.text);
         clipboard.setPrimaryClip(clip);
     }
 
     @Override
-    public void getClipData(GetClipDataResponse callback) {
+    public void getClipboardData(String format, GetClipboardDataResponse callback) {
         ClipboardManager clipboard =
             (ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
-        android.content.ClipData clip = clipboard.getPrimaryClip();
-        if (clip == null ||
-            !clip.getDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+        ClipData clip = clipboard.getPrimaryClip();
+        if (clip == null) {
             callback.call(null);
             return;
         }
 
-        android.content.ClipData.Item item = clip.getItemAt(0);
-        ClipData clipResult = new ClipData();
-        clipResult.text = item.getText().toString();
-        callback.call(clipResult);
+        if ((format == null || format.equals(kTextPlainFormat)) &&
+            clip.getDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
+          ClipboardData clipResult = new ClipboardData();
+          clipResult.text = clip.getItemAt(0).getText().toString();
+          callback.call(clipResult);
+          return;
+        }
+
+        // Unsupported or incompatible format.
+        callback.call(null);
     }
 }
