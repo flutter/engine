6798183f2e5eb0eb7189266ef3a2cab53d525400
Chinmay Garde <chinmaygarde@google.com>
2016-09-01 10:43:41 -0700

    Avoid using [NSArray firstObject] on the Mac platform view.

--- sky/shell/platform/mac/platform_view_mac.mm
+++ sky/shell/platform/mac/platform_view_mac.mm
@@ -31,8 +31,10 @@ PlatformViewMac::PlatformViewMac(NSOpenGLView* gl_view)
       weak_factory_(this) {
   NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,
                                                        NSUserDomainMask, YES);
-  sky::shell::Shell::Shared().tracing_controller().set_traces_base_path(
-      [paths.firstObject UTF8String]);
+  if (paths.count > 0) {
+    sky::shell::Shell::Shared().tracing_controller().set_traces_base_path(
+        [[paths objectAtIndex:0] UTF8String]);
+  }
 }
 
 PlatformViewMac::~PlatformViewMac() = default;
