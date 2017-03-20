c211ad4163578b28ebef33586c286b8da3d2f0fc
Chinmay Garde <chinmaygarde@google.com>
2017-03-20 12:43:30 -0700

    Implementations of platform agnostic portions of FML.
    This patch does not include updates to the BUILD.gn files or the platform specific backends. This allows us to land these patches without breaking existing builds. Meanwhile, the platform specific backends will be sent in different PRs for easier review.
    
    This patch builds upon the suggestions made on https://github.com/flutter/engine/pull/3459 (which was abandoned).

---
+++ fml/message_loop.cc
@@ -0,0 +1,68 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/fml/message_loop.h"
+#include <utility>
+#include "flutter/fml/message_loop_impl.h"
+#include "flutter/fml/task_runner.h"
+#include "flutter/fml/thread_local.h"
+#include "lib/ftl/memory/ref_counted.h"
+#include "lib/ftl/memory/ref_ptr.h"
+
+namespace fml {
+
+FML_THREAD_LOCAL ThreadLocal TLSMessageLoop([](intptr_t value) {
+  delete reinterpret_cast<MessageLoop*>(value);
+});
+
+MessageLoop& MessageLoop::GetCurrent() {
+  auto loop = reinterpret_cast<MessageLoop*>(TLSMessageLoop.Get());
+  FTL_DCHECK(loop != nullptr)
+      << "MessageLoop::EnsureInitializedForCurrentThread was not called on "
+         "this thread prior to message loop use.";
+  return *loop;
+}
+
+void MessageLoop::EnsureInitializedForCurrentThread() {
+  if (TLSMessageLoop.Get() != 0) {
+    // Already initialized.
+    return;
+  }
+  TLSMessageLoop.Set(reinterpret_cast<intptr_t>(new MessageLoop()));
+}
+
+bool MessageLoop::IsInitializedForCurrentThread() {
+  return TLSMessageLoop.Get() != 0;
+}
+
+MessageLoop::MessageLoop()
+    : loop_(MessageLoopImpl::Create()),
+      task_runner_(ftl::MakeRefCounted<fml::TaskRunner>(loop_)) {
+  FTL_CHECK(loop_);
+  FTL_CHECK(task_runner_);
+}
+
+MessageLoop::~MessageLoop() = default;
+
+void MessageLoop::Run() {
+  loop_->DoRun();
+}
+
+void MessageLoop::Terminate() {
+  loop_->DoTerminate();
+}
+
+ftl::RefPtr<ftl::TaskRunner> MessageLoop::GetTaskRunner() const {
+  return task_runner_;
+}
+
+ftl::RefPtr<MessageLoopImpl> MessageLoop::GetLoopImpl() const {
+  return loop_;
+}
+
+void MessageLoop::SetTaskObserver(TaskObserver observer) {
+  loop_->SetTaskObserver(std::move(observer));
+}
+
+}  // namespace fml
