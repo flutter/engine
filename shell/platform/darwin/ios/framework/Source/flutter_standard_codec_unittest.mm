05afd10f910dedb7da7b0db826b92336dbd34b85
Mikkel Ravn <mravn@google.com>
2017-03-21 11:34:57 +0100

    Better value coverage of std codec. Unit tests added.

--- shell/platform/darwin/ios/framework/Source/flutter_standard_codec_unittest.mm
+++ shell/platform/darwin/ios/framework/Source/flutter_standard_codec_unittest.mm
@@ -164,3 +164,138 @@ TEST(FlutterStandardCodec, CanEncodeAndDecodeFloat64Array) {
   ASSERT_TRUE(decoded.elementSize == 8);
   ASSERT_TRUE([data isEqualTo:decoded.data]);
 }
+
+TEST(FlutterStandardCodec, AgreesWithJSONCodecOnBooleans) {
+  FlutterStandardMessageCodec* stdCodec =
+      [FlutterStandardMessageCodec sharedInstance];
+  FlutterJSONMessageCodec* jsonCodec = [FlutterJSONMessageCodec sharedInstance];
+  NSArray* values = @[ @(true), @(false), @YES, @NO ];
+  NSData* stdEncoded = [stdCodec encode:values];
+  NSArray* stdDecoded = [stdCodec decode:stdEncoded];
+  NSData* jsonEncoded = [jsonCodec encode:values];
+  NSArray* jsonDecoded = [jsonCodec decode:jsonEncoded];
+  ASSERT_TRUE([stdDecoded isEqualTo:jsonDecoded]);
+  ASSERT_TRUE([stdDecoded isEqualTo:values]);
+}
+
+TEST(FlutterStandardCodec, AgreesWithJSONCodecOnSmallIntegers) {
+  FlutterStandardMessageCodec* stdCodec =
+      [FlutterStandardMessageCodec sharedInstance];
+  FlutterJSONMessageCodec* jsonCodec = [FlutterJSONMessageCodec sharedInstance];
+  UInt8 u8 = 0xff;
+  UInt16 u16 = 0xffff;
+  UInt32 u32 = 0xffffffff;
+  SInt8 s8 = 0xff;
+  SInt16 s16 = 0xffff;
+  SInt32 s32 = 0xffffffff;
+  SInt64 s64 = 0xffffffffffffffff;
+  NSArray* values = @[ @(u8), @(u16), @(u32), @(s8), @(s16), @(s32), @(s64) ];
+  NSData* stdEncoded = [stdCodec encode:values];
+  NSArray* stdDecoded = [stdCodec decode:stdEncoded];
+  NSData* jsonEncoded = [jsonCodec encode:values];
+  NSArray* jsonDecoded = [jsonCodec decode:jsonEncoded];
+  ASSERT_TRUE([stdDecoded isEqualTo:jsonDecoded]);
+  ASSERT_TRUE([stdDecoded isEqualTo:values]);
+}
+
+TEST(FlutterStandardCodec, UsesHexOnLargeIntegers) {
+  FlutterStandardMessageCodec* codec =
+      [FlutterStandardMessageCodec sharedInstance];
+  UInt64 u64 = 0xffffffffffffffff;
+  NSData* encoded = [codec encode:@(u64)];
+  FlutterStandardBigInteger* decoded = [codec decode:encoded];
+  ASSERT_TRUE([decoded.hex isEqualTo:@"ffffffffffffffff"]);
+}
+
+TEST(FlutterStandardCodec, Uses64BitFloats) {
+  FlutterStandardMessageCodec* codec =
+      [FlutterStandardMessageCodec sharedInstance];
+  float f32 = 0.1f;
+  double f64 = 0.1;
+  double f32converted = 0.1f;
+  NSArray* values = @[ @(f32), @(f64) ];
+  NSArray* decodedValues = @[ @(f32converted), @(f64) ];
+  NSData* encoded = [codec encode:values];
+  NSArray* decoded = [codec decode:encoded];
+  ASSERT_TRUE([decoded isEqualTo:decodedValues]);
+}
+
+TEST(FlutterStandardCodec, HandlesMethodCallsWithNilArguments) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  FlutterMethodCall* call =
+      [FlutterMethodCall methodCallWithMethodName:@"hello" arguments:nil];
+  NSData* encoded = [codec encodeMethodCall:call];
+  FlutterMethodCall* decoded = [codec decodeMethodCall:encoded];
+  ASSERT_TRUE([decoded.method isEqualTo:call.method]);
+  ASSERT_EQ(decoded.arguments, nil);
+}
+
+TEST(FlutterStandardCodec, HandlesMethodCallsWithSingleArgument) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  FlutterMethodCall* call =
+      [FlutterMethodCall methodCallWithMethodName:@"hello" arguments:@42];
+  NSData* encoded = [codec encodeMethodCall:call];
+  FlutterMethodCall* decoded = [codec decodeMethodCall:encoded];
+  ASSERT_TRUE([decoded.method isEqualTo:call.method]);
+  ASSERT_EQ(decoded.arguments, @42);
+}
+
+TEST(FlutterStandardCodec, HandlesMethodCallsWithArgumentList) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  NSArray* arguments = @[ @42, @"world" ];
+  FlutterMethodCall* call =
+      [FlutterMethodCall methodCallWithMethodName:@"hello" arguments:arguments];
+  NSData* encoded = [codec encodeMethodCall:call];
+  FlutterMethodCall* decoded = [codec decodeMethodCall:encoded];
+  ASSERT_TRUE([decoded.method isEqualTo:call.method]);
+  ASSERT_TRUE([decoded.arguments isEqualTo:arguments]);
+}
+
+TEST(FlutterStandardCodec, HandlesSuccessEnvelopesWithNilResult) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  NSData* encoded = [codec encodeSuccessEnvelope:nil];
+  FlutterError* error = nil;
+  id decoded = [codec decodeEnvelope:encoded error:&error];
+  ASSERT_EQ(error, nil);
+  ASSERT_EQ(decoded, nil);
+}
+
+TEST(FlutterStandardCodec, HandlesSuccessEnvelopesWithSingleResult) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  NSData* encoded = [codec encodeSuccessEnvelope:@42];
+  FlutterError* decodedError = nil;
+  id decodedResult = [codec decodeEnvelope:encoded error:&decodedError];
+  ASSERT_EQ(decodedError, nil);
+  ASSERT_EQ(decodedResult, @42);
+}
+
+TEST(FlutterStandardCodec, HandlesSuccessEnvelopesWithResultMap) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  NSDictionary* result = @{ @"a" : @42, @42 : @"a" };
+  NSData* encoded = [codec encodeSuccessEnvelope:result];
+  FlutterError* decodedError = nil;
+  id decodedResult = [codec decodeEnvelope:encoded error:&decodedError];
+  ASSERT_TRUE([decodedResult isEqualTo:result]);
+}
+
+TEST(FlutterStandardCodec, HandlesErrorEnvelopes) {
+  FlutterStandardMethodCodec* codec =
+      [FlutterStandardMethodCodec sharedInstance];
+  NSDictionary* details = @{ @"a" : @42, @42 : @"a" };
+  FlutterError* error = [FlutterError errorWithCode:@"errorCode"
+                                            message:@"something failed"
+                                            details:details];
+  NSData* encoded = [codec encodeErrorEnvelope:error];
+  FlutterError* decodedError = nil;
+  id decodedResult = [codec decodeEnvelope:encoded error:&decodedError];
+  ASSERT_EQ(decodedResult, nil);
+  ASSERT_TRUE([decodedError.code isEqualTo:error.code]);
+  ASSERT_TRUE([decodedError.message isEqualTo:error.message]);
+  ASSERT_TRUE([decodedError.details isEqualTo:error.details]);
+}
