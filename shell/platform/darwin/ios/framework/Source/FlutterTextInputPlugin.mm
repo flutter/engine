ef46a704345aca8e2efe255606cc75dec3a64f07
Chris Bracken <chris@bracken.jp>
2017-05-08 17:34:06 -0700

    Disable auto-correct for text editing when obscureText is set

--- shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterTextInputPlugin.mm
@@ -548,6 +548,7 @@ static UIKeyboardType ToUIKeyboardType(NSString* inputType) {
 
 - (void)setTextInputClient:(int)client withConfiguration:(NSDictionary*)configuration {
   _view.keyboardType = ToUIKeyboardType(configuration[@"inputType"]);
+  _view.secureTextEntry = [@"true" isEqualToString:configuration[@"obscureText"]];
   [_view setTextInputClient:client];
   [_view reloadInputViews];
 }
