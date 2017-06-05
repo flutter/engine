flutter/flutter

commit dcfcc9259d4f4321e8daea24f1778694a8dca98a

Simplify log forwarding on iOS, Android (#3726)
Eliminates logging to stdout on Android -- now using __android_log_print
only.

Eliminates logging to syslog on iOS -- now writing to stdout with the
existing ASL redirection (from platform_mac.mm) only. syslog() wasn't
ever picked up in logs.

This patch is a pre-factoring change before swapping out iOS engine
logging to a flutter-specific mechanism.

lib/ui/dart_runtime_hooks.cc
@@ -27,13 +27,6 @@
 #include <android/log.h>
 #endif
 
-#if __APPLE__
-extern "C" {
-// Cannot import the syslog.h header directly because of macro collision
-extern void syslog(int, const char*, ...);
-}
-#endif
-
 using tonic::LogIfError;
 using tonic::ToDart;
 
@@ -146,18 +139,18 @@ void Logger_PrintString(Dart_NativeArguments args) {
   if (Dart_IsError(result)) {
     Dart_PropagateError(result);
   } else {
-    // Uses fwrite to support printing NUL bytes.
-    fwrite(chars, 1, length, stdout);
-    fputs("\n", stdout);
-    fflush(stdout);
 #if defined(OS_ANDROID)
-    // In addition to writing to the stdout, write to the logcat so that the
-    // message is discoverable when running on an unrooted device.
+    // Write to the logcat on Android.
     const char* tag = Settings::Get().log_tag.c_str();
     __android_log_print(ANDROID_LOG_INFO, tag, "%.*s", (int)length,
                         chars);
 #elif __APPLE__
-    syslog(1 /* LOG_ALERT */, "%.*s", (int)length, chars);
+    // Write directly to stdout on iOS, which is redirected to ASL via
+    // RedirectIOConnectionsToSyslog in platform_mac.mm.
+    // TODO(cbracken) replace with dedicated (non-stdout) logging.
+    fwrite(chars, 1, length, stdout);
+    fputs("\n", stdout);
+    fflush(stdout);
 #endif
   }
   if (dart::bin::ShouldCaptureStdout()) {

flutter/engine

commit a571f64266425bcbf6dd83b0479c7960c0a82c2f

Log to stdout on all platforms except Android
Fixes a Fuchsia breakage introduced in
dcfcc9259d4f4321e8daea24f1778694a8dca98a.

lib/ui/dart_runtime_hooks.cc
@@ -143,9 +143,9 @@ void Logger_PrintString(Dart_NativeArguments args) {
     // Write to the logcat on Android.
     const char* tag = Settings::Get().log_tag.c_str();
     __android_log_print(ANDROID_LOG_INFO, tag, "%.*s", (int)length, chars);
-#elif defined(__APPLE__) || defined(OS_LINUX)
-    // On iOS and in flutter_tester (on both macOS and Linux, write directly to
-    // stdout. On iOS, this is redirected to ASL via
+#else
+    // On Fuchsia, iOS and in flutter_tester (on both macOS and Linux, write
+    // directly to stdout. On iOS, this is redirected to ASL via
     // RedirectIOConnectionsToSyslog in platform_mac.mm.
     // TODO(cbracken) replace with dedicated (non-stdout) logging on iOS.
     fwrite(chars, 1, length, stdout);