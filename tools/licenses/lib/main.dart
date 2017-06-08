6bfc6e04f23af5fbde39e5ffc7f1ba0ca3d43202
Todd Volkert <tvolkert@google.com>
2017-06-08 16:15:48 -0700

    Update license script

--- tools/licenses/lib/main.dart
+++ tools/licenses/lib/main.dart
@@ -971,7 +971,7 @@ class RepositoryDirectory extends RepositoryEntry implements LicenseSource {
 
   /// Searches the current and all parent directories (up to the license root)
   /// for a license of the specified type.
-  License _nearestAncestorLicenseWithType(type) {
+  License _nearestAncestorLicenseWithType(LicenseType type) {
     License result = _localLicenseWithType(type);
     if (result != null)
       return result;
@@ -1180,7 +1180,7 @@ class RepositoryDirectory extends RepositoryEntry implements LicenseSource {
     }
   }
 
-  Stream<List<int>> _signatureStream(List files) async* {
+  Stream<List<int>> _signatureStream(List<RepositoryLicensedFile> files) async* {
     for (RepositoryLicensedFile file in files) {
       yield file.io.fullName.codeUnits;
       yield file.io.readBytes();
@@ -1190,7 +1190,7 @@ class RepositoryDirectory extends RepositoryEntry implements LicenseSource {
   /// Compute a signature representing a hash of all the licensed files within
   /// this directory tree.
   Future<String> get signature async {
-    List allFiles = _signatureFiles.toList();
+    List<RepositoryLicensedFile> allFiles = _signatureFiles.toList();
     allFiles.sort((RepositoryLicensedFile a, RepositoryLicensedFile b) =>
         a.io.fullName.compareTo(b.io.fullName));
     crypto.Digest digest = await crypto.md5.bind(_signatureStream(allFiles)).single;
@@ -2079,6 +2079,8 @@ class RepositoryFlutterDirectory extends RepositoryDirectory {
   RepositoryDirectory createSubdirectory(fs.Directory entry) {
     if (entry.name == 'sky')
       return new RepositoryExcludeSubpathDirectory(this, entry, const <String>['packages', 'sky_engine', 'LICENSE']); // that's the output of this script!
+    if (entry.name == 'shell')
+      return new RepositoryExcludeSubpathDirectory(this, entry, const <String>['platform', 'android', 'lib', 'android-support-v4.jar']); // not bundled or statically linked.
     return super.createSubdirectory(entry);
   }
 }
@@ -2250,7 +2252,7 @@ Future<Null> main(List<String> arguments) async {
         Progress progress = new Progress(component.fileCount);
 
         system.File outFile = new system.File(
-            path.join(argResults['out'], 'licenses_${component.io.name}'));
+            path.join(argResults['out'], 'licenses_${component.name}'));
         system.IOSink sink = outFile.openWrite();
         if (signature != null)
           sink.writeln('Signature: $signature\n');
@@ -2266,7 +2268,7 @@ Future<Null> main(List<String> arguments) async {
           // contain any state left over from previous components.
           clearLicenseRegistry();
           componentRoot = new RepositoryRoot(rootDirectory).subdirectories.firstWhere(
-            (RepositoryDirectory dir) => dir.io.name == component.io.name
+            (RepositoryDirectory dir) => dir.name == component.name
           );
         }
         List<License> licenses = new Set<License>.from(
