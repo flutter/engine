5c9eb5f340e38e0ec4cbf14595c28d871c86b1f0
Chris Bracken <chris@bracken.jp>
2017-06-20 09:41:19 -0700

    Revert "Remove ParagraphConstriants (#3796)"
    This reverts commit 8ccf767ff71c781588fb1e79cf3369604099a7cc.

--- lib/ui/text/paragraph_impl_txt.cc
+++ lib/ui/text/paragraph_impl_txt.cc
@@ -8,6 +8,7 @@
 #include "flutter/lib/ui/text/paragraph.h"
 #include "flutter/lib/ui/text/paragraph_impl.h"
 #include "lib/ftl/tasks/task_runner.h"
+#include "lib/txt/src/paragraph_constraints.h"
 
 namespace blink {
 
@@ -46,7 +47,7 @@ bool ParagraphImplTxt::didExceedMaxLines() {
 
 void ParagraphImplTxt::layout(double width) {
   m_width = width;
-  m_paragraph->Layout(width);
+  m_paragraph->Layout(txt::ParagraphConstraints{width});
 }
 
 void ParagraphImplTxt::paint(Canvas* canvas, double x, double y) {
