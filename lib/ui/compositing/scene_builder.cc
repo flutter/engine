8725b57b70389276da3f9eda1b1d3bbdce762caf
Mikkel Ravn <mravn@google.com>
2017-10-02 13:17:29 +0200

    Use std::make_unique

--- lib/ui/compositing/scene_builder.cc
+++ lib/ui/compositing/scene_builder.cc
@@ -233,8 +233,7 @@ void SceneBuilder::addPlatformSurface(double dx,
   if (!m_currentLayer)
     return;
 
-  std::unique_ptr<flow::PlatformSurfaceLayer> layer(
-      new flow::PlatformSurfaceLayer());
+  auto layer = std::make_unique<flow::PlatformSurfaceLayer>();
   layer->set_offset(SkPoint::Make(dx, dy));
   layer->set_size(SkSize::Make(width, height));
   layer->set_surface_id(surfaceId);
