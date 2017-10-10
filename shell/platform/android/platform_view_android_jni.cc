0d69069ad36b9f5456a856c81d3b07b0e234ebf3
Mikkel Ravn <mravn@google.com>
2017-10-10 12:26:49 +0200

    Fix formatting

--- shell/platform/android/platform_view_android_jni.cc
+++ shell/platform/android/platform_view_android_jni.cc
@@ -59,9 +59,7 @@ void FlutterViewOnFirstFrame(JNIEnv* env, jobject obj) {
 }
 
 static jmethodID g_attach_to_gl_context_method = nullptr;
-void SurfaceTextureAttachToGLContext(JNIEnv* env,
-                                     jobject obj,
-                                     jint textureId) {
+void SurfaceTextureAttachToGLContext(JNIEnv* env, jobject obj, jint textureId) {
   ASSERT_IS_GPU_THREAD;
   env->CallVoidMethod(obj, g_attach_to_gl_context_method, textureId);
   FXL_CHECK(env->ExceptionCheck() == JNI_FALSE);
