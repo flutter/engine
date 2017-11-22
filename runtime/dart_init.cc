0207ddfa846dd8d8671ba5b7737019f55ec40bd8
Sarah Zakarias <zarah@google.com>
2017-11-22 10:44:11 +0100

    Read platform kernel blob from asset directory instead of FLX

--- runtime/dart_init.cc
+++ runtime/dart_init.cc
@@ -580,9 +580,10 @@ void InitDartVM(const uint8_t* vm_snapshot_data,
 #endif
 
   if (!bundle_path.empty()) {
-    auto zip_asset_store = fxl::MakeRefCounted<ZipAssetStore>(
-        GetUnzipperProviderForPath(std::move(bundle_path)));
-    zip_asset_store->GetAsBuffer(kPlatformKernelAssetKey, &platform_data);
+    std::unique_ptr<DirectoryAssetBundle> directory_asset_bundle =
+        std::make_unique<DirectoryAssetBundle>(bundle_path);
+    directory_asset_bundle->GetAsBuffer(kPlatformKernelAssetKey,
+                                        &platform_data);
     if (!platform_data.empty()) {
       kernel_platform = Dart_ReadKernelBinary(
           platform_data.data(), platform_data.size(), ReleaseFetchedBytes);
