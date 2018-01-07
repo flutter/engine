flutter/engine

commit 65f144f7b2cf9d2e91fdb9f82804004e920c4a68

Add unwrap to JSONUtil (#4491)
We have a use for this for the internal messaging plugin. Instead of rolling our own, it made sense to add it here especially since it has the mirror functionality (wrap).

shell/platform/android/io/flutter/plugin/common/JSONUtil.java
@@ -1,13 +1,59 @@
 package io.flutter.plugin.common;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class JSONUtil {
     private JSONUtil() {
+    }
+    
+    /**
+     * Convert the Json java representation to Java objects. Particularly used for converting
+     * JSONArray and JSONObject to Lists and Maps.
+     */
+    public static Object unwrap(Object o) {
+        if (JSONObject.NULL.equals(o) || o == null) {
+            return null;
+        }
+        if (o instanceof Boolean
+            || o instanceof Byte
+            || o instanceof Character
+            || o instanceof Double
+            || o instanceof Float
+            || o instanceof Integer
+            || o instanceof Long
+            || o instanceof Short
+            || o instanceof String) {
+            return o;
+        }
+        try {
+            if (o instanceof JSONArray) {
+                List<Object> list = new ArrayList<>();
+                JSONArray array = (JSONArray) o;
+                for (int i = 0; i < array.length(); i++) {
+                    list.add(unwrap(array.get(i)));
+                }
+                return list;
+            }
+            if (o instanceof JSONObject) {
+                Map<String, Object> map = new HashMap<>();
+                JSONObject jsonObject = (JSONObject) o;
+                Iterator<String> keyIterator = jsonObject.keys();
+                while (keyIterator.hasNext()) {
+                    String key = keyIterator.next();
+                    map.put(key, unwrap(jsonObject.get(key)));
+                }
+                return map;
+            }
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 
     /**

commit e358ef178d508c1368222fa77e6571db436a4abc

Fix build break caused by https://github.com/flutter/engine/pull/4491

shell/platform/android/io/flutter/plugin/common/JSONUtil.java
@@ -3,6 +3,7 @@ package io.flutter.plugin.common;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;