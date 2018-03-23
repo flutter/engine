7adbd70490fc2b66bd6f07a8fea4d40323369a66
Jonah Williams <jonahwilliams@google.com>
2018-03-23 09:43:56 -0700

    remove engine changes

--- shell/common/engine.cc
+++ shell/common/engine.cc
@@ -472,7 +472,8 @@ void Engine::DispatchPlatformMessage(
 bool Engine::HandleLifecyclePlatformMessage(blink::PlatformMessage* message) {
   const auto& data = message->data();
   std::string state(reinterpret_cast<const char*>(data.data()), data.size());
-  if (state == "AppLifecycleState.suspending") {
+  if (state == "AppLifecycleState.paused" ||
+      state == "AppLifecycleState.suspending") {
     activity_running_ = false;
     StopAnimator();
   } else if (state == "AppLifecycleState.resumed" ||
