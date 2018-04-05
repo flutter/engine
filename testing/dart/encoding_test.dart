e8a88202fc3d4cc687450bb7fb4afc792616747d
Majid Valipour <majidvp@chromium.org>
2018-04-04 22:38:19 -0400

    Make EncodingFormat a class

--- testing/dart/encoding_test.dart
+++ testing/dart/encoding_test.dart
@@ -12,27 +12,6 @@ import 'package:path/path.dart' as path;
 
 void main() {
 
-  Image createSquareTestImage() {
-    PictureRecorder recorder = new PictureRecorder();
-    Canvas canvas = new Canvas(recorder, new Rect.fromLTWH(0.0, 0.0, 10.0, 10.0));
-
-    var black = new Paint()
-      ..strokeWidth = 1.0
-      ..color = const Color.fromRGBO(0, 0, 0, 1.0);
-    var green = new Paint()
-      ..strokeWidth = 1.0
-      ..color = const Color.fromRGBO(0, 255, 0, 1.0);
-
-    canvas.drawRect(new Rect.fromLTWH(0.0, 0.0, 10.0, 10.0), black);
-    canvas.drawRect(new Rect.fromLTWH(2.0, 2.0, 6.0, 6.0), green);
-    return recorder.endRecording().toImage(10, 10);
-  }
-
-  Uint8List readFile(fileName) {
-    final file = new File(path.join('flutter', 'testing', 'resources', fileName));
-    return file.readAsBytesSync();
-  }
-
   final Image testImage = createSquareTestImage();
 
   test('Encode with default arguments', () async {
@@ -42,20 +21,41 @@ void main() {
   });
 
   test('Encode JPEG', () async {
-    Uint8List data = await testImage.toByteData(format: EncodeFormat.JPEG, quality:80);
+    Uint8List data = await testImage.toByteData(format: new EncodingFormat.jpeg(quality:80));
     Uint8List expected = readFile('square-80.jpg');
     expect(data, expected);
   });
 
   test('Encode PNG', () async {
-    Uint8List data = await testImage.toByteData(format: EncodeFormat.PNG, quality: 80);
-    Uint8List expected = readFile('square-80.png');
+    Uint8List data = await testImage.toByteData(format: new EncodingFormat.png());
+    Uint8List expected = readFile('square.png');
     expect(data, expected);
   });
 
   test('Encode WEBP', () async {
-    Uint8List data = await testImage.toByteData(format: EncodeFormat.WEBP, quality: 80);
+    Uint8List data = await testImage.toByteData(format: new EncodingFormat.webp(quality: 80));
     Uint8List expected = readFile('square-80.webp');
     expect(data, expected);
   });
+}
+
+Image createSquareTestImage() {
+  PictureRecorder recorder = new PictureRecorder();
+  Canvas canvas = new Canvas(recorder, new Rect.fromLTWH(0.0, 0.0, 10.0, 10.0));
+
+  Paint black = new Paint()
+    ..strokeWidth = 1.0
+    ..color = const Color.fromRGBO(0, 0, 0, 1.0);
+  Paint green = new Paint()
+    ..strokeWidth = 1.0
+    ..color = const Color.fromRGBO(0, 255, 0, 1.0);
+
+  canvas.drawRect(new Rect.fromLTWH(0.0, 0.0, 10.0, 10.0), black);
+  canvas.drawRect(new Rect.fromLTWH(2.0, 2.0, 6.0, 6.0), green);
+  return recorder.endRecording().toImage(10, 10);
+}
+
+Uint8List readFile(fileName) {
+  final file = new File(path.join('flutter', 'testing', 'resources', fileName));
+  return file.readAsBytesSync();
 }
