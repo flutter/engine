33e1b59752f2c87656cd86413829725f7fe6e22f
Ben Konyi <bkonyi@google.com>
2018-05-30 17:22:35 -0700

    Addressed comments.

--- lib/ui/isolate_name_server.dart
+++ lib/ui/isolate_name_server.dart
@@ -7,27 +7,38 @@ part of dart.ui;
 abstract class IsolateNameServer {
   // Looks up the [SendPort] associated with a given name. Returns null
   // if the name does not exist.
-  static SendPort lookupPortByName(String name) =>
-      _lookupPortByNameNative(name);
+  static SendPort lookupPortByName(String name) {
+    if (name == null) {
+      throw new ArgumentError("'name' cannot be null.");
+    }
+    return _lookupPortByName(name);
+  }
 
   // Registers a SendPort with a given name. Returns true if registration is
   // successful, false if the name entry already exists.
   static bool registerPortWithName(SendPort port, String name) {
+    if (name == null) {
+      throw new ArgumentError("'name' cannot be null.");
+    }
     if (port == null) {
       throw new ArgumentError("'port' cannot be null.");
     }
-    return _registerPortWithNameNative(port, name);
+    return _registerPortWithName(port, name);
   }
 
   // Removes a name to SendPort mapping given a name. Returns true if the
   // mapping was successfully removed, false if the mapping does not exist.
-  static bool removePortNameMapping(String name) =>
-      _removePortNameMappingNative(name);
+  static bool removePortNameMapping(String name) {
+    if (name == null) {
+      throw new ArgumentError("'name' cannot be null.");
+    }
+    return _removePortNameMapping(name);
+  }
 
-  static SendPort _lookupPortByNameNative(String name)
+  static SendPort _lookupPortByName(String name)
       native 'IsolateNameServerNatives_LookupPortByName';
-  static bool _registerPortWithNameNative(SendPort port, String name)
+  static bool _registerPortWithName(SendPort port, String name)
       native 'IsolateNameServerNatives_RegisterPortWithName';
-  static bool _removePortNameMappingNative(String name)
+  static bool _removePortNameMapping(String name)
       native 'IsolateNameServerNatives_RemovePortNameMapping';
 }
