flutter/engine

commit 52b9c36447f489e1b818ca24698889b5851cc07a

Support running bundles from zip file. (#5473)
Support running bundles from zip file.

shell/platform/android/platform_view_android_jni.cc
@@ -9,6 +9,7 @@
 #include <utility>
 
 #include "flutter/assets/directory_asset_bundle.h"
+#include "flutter/assets/zip_asset_store.h"
 #include "flutter/common/settings.h"
 #include "flutter/fml/file.h"
 #include "flutter/fml/platform/android/jni_util.h"
@@ -207,9 +208,16 @@ static void RunBundleAndSnapshot(
 
   if (bundlepath.size() > 0) {
     // If we got a bundle path, attempt to use that as a directory asset
-    // bundle.
-    asset_manager->PushBack(std::make_unique<blink::DirectoryAssetBundle>(
-        fml::OpenFile(bundlepath.c_str(), fml::OpenPermission::kRead, true)));
+    // bundle or a zip asset bundle.
+    const auto file_ext_index = bundlepath.rfind(".");
+    if (bundlepath.substr(file_ext_index) == ".zip") {
+      asset_manager->PushBack(std::make_unique<blink::ZipAssetStore>(
+          bundlepath));
+    } else {
+      asset_manager->PushBack(std::make_unique<blink::DirectoryAssetBundle>(
+          fml::OpenFile(bundlepath.c_str(), fml::OpenPermission::kRead, true),
+          bundlepath));
+    }
 
     // Use the last path component of the bundle path to determine the
     // directory in the APK assets.

assets/directory_asset_bundle.h
@@ -22,8 +22,6 @@ class DirectoryAssetBundle : public AssetResolver {
   const fml::UniqueFD descriptor_;
   bool is_valid_ = false;
 
-  std::string GetPathForAsset(const std::string& asset_name) const;
-
   // |blink::AssetResolver|
   bool IsValid() const override;
 

commit a916286fc850ea1bd2867e747d6c26d00c1e3000

Fix compile error introduced in #5473.

shell/platform/android/platform_view_android_jni.cc
@@ -215,8 +215,7 @@ static void RunBundleAndSnapshot(
           bundlepath));
     } else {
       asset_manager->PushBack(std::make_unique<blink::DirectoryAssetBundle>(
-          fml::OpenFile(bundlepath.c_str(), fml::OpenPermission::kRead, true),
-          bundlepath));
+          fml::OpenFile(bundlepath.c_str(), fml::OpenPermission::kRead, true)));
     }
 
     // Use the last path component of the bundle path to determine the