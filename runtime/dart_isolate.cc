flutter/engine

commit 0a5efed6d6258057e4ff4875d45db35ec7b5aedb

Reapply "Fix Dart 2 reload when running from a snapshot instead of platform.dill." (#5830)
Without this flag, the VM will attempt to interpret a kernel file as source code.

Affects Fuchsia (FL-71) and code-push.

runtime/dart_isolate.cc
@@ -719,6 +719,20 @@ DartIsolate::CreateDartVMAndEmbedderObjectPair(
             (*raw_embedder_isolate)->child_isolate_preparer_));
   }
 
+  // TODO(rmacnak): This flag setting business preserves a side effect of using
+  // Dart_CreateIsolateFromKernel. It should be removed when some of the
+  // internal logic in reload no longer uses this flag.
+  Dart_IsolateFlags nonnull_flags;
+  if (flags == nullptr) {
+    Dart_IsolateFlagsInitialize(&nonnull_flags);
+    flags = &nonnull_flags;
+  }
+  bool dart2 = (vm->GetPlatformKernel().GetSize() > 0) ||
+               Dart_IsDart2Snapshot(embedder_isolate->GetIsolateSnapshot()
+                                        ->GetData()
+                                        ->GetSnapshotPointer());
+  flags->use_dart_frontend = dart2;
+
   // Create the Dart VM isolate and give it the embedder object as the baton.
   Dart_Isolate isolate =
       (vm->GetPlatformKernel().GetSize() > 0)

commit ee58e76c7a4caa98748a428152cde48c20be65b1

Fix build break caused by https://github.com/flutter/engine/pull/5830

runtime/dart_isolate.cc
@@ -728,7 +728,8 @@ DartIsolate::CreateDartVMAndEmbedderObjectPair(
     flags = &nonnull_flags;
   }
   bool dart2 = (vm->GetPlatformKernel().GetSize() > 0) ||
-               Dart_IsDart2Snapshot(embedder_isolate->GetIsolateSnapshot()
+               Dart_IsDart2Snapshot((*embedder_isolate)
+                                        ->GetIsolateSnapshot()
                                         ->GetData()
                                         ->GetSnapshotPointer());
   flags->use_dart_frontend = dart2;