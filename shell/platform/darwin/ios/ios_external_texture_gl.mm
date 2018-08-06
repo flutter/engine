14fdd01a9547c4af6ddde6f54c64e702cb978b2a
Amir Hardon <amirha@google.com>
2018-08-06 15:03:19 -0700

    support freeze texture on iOS

--- shell/platform/darwin/ios/ios_external_texture_gl.mm
+++ shell/platform/darwin/ios/ios_external_texture_gl.mm
@@ -37,18 +37,20 @@ void IOSExternalTextureGL::Paint(SkCanvas& canvas, const SkRect& bounds, bool fr
     }
   }
   fml::CFRef<CVPixelBufferRef> bufferRef;
-  bufferRef.Reset([external_texture_ copyPixelBuffer]);
-  if (bufferRef != nullptr) {
-    CVOpenGLESTextureRef texture;
-    CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
-        kCFAllocatorDefault, cache_ref_, bufferRef, nullptr, GL_TEXTURE_2D, GL_RGBA,
-        static_cast<int>(CVPixelBufferGetWidth(bufferRef)),
-        static_cast<int>(CVPixelBufferGetHeight(bufferRef)), GL_BGRA, GL_UNSIGNED_BYTE, 0,
-        &texture);
-    texture_ref_.Reset(texture);
-    if (err != noErr) {
-      FML_LOG(WARNING) << "Could not create texture from pixel buffer: " << err;
-      return;
+  if (!freeze) {
+    bufferRef.Reset([external_texture_ copyPixelBuffer]);
+    if (bufferRef != nullptr) {
+      CVOpenGLESTextureRef texture;
+      CVReturn err = CVOpenGLESTextureCacheCreateTextureFromImage(
+          kCFAllocatorDefault, cache_ref_, bufferRef, nullptr, GL_TEXTURE_2D, GL_RGBA,
+          static_cast<int>(CVPixelBufferGetWidth(bufferRef)),
+          static_cast<int>(CVPixelBufferGetHeight(bufferRef)), GL_BGRA, GL_UNSIGNED_BYTE, 0,
+          &texture);
+      texture_ref_.Reset(texture);
+      if (err != noErr) {
+        FML_LOG(WARNING) << "Could not create texture from pixel buffer: " << err;
+        return;
+      }
     }
   }
   if (!texture_ref_) {
