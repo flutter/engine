18c5ca09ac0a02871d90f1411ac995b575cc5663
Amir Hardon <amirha@google.com>
2018-08-07 12:44:06 -0700

    review comments followup

--- lib/ui/compositing.dart
+++ lib/ui/compositing.dart
@@ -236,7 +236,13 @@ class SceneBuilder extends NativeFieldWrapperClass2 {
   ///
   /// The texture is scaled to the given size and rasterized at the given offset.
   ///
-  /// If `freeze` is true new frames will not be consumed from the texture.
+  /// If `freeze` is true the texture that is added to the scene will not
+  /// be updated with new frames. `freeze` is used when resizing an embedded
+  /// Android view: When resizing an Android view there is a short period during
+  /// which the framework cannot tell if the newest texture frame has the
+  /// previous or new size, to workaround this the framework "freezes" the
+  /// texture just before resizing the Android view and unfreezes it when it is
+  /// certain that a frame with the new size is ready.
   void addTexture(int textureId, { Offset offset: Offset.zero, double width: 0.0, double height: 0.0 , bool freeze: false}) {
     assert(offset != null, 'Offset argument was null');
     _addTexture(offset.dx, offset.dy, width, height, textureId, freeze);
