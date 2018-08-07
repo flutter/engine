04e06e28e55780437508fac21c278ba97237df5e
Todd Volkert <tvolkert@google.com>
2018-08-07 12:58:09 -0700

    Don't implicitly fall through in switch statement

--- shell/platform/android/io/flutter/view/AccessibilityBridge.java
+++ shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -824,6 +824,7 @@ class AccessibilityBridge
                         ROOT_NODE_ID, AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED);
                 e.getText().add((String) data.get("message"));
                 sendAccessibilityEvent(e);
+                break;
             }
             // Requires that the node id provided corresponds to a live region, or TalkBack will
             // ignore the event. The event will cause talkback to read out the new label even
@@ -834,6 +835,7 @@ class AccessibilityBridge
                     return;
                 }
                 sendAccessibilityEvent(nodeId, AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED);
+                break;
             }
         }
     }
