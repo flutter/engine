flutter/engine

commit a26b3ad7b28ac5d35a15b7fc654792121ce22809

Reland "Updated background execution implementation for Android" (#5954)
* Reland "Updated background execution implementation for Android" w/ JNI fixes for merge breakages

This reverts commit 5442c0a7da385ac5ebfe7ec66126ee593d84e2a2.

shell/platform/android/io/flutter/view/FlutterNativeView.java
@@ -29,9 +29,13 @@ public class FlutterNativeView implements BinaryMessenger {
     private boolean applicationIsRunning;
 
     public FlutterNativeView(Context context) {
+        this(context, false);
+    }
+
+    public FlutterNativeView(Context context, boolean isBackgroundView) {
         mContext = context;
         mPluginRegistry = new FlutterPluginRegistry(this, context);
-        attach(this);
+        attach(this, isBackgroundView);
         assertAttached();
         mMessageHandlers = new HashMap<>();
     }
@@ -68,22 +72,44 @@ public class FlutterNativeView implements BinaryMessenger {
     }
 
     public void assertAttached() {
-        if (!isAttached())
-            throw new AssertionError("Platform view is not attached");
+        if (!isAttached()) throw new AssertionError("Platform view is not attached");
     }
 
-    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint, boolean reuseRuntimeController) {
+    public void runFromBundle(FlutterRunArguments args) {
+        if (args.bundlePath == null) {
+          throw new AssertionError("A bundlePath must be specified");
+        } else if (args.entrypoint == null) {
+          throw new AssertionError("An entrypoint must be specified");
+        }
+      runFromBundleInternal(args.bundlePath, args.entrypoint,
+          args.libraryPath, null);
+    }
+
+    /**
+     * @deprecated
+     * Please use runFromBundle with `FlutterRunArguments`. Parameters
+     * `snapshotOverride` and `reuseRuntimeController` have no effect.
+     */
+    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint,
+            boolean reuseRuntimeController) {
+        runFromBundleInternal(bundlePath, entrypoint, null, null);
+    }
+
+    private void runFromBundleInternal(String bundlePath, String entrypoint,
+        String libraryPath, String snapshotOverride) {
         assertAttached();
         if (applicationIsRunning)
-            throw new AssertionError("This Flutter engine instance is already running an application");
-
-        nativeRunBundleAndSnapshot(mNativePlatformView, bundlePath, snapshotOverride, entrypoint, reuseRuntimeController, mContext.getResources().getAssets());
+            throw new AssertionError(
+                    "This Flutter engine instance is already running an application");
+        nativeRunBundleAndSnapshotFromLibrary(mNativePlatformView, bundlePath,
+            snapshotOverride, entrypoint, libraryPath,
+            mContext.getResources().getAssets());
 
         applicationIsRunning = true;
     }
 
     public boolean isApplicationRunning() {
-      return applicationIsRunning;
+        return applicationIsRunning;
     }
 
     public static String getObservatoryUri() {
@@ -92,7 +118,7 @@ public class FlutterNativeView implements BinaryMessenger {
 
     @Override
     public void send(String channel, ByteBuffer message) {
-      send(channel, message, null);
+        send(channel, message, null);
     }
 
     @Override
@@ -110,8 +136,8 @@ public class FlutterNativeView implements BinaryMessenger {
         if (message == null) {
             nativeDispatchEmptyPlatformMessage(mNativePlatformView, channel, replyId);
         } else {
-            nativeDispatchPlatformMessage(mNativePlatformView, channel, message,
-                message.position(), replyId);
+            nativeDispatchPlatformMessage(
+                    mNativePlatformView, channel, message, message.position(), replyId);
         }
     }
 
@@ -124,8 +150,8 @@ public class FlutterNativeView implements BinaryMessenger {
         }
     }
 
-    private void attach(FlutterNativeView view) {
-        mNativePlatformView = nativeAttach(view);
+    private void attach(FlutterNativeView view, boolean isBackgroundView) {
+        mNativePlatformView = nativeAttach(view, isBackgroundView);
     }
 
     // Called by native to send us a platform message.
@@ -135,27 +161,28 @@ public class FlutterNativeView implements BinaryMessenger {
         if (handler != null) {
             try {
                 final ByteBuffer buffer = (message == null ? null : ByteBuffer.wrap(message));
-                handler.onMessage(buffer,
-                    new BinaryReply() {
-                        private final AtomicBoolean done = new AtomicBoolean(false);
-                        @Override
-                        public void reply(ByteBuffer reply) {
-                            if (!isAttached()) {
-                                Log.d(TAG, "handlePlatformMessage replying to a detached view, channel=" + channel);
-                                return;
-                            }
-                            if (done.getAndSet(true)) {
-                                throw new IllegalStateException("Reply already submitted");
-                            }
-                            if (reply == null) {
-                                nativeInvokePlatformMessageEmptyResponseCallback(mNativePlatformView,
-                                    replyId);
-                            } else {
-                                nativeInvokePlatformMessageResponseCallback(mNativePlatformView,
-                                    replyId, reply, reply.position());
-                            }
+                handler.onMessage(buffer, new BinaryReply() {
+                    private final AtomicBoolean done = new AtomicBoolean(false);
+                    @Override
+                    public void reply(ByteBuffer reply) {
+                        if (!isAttached()) {
+                            Log.d(TAG,
+                                    "handlePlatformMessage replying to a detached view, channel="
+                                            + channel);
+                            return;
                         }
-                    });
+                        if (done.getAndSet(true)) {
+                            throw new IllegalStateException("Reply already submitted");
+                        }
+                        if (reply == null) {
+                            nativeInvokePlatformMessageEmptyResponseCallback(
+                                    mNativePlatformView, replyId);
+                        } else {
+                            nativeInvokePlatformMessageResponseCallback(
+                                    mNativePlatformView, replyId, reply, reply.position());
+                        }
+                    }
+                });
             } catch (Exception ex) {
                 Log.e(TAG, "Uncaught exception in binary message listener", ex);
                 nativeInvokePlatformMessageEmptyResponseCallback(mNativePlatformView, replyId);
@@ -179,8 +206,7 @@ public class FlutterNativeView implements BinaryMessenger {
 
     // Called by native to update the semantics/accessibility tree.
     private void updateSemantics(ByteBuffer buffer, String[] strings) {
-        if (mFlutterView == null)
-            return;
+        if (mFlutterView == null) return;
         mFlutterView.updateSemantics(buffer, strings);
     }
 
@@ -193,8 +219,7 @@ public class FlutterNativeView implements BinaryMessenger {
 
     // Called by native to notify first Flutter frame rendered.
     private void onFirstFrame() {
-        if (mFlutterView == null)
-            return;
+        if (mFlutterView == null) return;
         mFlutterView.onFirstFrame();
     }
 
@@ -206,32 +231,30 @@ public class FlutterNativeView implements BinaryMessenger {
         mPluginRegistry.onPreEngineRestart();
     }
 
-    private static native long nativeAttach(FlutterNativeView view);
+    private static native long nativeAttach(FlutterNativeView view, boolean isBackgroundView);
     private static native void nativeDestroy(long nativePlatformViewAndroid);
     private static native void nativeDetach(long nativePlatformViewAndroid);
 
-    private static native void nativeRunBundleAndSnapshot(long nativePlatformViewAndroid,
-        String bundlePath,
-        String snapshotOverride,
-        String entrypoint,
-        boolean reuseRuntimeController,
-        AssetManager manager);
+    private static native void nativeRunBundleAndSnapshotFromLibrary(
+            long nativePlatformViewAndroid, String bundlePath,
+            String snapshotOverride, String entrypoint, String libraryUrl,
+            AssetManager manager);
 
     private static native String nativeGetObservatoryUri();
 
     // Send an empty platform message to Dart.
-    private static native void nativeDispatchEmptyPlatformMessage(long nativePlatformViewAndroid,
-        String channel, int responseId);
+    private static native void nativeDispatchEmptyPlatformMessage(
+            long nativePlatformViewAndroid, String channel, int responseId);
 
     // Send a data-carrying platform message to Dart.
     private static native void nativeDispatchPlatformMessage(long nativePlatformViewAndroid,
-        String channel, ByteBuffer message, int position, int responseId);
+            String channel, ByteBuffer message, int position, int responseId);
 
     // Send an empty response to a platform message received from Dart.
     private static native void nativeInvokePlatformMessageEmptyResponseCallback(
-        long nativePlatformViewAndroid, int responseId);
+            long nativePlatformViewAndroid, int responseId);
 
     // Send a data-carrying response to a platform message received from Dart.
     private static native void nativeInvokePlatformMessageResponseCallback(
-        long nativePlatformViewAndroid, int responseId, ByteBuffer message, int position);
+            long nativePlatformViewAndroid, int responseId, ByteBuffer message, int position);
 }

shell/platform/android/io/flutter/view/FlutterRunArguments.java
@@ -1,0 +1,16 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package io.flutter.view;
+
+/**
+ * A class containing arguments for entering a FlutterNativeView's isolate for
+ * the first time.
+ */
+public class FlutterRunArguments {
+  public String bundlePath;
+  public String entrypoint;
+  public String libraryPath;
+  public String snapshotOverride;
+}

shell/platform/android/io/flutter/view/FlutterView.java
@@ -602,20 +602,41 @@ public class FlutterView extends SurfaceView
     private void postRun() {
     }
 
+    public void runFromBundle(FlutterRunArguments args) {
+      assertAttached();
+      preRun();
+      mNativeView.runFromBundle(args);
+      postRun();
+    }
+
+    /**
+     * @deprecated
+     * Please use runFromBundle with `FlutterRunArguments`. Parameter
+     * `snapshotOverride` has no effect.
+     */
     public void runFromBundle(String bundlePath, String snapshotOverride) {
         runFromBundle(bundlePath, snapshotOverride, "main", false);
     }
 
+    /**
+     * @deprecated
+     * Please use runFromBundle with `FlutterRunArguments`. Parameter
+     * `snapshotOverride` has no effect.
+     */
     public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint) {
         runFromBundle(bundlePath, snapshotOverride, entrypoint, false);
     }
 
-    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint,
-            boolean reuseRuntimeController) {
-        assertAttached();
-        preRun();
-        mNativeView.runFromBundle(bundlePath, snapshotOverride, entrypoint, reuseRuntimeController);
-        postRun();
+    /**
+     * @deprecated
+     * Please use runFromBundle with `FlutterRunArguments`. Parameters
+     * `snapshotOverride` and `reuseRuntimeController` have no effect.
+     */
+    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint, boolean reuseRuntimeController) {
+        FlutterRunArguments args = new FlutterRunArguments();
+        args.bundlePath = bundlePath;
+        args.entrypoint = entrypoint;
+        runFromBundle(args);
     }
 
     /**

shell/platform/android/platform_view_android_jni.cc
@@ -16,6 +16,7 @@
 #include "flutter/fml/platform/android/jni_util.h"
 #include "flutter/fml/platform/android/jni_weak_ref.h"
 #include "flutter/fml/platform/android/scoped_java_ref.h"
+#include "flutter/lib/ui/plugins/callback_cache.h"
 #include "flutter/runtime/dart_service_isolate.h"
 #include "flutter/shell/common/run_configuration.h"
 #include "flutter/shell/platform/android/android_external_texture_gl.h"
@@ -43,12 +44,27 @@ bool CheckException(JNIEnv* env) {
 
 }  // anonymous namespace
 
+static fml::jni::ScopedJavaGlobalRef<jclass>* g_flutter_callback_info_class =
+    nullptr;
 static fml::jni::ScopedJavaGlobalRef<jclass>* g_flutter_view_class = nullptr;
 static fml::jni::ScopedJavaGlobalRef<jclass>* g_flutter_native_view_class =
     nullptr;
 static fml::jni::ScopedJavaGlobalRef<jclass>* g_surface_texture_class = nullptr;
 
 // Called By Native
+
+static jmethodID g_flutter_callback_info_constructor = nullptr;
+jobject CreateFlutterCallbackInformation(
+    JNIEnv* env,
+    const std::string& callbackName,
+    const std::string& callbackClassName,
+    const std::string& callbackLibraryPath) {
+  return env->NewObject(g_flutter_callback_info_class->obj(),
+                        g_flutter_callback_info_constructor,
+                        env->NewStringUTF(callbackName.c_str()),
+                        env->NewStringUTF(callbackClassName.c_str()),
+                        env->NewStringUTF(callbackLibraryPath.c_str()));
+}
 
 static jmethodID g_handle_platform_message_method = nullptr;
 void FlutterViewHandlePlatformMessage(JNIEnv* env,
@@ -130,10 +146,13 @@ void SurfaceTextureDetachFromGLContext(JNIEnv* env, jobject obj) {
 
 // Called By Java
 
-static jlong Attach(JNIEnv* env, jclass clazz, jobject flutterView) {
+static jlong Attach(JNIEnv* env,
+                    jclass clazz,
+                    jobject flutterView,
+                    jboolean is_background_view) {
   fml::jni::JavaObjectWeakGlobalRef java_object(env, flutterView);
   auto shell_holder = std::make_unique<AndroidShellHolder>(
-      FlutterMain::Get().GetSettings(), java_object);
+      FlutterMain::Get().GetSettings(), java_object, is_background_view);
   if (shell_holder->IsValid()) {
     return reinterpret_cast<jlong>(shell_holder.release());
   } else {
@@ -218,15 +237,14 @@ std::unique_ptr<IsolateConfiguration> CreateIsolateConfiguration(
   return IsolateConfiguration::CreateForAppSnapshot();
 }
 
-static void RunBundleAndSnapshot(
-    JNIEnv* env,
-    jobject jcaller,
-    jlong shell_holder,
-    jstring jbundlepath,
-    jstring jsnapshotOverride,
-    jstring jEntrypoint,
-    jboolean /* reuse runtime controller (unused) */,
-    jobject jAssetManager) {
+static void RunBundleAndSnapshotFromLibrary(JNIEnv* env,
+                                            jobject jcaller,
+                                            jlong shell_holder,
+                                            jstring jbundlepath,
+                                            jstring jsnapshotOverride,
+                                            jstring jEntrypoint,
+                                            jstring jLibraryUrl,
+                                            jobject jAssetManager) {
   auto asset_manager = fml::MakeRefCounted<blink::AssetManager>();
 
   const auto bundlepath = fml::jni::JavaStringToString(env, jbundlepath);
@@ -275,12 +293,28 @@ static void RunBundleAndSnapshot(
 
   {
     auto entrypoint = fml::jni::JavaStringToString(env, jEntrypoint);
-    if (entrypoint.size() > 0) {
+    auto libraryUrl = fml::jni::JavaStringToString(env, jLibraryUrl);
+
+    if ((entrypoint.size() > 0) && (libraryUrl.size() > 0)) {
+      config.SetEntrypointAndLibrary(std::move(entrypoint),
+                                     std::move(libraryUrl));
+    } else if (entrypoint.size() > 0) {
       config.SetEntrypoint(std::move(entrypoint));
     }
   }
 
   ANDROID_SHELL_HOLDER->Launch(std::move(config));
+}
+
+static jobject LookupCallbackInformation(JNIEnv* env,
+                                         /* unused */ jobject,
+                                         jlong handle) {
+  auto cbInfo = blink::DartCallbackCache::GetCallbackInformation(handle);
+  if (cbInfo == nullptr) {
+    return nullptr;
+  }
+  return CreateFlutterCallbackInformation(env, cbInfo->name, cbInfo->class_name,
+                                          cbInfo->library_path);
 }
 
 static void SetViewportMetrics(JNIEnv* env,
@@ -516,6 +550,19 @@ bool PlatformViewAndroid::Register(JNIEnv* env) {
     return false;
   }
 
+  g_flutter_callback_info_class = new fml::jni::ScopedJavaGlobalRef<jclass>(
+      env, env->FindClass("io/flutter/view/FlutterCallbackInformation"));
+  if (g_flutter_callback_info_class->is_null()) {
+    return false;
+  }
+
+  g_flutter_callback_info_constructor = env->GetMethodID(
+      g_flutter_callback_info_class->obj(), "<init>",
+      "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V");
+  if (g_flutter_callback_info_constructor == nullptr) {
+    return false;
+  }
+
   g_flutter_view_class = new fml::jni::ScopedJavaGlobalRef<jclass>(
       env, env->FindClass("io/flutter/view/FlutterView"));
   if (g_flutter_view_class->is_null()) {
@@ -537,7 +584,7 @@ bool PlatformViewAndroid::Register(JNIEnv* env) {
   static const JNINativeMethod native_view_methods[] = {
       {
           .name = "nativeAttach",
-          .signature = "(Lio/flutter/view/FlutterNativeView;)J",
+          .signature = "(Lio/flutter/view/FlutterNativeView;Z)J",
           .fnPtr = reinterpret_cast<void*>(&shell::Attach),
       },
       {
@@ -546,10 +593,12 @@ bool PlatformViewAndroid::Register(JNIEnv* env) {
           .fnPtr = reinterpret_cast<void*>(&shell::Destroy),
       },
       {
-          .name = "nativeRunBundleAndSnapshot",
-          .signature = "(JLjava/lang/String;Ljava/lang/String;Ljava/lang/"
-                       "String;ZLandroid/content/res/AssetManager;)V",
-          .fnPtr = reinterpret_cast<void*>(&shell::RunBundleAndSnapshot),
+          .name = "nativeRunBundleAndSnapshotFromLibrary",
+          .signature =
+              "(JLjava/lang/String;Ljava/lang/String;Ljava/lang/String;"
+              "Ljava/lang/String;Landroid/content/res/AssetManager;)V",
+          .fnPtr =
+              reinterpret_cast<void*>(&shell::RunBundleAndSnapshotFromLibrary),
       },
       {
           .name = "nativeDetach",
@@ -654,6 +703,14 @@ bool PlatformViewAndroid::Register(JNIEnv* env) {
       },
   };
 
+  static const JNINativeMethod callback_info_methods[] = {
+      {
+          .name = "nativeLookupCallbackInformation",
+          .signature = "(J)Lio/flutter/view/FlutterCallbackInformation;",
+          .fnPtr = reinterpret_cast<void*>(&shell::LookupCallbackInformation),
+      },
+  };
+
   if (env->RegisterNatives(g_flutter_native_view_class->obj(),
                            native_view_methods,
                            arraysize(native_view_methods)) != 0) {
@@ -662,6 +719,12 @@ bool PlatformViewAndroid::Register(JNIEnv* env) {
 
   if (env->RegisterNatives(g_flutter_view_class->obj(), view_methods,
                            arraysize(view_methods)) != 0) {
+    return false;
+  }
+
+  if (env->RegisterNatives(g_flutter_callback_info_class->obj(),
+                           callback_info_methods,
+                           arraysize(callback_info_methods)) != 0) {
     return false;
   }
 

ci/licenses_golden/licenses_flutter
@@ -497,6 +497,8 @@ FILE: ../../../flutter/shell/platform/android/io/flutter/plugin/platform/Platfor
 FILE: ../../../flutter/shell/platform/android/io/flutter/plugin/platform/PlatformViewsController.java
 FILE: ../../../flutter/shell/platform/android/io/flutter/plugin/platform/SingleViewPresentation.java
 FILE: ../../../flutter/shell/platform/android/io/flutter/plugin/platform/VirtualDisplayController.java
+FILE: ../../../flutter/shell/platform/android/io/flutter/view/FlutterCallbackInformation.java
+FILE: ../../../flutter/shell/platform/android/io/flutter/view/FlutterRunArguments.java
 FILE: ../../../flutter/shell/platform/darwin/ios/framework/Headers/FlutterCallbackCache.h
 FILE: ../../../flutter/shell/platform/darwin/ios/framework/Headers/FlutterPluginAppLifeCycleDelegate.h
 FILE: ../../../flutter/shell/platform/darwin/ios/framework/Source/FlutterCallbackCache.mm

shell/platform/android/BUILD.gn
@@ -133,8 +133,10 @@ java_library("flutter_shell_java") {
     "io/flutter/util/PathUtils.java",
     "io/flutter/util/Preconditions.java",
     "io/flutter/view/AccessibilityBridge.java",
+    "io/flutter/view/FlutterCallbackInformation.java",
     "io/flutter/view/FlutterMain.java",
     "io/flutter/view/FlutterNativeView.java",
+    "io/flutter/view/FlutterRunArguments.java",
     "io/flutter/view/FlutterView.java",
     "io/flutter/view/ResourceCleaner.java",
     "io/flutter/view/ResourceExtractor.java",

commit fcd548db27e5fdffa8f59e2f74632962e2056c07

Fix codepush breakage caused by #5954.

shell/platform/android/io/flutter/view/FlutterNativeView.java
@@ -81,30 +81,28 @@ public class FlutterNativeView implements BinaryMessenger {
         } else if (args.entrypoint == null) {
           throw new AssertionError("An entrypoint must be specified");
         }
-      runFromBundleInternal(args.bundlePath, args.entrypoint,
-          args.libraryPath, null);
+      runFromBundleInternal(args.bundlePath, args.entrypoint, args.libraryPath, args.defaultPath);
     }
 
     /**
      * @deprecated
-     * Please use runFromBundle with `FlutterRunArguments`. Parameters
-     * `snapshotOverride` and `reuseRuntimeController` have no effect.
+     * Please use runFromBundle with `FlutterRunArguments`.
+     * Parameter `reuseRuntimeController` has no effect.
      */
     @Deprecated
-    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint,
+    public void runFromBundle(String bundlePath, String defaultPath, String entrypoint,
             boolean reuseRuntimeController) {
-        runFromBundleInternal(bundlePath, entrypoint, null, null);
+        runFromBundleInternal(bundlePath, entrypoint, null, defaultPath);
     }
 
     private void runFromBundleInternal(String bundlePath, String entrypoint,
-        String libraryPath, String snapshotOverride) {
+        String libraryPath, String defaultPath) {
         assertAttached();
         if (applicationIsRunning)
             throw new AssertionError(
                     "This Flutter engine instance is already running an application");
         nativeRunBundleAndSnapshotFromLibrary(mNativePlatformView, bundlePath,
-            snapshotOverride, entrypoint, libraryPath,
-            mContext.getResources().getAssets());
+            defaultPath, entrypoint, libraryPath, mContext.getResources().getAssets());
 
         applicationIsRunning = true;
     }
@@ -238,7 +236,7 @@ public class FlutterNativeView implements BinaryMessenger {
 
     private static native void nativeRunBundleAndSnapshotFromLibrary(
             long nativePlatformViewAndroid, String bundlePath,
-            String snapshotOverride, String entrypoint, String libraryUrl,
+            String defaultPath, String entrypoint, String libraryUrl,
             AssetManager manager);
 
     private static native String nativeGetObservatoryUri();

shell/platform/android/io/flutter/view/FlutterRunArguments.java
@@ -12,5 +12,5 @@ public class FlutterRunArguments {
   public String bundlePath;
   public String entrypoint;
   public String libraryPath;
-  public String snapshotOverride;
+  public String defaultPath;
 }

shell/platform/android/io/flutter/view/FlutterView.java
@@ -611,34 +611,33 @@ public class FlutterView extends SurfaceView
 
     /**
      * @deprecated
-     * Please use runFromBundle with `FlutterRunArguments`. Parameter
-     * `snapshotOverride` has no effect.
+     * Please use runFromBundle with `FlutterRunArguments`.
      */
     @Deprecated
-    public void runFromBundle(String bundlePath, String snapshotOverride) {
-        runFromBundle(bundlePath, snapshotOverride, "main", false);
+    public void runFromBundle(String bundlePath, String defaultPath) {
+        runFromBundle(bundlePath, defaultPath, "main", false);
     }
 
     /**
      * @deprecated
-     * Please use runFromBundle with `FlutterRunArguments`. Parameter
-     * `snapshotOverride` has no effect.
+     * Please use runFromBundle with `FlutterRunArguments`.
      */
     @Deprecated
-    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint) {
-        runFromBundle(bundlePath, snapshotOverride, entrypoint, false);
+    public void runFromBundle(String bundlePath, String defaultPath, String entrypoint) {
+        runFromBundle(bundlePath, defaultPath, entrypoint, false);
     }
 
     /**
      * @deprecated
-     * Please use runFromBundle with `FlutterRunArguments`. Parameters
-     * `snapshotOverride` and `reuseRuntimeController` have no effect.
+     * Please use runFromBundle with `FlutterRunArguments`.
+     * Parameter `reuseRuntimeController` has no effect.
      */
     @Deprecated
-    public void runFromBundle(String bundlePath, String snapshotOverride, String entrypoint, boolean reuseRuntimeController) {
+    public void runFromBundle(String bundlePath, String defaultPath, String entrypoint, boolean reuseRuntimeController) {
         FlutterRunArguments args = new FlutterRunArguments();
         args.bundlePath = bundlePath;
         args.entrypoint = entrypoint;
+        args.defaultPath = defaultPath;
         runFromBundle(args);
     }
 

shell/platform/android/platform_view_android_jni.cc
@@ -241,7 +241,7 @@ static void RunBundleAndSnapshotFromLibrary(JNIEnv* env,
                                             jobject jcaller,
                                             jlong shell_holder,
                                             jstring jbundlepath,
-                                            jstring jsnapshotOverride,
+                                            jstring jdefaultPath,
                                             jstring jEntrypoint,
                                             jstring jLibraryUrl,
                                             jobject jAssetManager) {
@@ -275,7 +275,7 @@ static void RunBundleAndSnapshotFromLibrary(JNIEnv* env,
     }
   }
 
-  const auto defaultpath = fml::jni::JavaStringToString(env, jsnapshotOverride);
+  const auto defaultpath = fml::jni::JavaStringToString(env, jdefaultPath);
   if (defaultpath.size() > 0) {
     asset_manager->PushBack(std::make_unique<blink::DirectoryAssetBundle>(
         fml::OpenFile(defaultpath.c_str(), fml::OpenPermission::kRead, true)));