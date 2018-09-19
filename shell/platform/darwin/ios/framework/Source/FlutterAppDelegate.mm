3189d61e9c5bca665ef82da430a3465258fb9948
KyleWong <kang.wang1988@gmail.com>
2018-09-19 13:40:34 +0800

    Format changed files.

--- shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
@@ -134,7 +134,8 @@
 
 - (BOOL)application:(UIApplication*)application
     continueUserActivity:(NSUserActivity*)userActivity
-      restorationHandler:(void(^)(NSArray<id<UIUserActivityRestoring>> * __nullable restorableObjects))restorationHandler {
+      restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>>* __nullable
+                                       restorableObjects))restorationHandler {
   return [_lifeCycleDelegate application:application
                     continueUserActivity:userActivity
                       restorationHandler:restorationHandler];
