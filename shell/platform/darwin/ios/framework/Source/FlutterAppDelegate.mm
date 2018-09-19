213afc256aacbf189395d83f1370a87bc0130cf2
KyleWong <kang.wang1988@gmail.com>
2018-09-19 13:23:41 +0800

    Fix a compilation problem when  using iPhoneOS12.0sdk(Xcode10).

--- shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
@@ -134,7 +134,7 @@
 
 - (BOOL)application:(UIApplication*)application
     continueUserActivity:(NSUserActivity*)userActivity
-      restorationHandler:(void (^)(NSArray*))restorationHandler {
+      restorationHandler:(void(^)(NSArray<id<UIUserActivityRestoring>> * __nullable restorableObjects))restorationHandler {
   return [_lifeCycleDelegate application:application
                     continueUserActivity:userActivity
                       restorationHandler:restorationHandler];
