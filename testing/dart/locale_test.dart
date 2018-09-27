b7b8b07875ca4f67b55e32279a17ffddfb24a7b2
Hugo van der Merwe <hugovdm@gmail.com>
2018-09-27 12:25:38 +0200

    Unit test for safe named constructor.

--- testing/dart/locale_test.dart
+++ testing/dart/locale_test.dart
@@ -7,7 +7,7 @@ import 'dart:ui';
 import 'package:test/test.dart';
 
 void main() {
-  group('Locale', () {
+  group('Locale:', () {
     test('Unnamed constructor', () {
       final Null $null = null;
       expect(const Locale('en').toString(), 'en');
@@ -19,6 +19,33 @@ void main() {
       expect(const Locale('iw').toString(), 'he');
       expect(const Locale('iw', 'DD').toString(), 'he_DE');
       expect(const Locale('iw', 'DD'), const Locale('he', 'DE'));
+    });
+
+    group('Standards compliance, BCP 47 and LDML:', () {
+      test('Safe constructor.', () {
+        expect(Locale.safe('IW').languageCode, 'he',
+            reason: "Case insensitive to input.");
+        expect(Locale.safe('root').languageCode, 'und',
+            reason: "BCP 47 Language Tag Conversion: "
+                "replace 'root' with 'und'.");
+        expect(Locale.safe('ROOT').languageCode, 'und',
+            reason: "BCP 47 Language Tag Conversion: "
+                "replace 'root' with 'und', and case insensitive.");
+
+        expect(Locale.safe('a').languageCode, 'und',
+            reason: "One character language subtags are invalid.");
+        expect(Locale.safe('abcd').languageCode, 'und',
+            reason: "The only valid 4 character language subtag is 'root'.");
+        expect(Locale.safe('abcdefghi').languageCode, 'und',
+            reason: "Language subtags may not be more than 8 characters.");
+
+        expect(Locale.safe('IW').languageCode, 'und',
+            reason: "One character language subtags are invalid.");
+        expect(Locale.safe('en').languageCode, 'und',
+            reason: "The only valid 4 character language subtag is 'root'.");
+        expect(Locale.safe('en').languageCode, 'und',
+            reason: "Language subtags may not be more than 8 characters.");
+      });
     });
   });
 }
