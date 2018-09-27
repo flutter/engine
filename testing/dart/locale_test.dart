ad523358e7fb59cd6a9155fa0dcb433e392ae8d5
Hugo van der Merwe <hugovdm@gmail.com>
2018-09-27 10:04:12 +0200

    Move Locale tests into a group.

--- testing/dart/locale_test.dart
+++ testing/dart/locale_test.dart
@@ -7,16 +7,18 @@ import 'dart:ui';
 import 'package:test/test.dart';
 
 void main() {
-  test('Locale', () {
-    final Null $null = null;
-    expect(const Locale('en').toString(), 'en');
-    expect(const Locale('en'), new Locale('en', $null));
-    expect(const Locale('en').hashCode, new Locale('en', $null).hashCode);
-    expect(const Locale('en'), isNot(new Locale('en', '')));
-    expect(const Locale('en').hashCode, isNot(new Locale('en', '').hashCode));
-    expect(const Locale('en', 'US').toString(), 'en_US');
-    expect(const Locale('iw').toString(), 'he');
-    expect(const Locale('iw', 'DD').toString(), 'he_DE');
-    expect(const Locale('iw', 'DD'), const Locale('he', 'DE'));
+  group('Locale', () {
+    test('Unnamed constructor', () {
+      final Null $null = null;
+      expect(const Locale('en').toString(), 'en');
+      expect(const Locale('en'), new Locale('en', $null));
+      expect(const Locale('en').hashCode, new Locale('en', $null).hashCode);
+      expect(const Locale('en'), isNot(new Locale('en', '')));
+      expect(const Locale('en').hashCode, isNot(new Locale('en', '').hashCode));
+      expect(const Locale('en', 'US').toString(), 'en_US');
+      expect(const Locale('iw').toString(), 'he');
+      expect(const Locale('iw', 'DD').toString(), 'he_DE');
+      expect(const Locale('iw', 'DD'), const Locale('he', 'DE'));
+    });
   });
 }
