b77e607ed643ec2b6db4fdc1767227d6238386dd
福居 <fuju.cjd@alibaba-inc.com>
2018-10-08 20:01:03 +0800

    Missing dealloc method in FlutterEventChannel

--- shell/platform/darwin/ios/framework/Source/FlutterChannels.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterChannels.mm
@@ -256,6 +256,14 @@ NSObject const* FlutterEndOfEventStream = [NSObject new];
   return self;
 }
 
+- (void)dealloc
+{
+    [_name release];
+    [_codec release];
+    [_messenger release];
+    [super dealloc];
+}
+
 - (void)setStreamHandler:(NSObject<FlutterStreamHandler>*)handler {
   if (!handler) {
     [_messenger setMessageHandlerOnChannel:_name binaryMessageHandler:nil];
