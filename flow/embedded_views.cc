734539b7586170cddafe174942c95f31ecd21f04
Michael Goderbauer <goderbauer@google.com>
2018-11-12 15:49:10 -0800

    ++

--- flow/embedded_views.cc
+++ flow/embedded_views.cc
@@ -1,5 +1,8 @@
-#include "flutter/flow/embedded_views.h"
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
+#include "flutter/flow/embedded_views.h"
 
 namespace flow {
 
