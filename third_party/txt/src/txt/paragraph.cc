f1ea883e9e3afe18243d9b0ff5e451be2870d875
GaryQian <garyq@google.com>
2018-12-10 15:13:00 -0800

    Remove TODO

--- third_party/txt/src/txt/paragraph.cc
+++ third_party/txt/src/txt/paragraph.cc
@@ -479,8 +479,8 @@ void Paragraph::Layout(double width, bool force) {
       }
     }
 
-    // Exclude trailing whitespace from right-justified lines so the last
-    // visible character in the line will be flush with the right margin.
+    // Exclude trailing whitespace from right and center-justified lines so the
+    // last visible character in the line will be flush with the right margin.
     size_t line_end_index =
         (paragraph_style_.effective_align() == TextAlign::right ||
          paragraph_style_.effective_align() == TextAlign::center)
