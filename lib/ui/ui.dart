fa1b3a9b57e77d5061705f60a00c83a16a94a380
Dan Field <dnfield@google.com>
2019-01-11 12:41:57 -0800

    remove blank line

--- lib/ui/ui.dart
+++ lib/ui/ui.dart
@@ -9,7 +9,6 @@
 /// This library exposes the lowest-level services that Flutter frameworks use
 /// to bootstrap applications, such as classes for driving the input, graphics
 /// text, layout, and rendering subsystems.
-
 library dart.ui;
 
 import 'dart:_internal' hide Symbol; // ignore: import_internal_library, unused_import
