b4813a2603749c77fdd853ab23dc3b701e542b84
GaryQian <garyq@google.com>
2019-01-18 13:48:07 -0800

    Remove negative check in strut lineHeight

--- third_party/txt/src/txt/paragraph.cc
+++ third_party/txt/src/txt/paragraph.cc
@@ -456,12 +456,9 @@ void Paragraph::ComputeStrut(StrutMetrics* strut, SkFont& font) {
     SkFontMetrics strut_metrics;
     font.getMetrics(&strut_metrics);
 
-    // Prevent values from being negative.
-    double canonicalized_line_height =
-        std::max(0.0, paragraph_style_.strut_line_height);
-
-    strut->ascent = canonicalized_line_height * -strut_metrics.fAscent;
-    strut->descent = canonicalized_line_height * strut_metrics.fDescent;
+    strut->ascent = paragraph_style_.strut_line_height * -strut_metrics.fAscent;
+    strut->descent =
+        paragraph_style_.strut_line_height * strut_metrics.fDescent;
     strut->leading =
         // Use font's leading if there is no user specified strut leading.
         paragraph_style_.strut_leading < 0
