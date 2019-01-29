e3a9be99f18c0ac315b791e834141125c813b2af
Dan Field <dnfield@google.com>
2019-01-29 10:34:25 -0800

    revert unintended change

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -4,12 +4,12 @@ set -o pipefail -e;
 
 # TODO(dnfield): Re-enable this when the upstream Dart changes that make it not be flaky land.
 # out/host_debug_unopt/embedder_unittests
-# out/host_debug_unopt/flow_unittests
-# out/host_debug_unopt/fml_unittests --gtest_filter="-*TimeSensitiveTest*"
-# out/host_debug_unopt/runtime_unittests
-# out/host_debug_unopt/shell_unittests
-# out/host_debug_unopt/synchronization_unittests
-# out/host_debug_unopt/txt_unittests  --font-directory=flutter/third_party/txt/third_party/fonts
+out/host_debug_unopt/flow_unittests
+out/host_debug_unopt/fml_unittests --gtest_filter="-*TimeSensitiveTest*"
+out/host_debug_unopt/runtime_unittests
+out/host_debug_unopt/shell_unittests
+out/host_debug_unopt/synchronization_unittests
+out/host_debug_unopt/txt_unittests  --font-directory=flutter/third_party/txt/third_party/fonts
 
 pushd flutter/testing/dart
 pub get
@@ -21,13 +21,13 @@ run_test () {
 }
 
 # Verify that a failing test returns a failure code.
-# ! run_test flutter/testing/smoke_test_failure/fail_test.dart
+! run_test flutter/testing/smoke_test_failure/fail_test.dart
 
-for TEST_SCRIPT in flutter/testing/dart/path*.dart; do
+for TEST_SCRIPT in flutter/testing/dart/*.dart; do
   run_test $TEST_SCRIPT
 done
 
 pushd flutter
-# ci/test.sh
+ci/test.sh
 popd
 exit 0
