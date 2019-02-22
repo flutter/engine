a2b11a152cd8d87e93dd76bbaf4b1fd03c2948e2
Mehmet Fidanboylu <mehmetf@google.com>
2019-02-22 14:25:41 -0800

    Default should be true.

--- common/settings.h
+++ common/settings.h
@@ -109,7 +109,7 @@ struct Settings {
   // switch because it is intended to be decided during build time, not runtime.
   // Some companies apply source modification here because their build system
   // brings its own ICU data files.
-  bool icu_initialization_required = false;
+  bool icu_initialization_required = true;
   std::string icu_data_path;
   MappingCallback icu_mapper;
 
