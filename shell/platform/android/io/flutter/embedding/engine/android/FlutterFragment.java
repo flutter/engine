31170254b5f5137a6e06cd1000891fc105870062
Matt Carroll <mattcarroll@nestlabs.com>
2019-02-28 19:53:18 -0800

    Added a null check.

--- shell/platform/android/io/flutter/embedding/engine/android/FlutterFragment.java
+++ shell/platform/android/io/flutter/embedding/engine/android/FlutterFragment.java
@@ -329,17 +329,21 @@ public class FlutterFragment extends Fragment {
   // TODO(mattcarroll): determine why this can't be in onResume(). Comment reason, or move if possible.
   public void onPostResume() {
     Log.d(TAG, "onPostResume()");
-    flutterEngine.getLifecycleChannel().appIsResumed();
+    if (flutterEngine != null) {
+      flutterEngine.getLifecycleChannel().appIsResumed();
 
-    // TODO(mattcarroll): find a better way to handle the update of UI overlays than calling through
-    //                    to platformPlugin. We're implicitly entangling the Window, Activity, Fragment,
-    //                    and engine all with this one call.
-    platformPlugin.onPostResume();
+      // TODO(mattcarroll): find a better way to handle the update of UI overlays than calling through
+      //                    to platformPlugin. We're implicitly entangling the Window, Activity, Fragment,
+      //                    and engine all with this one call.
+      platformPlugin.onPostResume();
 
-    // TODO(mattcarroll): consider a more abstract way to invoke this behavior. It is very strange for
-    //                    a Fragment to have a seemingly random View responsibility, but this is what
-    //                    existed in the original embedding and I don't have a good alternative yet.
-    flutterView.updateAccessibilityFeatures();
+      // TODO(mattcarroll): consider a more abstract way to invoke this behavior. It is very strange for
+      //                    a Fragment to have a seemingly random View responsibility, but this is what
+      //                    existed in the original embedding and I don't have a good alternative yet.
+      flutterView.updateAccessibilityFeatures();
+    } else {
+      Log.w(TAG, "onPostResume() invoked before FlutterFragment was attached to an Activity.");
+    }
   }
 
   @Override
