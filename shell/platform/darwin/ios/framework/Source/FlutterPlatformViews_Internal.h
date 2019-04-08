bab52a302b5b11d48592aa1f1e2c8f93f8f37b75
Rob Bogie <bogie.rob@gmail.com>
2019-04-08 17:55:04 +0200

    Revert "Update overlays_gr_context_ correctly (#8175)"
    This reverts commit aac8acdf028989defdc3fd357a77e9c4908e87a1.

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.h
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViews_Internal.h
@@ -101,7 +101,8 @@ class FlutterPlatformViewsController {
   void EnsureOverlayInitialized(int64_t overlay_id);
   void EnsureGLOverlayInitialized(int64_t overlay_id,
                                   std::shared_ptr<IOSGLContext> gl_context,
-                                  GrContext* gr_context);
+                                  GrContext* gr_context,
+                                  bool update_gr_context);
 
   FML_DISALLOW_COPY_AND_ASSIGN(FlutterPlatformViewsController);
 };
