6f6561a803b52310b072b7d01de0089aefb17cdd
Todd Volkert <tvolkert@google.com>
2019-04-24 13:08:20 -0700

    Review comments

--- lib/ui/geometry.dart
+++ lib/ui/geometry.dart
@@ -637,11 +637,10 @@ class Rect {
   /// Construct a rectangle that bounds the given circle.
   ///
   /// The `center` argument is assumed to be an offset from the origin.
-  Rect.fromCircle({ Offset center, double radius }) : this.fromLTRB(
-    center.dx - radius,
-    center.dy - radius,
-    center.dx + radius,
-    center.dy + radius,
+  Rect.fromCircle({ Offset center, double radius }) : this.fromCenter(
+    center: center,
+    width: radius * 2,
+    height: radius * 2,
   );
 
   /// Constructs a rectangle from its center point, width, and height.
