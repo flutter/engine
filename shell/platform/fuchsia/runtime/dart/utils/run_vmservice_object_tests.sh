ed3e21178c5333ade9be7ee77190139254e7c96d
Chinmay Garde <chinmaygarde@google.com>
2019-05-07 11:36:08 -0700

    Licenses.

--- shell/platform/fuchsia/runtime/dart/utils/run_vmservice_object_tests.sh
+++ shell/platform/fuchsia/runtime/dart/utils/run_vmservice_object_tests.sh
@@ -1,4 +1,7 @@
 #!/boot/bin/sh
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 set -x
 
