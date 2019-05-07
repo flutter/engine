dfb451eb1791e21c08a8983bce13822c4ab62bb4
Dan Field <dnfield@google.com>
2019-05-07 06:36:31 -0700

    revert web_sdk changes

--- web_sdk/test/api_conform_test.dart
+++ web_sdk/test/api_conform_test.dart
@@ -4,7 +4,7 @@
 
 import 'dart:io';
 
-import 'package:analyzer/analyzer.dart'; // ignore: deprecated_member_use
+import 'package:analyzer/analyzer.dart';
 
 void main() {
   // These files just contain imports to the part files;
