af3213ef34d278f57319512f779fc256f29c5294
Chinmay Garde <chinmaygarde@google.com>
2019-05-07 18:20:42 -0700

    Wire up the Skia Metal backend on iOS.

---
+++ shell/gpu/gpu_surface_metal.h
@@ -0,0 +1,47 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_GPU_GPU_SURFACE_METAL_H_
+#define FLUTTER_SHELL_GPU_GPU_SURFACE_METAL_H_
+
+#include "flutter/fml/macros.h"
+#include "flutter/fml/platform/darwin/scoped_nsobject.h"
+#include "flutter/shell/common/surface.h"
+#include "third_party/skia/include/gpu/GrContext.h"
+
+@class CAMetalLayer;
+
+namespace flutter {
+
+class GPUSurfaceMetal : public Surface {
+ public:
+  GPUSurfaceMetal(fml::scoped_nsobject<CAMetalLayer> layer);
+
+  ~GPUSurfaceMetal() override;
+
+ private:
+  fml::scoped_nsobject<CAMetalLayer> layer_;
+  sk_sp<GrContext> context_;
+
+  // |Surface|
+  bool IsValid() override;
+
+  // |Surface|
+  std::unique_ptr<SurfaceFrame> AcquireFrame(const SkISize& size) override;
+
+  // |Surface|
+  SkMatrix GetRootTransformation() const override;
+
+  // |Surface|
+  GrContext* GetContext() override;
+
+  // |Surface|
+  bool MakeRenderContextCurrent() override;
+
+  FML_DISALLOW_COPY_AND_ASSIGN(GPUSurfaceMetal);
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_GPU_GPU_SURFACE_METAL_H_
