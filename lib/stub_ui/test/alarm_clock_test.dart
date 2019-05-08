4c2523044f140b47d704c00df9c67073bcbb3d83
jonahwilliams <jonahwilliams@google.com>
2019-05-08 07:18:11 -0700

    update more licenses

--- lib/stub_ui/test/alarm_clock_test.dart
+++ lib/stub_ui/test/alarm_clock_test.dart
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:test/test.dart';
 import 'package:quiver/testing/async.dart';
 import 'package:quiver/time.dart';
