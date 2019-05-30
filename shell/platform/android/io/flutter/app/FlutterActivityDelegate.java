868e4cbdc0be5ba31c2fac348d6d314ba984a3f8
Ben Konyi <bkonyi@google.com>
2019-05-30 14:51:31 -0700

    Allow for whitelisted flags to be passed to the Dart VM
    Fixed part of https://github.com/flutter/flutter/issues/32176

--- shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
+++ shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
@@ -312,6 +312,12 @@ public final class FlutterActivityDelegate
         if (intent.getBooleanExtra("verbose-logging", false)) {
             args.add("--verbose-logging");
         }
+        // NOTE: all flags provided with this argument are subject to filtering
+        // based on a whitelist in shell/common/switches.cc. If any flag provided
+        // is not present in the whitelist, the engine will immediately shut down.
+        if (intent.hasExtra("dart-flags")) {
+            args.add("--dart-flags=" + intent.getStringExtra("dart-flags"));
+        }
         if (!args.isEmpty()) {
             String[] argsArray = new String[args.size()];
             return args.toArray(argsArray);
