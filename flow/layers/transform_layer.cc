3ce2e5c6b0cde1c2044e478fa7ffbd64ca01dbcd
Yuqian Li <liyuqian@google.com>
2019-06-10 16:46:48 -0700

    Set identity instead of crash in opt build
    According to https://github.com/flutter/flutter/wiki/Style-guide-for-Flutter-repo#expectations-around-potential-crashes-in-the-engine

--- flow/layers/transform_layer.cc
+++ flow/layers/transform_layer.cc
@@ -17,7 +17,10 @@ TransformLayer::TransformLayer(const SkMatrix& transform)
   //
   // We have to write this flaky test because there is no reliable way to test
   // whether a variable is initialized or not in C++.
-  FML_CHECK(transform_.isFinite());
+  FML_DCHECK(transform_.isFinite());
+  if (!transform_.isFinite()) {
+    transform_.setIdentity();
+  }
 }
 
 TransformLayer::~TransformLayer() = default;
