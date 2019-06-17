9d51735071bebcd29e00bc9a92a0d22d2483811b
GaryQian <garyq@google.com>
2019-06-17 14:23:11 -0700

    Cleanup

--- shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
+++ shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
@@ -139,7 +139,6 @@ class InputConnectionAdaptor extends BaseInputConnection {
     // Sanitizes the index to ensure the index is within the range of the
     // contents of editable.
     private static int clampIndexToEditable(int index, Editable editable) {
-        // return index;
         return Math.max(0, Math.min(editable.length(), index));
     }
 
