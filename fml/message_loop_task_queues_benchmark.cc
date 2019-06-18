837082143f0dcf782d3363ef6cada77c7e36fd04
Kaushik Iska <kaushikiska@google.com>
2019-06-18 14:39:53 -0700

    add some benchmarks

--- fml/message_loop_task_queues_benchmark.cc
+++ fml/message_loop_task_queues_benchmark.cc
@@ -2,20 +2,61 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cassert>
 #include <string>
+#include <thread>
+#include <vector>
 #include "benchmark/benchmark_api.h"
 #include "flutter/fml/message_loop_task_queues.h"
+#include "flutter/fml/synchronization/count_down_latch.h"
 
 namespace fml {
 namespace benchmarking {
 
-static void BM_StringCreation(benchmark::State& state) {
+static void BM_RegisterAndGetTasks(benchmark::State& state) {
   while (state.KeepRunning()) {
-    std::string empty_string;
+    auto task_queue = fml::MessageLoopTaskQueues::GetInstance();
+    std::vector<size_t> task_queue_ids;
+
+    const int num_task_queues = 10;
+    const int num_tasks_per_queue = 100;
+    const fml::TimePoint past = fml::TimePoint::Now();
+
+    for (int i = 0; i < num_task_queues; i++) {
+      task_queue->CreateTaskQueue();
+    }
+
+    std::vector<std::thread> threads;
+
+    CountDownLatch tasks_registered(num_task_queues);
+    CountDownLatch tasks_done(num_task_queues);
+
+    for (int i = 0; i < num_task_queues; i++) {
+      threads.emplace_back([task_runner_id = i, &task_queue, past, &tasks_done,
+                            &tasks_registered]() {
+        for (int j = 0; j < num_tasks_per_queue; j++) {
+          task_queue->RegisterTask(
+              task_runner_id, [] {}, past);
+        }
+        tasks_registered.CountDown();
+        tasks_registered.Wait();
+        std::vector<fml::closure> invocations;
+        task_queue->GetTasksToRunNow(task_runner_id, fml::FlushType::kAll,
+                                     invocations);
+        assert(invocations.size() == num_tasks_per_queue);
+        tasks_done.CountDown();
+      });
+    }
+
+    tasks_done.Wait();
+
+    for (auto& thread : threads) {
+      thread.join();
+    }
   }
 }
 
-BENCHMARK(BM_StringCreation);
+BENCHMARK(BM_RegisterAndGetTasks);
 
 int Main(int argc, char** argv) {
   benchmark::Initialize(&argc, argv);
