b78b05420c90d6cd6e116d1955a1de95d5ce26d0
Aaron Clarke <aaclarke@google.com>
2019-06-20 09:13:13 -0700

    Fixed formatting and removed third_party from the format check.

--- shell/platform/darwin/ios/framework/Source/FlutterEngineTest.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterEngineTest.mm
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <XCTest/XCTest.h>
 #import <OCMock/OCMock.h>
+#import <XCTest/XCTest.h>
 #import "flutter/shell/platform/darwin/ios/framework/Source/FlutterEngine_Internal.h"
 
 @interface FlutteEngineTest : XCTestCase
@@ -23,6 +23,5 @@
                                                        project:project] autorelease];
   XCTAssertNotNil(engine);
 }
-
 
 @end
