d0fa48959f9f44f50a74c549c96521c7ac20ebef
Aaron Clarke <aaclarke@google.com>
2019-06-21 10:38:25 -0700

    Added a unit test for the relay.

--- shell/platform/darwin/ios/framework/Source/FlutterBinaryMessengerRelay.h
+++ shell/platform/darwin/ios/framework/Source/FlutterBinaryMessengerRelay.h
@@ -2,8 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "flutter/shell/platform/darwin/common/framework/Headers/FlutterBinaryMessenger.h"
+#import "flutter/shell/platform/darwin/common/framework/Headers/FlutterBinaryMessenger.h"
+#import "flutter/shell/platform/darwin/common/framework/Headers/FlutterMacros.h"
 
+#ifndef NDEBUG
+FLUTTER_EXPORT
+#endif
 @interface FlutterBinaryMessengerRelay : NSObject <FlutterBinaryMessenger>
 @property (nonatomic, assign) NSObject<FlutterBinaryMessenger>* parent;
 - (instancetype)initWithParent:(NSObject<FlutterBinaryMessenger>*)parent;
