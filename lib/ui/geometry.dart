f69e7d0fd574c6d2922c0842fd6a10b7d850a226
Shi Hao Hong <shihaohong94@gmail.com>
2019-06-23 20:20:58 -0700

    convert RRect.scaleRadii to public method

--- lib/ui/geometry.dart
+++ lib/ui/geometry.dart
@@ -1421,7 +1421,7 @@ class RRect {
   //
   // Inspired from:
   //   https://github.com/google/skia/blob/master/src/core/SkRRect.cpp#L164
-  RRect _scaleRadii() {
+  RRect scaleRadii() {
     double scale = 1.0;
     scale = _getMin(scale, blRadiusY, tlRadiusY, height);
     scale = _getMin(scale, tlRadiusX, trRadiusX, width);
@@ -1472,7 +1472,7 @@ class RRect {
     if (point.dx < left || point.dx >= right || point.dy < top || point.dy >= bottom)
       return false; // outside bounding box
 
-    final RRect scaled = _scaleRadii();
+    final RRect scaled = scaleRadii();
 
     double x;
     double y;
