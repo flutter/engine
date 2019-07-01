6a1f257f970c490f207716f1e376b73527ffa2a5
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-07-01 15:55:09 -0700

    Implemented checkInputConnectionProxy in FlutterView.

--- shell/platform/android/io/flutter/embedding/android/FlutterView.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterView.java
@@ -18,6 +18,7 @@ import android.text.format.DateFormat;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowInsets;
 import android.view.accessibility.AccessibilityManager;
 import android.view.accessibility.AccessibilityNodeProvider;
@@ -379,6 +380,21 @@ public class FlutterView extends FrameLayout {
     }
 
     return textInputPlugin.createInputConnection(this, outAttrs);
+  }
+
+  /**
+   * Allows a {@code View} that is not currently the input connection target to invoke commands on
+   * the {@link android.view.inputmethod.InputMethodManager}, which is otherwise disallowed.
+   * <p>
+   * Returns true to allow non-input-connection-targets to invoke methods on
+   * {@code InputMethodManager}, or false to exclusively allow the input connection target to invoke
+   * such methods.
+   */
+  @Override
+  public boolean checkInputConnectionProxy(View view) {
+    return flutterEngine != null
+        ? flutterEngine.getPlatformViewsController().checkInputConnectionProxy(view)
+        : super.checkInputConnectionProxy(view);
   }
 
   /**
