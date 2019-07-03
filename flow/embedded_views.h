63edd738c99322e34309e747cdd213c94e2ea8ed
Kaushik Iska <kaushikiska@google.com>
2019-07-03 07:48:38 -0700

    ExternalViewEmbedder can CancelFrame after pre-roll
    - Resets the state so next pre-roll can be successful.
    - Commit any pending `CATransaction` so we don't create
      nested transactions.

--- flow/embedded_views.h
+++ flow/embedded_views.h
@@ -187,6 +187,10 @@ class ExternalViewEmbedder {
   // have mutated for last layer tree.
   virtual bool HasPendingViewOperations() = 0;
 
+  // Needs to be called before |SubmitFrame|. Clears pre-roll state and
+  // sets the stage for the next pre-roll.
+  virtual void CancelFrame() = 0;
+
   virtual void BeginFrame(SkISize frame_size) = 0;
 
   virtual void PrerollCompositeEmbeddedView(
