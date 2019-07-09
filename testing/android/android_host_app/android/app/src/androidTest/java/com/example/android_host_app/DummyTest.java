b74a4fd097f341096419b79da1007bbca3f3fd5a
Collin Jackson <jackson@google.com>
2019-07-09 16:25:51 -0700

    Dummy test file

---
+++ testing/android/android_host_app/android/app/src/androidTest/java/com/example/android_host_app/DummyTest.java
@@ -0,0 +1,8 @@
+package com.example.android_test_adapter;
+
+import org.junit.runner.RunWith;
+
+@RunWith(FlutterJUnitRunner.class)
+public class DummyTest {
+  // This class is intentionally left blank.
+}
