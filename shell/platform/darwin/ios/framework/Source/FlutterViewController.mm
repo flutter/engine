ba5e02dab1013980c4fbd84c3040e3ff039621db
Matt Carroll <matthew-carroll@users.noreply.github.com>
2019-07-09 03:28:15 -0700

    Formatting update.

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -874,7 +874,7 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 
 #pragma mark - Set user settings
 
-- (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
+- (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
   [self onUserSettingsChanged:nil];
 }
@@ -960,7 +960,7 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 - (NSString*)brightnessMode {
   if (@available(iOS 13, *)) {
     UIUserInterfaceStyle style = UITraitCollection.currentTraitCollection.userInterfaceStyle;
-  
+
     if (style == UIUserInterfaceStyleDark) {
       return @"dark";
     } else {
