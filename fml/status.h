9c3b023e38b9b10f17f25f2943c66d014b6f39ca
Aaron Clarke <aaclarke@google.com>
2019-07-09 17:11:56 -0700

    Removed enumeration values for StatusCodes.

--- fml/status.h
+++ fml/status.h
@@ -9,27 +9,24 @@
 
 namespace fml {
 
-/// Google's canonical list of errors, should be in sync with:
-/// https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto
-enum class StatusCode : int {
-  kOk = 0,
-  kCancelled = 1,
-  kUnknown = 2,
-  kInvalidArgument = 3,
-  kDeadlineExceeded = 4,
-  kNotFound = 5,
-  kAlreadyExists = 6,
-  kPermissionDenied = 7,
-  kResourceExhausted = 8,
-  kFailedPrecondition = 9,
-  kAborted = 10,
-  kOutOfRange = 11,
-  kUnimplemented = 12,
-  kInternal = 13,
-  kUnavailable = 14,
-  kDataLoss = 15,
-  kUnauthenticated = 16,
-  kDoNotUseReservedForFutureExpansionUseDefaultInSwitchInstead_ = 20
+enum class StatusCode {
+  kOk,
+  kCancelled,
+  kUnknown,
+  kInvalidArgument,
+  kDeadlineExceeded,
+  kNotFound,
+  kAlreadyExists,
+  kPermissionDenied,
+  kResourceExhausted,
+  kFailedPrecondition,
+  kAborted,
+  kOutOfRange,
+  kUnimplemented,
+  kInternal,
+  kUnavailable,
+  kDataLoss,
+  kUnauthenticated
 };
 
 /// Class that represents the resolution of the execution of a procedure.  This
