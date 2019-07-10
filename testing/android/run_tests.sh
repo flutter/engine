b9e1d5cdbf5a69a5c8ad08604589186565b5a3c8
Collin Jackson <jackson@google.com>
2019-07-09 20:37:57 -0700

    Some fixes to run_tests script

--- testing/android/run_tests.sh
+++ testing/android/run_tests.sh
@@ -7,7 +7,7 @@ if [ "$FRAMEWORK_PATH" = "" ]
 then
   FLUTTER_BIN=flutter
 else
-  FLUTTER_BIN=$FRAMEWORK_PATH/bin/flutter
+  FLUTTER_BIN=$FRAMEWORK_PATH/flutter/bin/flutter
 fi
 # Creates android/gradlew
 $FLUTTER_BIN build apk --debug
@@ -19,7 +19,7 @@ case $1 in
       -Pverbose=true \
       -Ptrack-widget-creation=false \
       -Pfilesystem-scheme=org-dartlang-root \
-      assembleAndroidTest
+      assembleAndroidTest || exit 1
     popd
     echo $GCLOUD_FIREBASE_TESTLAB_KEY > ${HOME}/gcloud-service-key.json
     gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
@@ -31,7 +31,7 @@ case $1 in
       --results-bucket=gs://flutter_firebase_testlab \
       --results-dir=engine_android_test/$GIT_REVISION/$CIRRUS_BUILD_ID
     # Check logcat for "E/flutter" - if it's there, something's wrong.
-    gsutil cp gs://flutter_firebase_testlab/release_smoke_test/$GIT_REVISION/$CIRRUS_BUILD_ID/walleye-26-en-portrait/logcat /tmp/logcat
+    gsutil cp gs://flutter_firebase_testlab/engine_android_test/$GIT_REVISION/$CIRRUS_BUILD_ID/walleye-26-en-portrait/logcat /tmp/logcat
     ! grep "E/flutter" /tmp/logcat || false
     grep "I/flutter" /tmp/logcat;;
   *)  # Default behavior: test using a local device
