db895117796040f7e28dbddf808d7f17eeb9be77
Michael Klimushyn <mklim@google.com>
2019-07-10 17:13:05 -0700

    Remove plumbing from PlatformView->TextInputPlugin->PlatformView

--- shell/platform/android/io/flutter/plugin/platform/PlatformViewsController.java
+++ shell/platform/android/io/flutter/plugin/platform/PlatformViewsController.java
@@ -148,7 +148,7 @@ public class PlatformViewsController implements PlatformViewsAccessibilityDelega
         public void resizePlatformView(@NonNull PlatformViewsChannel.PlatformViewResizeRequest request, @NonNull Runnable onComplete) {
             ensureValidAndroidVersion();
 
-            VirtualDisplayController vdController = vdControllers.get(request.viewId);
+            final VirtualDisplayController vdController = vdControllers.get(request.viewId);
             if (vdController == null) {
                 throw new IllegalStateException("Trying to resize a platform view with unknown id: "
                     + request.viewId);
@@ -158,22 +158,18 @@ public class PlatformViewsController implements PlatformViewsAccessibilityDelega
             int physicalHeight = toPhysicalPixels(request.newLogicalHeight);
             validateVirtualDisplayDimensions(physicalWidth, physicalHeight);
 
-            if (textInputPlugin != null) {
-                // Resizing involved moving the platform view to a new virtual display.
-                // Doing so potentially results in losing an active input connection.
-                // To make sure we preserve the input connection when resizing we lock it here
-                // and unlock after the resize is complete.
-                textInputPlugin.lockPlatformViewInputConnection();
-            }
+            // Resizing involved moving the platform view to a new virtual display. Doing so
+            // potentially results in losing an active input connection. To make sure we preserve
+            // the input connection when resizing we lock it here and unlock after the resize is
+            // complete.
+            lockInputConnection(vdController);
             vdController.resize(
                     physicalWidth,
                     physicalHeight,
                     new Runnable() {
                         @Override
                         public void run() {
-                            if (textInputPlugin != null) {
-                                textInputPlugin.unlockPlatformViewInputConnection();
-                            }
+                            unlockInputConnection(vdController);
                             onComplete.run();
                         }
                     }
@@ -366,27 +362,19 @@ public class PlatformViewsController implements PlatformViewsAccessibilityDelega
         return controller.getView();
     }
 
-    /**
-    * Callback fired when the platform's input connection is locked, or should be used. See also
-    * {@link TextInputPlugin#lockPlatformViewInputConnection}.
-    */
-    public void onInputConnectionLocked(int viewId) {
-        VirtualDisplayController controller = vdControllers.get(viewId);
-        if (controller == null) {
+    private void lockInputConnection(@NonNull VirtualDisplayController controller) {
+        if (textInputPlugin == null) {
             return;
         }
+        textInputPlugin.lockPlatformViewInputConnection();
         controller.onInputConnectionLocked();
     }
 
-    /**
-    * Callback fired when the platform input connection has been unlocked. See also
-    * {@link TextInputPlugin#lockPlatformViewInputConnection}.
-    */
-    public void onInputConnectionUnlocked(int viewId) {
-        VirtualDisplayController controller = vdControllers.get(viewId);
-        if (controller == null) {
+    private void unlockInputConnection(@NonNull VirtualDisplayController controller) {
+        if (textInputPlugin == null) {
             return;
         }
+        textInputPlugin.unlockPlatformViewInputConnection();
         controller.onInputConnectionUnlocked();
     }
 
