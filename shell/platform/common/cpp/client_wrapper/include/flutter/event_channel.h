0af9192c846c3e292beb0372cc92bb06915e8bcf
湖北捷智云技术有限公司 <duanweiwei1982@gmail.com>
2019-07-13 01:08:05 +0800

    Fixed EventChannel for embedder.
    - Add Success and Error handler for EventSink.
    - Send Event message or error using MethodCodec.

--- shell/platform/common/cpp/client_wrapper/include/flutter/event_channel.h
+++ shell/platform/common/cpp/client_wrapper/include/flutter/event_channel.h
@@ -18,12 +18,26 @@
 namespace flutter {
 
 template <typename T>
-using EventSink = std::function<void(const T *events)>;
+using SuccessHandler =
+        std::function<void(const T* event)>;
+
+template <typename T>
+using ErrorHandler =
+std::function<void(
+    const std::string& errorCode,
+    const std::string& errorMessage,
+    const T* errorDetails)>;
+
+template <typename T>
+struct EventSink {
+    SuccessHandler<T> Success;
+    ErrorHandler<T> Error;
+};
 
 template <typename T>
 struct StreamHandler {
   std::function<MethodResult<T> *(const T *arguments,
-                                  const EventSink<T> *event)>
+                                  const EventSink<T> *event_sink)>
       onListen;
   std::function<MethodResult<T> *(const T *arguments)> onCancel;
 };
@@ -32,12 +46,10 @@ template <typename T>
 class EventChannel {
  public:
   EventChannel(BinaryMessenger *messenger, const std::string &name,
-               const MethodCodec<T> *codec,
-               const MessageCodec<T> *message_codec)
+               const MethodCodec<T> *codec)
       : messenger_(messenger),
         name_(name),
-        codec_(codec),
-        message_code_(message_codec) {}
+        codec_(codec){}
   ~EventChannel() {}
 
   // Prevent copying.
@@ -46,18 +58,29 @@ class EventChannel {
 
   void SetStreamHandler(StreamHandler<T> stream_handler) {
     stream_handler_ = stream_handler;
-    const auto message_codec = message_code_;
     const auto messenger = messenger_;
     std::string channel_name = name_;
-    EventSink<T> sink = [messenger, channel_name,
-                         message_codec](const T *events) {
-      std::unique_ptr<std::vector<uint8_t>> message =
-          message_codec->EncodeMessage(events);
-      messenger->Send(channel_name, message->data(), message->size());
+    const auto* codec = codec_;
+ 
+    EventSink<T> event_sink = {};
+
+    event_sink.Success = [messenger, channel_name, codec](const T* events) {
+        std::unique_ptr<std::vector<uint8_t>> message =
+            codec->EncodeSuccessEnvelope(events);
+        messenger->Send(channel_name, message->data(), message->size());
     };
 
-    const auto *codec = codec_;
-    BinaryMessageHandler binary_handler = [&, sink, codec, channel_name](
+    event_sink.Error = [messenger, channel_name, codec](
+        const std::string& errorCode,
+        const std::string& errorMessage,
+        const T* errorDetails) {
+        std::unique_ptr<std::vector<uint8_t>> message =
+            codec->EncodeErrorEnvelope(errorCode, errorMessage,
+                errorDetails);
+        messenger->Send(channel_name, message->data(), message->size());
+    };
+
+    BinaryMessageHandler binary_handler = [&, event_sink, codec, channel_name](
                                               const uint8_t *message,
                                               const size_t message_size,
                                               BinaryReply reply) {
@@ -72,7 +95,7 @@ class EventChannel {
         return;
       }
       if (method_call->method_name().compare("listen") == 0) {
-        stream_handler_.onListen(method_call->arguments(), &sink);
+        stream_handler_.onListen(method_call->arguments(), &event_sink);
         result->Success(nullptr);
         return;
       } else if (method_call->method_name().compare("cancel") == 0) {
@@ -91,7 +114,6 @@ class EventChannel {
   BinaryMessenger *messenger_;
   std::string name_;
   const MethodCodec<T> *codec_;
-  const MessageCodec<T> *message_code_;
   StreamHandler<T> stream_handler_;
 };
 
