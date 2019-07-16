9167385fbe31656548168762ef4b4c829571cba4
Chinmay Garde <chinmaygarde@google.com>
2019-07-16 13:45:09 -0700

    Run engine unit-tests on Windows in the presubmits.
    Also removes a redundant Windows build and the old run_tests.sh script.

--- testing/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o pipefail -e;
-
-BUILD_VARIANT="${1:-host_debug_unopt}"
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-python "${CURRENT_DIR}/run_tests.py" --variant "${BUILD_VARIANT}"
