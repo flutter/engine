9c990ae7d639a5b025f9836d4de9ca554e76f290
Dan Field <dnfield@google.com>
2019-07-16 22:13:15 -0700

    Add clang version to Info.plist

--- build/copy_info_plist.py
+++ build/copy_info_plist.py
@@ -16,6 +16,7 @@ usage: copy_info_plist.py <src_path> <dest_path>
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import subprocess
 
 import sys
 import git_revision
@@ -25,7 +26,12 @@ def main():
   text = open(sys.argv[1]).read()
   engine_path = os.path.join(os.getcwd(), "..", "..", "flutter")
   revision = git_revision.GetRepositoryVersion(engine_path)
-  text = text.format(revision)
+  clang_executable = str(os.path.join("..", "..", "buildtools", "mac-x64", "clang", "bin", "clang++"))
+  if sys.argv[3] == "--bitcode=true":
+    clang_executable = "clang++"
+  version = subprocess.check_output([clang_executable, "--version"])
+  text = text.format(revision, version.splitlines()[0])
+
   with open(sys.argv[2], "w") as outfile:
     outfile.write(text)
 
