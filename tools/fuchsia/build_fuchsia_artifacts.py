6120a37c626f6a9bd5b62f2f2f654864428d1a5b
Kaushik Iska <kaushikiska@google.com>
2019-07-22 11:20:18 -0700

    Farify the artifacts

--- tools/fuchsia/build_fuchsia_artifacts.py
+++ tools/fuchsia/build_fuchsia_artifacts.py
@@ -3,7 +3,6 @@
 # Copyright 2013 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """ Builds all Fuchsia artifacts vended by Flutter.
 """
 
@@ -16,18 +15,39 @@ import subprocess
 import sys
 
 from gather_flutter_runner_artifacts import GatherArtifacts
-
+from gen_package import CreateFarPackage
 
 _script_dir = os.path.abspath(os.path.join(os.path.realpath(__file__), '..'))
 _src_root_dir = os.path.join(_script_dir, '..', '..', '..')
 _out_dir = os.path.join(_src_root_dir, 'out')
 _bucket_directory = os.path.join(_out_dir, 'fuchsia_bucket')
 
+
 def IsLinux():
   return platform.system() == 'Linux'
 
+
+def IsMac():
+  return platform.system() == 'Darwin'
+
+
+def GetPMBinPath():
+  # host_os references the gn host_os
+  # https://gn.googlesource.com/gn/+/master/docs/reference.md#var_host_os
+  host_os = ''
+  if IsLinux():
+    host_os = 'linux'
+  elif IsMac():
+    host_os = 'mac'
+  else:
+    host_os = 'windows'
+
+  return os.path.join(_src_root_dir, 'fuchsia', 'sdk', host_os, 'tools', 'pm')
+
+
 def RunExecutable(command):
   subprocess.check_call(command, cwd=_src_root_dir)
+
 
 def RunGN(variant_dir, flags):
   RunExecutable([
@@ -36,14 +56,12 @@ def RunGN(variant_dir, flags):
 
   assert os.path.exists(os.path.join(_out_dir, variant_dir))
 
+
 def BuildNinjaTargets(variant_dir, targets):
   assert os.path.exists(os.path.join(_out_dir, variant_dir))
 
-  RunExecutable([
-    'autoninja',
-    '-C',
-    os.path.join(_out_dir, variant_dir)
-  ] + targets)
+  RunExecutable(['autoninja', '-C',
+                 os.path.join(_out_dir, variant_dir)] + targets)
 
 
 def RemoveDirectoryIfExists(path):
@@ -55,10 +73,14 @@ def RemoveDirectoryIfExists(path):
   else:
     shutil.rmtree(path)
 
+
 def CopyToBucket(source, destination):
   source = os.path.join(_out_dir, source)
   destination = os.path.join(_bucket_directory, destination)
   GatherArtifacts(source, destination)
+  pm_bin = GetPMBinPath()
+  key_path = os.path.join(_script_dir, 'development.key')
+  CreateFarPackage(pm_bin, destination, key_path)
 
 
 def BuildBucket():
@@ -77,68 +99,56 @@ def ProcessCIPDPakcage(upload, engine_version):
 
   if upload and IsLinux():
     command = [
-      'cipd',
-      'create',
-      '-pkg-def',
-      'fuchsia.cipd.yaml',
-      '-ref',
-      'latest',
-      '-tag',
-      'git_revision:%s' % engine_version
+        'cipd', 'create', '-pkg-def', 'fuchsia.cipd.yaml', '-ref', 'latest',
+        '-tag',
+        'git_revision:%s' % engine_version
     ]
   else:
     command = [
-      'cipd',
-      'pkg-build',
-      '-pkg-def',
-      'fuchsia.cipd.yaml',
-      '-out',
-      os.path.join(_bucket_directory, 'fuchsia.cipd')
+        'cipd', 'pkg-build', '-pkg-def', 'fuchsia.cipd.yaml', '-out',
+        os.path.join(_bucket_directory, 'fuchsia.cipd')
     ]
 
   subprocess.check_call(command, cwd=_bucket_directory)
 
-def main():
-  parser = argparse.ArgumentParser();
 
-  parser.add_argument('--upload', default=False, action='store_true',
+def main():
+  parser = argparse.ArgumentParser()
+
+  parser.add_argument(
+      '--upload',
+      default=False,
+      action='store_true',
       help='If set, uploads the CIPD package and tags it as the latest.')
 
-  parser.add_argument('--engine-version', required=True,
+  parser.add_argument(
+      '--engine-version',
+      required=True,
       help='Specifies the flutter engine SHA.')
 
   args = parser.parse_args()
 
   common_flags = [
-    '--fuchsia',
-    # The source does not require LTO and LTO is not wired up for targets.
-    '--no-lto',
+      '--fuchsia',
+      # The source does not require LTO and LTO is not wired up for targets.
+      '--no-lto',
   ]
 
-  RunGN('fuchsia_debug', common_flags + [
-    '--runtime-mode',
-    'debug'
-  ]);
+  RunGN('fuchsia_debug', common_flags + ['--runtime-mode', 'debug'])
 
-  RunGN('fuchsia_profile', common_flags + [
-    '--runtime-mode',
-    'profile'
-  ]);
+  RunGN('fuchsia_profile', common_flags + ['--runtime-mode', 'profile'])
 
-  RunGN('fuchsia_release', common_flags + [
-    '--runtime-mode',
-    'release'
-  ]);
+  RunGN('fuchsia_release', common_flags + ['--runtime-mode', 'release'])
 
   targets_to_build = [
-    # The Flutter Runner.
-    'flutter/shell/platform/fuchsia/flutter:flutter',
+      # The Flutter Runner.
+      'flutter/shell/platform/fuchsia/flutter:flutter',
 
-    # The Dart Runner.
-    'flutter/shell/platform/fuchsia/dart:dart',
+      # The Dart Runner.
+      'flutter/shell/platform/fuchsia/dart:dart',
 
-    # The Snapshots.
-    'flutter/lib/snapshot:snapshot',
+      # The Snapshots.
+      'flutter/lib/snapshot:snapshot',
   ]
 
   BuildNinjaTargets('fuchsia_debug', targets_to_build)
@@ -152,4 +162,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
