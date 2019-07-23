54c18785fff0078b6a476e6288a8c559c029baf9
Dan Field <dnfield@google.com>
2019-07-23 13:31:40 -0700

    test scenario_app on CI

--- testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/MainActivity.java
+++ testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/MainActivity.java
@@ -3,10 +3,16 @@ package dev.flutter.scenarios;
 import android.os.Bundle;
 
 import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.engine.renderer.OnFirstFrameRenderedListener;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterActivity implements OnFirstFrameRenderedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onFirstFrameRendered() {
+        reportFullyDrawn();
     }
 }
