b040787666fcc7acb2f5c039d8822a9e63e63bed
Dan Field <dfield@gmail.com>
2019-07-24 12:24:49 -0700

    Update firebase_testlab.sh

--- ci/firebase_testlab.sh
+++ ci/firebase_testlab.sh
@@ -30,8 +30,4 @@ gcloud firebase test android run \
   --timeout 2m \
   --results-bucket=gs://flutter_firebase_testlab \
   --results-dir=engine_scenario_test/$GIT_REVISION/$CIRRUS_BUILD_ID
-
-# # Check logcat for "E/flutter" - if it's there, something's wrong.
-# gsutil cp gs://flutter_firebase_testlab/engine_scenario_test/$GIT_REVISION/$CIRRUS_BUILD_ID/walleye-26-en-portrait/logcat /tmp/logcat
-# ! grep "E/flutter" /tmp/logcat || false
-# grep "I/flutter" /tmp/logcat
+  
