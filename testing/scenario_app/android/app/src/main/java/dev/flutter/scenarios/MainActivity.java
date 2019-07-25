9cfc783ac3c7831f6498bac7470e942c0f5ced9e
Dan Field <dnfield@google.com>
2019-07-25 00:29:00 -0700

    TIMELINE

--- testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/MainActivity.java
+++ testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/MainActivity.java
@@ -1,11 +1,17 @@
 package dev.flutter.scenarios;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 import io.flutter.Log;
 import io.flutter.embedding.android.FlutterActivity;
@@ -14,17 +20,22 @@ import io.flutter.embedding.android.FlutterView;
 import io.flutter.embedding.engine.FlutterShellArgs;
 import io.flutter.embedding.engine.renderer.OnFirstFrameRenderedListener;
 import io.flutter.plugin.common.BasicMessageChannel;
-import io.flutter.plugin.common.StringCodec;
+import io.flutter.plugin.common.BinaryCodec;
 
 public class MainActivity extends FlutterActivity implements OnFirstFrameRenderedListener {
+    final static String TAG = "Scenarios";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         final Intent launchIntent = getIntent();
         if ("com.google.intent.action.TEST_LOOP".equals(launchIntent.getAction())) {
+            if(Build.VERSION.SDK_INT>22){
+                requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
             // Run for one minute, get the timeline data, write it, and finish.
             final Uri logFileUri = launchIntent.getData();
-            new Handler().postDelayed(() -> writeTimelineData(logFileUri), 10000);
+            new Handler().postDelayed(() -> writeTimelineData(logFileUri), 60000);
         }
     }
 
@@ -57,16 +68,27 @@ public class MainActivity extends FlutterActivity implements OnFirstFrameRendere
                 .build();
     }
 
-    private void writeTimelineData(Uri uri) {
-        if (uri == null) {
+    private void writeTimelineData(Uri logFile) {
+        if (logFile == null) {
             throw new IllegalArgumentException();
         }
         if (getFlutterEngine() == null) {
-            Log.e("Scenarios", "Could not write timeline data - no engine.");
+            Log.e(TAG, "Could not write timeline data - no engine.");
             return;
         }
-        final BasicMessageChannel<String> channel = new BasicMessageChannel<>(
-                getFlutterEngine().getDartExecutor(), "write_timeline", StringCodec.INSTANCE);
-        channel.send(uri.getPath(), (String reply) -> finish());
+        final BasicMessageChannel<ByteBuffer> channel = new BasicMessageChannel<>(
+                getFlutterEngine().getDartExecutor(), "write_timeline", BinaryCodec.INSTANCE);
+        channel.send(null, (ByteBuffer reply) -> {
+            try {
+                final FileDescriptor fd = getContentResolver()
+                        .openAssetFileDescriptor(logFile, "w").getFileDescriptor();
+                final FileOutputStream outputStream = new FileOutputStream(fd);
+                outputStream.write(reply.array());
+                outputStream.close();
+            } catch (IOException ex) {
+                Log.e(TAG, "Could not write timeline file: " + ex.toString());
+            }
+            finish();
+        });
     }
 }
--- testing/scenario_app/lib/main.dart
+++ testing/scenario_app/lib/main.dart
@@ -30,7 +30,7 @@ void main() {
   window.sendPlatformMessage('scenario_status', data, null);
 }
 
-void _handlePlatformMessage(String name, ByteData data, PlatformMessageResponseCallback callback) {
+Future<void> _handlePlatformMessage(String name, ByteData data, PlatformMessageResponseCallback callback) async {
   if (name == 'set_scenario' && data != null) {
     final String scenarioName = utf8.decode(data.buffer.asUint8List());
     final Scenario candidateScenario = _scenarios[scenarioName];
@@ -44,17 +44,12 @@ void _handlePlatformMessage(String name, ByteData data, PlatformMessageResponseC
       callback(data);
     }
   } else if (name == 'write_timeline') {
-    assert(data != null);
-    final String outputFileName = utf8.decode(data.buffer.asUint8List());
-    _writeTimelineData(outputFileName).then((_) {
-      final ByteData data = ByteData(1);
-      data.setUint8(0, 1);
-      callback(data);
-    });
+    final String timelineData = await _getTimelineData();
+    callback(Uint8List.fromList(utf8.encode(timelineData)).buffer.asByteData());
   }
 }
 
-Future<void> _writeTimelineData(String outputFileName) async {
+Future<String> _getTimelineData() async {
   final String isolateId = developer.Service.getIsolateID(Isolate.current);
   final developer.ServiceProtocolInfo info = await developer.Service.getInfo();
   final Uri cpuProfileTimelineUri = info.serverUri.resolve(
@@ -66,10 +61,10 @@ Future<void> _writeTimelineData(String outputFileName) async {
   final Map<String, dynamic> cpuResult = cpuTimelineJson['result'].cast<String, dynamic>();
   final Map<String, dynamic> vmServiceResult = vmServiceTimelineJson['result'].cast<String, dynamic>();
 
-  await File(outputFileName).writeAsString(json.encode(<String, dynamic>{
+  return json.encode(<String, dynamic>{
     'stackFrames': cpuResult['stackFrames'],
     'traceEvents': <dynamic>[...cpuResult['traceEvents'], ...vmServiceResult['traceEvents']],
-  }));
+  });
 }
 
 Future<Map<String, dynamic>> _getJson(Uri uri) async {
