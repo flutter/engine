90d687c81e65e52c195589970c6d1c3e9eb193f8
Kaushik Iska <kaushikiska@google.com>
2019-07-26 08:55:59 -0700

    fix references

--- tools/licenses/lib/main.dart
+++ tools/licenses/lib/main.dart
@@ -933,9 +933,11 @@ class _RepositoryDirectory extends _RepositoryEntry implements LicenseSource {
             entry.name != '.vscode' &&
             entry.name != 'test' &&
             entry.name != 'test.disabled' &&
+            entry.name != 'bin' &&
             entry.name != 'test_support' &&
             entry.name != 'tests' &&
             entry.name != 'javatests' &&
+            entry.name != '.DS_Store' &&
             entry.name != 'testing';
   }
 
