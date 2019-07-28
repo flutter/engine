77cb2a16f45170e4a302d0747dbb297e955fb8d3
James Clarke <james@clarkezone.net>
2019-07-28 05:32:39 -0700

    Format happy

--- shell/platform/windows/flutter_window.cc
+++ shell/platform/windows/flutter_window.cc
@@ -99,9 +99,9 @@ void FlutterWindow::HandlePlatformMessage(
 
   auto message = ConvertToDesktopMessage(*engine_message);
 
-  message_dispatcher_->HandleMessage(message,
-                                     [this] { this->process_events_ = false; },
-                                     [this] { this->process_events_ = true; });
+  message_dispatcher_->HandleMessage(
+      message, [this] { this->process_events_ = false; },
+      [this] { this->process_events_ = true; });
 }
 
 // When DesktopWindow notifies that a WM_Size message has come in
