49cf2b36fc554c06304ca09fbffe76f5594cbbed
Samir Jindel <sjindel@google.com>
2019-07-31 21:57:47 +0200

    Add licenses

--- runtime/ptrace_ios.cc
+++ runtime/ptrace_ios.cc
@@ -1,3 +1,7 @@
+// Copyright 2019 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // It is __imperative__ that the functions in this file are __not__ included in
 // release or profile builds.
 //
