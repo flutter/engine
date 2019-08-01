9c777bd6dcbe9f1d8141a3be5e958fb71cc53006
cfontas <cfontas@google.com>
2019-07-31 20:05:00 -0700

    Remove one last final call to AddPart() (#10273)
    * Remove one last final call to AddPart()
    
    * fix merge conflict
    
    * fix how i merged
    
    * remove add part again
    
    * remove pointer syntax
    
    * fix formatting

--- flow/scene_update_context.cc
+++ flow/scene_update_context.cc
@@ -289,6 +289,7 @@ SceneUpdateContext::Shape::Shape(SceneUpdateContext& context)
   entity_node().AddChild(shape_node_);
 }
 
+
 SceneUpdateContext::Frame::Frame(SceneUpdateContext& context,
                                  const SkRRect& rrect,
                                  SkColor color,
