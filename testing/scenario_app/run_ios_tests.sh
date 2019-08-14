11942d01a232767c85c753e2af05ec39eb433e88
Dan Field <dfield@gmail.com>
2019-08-14 10:06:34 -0700

    Update run_ios_tests.sh

--- testing/scenario_app/run_ios_tests.sh
+++ testing/scenario_app/run_ios_tests.sh
@@ -15,7 +15,7 @@ fi
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
 
-./compile_ios_jit.sh ../../../out/host_debug_unopt ../../../out/ios_debug_sim/clang_x64
+./compile_ios_jit.sh ../../../out/host_debug_unopt ../../../out/$FLUTTER_ENGINE/clang_x64
 
 pushd ios/Scenarios
 
