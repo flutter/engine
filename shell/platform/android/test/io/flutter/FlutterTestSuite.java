7dac45401751b32b74e8b964670691243c3661ce
Shi Hao Hong <shihaohong94@gmail.com>
2019-08-15 14:06:08 -0700

    Remove changes to FlutterTestSuite.java

--- shell/platform/android/test/io/flutter/FlutterTestSuite.java
+++ shell/platform/android/test/io/flutter/FlutterTestSuite.java
@@ -4,10 +4,6 @@
 
 package io.flutter;
 
-import io.flutter.SmokeTest;
-import io.flutter.util.PreconditionsTest;
-import io.flutter.embedding.android.FlutterActivityAndFragmentDelegateTest;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
