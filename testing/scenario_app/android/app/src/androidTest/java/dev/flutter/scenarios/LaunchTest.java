e8bd656932779b0bbebe779ec1a24e2203b6d508
Xiao Yu <xiao@xster.net>
2019-09-07 03:27:05 -0700

    make tests work

--- testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenarios/LaunchTest.java
+++ testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenarios/LaunchTest.java
@@ -2,59 +2,60 @@ package dev.flutter.scenarios;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.internal.runner.junit4.statement.UiThreadStatement;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.runner.Description;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runner.Runner;
-import org.junit.runner.notification.Failure;
-import org.junit.runner.notification.RunNotifier;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicReference;
 
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.embedding.engine.dart.DartExecutor;
 
-@RunWith(AndroidJUnit4.class)
-public class LaunchTest extends Runner {
-    @Override
-    public Description getDescription() {
-        return Description.createTestDescription(BlankActivity.class, "can launch engine");
-    }
+import static org.junit.Assert.fail;
 
-    @Override
-    public void run(RunNotifier notifier) {
-        notifier.fireTestStarted(getDescription());
+@RunWith(AndroidJUnit4.class)
+public class LaunchTest  {
+    @Test
+    public void testEngineLaunch() throws Throwable {
         Context applicationContext = InstrumentationRegistry.getTargetContext();
         // Specifically, create the engine without running FlutterMain first.
-        FlutterEngine engine = new FlutterEngine(applicationContext);
+        final AtomicReference<FlutterEngine> engine = new AtomicReference<>();
+
+        // Run the production under test on the UI thread instead of annotating the whole test
+        // as @UiThreadTest because having the message handler and the CompletableFuture both being
+        // on the same thread will create deadlocks.
+        UiThreadStatement.runOnUiThread(
+            () -> engine.set(new FlutterEngine(applicationContext))
+        );
         CompletableFuture<Boolean> statusReceived = new CompletableFuture<>();
 
-        engine.getDartExecutor().setMessageHandler(
+        engine.get().getDartExecutor().setMessageHandler(
             "scenario_status",
             (byteBuffer, binaryReply) -> statusReceived.complete(Boolean.TRUE)
         );
 
-        engine.getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault());
+        UiThreadStatement.runOnUiThread(
+            () -> engine.get().getDartExecutor().executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
+        );
 
         try {
             Boolean result = statusReceived.get(10, TimeUnit.SECONDS);
             if (!result) {
-                notifier.fireTestFailure(
-                        new Failure(getDescription(),
-                        new Exception("expected message on scenario_status not received")));
+                fail("expected message on scenario_status not received");
             }
         } catch (ExecutionException e) {
-            notifier.fireTestFailure(new Failure(getDescription(), e));
+            fail(e.getMessage());
         } catch (InterruptedException e) {
-            notifier.fireTestFailure(new Failure(getDescription(), e));
+            fail(e.getMessage());
         } catch (TimeoutException e) {
-            notifier.fireTestFailure(
-                    new Failure(getDescription(),
-                    new Exception("timed out waiting for engine started signal")));
+            fail("timed out waiting for engine started signal");
         }
+        // If it gets to here, statusReceived is true.
     }
 }
