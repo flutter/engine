bd53f5d7b0f50ebec70d37333e65c45781c67c76
Francisco Madgaleno <franciscojma86@gmail.com>
2019-09-25 17:02:23 -0700

    Format

--- shell/platform/windows/win32_flutter_window.cc
+++ shell/platform/windows/win32_flutter_window.cc
@@ -116,8 +116,8 @@ void Win32FlutterWindow::HandlePlatformMessage(
   auto message = ConvertToDesktopMessage(*engine_message);
 
   message_dispatcher_->HandleMessage(
-    message, [this] { this->process_events_ = false; },
-    [this] { this->process_events_ = true; });
+      message, [this] { this->process_events_ = false; },
+      [this] { this->process_events_ = true; });
 }
 
 void Win32FlutterWindow::OnDpiScale(unsigned int dpi){};
