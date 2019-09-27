060de29e39072b40575245f31fa0232cabfeeca2
Aaron Clarke <aaclarke@google.com>
2019-09-27 13:07:08 -0700

    Turned arc on for FlutterEngineTest.

--- shell/platform/darwin/ios/framework/Source/FlutterEngineTest.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterEngineTest.mm
@@ -6,6 +6,15 @@
 #import <XCTest/XCTest.h>
 #import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterEngine.h"
 
+#ifndef __has_feature
+#define __has_feature(x) 0 /* for non-clang compilers */
+#endif
+
+#if !__has_feature(objc_arc)
+#error ARC must be enabled!
+#endif
+
+
 @interface FlutteEngineTest : XCTestCase
 @end
 
@@ -19,15 +28,15 @@
 
 - (void)testCreate {
   id project = OCMClassMock([FlutterDartProject class]);
-  FlutterEngine* engine = [[[FlutterEngine alloc] initWithName:@"foobar"
-                                                       project:project] autorelease];
+  FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"
+                                                      project:project];
   XCTAssertNotNil(engine);
 }
 
 - (void)testSendMessageBeforeRun {
   id project = OCMClassMock([FlutterDartProject class]);
-  FlutterEngine* engine = [[[FlutterEngine alloc] initWithName:@"foobar"
-                                                       project:project] autorelease];
+  FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"
+                                                      project:project];
   XCTAssertNotNil(engine);
   XCTAssertThrows([engine.binaryMessenger
       sendOnChannel:@"foo"
@@ -37,8 +46,8 @@
 
 - (void)testSetMessageHandlerBeforeRun {
   id project = OCMClassMock([FlutterDartProject class]);
-  FlutterEngine* engine = [[[FlutterEngine alloc] initWithName:@"foobar"
-                                                       project:project] autorelease];
+  FlutterEngine* engine = [[FlutterEngine alloc] initWithName:@"foobar"
+                                                      project:project];
   XCTAssertNotNil(engine);
   XCTAssertThrows([engine.binaryMessenger
       setMessageHandlerOnChannel:@"foo"
