f142c7922a4826c4403629e5e199874f4dda0d7f
Dan Field <dnfield@google.com>
2019-09-30 10:35:01 -0700

    make script more flexible

--- ci/build_flutter_runner_tests.sh
+++ ci/build_flutter_runner_tests.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 set -ex
 
+build_mode=${1:-debug}
+build_arch=${2:-x64}
+build_variant="fuchsia_${build_mode}_${build_arch}"
+
 PATH="$HOME/depot_tools:$PATH"
 cd ..
 
 # Build the flutter runner tests far directory
-flutter/tools/gn --fuchsia --no-lto --runtime-mode debug
-ninja -C out/fuchsia_debug_x64 flutter/shell/platform/fuchsia/flutter:flutter_runner_tests
+flutter/tools/gn --fuchsia --no-lto --runtime-mode $build_mode --fuchsia-cpu $build_arch
+ninja -C out/${build_variant} flutter/shell/platform/fuchsia/flutter:flutter_runner_tests
 
 # Generate the far package
-flutter/tools/fuchsia/gen_package.py\
-  --pm-bin $PWD/fuchsia/sdk/linux/tools/pm\
-  --package-dir $PWD/out/fuchsia_debug_x64/flutter_runner_tests_far\
-  --signing-key $PWD/flutter/tools/fuchsia/development.key\
+flutter/tools/fuchsia/gen_package.py                                \
+--pm-bin $PWD/fuchsia/sdk/linux/tools/pm                            \
+  --package-dir $PWD/out/${build_variant}/flutter_runner_tests_far  \
+  --signing-key $PWD/flutter/tools/fuchsia/development.key          \
   --far-name flutter_runner_tests
 
