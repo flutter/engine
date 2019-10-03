4b2ab643829427f616df0a929141d0d5cfd40df2
Dan Field <dnfield@google.com>
2019-10-03 15:16:26 -0700

    print errors

--- tools/fuchsia/gen_package.py
+++ tools/fuchsia/gen_package.py
@@ -40,11 +40,18 @@ def CreateFarPackage(pm_bin, package_dir, signing_key, dst_dir):
   ]
 
   # Build the package
-  subprocess.check_call(pm_command_base + ['build'])
+  try:
+    subprocess.check_output(pm_command_base + ['build'])
+  except subprocess.CalledProcessError as e:
+    print("pm output: " + e.output)
+    raise
 
   # Archive the package
-  subprocess.check_call(pm_command_base + ['archive'])
-
+  try:
+    subprocess.check_output(pm_command_base + ['archive'])
+  except subprocess.CalledProcessError as e:
+    print("pm output: " + e.output)
+    raise
   return 0
 
 
