a680d037c8b0e454e69fe82779bfce0ef7931e46
jonahwilliams <jonahwilliams@google.com>
2019-10-04 10:49:51 -0700

    teach linux desktop to load so in aot runtime

--- shell/platform/glfw/flutter_glfw.cc
+++ shell/platform/glfw/flutter_glfw.cc
@@ -12,6 +12,7 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "flutter/common/runtime.h"
 #include "flutter/shell/platform/common/cpp/client_wrapper/include/flutter/plugin_registrar.h"
 #include "flutter/shell/platform/common/cpp/incoming_message_dispatcher.h"
 #include "flutter/shell/platform/embedder/embedder.h"
@@ -521,6 +522,12 @@ static FLUTTER_API_SYMBOL(FlutterEngine)
     config.open_gl.make_resource_current = GLFWMakeResourceContextCurrent;
     config.open_gl.gl_proc_resolver = GLFWProcResolver;
   }
+#if !FLUTTER_JIT_RUNTIME
+  std::string shared_library_path("--aot-shared-library-name=");
+  shared_library_path.append(engine_properties.assets_path);
+  shared_library_path.append("/libapp.so");
+  argv.insert(argv.end(), shared_library_path.c_str());
+#endif // !FLUTTER_JIT_RUNTIME
   FlutterProjectArgs args = {};
   args.struct_size = sizeof(FlutterProjectArgs);
   args.assets_path = engine_properties.assets_path;
