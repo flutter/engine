b9c5c5281b6f238b296a20da05e6cf111eb875ba
CloudWebRTC <duanweiwei1982@gmail.com>
2019-10-10 23:12:31 +0800

    Add arguments for generate.py.

--- third_party/glad/generate.py
+++ third_party/glad/generate.py
@@ -3,14 +3,24 @@
 
 import os
 import sys
+import argparse
 
-def main(argv):
+def generate(output):
     dirname, _ = os.path.split(os.path.abspath(__file__))
-    output_path = argv[1]
     command = 'cd ' + dirname + '/scripts;'
-    command += 'python -m glad --profile="compatibility" --api="gl=3.3" --generator="c" --spec="gl" --extensions="" --reproducible --out-path='
-    command += output_path
+    command += 'python -m glad --profile="compatibility" --api="gl=3.3" --generator="c" --spec="gl" --extensions="" --reproducible --quiet --out-path='
+    command += output
     os.system(command)
 
+
+def main():
+    parser = argparse.ArgumentParser();
+    parser.add_argument('-o', '--output', dest='output',
+      required=True, help='The output directory for coverage results.')
+    args = parser.parse_args()
+    output = os.path.abspath(args.output)
+
+    generate(output)
+
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
