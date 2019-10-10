7340fc21aaebe740ea17fbda1805543f67fd394a
Dan Field <dnfield@google.com>
2019-10-09 23:18:16 -0700

    review

--- build/zip.py
+++ build/zip.py
@@ -18,8 +18,8 @@ def _zip_dir(path, zip_file, prefix):
 
 
 def main(args):
-  zip_file = zipfile.ZipFile(args.o, 'w', zipfile.ZIP_DEFLATED)
-  for path, archive_name in args.i:
+  zip_file = zipfile.ZipFile(args.output, 'w', zipfile.ZIP_DEFLATED)
+  for path, archive_name in args.input_pairs:
     if os.path.isdir(path):
       _zip_dir(path, zip_file, archive_name)
     else:
@@ -30,8 +30,8 @@ def main(args):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(
       description='This script creates zip files.')
-  parser.add_argument('-o', action='store',
+  parser.add_argument('-o', dest='output' action='store',
       help='The name of the output zip file.')
-  parser.add_argument('-i', nargs=2, action='append',
+  parser.add_argument('-i', dest='input_pairs', nargs=2, action='append',
       help='The input file and its destination location in the zip archive.')
   sys.exit(main(parser.parse_args()))
