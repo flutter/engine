54d3267f0f127e7faeefca9ff60c6d6afdd23014
Nurhan Turgut <nurhan@google.com>
2019-10-21 16:23:06 -0700

    uncomment firefox.dart. Adding new CL parameters.

--- lib/web_ui/dev/firefox.dart
+++ lib/web_ui/dev/firefox.dart
@@ -1,82 +1,67 @@
+import 'dart:async';
+import 'dart:io';
 
-// import 'dart:async';
-// import 'dart:io';
+import 'package:pedantic/pedantic.dart';
 
-// import 'package:pedantic/pedantic.dart';
+import 'package:test_core/src/util/io.dart'; // ignore: implementation_imports
 
-// import 'package:test_core/src/util/io.dart'; // ignore: implementation_imports
+import 'browser.dart';
+import 'firefox_installer.dart';
+import 'common.dart';
 
-// import 'browser.dart';
-// import 'firefox_installer.dart';
-// import 'common.dart';
+/// A class for running an instance of Firefox.
+///
+/// Most of the communication with the browser is expected to happen via HTTP,
+/// so this exposes a bare-bones API. The browser starts as soon as the class is
+/// constructed, and is killed when [close] is called.
+///
+/// Any errors starting or running the process are reported through [onExit].
+class Firefox extends Browser {
+  @override
+  final name = 'Firefox';
 
-// /// A class for running an instance of Firefox.
-// ///
-// /// Most of the communication with the browser is expected to happen via HTTP,
-// /// so this exposes a bare-bones API. The browser starts as soon as the class is
-// /// constructed, and is killed when [close] is called.
-// ///
-// /// Any errors starting or running the process are reported through [onExit].
-// class Firefox extends Browser {
-//   @override
-//   final name = 'Firefox';
+  @override
+  final Future<Uri> remoteDebuggerUrl;
 
-//   @override
-//   final Future<Uri> remoteDebuggerUrl;
+  static String version;
 
-//   static String version;
+  /// Starts a new instance of Firefox open to the given [url], which may be a
+  /// [Uri] or a [String].
+  factory Firefox(Uri url, {bool debug = false}) {
+    assert(version != null);
+    var remoteDebuggerCompleter = Completer<Uri>.sync();
+    return Firefox._(() async {
+      final BrowserInstallation installation = await getOrInstallFirefox(
+        version,
+        infoLog: isCirrus ? stdout : DevNull(),
+      );
 
-//   /// Starts a new instance of Firefox open to the given [url], which may be a
-//   /// [Uri] or a [String].
-//   factory Firefox(Uri url, {bool debug = false}) {
-//     assert(version != null);
-//     var remoteDebuggerCompleter = Completer<Uri>.sync();
-//     return Firefox._(() async {
-//       final BrowserInstallation installation = await getOrInstallFirefox(
-//         version,
-//         infoLog: isCirrus ? stdout : DevNull(),
-//       );
+      // A good source of various Firefox Command Line options:
+      // https://developer.mozilla.org/en-US/docs/Mozilla/Command_Line_Options#Browser
+      //
+      var dir = createTempDir();
+      var args = [
+        url.toString(),
+        if (!debug) '--headless',
+        '-width $kMaxScreenshotWidth'
+        '-height $kMaxScreenshotHeight',
+        '-new-window',
+        '-new-instance',
+        '--start-debugger-server $kDevtoolsPort',
+      ];
 
-//       // A good source of various Firefox CLI options:
-//       // https://peter.sh/experiments/chromium-command-line-switches/
-//       //
-//       // Things to try:
-//       // --font-render-hinting
-//       // --enable-font-antialiasing
-//       // --gpu-rasterization-msaa-sample-count
-//       // --disable-gpu
-//       // --disallow-non-exact-resource-reuse
-//       // --disable-font-subpixel-positioning
-//       final bool isFirefoxNoSandbox = Platform.environment['CHROME_NO_SANDBOX'] == 'true';
-//       var dir = createTempDir();
-//       var args = [
-//         '--user-data-dir=$dir',
-//         url.toString(),
-//         if (!debug) '--headless',
-//         if (isFirefoxNoSandbox) '--no-sandbox',
-//         '--window-size=$kMaxScreenshotWidth,$kMaxScreenshotHeight', // When headless, this is the actual size of the viewport
-//         '--disable-extensions',
-//         '--disable-popup-blocking',
-//         '--bwsi',
-//         '--no-first-run',
-//         '--no-default-browser-check',
-//         '--disable-default-apps',
-//         '--disable-translate',
-//         '--remote-debugging-port=$kDevtoolsPort',
-//       ];
+      final Process process = await Process.start(installation.executable, args);
 
-//       final Process process = await Process.start(installation.executable, args);
+      remoteDebuggerCompleter.complete(getRemoteDebuggerUrl(
+          Uri.parse('http://localhost:$kDevtoolsPort')));
 
-//       remoteDebuggerCompleter.complete(getRemoteDebuggerUrl(
-//           Uri.parse('http://localhost:$_kFirefoxDevtoolsPort')));
+      unawaited(process.exitCode
+          .then((_) => Directory(dir).deleteSync(recursive: true)));
 
-//       unawaited(process.exitCode
-//           .then((_) => Directory(dir).deleteSync(recursive: true)));
+      return process;
+    }, remoteDebuggerCompleter.future);
+  }
 
-//       return process;
-//     }, remoteDebuggerCompleter.future);
-//   }
-
-//   Firefox._(Future<Process> startBrowser(), this.remoteDebuggerUrl)
-//       : super(startBrowser);
-// }
+  Firefox._(Future<Process> startBrowser(), this.remoteDebuggerUrl)
+      : super(startBrowser);
+}
