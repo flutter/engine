0772e1043f50a92bc34ab33e4d3e63c68376bddc
Aaron Clarke <aaclarke@google.com>
2019-10-22 10:17:27 -0700

    fixed weakptr assert in unittest

--- shell/common/shell_unittests.cc
+++ shell/common/shell_unittests.cc
@@ -214,13 +214,19 @@ TEST_F(ShellTest, LastEntrypoint) {
   configuration.SetEntrypoint(entry_point);
 
   fml::AutoResetWaitableEvent main_latch;
+  std::string last_entry_point;
   AddNativeCallback(
-      "SayHiFromFixturesAreFunctionalMain",
-      CREATE_NATIVE_ENTRY([&main_latch](auto args) { main_latch.Signal(); }));
+      "SayHiFromFixturesAreFunctionalMain", CREATE_NATIVE_ENTRY([&](auto args) {
+        last_entry_point = shell->GetEngine()->GetLastEntrypoint();
+        main_latch.Signal();
+      }));
 
   RunEngine(shell.get(), std::move(configuration));
   main_latch.Wait();
-  EXPECT_EQ(entry_point, shell->GetEngine()->GetLastEntrypoint());
+  EXPECT_EQ(entry_point, last_entry_point);
+  ASSERT_TRUE(DartVMRef::IsInstanceRunning());
+  DestroyShell(std::move(shell));
+  ASSERT_FALSE(DartVMRef::IsInstanceRunning());
 }
 
 TEST(ShellTestNoFixture, EnableMirrorsIsWhitelisted) {
