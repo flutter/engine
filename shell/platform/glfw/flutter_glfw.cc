89848a880c8dcb0f64cd5d59a4928c7520db8941
jonahwilliams <jonahwilliams@google.com>
2019-10-23 19:15:51 -0700

    use runtime flag

--- shell/platform/glfw/flutter_glfw.cc
+++ shell/platform/glfw/flutter_glfw.cc
@@ -521,12 +521,13 @@ static FLUTTER_API_SYMBOL(FlutterEngine)
     config.open_gl.make_resource_current = GLFWMakeResourceContextCurrent;
     config.open_gl.gl_proc_resolver = GLFWProcResolver;
   }
-#ifndef NDEBUG
-  std::string shared_library_path("--aot-shared-library-name=");
-  shared_library_path.append(engine_properties.assets_path);
-  shared_library_path.append("/libapp.so");
-  argv.insert(argv.end(), shared_library_path.c_str());
-#endif  // NDEBUG
+  // Provide path to .so in AOT configurations.
+  if (FlutterEngineRunsAOTCompiledDartCode()) {
+    std::string shared_library_path("--aot-shared-library-name=");
+    shared_library_path.append(engine_properties.assets_path);
+    shared_library_path.append("/libapp.so");
+    argv.insert(argv.end(), shared_library_path.c_str());
+  }
   FlutterProjectArgs args = {};
   args.struct_size = sizeof(FlutterProjectArgs);
   args.assets_path = engine_properties.assets_path;
