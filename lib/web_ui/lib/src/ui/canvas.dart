a03e6a8be04bca1a9dd2daf56fd922420412e507
Yegor Jbanov <yjbanov@google.com>
2019-10-28 10:52:36 -0700

    Implement basic Picture.toImage via BitmapCanvas

--- lib/web_ui/lib/src/ui/canvas.dart
+++ lib/web_ui/lib/src/ui/canvas.dart
@@ -1068,7 +1068,20 @@ class Picture {
   ///
   /// Although the image is returned synchronously, the picture is actually
   /// rasterized the first time the image is drawn and then cached.
-  Future<Image> toImage(int width, int height) => null;
+  Future<Image> toImage(int width, int height) async {
+    final engine.BitmapCanvas canvas = engine.BitmapCanvas(Rect.fromLTRB(0, 0, width.toDouble(), height.toDouble()));
+    recordingCanvas.apply(canvas);
+    final String imageDataUrl = canvas.canvas.toDataUrl();
+    final html.ImageElement imageElement = html.ImageElement()
+      ..src = imageDataUrl
+      ..width = width
+      ..height = height;
+    return engine.HtmlImage(
+      imageElement,
+      width,
+      height,
+    );
+  }
 
   /// Release the resources used by this object. The object is no longer usable
   /// after this method is called.
