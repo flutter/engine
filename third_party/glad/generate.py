056a1c7b7737198deac6d325572e19c34cf566d2
CloudWebRTC <duanweiwei1982@gmail.com>
2019-11-14 23:29:28 +0800

    Add comment for generate.py.

--- third_party/glad/generate.py
+++ third_party/glad/generate.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
+# GN can't run python scripts in `python -m scripts/glad` mode,
+# so the generate.py file seems to be necessary.
+
 import os
 import sys
 sys.path.append(os.path.split(os.path.abspath(__file__))[0] + '/scripts')
