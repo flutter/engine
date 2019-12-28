373b6e81a9a606e4052b86f62e2e5ea8c03b2a21
Dan Field <dnfield@google.com>
2019-12-28 07:21:36 -0800

    ..

--- tools/font-subset/test.py
+++ tools/font-subset/test.py
@@ -30,7 +30,6 @@ COMPARE_TESTS = (
   (True,  '3.ttf', MATERIAL_TTF, [r'0xE003', r'0xE004', r'57347',]),
 )
 
-
 FAIL_TESTS = [
   [FONT_SUBSET, 'output.ttf', 'does-not-exist.ttf', '1'], # non-existant input font
   [FONT_SUBSET, 'output.ttf', MATERIAL_TTF, '0xFFFFFFFF'], # Value too big.
@@ -39,18 +38,24 @@ FAIL_TESTS = [
   [FONT_SUBSET, 'output.ttf', MATERIAL_TTF, r'0xE003', r'0x12', r'0xE004',] # codepoint not in font
 ]
 
+def RunCmd(cmd, **kwargs):
+  try:
+    print(subprocess.check_output(cmd, **kwargs))
+  except subprocess.CalledProcessError as cpe:
+    print(cpe.output)
+    raise cpe
+
 
 def main():
-  subprocess.check_output(['python', 'flutter/tools/gn'], cwd=SRC_DIR)
-  subprocess.check_output(['autoninja' + BAT, '-C', 'out/host_debug', 'font-subset'],
-                          cwd=SRC_DIR)
+  RunCmd(['python', 'flutter/tools/gn'], cwd=SRC_DIR)
+  RunCmd(['autoninja' + BAT, '-C', 'out/host_debug', 'font-subset'], cwd=SRC_DIR)
   failures = 0
   for should_pass, golden_font, input_font, codepoints in COMPARE_TESTS:
     gen_ttf = os.path.join(SCRIPT_DIR, 'gen', golden_font)
     golden_ttf = os.path.join(SCRIPT_DIR, 'fixtures', golden_font)
     cmd = [FONT_SUBSET, gen_ttf, input_font] + codepoints
     print('Running test %s...' % cmd)
-    subprocess.check_output(cmd, cwd=SRC_DIR)
+    RunCmd(cmd, cwd=SRC_DIR)
     cmp = filecmp.cmp(gen_ttf, golden_ttf, shallow=False)
     if (should_pass and not cmp) or (not should_pass and cmp):
       print('Test case %s failed.' % cmd)
