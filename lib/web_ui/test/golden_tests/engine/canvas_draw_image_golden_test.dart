90912002c2db4ecb2b2ea203b88588ecb423e7b1
ferhatb <ferhat@gmail.com>
2020-01-02 15:30:19 -0800

    remove goldens write flag

--- lib/web_ui/test/golden_tests/engine/canvas_draw_image_golden_test.dart
+++ lib/web_ui/test/golden_tests/engine/canvas_draw_image_golden_test.dart
@@ -30,7 +30,7 @@ void main() async {
     try {
       sceneElement.append(engineCanvas.rootElement);
       html.document.body.append(sceneElement);
-      await matchGoldenFile('$fileName.png', region: region, maxDiffRate: 0.2, write: true);
+      await matchGoldenFile('$fileName.png', region: region, maxDiffRate: 0.2);
     } finally {
       // The page is reused across tests, so remove the element after taking the
       // Scuba screenshot.
