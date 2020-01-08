94281636a86d5629cf329617ee4222bc6cb92ad2
Mouad Debbar <mdebbar@google.com>
2020-01-08 14:00:27 -0800

    [web] Tests for browser history implementation

---
+++ lib/web_ui/test/spy.dart
@@ -0,0 +1,41 @@
+import 'dart:typed_data';
+
+import 'package:ui/src/engine.dart' hide window;
+import 'package:ui/ui.dart';
+
+class PlatformMessage {
+  PlatformMessage(this.channel, this.methodCall);
+
+  final String channel;
+  final MethodCall methodCall;
+
+  String get methodName => methodCall.method;
+  String get methodArguments => methodCall.arguments;
+}
+
+class PlatformMessagesSpy {
+  bool _isActive = false;
+  PlatformMessageCallback _backup;
+
+  final List<PlatformMessage> messages = <PlatformMessage>[];
+
+  void activate() {
+    assert(!_isActive);
+    _isActive = true;
+    _backup = window.onPlatformMessage;
+    window.onPlatformMessage = (String channel, ByteData data,
+        PlatformMessageResponseCallback callback) {
+      messages.add(PlatformMessage(
+        channel,
+        const JSONMethodCodec().decodeMethodCall(data),
+      ));
+    };
+  }
+
+  void deactivate() {
+    assert(_isActive);
+    _isActive = false;
+    messages.clear();
+    window.onPlatformMessage = _backup;
+  }
+}
