109519a9e31e0d0f10d8d257b0aad5f5b1b8d882
Nurhan Turgut <nurhan@google.com>
2020-01-16 09:13:39 -0800

    edge launcher for windows

--- lib/web_ui/dev/edge_installation.dart
+++ lib/web_ui/dev/edge_installation.dart
@@ -6,8 +6,11 @@ import 'dart:async';
 import 'dart:io' as io;
 
 import 'package:args/args.dart';
+import 'package:http/http.dart';
+import 'package:path/path.dart' as path;
 
 import 'common.dart';
+import 'environment.dart';
 
 class EdgeArgParser extends BrowserArgParser {
   static final EdgeArgParser _singletonInstance = EdgeArgParser._();
@@ -68,12 +71,73 @@ Future<BrowserInstallation> getEdgeInstallation(
     // Since Edge is included in Windows, always assume there will be one on the
     // system.
     infoLog.writeln('Using the system version that is already installed.');
+    final EdgeLauncher edgeLauncher = EdgeLauncher();
+    if (edgeLauncher.isInstalled) {
+      infoLog.writeln('Launcher installation was skipped, already installed.');
+    } else {
+      infoLog.writeln('Installing MicrosoftEdgeLauncher');
+      await edgeLauncher.install();
+      infoLog.writeln(
+          'Installations complete. To launch it run ${edgeLauncher.executable}');
+    }
+
     return BrowserInstallation(
       version: 'system',
-      executable: PlatformBinding.instance.getCommandToRunEdge(),
+      executable: io.Directory(path.join(
+              edgeLauncher.launcherInstallationDir.path,
+              '${PlatformBinding.instance.getCommandToRunEdge()}'))
+          .path,
     );
   } else {
     infoLog.writeln('Unsupported version $requestedVersion.');
     throw UnimplementedError();
+  }
+}
+
+class EdgeLauncher {
+  /// Url for downloading  `MicrosoftEdgeLauncher`.
+  ///
+  /// Only useful in Windows, hence not added to [PlatformBinding].
+  final String windowsEdgeLauncherDownloadUrl =
+      'https://github.com/MicrosoftEdge/edge-launcher/releases/download/1.2.0.0/MicrosoftEdgeLauncher.exe';
+
+  /// Path to the directory contains `MicrosoftEdgeLauncher.exe`.
+  io.Directory get launcherInstallationDir => io.Directory(
+        path.join(environment.webUiDartToolDir.path, 'microsoftedgelauncher'),
+      );
+
+  io.File get executable => io.File(
+      path.join(launcherInstallationDir.path, 'MicrosoftEdgeLauncher.exe'));
+
+  bool get isInstalled => executable.existsSync();
+
+  /// For running Edge from a batch script executable `MicrosoftEdgeLauncher`
+  /// is used.
+  /// Install it if it does not exists in this system.
+  /// See: https://github.com/MicrosoftEdge/edge-launcher
+  void install() async {
+    // Checks if the  `MicrosoftEdgeLauncher` executable exists.
+    if (isInstalled) {
+      return;
+    }
+
+    // Create directory for download.
+    if (!launcherInstallationDir.existsSync()) {
+      launcherInstallationDir.createSync(recursive: true);
+    }
+
+    final Client client = Client();
+
+    try {
+      // Download executable from Github.
+      final StreamedResponse download = await client.send(Request(
+        'GET',
+        Uri.parse(windowsEdgeLauncherDownloadUrl),
+      ));
+
+      await download.stream.pipe(executable.openWrite());
+    } finally {
+      client.close();
+    }
   }
 }
