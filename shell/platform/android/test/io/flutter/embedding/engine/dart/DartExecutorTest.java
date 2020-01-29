1973eb771a071a39e912517a1bc46024474dc92c
Chris Bracken <chris@bracken.jp>
2020-01-29 10:47:19 -0800

    Migrate to org.mockito.ArgumentMatchers
    org.mockito.Matchers#anyInt is deprecated and replaced by the
    ArgumentMatchers implementation in Mockito 2.

--- shell/platform/android/test/io/flutter/embedding/engine/dart/DartExecutorTest.java
+++ shell/platform/android/test/io/flutter/embedding/engine/dart/DartExecutorTest.java
@@ -1,23 +1,20 @@
 package test.io.flutter.embedding.engine.dart;
 
-import android.content.res.AssetManager;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-
-import java.nio.ByteBuffer;
-
-import io.flutter.embedding.engine.FlutterJNI;
-import io.flutter.embedding.engine.dart.DartExecutor;
-
 import static junit.framework.TestCase.assertNotNull;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import android.content.res.AssetManager;
+import io.flutter.embedding.engine.FlutterJNI;
+import io.flutter.embedding.engine.dart.DartExecutor;
+import java.nio.ByteBuffer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 @Config(manifest=Config.NONE)
 @RunWith(RobolectricTestRunner.class)
