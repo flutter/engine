flutter/flutter

commit d9080029af4b917fd5813c1db7f96d509343e01e

Remove use of the blobs snapshot format from unittests (#12563)
* Remove uses of the "blobs" snapshot format in Engine tests.

* Comments

* Rebasing against updated ELF loader API.

shell/platform/embedder/BUILD.gn
@@ -127,6 +127,7 @@ if (current_toolchain == host_toolchain) {
       "$flutter_root/testing:skia",
       "//third_party/skia",
       "//third_party/tonic",
+      "//third_party/dart/runtime/bin:elf_loader",
     ]
   }
 }

shell/platform/embedder/tests/embedder_test_context.cc
@@ -6,6 +6,7 @@
 
 #include "flutter/runtime/dart_vm.h"
 #include "flutter/shell/platform/embedder/tests/embedder_assertions.h"
+#include "third_party/dart/runtime/bin/elf_loader.h"
 #include "third_party/skia/include/core/SkSurface.h"
 
 namespace flutter {
@@ -16,16 +17,33 @@ EmbedderTestContext::EmbedderTestContext(std::string assets_path)
       native_resolver_(std::make_shared<TestDartNativeResolver>()) {
   auto assets_dir = fml::OpenDirectory(assets_path_.c_str(), false,
                                        fml::FilePermission::kRead);
-  vm_snapshot_data_ =
-      fml::FileMapping::CreateReadOnly(assets_dir, "vm_snapshot_data");
-  isolate_snapshot_data_ =
-      fml::FileMapping::CreateReadOnly(assets_dir, "isolate_snapshot_data");
 
   if (flutter::DartVM::IsRunningPrecompiledCode()) {
-    vm_snapshot_instructions_ =
-        fml::FileMapping::CreateReadExecute(assets_dir, "vm_snapshot_instr");
-    isolate_snapshot_instructions_ = fml::FileMapping::CreateReadExecute(
-        assets_dir, "isolate_snapshot_instr");
+    std::string filename(assets_path_);
+    filename += "/app_elf_snapshot.so";
+
+    const uint8_t *vm_snapshot_data = nullptr,
+                  *vm_snapshot_instructions = nullptr,
+                  *isolate_snapshot_data = nullptr,
+                  *isolate_snapshot_instructions = nullptr;
+    const char* error = nullptr;
+
+    elf_library_handle_ =
+        Dart_LoadELF(filename.c_str(), /*file_offset=*/0, &error,
+                     &vm_snapshot_data, &vm_snapshot_instructions,
+                     &isolate_snapshot_data, &isolate_snapshot_instructions);
+
+    if (elf_library_handle_ != nullptr) {
+      vm_snapshot_data_.reset(new fml::NonOwnedMapping(vm_snapshot_data, 0));
+      vm_snapshot_instructions_.reset(
+          new fml::NonOwnedMapping(vm_snapshot_instructions, 0));
+      isolate_snapshot_data_.reset(
+          new fml::NonOwnedMapping(isolate_snapshot_data, 0));
+      isolate_snapshot_instructions_.reset(
+          new fml::NonOwnedMapping(isolate_snapshot_instructions, 0));
+    } else {
+      FML_LOG(WARNING) << "Could not load snapshot: " << error;
+    }
   }
 
   isolate_create_callbacks_.push_back(
@@ -37,7 +55,11 @@ EmbedderTestContext::EmbedderTestContext(std::string assets_path)
       });
 }
 
-EmbedderTestContext::~EmbedderTestContext() = default;
+EmbedderTestContext::~EmbedderTestContext() {
+  if (elf_library_handle_ != nullptr) {
+    Dart_UnloadELF(elf_library_handle_);
+  }
+}
 
 const std::string& EmbedderTestContext::GetAssetsPath() const {
   return assets_path_;

shell/platform/embedder/tests/embedder_test_context.h
@@ -17,6 +17,7 @@
 #include "flutter/shell/platform/embedder/tests/embedder_test_compositor.h"
 #include "flutter/testing/test_dart_native_resolver.h"
 #include "flutter/testing/test_gl_surface.h"
+#include "third_party/dart/runtime/bin/elf_loader.h"
 #include "third_party/skia/include/core/SkImage.h"
 
 namespace flutter {
@@ -70,10 +71,14 @@ class EmbedderTestContext {
   friend class EmbedderConfigBuilder;
 
   std::string assets_path_;
+
+  // Pieces of the Dart snapshot in ELF form, loaded by Dart's ELF library.
+  Dart_LoadedElf* elf_library_handle_ = nullptr;
   std::unique_ptr<fml::Mapping> vm_snapshot_data_;
   std::unique_ptr<fml::Mapping> vm_snapshot_instructions_;
   std::unique_ptr<fml::Mapping> isolate_snapshot_data_;
   std::unique_ptr<fml::Mapping> isolate_snapshot_instructions_;
+
   std::vector<fml::closure> isolate_create_callbacks_;
   std::shared_ptr<TestDartNativeResolver> native_resolver_;
   SemanticsNodeCallback update_semantics_node_callback_;

testing/testing.gni
@@ -125,26 +125,16 @@ template("dart_snapshot_aot") {
       invoker.dart_kernel,
     ]
 
-    vm_snapshot_data = "$target_gen_dir/assets/vm_snapshot_data"
-    vm_snapshot_instr = "$target_gen_dir/assets/vm_snapshot_instr"
-    isolate_snapshot_data = "$target_gen_dir/assets/isolate_snapshot_data"
-    isolate_snapshot_instr = "$target_gen_dir/assets/isolate_snapshot_instr"
+    # Custom ELF loader is used for Mac and Windows.
+    elf_object = "$target_gen_dir/assets/app_elf_snapshot.so"
 
-    outputs = [
-      vm_snapshot_data,
-      vm_snapshot_instr,
-      isolate_snapshot_data,
-      isolate_snapshot_instr,
-    ]
+    outputs = [elf_object]
 
     args = [
       "--causal_async_stacks",
       "--deterministic",
-      "--snapshot_kind=app-aot-blobs",
-      "--vm_snapshot_data=" + rebase_path(vm_snapshot_data),
-      "--vm_snapshot_instructions=" + rebase_path(vm_snapshot_instr),
-      "--isolate_snapshot_data=" + rebase_path(isolate_snapshot_data),
-      "--isolate_snapshot_instructions=" + rebase_path(isolate_snapshot_instr),
+      "--snapshot_kind=app-aot-elf",
+      "--elf=" + rebase_path(elf_object),
       rebase_path(invoker.dart_kernel),
     ]
 

tools/gn
@@ -281,7 +281,7 @@ def parse_args(args):
 
   parser.add_argument('--runtime-mode', type=str, choices=['debug', 'profile', 'release', 'jit_release'], default='debug')
   parser.add_argument('--interpreter', default=False, action='store_true')
-  parser.add_argument('--dart-debug', default=False, action='store_true', help='Enables assertsion in the Dart VM. ' +
+  parser.add_argument('--dart-debug', default=False, action='store_true', help='Enables assertions in the Dart VM. ' +
       'Does not affect optimization levels. If you need to disable optimizations in Dart, use --full-dart-debug')
   parser.add_argument('--full-dart-debug', default=False, action='store_true', help='Implies --dart-debug ' +
       'and also disables optimizations in the Dart VM making it easier to step through VM code in the debugger.')

flutter/engine

commit add2051af0811ed49ec7cc9bffe460640e1d2876

Fix runtime_unittest in AOT mode by loading AOT symbols from ELF loader.
This regressed when the symbols were no longer packed in discrete blobs but
instead moved to an ELF file.

Regressed in https://github.com/flutter/engine/commit/d9080029af4b917fd5813c1db7f96d509343e01e
Unnoticed because of https://github.com/flutter/flutter/issues/49733
Fixes https://github.com/flutter/flutter/issues/49763

runtime/BUILD.gn
@@ -147,6 +147,7 @@ source_set("runtime_unittests_common") {
     "$flutter_root/shell/common",
     "$flutter_root/testing:dart",
     "$flutter_root/third_party/tonic",
+    "//third_party/dart/runtime/bin:elf_loader",
     "//third_party/skia",
   ]
 }

runtime/runtime_test.cc
@@ -4,17 +4,65 @@
 
 #include "flutter/runtime/runtime_test.h"
 
+#include "flutter/fml/file.h"
+#include "flutter/fml/native_library.h"
+#include "flutter/fml/paths.h"
+#include "flutter/runtime/dart_snapshot.h"
 #include "flutter/runtime/dart_vm.h"
 #include "flutter/testing/testing.h"
 
 namespace flutter {
 namespace testing {
 
+static constexpr const char* kAOTAppELFFileName = "app_elf_snapshot.so";
+
+static ELFAOTSymbols LoadELFIfNecessary() {
+  if (!DartVM::IsRunningPrecompiledCode()) {
+    return {};
+  }
+
+  const auto elf_path =
+      fml::paths::JoinPaths({GetFixturesPath(), kAOTAppELFFileName});
+
+  if (!fml::IsFile(elf_path)) {
+    FML_LOG(ERROR) << "App AOT file does not exist for this fixture. Attempts "
+                      "to launch the Dart VM will fail.";
+    return {};
+  }
+
+  ELFAOTSymbols symbols;
+
+  // Must not be freed.
+  const char* error = nullptr;
+
+  auto loaded_elf =
+      Dart_LoadELF(elf_path.c_str(),             // file path
+                   0,                            // file offset
+                   &error,                       // error (out)
+                   &symbols.vm_snapshot_data,    // vm snapshot data (out)
+                   &symbols.vm_snapshot_instrs,  // vm snapshot instrs (out)
+                   &symbols.vm_isolate_data,     // vm isolate data (out)
+                   &symbols.vm_isolate_instrs    // vm isolate instr (out)
+      );
+
+  if (loaded_elf == nullptr) {
+    FML_LOG(ERROR) << "Could not fetch AOT symbols from loaded ELF. Attempts "
+                      "to launch the Dart VM will fail. Error: "
+                   << error;
+    return {};
+  }
+
+  symbols.loaded_elf.reset(loaded_elf);
+
+  return symbols;
+}
+
 RuntimeTest::RuntimeTest()
     : native_resolver_(std::make_shared<TestDartNativeResolver>()),
       assets_dir_(fml::OpenDirectory(GetFixturesPath(),
                                      false,
-                                     fml::FilePermission::kRead)) {}
+                                     fml::FilePermission::kRead)),
+      aot_symbols_(LoadELFIfNecessary()) {}
 
 void RuntimeTest::SetSnapshotsAndAssets(Settings& settings) {
   if (!assets_dir_.is_valid()) {
@@ -24,28 +72,25 @@ void RuntimeTest::SetSnapshotsAndAssets(Settings& settings) {
   settings.assets_dir = assets_dir_.get();
 
   // In JIT execution, all snapshots are present within the binary itself and
-  // don't need to be explicitly suppiled by the embedder.
+  // don't need to be explicitly supplied by the embedder. In AOT, these
+  // snapshots will be present in the application AOT dylib.
   if (DartVM::IsRunningPrecompiledCode()) {
-    settings.vm_snapshot_data = [this]() {
-      return fml::FileMapping::CreateReadOnly(assets_dir_, "vm_snapshot_data");
+    settings.vm_snapshot_data = [&]() {
+      return std::make_unique<fml::NonOwnedMapping>(
+          aot_symbols_.vm_snapshot_data, 0u);
     };
-
-    settings.isolate_snapshot_data = [this]() {
-      return fml::FileMapping::CreateReadOnly(assets_dir_,
-                                              "isolate_snapshot_data");
+    settings.isolate_snapshot_data = [&]() {
+      return std::make_unique<fml::NonOwnedMapping>(
+          aot_symbols_.vm_isolate_data, 0u);
     };
-
-    if (DartVM::IsRunningPrecompiledCode()) {
-      settings.vm_snapshot_instr = [this]() {
-        return fml::FileMapping::CreateReadExecute(assets_dir_,
-                                                   "vm_snapshot_instr");
-      };
-
-      settings.isolate_snapshot_instr = [this]() {
-        return fml::FileMapping::CreateReadExecute(assets_dir_,
-                                                   "isolate_snapshot_instr");
-      };
-    }
+    settings.vm_snapshot_instr = [&]() {
+      return std::make_unique<fml::NonOwnedMapping>(
+          aot_symbols_.vm_snapshot_instrs, 0u);
+    };
+    settings.isolate_snapshot_instr = [&]() {
+      return std::make_unique<fml::NonOwnedMapping>(
+          aot_symbols_.vm_isolate_instrs, 0u);
+    };
   } else {
     settings.application_kernels = [this]() {
       std::vector<std::unique_ptr<const fml::Mapping>> kernel_mappings;

runtime/runtime_test.h
@@ -11,9 +11,24 @@
 #include "flutter/fml/macros.h"
 #include "flutter/testing/test_dart_native_resolver.h"
 #include "flutter/testing/thread_test.h"
+#include "third_party/dart/runtime/bin/elf_loader.h"
 
 namespace flutter {
 namespace testing {
+
+struct LoadedELFDeleter {
+  void operator()(Dart_LoadedElf* elf) { Dart_UnloadELF(elf); }
+};
+
+using UniqueLoadedELF = std::unique_ptr<Dart_LoadedElf, LoadedELFDeleter>;
+
+struct ELFAOTSymbols {
+  UniqueLoadedELF loaded_elf;
+  const uint8_t* vm_snapshot_data = nullptr;
+  const uint8_t* vm_snapshot_instrs = nullptr;
+  const uint8_t* vm_isolate_data = nullptr;
+  const uint8_t* vm_isolate_instrs = nullptr;
+};
 
 class RuntimeTest : public ThreadTest {
  public:
@@ -24,10 +39,11 @@ class RuntimeTest : public ThreadTest {
   void AddNativeCallback(std::string name, Dart_NativeFunction callback);
 
  private:
-  void SetSnapshotsAndAssets(Settings& settings);
-
   std::shared_ptr<TestDartNativeResolver> native_resolver_;
   fml::UniqueFD assets_dir_;
+  ELFAOTSymbols aot_symbols_;
+
+  void SetSnapshotsAndAssets(Settings& settings);
 
   FML_DISALLOW_COPY_AND_ASSIGN(RuntimeTest);
 };