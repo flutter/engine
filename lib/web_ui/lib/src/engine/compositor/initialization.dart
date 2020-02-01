6d2c8b71a91b7a8ef334dd90bf77e278d3ab4315
Vijay Menon <vsm@google.com>
2020-01-31 16:47:48 -0800

    fix runtime type errors when running with canvaskit

--- lib/web_ui/lib/src/engine/compositor/initialization.dart
+++ lib/web_ui/lib/src/engine/compositor/initialization.dart
@@ -6,7 +6,7 @@ part of engine;
 
 /// EXPERIMENTAL: Enable the Skia-based rendering backend.
 const bool experimentalUseSkia =
-    bool.fromEnvironment('FLUTTER_WEB_USE_SKIA', defaultValue: false);
+    bool.fromEnvironment('FLUTTER_WEB_USE_SKIA', defaultValue: true);
 
 /// The URL to use when downloading the CanvasKit script and associated wasm.
 const String canvasKitBaseUrl = 'https://unpkg.com/canvaskit-wasm@0.11.0/bin/';
