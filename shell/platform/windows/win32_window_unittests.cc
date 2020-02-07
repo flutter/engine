589923751ecd9d9ab631c9f9615ab23435a48a0c
Francisco Madgaleno <franciscojma86@gmail.com>
2020-02-06 17:31:27 -0800

    Fix

--- shell/platform/windows/win32_window_unittests.cc
+++ shell/platform/windows/win32_window_unittests.cc
@@ -9,18 +9,10 @@ TEST(Win32WindowTest, CreateDestroy) {
   ASSERT_TRUE(TRUE);
 }
 
-<<<<<<< HEAD
 TEST(Win32WindowTest, GetDpiAfterCreate) {
   Win32WindowTest window;
   ASSERT_TRUE(window.GetDpi() > 0);
-=======
-TEST(Win32FlutterWindowTest, CanFontChange) {
-  Win32FlutterWindowTest window(800, 600);
-  HWND hwnd = window.GetWindowHandle();
-  LRESULT result = SendMessage(hwnd, WM_FONTCHANGE, NULL, NULL);
-  ASSERT_EQ(result, 0);
-  ASSERT_TRUE(window.OnFontChangeWasCalled());
->>>>>>> master
 }
+
 }  // namespace testing
 }  // namespace flutter
