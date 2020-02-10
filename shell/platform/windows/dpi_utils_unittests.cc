6b06bb2c1ee35ef545665f1484e82b98852fbb9d
Francisco Madgaleno <franciscojma86@gmail.com>
2020-02-10 14:36:34 -0800

    Add test

---
+++ shell/platform/windows/dpi_utils_unittests.cc
@@ -0,0 +1,18 @@
+#include "flutter/shell/platform/windows/dpi_utils.h"
+#include "gtest/gtest.h"
+
+namespace flutter {
+namespace testing {
+
+TEST(DpiUtilsTest, NonZero) {
+  ASSERT_GT(GetDpiForHWND(nullptr),0);
+  ASSERT_GT(GetDpiForMonitor(nullptr),0);
+};
+
+TEST(DpiUtilsTest, EqualDpis) {
+  ASSERT_EQ(GetDpiForHWND(nullptr), GetDpiForMonitor(nullptr));
+  ASSERT_EQ(GetDpiForHWND(nullptr), 96);
+};
+
+}  // namespace testing
+}  // namespace flutter
