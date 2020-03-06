326f339ec1ec0cdf04149419106370525bd0cd01
Emmanuel Garcia <egarciad@google.com>
2020-03-05 19:57:56 -0800

    Minor fixes

--- flow/layers/performance_overlay_layer_unittests.cc
+++ flow/layers/performance_overlay_layer_unittests.cc
@@ -64,8 +64,7 @@ static void TestPerformanceOverlayLayerGold(int refresh_rate) {
   flutter::TextureRegistry unused_texture_registry;
   flutter::Layer::PaintContext paintContext = {
       nullptr,        surface->getCanvas(),    nullptr, nullptr, mock_stopwatch,
-      mock_stopwatch, unused_texture_registry, nullptr, false,
-  };
+      mock_stopwatch, unused_texture_registry, nullptr, false};
 
   // Specify font file to ensure the same font across different operation
   // systems.
