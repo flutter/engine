78dc4a850439fd9ccbac2235565eee5fd296c22e
ferhatb <ferhat@gmail.com>
2020-03-05 16:06:43 -0800

    Fix fontfallback

--- lib/web_ui/lib/src/engine/text/font_collection.dart
+++ lib/web_ui/lib/src/engine/text/font_collection.dart
@@ -245,8 +245,9 @@ class _PolyfillFontManager extends FontManager {
     paragraph.style.position = 'absolute';
     paragraph.style.visibility = 'hidden';
     paragraph.style.fontSize = '72px';
-    paragraph.style.fontFamily = browserEngine == BrowserEngine.ie11 ?
-        'Times New Roman' : 'sans-serif';
+    final String fallbackFontName = browserEngine == BrowserEngine.ie11 ?
+      'Times New Roman' : 'sans-serif';
+    paragraph.style.fontFamily = fallbackFontName;
     if (descriptors['style'] != null) {
       paragraph.style.fontStyle = descriptors['style'];
     }
@@ -258,7 +259,7 @@ class _PolyfillFontManager extends FontManager {
     html.document.body.append(paragraph);
     final int sansSerifWidth = paragraph.offsetWidth;
 
-    paragraph.style.fontFamily = "'$family', sans-serif";
+    paragraph.style.fontFamily = "'$family', $fallbackFontName";
 
     final Completer<void> completer = Completer<void>();
 
