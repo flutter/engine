44a638bf3d1444d9dc00683c58bc4065ff0f7937
Kurun <robo_matsu_rhhqd142@yahoo.co.jp>
2020-03-07 20:45:47 +0900

    Initial commit

---
+++ shell/platform/common/cpp/client_wrapper/include/flutter/event_sink.h
@@ -0,0 +1,54 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_COMMON_CPP_CLIENT_WRAPPER_INCLUDE_FLUTTER_EVENT_SINK_H_
+#define FLUTTER_SHELL_PLATFORM_COMMON_CPP_CLIENT_WRAPPER_INCLUDE_FLUTTER_EVENT_SINK_H_
+
+namespace flutter {
+
+// Event callback. Supports dual use: Producers of events to be sent to Flutter act as clients of
+// this interface for sending events. Consumers of events sent from Flutter implement this
+// interface for handling received events.
+template <typename T>
+class EventSink {
+ public:
+  EventSink() = default;
+  virtual ~EventSink() = default;
+
+  // Prevent copying.
+  EventSink(EventSink const&) = delete;
+  EventSink& operator=(EventSink const&) = delete;
+
+  // Consumes end of stream. Ensuing calls to Success(T) or
+  // Error(String, String, Object)}, if any, are ignored.
+  void EndOfStream() { EndOfStreamInternal(); }
+
+  // Consumes a successful event.
+  void Success(const T* event = nullptr) {
+    SuccessInternal(event);
+  }
+
+  // Consumes an error event.
+  void Error(const std::string& error_code,
+             const std::string& error_message = "",
+             const T* error_details = nullptr) {
+    ErrorInternal(error_code, error_message, error_details);
+  }
+
+ protected:
+  // Implementation of the public interface, to be provided by subclasses.
+  virtual void EndOfStreamInternal() = 0;
+
+  // Implementation of the public interface, to be provided by subclasses.
+  virtual void SuccessInternal(const T* event = nullptr) = 0;
+
+  // Implementation of the public interface, to be provided by subclasses.
+  virtual void ErrorInternal(const std::string& error_code,
+                             const std::string& error_message,
+                             const T* error_details) = 0;
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_COMMON_CPP_CLIENT_WRAPPER_INCLUDE_FLUTTER_EVENT_SINK_H_
---
+++ shell/platform/common/cpp/client_wrapper/include/flutter/event_stream_handler.h
@@ -0,0 +1,42 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_COMMON_CPP_CLIENT_WRAPPER_INCLUDE_FLUTTER_EVENT_STREAM_HANDLER_H_
+#define FLUTTER_SHELL_PLATFORM_COMMON_CPP_CLIENT_WRAPPER_INCLUDE_FLUTTER_EVENT_STREAM_HANDLER_H_
+
+#include "event_sink.h"
+
+namespace flutter {
+
+// Handler of stream setup and tear-down requests.
+// Implementations must be prepared to accept sequences of alternating calls to 
+// onListen(T, EventSink) and onCancel(Object). Implementations should ideally
+// consume no resources when the last such call is not onListen(). In typical situations,
+// this means that the implementation should register itself with platform-specific event sources
+// onListen() and deregister again onCancel().
+template <typename T>
+class StreamHandler {
+ public:
+  // Handles a request to set up an event stream.
+  // @param arguments stream configuration arguments, possibly null.
+  // @param event_sink an EventSink for emitting events to the Flutter receiver.
+  using OnListen = std::function<void(const T* arguments, const EventSink<T>* event_sink)>;
+
+  // Handles a request to tear down the most recently created event stream.
+  // @param arguments stream configuration arguments, possibly null.
+  using OnCancel = std::function<void(const T* arguments)>;
+
+  OnListen onListen;
+  OnCancel onCancel;
+
+  // Registers a stream handler on this channel.
+  // If no handler has been registered, any incoming stream setup requests will be handled
+  // silently by providing an empty stream.
+  StreamHandler( OnListen const& onListen, OnCancel const& onCancel)
+    : onListen(onListen), onCancel(onCancel) {}
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_SHELL_PLATFORM_COMMON_CPP_CLIENT_WRAPPER_INCLUDE_FLUTTER_EVENT_STREAM_HANDLER_H_
