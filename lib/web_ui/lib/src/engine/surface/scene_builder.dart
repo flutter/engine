47a11e5ec36644d01635e874441f51d3d1fa358d
ferhatb <ferhat@gmail.com>
2020-03-25 10:22:03 -0700

    Restrict top level transform check

--- lib/web_ui/lib/src/engine/surface/scene_builder.dart
+++ lib/web_ui/lib/src/engine/surface/scene_builder.dart
@@ -104,12 +104,10 @@ class SurfaceSceneBuilder implements ui.SceneBuilder {
       // Top level transform contains view configuration to scale
       // scene to devicepixelratio. Use identity instead since CSS uses
       // logical device pixels.
-      assert(matrix4[0] ==
-              (window._debugDevicePixelRatio ??
-                  EngineWindow.browserDevicePixelRatio) &&
-          matrix4[5] ==
-              (window._debugDevicePixelRatio ??
-                  EngineWindow.browserDevicePixelRatio));
+      if (!ui.debugEmulateFlutterTesterEnvironment) {
+        assert(matrix4[0] == window.devicePixelRatio &&
+           matrix4[5] == window.devicePixelRatio);
+      }
       matrix4 = Matrix4.identity().storage;
     }
     return _pushSurface(PersistedTransform(oldLayer, matrix4));
