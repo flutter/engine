7a84f1291ee89d35f115509c2f97745e275a043d
Stuart Morgan <stuartmorgan@google.com>
2020-03-24 19:30:23 -0700

    Move JSON codecs from wrapper to common internal

--- shell/platform/common/cpp/json_message_codec.cc
+++ shell/platform/common/cpp/json_message_codec.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "include/flutter/json_message_codec.h"
+#include "flutter/shell/platform/common/cpp/json_message_codec.h"
 
 #include <iostream>
 #include <string>
--- shell/platform/common/cpp/json_method_codec.cc
+++ shell/platform/common/cpp/json_method_codec.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "include/flutter/json_method_codec.h"
+#include "flutter/shell/platform/common/cpp/json_method_codec.h"
 
-#include "include/flutter/json_message_codec.h"
+#include "flutter/shell/platform/common/cpp/json_message_codec.h"
 
 namespace flutter {
 
