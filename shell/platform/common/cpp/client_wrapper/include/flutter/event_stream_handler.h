50576c61e1da76d02ffefca84b16325383125c69
Kurun <robo_matsu_rhhqd142@yahoo.co.jp>
2020-03-30 00:49:09 +0900

    Modify comment and change from class to structure

--- shell/platform/common/cpp/client_wrapper/include/flutter/event_stream_handler.h
+++ shell/platform/common/cpp/client_wrapper/include/flutter/event_stream_handler.h
@@ -11,29 +11,25 @@ namespace flutter {
 
 // Handler of stream setup and tear-down requests.
 // Implementations must be prepared to accept sequences of alternating calls to
-// onListen(T, EventSink) and onCancel(Object). Implementations should ideally
-// consume no resources when the last such call is not onListen(). In typical
-// situations, this means that the implementation should register itself with
+// onListen() and onCancel(). Implementations should ideally consume no resources
+// when the last such call is not onListen(). In typical situations,
+// this means that the implementation should register itself with
 // platform-specific event sources onListen() and deregister again onCancel().
 template <typename T>
-class StreamHandler {
- public:
+struct StreamHandler {
   // Handles a request to set up an event stream.
-  // @param arguments stream configuration arguments, possibly null.
-  // @param event_sink an EventSink for emitting events to the Flutter receiver.
+  // |arguments| is stream configuration arguments and
+  // |events| is an EventSink for emitting events to the Flutter receiver.
   using OnListen =
-      std::function<void(const T* arguments, EventSink<T>* event_sink)>;
+      std::function<void(const T* arguments, EventSink<T>* events)>;
 
   // Handles a request to tear down the most recently created event stream.
-  // @param arguments stream configuration arguments, possibly null.
+  // |arguments| is stream configuration arguments.
   using OnCancel = std::function<void(const T* arguments)>;
 
   OnListen onListen;
   OnCancel onCancel;
 
-  // Registers a stream handler on this channel.
-  // If no handler has been registered, any incoming stream setup requests will
-  // be handled silently by providing an empty stream.
   StreamHandler(OnListen const& onListen, OnCancel const& onCancel)
       : onListen(onListen), onCancel(onCancel) {}
 };
