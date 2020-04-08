a5621620561b3b66d321bcd221840863672d0067
Dan Field <dnfield@google.com>
2020-04-08 13:06:46 -0700

    build the necessary artifacts

--- testing/scenario_app/assemble_apk.sh
+++ testing/scenario_app/assemble_apk.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+pushd "${BASH_SOURCE%/*}/../../.."
+  ./flutter/tools/gn --unopt
+  ninja -C out/host_debug_unopt sky_engine sky_services
+popd
+
 pushd "${BASH_SOURCE%/*}"
   ./compile_android_aot.sh "$1" "$2"
 popd
