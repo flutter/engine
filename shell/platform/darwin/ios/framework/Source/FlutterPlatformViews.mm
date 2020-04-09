eab04fe130dd7d81a5454659652547866255a681
Chris Yang <cyang8954@gmail.com>
2020-04-09 11:50:57 -0700

    update has pending oeration

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -252,6 +252,9 @@ void FlutterPlatformViewsController::CancelFrame() {
 }
 
 bool FlutterPlatformViewsController::HasPendingViewOperations() {
+  if (!views_to_dispose_.empty()) {
+    return true;
+  }
   if (!views_to_recomposite_.empty()) {
     return true;
   }
