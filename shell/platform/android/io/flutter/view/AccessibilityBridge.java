b55f7cab36774ea34ab13ace96161672e4d99556
shuaiwang <909104292@qq.com>
2020-04-11 22:29:11 +0800

    solve some crash issues on some Huawei phones
    Solve some CRASH issues on Huawei and Xiaomi phones. The crash information is as follows:

--- shell/platform/android/io/flutter/view/AccessibilityBridge.java
+++ shell/platform/android/io/flutter/view/AccessibilityBridge.java
@@ -631,7 +631,9 @@ public class AccessibilityBridge extends AccessibilityNodeProvider {
     if (semanticsNode.parent != null) {
       Rect parentBounds = semanticsNode.parent.getGlobalRect();
       Rect boundsInParent = new Rect(bounds);
-      boundsInParent.offset(-parentBounds.left, -parentBounds.top);
+      if (parentBounds != null) {
+        boundsInParent.offset(-parentBounds.left, -parentBounds.top);
+      }
       result.setBoundsInParent(boundsInParent);
     } else {
       result.setBoundsInParent(bounds);
