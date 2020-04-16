13ded2af3c58bdc5bcfb088915ade180d53dee57
Chris Yang <cyang8954@gmail.com>
2020-04-16 15:50:24 -0700

    formatting

--- shell/common/rasterizer.cc
+++ shell/common/rasterizer.cc
@@ -131,7 +131,8 @@ void Rasterizer::Draw(fml::RefPtr<Pipeline<flutter::LayerTree>> pipeline) {
   // front of the queue and also change the consume status to more available.
   if (raster_status == RasterStatus::kResubmit) {
     auto front_continuation = pipeline->ProduceIfEmpty();
-    bool result = front_continuation.Complete(std::move(resubmitted_layer_tree_));
+    bool result =
+        front_continuation.Complete(std::move(resubmitted_layer_tree_));
     if (result) {
       consume_result = PipelineConsumeResult::MoreAvailable;
     }
