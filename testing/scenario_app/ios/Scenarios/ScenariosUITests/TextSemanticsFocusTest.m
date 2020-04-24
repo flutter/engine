28f5cca7abddbdaea62ee762295108e412eb8426
Xiao Yu <xiao@xster.net>
2020-04-24 13:02:29 -0700

    we should check the xcscheme file in

--- testing/scenario_app/ios/Scenarios/ScenariosUITests/TextSemanticsFocusTest.m
+++ testing/scenario_app/ios/Scenarios/ScenariosUITests/TextSemanticsFocusTest.m
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-FLUTTER_ASSERT_ARC
-
 #import "TextSemanticsFocusTest.h"
+
+FLUTTER_ASSERT_ARC
 
 @implementation TextSemanticsFocusTest
 
