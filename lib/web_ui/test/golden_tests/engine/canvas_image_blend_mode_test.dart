6f3e3cfbab2fed02fd2e3919af555a5dcb1ec039
ferhatb <ferhat@gmail.com>
2020-05-04 11:24:04 -0700

    disable write:true on golden

--- lib/web_ui/test/golden_tests/engine/canvas_image_blend_mode_test.dart
+++ lib/web_ui/test/golden_tests/engine/canvas_image_blend_mode_test.dart
@@ -30,7 +30,7 @@ void main() async {
     try {
       sceneElement.append(engineCanvas.rootElement);
       html.document.body.append(sceneElement);
-      await matchGoldenFile('$fileName.png', region: region, maxDiffRatePercent: maxDiffRatePercent, write: true);
+      await matchGoldenFile('$fileName.png', region: region, maxDiffRatePercent: maxDiffRatePercent);
     } finally {
       // The page is reused across tests, so remove the element after taking the
       // Scuba screenshot.
