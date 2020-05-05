0d8b348c3d045104f64de68421e8fef68159557d
cyanglaz <ychris@google.com>
2020-05-05 11:56:59 -0700

    return false in submitframe callback if the canvas is not available

--- shell/gpu/gpu_surface_metal.mm
+++ shell/gpu/gpu_surface_metal.mm
@@ -80,6 +80,10 @@ std::unique_ptr<SurfaceFrame> GPUSurfaceMetal::AcquireFrame(const SkISize& frame
 
   auto submit_callback = [this](const SurfaceFrame& surface_frame, SkCanvas* canvas) -> bool {
     TRACE_EVENT0("flutter", "GPUSurfaceMetal::Submit");
+    if (canvas == nullptr) {
+      return false;
+    }
+
     canvas->flush();
 
     if (next_drawable_ == nullptr) {
