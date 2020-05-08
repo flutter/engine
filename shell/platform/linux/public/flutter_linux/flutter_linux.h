e7a04e85dec463df8db87284ed7704573e55d2d4
Robert Ancell <robert.ancell@canonical.com>
2020-05-08 15:10:15 +1200

    Add FlJsonMessageCodec

--- shell/platform/linux/public/flutter_linux/flutter_linux.h
+++ shell/platform/linux/public/flutter_linux/flutter_linux.h
@@ -11,6 +11,7 @@
 #include <flutter_linux/fl_binary_messenger.h>
 #include <flutter_linux/fl_dart_project.h>
 #include <flutter_linux/fl_engine.h>
+#include <flutter_linux/fl_json_message_codec.h>
 #include <flutter_linux/fl_message_codec.h>
 #include <flutter_linux/fl_method_channel.h>
 #include <flutter_linux/fl_method_codec.h>
