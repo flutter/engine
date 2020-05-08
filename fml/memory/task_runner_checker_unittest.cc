9e7f27781c740a9b2b9cddd13fa6739335976e77
cyanglaz <ychris@google.com>
2020-05-08 13:18:29 -0700

    refactor

--- fml/memory/task_runner_checker_unittest.cc
+++ fml/memory/task_runner_checker_unittest.cc
@@ -4,14 +4,16 @@
 
 #define FML_USED_ON_EMBEDDER
 
-#include <thread>
-
-#include "flutter/fml/memory/task_runner_checker.h"
-#include "flutter/fml/message_loop.h"
-#include "flutter/fml/synchronization/count_down_latch.h"
-#include "flutter/fml/synchronization/waitable_event.h"
 
 #include <gtest/gtest.h>
+
+#include <thread>
+#include "flutter/fml/memory/task_runner_checker.h"
+
+#include "flutter/fml/message_loop.h"
+#include "flutter/fml/raster_thread_merger.h"
+#include "flutter/fml/synchronization/count_down_latch.h"
+#include "flutter/fml/synchronization/waitable_event.h"
 
 namespace fml {
 namespace testing {
@@ -24,7 +26,7 @@ TEST(TaskRunnerCheckerTests, RunsOnCurrentTaskRunner) {
 TEST(TaskRunnerCheckerTests, FailsTheCheckIfOnDifferentTaskRunner) {
   TaskRunnerChecker checker;
   EXPECT_EQ(checker.RunsOnCreationTaskRunner(), true);
-  fml::MessageLoop* loop = nullptr;
+  fml::MessageLoop* loop;
   fml::AutoResetWaitableEvent latch;
   std::thread anotherThread([&]() {
     fml::MessageLoop::EnsureInitializedForCurrentThread();
@@ -39,6 +41,29 @@ TEST(TaskRunnerCheckerTests, FailsTheCheckIfOnDifferentTaskRunner) {
   loop->Terminate();
   anotherThread.join();
   EXPECT_EQ(checker.RunsOnCreationTaskRunner(), true);
+}
+
+TEST(TaskRunnerCheckerTests, SameTaskRunnerRunsOnTheSameThread) {
+  fml::MessageLoop& loop1 = fml::MessageLoop::GetCurrent();
+  fml::MessageLoop& loop2 = fml::MessageLoop::GetCurrent();
+  TaskQueueId a = loop1.GetTaskRunner()->GetTaskQueueId();
+  TaskQueueId b = loop2.GetTaskRunner()->GetTaskQueueId();
+  EXPECT_EQ(TaskRunnerChecker::RunsOnTheSameThread(a, b), true);
+}
+
+TEST(TaskRunnerCheckerTests, RunsOnDifferentThreadsReturnsFalse) {
+  fml::MessageLoop& loop1 = fml::MessageLoop::GetCurrent();
+  TaskQueueId a = loop1.GetTaskRunner()->GetTaskQueueId();
+  fml::AutoResetWaitableEvent latch;
+  std::thread anotherThread([&]() {
+    fml::MessageLoop::EnsureInitializedForCurrentThread();
+    fml::MessageLoop& loop2 = fml::MessageLoop::GetCurrent();
+    TaskQueueId b = loop2.GetTaskRunner()->GetTaskQueueId();
+    EXPECT_EQ(TaskRunnerChecker::RunsOnTheSameThread(a, b), false);
+    latch.Signal();
+  });
+  latch.Wait();
+  anotherThread.join();
 }
 
 }  // namespace testing
