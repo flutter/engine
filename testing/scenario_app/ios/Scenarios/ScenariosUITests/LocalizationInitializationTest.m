73d2d8d32abdb439b7a5ee72c72da3e906d63dea
garyqian <garyq@google.com>
2020-05-19 00:21:18 -0700

    Remove extra code

--- testing/scenario_app/ios/Scenarios/ScenariosUITests/LocalizationInitializationTest.m
+++ testing/scenario_app/ios/Scenarios/ScenariosUITests/LocalizationInitializationTest.m
@@ -6,34 +6,6 @@
 
 FLUTTER_ASSERT_ARC
 
-@interface XCUIElement (ftr_waitForNonExistence)
-/// Keeps waiting until the element doesn't exist.  Returns NO if the timeout is
-/// reached before it doesn't exist.
-- (BOOL)ftr_waitForNonExistenceWithTimeout:(NSTimeInterval)timeout;
-/// Waits the full duration to ensure something doesn't exist for that duration.
-/// Returns NO if at some point the element exists during the duration.
-- (BOOL)ftr_waitForNonExistenceForDuration:(NSTimeInterval)duration;
-@end
-
-@implementation XCUIElement (ftr_waitForNonExistence)
-- (BOOL)ftr_waitForNonExistenceWithTimeout:(NSTimeInterval)timeout {
-  NSTimeInterval delta = 0.5;
-  while (timeout > 0.0) {
-    if (!self.exists) {
-      return YES;
-    }
-    usleep(delta * 1000000);
-    timeout -= delta;
-  }
-  return NO;
-}
-
-- (BOOL)ftr_waitForNonExistenceForDuration:(NSTimeInterval)duration {
-  return ![self waitForExistenceWithTimeout:duration];
-}
-
-@end
-
 @implementation LocalizationInitializationTest
 
 - (void)setUp {
