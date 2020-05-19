a9e37ec1eb469844ea3d5dd0c87403ab768f7335
garyqian <garyq@google.com>
2020-05-19 15:35:19 -0700

    Address comments

--- testing/scenario_app/ios/Scenarios/ScenariosUITests/LocalizationInitializationTest.m
+++ testing/scenario_app/ios/Scenarios/ScenariosUITests/LocalizationInitializationTest.m
@@ -2,9 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "LocalizationInitialization.h"
+#import <Flutter/Flutter.h>
+#import <XCTest/XCTest.h>
 
 FLUTTER_ASSERT_ARC
+
+@interface LocalizationInitializationTest : XCTestCase
+@property(nonatomic, strong) XCUIApplication* application;
+@end
 
 @implementation LocalizationInitializationTest
 
@@ -21,12 +26,11 @@ FLUTTER_ASSERT_ARC
   NSTimeInterval timeout = 10.0;
 
   XCUIElement* textInputSemanticsObject =
-      [[[self.application textFields] matchingIdentifier:@"[en]"] element];
+      [self.application.textFields matchingIdentifier:@"[en]"].element;
 
   // The locales recieved by dart:ui are exposed onBeginFrame via semantics label.
   // There should only be one locale, as we have removed the locale prepend on iOS.
   XCTAssertTrue([textInputSemanticsObject waitForExistenceWithTimeout:timeout]);
-  XCTAssertEqualObjects([textInputSemanticsObject valueForKey:@"hasKeyboardFocus"], @(NO));
 }
 
 @end
