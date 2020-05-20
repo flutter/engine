7eebc19d379814df26af2f14e2daae0a2e7de71a
Mehmet Fidanboylu <mehmetf@google.com>
2020-05-19 21:38:17 -0700

    Add tests for StandardMethodCodec

---
+++ shell/platform/android/test/io/flutter/plugin/common/StandardMethodCodecTest.java
@@ -0,0 +1,82 @@
+package io.flutter.plugin.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner.class)
+public class StandardMethodCodecTest {
+
+  @Test
+  public void encodeMethodTest() {
+    Map<String, String> args = new HashMap<>();
+    args.put("testArg", "testValue");
+    MethodCall call = new MethodCall("testMethod", args);
+    ByteBuffer buffer = StandardMethodCodec.INSTANCE.encodeMethodCall(call);
+    assertNotNull(buffer);
+    MethodCall result = StandardMethodCodec.INSTANCE.decodeMethodCall(buffer);
+    assertEquals(call.method, result.method);
+    assertEquals(call.arguments, result.arguments);
+  }
+
+  @Test
+  public void encodeSuccessEnvelopeTest() {
+    Map<String, Integer> success = new HashMap<>();
+    success.put("result", 1);
+    ByteBuffer buffer = StandardMethodCodec.INSTANCE.encodeSuccessEnvelope(success);
+    assertNotNull(buffer);
+    Object result = StandardMethodCodec.INSTANCE.decodeEnvelope(buffer);
+    assertEquals(success, result);
+  }
+
+  @Test
+  public void encodeSuccessEnvelopeUnsupportedObjectTest() {
+    StandardMethodCodecTest joke = new StandardMethodCodecTest();
+    try {
+      ByteBuffer buffer = StandardMethodCodec.INSTANCE.encodeSuccessEnvelope(joke);
+      fail("Should have failed to convert unsupported type.");
+    } catch (IllegalArgumentException e) {
+      // pass.
+    }
+  }
+
+  @Test
+  public void encodeErrorEnvelopeWithNullDetailsTest() {
+    ByteBuffer buffer = StandardMethodCodec.INSTANCE.encodeErrorEnvelope("code", "error", null);
+    assertNotNull(buffer);
+    FlutterException result =
+        (FlutterException) StandardMethodCodec.INSTANCE.decodeEnvelope(buffer);
+    assertEquals("code", result.code);
+    assertEquals("error", result.getMessage());
+    assertNull(result.details);
+  }
+
+  @Test
+  public void encodeErrorEnvelopeWithThrowableTest() {
+    try {
+      throw new IllegalArgumentException("foo");
+    } catch (IllegalArgumentException e) {
+      ByteBuffer buffer =
+          StandardMethodCodec.INSTANCE.encodeErrorEnvelope("code", e.getMessage(), e);
+      assertNotNull(buffer);
+      FlutterException result =
+          (FlutterException) StandardMethodCodec.INSTANCE.decodeEnvelope(buffer);
+      assertEquals("code", result.code);
+      assertEquals("error", result.getMessage());
+      // Must contain part of a stack.
+      String stack = (String) result.details;
+      assertTrue(stack.contains("StandardMethodCodecTest.java:15"));
+    }
+  }
+}
