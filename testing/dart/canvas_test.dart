fd87388c4eeab119bcc41837a3844f6122f7b4bb
Dan Field <dnfield@google.com>
2020-06-01 10:27:30 -0700

    test

--- testing/dart/canvas_test.dart
+++ testing/dart/canvas_test.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 // @dart = 2.6
+import 'dart:async';
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui';
@@ -12,6 +13,24 @@ import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
 typedef CanvasCallback = void Function(Canvas canvas);
+
+Future<Image> createImage(int width, int height) {
+  final Completer<Image> completer = Completer<Image>();
+  decodeImageFromPixels(
+    Uint8List.fromList(List<int>.generate(
+      width * height * 4,
+      (int pixel) => pixel % 255,
+    )),
+    width,
+    height,
+    PixelFormat.rgba8888,
+    (Image image) {
+      completer.complete(image);
+    },
+  );
+
+  return completer.future;
+}
 
 void testCanvas(CanvasCallback callback) {
   try {
@@ -198,4 +217,22 @@ void main() {
         await fuzzyGoldenImageCompare(image, 'canvas_test_dithered_gradient.png');
     expect(areEqual, true);
   }, skip: !Platform.isLinux); // https://github.com/flutter/flutter/issues/53784
+
+  test('Image size reflected in picture size for image* and drawPicture methods', () async {
+    final Image image = await createImage(100, 100);
+    final PictureRecorder recorder = PictureRecorder();
+    final Canvas canvas = Canvas(recorder);
+    const Rect rect = Rect.fromLTWH(0, 0, 100, 100);
+    canvas.drawImage(image, Offset.zero, Paint());
+    canvas.drawImageRect(image, rect, rect, Paint());
+    canvas.drawImageNine(image, rect, rect, Paint());
+    final Picture picture = recorder.endRecording();
+    expect(picture.approximateBytesUsed, 161235);
+
+    final PictureRecorder recorder2 = PictureRecorder();
+    final Canvas canvas2 = Canvas(recorder2);
+    canvas2.drawPicture(picture);
+    final Picture picture2 = recorder2.endRecording();
+    expect(picture2.approximateBytesUsed, 162999);
+  });
 }
