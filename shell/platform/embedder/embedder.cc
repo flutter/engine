6e178ec567e9daf9acac26afe7eaa72232b1b85d
Stuart Morgan <stuartmorgan@google.com>
2020-06-02 15:33:20 -0700

    Address review comments, format

--- shell/platform/embedder/embedder.cc
+++ shell/platform/embedder/embedder.cc
@@ -14,13 +14,13 @@
 #include "third_party/dart/runtime/bin/elf_loader.h"
 #include "third_party/dart/runtime/include/dart_native_api.h"
 
-#if !defined(EMBEDDER_AS_INTERNAL_LIBRARY)
+#if !defined(FLUTTER_NO_EXPORT)
 #if OS_WIN
 #define FLUTTER_EXPORT __declspec(dllexport)
 #else  // OS_WIN
 #define FLUTTER_EXPORT __attribute__((visibility("default")))
 #endif  // OS_WIN
-#endif  // !EMBEDDER_AS_INTERNAL_LIBRARY
+#endif  // !FLUTTER_NO_EXPORT
 
 extern "C" {
 #if FLUTTER_RUNTIME_MODE == FLUTTER_RUNTIME_MODE_DEBUG
