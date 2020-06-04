d990af63fb6344a3c7ca3c69fde31314ca1c81ac
cg021 <gonzchristian@google.com>
2020-06-03 20:02:38 -0500

    mockito

--- shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
+++ shell/platform/android/test/io/flutter/embedding/engine/FlutterJNITest.java
@@ -1,7 +1,7 @@
 package io.flutter.embedding.engine;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import io.flutter.embedding.engine.renderer.FlutterUiDisplayListener;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import org.mockito.Mock;
 
 @Config(manifest = Config.NONE)
 @RunWith(RobolectricTestRunner.class)
@@ -49,38 +48,17 @@ public class FlutterJNITest {
 
   @Test 
   public void onDisplayPlatformView__callsPlatformViewsController() {
-    int[] platformViewProps = new int[5];
-    int counter = 0;
+    PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
 
     FlutterJNI flutterJNI = new FlutterJNI();
-    flutterJNI.setPlatformViewsController(new PlatformViewsController() {
-      @override 
-      public void onDisplayPlatformView(int viewId, int x, int y, int width, int height) {
-        PlatformViewsController platformViewsController = mock(PlatformViewsController.class);
-        if (platformViewsController == null) {
-          throw new RuntimeException(
-              "platformViewsController must be set before attempting to position a platform view");
-        }
-
-        platformViewsController.onDisplayPlatformView(viewId, x, y, width, height);
-        platformViewProps[0] = viewId;
-        platformViewProps[1] = x;
-        platformViewProps[2] = y;
-        platformViewProps[3] = width;
-        platformViewProps[4] = height;
-        counter++;
-      }
-    });
+    flutterJNI.setPlatformViewsController(platformViewsController);
 
     // --- Execute Test ---
-    flutterJNI.onDisplayPlatformView(/*viewId=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
+    flutterJNI.onDisplayPlatformView(
+      /*viewId=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
 
     // --- Verify Results ---
-    assertEquals(1, counter);
-    assertEquals(1, platformViewProps[0]);
-    assertEquals(10, platformViewProps[1]);
-    assertEquals(20, platformViewProps[2]);
-    assertEquals(100, platformViewProps[3]);
-    assertEquals(200, platformViewProps[4]);
+    verify(platformViewsController, times(1)).onDisplayPlatformView(
+      /*viewId=*/ 1, /*x=*/ 10, /*y=*/ 20, /*width=*/ 100, /*height=*/ 200);
   }
 }
