bdf52fc294eb1f1bd4f47f74468f444b6f8b5c49
cg021 <gonzchristian@google.com>
2020-06-04 11:48:07 -0500

    onDisplayPlatformView JNI

--- shell/platform/android/io/flutter/embedding/engine/FlutterEngine.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterEngine.java
@@ -101,6 +101,10 @@ public class FlutterEngine {
 
           platformViewsController.onPreEngineRestart();
         }
+
+        public void onDisplayPlatformView(int viewId, int x, int y, int width, int height) {
+          platformViewsController.onDisplayPlatformView(viewId, x, y, width, height);
+        }
       };
 
   /**
@@ -205,6 +209,7 @@ public class FlutterEngine {
     flutterLoader.ensureInitializationComplete(context, dartVmArgs);
 
     flutterJNI.addEngineLifecycleListener(engineLifecycleListener);
+    flutterJNI.setPlatformViewsController(platformViewsController);
     attachToJni();
 
     this.dartExecutor = new DartExecutor(flutterJNI, context.getAssets());
