b3876acf4759144bb1a9ef7c67dd78eaa1fb8b88
Emmanuel Garcia <egarciad@google.com>
2020-06-08 22:43:19 -0700

    Update unit test

--- shell/platform/android/external_view_embedder/external_view_embedder_unittests.cc
+++ shell/platform/android/external_view_embedder/external_view_embedder_unittests.cc
@@ -68,7 +68,7 @@ TEST(AndroidExternalViewEmbedder, RasterizerRunsOnPlatformThread) {
 
   auto postpreroll_result = embedder->PostPrerollAction(raster_thread_merger);
   ASSERT_EQ(PostPrerollResult::kResubmitFrame, postpreroll_result);
-  ASSERT_FALSE(embedder->SubmitFrame(nullptr, nullptr));
+  ASSERT_TRUE(embedder->SubmitFrame(nullptr, nullptr));
 
   embedder->EndFrame(raster_thread_merger);
   ASSERT_TRUE(raster_thread_merger->IsMerged());
