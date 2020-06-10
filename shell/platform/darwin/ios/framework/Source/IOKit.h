a0c629ae5c59de4c62cf00cb828747b8cd4b937e
Aaron Clarke <aaclarke@google.com>
2020-06-10 11:04:46 -0700

    trimmed up iokit to minimal set required with annotations for where the defines come from

--- shell/platform/darwin/ios/framework/Source/IOKit.h
+++ shell/platform/darwin/ios/framework/Source/IOKit.h
@@ -22,58 +22,21 @@ extern "C" {
 #include <mach/mach.h>
 #include <stdint.h>
 
+#define IOKIT
+#include <device/device_types.h>
+
 static const char* kIOServicePlane = "IOService";
 
-typedef char io_name_t[128];
-typedef char io_string_t[512];
-typedef char io_struct_inband_t[4096];
-typedef mach_port_t io_object_t;
 typedef io_object_t io_registry_entry_t;
 typedef io_object_t io_service_t;
 typedef io_object_t io_connect_t;
 typedef io_object_t io_iterator_t;
 
 enum {
-  kIOCFSerializeToBinary = 0x00000001U,
-};
-
-enum {
   kIOReturnSuccess = 0,
 };
 
-enum {
-  kIORegistryIterateRecursively = 0x00000001U,
-  kIORegistryIterateParents = 0x00000002U,
-};
-
-enum {
-  kOSSerializeDictionary = 0x01000000U,
-  kOSSerializeArray = 0x02000000U,
-  kOSSerializeSet = 0x03000000U,
-  kOSSerializeNumber = 0x04000000U,
-  kOSSerializeSymbol = 0x08000000U,
-  kOSSerializeString = 0x09000000U,
-  kOSSerializeData = 0x0a000000U,
-  kOSSerializeBoolean = 0x0b000000U,
-  kOSSerializeObject = 0x0c000000U,
-
-  kOSSerializeTypeMask = 0x7F000000U,
-  kOSSerializeDataMask = 0x00FFFFFFU,
-
-  kOSSerializeEndCollection = 0x80000000U,
-
-  kOSSerializeMagic = 0x000000d3U,
-};
-
 extern const mach_port_t kIOMasterPortDefault;
-
-CF_RETURNS_RETAINED CFDataRef IOCFSerialize(CFTypeRef object,
-                                            CFOptionFlags options);
-CFTypeRef IOCFUnserializeWithSize(const char* buf,
-                                  size_t len,
-                                  CFAllocatorRef allocator,
-                                  CFOptionFlags options,
-                                  CFStringRef* err);
 
 kern_return_t IOObjectRetain(io_object_t object);
 kern_return_t IOObjectRelease(io_object_t object);
@@ -135,97 +98,6 @@ kern_return_t IOServiceGetMatchingServices(mach_port_t master,
                                            CFDictionaryRef matching
                                                CF_RELEASES_ARGUMENT,
                                            io_iterator_t* it);
-kern_return_t _IOServiceGetAuthorizationID(io_service_t service,
-                                           uint64_t* authID);
-kern_return_t _IOServiceSetAuthorizationID(io_service_t service,
-                                           uint64_t authID);
-kern_return_t IOServiceOpen(io_service_t service,
-                            task_t task,
-                            uint32_t type,
-                            io_connect_t* client);
-kern_return_t IOServiceClose(io_connect_t client);
-kern_return_t IOCloseConnection(io_connect_t client);
-kern_return_t IOConnectAddRef(io_connect_t client);
-kern_return_t IOConnectRelease(io_connect_t client);
-kern_return_t IOConnectGetService(io_connect_t client, io_service_t* service);
-kern_return_t IOConnectAddClient(io_connect_t client, io_connect_t other);
-kern_return_t IOConnectSetNotificationPort(io_connect_t client,
-                                           uint32_t type,
-                                           mach_port_t port,
-                                           uintptr_t ref);
-kern_return_t IOConnectMapMemory64(io_connect_t client,
-                                   uint32_t type,
-                                   task_t task,
-                                   mach_vm_address_t* addr,
-                                   mach_vm_size_t* size,
-                                   uint32_t options);
-kern_return_t IOConnectUnmapMemory64(io_connect_t client,
-                                     uint32_t type,
-                                     task_t task,
-                                     mach_vm_address_t addr);
-kern_return_t IOConnectSetCFProperties(io_connect_t client,
-                                       CFTypeRef properties);
-kern_return_t IOConnectCallMethod(io_connect_t client,
-                                  uint32_t selector,
-                                  const uint64_t* in,
-                                  uint32_t inCnt,
-                                  const void* inStruct,
-                                  size_t inStructCnt,
-                                  uint64_t* out,
-                                  uint32_t* outCnt,
-                                  void* outStruct,
-                                  size_t* outStructCnt);
-kern_return_t IOConnectCallScalarMethod(io_connect_t client,
-                                        uint32_t selector,
-                                        const uint64_t* in,
-                                        uint32_t inCnt,
-                                        uint64_t* out,
-                                        uint32_t* outCnt);
-kern_return_t IOConnectCallStructMethod(io_connect_t client,
-                                        uint32_t selector,
-                                        const void* inStruct,
-                                        size_t inStructCnt,
-                                        void* outStruct,
-                                        size_t* outStructCnt);
-kern_return_t IOConnectCallAsyncMethod(io_connect_t client,
-                                       uint32_t selector,
-                                       mach_port_t wake_port,
-                                       uint64_t* ref,
-                                       uint32_t refCnt,
-                                       const uint64_t* in,
-                                       uint32_t inCnt,
-                                       const void* inStruct,
-                                       size_t inStructCnt,
-                                       uint64_t* out,
-                                       uint32_t* outCnt,
-                                       void* outStruct,
-                                       size_t* outStructCnt);
-kern_return_t IOConnectCallAsyncScalarMethod(io_connect_t client,
-                                             uint32_t selector,
-                                             mach_port_t wake_port,
-                                             uint64_t* ref,
-                                             uint32_t refCnt,
-                                             const uint64_t* in,
-                                             uint32_t inCnt,
-                                             uint64_t* out,
-                                             uint32_t* outCnt);
-kern_return_t IOConnectCallAsyncStructMethod(io_connect_t client,
-                                             uint32_t selector,
-                                             mach_port_t wake_port,
-                                             uint64_t* ref,
-                                             uint32_t refCnt,
-                                             const void* inStruct,
-                                             size_t inStructCnt,
-                                             void* outStruct,
-                                             size_t* outStructCnt);
-kern_return_t IOConnectTrap6(io_connect_t client,
-                             uint32_t index,
-                             uintptr_t a,
-                             uintptr_t b,
-                             uintptr_t c,
-                             uintptr_t d,
-                             uintptr_t e,
-                             uintptr_t f);
 
 #if __cplusplus
 }
