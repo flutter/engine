5e68b40c88e635e9737041039c2f87fc08c4797c
zhouliang3 <zl398833@alibaba-inc.com>
2020-06-10 10:29:10 +0800

    add unittests

--- flow/layers/texture_layer_unittests.cc
+++ flow/layers/texture_layer_unittests.cc
@@ -54,5 +54,30 @@ TEST_F(TextureLayerTest, PaintingEmptyLayerDies) {
   EXPECT_EQ(mock_canvas().draw_calls(), std::vector<MockCanvas::DrawCall>());
 }
 
+TEST_F(TextureLayerTest, PaintingWithFilterQuality) {
+  const SkPoint layer_offset = SkPoint::Make(0.0f, 0.0f);
+  const SkSize layer_size = SkSize::Make(8.0f, 8.0f);
+  const int64_t texture_id = 0;
+  auto mock_texture = std::make_shared<MockTexture>(texture_id);
+  auto layer = std::make_shared<TextureLayer>(
+      layer_offset, layer_size, texture_id, false, kLow_SkFilterQuality);
+
+  // Ensure the texture is located by the Layer.
+  preroll_context()->texture_registry.RegisterTexture(mock_texture);
+
+  layer->Preroll(preroll_context(), SkMatrix());
+  EXPECT_EQ(layer->paint_bounds(),
+            (SkRect::MakeSize(layer_size)
+                 .makeOffset(layer_offset.fX, layer_offset.fY)));
+  EXPECT_TRUE(layer->needs_painting());
+
+  layer->Paint(paint_context());
+  EXPECT_EQ(mock_texture->paint_calls(),
+            std::vector({MockTexture::PaintCall{
+                mock_canvas(), layer->paint_bounds(), false, nullptr,
+                kLow_SkFilterQuality}}));
+  EXPECT_EQ(mock_canvas().draw_calls(), std::vector<MockCanvas::DrawCall>());
+}
+
 }  // namespace testing
 }  // namespace flutter
--- flow/testing/mock_texture_unittests.cc
+++ flow/testing/mock_texture_unittests.cc
@@ -41,5 +41,21 @@ TEST(MockTextureTest, PaintCalls) {
   EXPECT_EQ(texture->paint_calls(), expected_paint_calls);
 }
 
+TEST(MockTextureTest, PaintCallsWithFilerQuality) {
+  SkCanvas canvas;
+  const SkRect paint_bounds1 = SkRect::MakeWH(1.0f, 1.0f);
+  const SkRect paint_bounds2 = SkRect::MakeWH(2.0f, 2.0f);
+  const auto expected_paint_calls =
+      std::vector{MockTexture::PaintCall{canvas, paint_bounds1, false, nullptr,
+                                         kLow_SkFilterQuality},
+                  MockTexture::PaintCall{canvas, paint_bounds2, true, nullptr,
+                                         kLow_SkFilterQuality}};
+  auto texture = std::make_shared<MockTexture>(0);
+
+  texture->Paint(canvas, paint_bounds1, false, nullptr, kLow_SkFilterQuality);
+  texture->Paint(canvas, paint_bounds2, true, nullptr, kLow_SkFilterQuality);
+  EXPECT_EQ(texture->paint_calls(), expected_paint_calls);
+}
+
 }  // namespace testing
 }  // namespace flutter
