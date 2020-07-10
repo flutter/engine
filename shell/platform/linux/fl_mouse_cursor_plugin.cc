a9843971a66cbd749f168bc67254955742e981f7
Tong Mu <dkwingsmt@gmail.com>
2020-07-09 17:47:25 -0700

    Dont destroy unless nonnull

--- shell/platform/linux/fl_mouse_cursor_plugin.cc
+++ shell/platform/linux/fl_mouse_cursor_plugin.cc
@@ -116,7 +116,8 @@ static void fl_mouse_cursor_plugin_dispose(GObject* object) {
     self->view = nullptr;
   }
 
-  g_hash_table_destroy(self->system_cursor_table);
+  if (self->system_cursor_table)
+    g_hash_table_destroy(self->system_cursor_table);
 
   G_OBJECT_CLASS(fl_mouse_cursor_plugin_parent_class)->dispose(object);
 }
