052c6311a89fc89eb00df124e2fa91944a88f90d
ferhatb <ferhat@gmail.com>
2020-07-13 11:37:41 -0700

    Address review comments

--- lib/web_ui/lib/src/engine/ulps.dart
+++ lib/web_ui/lib/src/engine/ulps.dart
@@ -18,6 +18,8 @@ part of engine;
 //
 // For some good articles on the topic, see
 // https://randomascii.wordpress.com/category/floating-point/page/2/
+// Port based on:
+// https://github.com/google/skia/blob/master/include/private/SkFloatBits.h
 //
 // Here is the 32 bit IEEE representation:
 //   uint32_t mantissa : 23;
@@ -33,7 +35,7 @@ part of engine;
 /// int comparison.
 int signBitTo2sCompliment(int x) => (x & 0x80000000) != 0 ? (-(x & 0x7fffffff)) : x;
 
-/// Convert a 2s compliment int to a sign-bit (i.e. int interpreted as float).
+/// Convert a 2s complement int to a sign-bit (i.e. int interpreted as float).
 int twosComplimentToSignBit(int x) {
   if ((x & 0x80000000) == 0) {
     return x;
@@ -44,11 +46,16 @@ int twosComplimentToSignBit(int x) {
 }
 
 class _FloatBitConverter {
-  final Float32List float32List = Float32List(1);
-  late Int32List int32List;
-  _FloatBitConverter() {
-    int32List = float32List.buffer.asInt32List(0, 1);
+  final Float32List float32List;
+  final Int32List int32List;
+  _FloatBitConverter._(this.float32List, this.int32List);
+
+  factory _FloatBitConverter() {
+    Float32List float32List = Float32List(1);
+    return _FloatBitConverter._(float32List,
+        float32List.buffer.asInt32List(0, 1));
   }
+
   int toInt(Float32List source, int index) {
     float32List[0] = source[index];
     return int32List[0];
@@ -57,14 +64,15 @@ class _FloatBitConverter {
     float32List[0] = x;
     return int32List[0];
   }
-  double toFloat(int bits) {
+
+  double toDouble(int bits) {
     int32List[0] = bits;
     return float32List[0];
   }
 }
 
 // Singleton bit converter to prevent typed array allocations.
-_FloatBitConverter _floatBitConverter = _FloatBitConverter();
+final _FloatBitConverter _floatBitConverter = _FloatBitConverter();
 
 // Converts float to bits.
 int float2Bits(Float32List source, int index) {
@@ -73,13 +81,13 @@ int float2Bits(Float32List source, int index) {
 
 // Converts bits to float.
 double bitsToFloat(int bits) {
-  return _floatBitConverter.toFloat(bits);
+  return _floatBitConverter.toDouble(bits);
 }
 
 const int floatBitsExponentMask = 0x7F800000;
 const int floatBitsMatissaMask  = 0x007FFFFF;
 
-/// Returns a float as 2s compliment int to be able to compare floats to each
+/// Returns a float as 2s complement int to be able to compare floats to each
 /// other.
 int floatFromListAs2sCompliment(Float32List source, int index) =>
     signBitTo2sCompliment(float2Bits(source, index));
@@ -105,27 +113,32 @@ bool equalUlps(double a, double b, int epsilon, int depsilon) {
   return aBits < bBits + epsilon && bBits < aBits + epsilon;
 }
 
+/// General equality check that covers between, product and division by using
+/// ulps epsilon 16.
 bool almostEqualUlps(double a, double b) {
   const int kUlpsEpsilon = 16;
   return equalUlps(a, b, kUlpsEpsilon, kUlpsEpsilon);
 }
 
-// Equality using the same error term as between
+/// Equality using the same error term for between comparison.
 bool almostBequalUlps(double a, double b) {
   const int kUlpsEpsilon = 2;
   return equalUlps(a, b, kUlpsEpsilon, kUlpsEpsilon);
 }
 
+/// Equality check for product.
 bool almostPequalUlps(double a, double b) {
   const int kUlpsEpsilon = 8;
   return equalUlps(a, b, kUlpsEpsilon, kUlpsEpsilon);
 }
 
+/// Equality check for division.
 bool almostDequalUlps(double a, double b) {
   const int kUlpsEpsilon = 16;
   return equalUlps(a, b, kUlpsEpsilon, kUlpsEpsilon);
 }
 
+/// Checks if 2 points are roughly equal (ulp 256) to each other.
 bool approximatelyEqual(double ax, double ay, double bx, double by) {
   if (approximatelyEqualT(ax, bx) && approximatelyEqualT(ay, by)) {
     return true;
@@ -142,8 +155,10 @@ bool approximatelyEqual(double ax, double ay, double bx, double by) {
   return almostDequalUlps(largest, largest + dist);
 }
 
-// Use this for comparing Ts in the range of 0 to 1. For general numbers (larger and smaller) use
-// AlmostEqualUlps instead.
+/// Equality check for comparing curve T values in the range of 0 to 1.
+///
+/// For general numbers (larger and smaller) use
+/// AlmostEqualUlps instead.
 bool approximatelyEqualT(double t1, double t2) {
   return approximatelyZero(t1 - t2);
 }
@@ -151,9 +166,9 @@ bool approximatelyEqualT(double t1, double t2) {
 bool approximatelyZero(double value) => value.abs() < kFltEpsilon;
 
 bool roughlyEqualUlps(double a, double b) {
-  const int UlpsEpsilon = 256;
-  const int DUlpsEpsilon = 1024;
-  return equalUlps(a, b, UlpsEpsilon, DUlpsEpsilon);
+  const int kUlpsEpsilon = 256;
+  const int kDUlpsEpsilon = 1024;
+  return equalUlps(a, b, kUlpsEpsilon, kDUlpsEpsilon);
 }
 
 bool dEqualUlpsEpsilon(double a, double b, int epsilon) {
@@ -163,11 +178,12 @@ bool dEqualUlpsEpsilon(double a, double b, int epsilon) {
   return aBits < bBits + epsilon && bBits < aBits + epsilon;
 }
 
+// Checks equality for division.
 bool almostDequalUlpsDouble(double a, double b) {
   if (a.abs() < kScalarMax && b.abs() < kScalarMax) {
     return almostDequalUlps(a, b);
   }
-  return (a - b).abs() / math.max(a.abs(), b.abs()) < kFltEpsilon * 16;
+  return (a - b).abs() / math.max(a.abs(), b.abs()) < kDblEpsilonSubdivideErr;
 }
 
 const double kFltEpsilon = 1.19209290E-07; // == 1 / (2 ^ 23)
@@ -175,6 +191,7 @@ const double kDblEpsilon = 2.22045e-16;
 const double kFltEpsilonCubed = kFltEpsilon * kFltEpsilon * kFltEpsilon;
 const double kFltEpsilonHalf = kFltEpsilon / 2;
 const double kFltEpsilonDouble = kFltEpsilon * 2;
+// Epsilon to use when ordering vectors.
 const double kFltEpsilonOrderableErr = kFltEpsilon * 16;
 const double kFltEpsilonSquared = kFltEpsilon * kFltEpsilon;
 // Use a compile-time constant for FLT_EPSILON_SQRT to avoid initializers.
