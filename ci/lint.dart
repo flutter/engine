e5a00af7602072f50ab33936e05ce2c14418d0e2
Aaron Clarke <aaclarke@google.com>
2020-07-20 14:47:55 -0700

    split out args into arrays

--- ci/lint.dart
+++ ci/lint.dart
@@ -78,9 +78,9 @@ List<String> getListOfChangedFiles(String repoPath) {
       'git', ['diff', '--cached', '--name-only'],
       workingDirectory: repoPath);
 
-  final ProcessResult fetchResult = Process.runSync('git', ['fetch upstream master']);
+  final ProcessResult fetchResult = Process.runSync('git', ['fetch', 'upstream', 'master']);
   if (fetchResult.exitCode != 0) {
-    Process.runSync('git', ['fetch origin master']);
+    Process.runSync('git', ['fetch', 'origin', 'master']);
   }
   final ProcessResult mergeBaseResult = Process.runSync(
       'git', ['merge-base', '--fork-point', 'FETCH_HEAD', 'HEAD'],
