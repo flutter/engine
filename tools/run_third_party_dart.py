2d488487086a4fa10dc084027f23d4777f7301da
jonahwilliams <jonahwilliams@google.com>
2020-07-24 13:07:45 -0700

    use check call, backslashes

--- tools/run_third_party_dart.py
+++ tools/run_third_party_dart.py
@@ -8,7 +8,7 @@ import os
 if __name__ == '__main__':
     pub = "src/third_party/dart/tools/sdks/dart-sdk/bin/pub"
     if os.name == "nt":
-        pub = "src/third_party/dart/tools/sdks/dart-sdk/bin/pub.bat"
-    subprocess.call([pub, "global", "activate", "-spath", "./src/flutter/tools/generate_package_config"])
-    subprocess.call([pub, "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/flutter_frontend_server/.packages"])
-    subprocess.call([pub, "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/tools/const_finder/.packages"])
+        pub = "src\\third_party\\dart\\tools\\sdks\\dart-sdk\\bin\\pub.bat"
+    subprocess.check_call([pub, "global", "activate", "-spath", "./src/flutter/tools/generate_package_config"])
+    subprocess.check_call([pub, "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/flutter_frontend_server/.packages"])
+    subprocess.check_call([pub, "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/tools/const_finder/.packages"])
