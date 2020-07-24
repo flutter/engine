ccc65e068ffecfebfeaa55d66dbdf2b9e382374b
jonahwilliams <jonahwilliams@google.com>
2020-07-24 12:55:13 -0700

    fix gclient runhooks for windows

--- tools/run_third_party_dart.py
+++ tools/run_third_party_dart.py
@@ -3,8 +3,12 @@
 # found in the LICENSE file.
 
 import subprocess
+import os
 
 if __name__ == '__main__':
-    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/dart", "pub", "global", "activate", "-spath", "./src/flutter/tools/generate_package_config"])
-    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/dart", "pub", "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/flutter_frontend_server/.packages"])
-    subprocess.call(["src/third_party/dart/tools/sdks/dart-sdk/bin/dart", "pub", "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/tools/const_finder/.packages"])
+    pub = "src/third_party/dart/tools/sdks/dart-sdk/bin/pub"
+    if os.name == "nt":
+        pub = "src/third_party/dart/tools/sdks/dart-sdk/bin/pub.bat"
+    subprocess.call([pub, "global", "activate", "-spath", "./src/flutter/tools/generate_package_config"])
+    subprocess.call([pub, "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/flutter_frontend_server/.packages"])
+    subprocess.call([pub, "global", "run", "generate_package_config:generate_from_legacy", "src/flutter/tools/const_finder/.packages"])
