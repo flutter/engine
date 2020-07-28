ac95267aef5175b3f6c3387d502070c68f588ad5
nturgut <nurhan@google.com>
2020-07-28 12:40:49 -0700

    fixing cirrus flag issue (#20068)
    Co-authored-by: Christopher Fujino <christopherfujino@gmail.com>

--- tools/clone_flutter.sh
+++ tools/clone_flutter.sh
@@ -16,22 +16,16 @@ fi
 # Go to the engine git repo to get the date of the latest commit.
 cd $ENGINE_PATH/src/flutter
 
-# Special handling of release branches.
-ENGINE_BRANCH_NAME=$CIRRUS_BASE_BRANCH
-versionregex="^v[[:digit:]]+\."
-releasecandidateregex="^flutter-[[:digit:]]+\.[[:digit:]]+-candidate\.[[:digit:]]+$"
-ON_RELEASE_BRANCH=false
-echo "Engine on branch $ENGINE_BRANCH_NAME"
-if [[ $ENGINE_BRANCH_NAME =~ $versionregex || $ENGINE_BRANCH_NAME =~ $releasecandidateregex ]]
-then
-  echo "release branch $ENGINE_BRANCH_NAME"
-  ON_RELEASE_BRANCH=true
-fi
+# Special handling of release branches. We would like to run the tests against
+# the release branch of flutter.
+#
+# This is a shortcut for the release branch, since we didn't address this part
+# in LUCI yet.
+ENGINE_BRANCH_NAME="flutter-1.20-candidate.7"
 
-# Get latest commit's time for the engine repo.
-# Use date based on local time otherwise timezones might get mixed.
-LATEST_COMMIT_TIME_ENGINE=`git log -1 --date=local --format="%cd"`
-echo "Latest commit time on engine found as $LATEST_COMMIT_TIME_ENGINE"
+ON_RELEASE_BRANCH=true
+echo "release branch $ENGINE_BRANCH_NAME"
+echo "Engine on branch $ENGINE_BRANCH_NAME"
 
 # Check if there is an argument added for repo location.
 # If not use the location that should be set by Cirrus/LUCI.
@@ -53,27 +47,5 @@ else
 fi
 
 # Clone the Flutter Framework.
-git clone https://github.com/flutter/flutter.git
+git clone https://github.com/flutter/flutter.git -b "$ENGINE_BRANCH_NAME"
 cd flutter
-
-FRAMEWORK_BRANCH_NAME=`git branch | grep '*' | cut -d ' ' -f2`
-if [[ "$ON_RELEASE_BRANCH" = true && ENGINE_BRANCH_NAME != FRAMEWORK_BRANCH_NAME ]]
-then
-  echo "For a release framework and engine should be on the same version."
-  echo "Switching branches on Framework to from $FRAMEWORK_BRANCH_NAME to $ENGINE_BRANCH_NAME"
-  # Switch to the same version branch with the engine.
-  # If same version branch does not exits, fail.
-  SWITCH_RESULT=`git checkout $ENGINE_BRANCH_NAME` || true
-  if [[ -z "$SWITCH_RESULT" ]]
-  then
-    echo "$ENGINE_BRANCH_NAME Branch not found on framework. Quit."
-    exit 1
-  fi
-fi
-
-# Get the time of the youngest commit older than engine commit.
-# Git log uses commit date not the author date.
-# Before makes the comparison considering the timezone as well.
-COMMIT_NO=`git log --before="$LATEST_COMMIT_TIME_ENGINE" -n 1 | grep commit | cut -d ' ' -f2`
-echo "Using the flutter/flutter commit $COMMIT_NO";
-git reset --hard $COMMIT_NO
