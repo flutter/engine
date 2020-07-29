188b1749dcefef85c9590acbf8661ffd3ead3f29
nturgut <nurhan@google.com>
2020-07-29 14:00:04 -0700

    remove unused parts. add cast to fix analyze errors

--- lib/web_ui/dev/macos_info.dart
+++ lib/web_ui/dev/macos_info.dart
@@ -6,19 +6,17 @@ import 'dart:convert';
 import 'utils.dart';
 
 class MacOSInfo {
-  /// Default Safari Version.
-  String safariVersion = 'Safari 12';
-
   Future<void> collectInformation() async {
     final String systemProfileJson = await evalProcess(
         'system_profiler', ['SPApplicationsDataType', '-json']);
-    print('INFO: system_profiler run, information on Safari');
 
-    Map<String, dynamic> json = jsonDecode(systemProfileJson);
-    final List<dynamic> systemProfile = json.values.first;
+    final Map<String, dynamic> json =
+        jsonDecode(systemProfileJson) as Map<String, dynamic>;
+    final List<dynamic> systemProfile = json.values.first as List<dynamic>;
     for (int i = 0; i < systemProfile.length; i++) {
-      final Map<String, dynamic> application = systemProfile[i];
-      final String applicationName = application['_name'];
+      final Map<String, dynamic> application =
+          systemProfile[i] as Map<String, dynamic>;
+      final String applicationName = application['_name'] as String;
       if (applicationName.contains('Safari')) {
         print('application: $applicationName '
             'fullInfo: ${application.toString()}');
