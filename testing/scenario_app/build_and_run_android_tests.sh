94454f65f19a4f339a1bc7a7559095ee915c496a
Emmanuel Garcia <egarciad@google.com>
2020-08-07 17:24:52 -0700

    Remove pushd and popd

--- testing/scenario_app/build_and_run_android_tests.sh
+++ testing/scenario_app/build_and_run_android_tests.sh
@@ -11,11 +11,7 @@ if [ $# -eq 1 ]; then
   FLUTTER_ENGINE=$1
 fi
 
-cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
-
-pushd ../../..
-
-if [ ! -d "out/$FLUTTER_ENGINE" ]; then
+if [ ! -d "../../../out/$FLUTTER_ENGINE" ]; then
   echo "You must GN to generate out/$FLUTTER_ENGINE and its host engine."
   echo "Example: "
   echo "  ./tools/gn --android --unoptimized --android-cpu x64 --runtime-mode debug"
@@ -23,10 +19,6 @@ if [ ! -d "out/$FLUTTER_ENGINE" ]; then
   echo "to create out/android_debug_unopt_x64 and out/host_debug_unopt."
   exit 1
 fi
-
-autoninja -C out/$FLUTTER_ENGINE
-
-popd
 
 ./compile_android_jit.sh ../../../out/host_debug_unopt ../../../out/$FLUTTER_ENGINE/clang_x64
 
