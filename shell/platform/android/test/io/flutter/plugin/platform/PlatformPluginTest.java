f723cfcd28fea5f09911ae03cb72249b979891e5
Justin McCandless <jmccandless@google.com>
2020-08-10 16:20:27 -0700

    WIP trying and failing to test this

--- shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
+++ shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
@@ -1,15 +1,18 @@
 package io.flutter.plugin.platform;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import android.app.Activity;
+import android.content.ClipboardManager;
 import android.view.View;
 import android.view.Window;
 import io.flutter.embedding.engine.systemchannels.PlatformChannel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 @Config(manifest = Config.NONE)
@@ -31,5 +34,25 @@ public class PlatformPluginTest {
 
     // SELECTION_CLICK haptic response is only available on "LOLLIPOP" (21) and later.
     platformPlugin.vibrateHapticFeedback(PlatformChannel.HapticFeedbackType.SELECTION_CLICK);
+  }
+
+  @Test
+  public void platformPlugin_hasStrings() {
+    View fakeDecorView = mock(View.class);
+    Window fakeWindow = mock(Window.class);
+    when(fakeWindow.getDecorView()).thenReturn(fakeDecorView);
+    Activity fakeActivity = mock(Activity.class);
+    when(fakeActivity.getWindow()).thenReturn(fakeWindow);
+    PlatformChannel fakePlatformChannel = mock(PlatformChannel.class);
+    PlatformPlugin platformPlugin = new PlatformPlugin(fakeActivity, fakePlatformChannel);
+
+    ClipboardManager clipboardManager =
+        RuntimeEnvironment.application.getSystemService(ClipboardManager.class);
+    clipboardManager.setText("iamastring");
+
+    // TODO(justinmc): Can't do this because mPlatformMessageHandler is private.
+    // How can I send a message to the platform plugin?
+    //boolean hasStrings = platformPlugin.mPlatformMessageHandler.clipboardHasStrings();
+    //assertTrue(hasStrings);
   }
 }
