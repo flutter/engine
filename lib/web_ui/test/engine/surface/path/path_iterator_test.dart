54a89d224a0cdbec70ba43299d98472e00bd8f53
ferhatb <ferhat@gmail.com>
2020-08-10 13:07:56 -0700

    remove unused imports

--- lib/web_ui/test/engine/surface/path/path_iterator_test.dart
+++ lib/web_ui/test/engine/surface/path/path_iterator_test.dart
@@ -2,10 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:math' as math;
 import 'dart:typed_data';
 
-import 'package:ui/ui.dart';
 import 'package:ui/src/engine.dart';
 import 'package:test/test.dart';
 
