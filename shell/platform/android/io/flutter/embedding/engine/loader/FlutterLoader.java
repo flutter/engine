de357b2efa3423ee46e9a92f741b3213d3d2c4fb
Dan Field <dnfield@google.com>
2020-08-13 01:30:08 -0700

    bytes

--- shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
+++ shell/platform/android/io/flutter/embedding/engine/loader/FlutterLoader.java
@@ -245,7 +245,7 @@ public class FlutterLoader {
       }
 
       ActivityManager activityManager = (ActivityManager)getSystemService(ACTIVITY_SERVICE);
-      long oldGenHeapSizeBytes = activityManager.getLargeMemoryClass();
+      long oldGenHeapSizeBytes = activityManager.getLargeMemoryClass() * 1000000;
 
       FlutterJNI.nativeInit(
           applicationContext,
