a57c67439e92c96b307961319120aea4d8f2cd19
Gábor <deakjahn@gmail.com>
2020-08-27 08:19:45 +0200

    Update canvas_to_picture_test.dart

--- lib/web_ui/test/golden_tests/engine/canvas_to_picture_test.dart
+++ lib/web_ui/test/golden_tests/engine/canvas_to_picture_test.dart
@@ -7,9 +7,14 @@ import 'dart:html' as html;
 
 import 'package:ui/ui.dart';
 import 'package:ui/src/engine.dart';
+import 'package:test/bootstrap/browser.dart';
 import 'package:test/test.dart';
 
-void main() async {
+void main() {
+  internalBootstrapBrowserTest(() => testMain);
+}
+
+void testMain() async {
   final Rect region = Rect.fromLTWH(0, 0, 500, 500);
 
   setUp(() async {
