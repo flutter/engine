54301f3b8a5da36fc0e837384e737d055dd98fa9
Justin McCandless <jmccandless@google.com>
2020-09-03 12:07:57 -0700

    Split tests and some cleanup

--- shell/platform/darwin/macos/framework/Source/FlutterViewControllerTest.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterViewControllerTest.mm
@@ -2,16 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "flutter/shell/platform/darwin/macos/framework/Headers/FlutterViewController.h"
+
 #import "third_party/ocmock/Source/OCMock/OCMock.h"
 #include "flutter/shell/platform/darwin/macos/framework/Headers/FlutterEngine.h"
-#import "flutter/shell/platform/darwin/macos/framework/Headers/FlutterViewController.h"
 #include "flutter/shell/platform/darwin/macos/framework/Source/FlutterDartProject_Internal.h"
 #include "flutter/shell/platform/darwin/macos/framework/Source/FlutterEngine_Internal.h"
 #include "flutter/testing/testing.h"
 
 namespace flutter::testing {
 
-TEST(FlutterViewControllerTest, HasStrings) {
+TEST(FlutterViewControllerTest, HasStringsWhenPasteboardEmpty) {
   NSString* fixtures = @(testing::GetFixturesPath());
   FlutterDartProject* project = [[FlutterDartProject alloc]
       initWithAssetsPath:fixtures
@@ -22,43 +23,16 @@ TEST(FlutterViewControllerTest, HasStrings) {
   // real pasteboard.
   id pasteboardMock = OCMClassMock([NSPasteboard class]);
   __block NSString* clipboardString = @"";
-  void (^setStringMock)(NSInvocation*) = ^(NSInvocation *invocation) {
+  OCMStub([pasteboardMock setString:[OCMArg any] forType:[OCMArg any]]).andDo(^(NSInvocation *invocation) {
     [invocation getArgument:&clipboardString atIndex:2];
-  };
-  OCMStub([pasteboardMock setString:[OCMArg any] forType:[OCMArg any]]).andDo(setStringMock);
-  void (^stringForTypeMock)(NSInvocation*) = ^(NSInvocation *invocation) {
+  });
+  OCMExpect([pasteboardMock stringForType:[OCMArg any]]).andDo(^(NSInvocation *invocation) {
    [invocation setReturnValue:&clipboardString];
-  };
-  OCMExpect([pasteboardMock stringForType:[OCMArg any]]).andDo(stringForTypeMock);
+  });
   id viewControllerMock = OCMPartialMock(viewController);
   OCMStub([viewControllerMock _pasteboard]).andReturn(pasteboardMock);
 
-  // First call setClipboardData to put a string on the pasteboard.
-  __block bool calledSet = false;
-  FlutterResult resultSet = ^(id result) {
-    calledSet = true;
-  };
-  FlutterMethodCall* methodCallSet =
-      [FlutterMethodCall methodCallWithMethodName:@"Clipboard.setData"
-                                        arguments:@{@"text" : @"some string"}];
-  [viewControllerMock handleMethodCall:methodCallSet result:resultSet];
-  ASSERT_TRUE(calledSet);
-
-  // Call hasStrings and expect it to be true.
-  __block bool called = false;
-  __block bool value;
-  FlutterResult result = ^(id result) {
-    called = true;
-    NSNumber* valueNumber = [result valueForKey:@"value"];
-    value = [valueNumber boolValue];
-  };
-  FlutterMethodCall* methodCall =
-      [FlutterMethodCall methodCallWithMethodName:@"Clipboard.hasStrings" arguments:nil];
-  [viewControllerMock handleMethodCall:methodCall result:result];
-  ASSERT_TRUE(called);
-  ASSERT_TRUE(value);
-
-  // Now call setData again to clear the pasteboard.
+  // Call setData to make sure that the pasteboard is empty.
   __block bool calledSetClear = false;
   FlutterResult resultSetClear = ^(id result) {
     calledSetClear = true;
@@ -82,6 +56,52 @@ TEST(FlutterViewControllerTest, HasStrings) {
   [viewControllerMock handleMethodCall:methodCallAfterClear result:resultAfterClear];
   ASSERT_TRUE(calledAfterClear);
   ASSERT_FALSE(valueAfterClear);
+}
+
+TEST(FlutterViewControllerTest, HasStringsWhenPasteboardFull) {
+  NSString* fixtures = @(testing::GetFixturesPath());
+  FlutterDartProject* project = [[FlutterDartProject alloc]
+      initWithAssetsPath:fixtures
+             ICUDataPath:[fixtures stringByAppendingString:@"/icudtl.dat"]];
+  FlutterViewController* viewController = [[FlutterViewController alloc] initWithProject:project];
+
+  // Mock _pasteboard so that this test will work in environments without a
+  // real pasteboard.
+  id pasteboardMock = OCMClassMock([NSPasteboard class]);
+  __block NSString* clipboardString = @"";
+  OCMStub([pasteboardMock setString:[OCMArg any] forType:[OCMArg any]]).andDo(^(NSInvocation *invocation) {
+    [invocation getArgument:&clipboardString atIndex:2];
+  });
+  OCMExpect([pasteboardMock stringForType:[OCMArg any]]).andDo(^(NSInvocation *invocation) {
+   [invocation setReturnValue:&clipboardString];
+  });
+  id viewControllerMock = OCMPartialMock(viewController);
+  OCMStub([viewControllerMock _pasteboard]).andReturn(pasteboardMock);
+
+  // Call setClipboardData to make sure there's a string on the pasteboard.
+  __block bool calledSet = false;
+  FlutterResult resultSet = ^(id result) {
+    calledSet = true;
+  };
+  FlutterMethodCall* methodCallSet =
+      [FlutterMethodCall methodCallWithMethodName:@"Clipboard.setData"
+                                        arguments:@{@"text" : @"some string"}];
+  [viewControllerMock handleMethodCall:methodCallSet result:resultSet];
+  ASSERT_TRUE(calledSet);
+
+  // Call hasStrings and expect it to be true.
+  __block bool called = false;
+  __block bool value;
+  FlutterResult result = ^(id result) {
+    called = true;
+    NSNumber* valueNumber = [result valueForKey:@"value"];
+    value = [valueNumber boolValue];
+  };
+  FlutterMethodCall* methodCall =
+      [FlutterMethodCall methodCallWithMethodName:@"Clipboard.hasStrings" arguments:nil];
+  [viewControllerMock handleMethodCall:methodCall result:result];
+  ASSERT_TRUE(called);
+  ASSERT_TRUE(value);
 }
 
 }  // flutter::testing
