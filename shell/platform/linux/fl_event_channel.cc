8a6f24f11b70adc7cd90e9d11b8f67f3ca5d3c62
Robert Ancell <robert.ancell@canonical.com>
2020-09-16 14:21:56 +1200

    Add FlEventChannel (#65270)

---
+++ shell/platform/linux/fl_event_channel.cc
@@ -0,0 +1,233 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/linux/public/flutter_linux/fl_event_channel.h"
+
+#include <gmodule.h>
+
+#include "flutter/shell/platform/linux/fl_method_codec_private.h"
+
+static constexpr char kListenMethod[] = "listen";
+static constexpr char kCancelMethod[] = "cancel";
+static constexpr char kEventRequestError[] = "error";
+
+struct _FlEventChannel {
+  GObject parent_instance;
+
+  // Messenger to communicate on.
+  FlBinaryMessenger* messenger;
+
+  // Channel name.
+  gchar* name;
+
+  // Codec to en/decode messages.
+  FlMethodCodec* codec;
+
+  // Function called when the stream is listened to / cancelled.
+  FlEventChannelHandler listen_handler;
+  FlEventChannelHandler cancel_handler;
+  gpointer handler_data;
+  GDestroyNotify handler_data_destroy_notify;
+};
+
+struct _FlEventChannelResponseHandle {
+  GObject parent_instance;
+
+  FlBinaryMessengerResponseHandle* response_handle;
+};
+
+// Added here to stop the compiler from optimising this function away.
+G_MODULE_EXPORT GType fl_event_channel_get_type();
+
+G_DEFINE_TYPE(FlEventChannel, fl_event_channel, G_TYPE_OBJECT)
+
+// Handle method calls from the Dart side of the channel.
+static FlMethodResponse* handle_method_call(FlEventChannel* self,
+                                            const gchar* name) {
+  FlEventChannelHandler handler;
+  if (g_strcmp0(name, kListenMethod) == 0) {
+    handler = self->listen_handler;
+  } else if (g_strcmp0(name, kCancelMethod) == 0) {
+    handler = self->cancel_handler;
+  } else {
+    g_autofree gchar* message =
+        g_strdup_printf("Unknown event channel request '%s'", name);
+    return FL_METHOD_RESPONSE(
+        fl_method_error_response_new(kEventRequestError, message, nullptr));
+  }
+
+  // If not handled, just accept requests.
+  if (handler == nullptr) {
+    return FL_METHOD_RESPONSE(fl_method_success_response_new(nullptr));
+  }
+
+  FlMethodResponse* response = handler(self, self->handler_data);
+  if (response == nullptr) {
+    g_autofree gchar* message =
+        g_strdup_printf("Event channel request '%s' not responded to", name);
+    return FL_METHOD_RESPONSE(
+        fl_method_error_response_new(kEventRequestError, message, nullptr));
+  }
+
+  if (FL_IS_METHOD_SUCCESS_RESPONSE(response)) {
+    if (fl_method_success_response_get_result(
+            FL_METHOD_SUCCESS_RESPONSE(response)) != nullptr) {
+      g_warning("Event channel %s responded to '%s' with value", self->name,
+                name);
+    }
+  } else if (FL_IS_METHOD_ERROR_RESPONSE(response)) {
+  } else {
+    g_warning("Event channel %s responded to '%s' with response other than ",
+              self->name, name);
+  }
+
+  return response;
+}
+
+// Called when a binary message is received on this channel.
+static void message_cb(FlBinaryMessenger* messenger,
+                       const gchar* channel,
+                       GBytes* message,
+                       FlBinaryMessengerResponseHandle* response_handle,
+                       gpointer user_data) {
+  FlEventChannel* self = FL_EVENT_CHANNEL(user_data);
+
+  g_autofree gchar* name = nullptr;
+  g_autoptr(GError) error = nullptr;
+  g_autoptr(FlValue) args = nullptr;
+  if (!fl_method_codec_decode_method_call(self->codec, message, &name, &args,
+                                          &error)) {
+    g_warning("Failed to decode message on event channel %s: %s", self->name,
+              error->message);
+    fl_binary_messenger_send_response(messenger, response_handle, nullptr,
+                                      nullptr);
+    return;
+  }
+
+  g_autoptr(FlMethodResponse) response = handle_method_call(self, name);
+
+  g_autoptr(GBytes) data = nullptr;
+  if (FL_IS_METHOD_SUCCESS_RESPONSE(response)) {
+    FlMethodSuccessResponse* r = FL_METHOD_SUCCESS_RESPONSE(response);
+    g_autoptr(GError) codec_error = nullptr;
+    data = fl_method_codec_encode_success_envelope(
+        self->codec, fl_method_success_response_get_result(r), &codec_error);
+    if (data == nullptr) {
+      g_warning("Failed to encode event channel %s success response: %s",
+                self->name, codec_error->message);
+    }
+  } else if (FL_IS_METHOD_ERROR_RESPONSE(response)) {
+    FlMethodErrorResponse* r = FL_METHOD_ERROR_RESPONSE(response);
+    g_autoptr(GError) codec_error = nullptr;
+    data = fl_method_codec_encode_error_envelope(
+        self->codec, fl_method_error_response_get_code(r),
+        fl_method_error_response_get_message(r),
+        fl_method_error_response_get_details(r), &codec_error);
+    if (data == nullptr) {
+      g_warning("Failed to encode event channel %s error response: %s",
+                self->name, codec_error->message);
+    }
+  } else if (FL_IS_METHOD_NOT_IMPLEMENTED_RESPONSE(response)) {
+    data = nullptr;
+  } else {
+    g_assert_not_reached();
+  }
+
+  if (!fl_binary_messenger_send_response(messenger, response_handle, data,
+                                         &error)) {
+    g_warning("Failed to send event channel response: %s", error->message);
+  }
+}
+
+// Called when the channel handler is closed.
+static void channel_closed_cb(gpointer user_data) {
+  g_autoptr(FlEventChannel) self = FL_EVENT_CHANNEL(user_data);
+
+  // Disconnect handlers.
+  if (self->handler_data_destroy_notify != nullptr) {
+    self->handler_data_destroy_notify(self->handler_data);
+  }
+  self->listen_handler = nullptr;
+  self->cancel_handler = nullptr;
+  self->handler_data = nullptr;
+  self->handler_data_destroy_notify = nullptr;
+}
+
+static void fl_event_channel_dispose(GObject* object) {
+  FlEventChannel* self = FL_EVENT_CHANNEL(object);
+
+  if (self->messenger != nullptr) {
+    fl_binary_messenger_set_message_handler_on_channel(
+        self->messenger, self->name, nullptr, nullptr, nullptr);
+  }
+
+  g_clear_object(&self->messenger);
+  g_clear_pointer(&self->name, g_free);
+  g_clear_object(&self->codec);
+
+  if (self->handler_data_destroy_notify != nullptr) {
+    self->handler_data_destroy_notify(self->handler_data);
+  }
+  self->listen_handler = nullptr;
+  self->cancel_handler = nullptr;
+  self->handler_data = nullptr;
+  self->handler_data_destroy_notify = nullptr;
+
+  G_OBJECT_CLASS(fl_event_channel_parent_class)->dispose(object);
+}
+
+static void fl_event_channel_class_init(FlEventChannelClass* klass) {
+  G_OBJECT_CLASS(klass)->dispose = fl_event_channel_dispose;
+}
+
+static void fl_event_channel_init(FlEventChannel* self) {}
+
+G_MODULE_EXPORT FlEventChannel* fl_event_channel_new(
+    FlBinaryMessenger* messenger,
+    const gchar* name,
+    FlMethodCodec* codec,
+    FlEventChannelHandler listen_handler,
+    FlEventChannelHandler cancel_handler,
+    gpointer user_data,
+    GDestroyNotify destroy_notify) {
+  g_return_val_if_fail(FL_IS_BINARY_MESSENGER(messenger), nullptr);
+  g_return_val_if_fail(name != nullptr, nullptr);
+  g_return_val_if_fail(FL_IS_METHOD_CODEC(codec), nullptr);
+
+  FlEventChannel* self =
+      FL_EVENT_CHANNEL(g_object_new(fl_event_channel_get_type(), nullptr));
+
+  self->messenger = FL_BINARY_MESSENGER(g_object_ref(messenger));
+  self->name = g_strdup(name);
+  self->codec = FL_METHOD_CODEC(g_object_ref(codec));
+  self->listen_handler = listen_handler;
+  self->cancel_handler = cancel_handler;
+  self->handler_data = user_data;
+  self->handler_data_destroy_notify = destroy_notify;
+
+  fl_binary_messenger_set_message_handler_on_channel(
+      self->messenger, self->name, message_cb, g_object_ref(self),
+      channel_closed_cb);
+
+  return self;
+}
+
+G_MODULE_EXPORT gboolean fl_event_channel_send(FlEventChannel* self,
+                                               FlValue* event,
+                                               GCancellable* cancellable,
+                                               GError** error) {
+  g_return_val_if_fail(FL_IS_EVENT_CHANNEL(self), FALSE);
+  g_return_val_if_fail(event != nullptr, FALSE);
+
+  g_autoptr(GBytes) data =
+      fl_method_codec_encode_success_envelope(self->codec, event, error);
+  if (data == nullptr) {
+    return FALSE;
+  }
+
+  fl_binary_messenger_send_on_channel(self->messenger, self->name, data,
+                                      cancellable, nullptr, nullptr);
+
+  return TRUE;
+}
---
+++ shell/platform/linux/public/flutter_linux/fl_event_channel.h
@@ -0,0 +1,144 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_SHELL_PLATFORM_LINUX_FL_EVENT_CHANNEL_H_
+#define FLUTTER_SHELL_PLATFORM_LINUX_FL_EVENT_CHANNEL_H_
+
+#if !defined(__FLUTTER_LINUX_INSIDE__) && !defined(FLUTTER_LINUX_COMPILATION)
+#error "Only <flutter_linux/flutter_linux.h> can be included directly."
+#endif
+
+#include <gio/gio.h>
+#include <glib-object.h>
+
+#include "fl_binary_messenger.h"
+#include "fl_method_channel.h"
+#include "fl_method_response.h"
+
+G_BEGIN_DECLS
+
+G_DECLARE_FINAL_TYPE(FlEventChannel,
+                     fl_event_channel,
+                     FL,
+                     EVENT_CHANNEL,
+                     GObject)
+
+G_DECLARE_FINAL_TYPE(FlEventChannelResponseHandle,
+                     fl_event_channel_response_handle,
+                     FL,
+                     EVENT_CHANNEL_RESPONSE_HANDLE,
+                     GObject)
+
+/**
+ * FlEventChannel:
+ *
+ * #FlEventChannel is an object that allows sending
+ * an events stream to Dart code over platform channels.
+ *
+ * The following example shows how to send events on a channel:
+ *
+ * |[<!-- language="C" -->
+ * static FlEventChannel *channel = NULL;
+ * static gboolean send_events = FALSE;
+ *
+ * static void event_occurs_cb (FooEvent *event) {
+ *   if (send_events) {
+ *     g_autoptr(FlValue) message = foo_event_to_value (event);
+ *     g_autoptr(GError) error = NULL;
+ *     if (!fl_event_channel_send (channel, message, NULL, &error)) {
+ *       g_warning ("Failed to send event: %s", error->message);
+ *     }
+ *   }
+ * }
+ *
+ * static FlMethodResponse* listen_cb (FlEventChannel* channel,
+ *                                     gpointer user_data) {
+ *   send_events = TRUE;
+ *   return FL_METHOD_RESPONSE (fl_method_success_response_new (NULL));
+ * }
+ *
+ * static FlMethodResponse* cancel_cb (GObject *object,
+ *                                     gpointer user_data) {
+ *   send_events = FALSE;
+ *   return FL_METHOD_RESPONSE (fl_method_success_response_new (NULL));
+ * }
+ *
+ * static void setup_channel () {
+ *   g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new ();
+ *   channel = fl_event_channel_new (messenger, "flutter/foo",
+ *                                   FL_METHOD_CODEC (codec),
+ *                                   listen_cb, cancel_cb, NULL, NULL);
+ * }
+ * ]|
+ *
+ * #FlEventChannel matches the EventChannel class in the Flutter
+ * services library.
+ */
+
+/**
+ * FlEventChannelResponseHandle:
+ *
+ * #FlEventChannelResponseHandle is an object used to send responses
+ * with.
+ */
+
+/**
+ * FlEventChannelHandler:
+ * @channel: an #FlEventChannel.
+ * @user_data: (closure): data provided when registering this handler.
+ *
+ * Function called when the stream is listened to or cancelled.
+ *
+ * Returns: (transfer full): an #FlMethodResponse for this request.
+ */
+typedef FlMethodResponse* (*FlEventChannelHandler)(FlEventChannel* channel,
+                                                   gpointer user_data);
+
+/**
+ * fl_event_channel_new:
+ * @messenger: an #FlBinaryMessenger.
+ * @name: a channel name.
+ * @codec: the message codec.
+ * @listen_handler: (allow-none): function to call when the Dart side of the
+ * channel starts listening to the stream.
+ * @cancel_handler: (allow-none): function to call when the Dart side of the
+ * channel cancels their subscription to the stream.
+ * @user_data: (closure): user data to pass to @listen_handler and
+ * @cancel_handler.
+ * @destroy_notify: (allow-none): a function which gets called to free
+ * @user_data, or %NULL.
+ *
+ * Creates an event channel. @codec must match the codec used on the Dart
+ * end of the channel.
+ *
+ * Returns: a new #FlEventChannel.
+ */
+FlEventChannel* fl_event_channel_new(FlBinaryMessenger* messenger,
+                                     const gchar* name,
+                                     FlMethodCodec* codec,
+                                     FlEventChannelHandler listen_handler,
+                                     FlEventChannelHandler cancel_handler,
+                                     gpointer user_data,
+                                     GDestroyNotify destroy_notify);
+
+/**
+ * fl_event_channel_send:
+ * @channel: an #FlEventChannel.
+ * @event: event to send, must match what the #FlMethodCodec supports.
+ * @cancellable: (allow-none): a #GCancellable or %NULL.
+ * @error: (allow-none): #GError location to store the error occurring, or %NULL
+ * to ignore.
+ *
+ * Sends an event on the channel.
+ *
+ * Returns: %TRUE if successful.
+ */
+gboolean fl_event_channel_send(FlEventChannel* channel,
+                               FlValue* event,
+                               GCancellable* cancellable,
+                               GError** error);
+
+G_END_DECLS
+
+#endif  // FLUTTER_SHELL_PLATFORM_LINUX_FL_EVENT_CHANNEL_H_
