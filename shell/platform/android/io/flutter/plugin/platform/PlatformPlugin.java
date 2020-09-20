9e4c909097592ccd73ee15169feb3036dda07ede
Hamdi Kahloun <32666446+hamdikahloun@users.noreply.github.com>
2020-09-20 10:49:52 +0100

    Update PlatformPlugin.java

--- shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
+++ shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
@@ -292,6 +292,7 @@ public class PlatformPlugin {
     }
 
     ClipData clip = clipboard.getPrimaryClip();
+    if (clip == null) return null;
 
     if (format == null || format == PlatformChannel.ClipboardContentFormat.PLAIN_TEXT) {
       return clip.getItemAt(0).coerceToText(activity);
