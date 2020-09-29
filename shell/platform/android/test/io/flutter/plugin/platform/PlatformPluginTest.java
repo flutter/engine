d9ea4ae344c8885d6a2f4c36c52f310b6868cbc5
Hamdi Kahloun <32666446+hamdikahloun@users.noreply.github.com>
2020-09-29 23:26:34 +0100

    Update PlatformPluginTest.java

--- shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
+++ shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
@@ -64,12 +64,7 @@ public class PlatformPluginTest {
     clipboardManager.setPrimaryClip(clip);
     assertNotNull(platformPlugin.mPlatformMessageHandler.getClipboardData(clipboardFormat));
 
-    Uri uri = Uri.parse("content://media/external_primary/images/media/");
-    clip = ClipData.newUri(fakeActivity.getContentResolver(), "URI", uri);
-    clipboardManager.setPrimaryClip(clip);
-    assertNull(platformPlugin.mPlatformMessageHandler.getClipboardData(clipboardFormat));
-
-    uri =
+    Uri uri =
         RingtoneManager.getActualDefaultRingtoneUri(
             fakeActivity.getApplicationContext(), RingtoneManager.TYPE_RINGTONE);
     clip = ClipData.newUri(fakeActivity.getContentResolver(), "URI", uri);
