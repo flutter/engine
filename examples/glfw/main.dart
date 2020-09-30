07e6e2f014c9c5747be11500a083791b534f097a
Neha <55926436+Neha-alt@users.noreply.github.com>
2020-09-30 22:55:06 +0530

    Update main.dart

--- examples/glfw/main.dart
+++ examples/glfw/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
