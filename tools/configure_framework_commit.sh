3afd52e7a52250198a8f99df770e6a37d28c223f
nturgut <nurhan@google.com>
2020-10-01 18:56:32 -0700

    use the file method

--- tools/configure_framework_commit.sh
+++ tools/configure_framework_commit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 if [[ -z $ENGINE_PATH ]]
 then
@@ -13,6 +14,7 @@ cd $ENGINE_PATH/src/flutter
 # Get latest commit's time for the engine repo.
 # Use date based on local time otherwise timezones might get mixed.
 LATEST_COMMIT_TIME_ENGINE=`git log -1 --date=local --format="%cd"`
+echo "Latest commit time on engine found as $LATEST_COMMIT_TIME_ENGINE"
 
 if [[ -z $FLUTTER_CLONE_REPO_PATH ]]
 then
@@ -26,7 +28,11 @@ fi
 # Git log uses commit date not the author date.
 # Before makes the comparison considering the timezone as well.
 COMMIT_NO=`git log --before="$LATEST_COMMIT_TIME_ENGINE" -n 1 | grep commit | cut -d ' ' -f2`
+echo "Using the flutter/flutter commit $COMMIT_NO";
 git reset --hard $COMMIT_NO
 
-# Set commit no to an env variable.
-echo '{ "FLUTTER_REF" : ' $COMMIT_NO '} '
+# Write the commit number to a file. This file will be read by the LUCI recipe.
+echo "$COMMIT_NO" >> flutter_ref.txt
+
+# Print out the flutter version for troubleshooting
+$FLUTTER_CLONE_REPO_PATH/bin/flutter --version
