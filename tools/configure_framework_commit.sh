4f243aac416bd8d67cfb42f615446b61dc19eafd
nturgut <nurhan@google.com>
2020-10-01 18:26:00 -0700

    remove all the other stdout

--- tools/configure_framework_commit.sh
+++ tools/configure_framework_commit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-set -x
 
 if [[ -z $ENGINE_PATH ]]
 then
@@ -14,7 +13,6 @@ cd $ENGINE_PATH/src/flutter
 # Get latest commit's time for the engine repo.
 # Use date based on local time otherwise timezones might get mixed.
 LATEST_COMMIT_TIME_ENGINE=`git log -1 --date=local --format="%cd"`
-echo "Latest commit time on engine found as $LATEST_COMMIT_TIME_ENGINE"
 
 if [[ -z $FLUTTER_CLONE_REPO_PATH ]]
 then
@@ -28,11 +26,7 @@ fi
 # Git log uses commit date not the author date.
 # Before makes the comparison considering the timezone as well.
 COMMIT_NO=`git log --before="$LATEST_COMMIT_TIME_ENGINE" -n 1 | grep commit | cut -d ' ' -f2`
-echo "Using the flutter/flutter commit $COMMIT_NO";
 git reset --hard $COMMIT_NO
 
 # Set commit no to an env variable.
 echo "$COMMIT_NO"
-
-# Print out the flutter version for troubleshooting
-$FLUTTER_CLONE_REPO_PATH/bin/flutter --version
