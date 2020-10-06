46777f0807cc1237167239b3a0925b7a49528d07
LongCat is Looong <31859944+LongCatIsLooong@users.noreply.github.com>
2020-10-05 17:48:19 -0700

    test

--- testing/scenario_app/compile_ios_jit.sh
+++ testing/scenario_app/compile_ios_jit.sh
@@ -85,7 +85,7 @@ echo "Compiling JIT Snapshot..."
 
 cp "$DEVICE_TOOLS/../gen/flutter/lib/snapshot/vm_isolate_snapshot.bin" "$OUTDIR/App.framework/flutter_assets/vm_snapshot_data"
 
-SYSROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+SYSROOT=$(xcrun --sdk iphonesimulator13.0 --show-sdk-path)
 echo "Using $SYSROOT as sysroot."
 
 echo "Creating stub App using $SYSROOT..."
