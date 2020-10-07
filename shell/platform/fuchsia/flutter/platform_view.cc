ff4e585fd40c4f1b4c0b9475edaf8a54133c842b
Yuqian Li <liyuqian@google.com>
2020-10-07 12:56:25 -0700

    Revert "fuchsia: Don't send ViewportMetrics w/ 0 DPR (#21392)"
    This reverts commit d5dddf32e77dd12db470fcefd37b686dd1f50696.

--- shell/platform/fuchsia/flutter/platform_view.cc
+++ shell/platform/fuchsia/flutter/platform_view.cc
@@ -14,11 +14,10 @@
 #include "flutter/fml/logging.h"
 #include "flutter/lib/ui/window/pointer_data.h"
 #include "flutter/lib/ui/window/window.h"
-#include "third_party/rapidjson/include/rapidjson/document.h"
-#include "third_party/rapidjson/include/rapidjson/stringbuffer.h"
-#include "third_party/rapidjson/include/rapidjson/writer.h"
-
 #include "logging.h"
+#include "rapidjson/document.h"
+#include "rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
 #include "runtime/dart/utils/inlines.h"
 #include "vsync_waiter.h"
 
@@ -215,8 +214,7 @@ void PlatformView::OnScenicError(std::string error) {
 void PlatformView::OnScenicEvent(
     std::vector<fuchsia::ui::scenic::Event> events) {
   TRACE_EVENT0("flutter", "PlatformView::OnScenicEvent");
-
-  bool metrics_changed = false;
+  bool should_update_metrics = false;
   for (const auto& event : events) {
     switch (event.Which()) {
       case fuchsia::ui::scenic::Event::Tag::kGfx:
@@ -225,52 +223,31 @@ void PlatformView::OnScenicEvent(
             const fuchsia::ui::gfx::Metrics& metrics =
                 event.gfx().metrics().metrics;
             const float new_view_pixel_ratio = metrics.scale_x;
-            if (new_view_pixel_ratio <= 0.f) {
-              FML_DLOG(ERROR)
-                  << "Got an invalid pixel ratio from Scenic; ignoring: "
-                  << new_view_pixel_ratio;
-              break;
-            }
 
             // Avoid metrics update when possible -- it is computationally
             // expensive.
-            if (view_pixel_ratio_.has_value() &&
-                *view_pixel_ratio_ == new_view_pixel_ratio) {
-              FML_DLOG(ERROR)
-                  << "Got an identical pixel ratio from Scenic; ignoring: "
-                  << new_view_pixel_ratio;
-              break;
+            if (view_pixel_ratio_ != new_view_pixel_ratio) {
+              view_pixel_ratio_ = new_view_pixel_ratio;
+              should_update_metrics = true;
             }
-
-            view_pixel_ratio_ = new_view_pixel_ratio;
-            metrics_changed = true;
             break;
           }
           case fuchsia::ui::gfx::Event::Tag::kViewPropertiesChanged: {
             const fuchsia::ui::gfx::BoundingBox& bounding_box =
                 event.gfx().view_properties_changed().properties.bounding_box;
-            const std::pair<float, float> new_view_size = {
-                std::max(bounding_box.max.x - bounding_box.min.x, 0.0f),
-                std::max(bounding_box.max.y - bounding_box.min.y, 0.0f)};
-            if (new_view_size.first <= 0.f || new_view_size.second <= 0.f) {
-              FML_DLOG(ERROR)
-                  << "Got an invalid view size from Scenic; ignoring: "
-                  << new_view_size.first << " " << new_view_size.second;
-              break;
-            }
+            const float new_view_width =
+                std::max(bounding_box.max.x - bounding_box.min.x, 0.0f);
+            const float new_view_height =
+                std::max(bounding_box.max.y - bounding_box.min.y, 0.0f);
 
             // Avoid metrics update when possible -- it is computationally
             // expensive.
-            if (view_logical_size_.has_value() &&
-                *view_logical_size_ == new_view_size) {
-              FML_DLOG(ERROR)
-                  << "Got an identical view size from Scenic; ignoring: "
-                  << new_view_size.first << " " << new_view_size.second;
-              break;
+            if (view_width_ != new_view_width ||
+                view_height_ != new_view_width) {
+              view_width_ = new_view_width;
+              view_height_ = new_view_height;
+              should_update_metrics = true;
             }
-
-            view_logical_size_ = new_view_size;
-            metrics_changed = true;
             break;
           }
           case fuchsia::ui::gfx::Event::Tag::kViewConnected:
@@ -321,22 +298,19 @@ void PlatformView::OnScenicEvent(
     }
   }
 
-  if (view_pixel_ratio_.has_value() && view_logical_size_.has_value() &&
-      metrics_changed) {
-    const float pixel_ratio = *view_pixel_ratio_;
-    const std::pair<float, float> logical_size = *view_logical_size_;
+  if (should_update_metrics) {
     SetViewportMetrics({
-        pixel_ratio,                        // device_pixel_ratio
-        logical_size.first * pixel_ratio,   // physical_width
-        logical_size.second * pixel_ratio,  // physical_height
-        0.0f,                               // physical_padding_top
-        0.0f,                               // physical_padding_right
-        0.0f,                               // physical_padding_bottom
-        0.0f,                               // physical_padding_left
-        0.0f,                               // physical_view_inset_top
-        0.0f,                               // physical_view_inset_right
-        0.0f,                               // physical_view_inset_bottom
-        0.0f,                               // physical_view_inset_left
+        view_pixel_ratio_,                 // device_pixel_ratio
+        view_width_ * view_pixel_ratio_,   // physical_width
+        view_height_ * view_pixel_ratio_,  // physical_height
+        0.0f,                              // physical_padding_top
+        0.0f,                              // physical_padding_right
+        0.0f,                              // physical_padding_bottom
+        0.0f,                              // physical_padding_left
+        0.0f,                              // physical_view_inset_top
+        0.0f,                              // physical_view_inset_right
+        0.0f,                              // physical_view_inset_bottom
+        0.0f,                              // physical_view_inset_left
         0.0f,  // p_physical_system_gesture_inset_top
         0.0f,  // p_physical_system_gesture_inset_right
         0.0f,  // p_physical_system_gesture_inset_bottom
@@ -447,9 +421,6 @@ bool PlatformView::OnHandlePointerEvent(
       PointerTraceHACK(pointer.radius_major, pointer.radius_minor);
   TRACE_FLOW_END("input", "dispatch_event_to_client", trace_id);
 
-  const float pixel_ratio =
-      view_pixel_ratio_.has_value() ? *view_pixel_ratio_ : 0.f;
-
   flutter::PointerData pointer_data;
   pointer_data.Clear();
   pointer_data.time_stamp = pointer.event_time / 1000;
@@ -457,8 +428,8 @@ bool PlatformView::OnHandlePointerEvent(
   pointer_data.kind = GetKindFromPointerType(pointer.type);
   pointer_data.device = pointer.pointer_id;
   // Pointer events are in logical pixels, so scale to physical.
-  pointer_data.physical_x = pointer.x * pixel_ratio;
-  pointer_data.physical_y = pointer.y * pixel_ratio;
+  pointer_data.physical_x = pointer.x * view_pixel_ratio_;
+  pointer_data.physical_y = pointer.y * view_pixel_ratio_;
   // Buttons are single bit values starting with kMousePrimaryButton = 1.
   pointer_data.buttons = static_cast<uint64_t>(pointer.buttons);
 
@@ -630,10 +601,7 @@ void PlatformView::DispatchSemanticsAction(int32_t node_id,
 void PlatformView::UpdateSemantics(
     flutter::SemanticsNodeUpdates update,
     flutter::CustomAccessibilityActionUpdates actions) {
-  const float pixel_ratio =
-      view_pixel_ratio_.has_value() ? *view_pixel_ratio_ : 0.f;
-
-  accessibility_bridge_->AddSemanticsNodeUpdate(update, pixel_ratio);
+  accessibility_bridge_->AddSemanticsNodeUpdate(update, view_pixel_ratio_);
 }
 
 // Channel handler for kAccessibilityChannel
