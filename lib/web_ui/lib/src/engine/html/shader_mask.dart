dadfb3bee867b8d8853e4d4a14b99c53ff9c29bd
anirudhb <anirudhb@users.noreply.github.com>
2020-10-11 16:23:41 -0700

    Use SVG for shader mask blending

--- lib/web_ui/lib/src/engine/html/shader_mask.dart
+++ lib/web_ui/lib/src/engine/html/shader_mask.dart
@@ -62,48 +62,6 @@ class PersistedShaderMask extends PersistedContainerSurface
     if (_shaderElement != null) {
       _shaderElement?.remove();
     }
-    String? cssBlendMode = {
-      ui.BlendMode.screen: 'screen',
-      ui.BlendMode.overlay: 'overlay',
-      ui.BlendMode.darken: 'darken',
-      ui.BlendMode.lighten: 'lighten',
-      ui.BlendMode.colorDodge: 'color-dodge',
-      ui.BlendMode.colorBurn: 'color-burn',
-      ui.BlendMode.hardLight: 'hard-light',
-      ui.BlendMode.softLight: 'soft-light',
-      ui.BlendMode.difference: 'difference',
-      ui.BlendMode.exclusion: 'exclusion',
-      ui.BlendMode.multiply: 'multiply',
-      ui.BlendMode.hue: 'hue',
-      ui.BlendMode.saturation: 'saturation',
-      ui.BlendMode.color: 'color',
-      ui.BlendMode.luminosity: 'luminosity',
-    }[blendMode];
-    if (cssBlendMode == null) {
-      switch (blendMode) {
-        case ui.BlendMode.clear:
-        case ui.BlendMode.dstOut:
-        case ui.BlendMode.srcOut:
-          childContainer?.style.visibility = 'hidden';
-          return;
-        // TODO(flutter-web): Additional blend modes for ShaderMask
-        case ui.BlendMode.dst:
-        case ui.BlendMode.dstIn:
-        case ui.BlendMode.src:
-        case ui.BlendMode.srcOver:
-        case ui.BlendMode.dstOver:
-        case ui.BlendMode.srcIn:
-        case ui.BlendMode.srcATop:
-        case ui.BlendMode.dstATop:
-        case ui.BlendMode.xor:
-        case ui.BlendMode.plus:
-        case ui.BlendMode.modulate:
-          break;
-        default:
-          // Handled above
-          break;
-      }
-    }
 
     // TODO(flutter-web): Support non-gradient shaders for ShaderMask.
     if (!(shader is ui.Gradient)) {
@@ -115,12 +73,7 @@ class PersistedShaderMask extends PersistedContainerSurface
       return;
     }
 
-    _shaderElement = html.Element.tag('flt-mask');
-    rootElement!.append(_shaderElement!);
-    if (cssBlendMode != null) {
-      _shaderElement!.style.mixBlendMode = cssBlendMode;
-    }
-
+    String inputImageSvg;
     if (gradient is GradientLinear) {
       // TODO(flutter-web): More accurate linear gradients for ShaderMask.
       // This seems to be accurate enough, but currently it only uses the angle.
@@ -143,35 +96,96 @@ class PersistedShaderMask extends PersistedContainerSurface
         toY = matrix4.transformedY + centerY;
       }
       final angle = math.atan2(toY - fromY, toX - fromX) * (180 / math.pi) + 90;
-      final stops = <String>[];
+      final lines = [
+        '<svg xmlns="http://www.w3.org/2000/svg" x=\'${maskRect.left}px\' y=\'${maskRect.top}px\' width=\'${maskRect.width}px\' height=\'${maskRect.height}px\'>'
+            '<defs><linearGradient id=\'gradient\' gradientTransform=\'rotate($angle)\'>'
+      ];
       for (int i = 0; i < gradient.colors.length; i++) {
-        final stop = gradient.colorStops?[i];
+        final stop =
+            gradient.colorStops?[i] ?? i / (gradient.colors.length - 1);
         final color = gradient.colors[i];
-        stops.add('${colorToCssString(color)}' +
-            (stop != null ? ' ${(stop * 100).round()}%' : ''));
+        lines.add(
+            '<stop stop-color=\'${colorToCssString(color)}\' offset=\'${(stop * 100).round()}%\'/>');
       }
-      _shaderElement!.style.background =
-          'linear-gradient(${angle}deg, ${stops.join(', ')})';
+      lines.add('</linearGradient></defs><rect x=\'0%\' y=\'0%\' '
+          'width=\'100%\' height=\'100%\' fill=\'url(#gradient)\'></rect></svg>');
+      inputImageSvg = lines.join('');
     } else if (gradient is GradientRadial) {
-      // TODO(flutter-web): Use the radius property of radial gradients.
-      // Not sure what effect it is supposed to have and they seem to be working
-      // well enough without it.
-      final stops = <String>[];
+      final lines = [
+        '<svg xmlns="http://www.w3.org/2000/svg" x=\'${maskRect.left}px\' y=\'${maskRect.top}px\' width=\'${maskRect.width}px\' height=\'${maskRect.height}px\'><defs>'
+            '<radialGradient id=\'gradient\' '
+            'cx=\'${maskRect.left + gradient.center.dx}px\' cy=\'${maskRect.top + gradient.center.dy}px\' r=\'${gradient.radius}px\'>'
+      ];
       for (int i = 0; i < gradient.colors.length; i++) {
-        final stop = gradient.colorStops?[i];
+        final stop =
+            gradient.colorStops?[i] ?? i / (gradient.colors.length - 1);
         final color = gradient.colors[i];
-        stops.add('${colorToCssString(color)}' +
-            (stop != null ? ' ${(stop * 100).round()}%' : ''));
+        lines.add(
+            '<stop stop-color=\'${colorToCssString(color)}\' offset=\'${(stop * 100).round()}%\'/>');
       }
-      _shaderElement!.style.background =
-          'radial-gradient(circle at ${gradient.center.dx}px ${gradient.center.dy}px, ${stops.join(', ')}';
+      lines.add(
+          '</radialGradient></defs><rect x=\'0%\' y=\'0%\' '
+          'width=\'100%\' height=\'100%\' fill=\'url(#gradient)\'></rect></svg>');
+      inputImageSvg = lines.join('');
+    } else {
+      // unreachable, just added to make analyzer happy
+      return;
     }
-    _shaderElement!.style
-      ..left = '${maskRect.left}px'
-      ..top = '${maskRect.top}px'
-      ..width = '${maskRect.width}px'
-      ..height = '${maskRect.height}px'
-      ..position = 'absolute';
+    // This is required since Firefox doesn't support feImage pointing to
+    // a fragment of the SVG yet.
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1538554
+    inputImageSvg = Uri.encodeComponent(inputImageSvg);
+    String imageUrl = 'data:image/svg+xml;charset=utf-8,$inputImageSvg';
+
+    ui.BlendMode blendModeTemp = blendMode;
+    switch (blendModeTemp) {
+      case ui.BlendMode.clear:
+      case ui.BlendMode.dstOut:
+      case ui.BlendMode.srcOut:
+        childContainer?.style.visibility = 'hidden';
+        return;
+      case ui.BlendMode.dst:
+      case ui.BlendMode.dstIn:
+        // Noop.
+        return;
+      case ui.BlendMode.src:
+      case ui.BlendMode.srcOver:
+        // Uses source filter color.
+        // Since we don't have a size, we can't use background color.
+        // Use svg filter srcIn instead.
+        blendModeTemp = ui.BlendMode.srcIn;
+        break;
+      case ui.BlendMode.dstOver:
+      case ui.BlendMode.srcIn:
+      case ui.BlendMode.srcATop:
+      case ui.BlendMode.dstATop:
+      case ui.BlendMode.xor:
+      case ui.BlendMode.plus:
+      case ui.BlendMode.modulate:
+      case ui.BlendMode.screen:
+      case ui.BlendMode.overlay:
+      case ui.BlendMode.darken:
+      case ui.BlendMode.lighten:
+      case ui.BlendMode.colorDodge:
+      case ui.BlendMode.colorBurn:
+      case ui.BlendMode.hardLight:
+      case ui.BlendMode.softLight:
+      case ui.BlendMode.difference:
+      case ui.BlendMode.exclusion:
+      case ui.BlendMode.multiply:
+      case ui.BlendMode.hue:
+      case ui.BlendMode.saturation:
+      case ui.BlendMode.color:
+      case ui.BlendMode.luminosity:
+        break;
+    }
+
+    String code = svgMaskFilterFromImageAndBlendMode(imageUrl, blendModeTemp)!;
+
+    _shaderElement =
+        html.Element.html(code, treeSanitizer: _NullTreeSanitizer());
+    rootElement!.append(_shaderElement!);
+    rootElement!.style.filter = 'url(#_fmf${_maskFilterIdCounter}';
   }
 
   @override
@@ -184,4 +198,181 @@ class PersistedShaderMask extends PersistedContainerSurface
       apply();
     }
   }
+}
+
+String? svgMaskFilterFromImageAndBlendMode(
+    String imageUrl, ui.BlendMode blendMode) {
+  String? svgFilter;
+  switch (blendMode) {
+    case ui.BlendMode.srcIn:
+    case ui.BlendMode.srcATop:
+      svgFilter = _srcInImageToSvg(imageUrl);
+      break;
+    case ui.BlendMode.srcOut:
+      svgFilter = _srcOutImageToSvg(imageUrl);
+      break;
+    case ui.BlendMode.xor:
+      svgFilter = _xorImageToSvg(imageUrl);
+      break;
+    case ui.BlendMode.plus:
+      // Porter duff source + destination.
+      svgFilter = _compositeImageToSvg(imageUrl, 0, 1, 1, 0);
+      break;
+    case ui.BlendMode.modulate:
+      // Porter duff source * destination but preserves alpha.
+      svgFilter = _modulateImageToSvg(imageUrl);
+      break;
+    case ui.BlendMode.overlay:
+      // Since overlay is the same as hard-light by swapping layers,
+      // pass hard-light blend function.
+      svgFilter = _blendImageToSvg(imageUrl, 'hard-light', swapLayers: true);
+      break;
+    // Several of the filters below (although supported) do not render the
+    // same (close but not exact) as native flutter when used as blend mode
+    // for a background-image with a background color. They only look
+    // identical when feBlend is used within an svg filter definition.
+    //
+    // Saturation filter uses destination when source is transparent.
+    // cMax = math.max(r, math.max(b, g));
+    // cMin = math.min(r, math.min(b, g));
+    // delta = cMax - cMin;
+    // lightness = (cMax + cMin) / 2.0;
+    // saturation = delta / (1.0 - (2 * lightness - 1.0).abs());
+    case ui.BlendMode.saturation:
+    case ui.BlendMode.colorDodge:
+    case ui.BlendMode.colorBurn:
+    case ui.BlendMode.hue:
+    case ui.BlendMode.color:
+    case ui.BlendMode.luminosity:
+    case ui.BlendMode.multiply:
+    case ui.BlendMode.screen:
+    case ui.BlendMode.overlay:
+    case ui.BlendMode.darken:
+    case ui.BlendMode.lighten:
+    case ui.BlendMode.colorDodge:
+    case ui.BlendMode.colorBurn:
+    case ui.BlendMode.hardLight:
+    case ui.BlendMode.softLight:
+    case ui.BlendMode.difference:
+    case ui.BlendMode.exclusion:
+      svgFilter = _blendImageToSvg(imageUrl, _stringForBlendMode(blendMode));
+      break;
+    case ui.BlendMode.src:
+    case ui.BlendMode.dst:
+    case ui.BlendMode.dstATop:
+    case ui.BlendMode.dstIn:
+    case ui.BlendMode.dstOut:
+    case ui.BlendMode.dstOver:
+    case ui.BlendMode.clear:
+    case ui.BlendMode.srcOver:
+      assert(
+          false,
+          'Invalid svg filter request for blend-mode '
+          '$blendMode');
+      break;
+  }
+  return svgFilter;
+}
+
+int _maskFilterIdCounter = 0;
+
+// The color matrix for feColorMatrix element changes colors based on
+// the following:
+//
+// | R' |     | r1 r2 r3 r4 r5 |   | R |
+// | G' |     | g1 g2 g3 g4 g5 |   | G |
+// | B' |  =  | b1 b2 b3 b4 b5 | * | B |
+// | A' |     | a1 a2 a3 a4 a5 |   | A |
+// | 1  |     | 0  0  0  0  1  |   | 1 |
+//
+// R' = r1*R + r2*G + r3*B + r4*A + r5
+// G' = g1*R + g2*G + g3*B + g4*A + g5
+// B' = b1*R + b2*G + b3*B + b4*A + b5
+// A' = a1*R + a2*G + a3*B + a4*A + a5
+String _srcInImageToSvg(String imageUrl) {
+  _maskFilterIdCounter += 1;
+  return '<svg width="0" height="0" xmlns:xlink="http://www.w3.org/1999/xlink">'
+      '<filter id="_fmf$_maskFilterIdCounter" '
+      'filterUnits="objectBoundingBox" x="0%" y="0%" width="100%" height="100%">'
+      '<feColorMatrix values="0 0 0 0 1 ' // Ignore input, set it to absolute.
+      '0 0 0 0 1 '
+      '0 0 0 0 1 '
+      '0 0 0 1 0" result="destalpha"/>' // Just take alpha channel of destination
+      '<feImage xlink:href="$imageUrl" result="image">'
+      '</feImage>'
+      '<feComposite in="image" in2="destalpha" '
+      'operator="arithmetic" k1="1" k2="0" k3="0" k4="0" result="comp">'
+      '</feComposite>'
+      '</filter></svg>';
+}
+
+String _srcOutImageToSvg(String imageUrl) {
+  _maskFilterIdCounter += 1;
+  return '<svg width="0" height="0" xmlns:xlink="http://www.w3.org/1999/xlink">'
+      '<filter id="_fmf$_maskFilterIdCounter" '
+      'filterUnits="objectBoundingBox" x="0%" y="0%" width="100%" height="100%">'
+      '<feImage xlink:href="$imageUrl" result="image">'
+      '</feImage>'
+      '<feComposite in="image" in2="SourceGraphic" operator="out" result="comp">'
+      '</feComposite>'
+      '</filter></svg>';
+}
+
+String _xorImageToSvg(String imageUrl) {
+  _maskFilterIdCounter += 1;
+  return '<svg width="0" height="0" xmlns:xlink="http://www.w3.org/1999/xlink">'
+      '<filter id="_fmf$_maskFilterIdCounter" '
+      'filterUnits="objectBoundingBox" x="0%" y="0%" width="100%" height="100%">'
+      '<feImage xlink:href="$imageUrl" result="image">'
+      '</feImage>'
+      '<feComposite in="image" in2="SourceGraphic" operator="xor" result="comp">'
+      '</feComposite>'
+      '</filter></svg>';
+}
+
+// The source image and color are composited using :
+// result = k1 *in*in2 + k2*in + k3*in2 + k4.
+String _compositeImageToSvg(
+    String imageUrl, double k1, double k2, double k3, double k4) {
+  _maskFilterIdCounter += 1;
+  return '<svg width="0" height="0" xmlns:xlink="http://www.w3.org/1999/xlink">'
+      '<filter id="_fmf$_maskFilterIdCounter" '
+      'filterUnits="objectBoundingBox" x="0%" y="0%" width="100%" height="100%">'
+      '<feImage xlink:href="$imageUrl" result="image">'
+      '</feImage>'
+      '<feComposite in="image" in2="SourceGraphic" '
+      'operator="arithmetic" k1="$k1" k2="$k2" k3="$k3" k4="$k4" result="comp">'
+      '</feComposite>'
+      '</filter></svg>';
+}
+
+// Porter duff source * destination , keep source alpha.
+// First apply color filter to source to change it to [color], then
+// composite using multiplication.
+String _modulateImageToSvg(String imageUrl) {
+  _maskFilterIdCounter += 1;
+  return '<svg width="0" height="0" xmlns:xlink="http://www.w3.org/1999/xlink">'
+      '<filter id="_fmf$_maskFilterIdCounter" '
+      'filterUnits="objectBoundingBox" x="0%" y="0%" width="100%" height="100%">'
+      '<feImage xlink:href="$imageUrl" result="image">'
+      '</feImage>'
+      '<feComposite in="image" in2="SourceGraphic" '
+      'operator="arithmetic" k1="1" k2="0" k3="0" k4="0" result="comp">'
+      '</feComposite>'
+      '</filter></svg>';
+}
+
+// Uses feBlend element to blend source image with a color.
+String _blendImageToSvg(String imageUrl, String? feBlend,
+    {bool swapLayers = false}) {
+  _maskFilterIdCounter += 1;
+  return '<svg width="0" height="0" xmlns:xlink="http://www.w3.org/1999/xlink">'
+          '<filter id="_fmf$_maskFilterIdCounter" filterUnits="objectBoundingBox" '
+          'x="0%" y="0%" width="100%" height="100%">'
+          '<feImage xlink:href="$imageUrl" result="image">'
+          '</feImage>' +
+      (swapLayers
+          ? '<feBlend in="SourceGraphic" in2="image" mode="$feBlend"/>'
+          : '<feBlend in="image" in2="SourceGraphic" mode="$feBlend"/>') +
+      '</filter></svg>';
 }
