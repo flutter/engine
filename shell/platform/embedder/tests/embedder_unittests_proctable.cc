7271a9c237d976cd5ace3b610526770501fa5d3b
Stuart Morgan <stuartmorgan@google.com>
2020-10-13 11:19:17 -0700

    Add a new test binary

---
+++ shell/platform/embedder/tests/embedder_unittests_proctable.cc
@@ -0,0 +1,60 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/embedder/embedder.h"
+
+#include "flutter/testing/testing.h"
+
+namespace flutter {
+namespace testing {
+
+// Verifies that the proc table is fully populated.
+TEST(EmbedderProcTable, AllPointersProvided) {
+  FlutterEngineProcTable procs = {};
+  procs.struct_size = sizeof(FlutterEngineProcTable);
+  FlutterEngineGetProcAddresses(&procs);
+
+  void (**proc)() = reinterpret_cast<void (**)()>(&procs.create_aot_data);
+  const unsigned long long end_address =
+      reinterpret_cast<unsigned long long>(&procs) + procs.struct_size;
+  while (reinterpret_cast<unsigned long long>(proc) < end_address) {
+    EXPECT_NE(*proc, nullptr);
+    ++proc;
+  }
+}
+
+// Ensures that there are no duplicate pointers in the proc table, to catch
+// copy/paste mistakes when adding a new entry to FlutterEngineGetProcAddresses.
+TEST(EmbedderProcTable, NoDuplicatePointers) {
+  FlutterEngineProcTable procs = {};
+  procs.struct_size = sizeof(FlutterEngineProcTable);
+  FlutterEngineGetProcAddresses(&procs);
+
+  void (**proc)() = reinterpret_cast<void (**)()>(&procs.create_aot_data);
+  const unsigned long long end_address =
+      reinterpret_cast<unsigned long long>(&procs) + procs.struct_size;
+  while (reinterpret_cast<unsigned long long>(proc) < end_address) {
+    void (**other_proc)() =
+        reinterpret_cast<void (**)()>(&procs.create_aot_data);
+    while (reinterpret_cast<unsigned long long>(other_proc) < end_address) {
+      if (other_proc != proc) {
+        EXPECT_NE(*proc, *other_proc);
+      }
+      ++other_proc;
+    }
+    ++proc;
+  }
+}
+
+// Spot-checks that calling one of the function pointers works.
+TEST(EmbedderProcTable, CallProc) {
+  FlutterEngineProcTable procs = {};
+  procs.struct_size = sizeof(FlutterEngineProcTable);
+  FlutterEngineGetProcAddresses(&procs);
+
+  EXPECT_NE(procs.get_current_time(), 0ULL);
+}
+
+}  // namespace testing
+}  // namespace flutter
