9cac2dbea405abe752c27dfb1139614b98f5f81d
ferhatb <ferhat@gmail.com>
2020-10-19 09:49:08 -0700

    merge

--- lib/web_ui/test/golden_tests/engine/canvas_golden_test.dart
+++ lib/web_ui/test/golden_tests/engine/canvas_golden_test.dart
@@ -93,7 +93,7 @@ void testMain() async {
     // compensate by shifting the contents of the canvas in the opposite
     // direction.
     canvas = BitmapCanvas(const Rect.fromLTWH(0.5, 0.5, 60, 60));
-    canvas.clipRect(const Rect.fromLTWH(0, 0, 50, 50));
+    canvas.clipRect(const Rect.fromLTWH(0, 0, 50, 50), ClipOp.intersect);
     drawMisalignedLines(canvas);
 
     appendToScene();
@@ -103,7 +103,7 @@ void testMain() async {
 
   test('fill the whole canvas with color even when transformed', () async {
     canvas = BitmapCanvas(const Rect.fromLTWH(0, 0, 50, 50));
-    canvas.clipRect(const Rect.fromLTWH(0, 0, 50, 50));
+    canvas.clipRect(const Rect.fromLTWH(0, 0, 50, 50), ClipOp.intersect);
     canvas.translate(25, 25);
     canvas.drawColor(const Color.fromRGBO(0, 255, 0, 1.0), BlendMode.src);
 
@@ -116,7 +116,7 @@ void testMain() async {
 
   test('fill the whole canvas with paint even when transformed', () async {
     canvas = BitmapCanvas(const Rect.fromLTWH(0, 0, 50, 50));
-    canvas.clipRect(const Rect.fromLTWH(0, 0, 50, 50));
+    canvas.clipRect(const Rect.fromLTWH(0, 0, 50, 50), ClipOp.intersect);
     canvas.translate(25, 25);
     canvas.drawPaint(SurfacePaintData()
       ..color = const Color.fromRGBO(0, 255, 0, 1.0)
