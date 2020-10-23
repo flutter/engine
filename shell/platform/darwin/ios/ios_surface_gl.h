0739cf7c7ef4c1dae20d5aee11e35b40225e5f5d
Kaushik Iska <iska.kaushik@gmail.com>
2020-10-23 11:33:02 -0700

    Convert FlutterPlatformViewsController to smart pointer

--- shell/platform/darwin/ios/ios_surface_gl.h
+++ shell/platform/darwin/ios/ios_surface_gl.h
@@ -20,7 +20,7 @@ class IOSSurfaceGL final : public IOSSurface, public GPUSurfaceGLDelegate {
  public:
   IOSSurfaceGL(fml::scoped_nsobject<CAEAGLLayer> layer,
                std::shared_ptr<IOSContext> context,
-               FlutterPlatformViewsController* platform_views_controller = nullptr);
+               std::shared_ptr<FlutterPlatformViewsController> platform_views_controller = nullptr);
 
   ~IOSSurfaceGL() override;
 
--- shell/platform/darwin/ios/ios_surface_software.h
+++ shell/platform/darwin/ios/ios_surface_software.h
@@ -20,7 +20,7 @@ class IOSSurfaceSoftware final : public IOSSurface, public GPUSurfaceSoftwareDel
  public:
   IOSSurfaceSoftware(fml::scoped_nsobject<CALayer> layer,
                      std::shared_ptr<IOSContext> context,
-                     FlutterPlatformViewsController* platform_views_controller);
+                     std::shared_ptr<FlutterPlatformViewsController> platform_views_controller);
 
   ~IOSSurfaceSoftware() override;
 
