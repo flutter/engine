1b5f05b85fd660421c4e1a32c52722e6870fe9ba
Christopher Fujino <christopherfujino@gmail.com>
2020-10-28 13:16:44 -0700

    import assertNull

--- shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
+++ shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
@@ -2,6 +2,7 @@ package io.flutter.plugin.platform;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
