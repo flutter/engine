eb5b234609667e6e8e2ef25d7b486f2124064a7c
ferhatb <ferhat@gmail.com>
2020-10-28 18:14:57 -0700

    Assign default natural width/height for svgs that report 0,0 on firefox and ie11

--- lib/web_ui/lib/src/engine/html_image_codec.dart
+++ lib/web_ui/lib/src/engine/html_image_codec.dart
@@ -39,10 +39,19 @@ class HtmlCodec implements ui.Codec {
       js_util.setProperty(imgElement, 'decoding', 'async');
       imgElement.decode().then((dynamic _) {
         chunkCallback?.call(100, 100);
+        int naturalWidth = imgElement.naturalWidth;
+        int naturalHeight = imgElement.naturalHeight;
+        // Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=700533.
+        if (naturalWidth == 0 && naturalHeight == 0 && (
+            browserEngine == BrowserEngine.firefox ||
+                browserEngine == BrowserEngine.ie11)) {
+          naturalWidth = 300;
+          naturalHeight = 300;
+        }
         final HtmlImage image = HtmlImage(
           imgElement,
-          imgElement.naturalWidth,
-          imgElement.naturalHeight,
+          naturalWidth,
+          naturalHeight,
         );
         completer.complete(SingleFrameInfo(image));
       }).catchError((dynamic e) {
