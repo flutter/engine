60b373315e625ff3ad9dde7631b5ca135f720f01
Aaron Clarke <aaclarke@google.com>
2020-11-09 09:54:02 -0800

    mocked out getting the infoplist

--- shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterAppDelegate.mm
@@ -131,9 +131,8 @@ static NSString* kBackgroundFetchCapatibility = @"fetch";
   }
 }
 
-static BOOL IsDeepLinkingEnabled() {
-  NSDictionary* infoDict = [[NSBundle mainBundle] infoDictionary];
-  NSNumber* isEnabled = [infoDict objectForKey:@"FlutterDeepLinkingEnabled"];
+static BOOL IsDeepLinkingEnabled(NSDictionary* infoDictionary) {
+  NSNumber* isEnabled = [infoDictionary objectForKey:@"FlutterDeepLinkingEnabled"];
   if (isEnabled) {
     return [isEnabled boolValue];
   } else {
@@ -143,10 +142,11 @@ static BOOL IsDeepLinkingEnabled() {
 
 - (BOOL)application:(UIApplication*)application
             openURL:(NSURL*)url
-            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id>*)options {
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id>*)options
+    infoPlistGetter:(NSDictionary* (^)())infoPlistGetter {
   if ([_lifeCycleDelegate application:application openURL:url options:options]) {
     return YES;
-  } else if (!IsDeepLinkingEnabled()) {
+  } else if (!IsDeepLinkingEnabled(infoPlistGetter())) {
     return NO;
   } else {
     FlutterViewController* flutterViewController = [self rootFlutterViewController];
@@ -168,6 +168,17 @@ static BOOL IsDeepLinkingEnabled() {
       return NO;
     }
   }
+}
+
+- (BOOL)application:(UIApplication*)application
+            openURL:(NSURL*)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id>*)options {
+  return [self application:application
+                   openURL:url
+                   options:options
+           infoPlistGetter:^NSDictionary*() {
+             return [[NSBundle mainBundle] infoDictionary];
+           }];
 }
 
 - (BOOL)application:(UIApplication*)application handleOpenURL:(NSURL*)url {
