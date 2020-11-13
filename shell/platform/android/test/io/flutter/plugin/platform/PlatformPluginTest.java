b87f4055edcf7a7d1bc811bf4e19af81a0e35789
Hamdi Kahloun <hamdikahlounacil@gmail.com>
2020-11-13 22:40:39 +0100

    PlatformPluginTest Format.java

--- shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
+++ shell/platform/android/test/io/flutter/plugin/platform/PlatformPluginTest.java
@@ -6,9 +6,9 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
