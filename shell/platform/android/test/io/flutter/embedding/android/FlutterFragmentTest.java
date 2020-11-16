39df2e0025d21f38c7fdc3c4f2b80b59f7c76f3d
math1man <aweiland@google.com>
2020-11-16 15:28:49 -0800

    Add Mockito.when import.

--- shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
+++ shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.content.Context;
 import androidx.activity.OnBackPressedCallback;
