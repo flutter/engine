2e5a05ce1e969b39080b992424ff3612b26fdb52
Tong Mu <dkwingsmt@gmail.com>
2020-11-17 19:10:13 -0800

    Android: Basic logic

--- shell/platform/android/io/flutter/embedding/android/HardwareKeyboard.java
+++ shell/platform/android/io/flutter/embedding/android/HardwareKeyboard.java
@@ -32,30 +32,81 @@ public class HardwareKeyboard {
     return scanCode;
   }
 
+  private long getLastLogicalRecord(long physicalKey) {
+    final Long objValue = mPressingRecords.get(Long.valueOf(physicalKey));
+    if (objValue == null) {
+      return 0;
+    }
+    return objValue.longValue();
+  }
+
   public List<KeyDatum> convertEvent(KeyEvent event) {
     final long logicalKey = logicalKeyFromEvent(event);
-    final long physicalKey = logicalKeyFromEvent(event);
+    final long physicalKey = physicalKeyFromEvent(event);
     final boolean isPhysicalDown = event.getAction() == KeyEvent.ACTION_DOWN;
+    final long timeStamp = event.getEventTime() * 1000; // Convert from milliseconds to microseconds.
 
-    final LogicalKeyDatum logicalDatum = new LogicalKeyDatum(
-      isPhysicalDown ? EventKind.down : EventKind.up,
+    final long lastLogicalRecord = getLastLogicalRecord(physicalKey);
+
+    EventKind change;
+
+    if (isPhysicalDown) {
+      if (lastLogicalRecord != 0) {
+        // This physical key is being pressed according to the record.
+        if (event.getRepeatCount() == 0) {
+          // A non-repeated key has been pressed that has the exact physical key as
+          // a currently pressed one, usually indicating multiple keyboards are
+          // pressing keys with the same physical key, or the up event was lost
+          // during a loss of focus. The down event is ignored.
+          return new ArrayList<KeyDatum>();
+        } else {
+          // A normal repeated key.
+          change = EventKind.repeat;
+        }
+      } else {
+        // This physical key is not being pressed according to the record. It's a
+        // normal down event, whether the system event is a repeat or not.
+        change = EventKind.down;
+      }
+    } else { // isPhysicalDown false
+      if (lastLogicalRecord == 0) {
+        // The physical key has been released before. It indicates multiple
+        // keyboards pressed keys with the same physical key. Ignore the up event.
+        return new ArrayList<KeyDatum>();
+      }
+
+      change = EventKind.up;
+    }
+
+    Long nextLogicalRecord = null;
+    switch (change) {
+      case down:
+        nextLogicalRecord = logicalKey;
+        break;
+      case up:
+        nextLogicalRecord = null;
+        break;
+      case repeat:
+        nextLogicalRecord = lastLogicalRecord;
+        break;
+    }
+    if (nextLogicalRecord == null) {
+      mPressingRecords.remove(physicalKey);
+    } else {
+      mPressingRecords.put(physicalKey, nextLogicalRecord);
+    }
+
+    final KeyDatum keyDatum = new KeyDatum(
+      change,
+      timeStamp,
+      physicalKey,
       logicalKey,
-      null,
+      event.getCharacters(),
       false);
 
-    final List<LogicalKeyDatum> logicalData = new ArrayList<LogicalKeyDatum>();
-    logicalData.add(logicalDatum);
-
-    final KeyDatum physicalDatum = new KeyDatum(
-      isPhysicalDown ? EventKind.down : EventKind.up,
-      logicalKey,
-      false,
-      0,
-      logicalData);
-
-    final List<KeyDatum> physicalData = new ArrayList<KeyDatum>();
-    physicalData.add(physicalDatum);
-    return physicalData;
+    final List<KeyDatum> keyData = new ArrayList<KeyDatum>();
+    keyData.add(keyDatum);
+    return keyData;
   }
 
   public ByteBuffer packDatum(KeyDatum keyDatum) {
@@ -68,46 +119,31 @@ public class HardwareKeyboard {
   public enum EventKind {
     down,
     up,
-    sync,
-    cancel,
-  }
-
-  public static class LogicalKeyDatum {
-    @NonNull public final EventKind kind;
-    @NonNull public final long key;
-    @Nullable public final String character;
-    @NonNull public final boolean repeated;
-
-    public LogicalKeyDatum(
-        @NonNull EventKind kind,
-        @NonNull long key,
-        @Nullable String character,
-        @NonNull boolean repeated) {
-      this.kind = kind;
-      this.key = key;
-      this.character = character;
-      this.repeated = repeated;
-    }
+    repeat,
   }
 
   public static class KeyDatum {
     @NonNull public final EventKind kind;
-    @NonNull public final long key;
-    @NonNull public final boolean repeated;
-    @NonNull public final long activeLocks;
-    @NonNull public final List<LogicalKeyDatum> logicalData;
+    // Time in microseconds from an arbitrary and consistent start.
+    @NonNull public final long timeStamp;
+    @NonNull public final long physical;
+    @NonNull public final long logical;
+    @Nullable public final String character;
+    @NonNull public final boolean synthesized;
 
     public KeyDatum(
         @NonNull EventKind kind,
-        @NonNull long key,
-        @NonNull boolean repeated,
-        @NonNull long activeLocks,
-        @Nullable List<LogicalKeyDatum> logicalData) {
+        @NonNull long timeStamp,
+        @NonNull long physical,
+        @NonNull long logical,
+        @Nullable String character,
+        @NonNull boolean synthesized) {
       this.kind = kind;
-      this.key = key;
-      this.repeated = repeated;
-      this.activeLocks = activeLocks;
-      this.logicalData = logicalData;
+      this.timeStamp = timeStamp;
+      this.physical = physical;
+      this.logical = logical;
+      this.character = character;
+      this.synthesized = synthesized;
     }
   }
 }
