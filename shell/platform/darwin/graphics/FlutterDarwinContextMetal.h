1959c9277ffa61b205f59e34f6f4eed413fff8f3
Kaushik Iska <iska.kaushik@gmail.com>
2020-11-19 13:08:01 -0600

    Introduce a delegate class for gpu metal rendering
    This supports rendering to CAMetalLayer and also MTLTexture.

---
+++ shell/platform/darwin/graphics/FlutterDarwinContextMetal.h
@@ -0,0 +1,33 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHELL_PLATFORM_DARWIN_GRAPHICS_DARWIN_CONTEXT_METAL_H_
+#define SHELL_PLATFORM_DARWIN_GRAPHICS_DARWIN_CONTEXT_METAL_H_
+
+#import <Foundation/Foundation.h>
+#import <Metal/Metal.h>
+
+#include "third_party/skia/include/gpu/GrDirectContext.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FlutterDarwinContextMetal : NSObject
+
+- (instancetype)initWithDefaultMTLDevice;
+
+- (instancetype)initWithMTLDevice:(id<MTLDevice>)mtlDevice;
+
+- (instancetype)initWithMTLDevice:(id<MTLDevice>)mtlDevice
+                     commandQueue:(id<MTLCommandQueue>)commandQueue;
+
+@property(nonatomic, readonly) id<MTLDevice> mtlDevice;
+@property(nonatomic, readonly) id<MTLCommandQueue> mtlCommandQueue;
+@property(nonatomic, readonly) sk_sp<GrDirectContext> mainContext;
+@property(nonatomic, readonly) sk_sp<GrDirectContext> resourceContext;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#endif  // SHELL_PLATFORM_DARWIN_GRAPHICS_DARWIN_CONTEXT_METAL_H_
---
+++ shell/platform/darwin/graphics/FlutterDarwinContextMetal.mm
@@ -0,0 +1,75 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "flutter/shell/platform/darwin/graphics/FlutterDarwinContextMetal.h"
+
+#include "flutter/common/graphics/persistent_cache.h"
+#include "flutter/fml/logging.h"
+#include "third_party/skia/include/gpu/GrContextOptions.h"
+
+namespace {
+
+static GrContextOptions CreateMetalGrContextOptions() {
+  GrContextOptions options = {};
+  if (flutter::PersistentCache::cache_sksl()) {
+    options.fShaderCacheStrategy = GrContextOptions::ShaderCacheStrategy::kSkSL;
+  }
+  flutter::PersistentCache::MarkStrategySet();
+  options.fPersistentCache = flutter::PersistentCache::GetCacheForProcess();
+  return options;
+}
+
+}  // namespace
+
+@implementation FlutterDarwinContextMetal {
+}
+
+- (instancetype)initWithDefaultMTLDevice {
+  return [self initWithMTLDevice:MTLCreateSystemDefaultDevice()];
+}
+
+- (instancetype)initWithMTLDevice:(id<MTLDevice>)mtlDevice {
+  return [self initWithMTLDevice:mtlDevice commandQueue:[mtlDevice newCommandQueue]];
+}
+
+- (instancetype)initWithMTLDevice:(id<MTLDevice>)mtlDevice
+                     commandQueue:(id<MTLCommandQueue>)commandQueue {
+  self = [super init];
+  if (self != nil) {
+    _mtlDevice = mtlDevice;
+
+    if (!_mtlDevice) {
+      FML_DLOG(ERROR) << "Could not acquire Metal device.";
+      return nil;
+    }
+
+    _mtlCommandQueue = commandQueue;
+
+    if (!_mtlCommandQueue) {
+      FML_DLOG(ERROR) << "Could not create Metal command queue.";
+      return nil;
+    }
+
+    [_mtlCommandQueue setLabel:@"Flutter Main Queue"];
+
+    auto contextOptions = CreateMetalGrContextOptions();
+
+    // Skia expect arguments to `MakeMetal` transfer ownership of the reference in for release later
+    // when the GrDirectContext is collected.
+    _mainContext =
+        GrDirectContext::MakeMetal([_mtlDevice retain], [_mtlCommandQueue retain], contextOptions);
+    _resourceContext =
+        GrDirectContext::MakeMetal([_mtlDevice retain], [_mtlCommandQueue retain], contextOptions);
+
+    if (!_mainContext || !_resourceContext) {
+      FML_DLOG(ERROR) << "Could not create Skia Metal contexts.";
+      return nil;
+    }
+
+    _resourceContext->setResourceCacheLimits(0u, 0u);
+  }
+  return self;
+}
+
+@end
