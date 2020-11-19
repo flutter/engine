83fc8a9b4d5207dbfa7fe1d8a84a19628c02a906
Vijay Menon <vsm@google.com>
2020-11-19 05:43:37 -0800

    Remove opt outs for dart:ui

--- lib/ui/channel_buffers.dart
+++ lib/ui/channel_buffers.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @dart = 2.10
 
 // KEEP THIS SYNCHRONIZED WITH ../web_ui/lib/src/ui/channel_buffers.dart
 
