385b1036eff4d792ad3673f1a6c752e026b8de13
richardjcai <caioftherichard@gmail.com>
2020-11-19 16:12:27 -0500

    Add licenses to FlutterViewControllerTestUtils files

--- shell/platform/darwin/macos/framework/Source/FlutterViewControllerTestUtils.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterViewControllerTestUtils.mm
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #import "flutter/shell/platform/darwin/macos/framework/Source/FlutterViewControllerTestUtils.h"
 
 namespace flutter::testing {
