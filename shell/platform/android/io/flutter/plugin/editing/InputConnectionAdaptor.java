cf43e9ee70af04cccfd62695fdb7f9b5e1e8df6c
Greg Spencer <gspencer@google.com>
2020-11-19 18:24:18 -0800

    Remove engine-side delete key handling.

--- shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
+++ shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
@@ -287,7 +287,6 @@ class InputConnectionAdaptor extends BaseInputConnection
 
   @Override
   public boolean sendKeyEvent(KeyEvent event) {
-    Log.w(TAG, "Received " + (event.getAction() == KeyEvent.ACTION_DOWN ? "down" : "up") + " event for " + Character.getName(event.getUnicodeChar()));
     // Give the key processor a chance to process this event.  It will send it
     // to the framework to be handled and return true. If the framework ends up
     // not handling it, the processor will re-send the event, this time
@@ -297,21 +296,6 @@ class InputConnectionAdaptor extends BaseInputConnection
     }
 
     if (event.getAction() == KeyEvent.ACTION_DOWN) {
-      // if (event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
-      //   int selStart = clampIndexToEditable(Selection.getSelectionStart(mEditable), mEditable);
-      //   int selEnd = clampIndexToEditable(Selection.getSelectionEnd(mEditable), mEditable);
-      //   if (selStart == selEnd && selStart > 0) {
-      //     // Extend selection to left of the last character
-      //     selStart = flutterTextUtils.getOffsetBefore(mEditable, selStart);
-      //   }
-      //   if (selEnd > selStart) {
-      //     // Delete the selection.
-      //     Selection.setSelection(mEditable, selStart);
-      //     mEditable.delete(selStart, selEnd);
-      //     return true;
-      //   }
-      //   return false;
-      // } else
       if (event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
         int selStart = Selection.getSelectionStart(mEditable);
         int selEnd = Selection.getSelectionEnd(mEditable);
