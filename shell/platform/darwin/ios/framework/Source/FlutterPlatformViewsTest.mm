84d540e80297cfb534b389741bb11aab81fa87a7
Chris Yang <ychris@google.com>
2020-12-08 16:03:24 -0800

    cherry-pick fix build error

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
@@ -631,7 +631,6 @@ fml::RefPtr<fml::TaskRunner> CreateNewThread(std::string name) {
   auto flutterPlatformViewsController = std::make_shared<flutter::FlutterPlatformViewsController>();
   auto platform_view = std::make_unique<flutter::PlatformViewIOS>(
       /*delegate=*/mock_delegate,
-      /*rendering_api=*/flutter::IOSRenderingAPI::kSoftware,
       /*platform_views_controller=*/flutterPlatformViewsController,
       /*task_runners=*/runners);
 
