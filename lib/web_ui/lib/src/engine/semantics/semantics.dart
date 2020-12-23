134bfa1d57864558cdabcdc3fb76f4e289388891
nturgut <nurhan@google.com>
2020-12-22 17:09:27 -0800

    remove focus related changes

--- lib/web_ui/lib/src/engine/semantics/semantics.dart
+++ lib/web_ui/lib/src/engine/semantics/semantics.dart
@@ -583,8 +583,6 @@ class SemanticsObject {
       hasAction(ui.SemanticsAction.scrollUp);
 
   bool get hasFocus => hasFlag(ui.SemanticsFlag.isFocused);
-  // bool get looseFocus => hasAction(ui.SemanticsAction.didLoseAccessibilityFocus);
-  // bool get gainFocus => hasAction(ui.SemanticsAction.didGainAccessibilityFocus);
 
   /// Whether this object represents a hotizontally scrollable area.
   bool get isHorizontalScrollContainer =>
@@ -736,9 +734,6 @@ class SemanticsObject {
     // Apply updates to the DOM.
     _updateRoles();
     _updateChildrenInTraversalOrder();
-
-
-    // print('has focus: ${hasFocus}');
 
     // All properties that affect positioning and sizing are checked together
     // any one of them triggers position and size recomputation.
