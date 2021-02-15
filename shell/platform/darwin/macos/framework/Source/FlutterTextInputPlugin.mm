051128155447b5cd3ddc2344a96c3d8db931c645
Chris Bracken <chris@bracken.jp>
2021-02-15 11:42:04 -0800

    Use common desktop TextInputModel on macOS
    This replaces the custom Obj-C TextInputModel implementation used on
    macOS with the common C++ implementation used on Linux and Windows. Note
    that as a side-effect, this change enables *some* direct IME input for
    CJK input but full input will land in follow-up patches that land:
    
    1. Add handling for TextInput.setMarkedTextRect message
    2. Add handling for TextInput.setEditableSizeAndTransform message
    3. Implement firstRectForCharacterRange:actualRange: using the above.
    4. Add NSTextInputContext handling.

--- shell/platform/darwin/macos/framework/Source/FlutterTextInputPlugin.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterTextInputPlugin.mm
@@ -6,8 +6,11 @@
 
 #import <objc/message.h>
 
+#include <algorithm>
+#include <memory>
+
+#include "flutter/shell/platform/common/text_input_model.h"
 #import "flutter/shell/platform/darwin/common/framework/Headers/FlutterCodecs.h"
-#import "flutter/shell/platform/darwin/macos/framework/Source/FlutterTextInputModel.h"
 #import "flutter/shell/platform/darwin/macos/framework/Source/FlutterViewController_Internal.h"
 
 static NSString* const kTextInputChannel = @"flutter/textinput";
@@ -22,6 +25,50 @@ static NSString* const kUpdateEditStateResponseMethod = @"TextInputClient.update
 static NSString* const kPerformAction = @"TextInputClient.performAction";
 static NSString* const kMultilineInputType = @"TextInputType.multiline";
 
+static NSString* const kTextAffinityDownstream = @"TextAffinity.downstream";
+static NSString* const kTextAffinityUpstream = @"TextAffinity.upstream";
+
+static NSString* const kTextInputAction = @"inputAction";
+static NSString* const kTextInputType = @"inputType";
+static NSString* const kTextInputTypeName = @"name";
+
+static NSString* const kSelectionBaseKey = @"selectionBase";
+static NSString* const kSelectionExtentKey = @"selectionExtent";
+static NSString* const kSelectionAffinityKey = @"selectionAffinity";
+static NSString* const kSelectionIsDirectionalKey = @"selectionIsDirectional";
+static NSString* const kComposingBaseKey = @"composingBase";
+static NSString* const kComposingExtentKey = @"composingExtent";
+static NSString* const kTextKey = @"text";
+
+/**
+ * The affinity of the current cursor position. If the cursor is at a position representing
+ * a line break, the cursor may be drawn either at the end of the current line (upstream)
+ * or at the beginning of the next (downstream).
+ */
+typedef NS_ENUM(NSUInteger, FlutterTextAffinity) {
+  FlutterTextAffinityUpstream,
+  FlutterTextAffinityDownstream
+};
+
+/*
+ * Updates a range given base and extent fields.
+ */
+static flutter::TextRange UpdateRangeFromBaseExtent(NSNumber* base,
+                                                    NSNumber* extent,
+                                                    flutter::TextRange range) {
+  if (base == nil || extent == nil) {
+    return range;
+  }
+  if (base.intValue == -1 && extent.intValue == -1) {
+    range.set_base(0);
+    range.set_extent(0);
+  } else {
+    range.set_base([base unsignedLongValue]);
+    range.set_extent([extent unsignedLongValue]);
+  }
+  return range;
+}
+
 /**
  * Private properties of FlutterTextInputPlugin.
  */
@@ -31,11 +78,6 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
  * A text input context, representing a connection to the Cocoa text input system.
  */
 @property(nonatomic) NSTextInputContext* textInputContext;
-
-/**
- * The currently active text input model.
- */
-@property(nonatomic, nullable) FlutterTextInputModel* activeModel;
 
 /**
  * The channel used to communicate with Flutter.
@@ -48,13 +90,40 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
 @property(nonatomic, weak) FlutterViewController* flutterViewController;
 
 /**
+ * The affinity for the current cursor position.
+ */
+@property FlutterTextAffinity textAffinity;
+
+/**
+ * ID of the text input client.
+ */
+@property(nonatomic, readonly, nonnull) NSNumber* clientID;
+
+/**
+ * Keyboard type of the client. See available options:
+ * https://api.flutter.dev/flutter/services/TextInputType-class.html
+ */
+@property(nonatomic, readonly, nonnull) NSString* inputType;
+
+/**
+ * An action requested by the user on the input client. See available options:
+ * https://api.flutter.dev/flutter/services/TextInputAction-class.html
+ */
+@property(nonatomic, readonly, nonnull) NSString* inputAction;
+
+/**
  * Handles a Flutter system message on the text input channel.
  */
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result;
 
 @end
 
-@implementation FlutterTextInputPlugin
+@implementation FlutterTextInputPlugin {
+  /**
+   * The currently active text input model.
+   */
+  std::unique_ptr<flutter::TextInputModel> _active_model;
+}
 
 - (instancetype)initWithViewController:(FlutterViewController*)viewController {
   self = [super init];
@@ -87,14 +156,15 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
     }
     NSNumber* clientID = call.arguments[0];
     if (clientID != nil) {
-      self.activeModel = [[FlutterTextInputModel alloc] initWithClientID:clientID
-                                                           configuration:call.arguments[1]];
-      if (!self.activeModel) {
-        result([FlutterError errorWithCode:@"error"
-                                   message:@"Failed to create an input model"
-                                   details:@"Configuration arguments might be missing"]);
-        return;
-      }
+      NSDictionary* config = call.arguments[1];
+
+      _clientID = clientID;
+      _inputAction = config[kTextInputAction];
+      NSDictionary* inputTypeInfo = config[kTextInputType];
+      _inputType = inputTypeInfo[kTextInputTypeName];
+      self.textAffinity = FlutterTextAffinityUpstream;
+
+      _active_model = std::make_unique<flutter::TextInputModel>();
     }
   } else if ([method isEqualToString:kShowMethod]) {
     [self.flutterViewController addKeyResponder:self];
@@ -103,10 +173,14 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
     [self.flutterViewController removeKeyResponder:self];
     [_textInputContext deactivate];
   } else if ([method isEqualToString:kClearClientMethod]) {
-    self.activeModel = nil;
+    _clientID = nil;
+    _inputAction = nil;
+    _inputType = nil;
+    _active_model = nullptr;
   } else if ([method isEqualToString:kSetEditingStateMethod]) {
     NSDictionary* state = call.arguments;
-    self.activeModel.state = state;
+    [self setEditingState:state];
+
     // Close the loop, since the framework state could have been updated by the
     // engine since it sent this update, and needs to now be made to match the
     // engine's version of the state.
@@ -118,14 +192,55 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
 }
 
 /**
+ * Updates the text input model with state received from the framework via the
+ * TextInput.setEditingState message.
+ */
+- (void)setEditingState:(NSDictionary*)state {
+  NSString* selectionAffinity = state[kSelectionAffinityKey];
+  if (selectionAffinity != nil) {
+    _textAffinity = [selectionAffinity isEqualToString:kTextAffinityUpstream]
+                        ? FlutterTextAffinityUpstream
+                        : FlutterTextAffinityDownstream;
+  }
+
+  NSString* text = state[kTextKey];
+  if (text != nil) {
+    _active_model->SetText([text cStringUsingEncoding:NSUTF8StringEncoding]);
+  }
+
+  flutter::TextRange selected_range = UpdateRangeFromBaseExtent(
+      state[kSelectionBaseKey], state[kSelectionExtentKey], _active_model->selection());
+  _active_model->SetSelection(selected_range);
+
+  flutter::TextRange composing_range = UpdateRangeFromBaseExtent(
+      state[kComposingBaseKey], state[kComposingExtentKey], _active_model->composing_range());
+  size_t cursor_offset = selected_range.base() - composing_range.start();
+  _active_model->SetComposingRange(composing_range, cursor_offset);
+}
+
+/**
  * Informs the Flutter framework of changes to the text input model's state.
  */
 - (void)updateEditState {
-  if (self.activeModel == nil) {
+  if (_active_model == nullptr) {
     return;
   }
-  [_channel invokeMethod:kUpdateEditStateResponseMethod
-               arguments:@[ self.activeModel.clientID, self.activeModel.state ]];
+
+  NSString* const textAffinity = (self.textAffinity == FlutterTextAffinityUpstream)
+                                     ? kTextAffinityUpstream
+                                     : kTextAffinityDownstream;
+  NSDictionary* state = @{
+    kSelectionBaseKey : @(_active_model->selection().base()),
+    kSelectionExtentKey : @(_active_model->selection().extent()),
+    kSelectionAffinityKey : textAffinity,
+    kSelectionIsDirectionalKey : @NO,
+    kComposingBaseKey : @(_active_model->composing_range().base()),
+    kComposingExtentKey : @(_active_model->composing_range().extent()),
+    kTextKey : [NSString stringWithCString:_active_model->GetText().c_str()
+                                  encoding:NSUTF8StringEncoding]
+  };
+
+  [_channel invokeMethod:kUpdateEditStateResponseMethod arguments:@[ self.clientID, state ]];
 }
 
 #pragma mark -
@@ -149,35 +264,29 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
 #pragma mark NSTextInputClient
 
 - (void)insertText:(id)string replacementRange:(NSRange)range {
-  if (self.activeModel != nil) {
-    if (range.location == NSNotFound && range.length == 0) {
-      // Use selection
-      range = self.activeModel.selectedRange;
-    }
+  if (_active_model == nullptr) {
+    return;
+  }
+
+  if (range.location != NSNotFound) {
     // The selected range can actually have negative numbers, since it can start
     // at the end of the range if the user selected the text going backwards.
     // NSRange uses NSUIntegers, however, so we have to cast them to know if the
     // selection is reversed or not.
     long signedLength = static_cast<long>(range.length);
+    long location = range.location;
+    long textLength = _active_model->text_range().end();
 
-    NSUInteger length;
-    NSUInteger location;
-    if (signedLength >= 0) {
-      location = range.location;
-      length = range.length;
-    } else {
-      location = range.location + range.length;
-      length = ABS(signedLength);
-    }
-    if (location > self.activeModel.text.length)
-      location = self.activeModel.text.length;
-    if (length > (self.activeModel.text.length - location))
-      length = self.activeModel.text.length - location;
-    [self.activeModel.text replaceCharactersInRange:NSMakeRange(location, length)
-                                         withString:string];
-    self.activeModel.selectedRange = NSMakeRange(location + ((NSString*)string).length, 0);
-    [self updateEditState];
+    size_t base = std::clamp(location, 0L, textLength);
+    size_t extent = std::clamp(location + signedLength, 0L, textLength);
+    _active_model->SetSelection(flutter::TextRange(base, extent));
   }
+
+  _active_model->AddText([string cStringUsingEncoding:NSUTF8StringEncoding]);
+  if (_active_model->composing()) {
+    _active_model->CommitComposing();
+  }
+  [self updateEditState];
 }
 
 - (void)doCommandBySelector:(SEL)selector {
@@ -192,54 +301,79 @@ static NSString* const kMultilineInputType = @"TextInputType.multiline";
 }
 
 - (void)insertNewline:(id)sender {
-  if (self.activeModel != nil) {
-    if ([self.activeModel.inputType isEqualToString:kMultilineInputType]) {
-      [self insertText:@"\n" replacementRange:self.activeModel.selectedRange];
-    }
-    [_channel invokeMethod:kPerformAction
-                 arguments:@[ self.activeModel.clientID, self.activeModel.inputAction ]];
+  if (_active_model == nullptr) {
+    return;
   }
+  if (_active_model->composing()) {
+    _active_model->CommitComposing();
+    _active_model->EndComposing();
+  }
+  if ([self.inputType isEqualToString:kMultilineInputType]) {
+    [self insertText:@"\n" replacementRange:self.selectedRange];
+  }
+  [_channel invokeMethod:kPerformAction arguments:@[ self.clientID, self.inputAction ]];
 }
 
 - (void)setMarkedText:(id)string
         selectedRange:(NSRange)selectedRange
      replacementRange:(NSRange)replacementRange {
-  if (self.activeModel != nil) {
-    [self.activeModel.text replaceCharactersInRange:replacementRange withString:string];
-    self.activeModel.selectedRange = selectedRange;
-    [self updateEditState];
+  if (_active_model == nullptr) {
+    return;
   }
+  if (!_active_model->composing()) {
+    _active_model->BeginComposing();
+  }
+
+  // Input string may be NSString or NSAttributredString.
+  BOOL isAttributedString = [string isKindOfClass:[NSAttributedString class]];
+  NSString* marked_text = isAttributedString ? [string string] : string;
+  _active_model->UpdateComposingText([marked_text cStringUsingEncoding:NSUTF8StringEncoding]);
+
+  [self updateEditState];
 }
 
 - (void)unmarkText {
-  if (self.activeModel != nil) {
-    self.activeModel.markedRange = NSMakeRange(NSNotFound, 0);
-    [self updateEditState];
+  if (_active_model != nullptr) {
+    return;
   }
+  _active_model->CommitComposing();
+  _active_model->EndComposing();
+  [self updateEditState];
 }
 
 - (NSRange)selectedRange {
-  return (self.activeModel == nil) ? NSMakeRange(NSNotFound, 0) : self.activeModel.selectedRange;
+  if (_active_model == nullptr) {
+    return NSMakeRange(NSNotFound, 0);
+  }
+  return NSMakeRange(_active_model->selection().base(),
+                     _active_model->selection().extent() - _active_model->selection().base());
 }
 
 - (NSRange)markedRange {
-  return (self.activeModel == nil) ? NSMakeRange(NSNotFound, 0) : self.activeModel.markedRange;
+  if (_active_model == nullptr) {
+    return NSMakeRange(NSNotFound, 0);
+  }
+  return NSMakeRange(
+      _active_model->composing_range().base(),
+      _active_model->composing_range().extent() - _active_model->composing_range().base());
 }
 
 - (BOOL)hasMarkedText {
-  return (self.activeModel == nil) ? NO : self.activeModel.markedRange.location != NSNotFound;
+  return _active_model != nullptr && _active_model->composing_range().length() > 0;
 }
 
 - (NSAttributedString*)attributedSubstringForProposedRange:(NSRange)range
                                                actualRange:(NSRangePointer)actualRange {
-  if (self.activeModel) {
-    if (actualRange != nil)
-      *actualRange = range;
-    NSString* substring = [self.activeModel.text substringWithRange:range];
-    return [[NSAttributedString alloc] initWithString:substring attributes:nil];
-  } else {
+  if (_active_model == nullptr) {
     return nil;
   }
+  if (actualRange != nil) {
+    *actualRange = range;
+  }
+  NSString* text = [NSString stringWithCString:_active_model->GetText().c_str()
+                                      encoding:NSUTF8StringEncoding];
+  NSString* substring = [text substringWithRange:range];
+  return [[NSAttributedString alloc] initWithString:substring attributes:nil];
 }
 
 - (NSArray<NSString*>*)validAttributesForMarkedText {
