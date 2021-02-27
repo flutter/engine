6ff7089c0d21f4e169d5a5c41463644ea5146fa9
Chris Bracken <chris@bracken.jp>
2021-02-27 11:59:09 -0800

    Add tests for UpdateCursorRect

--- shell/platform/windows/win32_flutter_window.cc
+++ shell/platform/windows/win32_flutter_window.cc
@@ -194,12 +194,12 @@ void Win32FlutterWindow::OnScroll(double delta_x, double delta_y) {
                                       kScrollOffsetMultiplier);
 }
 
-void Win32FlutterWindow::UpdateCursorRect(const Rect& rect) {
+void Win32FlutterWindow::OnCursorRectUpdated(const Rect& rect) {
   // Convert the rect from Flutter logical coordinates to device coordinates.
   auto scale = GetDpiScale();
   Point origin(rect.left() * scale, rect.top() * scale);
   Size size(rect.width() * scale, rect.height() * scale);
-  text_input_manager_.UpdateCaretRect(Rect(origin, size));
+  UpdateCursorRect(Rect(origin, size));
 }
 
 }  // namespace flutter
