1f6f920e6797f60cdd9134015e3698b29f2142b4
Tong Mu <dkwingsmt@gmail.com>
2021-03-03 21:45:10 -0800

    Shift and A unit test

--- shell/platform/darwin/macos/framework/Source/FlutterKeyEmbedderHandlerUnittests.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterKeyEmbedderHandlerUnittests.mm
@@ -44,14 +44,14 @@ constexpr uint64_t kPhysicalKeyA = 0x00070004;
 // constexpr uint64_t kPhysicalControlLeft = 0x000700e0;
 // constexpr uint64_t kPhysicalControlRight = 0x000700e4;
 // constexpr uint64_t kPhysicalShiftLeft = 0x000700e1;
-// constexpr uint64_t kPhysicalShiftRight = 0x000700e5;
+constexpr uint64_t kPhysicalShiftRight = 0x000700e5;
 // constexpr uint64_t kPhysicalKeyNumLock = 0x00070053;
 
 constexpr uint64_t kLogicalKeyA = 0x00000061;
 // constexpr uint64_t kLogicalControlLeft = 0x00300000105;
 // constexpr uint64_t kLogicalControlRight = 0x00400000105;
 // constexpr uint64_t kLogicalShiftLeft = 0x0030000010d;
-// constexpr uint64_t kLogicalShiftRight = 0x0040000010d;
+constexpr uint64_t kLogicalShiftRight = 0x0040000010d;
 // constexpr uint64_t kLogicalKeyNumLock = 0x0000000010a;
 
 typedef void (^ResponseCallback)(bool handled);
@@ -163,5 +163,102 @@ TEST(FlutterKeyEmbedderHandlerUnittests, BasicKeyEvent) {
   [callbacks removeAllObjects];
   [events removeAllObjects];
 }
+
+// Press L shift, A, then release L shift then A, on an US keyboard.
+//
+// This is special because the characters for the A key will change in this
+// process.
+TEST(FlutterKeyEmbedderHandlerUnittests, ToggleModifiersDuringKeyTap) {
+  __block NSMutableArray<TestKeyEvent*>* events = [[NSMutableArray<TestKeyEvent*> alloc] init];
+  FlutterKeyEvent* event;
+
+  FlutterKeyEmbedderHandler* handler = [[FlutterKeyEmbedderHandler alloc]
+      initWithSendEvent:^(const FlutterKeyEvent& event, _Nullable FlutterKeyEventCallback callback,
+                          _Nullable _VoidPtr user_data) {
+        [events addObject:[[TestKeyEvent alloc] initWithEvent:&event]];
+        if (callback != nullptr) {
+          callback(true, user_data);
+        }
+      }];
+
+  [handler handleEvent:keyEvent(NSEventTypeFlagsChanged, 0x20104, @"", @"", FALSE, 60)
+              callback:^(BOOL handled) {}];
+
+  EXPECT_EQ([events count], 1u);
+  event = [events lastObject]->data;
+  EXPECT_EQ(event->type, kFlutterKeyEventTypeDown);
+  EXPECT_EQ(event->physical, kPhysicalShiftRight);
+  EXPECT_EQ(event->logical, kLogicalShiftRight);
+  EXPECT_STREQ(event->character, nullptr);
+  EXPECT_EQ(event->synthesized, false);
+
+  [events removeAllObjects];
+
+  [handler handleEvent:keyEvent(NSEventTypeKeyDown, 0x20104, @"A", @"A", FALSE, 0)
+              callback:^(BOOL handled) {}];
+
+  EXPECT_EQ([events count], 1u);
+  event = [events lastObject]->data;
+  EXPECT_EQ(event->type, kFlutterKeyEventTypeDown);
+  EXPECT_EQ(event->physical, kPhysicalKeyA);
+  EXPECT_EQ(event->logical, kLogicalKeyA);
+  EXPECT_STREQ(event->character, "A");
+  EXPECT_EQ(event->synthesized, false);
+
+  [events removeAllObjects];
+
+  [handler handleEvent:keyEvent(NSEventTypeKeyDown, 0x20104, @"A", @"A", TRUE, 0)
+              callback:^(BOOL handled) {}];
+
+  EXPECT_EQ([events count], 1u);
+  event = [events lastObject]->data;
+  EXPECT_EQ(event->type, kFlutterKeyEventTypeRepeat);
+  EXPECT_EQ(event->physical, kPhysicalKeyA);
+  EXPECT_EQ(event->logical, kLogicalKeyA);
+  EXPECT_STREQ(event->character, "A");
+  EXPECT_EQ(event->synthesized, false);
+
+  [events removeAllObjects];
+
+  [handler handleEvent:keyEvent(NSEventTypeFlagsChanged, 0x100, @"", @"", FALSE, 60)
+              callback:^(BOOL handled) {}];
+
+  EXPECT_EQ([events count], 1u);
+  event = [events lastObject]->data;
+  EXPECT_EQ(event->type, kFlutterKeyEventTypeUp);
+  EXPECT_EQ(event->physical, kPhysicalShiftRight);
+  EXPECT_EQ(event->logical, kLogicalShiftRight);
+  EXPECT_STREQ(event->character, nullptr);
+  EXPECT_EQ(event->synthesized, false);
+
+  [events removeAllObjects];
+
+  [handler handleEvent:keyEvent(NSEventTypeKeyDown, 0x100, @"a", @"a", TRUE, 0)
+              callback:^(BOOL handled) {}];
+
+  EXPECT_EQ([events count], 1u);
+  event = [events lastObject]->data;
+  EXPECT_EQ(event->type, kFlutterKeyEventTypeRepeat);
+  EXPECT_EQ(event->physical, kPhysicalKeyA);
+  EXPECT_EQ(event->logical, kLogicalKeyA);
+  EXPECT_STREQ(event->character, "a");
+  EXPECT_EQ(event->synthesized, false);
+
+  [events removeAllObjects];
+
+  [handler handleEvent:keyEvent(NSEventTypeKeyUp, 0x20104, @"a", @"a", FALSE, 0)
+              callback:^(BOOL handled) {}];
+
+  EXPECT_EQ([events count], 1u);
+  event = [events lastObject]->data;
+  EXPECT_EQ(event->type, kFlutterKeyEventTypeUp);
+  EXPECT_EQ(event->physical, kPhysicalKeyA);
+  EXPECT_EQ(event->logical, kLogicalKeyA);
+  EXPECT_STREQ(event->character, nullptr);
+  EXPECT_EQ(event->synthesized, false);
+
+  [events removeAllObjects];
+}
+
 
 }  // namespace flutter::testing
