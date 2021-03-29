1265af369b27725b006dfe943c855b9e590da3bc
Xiao Yu <xster@google.com>
2021-03-29 15:11:01 -0700

    Deduplicate plugin registration logic

--- shell/platform/android/io/flutter/embedding/engine/plugins/util/GeneratedPluginRegister.java
+++ shell/platform/android/io/flutter/embedding/engine/plugins/util/GeneratedPluginRegister.java
@@ -33,11 +33,14 @@ public class GeneratedPluginRegister {
           generatedPluginRegistrant.getDeclaredMethod("registerWith", FlutterEngine.class);
       registrationMethod.invoke(null, flutterEngine);
     } catch (Exception e) {
-      Log.w(
+      Log.e(
           TAG,
           "Tried to automatically register plugins with FlutterEngine ("
               + flutterEngine
               + ") but could not find and invoke the GeneratedPluginRegistrant.");
+      Log.e(
+        TAG,
+        "Received exception " + e.getCause());
     }
   }
 }
