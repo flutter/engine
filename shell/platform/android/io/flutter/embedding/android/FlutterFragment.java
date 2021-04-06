2058a169296ed0b2e64281efa3b606dd795e0de3
math1man <aweiland@google.com>
2021-04-06 08:09:53 -0700

    Remove bad import

--- shell/platform/android/io/flutter/embedding/android/FlutterFragment.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterFragment.java
@@ -9,7 +9,6 @@ import android.content.ComponentCallbacks2;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
