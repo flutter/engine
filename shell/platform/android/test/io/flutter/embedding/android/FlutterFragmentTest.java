a59d9ef5b665f64b789a2f418f94975a46329f8a
math1man <aweiland@google.com>
2021-04-07 15:03:46 -0700

    Remove unused import in FlutterFragmentTest

--- shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
+++ shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
@@ -17,7 +17,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
 @Config(manifest = Config.NONE)
