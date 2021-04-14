b268c8bc3de661040a0e19de715f58b3db3fc3ce
Dan Field <dnfield@google.com>
2021-04-13 22:46:35 -0700

    ..

--- shell/platform/windows/flutter_windows_view.h
+++ shell/platform/windows/flutter_windows_view.h
@@ -182,20 +182,20 @@ class FlutterWindowsView : public WindowBindingHandlerDelegate,
   void SendWindowMetrics(size_t width, size_t height, double dpiscale) const;
 
   // Reports a mouse movement to Flutter engine.
-  void SendPointerMove(double x, double y);
+  void SendPointerMove(double x, double y, FlutterPointerDeviceKind device_kind);
 
   // Reports mouse press to Flutter engine.
-  void SendPointerDown(double x, double y);
+  void SendPointerDown(double x, double y, FlutterPointerDeviceKind device_kind);
 
   // Reports mouse release to Flutter engine.
-  void SendPointerUp(double x, double y);
+  void SendPointerUp(double x, double y, FlutterPointerDeviceKind device_kind);
 
   // Reports mouse left the window client area.
   //
   // Win32 api doesn't have "mouse enter" event. Therefore, there is no
   // SendPointerEnter method. A mouse enter event is tracked then the "move"
   // event is called.
-  void SendPointerLeave();
+  void SendPointerLeave(FlutterPointerDeviceKind device_kind);
 
   // Reports a keyboard character to Flutter engine.
   void SendText(const std::u16string&);
