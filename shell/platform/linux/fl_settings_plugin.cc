0ab457825de3473d3f0f78ca9836c4d441624f5b
J-P Nurmi <jpnurmi@gmail.com>
2021-04-29 19:41:34 +0200

    Resolve review comments

--- shell/platform/linux/fl_settings_plugin.cc
+++ shell/platform/linux/fl_settings_plugin.cc
@@ -6,7 +6,7 @@
 
 #include <gmodule.h>
 #include <gtk/gtk.h>
-#include <cstring>
+#include <math.h>
 
 #include "flutter/shell/platform/linux/public/flutter_linux/fl_basic_message_channel.h"
 #include "flutter/shell/platform/linux/public/flutter_linux/fl_json_message_codec.h"
@@ -23,6 +23,8 @@ static constexpr char kDesktopTextScalingFactorKey[] = "text-scaling-factor";
 static constexpr char kDesktopClockFormatKey[] = "clock-format";
 static constexpr char kClockFormat24Hour[] = "24h";
 
+enum class Brightness { Light, Dark };
+
 struct _FlSettingsPlugin {
   GObject parent_instance;
 
@@ -33,25 +35,49 @@ struct _FlSettingsPlugin {
 
 G_DEFINE_TYPE(FlSettingsPlugin, fl_settings_plugin, G_TYPE_OBJECT)
 
-static bool is_dark_color(GdkRGBA* color) {
-  return color->red < 0.5 && color->green < 0.5 && color->blue < 0.5;
+// The color brightness calculation has been adapted from theme_data.dart:
+// https://github.com/flutter/flutter/blob/8fe4cc79648a952f9c7e49a5248756c2ff98fa3b/packages/flutter/lib/src/material/theme_data.dart#L1470-L1488
+
+// See <https://www.w3.org/TR/WCAG20/#relativeluminancedef>.
+static gdouble linearize_color_component(gdouble component) {
+  if (component <= 0.03928)
+    return component / 12.92;
+  return pow((component + 0.055) / 1.055, 2.4);
+}
+
+// See <https://en.wikipedia.org/wiki/Relative_luminance>.
+gdouble compute_luminance(GdkRGBA* color) {
+  gdouble r = linearize_color_component(color->red);
+  gdouble g = linearize_color_component(color->green);
+  gdouble b = linearize_color_component(color->blue);
+  return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+}
+
+static Brightness estimate_brightness_for_color(GdkRGBA* color) {
+  gdouble relative_luminance = compute_luminance(color);
+
+  // See <https://www.w3.org/TR/WCAG20/#contrast-ratiodef> and
+  // <https://material.io/go/design-theming#color-color-palette>.
+  const gdouble kThreshold = 0.15;
+  if ((relative_luminance + 0.05) * (relative_luminance + 0.05) > kThreshold)
+    return Brightness::Light;
+  return Brightness::Dark;
 }
 
 static bool is_dark_theme() {
   // GTK doesn't have a specific flag for dark themes, so we check if the
   // style text color is light or dark
   GList* windows = gtk_window_list_toplevels();
-  if (windows == nullptr) {
+  if (windows == nullptr)
     return false;
-  }
 
   GtkWidget* window = GTK_WIDGET(windows->data);
   g_list_free(windows);
 
-  GdkRGBA color;
+  GdkRGBA text_color;
   GtkStyleContext* style = gtk_widget_get_style_context(window);
-  gtk_style_context_get_color(style, GTK_STATE_FLAG_NORMAL, &color);
-  return !is_dark_color(&color);
+  gtk_style_context_get_color(style, GTK_STATE_FLAG_NORMAL, &text_color);
+  return estimate_brightness_for_color(&text_color) == Brightness::Light;
 }
 
 // Sends the current settings to the Flutter engine.
