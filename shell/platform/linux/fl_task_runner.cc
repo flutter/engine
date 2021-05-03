1334ea8e2add5ca1f47fc51f57e86f36139a2842
Matej Knopp <matej.knopp@gmail.com>
2021-05-03 13:28:30 +0200

    Fix licenses

--- shell/platform/linux/fl_task_runner.cc
+++ shell/platform/linux/fl_task_runner.cc
@@ -1,3 +1,7 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "flutter/shell/platform/linux/fl_task_runner.h"
 
 static constexpr int kMicrosecondsPerNanosecond = 1000;
