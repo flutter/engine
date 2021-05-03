3fe06d9f53af6e127481f3e4f55f520d885554cb
Yegor <yjbanov@google.com>
2021-05-03 11:21:45 -0700

    add gclient/ninja build logic

--- lib/web_ui/dev/felt.bat
+++ lib/web_ui/dev/felt.bat
@@ -5,6 +5,13 @@
 @ECHO OFF
 SETLOCAL
 
+:: Make sure gclient and ninja exist. Otherwise felt won't work.
+FOR /F "tokens=1-2 delims=:" %%a in ('where gclient') DO SET GCLIENT_PATH=%%b
+IF %GCLIENT_PATH%==[] (ECHO "ERROR: gclient is not in your PATH")
+
+FOR /F "tokens=1-2 delims=:" %%a in ('where ninja') DO SET NINJA_PATH=%%b
+IF %NINJA_PATH%==[] (ECHO "ERROR: ninja is not in your PATH")
+
 :: Starting from this script's path, walk up to engine source directory.
 SET SCRIPT_DIR=%~dp0
 FOR %%a IN ("%SCRIPT_DIR:~0,-1%") DO SET TMP=%%~dpa
@@ -25,9 +32,26 @@ SET FELT_PATH=%DEV_DIR%\felt.dart
 SET DART_TOOL_DIR=%WEB_UI_DIR%\.dart_tool
 SET SNAPSHOT_PATH=%DART_TOOL_DIR%\felt.snapshot
 
-:: Set revision from using git in Flutter directory.
-CD %FLUTTER_DIR%
-FOR /F "tokens=1 delims=:" %%a in ('git rev-parse HEAD') DO SET REVISION=%%a
+SET needsHostDebugUnoptRebuild=0
+for %%x in (%*) do (
+  if ["%%~x"]==["--clean"] (
+    ECHO Clean rebuild requested
+    SET needsHostDebugUnoptRebuild=1
+  )
+)
+
+IF NOT EXIST %OUT_DIR% (SET needsHostDebugUnoptRebuild=1)
+IF NOT EXIST %HOST_DEBUG_UNOPT_DIR% (SET needsHostDebugUnoptRebuild=1)
+
+IF %needsHostDebugUnoptRebuild%==1 (
+  ECHO Building host_debug_unopt
+  :: Delete old snapshot, if any, because the new Dart SDK may invalidate it.
+  IF EXIST "%SNAPSHOT_PATH%" (
+    del %SNAPSHOT_PATH%
+  )
+  CALL gclient sync -D
+  CALL python %GN% --unoptimized --full-dart-sdk
+  CALL ninja -C %HOST_DEBUG_UNOPT_DIR%)
 
 cd %WEB_UI_DIR%
 IF NOT EXIST "%SNAPSHOT_PATH%" (
