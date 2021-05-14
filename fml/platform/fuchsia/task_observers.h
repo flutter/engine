e61616f05de1020dcdfc8e89e1de8dadcde9465a
Hunter Freyer <hjfreyer@google.com>
2021-05-14 13:40:08 +0000

    Replace flutter_runner::Thread with fml::Thread.
    This CL makes the treading logic in Fuchsia's flutter_runner more
    consistent with fml expectations, but it still has quirks. Notably:
    
    - Not all async work get posted to a fml::TaskRunner. Some work done
      by Fuchsia libraries gets posted directly to
      async_get_default_dispatcher(). This work doesn't trigger the
      fml::MessageLoop's task observers. As a result, we continue to have
      Fuchsia-specific task observers which fire for all async work,
      regardless of which way it was posted.
    
    - There's awkwardness when trying to run Fuchsia code on a specific
      fml::TaskRunner if that fuchsia code accepts an
      async_dispatcher_t. Since you can no longer get an
      async_dispatcher_t for a given thread, you instead must post a
      closure to the fml::TaskRunner that calls
      async_get_default_dispatcher(), and then calls the fuchsia library
      with the default dispatcher.
    
    - Some tests still use task_runner_adapter.h because async::Loop
      offers more control in unit tests than fml::MessageLoop does.
    
    - If this successfully lands, there will be some cosmetic follow-up
      changes to make, like using ThreadHost instead of an array of
      fml::Threads.

--- fml/platform/fuchsia/task_observers.h
+++ fml/platform/fuchsia/task_observers.h
@@ -9,6 +9,25 @@
 
 namespace fml {
 
+// Executes all closures that were registered via
+// `CurrentMessageLoopAddAfterTaskObserver` on this thread.
+//
+// WARNING(fxbug.dev/77957): These task observers are distinct from the task
+// observers that can be specified via `fml::MessageLoop::AddTaskObserver` and
+// they behave differently!
+//
+// Task observers registered via `fml::MessageLoop::AddTaskObserver` only fire
+// after work that was posted via the `fml::MessageLoop`'s `TaskRunner`
+// completes. Work that is posted directly to the Fuchsia message loop (e.g.
+// using `async::PostTask(async_get_default_dispatcher(), ...)`) is invisible to
+// `fml::MessageLoop`, so the `fml::MessageLoop`'s task observers don't fire.
+//
+// The task observers registered with `CurrentMessageLoopAddAfterTaskObserver`,
+// however, fire after _every_ work item is completed, regardless of whether it
+// was posted to the Fuchsia message loop directly or via `fml::MessageLoop`.
+//
+// These two mechanisms are redundant and confusing, so we should fix it
+// somehow.
 void ExecuteAfterTaskObservers();
 
 void CurrentMessageLoopAddAfterTaskObserver(intptr_t key,
