997baa97d2dd0fca8a640abeadaa5779f61b472d
Anthony Robledo <anthonyrobledo@gmail.com>
2021-06-14 17:01:34 -0400

    Add some more ops tests

---
+++ lib/spirv/test/e2e_fragment_shaders/op_type_vector.glsl
@@ -0,0 +1,11 @@
+#version 310 es
+
+precision highp float;
+
+layout(location = 0) out vec4 fragColor;
+
+void main() {
+    vec2 ones = vec2(1.0, 1.0);
+    vec3 zeros = vec3(0.0, 0.0, 0.0);
+    fragColor = vec4(zeros[0], ones[0], zeros[2], ones[1]);
+}
