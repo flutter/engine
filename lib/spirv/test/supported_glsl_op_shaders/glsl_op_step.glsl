48179257dc8c472c728ac21385eeeac231aa6dd2
Anthony Robledo <anthonyrobledo@gmail.com>
2021-06-30 17:37:01 -0400

    better color comparison, use string instead of int for debugging

--- lib/spirv/test/supported_glsl_op_shaders/glsl_op_step.glsl
+++ lib/spirv/test/supported_glsl_op_shaders/glsl_op_step.glsl
@@ -4,12 +4,12 @@ precision highp float;
 
 layout(location = 0) out vec4 fragColor;
 
-// TODO
+// 0.0 is returned if x (second param) < edge (first param), and 1.0 is returned otherwise.
 void main() {
     fragColor = vec4(
-        0.0,
-        1.0,
-        0.0,
-        1.0
+        step(5.0, 4.0),
+        step(4.0, 5.0),
+        step(-4.0, -5.0),
+        step(-5.0, -4.0)
     );
 }
