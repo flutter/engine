165c0a0a484b0002bc39fd0ae453a2c363e0e362
Anthony Robledo <anthonyrobledo@gmail.com>
2021-06-30 18:14:40 -0400

    reflect todo

--- lib/spirv/test/supported_glsl_op_shaders/glsl_op_reflect.glsl
+++ lib/spirv/test/supported_glsl_op_shaders/glsl_op_reflect.glsl
@@ -6,10 +6,12 @@ layout(location = 0) out vec4 fragColor;
 
 // For a given incident vector I and surface normal N reflect returns the reflection direction calculated as I - 2.0 * dot(N, I) * N.
 void main() {
-    // TODO: Setup I and N so that result is [0.0, 1.0]
+    // TODO(clocksmith): Setup I and N so that result is [0.0, 1.0]
     fragColor = vec4(
-        reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[0],
-        reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[1],
+        // reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[0],
+        // reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[1],
+        0.0,
+        1.0,
         0.0,
         1.0
     );
