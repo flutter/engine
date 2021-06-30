09cc3a658f824e58ec93a711432b9e9ccd10f428
Anthony Robledo <anthonyrobledo@gmail.com>
2021-06-30 18:03:59 -0400

    reflect still fails, but closer

--- lib/spirv/test/supported_glsl_op_shaders/glsl_op_reflect.glsl
+++ lib/spirv/test/supported_glsl_op_shaders/glsl_op_reflect.glsl
@@ -4,12 +4,12 @@ precision highp float;
 
 layout(location = 0) out vec4 fragColor;
 
-// TODO
-// I - 2.0 * dot(N, I) * N.
+// For a given incident vector I and surface normal N reflect returns the reflection direction calculated as I - 2.0 * dot(N, I) * N.
 void main() {
+    // TODO: Setup I and N so that result is [0.0, 1.0]
     fragColor = vec4(
-        0.0,
-        1.0,
+        reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[0],
+        reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[1],
         0.0,
         1.0
     );
