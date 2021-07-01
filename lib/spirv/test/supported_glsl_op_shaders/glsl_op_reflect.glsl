2d6f1063fa056b537053918b3c9c9871586d6c7f
Anthony Robledo <anthonyrobledo@gmail.com>
2021-07-01 12:09:02 -0400

    reflect test

--- lib/spirv/test/supported_glsl_op_shaders/glsl_op_reflect.glsl
+++ lib/spirv/test/supported_glsl_op_shaders/glsl_op_reflect.glsl
@@ -6,12 +6,18 @@ layout(location = 0) out vec4 fragColor;
 
 // For a given incident vector I and surface normal N reflect returns the reflection direction calculated as I - 2.0 * dot(N, I) * N.
 void main() {
-    // TODO(clocksmith): Setup I and N so that result is [0.0, 1.0]
+    // To get [0.0, 1.0] as the output, choose [0.6, 0.8] as N, and solve for I.
+    // Since the reflection is symmetric:
+    // I’ = reflect(I)
+    // I’ = I - 2 dot(N, I) N
+    // I = I’ - 2 dot(N, I’) N
+    // N = [0.6, 0.8]
+    // I’ = [0, 1]
+    // I = [0, 1] - 2 * 0.8 [0.6, 0.8]
+    // I = [-0.96, -0.28]
     fragColor = vec4(
-        // reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[0],
-        // reflect(vec2(0.99337748344, -0.1655629139), vec2(0.6, 0.8))[1],
-        0.0,
-        1.0,
+        reflect(vec2(-0.96, -0.28), vec2(0.6, 0.8))[0],
+        reflect(vec2(-0.96, -0.28), vec2(0.6, 0.8))[1],
         0.0,
         1.0
     );
--- lib/spirv/test/supported_op_shaders/op_type_float.glsl
+++ lib/spirv/test/supported_op_shaders/op_type_float.glsl
@@ -5,7 +5,7 @@ precision highp float;
 layout(location = 0) out vec4 fragColor;
 
 void main() {
-    float one = 1.0;
     float zero = 0.0;
+    float one = 1.0;
     fragColor = vec4(zero, one, zero, one);
 }
