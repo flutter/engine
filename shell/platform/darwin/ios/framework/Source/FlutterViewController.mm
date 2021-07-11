d8530053d175fb9e8f4d2bb8d89217c95fdc782a
ColdPaleLight <coldpalelight@gmail.com>
2021-07-11 18:18:58 +0800

    Fixes some bugs when multiple Flutter VC shared one engine

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -627,7 +627,9 @@ static void sendFakeTouchEvent(FlutterEngine* engine,
     _engineNeedsLaunch = NO;
   }
 
-  [_engine.get() attachView];
+  if ([_engine.get() viewController] == self) {
+    [_engine.get() attachView];
+  }
 
   if (@available(iOS 13.4, *)) {
     _pointerInteraction.reset([[UIPointerInteraction alloc] initWithDelegate:self]);
@@ -967,7 +969,9 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 #pragma mark - Handle view resizing
 
 - (void)updateViewportMetrics {
-  [_engine.get() updateViewportMetrics:_viewportMetrics];
+  if ([_engine.get() viewController] == self) {
+    [_engine.get() updateViewportMetrics:_viewportMetrics];
+  }
 }
 
 - (CGFloat)statusBarPadding {
--- shell/platform/darwin/ios/framework/Source/FlutterViewControllerTest.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewControllerTest.mm
@@ -5,6 +5,7 @@
 #import <OCMock/OCMock.h>
 #import <XCTest/XCTest.h>
 
+#import "flutter/lib/ui/window/viewport_metrics.h"
 #import "flutter/shell/platform/darwin/common/framework/Headers/FlutterBinaryMessenger.h"
 #import "flutter/shell/platform/darwin/common/framework/Headers/FlutterMacros.h"
 #import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterViewController.h"
@@ -37,6 +38,8 @@ class PointerDataPacket {};
          libraryURI:(NSString*)libraryURI
        initialRoute:(NSString*)initialRoute;
 - (void)dispatchPointerDataPacket:(std::unique_ptr<flutter::PointerDataPacket>)packet;
+- (void)updateViewportMetrics:(flutter::ViewportMetrics)viewportMetrics;
+- (void)attachView;
 @end
 
 @interface FlutterEngine (TestLowMemory)
@@ -86,6 +89,7 @@ typedef enum UIAccessibilityContrast : NSInteger {
 - (void)performOrientationUpdate:(UIInterfaceOrientationMask)new_preferences;
 - (void)dispatchPresses:(NSSet<UIPress*>*)presses;
 - (void)scrollEvent:(UIPanGestureRecognizer*)recognizer;
+- (void)updateViewportMetrics;
 @end
 
 @implementation FlutterViewControllerTest
@@ -134,6 +138,63 @@ typedef enum UIAccessibilityContrast : NSInteger {
     OCMVerify([viewControllerMock surfaceUpdated:NO]);
   }
   XCTAssertNil(weakViewController);
+}
+
+- (void)testUpdateViewportMetricsDoesntInvokeEngineWhenNotTheViewController {
+  FlutterEngine* mockEngine = OCMPartialMock([[FlutterEngine alloc] init]);
+  [mockEngine createShell:@"" libraryURI:@"" initialRoute:nil];
+  FlutterViewController* viewControllerA = [[FlutterViewController alloc] initWithEngine:mockEngine
+                                                                                 nibName:nil
+                                                                                  bundle:nil];
+  mockEngine.viewController = nil;
+  FlutterViewController* viewControllerB = [[FlutterViewController alloc] initWithEngine:mockEngine
+                                                                                 nibName:nil
+                                                                                  bundle:nil];
+  mockEngine.viewController = viewControllerB;
+  [viewControllerA updateViewportMetrics];
+  flutter::ViewportMetrics viewportMetrics;
+  OCMVerify(never(), [mockEngine updateViewportMetrics:viewportMetrics]);
+}
+
+- (void)testUpdateViewportMetricsDoesInvokeEngineWhenIsTheViewController {
+  FlutterEngine* mockEngine = OCMPartialMock([[FlutterEngine alloc] init]);
+  [mockEngine createShell:@"" libraryURI:@"" initialRoute:nil];
+  FlutterViewController* viewController = [[FlutterViewController alloc] initWithEngine:mockEngine
+                                                                                nibName:nil
+                                                                                 bundle:nil];
+  mockEngine.viewController = viewController;
+  [viewController updateViewportMetrics];
+  flutter::ViewportMetrics viewportMetrics;
+  OCMVerify([mockEngine updateViewportMetrics:viewportMetrics]);
+}
+
+- (void)testViewDidLoadDoesntInvokeEngineWhenNotTheViewController {
+  FlutterEngine* mockEngine = OCMPartialMock([[FlutterEngine alloc] init]);
+  [mockEngine createShell:@"" libraryURI:@"" initialRoute:nil];
+  FlutterViewController* viewControllerA = [[FlutterViewController alloc] initWithEngine:mockEngine
+                                                                                 nibName:nil
+                                                                                  bundle:nil];
+  mockEngine.viewController = nil;
+  FlutterViewController* viewControllerB = [[FlutterViewController alloc] initWithEngine:mockEngine
+                                                                                 nibName:nil
+                                                                                  bundle:nil];
+  mockEngine.viewController = viewControllerB;
+  UIView* view = viewControllerA.view;
+  XCTAssertNotNil(view);
+  OCMVerify(never(), [mockEngine attachView]);
+}
+
+- (void)testViewDidLoadDoesInvokeEngineWhenNotTheViewController {
+  FlutterEngine* mockEngine = OCMPartialMock([[FlutterEngine alloc] init]);
+  [mockEngine createShell:@"" libraryURI:@"" initialRoute:nil];
+  mockEngine.viewController = nil;
+  FlutterViewController* viewController = [[FlutterViewController alloc] initWithEngine:mockEngine
+                                                                                nibName:nil
+                                                                                 bundle:nil];
+  mockEngine.viewController = viewController;
+  UIView* view = viewController.view;
+  XCTAssertNotNil(view);
+  OCMVerify([mockEngine attachView]);
 }
 
 - (void)testBinaryMessenger {
