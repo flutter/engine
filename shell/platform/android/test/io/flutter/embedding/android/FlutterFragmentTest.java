b1c782d147acfa84ca4de189e0b4762c1c62cd5a
Emmanuel Garcia <egarciad@google.com>
2021-07-12 11:36:13 -0700

    Fix import

--- shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
+++ shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
@@ -3,6 +3,7 @@ package io.flutter.embedding.android;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
