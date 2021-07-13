58f4642adfc7a3cb04af606f714f6b275ad01995
Dan Field <dnfield@google.com>
2021-07-13 12:02:16 -0700

    update docs

--- lib/ui/semantics.dart
+++ lib/ui/semantics.dart
@@ -665,7 +665,13 @@ abstract class StringAttribute extends NativeFieldWrapperClass1 {
   /// The range of the text to which this attribute applies.
   final TextRange range;
 
-  /// Returns a copy of this atttribute with the given range.
+  /// Creates a new attribute with all properties copied except for range, which
+  /// is updated to the specified value.
+  ///
+  /// For example, the [LocaleStringAttribute] specifies a [Locale] for its
+  /// range of characters. Copying it will result in a new
+  /// [LocaleStringAttribute] that has the same locale but an updated
+  /// [TextRange].
   StringAttribute copy({required TextRange range});
 }
 
