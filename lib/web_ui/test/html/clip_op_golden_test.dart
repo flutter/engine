14066f6048e628c182bc3286382a88071d073797
Mouad Debbar <mdebbar@google.com>
2021-07-22 13:13:08 -0700

    [web] Fixing clipping not being applied when height is zero

--- lib/web_ui/test/html/clip_op_golden_test.dart
+++ lib/web_ui/test/html/clip_op_golden_test.dart
@@ -6,6 +6,8 @@ import 'package:test/bootstrap/browser.dart';
 import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart';
+
+import 'paragraph/helper.dart';
 import 'screenshot.dart';
 
 void main() {
@@ -37,5 +39,51 @@ Future<void> testMain() async {
     canvas.drawRect(titleRect, paint);
     await canvasScreenshot(canvas, 'clip_op_difference',
         region: const Rect.fromLTRB(0, 0, 420, 360));
+  });
+
+  /// Regression test for https://github.com/flutter/flutter/issues/86345
+  test('Clips with zero width or height', () async {
+    const Rect region = Rect.fromLTRB(0, 0, 400, 300);
+    final RecordingCanvas canvas = RecordingCanvas(region);
+
+    final SurfacePaint paint = SurfacePaint()
+      ..style = PaintingStyle.fill
+      ..color = const Color(0xff00ff00);
+    final SurfacePaint borderPaint = SurfacePaint()
+      ..style = PaintingStyle.stroke
+      ..color = const Color(0xffff0000)
+      ..strokeWidth = 1;
+
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        final double x = 10 + i * 70;
+        final double y = 10 + j * 70;
+        canvas.save();
+        // Clip.
+        canvas.clipRect(
+          Rect.fromLTWH(x, y, i * 25, j * 25),
+          ClipOp.intersect,
+        );
+        // Draw the blue (clipped) rect.
+        canvas.drawRect(
+          Rect.fromLTWH(x, y, 50, 50),
+          paint,
+        );
+        final Paragraph p = plain(
+          EngineParagraphStyle(fontFamily: 'Roboto', fontSize: 34),
+          '23',
+          textStyle: EngineTextStyle.only(color: const Color(0xff0000ff)),
+        );
+        p.layout(ParagraphConstraints(width: double.infinity));
+        canvas.drawParagraph(p, Offset(x, y));
+        canvas.restore();
+        // Draw the red border.
+        canvas.drawRect(
+          Rect.fromLTWH(x, y, 50, 50),
+          borderPaint,
+        );
+      }
+    }
+    await canvasScreenshot(canvas, 'clip_zero_width_height', region: region);
   });
 }
