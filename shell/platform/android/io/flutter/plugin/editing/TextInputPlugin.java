a89c8651e7a899502f0324672959d03123ba5bd3
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-07-22 22:57:19 -0700

    update

--- shell/platform/android/io/flutter/plugin/editing/TextInputPlugin.java
+++ shell/platform/android/io/flutter/plugin/editing/TextInputPlugin.java
@@ -314,7 +314,7 @@ public class TextInputPlugin implements ListenableEditingState.EditingStateWatch
             configuration.enableSuggestions,
             configuration.requestPrivacy,
             configuration.textCapitalization);
-    outAttrs.imeOptions = EditorInfo.IME_FLAG_NO_FULLSCREEN;
+    outAttrs.imeOptions = EditorInfo.IIME_FLAG_NO_PERSONALIZED_LEARNING;
     int enterAction;
     if (configuration.inputAction == null) {
       // If an explicit input action isn't set, then default to none for multi-line fields
