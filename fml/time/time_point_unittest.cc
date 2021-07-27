d5ee76b6cb92a08a79bc0323aaeee51fbba1106b
Kaushik Iska <iska.kaushik@gmail.com>
2021-07-27 11:24:01 -0700

    TimePoint::Now uses DartTimestampProvider
    Fixes: https://github.com/flutter/flutter/issues/76875

--- fml/time/time_point_unittest.cc
+++ fml/time/time_point_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "flutter/fml/time/chrono_timestamp_provider.h"
 
+#include "flutter/fml/time/dart_timestamp_provider.h"
+
 #include "gtest/gtest.h"
 
 namespace fml {
@@ -12,6 +14,16 @@ namespace {
 TEST(TimePoint, Control) {
   EXPECT_LT(TimePoint::Min(), ChronoTicksSinceEpoch());
   EXPECT_GT(TimePoint::Max(), ChronoTicksSinceEpoch());
+}
+
+TEST(TimePoint, DartClockIsMonotonic) {
+  const auto t1 = DartTimelineTicksSinceEpoch();
+  const auto t2 = DartTimelineTicksSinceEpoch();
+  const auto t3 = DartTimelineTicksSinceEpoch();
+  EXPECT_LT(TimePoint::Min(), t1);
+  EXPECT_LT(t1, t2);
+  EXPECT_LT(t2, t3);
+  EXPECT_LT(t3, TimePoint::Max());
 }
 
 }  // namespace
