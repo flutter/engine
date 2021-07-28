2804014edefe3823744d2663374bdbd04f3d1814
Christopher Crawford <chriscraws@gmail.com>
2021-07-28 16:15:20 -0700

    add copyright header to test shaders

--- lib/spirv/test/general_shaders/uniforms.glsl
+++ lib/spirv/test/general_shaders/uniforms.glsl
@@ -1,5 +1,9 @@
 #version 320 es
 
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 precision highp float;
 
 layout ( location = 0 ) out vec4 oColor;
