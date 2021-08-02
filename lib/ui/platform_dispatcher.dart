cf8485b20457efee970b3b94e4b9e0a9dc18f90f
Jonah Williams <campfish91@gmail.com>
2021-08-02 10:34:15 -0700

    Add more docs

--- lib/ui/platform_dispatcher.dart
+++ lib/ui/platform_dispatcher.dart
@@ -1131,7 +1131,11 @@ class ViewConfiguration {
   /// phone sensor housings).
   final WindowPadding padding;
 
-  /// The view specific gesture settings.
+  /// Additional configuration for touch gestures performed on this view.
+  ///
+  /// For example, the touch slop defined in physical pixels may be provided
+  /// by the gesture settings and should be preferred over the framework
+  /// touch slop constant.
   final GestureSettings gestureSettings;
 
   @override
