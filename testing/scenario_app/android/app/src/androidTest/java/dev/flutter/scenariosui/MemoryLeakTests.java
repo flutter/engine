f87d0de5612ce9a6c1a96ba49fd256939abb3741
Maurice Parrish <bmparr@google.com>
2021-08-10 16:04:58 -0700

    add and release a texture entry

--- testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenariosui/MemoryLeakTests.java
+++ testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenariosui/MemoryLeakTests.java
@@ -11,7 +11,8 @@ import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 import dev.flutter.scenarios.StrictModeFlutterActivity;
 import dev.flutter.scenarios.TextPlatformViewActivity;
-import io.flutter.embedding.engine.FlutterJNI;
+import io.flutter.view.TextureRegistry;
+import io.flutter.view.TextureRegistry.SurfaceTextureEntry;
 import leakcanary.FailTestOnLeak;
 import org.junit.Rule;
 import org.junit.Test;
@@ -60,49 +61,9 @@ public class MemoryLeakTests {
   @FailTestOnLeak
   public void platformViewHybridComposition_releaseRenderSurface() throws Exception {
     strictActivityRule.launchActivity(intent);
-
-    final PlatformViewsController platformViewsController = new PlatformViewsController();
-
-    final int platformViewId = 0;
-    assertNull(platformViewsController.getPlatformViewById(platformViewId));
-
-    final PlatformViewFactory viewFactory = mock(PlatformViewFactory.class);
-    final PlatformView platformView = mock(PlatformView.class);
-    final View androidView = mock(View.class);
-    when(platformView.getView()).thenReturn(androidView);
-    when(viewFactory.create(any(), eq(platformViewId), any())).thenReturn(platformView);
-
-    platformViewsController.getRegistry().registerViewFactory("testType", viewFactory);
-
-    final FlutterJNI jni = new FlutterJNI();
-    jni.attachToNative(false);
-    final FlutterView flutterView = attach(jni, platformViewsController);
-
-    jni.onFirstFrame();
-
-    // Simulate create call from the framework.
-    final Map<String, Object> platformViewCreateArguments = new HashMap<>();
-    platformViewCreateArguments.put("hybrid", true);
-    platformViewCreateArguments.put("id", platformViewId);
-    platformViewCreateArguments.put("viewType", "testType");
-    platformViewCreateArguments.put("direction", 0);
-    final MethodCall platformCreateMethodCall =
-        new MethodCall("create", platformViewCreateArguments);
-    jni.handlePlatformMessage(
-        "flutter/platform_views", encodeMethodCall(platformCreateMethodCall), /*replyId=*/ 0);
-
-    // Produce a frame that displays a platform view and an overlay surface.
-    platformViewsController.onBeginFrame();
-    platformViewsController.onDisplayPlatformView(
-        platformViewId,
-        /* x=*/ 0,
-        /* y=*/ 0,
-        /* width=*/ 10,
-        /* height=*/ 10,
-        /* viewWidth=*/ 10,
-        /* viewHeight=*/ 10,
-        /* mutatorsStack=*/ new FlutterMutatorsStack());
-
-    assertEquals(flutterView.getChildCount(), 7);
+    final TextureRegistry textureRegistry =
+        strictActivityRule.getActivity().flutterEngine.getRenderer();
+    final SurfaceTextureEntry entry = textureRegistry.createSurfaceTexture();
+    entry.release();
   }
 }
