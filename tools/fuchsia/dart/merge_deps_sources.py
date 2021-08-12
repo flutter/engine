e5efb61cfe494f8411ebbc53e811336519232508
Rich Kadel <richkadel@google.com>
2021-08-12 10:48:03 -0700

    Addressed code review comments

--- tools/fuchsia/dart/merge_deps_sources.py
+++ tools/fuchsia/dart/merge_deps_sources.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.8
 """Merges sources of a Dart target and its dependencies"""
 
-# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Copyright 2013 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
--- tools/fuchsia/python/package_python_binary.py
+++ tools/fuchsia/python/package_python_binary.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.8
 """Creats a Python zip archive for the input main source."""
 
-# Copyright 2021 The Fuchsia Authors. All rights reserved.
+# Copyright 2013 The Flutter Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
