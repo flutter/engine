e23a10e0df9c26b2da77ab4a61b5e617d58172cb
moko256 <koutaro.mo@gmail.com>
2021-08-13 15:56:27 +0900

    [WinUWP] Implement clipboard handler.

--- shell/platform/windows/platform_handler_winuwp.cc
+++ shell/platform/windows/platform_handler_winuwp.cc
@@ -4,8 +4,12 @@
 
 #include "flutter/shell/platform/windows/platform_handler_winuwp.h"
 
-#include "flutter/shell/platform/windows/flutter_windows_view.h"
+#include "third_party/cppwinrt/generated/winrt/Windows.ApplicationModel.DataTransfer.h"
+#include "third_party/cppwinrt/generated/winrt/Windows.Foundation.h"
+#include "third_party/cppwinrt/generated/winrt/Windows.UI.Core.h"
 
+#include "flutter/shell/platform/windows/flutter_windows_view.h"
+#include "flutter/shell/platform/windows/string_conversion.h"
 namespace flutter {
 
 // static
@@ -24,8 +28,42 @@ PlatformHandlerWinUwp::~PlatformHandlerWinUwp() = default;
 void PlatformHandlerWinUwp::GetPlainText(
     std::unique_ptr<MethodResult<rapidjson::Document>> result,
     std::string_view key) {
-  // TODO: Implement. See https://github.com/flutter/flutter/issues/70214.
-  result->NotImplemented();
+  auto activation_mode =
+      winrt::Windows::UI::Core::CoreWindow::GetForCurrentThread()
+          .ActivationMode();
+  // We call `Clipboard::GetContent()` when the application window is in
+  // focus, otherwise calling this will throw an error.
+  if (activation_mode == winrt::Windows::UI::Core::CoreWindowActivationMode::
+                             ActivatedInForeground) {
+    auto content =
+        winrt::Windows::ApplicationModel::DataTransfer::Clipboard::GetContent();
+    // Calling `DataPackageView::GetTextAsync()` when the clipboard has no text
+    // content will throw an error.
+    if (content.Contains(winrt::Windows::ApplicationModel::DataTransfer::
+                             StandardDataFormats::Text())) {
+      // Waiting `DataPackageView::GetTextAsync()` using `TResult.get()` on the
+      // platform thread will causes the application stop, so we continue
+      // response on this callback.
+      content.GetTextAsync().Completed([result = std::move(result), key](
+                                           auto async_info,
+                                           auto _async_status) {
+        auto clipboard_string = async_info.GetResults();
+
+        rapidjson::Document document;
+        document.SetObject();
+        rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
+        document.AddMember(
+            rapidjson::Value(key.data(), allocator),
+            rapidjson::Value(Utf8FromUtf16(clipboard_string), allocator),
+            allocator);
+        result->Success(document);
+      });
+    } else {
+      result->Success(rapidjson::Document());
+    }
+  } else {
+    result->Success(rapidjson::Document());
+  }
 }
 
 void PlatformHandlerWinUwp::GetHasStrings(
@@ -37,8 +75,12 @@ void PlatformHandlerWinUwp::GetHasStrings(
 void PlatformHandlerWinUwp::SetPlainText(
     const std::string& text,
     std::unique_ptr<MethodResult<rapidjson::Document>> result) {
-  // TODO: Implement. See https://github.com/flutter/flutter/issues/70214.
-  result->NotImplemented();
+  winrt::Windows::ApplicationModel::DataTransfer::DataPackage content;
+  content.SetText(Utf16FromUtf8(text));
+  winrt::Windows::ApplicationModel::DataTransfer::Clipboard::SetContent(
+      content);
+
+  result->Success();
 }
 
 }  // namespace flutter
