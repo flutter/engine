62cc3a0023e0761aec1cabbc0fe5fd0a8ebbae2f
Maurice Parrish <bmparr@google.com>
2021-08-13 11:14:08 -0700

    revert files

--- testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/StrictModeFlutterActivity.java
+++ testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/StrictModeFlutterActivity.java
@@ -6,45 +6,19 @@ package dev.flutter.scenarios;
 
 import android.os.Bundle;
 import android.os.StrictMode;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import io.flutter.Log;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.android.RenderMode;
-import io.flutter.embedding.engine.FlutterEngine;
 
 // TODO: Trigger this activity.
 // https://github.com/flutter/flutter/issues/60635
 public class StrictModeFlutterActivity extends FlutterActivity {
-  @Nullable public FlutterEngine flutterEngine;
-
   @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    Log.setLogLevel(Log.VERBOSE);
+  protected void onCreate(Bundle savedInstanceState) {
     StrictMode.setThreadPolicy(
         new StrictMode.ThreadPolicy.Builder()
             .detectDiskReads()
             .detectDiskWrites()
             .penaltyDeath()
             .build());
-    StrictMode.setVmPolicy(
-        new StrictMode.VmPolicy.Builder()
-            .detectLeakedClosableObjects()
-            .penaltyLog()
-            .penaltyDeath()
-            .build());
     super.onCreate(savedInstanceState);
-  }
-
-  @NonNull
-  @Override
-  public RenderMode getRenderMode() {
-    return RenderMode.texture;
-  }
-
-  @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    super.configureFlutterEngine(flutterEngine);
-    this.flutterEngine = flutterEngine;
   }
 }
