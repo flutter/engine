d2dd6adf58ef84fd9cc116cd3897cfde64f24b13
Brandon DeRosier <bdero@google.com>
2021-08-23 12:36:22 -0700

    Configure ASan to symbolize on linux

--- testing/sanitizer_suppressions.sh
+++ testing/sanitizer_suppressions.sh
@@ -1,5 +1,9 @@
 TESTING_DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 
+pushd $TESTING_DIRECTORY/../.. >/dev/null
+ENGINE_BUILDROOT=$(pwd)
+popd >/dev/null
+
 TSAN_SUPPRESSIONS_FILE="${TESTING_DIRECTORY}/tsan_suppressions.txt"
 export TSAN_OPTIONS="suppressions=${TSAN_SUPPRESSIONS_FILE}"
 echo "Using Thread Sanitizer suppressions in ${TSAN_SUPPRESSIONS_FILE}"
@@ -13,4 +17,5 @@ export UBSAN_OPTIONS="suppressions=${UBSAN_SUPPRESSIONS_FILE}"
 echo "Using Undefined Behavior suppressions in ${UBSAN_SUPPRESSIONS_FILE}"
 
 
-export ASAN_OPTIONS="detect_leaks=0:detect_container_overflow=0"
+export ASAN_OPTIONS="symbolize=1:detect_leaks=0:detect_container_overflow=0"
+export ASAN_SYMBOLIZER_PATH=$ENGINE_BUILDROOT/buildtools/linux-x64/clang/bin/llvm-symbolizer
