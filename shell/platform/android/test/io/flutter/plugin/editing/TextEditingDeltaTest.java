31c188d92d87d8ebc9d5bf596f0428d7e93c8302
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-08-29 17:10:11 -0700

    remove unused imports

--- shell/platform/android/test/io/flutter/plugin/editing/TextEditingDeltaTest.java
+++ shell/platform/android/test/io/flutter/plugin/editing/TextEditingDeltaTest.java
@@ -2,7 +2,6 @@ package io.flutter.plugin.editing;
 
 import static org.junit.Assert.assertEquals;
 
-import dalvik.annotation.TestTarget;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
