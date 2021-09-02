fe7146ce3a60b7dabdd45ad9475e822c031f8057
Renzo-Olivares <rmolivares@renzo-olivares.dev>
2021-09-01 22:23:49 -0700

    Add test file

---
+++ shell/platform/darwin/ios/framework/Source/FlutterTextEditingDeltaTest.mm
@@ -0,0 +1,84 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "flutter/shell/platform/darwin/ios/framework/Source/FlutterTextEditingDelta.h"
+
+#import <OCMock/OCMock.h>
+#import <XCTest/XCTest.h>
+
+#import "flutter/shell/platform/darwin/common/framework/Headers/FlutterMacros.h"
+#import "flutter/shell/platform/darwin/ios/framework/Headers/FlutterEngine.h"
+
+@interface FlutterTextEditingDeltaTest : XCTestCase
+@end
+
+@implementation FlutterEditingDeltaTest {
+}
+
+- (NSRange)clampSelection:(NSRange)range forText:(NSString*)text {
+  int start = MIN(MAX(range.location, 0), text.length);
+  int length = MIN(range.length, text.length - start);
+  return NSMakeRange(start, length);
+}
+
+#pragma mark - TextEditingDelta Insertion tests
+- (void)testInsertionTextEditingDeltaAtEndOfComposing {
+  // Here we are simulating inserting an "o" at the end of "hell".
+  NSString* oldText = @"hell";
+  NSString* textAfterChange = @"hello";
+  NSString* replacementText = textAfterChange;
+  NSRange range = NSMakeRange(0, 4);
+
+  FlutterTextEditingDelta* delta = [[FlutterTextEditingDelta alloc]
+      initTextEditingDelta:oldText
+           textAfterChange:textAfterChange
+             replacedRange:[self clampSelection:range forText:oldText]
+               updatedText:replacementText];
+
+  XCTAssertEqual(delta.oldText, oldText);
+  XCTAssertEqual(delta.deltaText, @"o");
+  XCTAssertEqual(delta.deltaType, @"TextEditingDeltaType.insertion");
+  XCTAssertEqual(delta.deltaStart, 4);
+  XCTAssertEqual(delta.deltaEnd, 4);
+}
+
+- (void)testInsertionTextEditingDeltaInsideOfComposing() {
+
+}
+
+#pragma mark - TextEditingDelta Deletion tests
+
+- (void)testDeletionTextEditingDeltaAtEndOfComposing() {
+
+}
+
+- (void)testDeletionTextEditingDeltaInsideOfComposing() {
+
+}
+
+- (void)testDeletionTextEditingDeltaForSelection() {
+
+}
+
+#pragma mark - TextEditingDelta Replacement tests
+
+- (void)testReplacementSameTextEditingDelta() {
+
+}
+
+- (void)testReplacementLongerTextEditingDelta() {
+
+}
+
+- (void)testReplacementShorterTextEditingDelta() {
+
+}
+
+#pragma mark - TextEditingDelta Equality tests
+
+- (void)testEqualityTextEditingDelta() {
+
+}
+
+@end
