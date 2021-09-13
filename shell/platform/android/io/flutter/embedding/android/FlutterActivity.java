2ee3a625f967e4dec54cf6a1570d0fd326e129d4
Emmanuel Garcia <egarciad@google.com>
2021-09-13 14:24:11 -0700

    Comments

--- shell/platform/android/io/flutter/embedding/android/FlutterActivity.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterActivity.java
@@ -1094,9 +1094,9 @@ public class FlutterActivity extends Activity
   public void onFlutterUiDisplayed() {
     // Notifies Android that we're fully drawn so that performance metrics can be collected by
     // Flutter performance tests. A few considerations:
-    // * rFD was supported in KitKat (API 19), but has a bug around requiring
+    // * reportFullyDrawn was supported in KitKat (API 19), but has a bug around requiring
     // permissions in some Android versions.
-    // * rFD behavior isn't tested on pre-Q versions.
+    // * reportFullyDrawn behavior isn't tested on pre-Q versions.
     // See https://github.com/flutter/flutter/issues/46172, and
     // https://github.com/flutter/flutter/issues/88767.
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
--- shell/platform/android/test/io/flutter/embedding/android/FlutterActivityTest.java
+++ shell/platform/android/test/io/flutter/embedding/android/FlutterActivityTest.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
