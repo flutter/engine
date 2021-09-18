615ec40740508657809468654eda4c0eac4807f9
Alexander Biggs <akbiggs@google.com>
2021-09-18 15:22:26 +0000

    Revert "fuchsia: Use buffer_collection_x extension (#28683)"
    This reverts commit 47b4527833347c4c8690b2ab72dbccdadeb5348b.

--- shell/platform/fuchsia/flutter/vulkan_surface.cc
+++ shell/platform/fuchsia/flutter/vulkan_surface.cc
@@ -45,7 +45,7 @@ bool VulkanSurface::CreateVulkanImage(vulkan::VulkanProvider& vulkan_provider,
   FML_CHECK(out_vulkan_image != nullptr);
 
   out_vulkan_image->vk_collection_image_create_info = {
-      .sType = VK_STRUCTURE_TYPE_BUFFER_COLLECTION_IMAGE_CREATE_INFO_FUCHSIAX,
+      .sType = VK_STRUCTURE_TYPE_BUFFER_COLLECTION_IMAGE_CREATE_INFO_FUCHSIA,
       .pNext = nullptr,
       .collection = collection_,
       .index = 0,
@@ -71,7 +71,7 @@ bool VulkanSurface::CreateVulkanImage(vulkan::VulkanProvider& vulkan_provider,
   };
 
   if (VK_CALL_LOG_ERROR(
-          vulkan_provider.vk().SetBufferCollectionConstraintsFUCHSIAX(
+          vulkan_provider.vk().SetBufferCollectionConstraintsFUCHSIA(
               vulkan_provider.vk_device(), collection_,
               &out_vulkan_image->vk_image_create_info)) != VK_SUCCESS) {
     return false;
@@ -186,11 +186,13 @@ vulkan::VulkanHandle<VkSemaphore> VulkanSurface::SemaphoreFromEvent(
   }
 
   VkImportSemaphoreZirconHandleInfoFUCHSIA import_info = {
-      .sType = VK_STRUCTURE_TYPE_IMPORT_SEMAPHORE_ZIRCON_HANDLE_INFO_FUCHSIA,
+      .sType =
+          VK_STRUCTURE_TYPE_TEMP_IMPORT_SEMAPHORE_ZIRCON_HANDLE_INFO_FUCHSIA,
       .pNext = nullptr,
       .semaphore = semaphore,
-      .handleType = VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_ZIRCON_EVENT_BIT_FUCHSIA,
-      .zirconHandle = static_cast<uint32_t>(semaphore_event.release())};
+      .handleType =
+          VK_EXTERNAL_SEMAPHORE_HANDLE_TYPE_TEMP_ZIRCON_EVENT_BIT_FUCHSIA,
+      .handle = static_cast<uint32_t>(semaphore_event.release())};
 
   result = VK_CALL_LOG_ERROR(
       vulkan_provider_.vk().ImportSemaphoreZirconHandleFUCHSIA(
@@ -278,21 +280,18 @@ bool VulkanSurface::AllocateDeviceMemory(
         });
   }
 
-  VkBufferCollectionCreateInfoFUCHSIAX import_info{
-      .sType = VK_STRUCTURE_TYPE_BUFFER_COLLECTION_CREATE_INFO_FUCHSIAX,
-      .pNext = nullptr,
-      .collectionToken = vulkan_token.Unbind().TakeChannel().release(),
-  };
-  VkBufferCollectionFUCHSIAX collection;
-  if (VK_CALL_LOG_ERROR(vulkan_provider_.vk().CreateBufferCollectionFUCHSIAX(
+  VkBufferCollectionCreateInfoFUCHSIA import_info;
+  import_info.collectionToken = vulkan_token.Unbind().TakeChannel().release();
+  VkBufferCollectionFUCHSIA collection;
+  if (VK_CALL_LOG_ERROR(vulkan_provider_.vk().CreateBufferCollectionFUCHSIA(
           vulkan_provider_.vk_device(), &import_info, nullptr, &collection)) !=
       VK_SUCCESS) {
     return false;
   }
 
   collection_ = {collection, [&vulkan_provider = vulkan_provider_](
-                                 VkBufferCollectionFUCHSIAX collection) {
-                   vulkan_provider.vk().DestroyBufferCollectionFUCHSIAX(
+                                 VkBufferCollectionFUCHSIA collection) {
+                   vulkan_provider.vk().DestroyBufferCollectionFUCHSIA(
                        vulkan_provider.vk_device(), collection, nullptr);
                  }};
 
@@ -305,17 +304,17 @@ bool VulkanSurface::AllocateDeviceMemory(
       vulkan_image_.vk_memory_requirements;
   VkImageCreateInfo& image_create_info = vulkan_image_.vk_image_create_info;
 
-  VkBufferCollectionPropertiesFUCHSIAX properties{
-      .sType = VK_STRUCTURE_TYPE_BUFFER_COLLECTION_PROPERTIES_FUCHSIAX};
+  VkBufferCollectionPropertiesFUCHSIA properties = {
+      .sType = VK_STRUCTURE_TYPE_BUFFER_COLLECTION_PROPERTIES_FUCHSIA};
   if (VK_CALL_LOG_ERROR(
-          vulkan_provider_.vk().GetBufferCollectionPropertiesFUCHSIAX(
+          vulkan_provider_.vk().GetBufferCollectionPropertiesFUCHSIA(
               vulkan_provider_.vk_device(), collection_, &properties)) !=
       VK_SUCCESS) {
     return false;
   }
 
-  VkImportMemoryBufferCollectionFUCHSIAX import_memory_info = {
-      .sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_BUFFER_COLLECTION_FUCHSIAX,
+  VkImportMemoryBufferCollectionFUCHSIA import_memory_info = {
+      .sType = VK_STRUCTURE_TYPE_IMPORT_MEMORY_BUFFER_COLLECTION_FUCHSIA,
       .pNext = nullptr,
       .collection = collection_,
       .index = 0,
