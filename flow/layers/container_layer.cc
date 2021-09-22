07cdb10dc6f2869eb734833ed7707d03398c5c9e
Matej Knopp <matej.knopp@gmail.com>
2021-09-22 13:02:56 +0200

    Do not evict culled-out layers from raster cache
    This should preserve current behavior - the lifespan of raster cache entries should be identical with and without partial repaint.

--- flow/layers/container_layer.cc
+++ flow/layers/container_layer.cc
@@ -171,6 +171,9 @@ void ContainerLayer::TryToPrepareRasterCache(PrerollContext* context,
       context->raster_cache &&
       SkRect::Intersects(context->cull_rect, layer->paint_bounds())) {
     context->raster_cache->Prepare(context, layer, matrix);
+  } else if (context->raster_cache) {
+    // Don't evict raster cache entry during partial repaint
+    context->raster_cache->Touch(layer, matrix);
   }
 }
 
