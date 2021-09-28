c0a1dbd8c1bd91bfbd33b8fe1444800ed0c13ebb
camsim99 <camillesimon90@gmail.com>
2021-09-28 11:57:47 -0700

    Added note and deprecation annotation for splash screen provider

--- shell/platform/android/io/flutter/embedding/android/SplashScreenProvider.java
+++ shell/platform/android/io/flutter/embedding/android/SplashScreenProvider.java
@@ -8,7 +8,14 @@ import androidx.annotation.Nullable;
 
 /**
  * Provides a {@link SplashScreen} to display while Flutter initializes and renders its first frame.
+ *
+ * <p>Please use the new Splash screen API available on Android S. On lower versions of Android,
+ * it's no longer necessary to display a splash screen to wait for the Flutter first frame, and thus,
+ * there is no longer a need to provide an implementation of this interface.
+ *
+ * @deprecated
  */
+@Deprecated
 public interface SplashScreenProvider {
   /**
    * Provides a {@link SplashScreen} to display while Flutter initializes and renders its first
