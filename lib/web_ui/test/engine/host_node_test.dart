8b7fefc837f013e4ca443fe3f7d8d9e6cbc9d1ae
Mouad Debbar <mdebbar@google.com>
2021-10-19 14:55:50 -0400

    [web] Fix keyboard popping up unexpectedly on iOS

--- lib/web_ui/test/engine/host_node_test.dart
+++ lib/web_ui/test/engine/host_node_test.dart
@@ -23,6 +23,10 @@ void testMain() {
       expect(hostNode.node, isA<html.ShadowRoot>());
       expect((hostNode.node as html.ShadowRoot).host, rootNode);
       expect(hostNode.node, rootNode.shadowRoot);
+
+      // The shadow root should be initialized with correct parameters.
+      expect(rootNode.shadowRoot!.mode, 'open');
+      expect(rootNode.shadowRoot!.delegatesFocus, isFalse);
     });
 
     test('Attaches a stylesheet to the shadow root', () {
