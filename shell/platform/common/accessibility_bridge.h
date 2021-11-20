fdb8e41c1dfecc3f4a285b4a343c152a7e3e146b
Chris Bracken <chris@bracken.jp>
2021-11-19 17:07:35 -0800

    Extract AccessibilityBridge::kRootNodeId
    The ID of the root semantics node in Flutter's semantics tree is always
    0. Since we'll be adding support for Windows, extract this constant to a
    common location.

--- shell/platform/common/accessibility_bridge.h
+++ shell/platform/common/accessibility_bridge.h
@@ -100,6 +100,7 @@ class AccessibilityBridge
     virtual std::shared_ptr<FlutterPlatformNodeDelegate>
     CreateFlutterPlatformNodeDelegate() = 0;
   };
+
   //-----------------------------------------------------------------------------
   /// @brief      Creates a new instance of a accessibility bridge.
   ///
@@ -108,6 +109,11 @@ class AccessibilityBridge
   ///                                 through GetUserData().
   AccessibilityBridge(std::unique_ptr<AccessibilityBridgeDelegate> delegate);
   ~AccessibilityBridge();
+
+  //-----------------------------------------------------------------------------
+  /// @brief      The ID of the root node in the accessibility tree. In Flutter,
+  //              this is always 0.
+  static constexpr int32_t kRootNodeId = 0;
 
   //------------------------------------------------------------------------------
   /// @brief      Adds a semantics node update to the pending semantics update.
