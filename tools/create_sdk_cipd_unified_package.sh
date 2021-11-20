c13d7429b18f7eff685094aafe881c77c41d0f6d
Gary Qian <garyq@google.com>
2021-11-19 14:00:28 -1000

    More robust script

--- tools/create_sdk_cipd_unified_package.sh
+++ tools/create_sdk_cipd_unified_package.sh
@@ -8,16 +8,17 @@ print_usage () {
   echo "    - PATH_TO_SDK_DIR is the path to the sdk folder"
   echo "    - VERSION_TAG is the version of the package, e.g. 28r6 or 31v1"
   echo ""
-  echo "This script downloads the specified packages and uploads them to CIPD for linux, mac, and windows."
-  echo "You may need to accept licences during the download process."
+  echo "This script downloads the packages specified in android_sdk_packages.txt and uploads"
+  echo "them to CIPD for linux, mac, and windows."
   echo ""
   echo "Manage the packages to download in 'android_sdk_packages.txt'. You can use"
-  echo "'sdkmanager --list --include_obsolete' in cmdline-tools to list all available packages"
+  echo "'sdkmanager --list --include_obsolete' in cmdline-tools to list all available packages."
+  echo "Packages should be listed in the format of <package-name>:<directory-to-upload>."
+  echo "For example, build-tools;31.0.0:build-tools"
+  echo "Multiple directories to upload can be specified by delimiting by additional ':'"
   echo ""
-  echo "This script expects sdkmanager to be available at '<PATH_TO_SDK_DIR>/cmdline-tools/latest/bin'"
+  echo "This script expects the cmdline-tools to be installed in your specified PATH_TO_SDK_DIR"
   echo "and should only be run on linux or macos hosts."
-  echo ""
-  echo "For more see: https://developer.android.com/studio/command-line/sdkmanager"
 }
 
 # Validate version is provided
@@ -39,29 +40,46 @@ if [[ ! -d "$1/cmdline-tools" ]]; then
 fi
 
 platforms=("linux" "macosx" "windows")
-upload_dirs=("platform-tools" "build-tools" "platforms" "tools" "cmdline-tools")
+
+# Find the sdkmanager in cmdline-tools. We default to using latest if available.
 sdkmanager_path="$1/cmdline-tools/latest/bin/sdkmanager"
-temp_dir="$1/temp"
+find_results=()
+while IFS= read -r line; do
+  find_results+=("$line")
+done < <(find "$1/cmdline-tools" -name sdkmanager)
+i=0
+while [ ! -f "$sdkmanager_path" ]; do
+  if [ $i -ge ${#find_results[@]} ]; then
+    echo "Unable to find sdkmanager in the SDK directory. Please ensure cmdline-tools is installed."
+    exit 1
+  fi
+  sdkmanager_path="${find_results[$i]}"
+  echo $sdkmanager_path
+  ((i++))
+done
+
+temp_dir="$1/temp_cipd_android_sdk"
+rm -rf $temp_dir
 mkdir $temp_dir
 
 for platform in "${platforms[@]}"; do
   sdk_root="$temp_dir/sdk_$platform"
+  upload_dir="$sdk_root/upload"
   echo "Creating temporary working directory for $platform: $sdk_root"
   mkdir $sdk_root
+  mkdir $upload_dir
 
   # Download all the packages with sdkmanager.
   for package in $(cat android_sdk_packages.txt); do
-    echo "Installing $package"
-    REPO_OS_OVERRIDE=$platform $sdkmanager_path --sdk_root=$sdk_root $package
-  done
-
-  # We copy only the relevant directories to a temporary dir
-  # for upload. sdkmanager creates extra files that we don't need.
-  echo "Consolidating files for upload"
-  upload_dir="$sdk_root/upload"
-  mkdir $upload_dir
-  for dir in "${upload_dirs[@]}"; do
-    cp -r "$sdk_root/$dir" "$upload_dir"
+    split=(${package//:/ })
+    echo "Installing ${split[0]}"
+    REPO_OS_OVERRIDE=$platform yes "y" | $sdkmanager_path --sdk_root=$sdk_root ${split[0]}
+    # We copy only the relevant directories to a temporary dir
+    # for upload. sdkmanager creates extra files that we don't need.
+    array_length=${#split[@]}
+    for (( i=1; i<${array_length}; i++ )); do
+      cp -r "$sdk_root/${split[$i]}" "$upload_dir"
+    done
   done
 
   # Mac uses a different sdkmanager name than the platform name used in gn.
@@ -70,6 +88,7 @@ for platform in "${platforms[@]}"; do
     cipd_name="mac-amd64"
   fi
   echo "Uploading $cipd_name to CIPD"
-  cipd create -in $upload_dir -name "flutter/android/sdk/all/$cipd_name" -install-mode copy -tag version:$2
+  # cipd create -in $upload_dir -name "flutter/android/sdk/all/$cipd_name" -install-mode copy -tag version:$2
   rm -rf $sdk_root
 done
+rm -rf $temp_dir
