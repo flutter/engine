2e7a148beaa8abb55a3ca963184a21ba7bf17df5
Godofredo Contreras <godofredoc@google.com>
2021-12-10 08:16:48 -0800

    Support presubmit and postsubmit cases.

--- tools/configure_framework_commit.sh
+++ tools/configure_framework_commit.sh
@@ -15,15 +15,6 @@ cd $ENGINE_PATH/src/flutter
 ENGINE_COMMIT=`git rev-parse HEAD`
 echo "Using engine commit: $ENGINE_COMMIT"
 
-#if [[ $GIT_BRANCH =~ ^flutter-.*-candidate.*$ ]]
-#then
-#  # $GIT_BRANCH exists this is comming from presubmit.
-#  RELEASE_BRANCH="remotes/upstream/$GIT_BRANCH"
-#else
-#  # Try to get it from the checkout. 
-#  RELEASE_BRANCH=`git branch -a --contains $ENGINE_COMMIT | grep 'flutter-.*-candidate.*' || true`
-#fi
-
 if [[ -z $FLUTTER_CLONE_REPO_PATH ]]
 then
   echo "Please set FLUTTER_CLONE_REPO_PATH environment variable."
@@ -33,19 +24,26 @@ else
 fi
 
 if [[ $GIT_BRANCH =~ ^flutter-.*-candidate.*$ ]]
-#if [[ -z $RELEASE_BRANCH ]]
 then
+  # Coming from presubmit and assuming the correct branch has been already checked out.
   COMMIT_NO==`git rev-parse HEAD`
 else
-  # If this is not a release branch commit get latest commit's time for the engine repo.
-  # Use date based on local time otherwise timezones might get mixed.
-  LATEST_COMMIT_TIME_ENGINE=`git log -1 --date=local --format="%cd"`
-  echo "Latest commit time on engine found as $LATEST_COMMIT_TIME_ENGINE"
+  # Try to get release branch from the checkout. 
+  RELEASE_BRANCH=`git branch -a --contains $ENGINE_COMMIT | grep 'flutter-.*-candidate.*' || true`
+  if [[ -z $RELEASE_BRANCH ]]
+  then
+    # If this is not a release branch commit get latest commit's time for the engine repo.
+    # Use date based on local time otherwise timezones might get mixed.
+    LATEST_COMMIT_TIME_ENGINE=`git log -1 --date=local --format="%cd"`
+    echo "Latest commit time on engine found as $LATEST_COMMIT_TIME_ENGINE"
 
-  # Get the time of the youngest commit older than engine commit.
-  # Git log uses commit date not the author date.
-  # Before makes the comparison considering the timezone as well.
-  COMMIT_NO=`git log --before="$LATEST_COMMIT_TIME_ENGINE" -n 1 | grep commit | cut -d ' ' -f2`
+    # Get the time of the youngest commit older than engine commit.
+    # Git log uses commit date not the author date.
+    # Before makes the comparison considering the timezone as well.
+    COMMIT_NO=`git log --before="$LATEST_COMMIT_TIME_ENGINE" -n 1 | grep commit | cut -d ' ' -f2`
+  else
+    COMMIT_NO=`git rev-parse $RELEASE_BRANCH`
+  fi
 fi
 #else
 #  # If this is a release branch use the ToT commit of the same release branch as the engine.
