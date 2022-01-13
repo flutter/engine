dbfabbf777bc02431c8dabde0d77b219ee48e46e
Kaushik Iska <iska.kaushik@gmail.com>
2022-01-13 08:45:42 -0800

    [macos] Wireup platform views on macOS
    * The plan is to only support platform views when using Metal rendering backend.
    * This PR is based on the work done in: https://github.com/flutter/engine/pull/22905
    * There are some threading issues to be addressed still to make this feature usable: https://github.com/flutter/flutter/issues/96668
    * Example is at: https://github.com/iskakaushik/flutter_macos_platform_view_example

---
+++ shell/platform/darwin/macos/framework/Source/TestFlutterPlatformView.mm
@@ -0,0 +1,30 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
+
+#import "flutter/shell/platform/darwin/macos/framework/Source/TestFlutterPlatformView.h"
+
+@implementation TestFlutterPlatformView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+  self = [super initWithFrame:frame];
+  return self;
+}
+
+@end
+
+@implementation TestFlutterPlatformViewFactory
+- (NSView*)createWithFrame:(CGRect)frame
+            viewIdentifier:(int64_t)viewId
+                 arguments:(nullable id)args {
+  return [[TestFlutterPlatformView alloc] initWithFrame:frame];
+}
+
+- (NSObject<FlutterMessageCodec>*)createArgsCodec {
+  return [FlutterStandardMessageCodec sharedInstance];
+}
+
+@end
