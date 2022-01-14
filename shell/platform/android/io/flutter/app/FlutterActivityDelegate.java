03c7cdf6968e6540e5b878f3b6088a483c0f94af
camsim99 <camillesimon90@gmail.com>
2022-01-14 13:23:34 -0800

    Formatting

--- shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
+++ shell/platform/android/io/flutter/app/FlutterActivityDelegate.java
@@ -24,7 +24,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager.LayoutParams;
-import androidx.core.view.WindowCompat;
 import io.flutter.Log;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.util.Preconditions;
