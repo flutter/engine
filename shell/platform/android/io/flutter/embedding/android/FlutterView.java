a1f1934cc91144915ffc3ff923b8f5a875fa724c
Rulong Chen（陈汝龙） <rulong.crl@alibaba-inc.com>
2022-01-19 11:28:41 +0800

    Modify comments.

--- shell/platform/android/io/flutter/embedding/android/FlutterView.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterView.java
@@ -1227,7 +1227,8 @@ public class FlutterView extends FrameLayout implements MouseCursorPlugin.MouseC
     if (flutterImageView != null) {
       flutterImageView.closeImageReader();
       // Remove the FlutterImageView that was previously added by {@code convertToImageView} to
-      // avoid leaks when this FlutterView is reused later.
+      // avoid leaks when this FlutterView is reused later in the scenario where multiple
+      // FlutterActivitiy/FlutterFragment share one engine.
       removeView(flutterImageView);
       flutterImageView = null;
     }
