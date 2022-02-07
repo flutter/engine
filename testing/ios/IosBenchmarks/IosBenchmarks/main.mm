b1c7064aefdff9f9a04bbb51152d94af4a98f751
George Wright <gw280@google.com>
2022-02-07 15:13:45 -0800

    Delete AppDelegate/SceneDelegate/ViewController

--- testing/ios/IosBenchmarks/IosBenchmarks/main.mm
+++ testing/ios/IosBenchmarks/IosBenchmarks/main.mm
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
 extern "C" {
 int RunBenchmarks(int argc, char** argv);
 }
