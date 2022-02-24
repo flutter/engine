e297b2a719ece2b5ce18a0df87f68345807b95bd
Kevin Lubick <kjlubick@google.com>
2022-02-24 10:35:22 -0500

    format

--- display_list/display_list_benchmarks_canvas_provider.h
+++ display_list/display_list_benchmarks_canvas_provider.h
@@ -8,8 +8,8 @@
 #include "flutter/fml/mapping.h"
 #include "flutter/testing/testing.h"
 
-#include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/core/SkData.h"
+#include "third_party/skia/include/core/SkSurface.h"
 
 namespace flutter {
 namespace testing {
