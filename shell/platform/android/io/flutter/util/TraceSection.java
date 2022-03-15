c3b400338e368be913537da7535ec9f04babfb86
Dan Field <dnfield@google.com>
2022-03-15 14:20:50 -0700

    missing files

--- shell/platform/android/io/flutter/util/TraceSection.java
+++ shell/platform/android/io/flutter/util/TraceSection.java
@@ -8,9 +8,8 @@ import androidx.tracing.Trace;
 
 public final class TraceSection implements AutoCloseable {
   /**
-   * Wraps Trace.beginSection to ensure that the line length stays below 127
-   * code units, and automatically closes the section in a try-with-resources
-   * block.
+   * Wraps Trace.beginSection to ensure that the line length stays below 127 code units, and
+   * automatically closes the section in a try-with-resources block.
    *
    * @param sectionName The string to display as the section name in the trace.
    */
@@ -22,7 +21,7 @@ public final class TraceSection implements AutoCloseable {
   /**
    * Wraps Trace.endSection.
    *
-   * This method exists solely to avoid problematic usage of Trace.beginSection.
+   * <p>This method exists solely to avoid problematic usage of Trace.beginSection.
    */
   public void close() throws Exception {
     Trace.endSection();
