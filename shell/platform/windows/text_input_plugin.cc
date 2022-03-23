e786a8fda4bf400ca0dda7214cc3fe0be9afa0b3
Renzo Olivares <roliv@google.com>
2022-03-23 10:59:01 -0700

    updates

--- shell/platform/windows/text_input_plugin.cc
+++ shell/platform/windows/text_input_plugin.cc
@@ -448,7 +448,7 @@ void TextInputPlugin::EnterPressed(TextInputModel* model) {
   if (input_type_ == kMultilineInputType) {
     std::u16string text_before_change = fml::Utf8ToUtf16(model->GetText());
     TextRange selection_before_change = model->selection();
-    model->AddText(std::u16string({u'\n'}));
+    model->AddText(u"\n");
     if (enable_delta_model) {
       TextEditingDelta delta(text_before_change, selection_before_change,
                              u"\n");
