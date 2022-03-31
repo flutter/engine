054d67247a1f63c840c5e6b2fb9dd027dedd4156
Tong Mu <dkwingsmt@gmail.com>
2022-03-30 17:01:03 -0700

    Fix problem

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -139,9 +139,7 @@ def RunEngineExecutable(build_dir, executable_name, filter, flags=[],
   if not env:
     env = os.environ.copy()
   env['FLUTTER_BUILD_DIRECTORY'] = build_dir
-  for key, value in extra_env.items():
-    env[key] = value
-  print(env)
+  env |= extra_env
 
   try:
     RunCmd(test_command, cwd=cwd, forbidden_output=forbidden_output, expect_failure=expect_failure, env=env)
