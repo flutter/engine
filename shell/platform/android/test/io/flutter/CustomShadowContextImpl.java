49583d45e940e062d94a1f90b68707064260a11d
camsim99 <camillesimon90@gmail.com>
2022-04-13 15:22:13 -0700

    Formatting

--- shell/platform/android/test/io/flutter/CustomShadowContextImpl.java
+++ shell/platform/android/test/io/flutter/CustomShadowContextImpl.java
@@ -3,22 +3,18 @@ package io.flutter;
 import android.content.Context;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
-import org.robolectric.annotation.RealObject;
-import org.robolectric.shadow.api.Shadow;
 import org.robolectric.shadows.ShadowContextImpl;
 
-
 @Implements(className = ShadowContextImpl.CLASS_NAME)
-public class CustomShadowContextImpl extends ShadowContextImpl{
+public class CustomShadowContextImpl extends ShadowContextImpl {
+  public static final String CLASS_NAME = "android.app.ContextImpl";
 
-    public static final String CLASS_NAME = "android.app.ContextImpl";
-
-    @Implementation
-    @Override
-    public final Object getSystemService (String name) {
-        if (name == Context.TEXT_SERVICES_MANAGER_SERVICE) {
-            return null;
-        }
-        return super.getSystemService(name);
+  @Implementation
+  @Override
+  public final Object getSystemService(String name) {
+    if (name == Context.TEXT_SERVICES_MANAGER_SERVICE) {
+      return null;
     }
+    return super.getSystemService(name);
+  }
 }
