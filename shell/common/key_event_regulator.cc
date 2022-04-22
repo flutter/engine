fbd8d2bfd7a8636a797b872315b05521bfd9f495
Tong Mu <dkwingsmt@gmail.com>
2022-04-22 13:09:09 -0700

    Compilable

--- shell/common/key_event_regulator.cc
+++ shell/common/key_event_regulator.cc
@@ -4,65 +4,325 @@
 
 #include "flutter/shell/common/key_event_regulator.h"
 
+#include <set>
 #include "flutter/fml/logging.h"
 
 namespace flutter {
 
 namespace {
-typedef KeyEventRegulator::KeyPhase KeyPhase;
+typedef KeyEventRegulator::Phase Phase;
+typedef KeyEventRegulator::Type EventType;
 
-bool IsValidPhase(KeyPhase phase) {
-  return phase >= KeyEventRegulator::kReleasedDisabled &&
-         phase <= KeyEventRegulator::kPressed;
+bool IsSynonymKey(uint64_t key) {
+  constexpr uint64_t kPlaneMask = 0xFF00000000;
+  return (key & kPlaneMask) == KeyEventRegulator::kSynonymPlane;
 }
 
-bool IsPressed(KeyPhase phase) {
-  FML_CHECK(IsValidPhase(phase));
-  return phase & 0x01;
-}
-
-KeyPhase OverridePhase(KeyPhase base, KeyPhase new_value) {
-  FML_CHECK(IsValidPhase(base));
-  FML_CHECK(IsValidPhase(new_value));
-  switch (base) {
-    case KeyEventRegulator::kReleasedDisabled:
-    case KeyEventRegulator::kReleasedEnabled:
-      if (new_value == KeyEventRegulator::kReleased) {
-        return base;
-      }
-      break;
-    case KeyEventRegulator::kPressedDisabled:
-    case KeyEventRegulator::kPressedEnabled:
-      if (new_value == KeyEventRegulator::kPressed) {
-        return base;
-      }
-      break;
+template <class T>
+bool NoneIsWithin(const std::vector<uint64_t>& none_of,
+                  const std::map<uint64_t, T>& is_within) {
+  for (uint64_t testee : none_of) {
+    if (is_within.find(testee) != is_within.end()) {
+      return false;
+    }
   }
-  return new_value;
+  return true;
+}
+
+KeyEventRegulator::Phase StateToPhase(bool pressed, bool toggled) {
+  return ((pressed ? 0 : 1) + (toggled ? 1 : 3)) % KeyEventRegulator::kMaxPhase;
+}
+
+bool OfCompatibleTypes(EventType input_type, EventType event_type) {
+  switch (input_type) {
+    case EventType::kDown:
+    case EventType::kRepeat:
+      return event_type == EventType::kDown || event_type == EventType::kRepeat;
+    case EventType::kUp:
+      return event_type == EventType::kUp;
+    case EventType::kUnknown:
+      return event_type != EventType::kRepeat;
+  }
 }
 
 } // namespace
 
-KeyEventRegulator::KeyEventRegulator(Config config) {
+KeyEventRegulator::KeyEventRegulator(Config config)
+    : on_duplicate_down(config.on_duplicate_down),
+      on_abrupt_up(config.on_abrupt_up) {
+  for (uint64_t toggling_key : config.toggling_keys) {
+    FML_CHECK(!IsSynonymKey(toggling_key));
+    key_states[toggling_key] = KeyState{.is_toggling = true};
+  }
+  for (const SynonymKey& synonym_key : config.synonym_keys) {
+    FML_CHECK(synonym_key.concrete_keys.size() > 1);
+    // Ensure that no concrete key is toggling, since only toggling keys are
+    // initialized at this moment.
+    FML_CHECK(NoneIsWithin(synonym_key.concrete_keys, key_states));
+    synonym_keys[synonym_key.id] = synonym_key.concrete_keys;
+  }
 }
 
 std::vector<KeyEventRegulator::OutputEvent> KeyEventRegulator::SettleByEvent(InputEvent input) {
+
+  // Resolve pending state of synonym keys' into concrete keys'.
+  for (const auto& pending_item : pending_synonym_pressing) {
+    // `pending_item.first` is guaranteed by ReportPressed a valid toggling key.
+    const auto& concrete_keys = synonym_keys[pending_item.first];
+    bool should_be_pressed = pending_item.second;
+    if (should_be_pressed) {
+      // The key should be pressed. If any concrete key is pressed, no changes
+      // are needed. Otherwise, set the first concrete key as pressed.
+      bool any_pressed = false;
+      for (uint64_t concrete_key : concrete_keys) {
+        auto found = key_states.find(concrete_key);
+        bool overall_pressed;
+        if (found == key_states.end()) {
+          overall_pressed = false;
+        } else {
+          const auto& key_state = found->second;
+          FML_CHECK(!key_state.is_toggling);
+          overall_pressed =
+              key_state.dirty ? key_state.pending.pressed : key_state.pressed;
+        }
+        if (overall_pressed) {
+          any_pressed = true;
+          break;
+        }
+      }
+      if (!any_pressed) {
+        ReportPressed(concrete_keys[0], true);
+      }
+    } else {
+      // The key should not be pressed, so set all concrete states to false.
+      for (uint64_t concrete_key : concrete_keys) {
+        ReportPressed(concrete_key, false);
+      }
+    }
+  }
+  pending_synonym_pressing.clear();
+
+  // Always mark the input key as dirty.
+  MarkAsDirty(key_states.try_emplace(input.logical_key, KeyState{}).first);
+
   std::vector<OutputEvent> synthesized_outputs;
   std::vector<OutputEvent> main_outputs;
 
-  KeyPhase input_phase = StateFor(input.logical_key);
-  if (input.type == Type::kDown && IsPressed(input_phase)) {
+  // Synthesize events for each key.
+  for (const auto& pending_item : dirty_keys) {
+    uint64_t key = pending_item.first;
+    KeyState& key_state = *pending_item.second;
+    bool is_input_key = key == input.logical_key;
 
+    std::vector<Type> following_events;
+    bool no_main_event = false;
+
+    if (is_input_key) {
+      key_state.last_seen_physical_key = input.physical_key;
+
+      // Ensure that the state matches the type of the input event.
+      switch(input.type) {
+        case kRepeat:
+        case kDown:
+          if (key_state.is_toggling) {
+            ReportPhase(key, {kPressedEnabled, kPressedDisabled});
+          } else {
+            ReportPressed(key, true);
+            // Resolve duplicate down.
+            if (key_state.pressed && input.type == kDown) {
+              switch(on_duplicate_down) {
+                case DuplicateDownBehavior::kSynthesizeRepeat:
+                  following_events.push_back(kRepeat);
+                  break;
+                case DuplicateDownBehavior::kSynthesizeUp:
+                  break;
+                case DuplicateDownBehavior::kIgnore:
+                  no_main_event = true;
+                  break;
+              }
+            }
+          }
+          break;
+        case kUp:
+          if (key_state.is_toggling) {
+            ReportPhase(key, {kReleasedEnabled, kReleasedDisabled});
+          } else {
+            ReportPressed(key, false);
+            // Resolve abrupt up.
+            if (!key_state.pressed) {
+              switch(on_abrupt_up) {
+                case AbruptUpBehavior::kIgnore:
+                  no_main_event = true;
+                  break;
+                case AbruptUpBehavior::kSynthesizeDown:
+                  break;
+              }
+            }
+          }
+          break;
+        case kUnknown:
+          no_main_event = true;
+          break;
+      }
+    }
+
+    // Synthesize events that lead from the current to the target state.
+    if (key_state.is_toggling) {
+      TransitToggling(following_events, key_state, key_state.pending.toggled);
+    } else {
+      TransitPressing(following_events, key_state, key_state.pending.pressed);
+    }
+
+    // Ensure that the input key always has some events by pushing a full cycle.
+    if (is_input_key && !no_main_event && following_events.size() == 0) {
+      Type first_type = key_state.pressed ? kUp : kDown;
+      Type second_type = key_state.pressed == kUp ? kDown : kUp;
+      following_events.push_back(first_type);
+      following_events.push_back(second_type);
+      if (key_state.is_toggling) {
+        following_events.push_back(first_type);
+        following_events.push_back(second_type);
+      }
+    }
+
+    FML_CHECK(StateTransitionIsRegular(key_state, following_events));
+
+    // Allow the only down event to be a repeat.
+    if (following_events.size() == 1 && input.type == kRepeat) {
+      following_events[0] = kRepeat;
+    }
+
+    uint64_t physical_key = key_state.pressed
+                                ? key_state.pressed_physical_key
+                                : key_state.last_seen_physical_key;
+     // Generate events.
+    for (Type type : following_events) {
+      synthesized_outputs.push_back(OutputEvent{
+        .type = type,
+        .physical_key = physical_key,
+        .logical_key = key,
+        .synthesized = true,
+      });
+      physical_key = key_state.last_seen_physical_key;
+      key_state.ForwardPhase();
+    }
+    key_state.dirty = false;
+    key_state.pressed_physical_key = physical_key;
+    // Possibly change the last event to be a main event.
+    if (is_input_key && !no_main_event) {
+      main_outputs.push_back(synthesized_outputs.back());
+      synthesized_outputs.pop_back();
+      main_outputs.back().synthesized = false;
+      FML_CHECK(OfCompatibleTypes(main_outputs.back().type, input.type));
+    }
   }
-  return {};
+
+  synthesized_outputs.insert(synthesized_outputs.end(), main_outputs.begin(), main_outputs.end());
+  dirty_keys.clear();
+
+  return synthesized_outputs;
 }
 
-KeyEventRegulator::KeyPhase KeyEventRegulator::StateFor(uint64_t key) {
-  auto found = key_states.find(key);
-  if (found == key_states.end()) {
-    return KeyEventRegulator::kReleased;
+void KeyEventRegulator::ReportPressed(uint64_t key, bool pressed) {
+  if (IsSynonymKey(key)) {
+    pending_synonym_pressing[key] = pressed;
+  } else {
+    auto found = key_states.try_emplace(key, KeyState{}).first;
+    FML_CHECK(!found->second.is_toggling);
+    found->second.pending.pressed = pressed;
+    MarkAsDirty(found);
   }
-  return found->second;
+}
+
+void KeyEventRegulator::ReportPhase(uint64_t key, std::vector<Phase> phases) {
+  FML_CHECK(!IsSynonymKey(key));
+  auto found = key_states.find(key);
+  FML_CHECK(found != key_states.end());
+  FML_CHECK(found->second.is_toggling);
+  found->second.pending.toggled = {false, false, false, false};
+  for (Phase phase : phases) {
+    found->second.pending.toggled[phase] = true;
+  }
+  MarkAsDirty(found);
+}
+
+void KeyEventRegulator::MarkAsDirty(std::map<uint64_t, KeyState>::iterator key_state_iter) {
+  if (!key_state_iter->second.dirty) {
+    key_state_iter->second.dirty = true;
+    dirty_keys[key_state_iter->first] = &key_state_iter->second;
+  }
+}
+
+void KeyEventRegulator::KeyState::ForwardPhase() {
+  if (is_toggling && !pressed) {
+    toggled = !toggled;
+  }
+  pressed = !pressed;
+}
+
+void KeyEventRegulator::TransitToggling(
+    std::vector<Type>& result,
+    const KeyState& key_state,
+    const std::array<bool, kMaxPhase>& allowed_phases) {
+  size_t from_phase = StateToPhase(key_state.pressed, key_state.toggled);
+  for (size_t elapsed = 0; elapsed < kMaxPhase; elapsed += 1) {
+    size_t current_phase = (from_phase + elapsed) % kMaxPhase;
+    if (allowed_phases[current_phase]) {
+      break;
+    }
+    result.push_back((current_phase == kReleasedDisabled ||
+                      current_phase == kReleasedEnabled)
+                         ? kDown
+                         : kUp);
+  }
+  FML_CHECK(result.size() < kMaxPhase);
+}
+
+void KeyEventRegulator::TransitPressing(
+    std::vector<Type>& result,
+    const KeyState& key_state,
+    bool pending_pressed) {
+  if (key_state.pressed && !pending_pressed) {
+    result.push_back(kUp);
+  } else if (!key_state.pressed && pending_pressed) {
+    result.push_back(kDown);
+  }
+}
+
+bool KeyEventRegulator::StateTransitionIsRegular(
+    const KeyState& state,
+    const std::vector<Type>& following_events) {
+  bool pressed = state.pressed;
+  bool toggled = state.toggled;
+  for (Type type : following_events) {
+    switch (type) {
+      case kUp:
+        if (!pressed) {
+          return false;
+        }
+        pressed = !pressed;
+        break;
+      case kDown:
+        if (pressed) {
+          return false;
+        }
+        toggled = !toggled;
+        pressed = !pressed;
+        break;
+      case kRepeat:
+        if (!pressed) {
+          return false;
+        }
+        break;
+      default:
+        return false;
+    }
+  }
+  if (state.is_toggling) {
+    return state.pending.toggled[StateToPhase(pressed, toggled)];
+  } else {
+    return state.pending.pressed == pressed;
+  }
 }
 
 
--- shell/common/key_event_regulator.h
+++ shell/common/key_event_regulator.h
@@ -5,6 +5,7 @@
 #ifndef COMMON_KEY_EVENT_REGULARIZER_H_
 #define COMMON_KEY_EVENT_REGULARIZER_H_
 
+#include <array>
 #include <cinttypes>
 #include <string>
 #include <vector>
@@ -16,15 +17,18 @@ namespace flutter {
 
 class KeyEventRegulator {
  public:
-  typedef uint8_t KeyPhase;
-  static constexpr KeyPhase kReleasedDisabled = 2;
-  static constexpr KeyPhase kPressedEnabled = 3;
-  static constexpr KeyPhase kReleasedEnabled = 4;
-  static constexpr KeyPhase kPressedDisabled = 5;
-  static constexpr KeyPhase kReleased = 6;
-  static constexpr KeyPhase kPressed = 7;
+  typedef uint8_t Phase;
 
-  enum class Type : uint8_t {
+  static constexpr bool kReleased = false;
+  static constexpr bool kPressed = true;
+  static constexpr Phase kReleasedDisabled = 0;
+  static constexpr Phase kPressedEnabled = 1;
+  static constexpr Phase kReleasedEnabled = 2;
+  static constexpr Phase kPressedDisabled = 3;
+
+  static constexpr size_t kMaxPhase = 4;
+
+  enum Type : uint8_t {
     kUnknown = 0,
     kUp,
     kDown,
@@ -36,23 +40,21 @@ class KeyEventRegulator {
   struct SynonymKey {
     // Should has kSynonymPlane as the high 32 bits.
     uint64_t id;
-    std::vector<uint64_t> logical_keys;
-    // Must be of the same length as `logical_keys`.
-    std::vector<uint64_t> default_physical_keys;
+    std::vector<uint64_t> concrete_keys;
+    // // Must be of the same length as `concrete_keys`.
+    // std::vector<uint64_t> default_physical_keys;
   };
 
   struct InputEvent {
     Type type;
     uint64_t physical_key;
     uint64_t logical_key;
-    std::string characters;
   };
 
   struct OutputEvent {
     Type type;
     uint64_t physical_key;
     uint64_t logical_key;
-    std::string characters;
     bool synthesized;
   };
 
@@ -74,25 +76,62 @@ class KeyEventRegulator {
 
     // Declare that multiple logical keys can be recognized as the same
     // goal.
+    //
+    // The concrete keys must not be toggling keys.
     std::vector<SynonymKey> synonym_keys;
 
     // Declare that a goal, whether a logical key or a synonym key, has a
     // toggling state, and therefore has 4 phases.
+    //
+    // Must not have synonym keys.
     std::vector<uint64_t> toggling_keys;
   };
 
   explicit KeyEventRegulator(Config config);
 
-  void ReportState(std::vector<std::pair<uint64_t, KeyPhase>> goal_states);
+  void ReportPressed(uint64_t key, bool pressed);
+  void ReportPhase(uint64_t key, std::vector<Phase> phase);
   std::vector<OutputEvent> SettleByEvent(InputEvent event);
   // std::vector<OutputEvent> Settle();
 
  private:
-  std::map<uint64_t, uint64_t> recorded_physical_keys;
-  std::map<uint64_t, KeyPhase> key_states;
-  std::map<uint64_t, KeyPhase> unsettled_states;
+  struct KeyState {
+    uint64_t pressed_physical_key;
+    uint64_t last_seen_physical_key;
+    bool is_toggling;
+    bool pressed;
+    bool toggled;
+    bool dirty;
 
-  KeyPhase StateFor(uint64_t key);
+    union {
+      bool pressed;
+      std::array<bool, kMaxPhase> toggled;
+    } pending;
+
+    void ForwardPhase();
+  };
+
+  DuplicateDownBehavior on_duplicate_down;
+  AbruptUpBehavior on_abrupt_up;
+
+  std::map<uint64_t, std::vector<uint64_t>> synonym_keys;
+  std::map<uint64_t, KeyState> key_states;
+
+  std::map<uint64_t, bool> pending_synonym_pressing;
+  std::map<uint64_t, KeyState*> dirty_keys;
+
+  void MarkAsDirty(std::map<uint64_t, KeyState>::iterator key_state_iter);
+
+  static bool StateTransitionIsRegular(
+      const KeyState& current_state,
+      const std::vector<Type>& following_events);
+  static void TransitToggling(
+      std::vector<Type>& result,
+      const KeyState& key_state,
+      const std::array<bool, kMaxPhase>& allowed_phases);
+  static void TransitPressing(std::vector<Type>& result,
+                              const KeyState& key_state,
+                              bool pending_pressed);
 
   FML_DISALLOW_COPY_AND_ASSIGN(KeyEventRegulator);
 };
