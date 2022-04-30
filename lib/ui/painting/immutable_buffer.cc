8d31015cf52cef4084362c0ff74c798faf3845c7
Jonah Williams <jonahwilliams@google.com>
2022-04-29 23:06:43 -0700

    something like this

--- lib/ui/painting/immutable_buffer.cc
+++ lib/ui/painting/immutable_buffer.cc
@@ -87,7 +87,9 @@ void ImmutableBuffer::initFromAsset(Dart_NativeArguments args) {
   const void* bytes = static_cast<const void*>(data->GetMapping());
   auto size = data->GetSize();
   void* peer = reinterpret_cast<void*>(data.release());
-  SkData::ReleaseProc proc = [](const void* ptr, void* context) {};
+  SkData::ReleaseProc proc = [](const void* ptr, void* context) {
+    auto* mapping = static_cast<fml::Mapping*>(const_cast<void*>(ptr));
+  };
 
   auto sk_data = SkData::MakeWithProc(bytes, size, proc, peer);
   auto buffer = fml::MakeRefCounted<ImmutableBuffer>(sk_data);
