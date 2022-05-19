8f3f1d3a088b393f39d82fbe8af2e4fe1959fb30
Jonah Williams <jonahwilliams@google.com>
2022-05-19 13:07:35 -0700

    use Future.microtask

--- lib/ui/painting.dart
+++ lib/ui/painting.dart
@@ -3910,11 +3910,10 @@ class FragmentProgram extends NativeFieldWrapperClass1 {
   static Future<FragmentProgram> compile({
     required ByteBuffer spirv,
     bool debugPrint = false,
-  }) async {
+  }) {
     // Delay compilation without creating a timer, which interacts poorly with the
     // flutter test framework. See: https://github.com/flutter/flutter/issues/104084
-    await null;
-    return FragmentProgram._(spirv: spirv, debugPrint: debugPrint);
+    return Future<FragmentProgram>.microtask(() => FragmentProgram._(spirv: spirv, debugPrint: debugPrint));
   }
 
   @pragma('vm:entry-point')
