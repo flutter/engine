ad77121bd8af9d90fce2c67a676d8fa2085be039
Joshua Litt <joshualitt@google.com>
2022-05-21 00:08:11 +0000

    5

--- lib/web_ui/lib/src/engine/html/bitmap_canvas.dart
+++ lib/web_ui/lib/src/engine/html/bitmap_canvas.dart
@@ -1078,7 +1078,6 @@ class BitmapCanvas extends EngineCanvas {
         _children.add(paintOrderElement as html.Element);
       }
     }
-    if (firstChild != null) {
       if (domInstanceOfString(firstChild, 'HTMLElement')) {
         final DomHTMLElement maybeCanvas = firstChild as DomHTMLElement;
         if (maybeCanvas.tagName.toLowerCase() == 'canvas') {
