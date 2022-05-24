301d4a6aac21691b1a60afd596b23eb2dbcf7552
Anthony Oleinik <antholeinik@gmail.com>
2022-05-24 10:35:30 -0700

    added comments to composition_aware_mixin

--- lib/web_ui/lib/src/engine/text_editing/composition_aware_mixin.dart
+++ lib/web_ui/lib/src/engine/text_editing/composition_aware_mixin.dart
@@ -2,15 +2,39 @@ import 'dart:html' as html;
 
 import 'text_editing.dart';
 
+
+/// Provides default functionality for listening to HTML composition events.
+///
+/// A class with this mixin generally calls [determineCompositionState] in order to update
+/// an [EditingState] with new composition values; namely, [EditingState.composingBaseOffset]
+/// and [EditingState.composingExtentOffset].
+///
+/// A class with this mixin should call [addCompositionEventHandlers] on initalization, and
+/// [removeCompositionEventHandlers] on deinitalization.
+///
+/// see also:
+///
+/// * [EditingState], the state of a text field that [CompositionAwareMixin] updates.
+/// * [DefaultTextEditingStrategy], the primary implementer of [CompositionAwareMixin].
 mixin CompositionAwareMixin {
+  /// the name of the HTML composition event type that triggers on starting a composition.
   static const String kCompositionStart = 'compositionstart';
+
+  /// the name of the HTML composition event type that triggers on updating a composition.
   static const String kCompositionUpdate = 'compositionupdate';
+
+  /// the name of the HTML composition event type that triggers on ending a composition.
   static const String kCompositionEnd = 'compositionend';
 
   late final html.EventListener _compositionStartListener = _handleCompositionStart;
   late final html.EventListener _compositionUpdateListener = _handleCompositionUpdate;
   late final html.EventListener _compositionEndListener = _handleCompositionEnd;
 
+  /// the currently composing text in the domElement.
+  ///
+  /// Will be null if composing just started, ended, or no composing is being done.
+  /// This member is kept up to date provided compositionEventHandlers are in place,
+  /// so it is safe to reference it to get the current composingText.
   String? composingText;
 
   void addCompositionEventHandlers(html.HtmlElement domElement) {
