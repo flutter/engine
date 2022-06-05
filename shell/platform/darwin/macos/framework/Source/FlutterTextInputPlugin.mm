d7d60a77d680f5d0be73e61de3e0ba613f952f33
Matej Knopp <matej.knopp@gmail.com>
2022-06-05 20:25:51 +0200

    Forward editing intents to framework

--- shell/platform/darwin/macos/framework/Source/FlutterTextInputPlugin.mm
+++ shell/platform/darwin/macos/framework/Source/FlutterTextInputPlugin.mm
@@ -32,6 +32,7 @@ static NSString* const kUpdateEditStateResponseMethod = @"TextInputClient.update
 static NSString* const kUpdateEditStateWithDeltasResponseMethod =
     @"TextInputClient.updateEditingStateWithDeltas";
 static NSString* const kPerformAction = @"TextInputClient.performAction";
+static NSString* const kPerformIntent = @"TextInputClient.performIntent";
 static NSString* const kMultilineInputType = @"TextInputType.multiline";
 
 static NSString* const kTextAffinityDownstream = @"TextAffinity.downstream";
@@ -665,6 +666,16 @@ static char markerKey;
     IMP imp = [self methodForSelector:selector];
     void (*func)(id, SEL, id) = reinterpret_cast<void (*)(id, SEL, id)>(imp);
     func(self, selector, nil);
+  }
+  // All NSStandardKeyBindingResponding method have single trailing space.
+  // For now forward all selectors to framework
+  NSString* name = NSStringFromSelector(selector);
+
+  if ([name hasSuffix:@":"]) {
+    name = [name substringToIndex:name.length - 1];
+    if (![name containsString:@":"]) {
+      [_channel invokeMethod:kPerformIntent arguments:@[ self.clientID, name ]];
+    }
   }
 }
 
