e6ed66ecbcc81c144b5e85d14ac1161a4f86880f
曾锦和(智鹰) <jinhe.zjh@alibaba-inc.com>
2022-06-10 17:41:08 +0800

    Use reinterpret_cast instead
    Change-Id: I07078197985bd8c857905a894721f079b93a15ec

--- shell/platform/android/android_image_generator.cc
+++ shell/platform/android/android_image_generator.cc
@@ -99,7 +99,8 @@ void AndroidImageGenerator::DoDecodeImage() {
   auto bitmap = std::make_unique<fml::jni::ScopedJavaGlobalRef<jobject>>(
       env, env->CallStaticObjectMethod(g_flutter_jni_class->obj(),
                                        g_decode_image_method,
-                                       direct_buffer.obj(), (long)this));
+                                       direct_buffer.obj(),
+                                       reinterpret_cast<long>(this));
   FML_CHECK(fml::jni::CheckException(env));
 
   if (bitmap->is_null()) {
