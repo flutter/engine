9b6f748ef9f712d05a9952d358026d4c3290ad61
Gary Qian <garyq@google.com>
2022-06-22 16:27:40 -0700

    Import

--- shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
+++ shell/platform/android/test/io/flutter/embedding/android/FlutterFragmentTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
