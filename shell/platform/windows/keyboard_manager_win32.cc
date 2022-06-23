eaac474f0f9e36e2832bbef6320c67ab736259fb
Tong Mu <dkwingsmt@gmail.com>
2022-06-23 13:52:08 -0700

    Extract EncodeUtf16

--- shell/platform/windows/keyboard_manager_win32.cc
+++ shell/platform/windows/keyboard_manager_win32.cc
@@ -97,25 +97,6 @@ static bool IsPrintable(uint32_t c) {
   return c >= kMinPrintable && c != kDelete;
 }
 
-static std::u16string EncodeUtf16(char32_t character) {
-  // Algorithm: https://en.wikipedia.org/wiki/UTF-16#Description
-  std::u16string result;
-  // Invalid value.
-  assert(!(character >= 0xD800 && character <= 0xDFFF) &&
-         !(character > 0x10FFFF));
-  if ((character >= 0xD800 && character <= 0xDFFF) || (character > 0x10FFFF)) {
-    return result;
-  }
-  if (character <= 0xD7FF || (character >= 0xE000 && character <= 0xFFFF)) {
-    result.push_back((char16_t)character);
-    return result;
-  }
-  uint32_t rem = character - 0x10000;
-  result.push_back((rem >> 10) + 0xD800);
-  result.push_back((rem & 0x3FF) + 0xDC00);
-  return result;
-}
-
 }  // namespace
 
 KeyboardManagerWin32::KeyboardManagerWin32(WindowDelegate* delegate)
