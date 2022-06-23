24c077f84a3c770569832d7951a038173863fe89
Javon Thomas <javont@google.com>
2022-06-23 16:11:55 -0700

    Removed Logs

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -320,7 +320,6 @@ void FlutterPlatformViewsController::EndFrame(
 }
 
 void FlutterPlatformViewsController::FilterPlatformViews(int64_t platform_view_id, sk_sp<SkImageFilter> filter) {
-  FML_DLOG(ERROR) << "FilterPlatformViews";
   EmbeddedViewParams params = current_composition_params_[platform_view_id];
   params.PushFilter(filter);
   current_composition_params_[platform_view_id] = params;
@@ -430,7 +429,6 @@ void FlutterPlatformViewsController::ApplyMutators(const MutatorsStack& mutators
         embedded_view.alpha = (*iter)->GetAlphaFloat() * embedded_view.alpha;
         break;
       case backdrop_filter:
-        FML_DLOG(ERROR) << "SWITCH TEST";
         break;
     }
     ++iter;
