2ff37e51d3cb53420dfec47b88a1f7ad9f9d3ca4
Harry Terkelsen <het@google.com>
2022-06-23 17:55:17 -0700

    wip

--- lib/web_ui/dev/generate_fallback_font_data.dart
+++ lib/web_ui/dev/generate_fallback_font_data.dart
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import 'dart:convert' show jsonDecode;
+import 'dart:io' as io;
 
 import 'package:args/command_runner.dart';
 import 'package:http/http.dart' as http;
@@ -47,14 +48,29 @@ class GenerateFallbackFontDataCommand extends Command<bool>
     }
     final Map<String, dynamic> googleFontsResult = jsonDecode(response.body);
     final List<dynamic> fontDatas = googleFontsResult['items'] as List<dynamic>;
+    final Map<String, Uri> urlForFamily = <String, Uri>{};
     for (final Map<String, dynamic> fontData in fontDatas) {
-      print(fontData['family']);
+      if (fallbackFonts.contains(fontData['family'])) {
+        final Uri uri = Uri.parse(fontData['files']['regular'] as String);
+        urlForFamily[fontData['family']] = uri;
+      }
+    }
+    final io.Directory tempDir =
+        await io.Directory.systemTemp.createTemp('fonts');
+    for (final String family in fallbackFonts) {
+      print('Downloading $family...');
+      final http.Response fontResponse =
+          await client.get(urlForFamily[family]!);
+      if (fontResponse.statusCode != 200) {
+        throw ToolExit('Failed to download font for $family');
+      }
     }
   }
 }
 
 const List<String> fallbackFonts = <String>[
   'Noto Sans',
+  'Noto Emoji',
   'Noto Sans Adlam',
   'Noto Sans Anatolian Hieroglyphs',
   'Noto Sans Arabic',
@@ -93,5 +109,105 @@ const List<String> fallbackFonts = <String>[
   'Noto Sans Gurmukhi',
   'Noto Sans HK',
   'Noto Sans Hanunoo',
-  'Noto Emoji',
+  'Noto Sans Hatran',
+  'Noto Sans Hebrew',
+  'Noto Sans Imperial Aramaic',
+  'Noto Sans Indic Siyaq Numbers',
+  'Noto Sans Inscriptional Pahlavi',
+  'Noto Sans Inscriptional Parthian',
+  'Noto Sans JP',
+  'Noto Sans Javanese',
+  'Noto Sans KR',
+  'Noto Sans Kaithi',
+  'Noto Sans Kannada',
+  'Noto Sans Kayah Li',
+  'Noto Sans Kharoshthi',
+  'Noto Sans Khmer',
+  'Noto Sans Khojki',
+  'Noto Sans Khudawadi',
+  'Noto Sans Lao',
+  'Noto Sans Lepcha',
+  'Noto Sans Limbu',
+  'Noto Sans Linear A',
+  'Noto Sans Linear B',
+  'Noto Sans Lisu',
+  'Noto Sans Lycian',
+  'Noto Sans Lydian',
+  'Noto Sans Mahajani',
+  'Noto Sans Malayalam',
+  'Noto Sans Mandaic',
+  'Noto Sans Manichean',
+  'Noto Sans Marchen',
+  'Noto Sans Masaram Gondi',
+  'Noto Sans Math',
+  'Noto Sans Mayan Numerals',
+  'Noto Sans Medefaidrin',
+  'Noto Sans Meetei Mayek',
+  'Noto Sans Meroitic',
+  'Noto Sans Miao',
+  'Noto Sans Modi',
+  'Noto Sans Mongolian',
+  'Noto Sans Mro',
+  'Noto Sans Multani',
+  'Noto Sans Myanmar',
+  'Noto Sans N Ko',
+  'Noto Sans Nabataean',
+  'Noto Sans New Tai Lue',
+  'Noto Sans Newa',
+  'Noto Sans Nushu',
+  'Noto Sans Ogham',
+  'Noto Sans Ol Chiki',
+  'Noto Sans Old Hungarian',
+  'Noto Sans Old Italic',
+  'Noto Sans Old North Arabian',
+  'Noto Sans Old Permic',
+  'Noto Sans Old Persian',
+  'Noto Sans Old Sogdian',
+  'Noto Sans Old South Arabian',
+  'Noto Sans Old Turkic',
+  'Noto Sans Oriya',
+  'Noto Sans Osage',
+  'Noto Sans Osmanya',
+  'Noto Sans Pahawh Hmong',
+  'Noto Sans Palmyrene',
+  'Noto Sans Pau Cin Hau',
+  'Noto Sans Phags Pa',
+  'Noto Sans Phoenician',
+  'Noto Sans Psalter Pahlavi',
+  'Noto Sans Rejang',
+  'Noto Sans Runic',
+  'Noto Sans SC',
+  'Noto Sans Saurashtra',
+  'Noto Sans Sharada',
+  'Noto Sans Shavian',
+  'Noto Sans Siddham',
+  'Noto Sans Sinhala',
+  'Noto Sans Sogdian',
+  'Noto Sans Sora Sompeng',
+  'Noto Sans Soyombo',
+  'Noto Sans Sundanese',
+  'Noto Sans Syloti Nagri',
+  'Noto Sans Symbols',
+  'Noto Sans Symbols 2',
+  'Noto Sans Syriac',
+  'Noto Sans TC',
+  'Noto Sans Tagalog',
+  'Noto Sans Tagbanwa',
+  'Noto Sans Tai Le',
+  'Noto Sans Tai Tham',
+  'Noto Sans Tai Viet',
+  'Noto Sans Takri',
+  'Noto Sans Tamil',
+  'Noto Sans Tamil Supplement',
+  'Noto Sans Telugu',
+  'Noto Sans Thaana',
+  'Noto Sans Thai',
+  'Noto Sans Tifinagh',
+  'Noto Sans Tirhuta',
+  'Noto Sans Ugaritic',
+  'Noto Sans Vai',
+  'Noto Sans Wancho',
+  'Noto Sans Warang Citi',
+  'Noto Sans Yi',
+  'Noto Sans Zanabazar Square',
 ];
