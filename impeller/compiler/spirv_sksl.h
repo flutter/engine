504e9b3ce492db6ff6ac0717a2c9823abb58fd42
Zachary Anderson <zra@google.com>
2022-07-02 08:54:52 -0700

    [impellerc] Adds an SkSL backend

---
+++ impeller/compiler/spirv_sksl.h
@@ -0,0 +1,39 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include <memory>
+#include <utility>
+#include <variant>
+
+#include "flutter/fml/logging.h"
+#include "flutter/fml/macros.h"
+#include "spirv_glsl.hpp"
+
+namespace impeller {
+namespace compiler {
+
+class CompilerSkSL : public spirv_cross::CompilerGLSL {
+ public:
+  explicit CompilerSkSL(std::vector<uint32_t> spirv_)
+      : CompilerGLSL(std::move(spirv_)) {}
+
+  CompilerSkSL(const uint32_t* ir_, size_t word_count)
+      : CompilerGLSL(ir_, word_count) {}
+
+  explicit CompilerSkSL(const spirv_cross::ParsedIR& ir_)
+      : spirv_cross::CompilerGLSL(ir_) {}
+
+  explicit CompilerSkSL(spirv_cross::ParsedIR&& ir_)
+      : spirv_cross::CompilerGLSL(std::move(ir_)) {}
+
+  std::string compile() override;
+
+ private:
+  void emit_header() override;
+};
+
+}  // namespace compiler
+}  // namespace impeller
