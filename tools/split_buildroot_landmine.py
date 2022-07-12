e423cb06703c606d09f46de6310b2301b4f1ff2e
Chinmay Garde <chinmaygarde@google.com>
2022-07-12 15:28:49 -0700

    Update commit message.

--- tools/split_buildroot_landmine.py
+++ tools/split_buildroot_landmine.py
@@ -7,6 +7,7 @@
 # If you find that this gclient hook is failing. You are using a split
 # buildroot. This is no longer supported and you must patch you .gclient file
 # and update the "name" attribute to point from "src/flutter" to "src". After,
-# run `gclient sync -D`.
+# move into to directory containing the .gclient file, remove "src" and run
+# `gclient sync`. Don't forget to save any stashes, origins, and local branches.
 #
 # You will only have to do this once.
