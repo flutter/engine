dd12e00d543d9cb1a4a1902205eda41001038635
Bernardo Eilert Trevisan <bernardo.trevisan@gmail.com>
2022-07-26 14:23:12 -0700

    Remove unused libraries.

--- shell/platform/embedder/embedder.cc
+++ shell/platform/embedder/embedder.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "include/core/SkRect.h"
-#include "shell/gpu/gpu_surface_gl_delegate.h"
 #define FML_USED_ON_EMBEDDER
 #define RAPIDJSON_HAS_STDSTRING 1
 
