31839d1cb1a69e0e2ef7ad0e88dfa1e2f8e06340
Bernardo Eilert Trevisan <btrevisan@google.com>
2022-08-01 20:05:07 +0000

    Documenting.

--- examples/glfw/FlutterEmbedderGLFW.cc
+++ examples/glfw/FlutterEmbedderGLFW.cc
@@ -13,6 +13,7 @@
 #include <EGL/eglext.h>
 #include <array>
 #include <list>
+#include <cstring>
 #include "GLFW/glfw3.h"
 #include "GLFW/glfw3native.h"
 #include "embedder.h"
@@ -25,6 +26,8 @@ static const size_t kInitialWindowHeight = 600;
 // last two frames; Some Android devices (Pixel 4) use quad buffering.
 static const int kMaxHistorySize = 10;
 
+// Keeps track of the most recent frame damages so that existing damage can
+// be easily computed.
 std::list<FlutterRect> damage_history_;
 
 static_assert(FLUTTER_ENGINE_VERSION == 1,
@@ -95,7 +98,9 @@ void GLFWwindowSizeCallback(GLFWwindow* window, int width, int height) {
       &event);
 }
 
-std::array<EGLint, 4> static RectToInts(EGLDisplay display,
+// Auxiliary function used to transform a FlutterRect into the format that is
+// expected by the EGL functions (i.e. array of EGLint).
+static std::array<EGLint, 4> RectToInts(EGLDisplay display,
                                         EGLSurface surface,
                                         const FlutterRect rect) {
   EGLint height;
@@ -108,11 +113,22 @@ std::array<EGLint, 4> static RectToInts(EGLDisplay display,
   return res;
 }
 
-void JoinFlutterRect(FlutterRect* rect, FlutterRect additional_rect) {
+// Auxiliary function to union the damage regions comprised by two FlutterRect
+// element. It saves the result of this join in the rect variable.
+static void JoinFlutterRect(FlutterRect* rect, FlutterRect additional_rect) {
   rect->left = std::min(rect->left, additional_rect.left);
   rect->top = std::min(rect->top, additional_rect.top);
   rect->right = std::max(rect->right, additional_rect.right);
   rect->bottom = std::max(rect->bottom, additional_rect.bottom);
+}
+
+// Auxiliary function used to check if the given list of extensions contains the
+// requested extension name.
+static bool HasExtension(const char* extensions, const char* name) {
+  const char* r = strstr(extensions, name);
+  auto len = strlen(name);
+  // check that the extension name is terminated by space or null terminator
+  return r != nullptr && (r[len] == ' ' || r[len] == 0);
 }
 
 bool RunFlutter(GLFWwindow* window,
@@ -131,58 +147,75 @@ bool RunFlutter(GLFWwindow* window,
   };
   config.open_gl.present_with_info =
       [](void* userdata, const FlutterPresentInfo* info) -> bool {
-    PFNEGLSETDAMAGEREGIONKHRPROC set_damage_region_ =
-        reinterpret_cast<PFNEGLSETDAMAGEREGIONKHRPROC>(
-            eglGetProcAddress("eglSetDamageRegionKHR"));
-    PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC swap_buffers_with_damage_ =
-        reinterpret_cast<PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC>(
-            eglGetProcAddress("eglSwapBuffersWithDamageKHR"));
-
+    // Get the display and surface variables.
     GLFWwindow* window = static_cast<GLFWwindow*>(userdata);
     EGLDisplay display = glfwGetEGLDisplay();
     EGLSurface surface = glfwGetEGLSurface(window);
 
-    auto buffer_rects = RectToInts(
-        display, surface, info->buffer_damage.damage[0]);
-    set_damage_region_(display, surface, buffer_rects.data(), 1);
+    // Get list of extensions.
+    const char* extensions = eglQueryString(display, EGL_EXTENSIONS);
 
-    // Swap buffers with frame damage
-    auto frame_rects = RectToInts(display, surface, info->frame_damage.damage[0]);
-    swap_buffers_with_damage_(display, surface, frame_rects.data(), 1);
+    // Retrieve the set damage region function.
+    PFNEGLSETDAMAGEREGIONKHRPROC set_damage_region_ = nullptr;
+    if (HasExtension(extensions, "EGL_KHR_partial_update")) {
+      set_damage_region_ =
+        reinterpret_cast<PFNEGLSETDAMAGEREGIONKHRPROC>(
+            eglGetProcAddress("eglSetDamageRegionKHR"));
+    }
+
+    // Retrieve the swap buffers with damage function.
+    PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC swap_buffers_with_damage_ = nullptr;
+    if (HasExtension(extensions, "EGL_EXT_swap_buffers_with_damage")) {
+      swap_buffers_with_damage_ =
+          reinterpret_cast<PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC>(
+              eglGetProcAddress("eglSwapBuffersWithDamageEXT"));
+    } else if (HasExtension(extensions, "EGL_KHR_swap_buffers_with_damage")) {
+      swap_buffers_with_damage_ =
+          reinterpret_cast<PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC>(
+              eglGetProcAddress("eglSwapBuffersWithDamageKHR"));
+    }
+
+    if (set_damage_region_) {
+      // Set the buffer damage as the damage region.
+      auto buffer_rects = RectToInts(
+          display, surface, info->buffer_damage.damage[0]);
+      set_damage_region_(display, surface, buffer_rects.data(), 1);
+    }
 
     // Add frame damage to damage history
     damage_history_.push_back(info->frame_damage.damage[0]);
     if (damage_history_.size() > kMaxHistorySize) {
       damage_history_.pop_front();
     }
-    std::cout << "Buffer Damage: " << info->buffer_damage.damage[0].left << ", "
-              << info->buffer_damage.damage[0].top << ", "
-              << info->buffer_damage.damage[0].right << ", "
-              << info->buffer_damage.damage[0].bottom << std::endl;
-    std::cout << "Frame Damage: " << info->frame_damage.damage[0].left << ", "
-              << info->frame_damage.damage[0].top << ", "
-              << info->frame_damage.damage[0].right << ", "
-              << info->frame_damage.damage[0].bottom << std::endl;
-    return true;
+
+    if (swap_buffers_with_damage_) {
+      // Swap buffers with frame damage.
+      auto frame_rects = RectToInts(display, surface, info->frame_damage.damage[0]);
+      return swap_buffers_with_damage_(display, surface, frame_rects.data(), 1);
+    } else {
+      // If the required extensions for partial repaint were not provided, do
+      // full repaint.
+      return eglSwapBuffers(display, surface);
+    }
   };
   config.open_gl.fbo_callback = [](void*) -> uint32_t {
     return 0;  // FBO0
   };
   config.open_gl.fbo_with_damage_callback =
       [](void* userdata, intptr_t fbo_id, FlutterDamage* existing_damage) -> void {
-    // Given the FBO age, create existing damage region by joining all frame
-    // damages since FBO was last used
+    // Get the display and surface variables.
     GLFWwindow* window = static_cast<GLFWwindow*>(userdata);
     EGLDisplay display = glfwGetEGLDisplay();
     EGLSurface surface = glfwGetEGLSurface(window);
 
+    // Given the FBO age, create existing damage region by joining all frame
+    // damages since FBO was last used
     EGLint age;
     if (glfwExtensionSupported("GL_EXT_buffer_age") == GLFW_TRUE) {
       eglQuerySurface(display, surface, EGL_BUFFER_AGE_EXT, &age);
     } else {
       age = 4;  // Virtually no driver should have a swapchain length > 4.
     }
-    std::cout << "Buffer age: " << age << std::endl;
 
     existing_damage->num_rects = 1;
     std::array<FlutterRect, 1> existing_damage_rect = {FlutterRect{0, 0, kInitialWindowWidth, kInitialWindowHeight}};
@@ -193,8 +226,6 @@ bool RunFlutter(GLFWwindow* window,
       // join up to (age - 1) last rects from damage history
       for (auto i = damage_history_.rbegin();
            i != damage_history_.rend() && age > 0; ++i, --age) {
-        std::cout << "Damage in history: " << i->left << ", " << i->top << ", "
-                  << i->right << ", " << i->bottom << std::endl;
         if (i == damage_history_.rbegin()) {
           if (i != damage_history_.rend()) {
             existing_damage->damage[0] = {i->left, i->top, i->right, i->bottom};
@@ -204,11 +235,6 @@ bool RunFlutter(GLFWwindow* window,
         }
       }
     }
-
-    std::cout << "Existing Damage: " << existing_damage->damage[0].left
-              << ", " << existing_damage->damage[0].top << ", "
-              << existing_damage->damage[0].right << ", "
-              << existing_damage->damage[0].bottom << std::endl;
   };
   config.open_gl.gl_proc_resolver = [](void*, const char* name) -> void* {
     return reinterpret_cast<void*>(glfwGetProcAddress(name));
