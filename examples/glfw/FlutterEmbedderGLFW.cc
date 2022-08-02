7f75cf992f7ca6ea5c671eae9cd9a9610a460df1
Bernardo Eilert Trevisan <btrevisan@google.com>
2022-08-02 16:46:13 +0000

    Move glfw_drm example to a new folder.

--- examples/glfw/FlutterEmbedderGLFW.cc
+++ examples/glfw/FlutterEmbedderGLFW.cc
@@ -6,29 +6,13 @@
 #include <chrono>
 #include <iostream>
 
-#define GLFW_EXPOSE_NATIVE_EGL
-#define GLFW_INCLUDE_GLEXT
-
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
-#include <array>
-#include <cstring>
-#include <list>
 #include "GLFW/glfw3.h"
-#include "GLFW/glfw3native.h"
 #include "embedder.h"
 
 // This value is calculated after the window is created.
 static double g_pixelRatio = 1.0;
 static const size_t kInitialWindowWidth = 800;
 static const size_t kInitialWindowHeight = 600;
-// Maximum damage history - for triple buffering we need to store damage for
-// last two frames; Some Android devices (Pixel 4) use quad buffering.
-static const int kMaxHistorySize = 10;
-
-// Keeps track of the most recent frame damages so that existing damage can
-// be easily computed.
-std::list<FlutterRect> damage_history_;
 
 static_assert(FLUTTER_ENGINE_VERSION == 1,
               "This Flutter Embedder was authored against the stable Flutter "
@@ -98,39 +82,6 @@ void GLFWwindowSizeCallback(GLFWwindow* window, int width, int height) {
       &event);
 }
 
-// Auxiliary function used to transform a FlutterRect into the format that is
-// expected by the EGL functions (i.e. array of EGLint).
-static std::array<EGLint, 4> RectToInts(EGLDisplay display,
-                                        EGLSurface surface,
-                                        const FlutterRect rect) {
-  EGLint height;
-  eglQuerySurface(display, surface, EGL_HEIGHT, &height);
-
-  std::array<EGLint, 4> res{
-      static_cast<int>(rect.left), height - static_cast<int>(rect.bottom),
-      static_cast<int>(rect.right) - static_cast<int>(rect.left),
-      static_cast<int>(rect.bottom) - static_cast<int>(rect.top)};
-  return res;
-}
-
-// Auxiliary function to union the damage regions comprised by two FlutterRect
-// element. It saves the result of this join in the rect variable.
-static void JoinFlutterRect(FlutterRect* rect, FlutterRect additional_rect) {
-  rect->left = std::min(rect->left, additional_rect.left);
-  rect->top = std::min(rect->top, additional_rect.top);
-  rect->right = std::max(rect->right, additional_rect.right);
-  rect->bottom = std::max(rect->bottom, additional_rect.bottom);
-}
-
-// Auxiliary function used to check if the given list of extensions contains the
-// requested extension name.
-static bool HasExtension(const char* extensions, const char* name) {
-  const char* r = strstr(extensions, name);
-  auto len = strlen(name);
-  // check that the extension name is terminated by space or null terminator
-  return r != nullptr && (r[len] == ' ' || r[len] == 0);
-}
-
 bool RunFlutter(GLFWwindow* window,
                 const std::string& project_path,
                 const std::string& icudtl_path) {
@@ -145,103 +96,16 @@ bool RunFlutter(GLFWwindow* window,
     glfwMakeContextCurrent(nullptr);  // is this even a thing?
     return true;
   };
-  config.open_gl.present_with_info =
-      [](void* userdata, const FlutterPresentInfo* info) -> bool {
-    // Get the display and surface variables.
-    GLFWwindow* window = static_cast<GLFWwindow*>(userdata);
-    EGLDisplay display = glfwGetEGLDisplay();
-    EGLSurface surface = glfwGetEGLSurface(window);
-
-    // Get list of extensions.
-    const char* extensions = eglQueryString(display, EGL_EXTENSIONS);
-
-    // Retrieve the set damage region function.
-    PFNEGLSETDAMAGEREGIONKHRPROC set_damage_region_ = nullptr;
-    if (HasExtension(extensions, "EGL_KHR_partial_update")) {
-      set_damage_region_ = reinterpret_cast<PFNEGLSETDAMAGEREGIONKHRPROC>(
-          eglGetProcAddress("eglSetDamageRegionKHR"));
-    }
-
-    // Retrieve the swap buffers with damage function.
-    PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC swap_buffers_with_damage_ = nullptr;
-    if (HasExtension(extensions, "EGL_EXT_swap_buffers_with_damage")) {
-      swap_buffers_with_damage_ =
-          reinterpret_cast<PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC>(
-              eglGetProcAddress("eglSwapBuffersWithDamageEXT"));
-    } else if (HasExtension(extensions, "EGL_KHR_swap_buffers_with_damage")) {
-      swap_buffers_with_damage_ =
-          reinterpret_cast<PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC>(
-              eglGetProcAddress("eglSwapBuffersWithDamageKHR"));
-    }
-
-    if (set_damage_region_) {
-      // Set the buffer damage as the damage region.
-      auto buffer_rects =
-          RectToInts(display, surface, info->buffer_damage.damage[0]);
-      set_damage_region_(display, surface, buffer_rects.data(), 1);
-    }
-
-    // Add frame damage to damage history
-    damage_history_.push_back(info->frame_damage.damage[0]);
-    if (damage_history_.size() > kMaxHistorySize) {
-      damage_history_.pop_front();
-    }
-
-    if (swap_buffers_with_damage_) {
-      // Swap buffers with frame damage.
-      auto frame_rects =
-          RectToInts(display, surface, info->frame_damage.damage[0]);
-      return swap_buffers_with_damage_(display, surface, frame_rects.data(), 1);
-    } else {
-      // If the required extensions for partial repaint were not provided, do
-      // full repaint.
-      return eglSwapBuffers(display, surface);
-    }
+  config.open_gl.present = [](void* userdata) -> bool {
+    glfwSwapBuffers(static_cast<GLFWwindow*>(userdata));
+    return true;
   };
   config.open_gl.fbo_callback = [](void*) -> uint32_t {
     return 0;  // FBO0
   };
-  config.open_gl.populate_existing_damage_callback =
-      [](void* userdata, intptr_t fbo_id,
-         FlutterDamage* existing_damage) -> void {
-    // Get the display and surface variables.
-    GLFWwindow* window = static_cast<GLFWwindow*>(userdata);
-    EGLDisplay display = glfwGetEGLDisplay();
-    EGLSurface surface = glfwGetEGLSurface(window);
-
-    // Given the FBO age, create existing damage region by joining all frame
-    // damages since FBO was last used
-    EGLint age;
-    if (glfwExtensionSupported("GL_EXT_buffer_age") == GLFW_TRUE) {
-      eglQuerySurface(display, surface, EGL_BUFFER_AGE_EXT, &age);
-    } else {
-      age = 4;  // Virtually no driver should have a swapchain length > 4.
-    }
-
-    existing_damage->num_rects = 1;
-    std::array<FlutterRect, 1> existing_damage_rect = {
-        FlutterRect{0, 0, kInitialWindowWidth, kInitialWindowHeight}};
-    existing_damage->damage = existing_damage_rect.data();
-
-    if (age > 1) {
-      --age;
-      // join up to (age - 1) last rects from damage history
-      for (auto i = damage_history_.rbegin();
-           i != damage_history_.rend() && age > 0; ++i, --age) {
-        if (i == damage_history_.rbegin()) {
-          if (i != damage_history_.rend()) {
-            existing_damage->damage[0] = {i->left, i->top, i->right, i->bottom};
-          }
-        } else {
-          JoinFlutterRect(&(existing_damage->damage[0]), *i);
-        }
-      }
-    }
-  };
   config.open_gl.gl_proc_resolver = [](void*, const char* name) -> void* {
     return reinterpret_cast<void*>(glfwGetProcAddress(name));
   };
-  config.open_gl.fbo_reset_after_present = true;
 
   // This directory is generated by `flutter build bundle`.
   std::string assets_path = project_path + "/build/flutter_assets";
--- examples/glfw/main.dart
+++ examples/glfw/main.dart
@@ -5,8 +5,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'
     show debugDefaultTargetPlatformOverride;
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-
 
 void main() {
   // This is a hack to make Flutter think you are running on Google Fuchsia,
@@ -57,6 +55,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -74,10 +85,38 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: RepaintBoundary(
-          child: SpinKitRotatingCircle(color: Colors.blue, size: 50.0),
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
