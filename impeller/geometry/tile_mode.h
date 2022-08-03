73328f82b556dbe1aaf95359f0507ac420c403d1
ColdPaleLight <zhangzhijian.123@bytedance.com>
2022-08-03 15:13:27 +0800

    Update comments

--- impeller/geometry/tile_mode.h
+++ impeller/geometry/tile_mode.h
@@ -10,20 +10,21 @@ namespace impeller {
 // typically defined range of the source of the colors (such as the
 // bounds of an image or the defining geoetry of a gradient).
 enum class TileMode {
-  // Replicate the edge color if the |DlColorSource| draws outside of the
-  // defined bounds.
+  // Replicate the edge color if the shader draws outside of its original
+  // bounds.
   kClamp,
 
-  // Repeat the |DlColorSource|'s defined colors both horizontally and
-  // vertically (or both along and perpendicular to a gradient's geometry).
+  // Repeat the shader's image horizontally and vertically (or both along and
+  // perpendicular to a gradient's geometry).
   kRepeat,
 
-  // Repeat the |DlColorSource|'s colors horizontally and vertically,
-  // alternating mirror images so that adjacent images always seam.
+  // Repeat the shader's image horizontally and vertically, alternating mirror
+  // images so that adjacent images always seam.
   kMirror,
 
-  // Only draw within the original domain, return transparent-black everywhere
-  // else.
+  // Render the shader's image pixels only within its original bounds. If the
+  // shader draws outside of its original bounds, transparent black is drawn
+  // instead.
   kDecal,
 };
 
