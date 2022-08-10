938d74ec378d0b8bfcdf962a052d4b799b825742
Bernardo Eilert Trevisan <btrevisan@google.com>
2022-08-10 23:14:19 +0000

    Update comment

--- examples/glfw_drm/main.dart
+++ examples/glfw_drm/main.dart
@@ -59,9 +59,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
