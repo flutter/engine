60a95a010517e0e82a921538c11518a3ea99d7b4
Polina Cherkasova <polina.c@live.com>
2022-08-10 21:35:49 -0700

    Update image_test.dart

--- testing/dart/image_test.dart
+++ testing/dart/image_test.dart
@@ -3,43 +3,36 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'dart:io';
-import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:litetest/litetest.dart';
-import 'package:path/path.dart' as path;
 
 void main() {
-  tearDown((){
-    Image.onDispose = null; 
-    Image.onCreate = null; 
-  });
-
   test('Image constructor invokes onCreate once', () async {
     int onCreateInvokedCount = 0;
     int instanceHashCode = 0;
-    Image.onCreate = (obj) {
-      onCreateInvokedCount++; 
+    Image.onCreate = (Object obj) {
+      onCreateInvokedCount++;
       instanceHashCode = identityHashCode(obj);
     };
 
-    final image1 = _createPicture().toImage();
+    final Future<Image> image1 = _createPicture().toImage(10, 10);
 
     expect(onCreateInvokedCount, 1);
     expect(instanceHashCode, identityHashCode(image1));
 
-    final image2 = _createPicture().toImage();
+    final Future<Image> image2 = _createPicture().toImage(10, 10);
     
     expect(onCreateInvokedCount, 2);
     expect(instanceHashCode, identityHashCode(image2));
-  }
+    Image.onCreate = null;
+  });
 }
 
 Picture _createPicture() {
-  final recorder = PictureRecorder();
-  final canvas = Canvas(recorder);
-  var rect = Rect.fromLTWH(0.0, 0.0, 100.0, 100.0);
+  final PictureRecorder recorder = PictureRecorder();
+  final Canvas canvas = Canvas(recorder);
+  const Rect rect = Rect.fromLTWH(0.0, 0.0, 100.0, 100.0);
   canvas.clipRect(rect);
   return recorder.endRecording();
 }
