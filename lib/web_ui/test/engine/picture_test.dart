0f53a5b89d3bd366c5592fc6df8b4da70a79bafa
Polina Cherkasova <polina.c@live.com>
2022-08-12 21:50:09 -0700

    -

--- lib/web_ui/test/engine/picture_test.dart
+++ lib/web_ui/test/engine/picture_test.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:test/bootstrap/browser.dart';
 import 'package:test/test.dart';
 import 'package:ui/ui.dart';
 
