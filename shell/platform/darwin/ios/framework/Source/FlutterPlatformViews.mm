ee25e17e0fb6690ac5bf255204f45c79a9b7bce1
Chris Yang <ychris@google.com>
2022-08-18 11:13:43 -0700

    remove TODOs

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViews.mm
@@ -35,7 +35,6 @@
 namespace flutter {
 // Becomes NO if Apple's API changes and blurred backdrop filters cannot be applied.
 BOOL canApplyBlurBackdrop = YES;
-// int changingBlurRadius = 0; // TODO EMILY part of visual tests. delete before landing PR
 
 std::shared_ptr<FlutterPlatformViewLayer> FlutterPlatformViewLayerPool::GetLayer(
     GrDirectContext* gr_context,
@@ -419,33 +418,12 @@ void FlutterPlatformViewsController::ApplyMutators(const MutatorsStack& mutators
 
   NSMutableArray* blurRadii = [[[NSMutableArray alloc] init] autorelease];
 
-  // TODO EMILY: this line is for visual simulator tests, delete before landing PR
-  //                        int numFilters = 0;
-
   auto iter = mutators_stack.Begin();
   while (iter != mutators_stack.End()) {
     switch ((*iter)->GetType()) {
       case kTransform: {
         CATransform3D transform = GetCATransform3DFromSkMatrix((*iter)->GetMatrix());
         finalTransform = CATransform3DConcat(transform, finalTransform);
-
-        //             TODO EMILY: these lines are for visual simulator tests, delete before landing
-        //             PR
-        //                                  if(numFilters < 1) {
-        //                                    flutter::DlBlurImageFilter filter =
-        //                                        flutter::DlBlurImageFilter(changingBlurRadius,
-        //                                        changingBlurRadius, flutter::DlTileMode::kDecal);
-        //
-        //                                    NSNumber* blurRadius =
-        //                                    @(filter.asBlur()->sigma_x());
-        //                                    [blurRadii addObject:blurRadius];
-        //
-        //                                    numFilters++;
-        //                                    FML_DLOG(ERROR) << "****checkpoint***" <<
-        //                                    changingBlurRadius;
-        //
-        //                                    changingBlurRadius++;
-        //                                  }
         break;
       }
       case kClipRect:
