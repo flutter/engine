5a55dcad83ae3c3a06ed8cb9d03a687751460fe9
ColdPaleLight <zhangzhijian.123@bytedance.com>
2022-08-23 14:21:31 +0800

    reduce complexity of the UV mapping

--- impeller/entity/contents/filters/morphology_filter_contents.cc
+++ impeller/entity/contents/filters/morphology_filter_contents.cc
@@ -59,47 +59,11 @@ std::optional<Snapshot> DirectionalMorphologyFilterContents::RenderFilter(
     return input_snapshot.value();
   }
 
-  auto transformed_radius = entity.GetTransformation().TransformDirection(
-      direction_ * radius_.radius);
-
-  auto transformed_radius_length = transformed_radius.GetLength();
-
-  // A matrix that rotates the snapshot space such that the direction is
-  // +X.
-  auto texture_rotate =
-      Matrix::MakeRotationZ(transformed_radius.Normalize().AngleTo({1, 0}));
-
-  // Converts local pass space to screen space. This is just the snapshot space
-  // rotated such that the direction is +X.
-  auto pass_transform = texture_rotate * input_snapshot->transform;
-
-  // The pass texture coverage, but rotated such that the filter is in the +X
-  // direction, and expanded to include the radius. This is used for UV
-  // projection and as a source for the pass size. Note that it doesn't matter
-  // which direction the space is rotated in when grabbing the pass size.
-  auto pass_texture_rect = Rect::MakeSize(input_snapshot->texture->GetSize())
-                               .TransformBounds(pass_transform);
-
-  // auto original_pass_texture_rect = pass_texture_rect;
-  pass_texture_rect.origin.x -= transformed_radius_length;
-  pass_texture_rect.size.width += transformed_radius_length * 2;
-
-  // UV mapping.
-  auto pass_uv_project = [&texture_rotate,
-                          &pass_texture_rect](Snapshot& input) {
-    auto uv_matrix = Matrix::MakeScale(1 / Vector2(input.texture->GetSize())) *
-                     (texture_rotate * input.transform).Invert();
-    return pass_texture_rect.GetTransformedPoints(uv_matrix);
-  };
-  auto input_uvs = pass_uv_project(input_snapshot.value());
-
-  auto transformed_texture_vertices =
-      Rect(Size(input_snapshot->texture->GetSize()))
-          .GetTransformedPoints(pass_transform);
-  auto transformed_texture_width = transformed_texture_vertices[0].GetDistance(
-      transformed_texture_vertices[1]);
-  auto transformed_texture_height = transformed_texture_vertices[0].GetDistance(
-      transformed_texture_vertices[2]);
+  auto maybe_input_uvs = input_snapshot->GetCoverageUVs(coverage);
+  if (!maybe_input_uvs.has_value()) {
+    return std::nullopt;
+  }
+  auto input_uvs = maybe_input_uvs.value();
 
   //----------------------------------------------------------------------------
   /// Render to texture.
@@ -124,13 +88,25 @@ std::optional<Snapshot> DirectionalMorphologyFilterContents::RenderFilter(
     VS::FrameInfo frame_info;
     frame_info.mvp = Matrix::MakeOrthographic(ISize(1, 1));
 
+    auto transformed_radius = entity.GetTransformation().TransformDirection(
+        direction_ * radius_.radius);
+    auto transformed_texture_vertices =
+        Rect(Size(input_snapshot->texture->GetSize()))
+            .GetTransformedPoints(input_snapshot->transform);
+    auto transformed_texture_width =
+        transformed_texture_vertices[0].GetDistance(
+            transformed_texture_vertices[1]);
+    auto transformed_texture_height =
+        transformed_texture_vertices[0].GetDistance(
+            transformed_texture_vertices[2]);
+
     FS::FragInfo frag_info;
     frag_info.texture_sampler_y_coord_scale =
         input_snapshot->texture->GetYCoordScale();
-    frag_info.radius = std::round(transformed_radius_length);
-    // The direction is in input UV space.
-    frag_info.direction =
-        pass_transform.Invert().TransformDirection(Vector2(1, 0)).Normalize();
+    frag_info.radius = std::round(transformed_radius.GetLength());
+    frag_info.direction = input_snapshot->transform.Invert()
+                              .TransformDirection(transformed_radius)
+                              .Normalize();
     frag_info.texture_size =
         Point(transformed_texture_width, transformed_texture_height);
     frag_info.morph_type = static_cast<Scalar>(morph_type_);
@@ -152,9 +128,7 @@ std::optional<Snapshot> DirectionalMorphologyFilterContents::RenderFilter(
     return pass.AddCommand(cmd);
   };
 
-  auto out_texture = renderer.MakeSubpass(
-      ISize(pass_texture_rect.size.width, pass_texture_rect.size.height),
-      callback);
+  auto out_texture = renderer.MakeSubpass(ISize(coverage.size), callback);
   if (!out_texture) {
     return std::nullopt;
   }
@@ -164,11 +138,9 @@ std::optional<Snapshot> DirectionalMorphologyFilterContents::RenderFilter(
   sampler_desc.min_filter = MinMagFilter::kLinear;
   sampler_desc.mag_filter = MinMagFilter::kLinear;
 
-  return Snapshot{
-      .texture = out_texture,
-      .transform = texture_rotate.Invert() *
-                   Matrix::MakeTranslation(pass_texture_rect.origin),
-      .sampler_descriptor = sampler_desc};
+  return Snapshot{.texture = out_texture,
+                  .transform = Matrix::MakeTranslation(coverage.origin),
+                  .sampler_descriptor = sampler_desc};
 }
 
 std::optional<Rect> DirectionalMorphologyFilterContents::GetFilterCoverage(
