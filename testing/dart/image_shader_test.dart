46d5c50bf3b7ef6238e861ed1c42c541bb3eddce
Dan Field <dnfield@google.com>
2022-08-24 01:52:41 -0700

    Fix release test

--- testing/dart/image_shader_test.dart
+++ testing/dart/image_shader_test.dart
@@ -39,16 +39,9 @@ void main() {
 
     if (assertsEnabled) {
       expectAssertion(() => ImageShader(image, TileMode.clamp, TileMode.clamp, Float64List(16)));
-      return;
+    } else {
+      throwsException(() => ImageShader(image, TileMode.clamp, TileMode.clamp, Float64List(16)));
     }
-
-    final ImageShader shader = ImageShader(image, TileMode.clamp, TileMode.clamp, Float64List(16));
-
-    final Paint paint = Paint()..shader = shader;
-    const Rect rect = Rect.fromLTRB(0, 0, 100, 100);
-    testCanvas((Canvas canvas) => canvas.drawRect(rect, paint));
-    shader.dispose();
-
   });
 
   test('Disposed image shader in a paint', () async {
