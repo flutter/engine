22babb6522c7e741a4d6e037054d38b45bfdbb5e
Polina Cherkasova <polina.c@live.com>
2022-08-29 15:50:58 -0700

    Create image_test.dart

---
+++ lib/web_ui/test/canvaskit/image_test.dart
@@ -0,0 +1,71 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:test/bootstrap/browser.dart';
+import 'package:test/test.dart';
+import 'package:ui/ui.dart' as ui;
+
+void main() {
+  internalBootstrapBrowserTest(() => testMain);
+}
+
+Future<void> testMain() async {
+  test('Image constructor invokes onCreate once', () async {
+    int onCreateInvokedCount = 0;
+    ui.Image? createdImage;
+    ui.Image.onCreate = (ui.Image image) {
+      onCreateInvokedCount++;
+      createdImage = image;
+    };
+
+print(10);
+    final Future<ui.Image> future = _createImage();
+print(20);
+    final image = await future;
+print(30);
+
+    // expect(onCreateInvokedCount, 1);
+    // expect(createdImage, image1);
+
+    // final ui.Image image2 = await _createImage();
+
+    // expect(onCreateInvokedCount, 2);
+    // expect(createdImage, image2);
+
+    ui.Image.onCreate = null;
+  });
+
+  // test('dispose() invokes onDispose once', () async {
+  //   int onDisposeInvokedCount = 0;
+  //   ui.Image? disposedImage;
+  //   ui.Image.onDispose = (ui.Image image) {
+  //     onDisposeInvokedCount++;
+  //     disposedImage = image;
+  //   };
+
+  //   final ui.Image image1 = await _createImage()..dispose();
+
+  //   expect(onDisposeInvokedCount, 1);
+  //   expect(disposedImage, image1);
+
+  //   final ui.Image image2 = await _createImage()..dispose();
+
+  //   expect(onDisposeInvokedCount, 2);
+  //   expect(disposedImage, image2);
+
+  //   ui.Image.onDispose = null;
+  // });
+}
+
+Future<ui.Image> _createImage() => _createPicture().toImage(10, 10);
+
+ui.Picture _createPicture() {
+  final ui.PictureRecorder recorder = ui.PictureRecorder();
+  final ui.Canvas canvas = ui.Canvas(recorder);
+  const ui.Rect rect = ui.Rect.fromLTWH(0.0, 0.0, 100.0, 100.0);
+  canvas.clipRect(rect);
+  final result = recorder.endRecording();
+  print('created');
+  return result;
+}
