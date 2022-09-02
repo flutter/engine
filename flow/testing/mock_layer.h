15ae92ee0c44b93d9faa49da1ee7a7d48eabefae
xieguo <xieguo@bytedance.com>
2022-09-02 11:36:08 +0800

    Revert "purify."
    This reverts commit 17c40f9a5e89d221376c56f58967830097d9a20f.

--- flow/testing/mock_layer.h
+++ flow/testing/mock_layer.h
@@ -144,9 +144,8 @@ class MockCacheableLayer : public MockLayer {
  public:
   explicit MockCacheableLayer(SkPath path,
                               SkPaint paint = SkPaint(),
-                              int render_limit = 3,
-                              int mock_flag = 0)
-      : MockLayer(path, paint, mock_flag) {
+                              int render_limit = 3)
+      : MockLayer(path, paint) {
     raster_cache_item_ =
         std::make_unique<MockLayerCacheableItem>(this, render_limit);
   }
