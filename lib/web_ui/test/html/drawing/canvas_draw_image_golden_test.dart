d89254abfef68a2a9fe2d9551a76e281c446d159
Mouad Debbar <mdebbar@google.com>
2022-09-12 14:34:31 -0400

    [web] Clean up duplicate golden methods

--- lib/web_ui/test/html/drawing/canvas_draw_image_golden_test.dart
+++ lib/web_ui/test/html/drawing/canvas_draw_image_golden_test.dart
@@ -34,8 +34,7 @@ Future<void> testMain() async {
     rc.save();
     rc.drawImage(createTestImage(), Offset.zero, SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image');
   });
 
   test('Images from raw data are composited when picture is roundtripped through toImage', () async {
@@ -68,8 +67,7 @@ Future<void> testMain() async {
     rc.rotate(math.pi / 4.0);
     rc.drawImage(createTestImage(), Offset.zero, SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image_with_transform',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image_with_transform');
   });
 
   test('Paints image with transform and offset', () async {
@@ -80,8 +78,7 @@ Future<void> testMain() async {
     rc.rotate(math.pi / 4.0);
     rc.drawImage(createTestImage(), const Offset(30, 20), SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image_with_transform_and_offset',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image_with_transform_and_offset');
   });
 
   test('Paints image with transform using destination', () async {
@@ -96,8 +93,7 @@ Future<void> testMain() async {
     rc.drawImageRect(testImage, Rect.fromLTRB(0, 0, testWidth, testHeight),
         Rect.fromLTRB(100, 30, 2 * testWidth, 2 * testHeight), SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image_rect_with_transform',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image_rect_with_transform');
   });
 
   test('Paints image with source and destination', () async {
@@ -113,8 +109,7 @@ Future<void> testMain() async {
         Rect.fromLTRB(100, 30, 2 * testWidth, 2 * testHeight),
         SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image_rect_with_source',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image_rect_with_source');
   });
 
   test('Paints image with source and destination and round clip', () async {
@@ -132,8 +127,7 @@ Future<void> testMain() async {
         Rect.fromLTRB(100, 30, 2 * testWidth, 2 * testHeight),
         SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image_rect_with_source_and_clip',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image_rect_with_source_and_clip');
   });
 
   test('Paints image with transform using source and destination', () async {
@@ -151,8 +145,7 @@ Future<void> testMain() async {
         Rect.fromLTRB(100, 30, 2 * testWidth, 2 * testHeight),
         SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_image_rect_with_transform_source',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_image_rect_with_transform_source');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -173,8 +166,7 @@ Future<void> testMain() async {
           ..strokeWidth = 3
           ..color = const Color.fromARGB(128, 0, 0, 0));
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_on_image',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_on_image');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -195,8 +187,7 @@ Future<void> testMain() async {
     rc.drawImageRect(testImage, Rect.fromLTRB(0, 0, testWidth, testHeight),
         Rect.fromLTRB(100, 30, 2 * testWidth, 2 * testHeight), SurfacePaint());
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_below_image',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_below_image');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -218,8 +209,7 @@ Future<void> testMain() async {
           ..strokeWidth = 3
           ..color = const Color.fromARGB(128, 0, 0, 0));
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rect',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rect');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -245,8 +235,7 @@ Future<void> testMain() async {
           ..strokeWidth = 3
           ..color = const Color.fromARGB(128, 0, 0, 0));
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rect_with_transform',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rect_with_transform');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -274,8 +263,7 @@ Future<void> testMain() async {
           ..color = const Color.fromARGB(128, 0, 0, 0));
     rc.restore();
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rect_with_stack',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rect_with_stack');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -297,8 +285,7 @@ Future<void> testMain() async {
           ..strokeWidth = 3
           ..color = const Color.fromARGB(128, 0, 0, 0));
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rrect',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_on_image_clip_rrect');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/44845
@@ -325,8 +312,7 @@ Future<void> testMain() async {
           ..strokeWidth = 3
           ..color = const Color.fromARGB(128, 0, 0, 0));
     rc.restore();
-    await canvasScreenshot(rc, 'draw_circle_on_image_clip_path',
-        region: const Rect.fromLTWH(0, 0, 500, 500));
+    await canvasScreenshot(rc, 'draw_circle_on_image_clip_path');
   });
 
   // Regression test for https://github.com/flutter/flutter/issues/53078
