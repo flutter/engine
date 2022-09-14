53864c526de184b3ac6cf475e1477de2a4f3f708
Kingtous <kingtous@qq.com>
2022-09-14 17:59:39 +0800

    feat: initial custom cursor and cache support

--- shell/platform/windows/cursor_handler.cc
+++ shell/platform/windows/cursor_handler.cc
@@ -11,6 +11,7 @@
 static constexpr char kChannelName[] = "flutter/mousecursor";
 
 static constexpr char kActivateSystemCursorMethod[] = "activateSystemCursor";
+static constexpr char kSetSystemCursorMethod[] = "setSystemCursor";
 
 static constexpr char kKindKey[] = "kind";
 
@@ -44,6 +45,31 @@ void CursorHandler::HandleMethodCall(
     }
     const auto& kind = std::get<std::string>(kind_iter->second);
     delegate_->UpdateFlutterCursor(kind);
+    result->Success();
+  } else if (method.compare(kSetSystemCursorMethod) == 0) {
+    const auto& map = std::get<EncodableMap>(*method_call.arguments());
+    auto buffer = std::get<std::vector<uint8_t>>(
+        map.at(flutter::EncodableValue("buffer")));
+    auto width = std::get<int>(map.at(flutter::EncodableValue("width")));
+    auto height = std::get<int>(map.at(flutter::EncodableValue("height")));
+    auto hotx = std::get<double>(map.at(flutter::EncodableValue("hotx")));
+    auto hoty = std::get<double>(map.at(flutter::EncodableValue("hoty")));
+    HCURSOR cursor = nullptr;
+    // Flutter returns rawRgba, which has 8bits*4channels
+    auto bitmap = CreateBitmap(width, height, 1, 32, &buffer[0]);
+    if (bitmap == nullptr) {
+      result->Error("Argument error", "Invalid rawRgba bitmap from flutter");
+      return;
+    }
+    ICONINFO icon_info;
+    icon_info.fIcon = 0;
+    icon_info.xHotspot = hotx;
+    icon_info.yHotspot = hoty;
+    icon_info.hbmMask = bitmap;
+    icon_info.hbmColor = bitmap;
+    cursor = CreateIconIndirect(&icon_info);
+    DeleteObject(bitmap);
+    delegate_->SetFlutterCursor(cursor);
     result->Success();
   } else {
     result->NotImplemented();
