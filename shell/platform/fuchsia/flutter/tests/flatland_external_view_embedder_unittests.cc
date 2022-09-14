ab37f56c273f45b10c317a40ebd6bc397127e91f
Chris Fontas <cfontas@google.com>
2022-09-14 18:02:03 +0000

    fix formatting

--- shell/platform/fuchsia/flutter/tests/flatland_external_view_embedder_unittests.cc
+++ shell/platform/fuchsia/flutter/tests/flatland_external_view_embedder_unittests.cc
@@ -215,8 +215,8 @@ Matcher<FakeGraph> IsFlutterGraph(
       /*content_map*/ _, /*transform_map*/ _,
       Pointee(FieldsAre(
           /*id*/ _, FakeTransform::kDefaultTranslation,
-           FakeTransform::kDefaultScale, FakeTransform::kDefaultOrientation,
-           /*clip_bounds*/ _,
+          FakeTransform::kDefaultScale, FakeTransform::kDefaultOrientation,
+          /*clip_bounds*/ _,
           /*children*/ ElementsAreArray(layer_matchers),
           /*content*/ Eq(nullptr), /*num_hit_regions*/ _)),
       Eq(FakeView{
@@ -237,7 +237,8 @@ Matcher<std::shared_ptr<FakeTransform>> IsImageLayer(
     size_t num_hit_regions) {
   return Pointee(FieldsAre(
       /*id*/ _, FakeTransform::kDefaultTranslation,
-       FakeTransform::kDefaultScale, FakeTransform::kDefaultOrientation, /*clip_bounds*/ _,
+      FakeTransform::kDefaultScale, FakeTransform::kDefaultOrientation,
+      /*clip_bounds*/ _,
       /*children*/ IsEmpty(),
       /*content*/
       Pointee(VariantWith<FakeImage>(FieldsAre(
@@ -253,8 +254,8 @@ Matcher<std::shared_ptr<FakeTransform>> IsViewportLayer(
     const fuchsia::math::SizeU& view_logical_size,
     const fuchsia::math::Vec& view_transform) {
   return Pointee(
-      FieldsAre(/* id */ _, view_transform,
-                FakeTransform::kDefaultScale, FakeTransform::kDefaultOrientation, /*clip_bounds*/ _, 
+      FieldsAre(/* id */ _, view_transform, FakeTransform::kDefaultScale,
+                FakeTransform::kDefaultOrientation, /*clip_bounds*/ _,
                 /*children*/ IsEmpty(),
                 /*content*/
                 Pointee(VariantWith<FakeViewport>(FieldsAre(
