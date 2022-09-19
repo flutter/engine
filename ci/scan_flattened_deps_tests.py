56db17209783c3d24afc9e052151609e7271b613
Jesse Seales <jseales@google.com>
2022-09-19 16:54:22 -0400

    add back test

--- ci/scan_flattened_deps_tests.py
+++ ci/scan_flattened_deps_tests.py
@@ -1,12 +1,11 @@
 import unittest
-import scan_flattened_deps
-import requests
+from scan_flattened_deps import getCommonAncestorCommit
 
 class TestDepsScanMethods(unittest.TestCase):
 
     def test_common_ancestor_commit(self):
         dep = ['https://chromium.googlesource.com/webm/libwebp.git', '7dfde712a477e420968732161539011e0fd446cf']
-        self.assertEqual('fedac6cc69cda3e9e04b780d324cf03921fb3ff4', scan_flattened_deps.getCommonAncestorCommit(dep))
+        self.assertEqual('fedac6cc69cda3e9e04b780d324cf03921fb3ff4', getCommonAncestorCommit(dep))
 
 if __name__ == '__main__':
     unittest.main()
