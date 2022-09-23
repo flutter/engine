0e581b59de48243283c6a13142ec8bb4d39717da
Dan Field <dnfield@google.com>
2022-09-23 13:14:29 -0700

    C++

--- impeller/aiks/picture.cc
+++ impeller/aiks/picture.cc
@@ -52,12 +52,12 @@ std::shared_ptr<Texture> Picture::RenderToTexture(
 
   // This texture isn't host visible, but we might want to add host visible
   // features to Image someday.
-  auto impeller_context = *context.GetContext();
+  auto impeller_context = context.GetContext();
   RenderTarget target;
   if (impeller_context->SupportsOffscreenMSAA()) {
-    target = RenderTarget::CreateOffscreenMSAA(impeller_context, size);
+    target = RenderTarget::CreateOffscreenMSAA(*impeller_context, size);
   } else {
-    target = RenderTarget::CreateOffscreen(impeller_context, size);
+    target = RenderTarget::CreateOffscreen(*impeller_context, size);
   }
   if (!target.IsValid()) {
     VALIDATION_LOG << "Could not create valid RenderTarget.";
