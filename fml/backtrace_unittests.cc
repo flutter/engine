20b6dc45bb93579fb5511977e22034f8f7778d75
moko256 <koutaro.mo@gmail.com>
2022-09-26 03:13:13 +0900

    Use gmock's matcher

--- fml/backtrace_unittests.cc
+++ fml/backtrace_unittests.cc
@@ -4,6 +4,7 @@
 
 #include "backtrace.h"
 
+#include "gmock/gmock-matchers.h"
 #include "gtest/gtest.h"
 #include "logging.h"
 
@@ -34,20 +35,22 @@ TEST(BacktraceTest, CanGatherBacktrace) {
   }
 }
 
-TEST(BacktraceTest, BacktraceStartsWithCallerName) {
+TEST(BacktraceTest, BacktraceStartsWithCallerFunction) {
   auto trace = BacktraceHere(0);
   ASSERT_GT(trace.size(), 0u);
 
   auto first_line_end = trace.find("\n");
-  ASSERT_GT(first_line_end, 0u);
+  ASSERT_NE(first_line_end, std::string::npos);
 
-  if (trace.rfind("Unknown", first_line_end) != std::string::npos) {
+  auto first_line = trace.substr(0, first_line_end);
+
+  if (first_line.find("Unknown") != std::string::npos) {
     GTEST_SKIP() << "The symbols has been stripped from the executable.";
     return;
   }
 
-  ASSERT_NE(trace.rfind("Frame 0", first_line_end), std::string::npos);
-  ASSERT_NE(trace.rfind(__FUNCTION__, first_line_end), std::string::npos);
+  EXPECT_THAT(first_line, ::testing::HasSubstr("Frame 0"));
+  EXPECT_THAT(first_line, ::testing::HasSubstr(__FUNCTION__));
 }
 
 }  // namespace testing
