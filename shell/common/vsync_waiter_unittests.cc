c36a6bc366b3ac6c22d21fb1349b4e66b6d462d6
fzyzcjy <ch271828n@outlook.com>
2022-09-26 21:28:16 +0800

    add blank vsync_waiter_unittests.cc

---
+++ shell/common/vsync_waiter_unittests.cc
@@ -0,0 +1,17 @@
+#include <initializer_list>
+
+#include "flutter/common/settings.h"
+#include "flutter/fml/command_line.h"
+#include "flutter/shell/common/switches.h"
+
+#include "gtest/gtest.h"
+
+namespace flutter {
+namespace testing {
+
+TEST(VsyncWaiterTest, NoUnneededAwaitVsync) {
+  // TODO
+}
+
+}  // namespace testing
+}  // namespace flutter
