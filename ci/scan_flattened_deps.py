1f1f199cb3670ef644d127b1839c2a9ec52b0142
Jesse Seales <jseales@google.com>
2022-09-30 11:19:17 -0400

    use output

--- ci/scan_flattened_deps.py
+++ ci/scan_flattened_deps.py
@@ -262,10 +262,8 @@ def get_common_ancestor_commit(dep):
         ancestor_commit = ancestor_commit.decode().strip()
         print('FOUND ANCESTOR COMMIT: ' + ancestor_commit)
         return ancestor_commit
-      except SyntaxError as syntax_error:
-        print("SyntaxError '{0}' occured.".format(syntax_error.text))
-      except Exception as error:
-        print("Error '{0}' occured.  Type: {1}".format(str(error), type(error)))
+      except subprocess.CalledProcessError as error:
+        print("Subprocess error '{0}' occured.".format(error.output))
     else:
       print('did not find dep: ' + dep_name)
     return {}
