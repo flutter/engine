0702b111cb850383a8f5dbd9f07b10b94d76d262
schectman <schectman@google.com>
2022-10-03 14:15:38 -0400

    Spacing

--- shell/platform/windows/system_utils.cc
+++ shell/platform/windows/system_utils.cc
@@ -33,10 +33,9 @@ std::vector<std::wstring> GetPreferredLanguages(
   DWORD flags = MUI_LANGUAGE_NAME | MUI_UI_FALLBACK;
 
   // Determine where languages are defined and get buffer length
-  if (registry.GetRegistryValue(HKEY_CURRENT_USER,
-                                WIN_REG_I18N_PROFILE,
-                                WIN_REG_LANGUAGES, RRF_RT_REG_MULTI_SZ, NULL, NULL,
-                                &buffer_size) != ERROR_SUCCESS) {
+  if (registry.GetRegistryValue(HKEY_CURRENT_USER, WIN_REG_I18N_PROFILE,
+                                WIN_REG_LANGUAGES, RRF_RT_REG_MULTI_SZ, NULL,
+                                NULL, &buffer_size) != ERROR_SUCCESS) {
     languages_from_registry = FALSE;
     if (!::GetThreadPreferredUILanguages(flags, &count, nullptr,
                                          &buffer_size)) {
@@ -56,10 +55,10 @@ std::vector<std::wstring> GetPreferredLanguages(
   // Initialize the buffer
   std::wstring buffer(buffer_size, '\0');
   if (languages_from_registry) {
-    if (registry.GetRegistryValue(
-            HKEY_CURRENT_USER, WIN_REG_I18N_PROFILE,
-            WIN_REG_LANGUAGES, RRF_RT_REG_MULTI_SZ, NULL, buffer.data(),
-            &buffer_size) != ERROR_SUCCESS) {
+    if (registry.GetRegistryValue(HKEY_CURRENT_USER, WIN_REG_I18N_PROFILE,
+                                  WIN_REG_LANGUAGES, RRF_RT_REG_MULTI_SZ, NULL,
+                                  buffer.data(),
+                                  &buffer_size) != ERROR_SUCCESS) {
       return languages;
     }
   } else {
