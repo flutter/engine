e07b63ed8ee9f5c84c21e8db3e0e85a7e7385acb
Erik <erkln@google.com>
2022-10-10 17:48:58 +0000

    Throw on AppSnapshot

--- shell/platform/fuchsia/dart_runner/dart_component_controller.cc
+++ shell/platform/fuchsia/dart_runner/dart_component_controller.cc
@@ -158,6 +158,8 @@ bool DartComponentController::SetUp() {
   }
 
   if (SetUpFromAppSnapshot()) {
+    FX_LOGF(ERROR, LOG_TAG, "TESTING LOG: AppSnapshot");
+    return false;
     FX_LOGF(INFO, LOG_TAG, "%s is running from an app snapshot", url_.c_str());
   } else if (SetUpFromKernel()) {
     FX_LOGF(INFO, LOG_TAG, "%s is running from kernel", url_.c_str());
