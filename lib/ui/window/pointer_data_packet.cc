0c149ed5de26fd316feabcab722e66041e0bc3cc
fzyzcjy <ch271828n@outlook.com>
2022-10-19 07:35:07 +0800

    add boundary checks

--- lib/ui/window/pointer_data_packet.cc
+++ lib/ui/window/pointer_data_packet.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "flutter/lib/ui/window/pointer_data_packet.h"
+#include "flutter/fml/logging.h"
 
 #include <cstring>
 
@@ -17,10 +18,12 @@ PointerDataPacket::PointerDataPacket(uint8_t* data, size_t num_bytes)
 PointerDataPacket::~PointerDataPacket() = default;
 
 void PointerDataPacket::SetPointerData(size_t i, const PointerData& data) {
+  FML_DCHECK(i < GetLength());
   memcpy(&data_[i * sizeof(PointerData)], &data, sizeof(PointerData));
 }
 
 PointerData PointerDataPacket::GetPointerData(size_t i) const {
+  FML_DCHECK(i < GetLength());
   PointerData result;
   memcpy(&result, &data_[i * sizeof(PointerData)], sizeof(PointerData));
   return result;
