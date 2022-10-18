91c498cd9779591085c007d66a15d60c115085b6
fzyzcjy <ch271828n@outlook.com>
2022-10-18 10:31:05 +0800

    add blank file

---
+++ lib/ui/window/pointer_data_packet_merged_dispatcher.h
@@ -0,0 +1,19 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_LIB_UI_WINDOW_POINTER_DATA_PACKET_MERGED_DISPATCHER_H_
+#define FLUTTER_LIB_UI_WINDOW_POINTER_DATA_PACKET_MERGED_DISPATCHER_H_
+
+#include <cstring>
+#include <vector>
+
+namespace flutter {
+
+class PointerDataPacketMergedDispatcher {
+  // TODO
+};
+
+}  // namespace flutter
+
+#endif  // FLUTTER_LIB_UI_WINDOW_POINTER_DATA_PACKET_MERGED_DISPATCHER_H_
---
+++ lib/ui/window/pointer_data_packet_merged_dispatcher_unittests.cc
@@ -0,0 +1,17 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/lib/ui/window/pointer_data_packet_merged_dispatcher.h"
+
+#include <cstring>
+
+#include "gtest/gtest.h"
+
+namespace flutter {
+namespace testing {
+
+// TODO
+
+}  // namespace testing
+}  // namespace flutter
