7c8cd4b510c7fc2d461699b5fc9f4790ca5c5040
fzyzcjy <ch271828n@outlook.com>
2022-10-21 13:36:44 +0800

    empty PointerDataPeek

---
+++ shell/common/pointer_data_peek.h
@@ -0,0 +1,19 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef POINTER_DATA_PEEK_H_
+#define POINTER_DATA_PEEK_H_
+
+#include "flutter/runtime/runtime_controller.h"
+#include "flutter/shell/common/animator.h"
+
+namespace flutter {
+
+class PointerDataPeek {
+  // TODO
+};
+
+}  // namespace flutter
+
+#endif  // POINTER_DATA_PEEK_H_
