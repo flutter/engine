035f52326c26e412e09af89fa87f7dc4971d7d98
Kingtous <kingtous@qq.com>
2022-10-26 09:25:13 +0800

    opt: add cursor validity check

--- shell/platform/windows/cursor_handler.cc
+++ shell/platform/windows/cursor_handler.cc
@@ -14,6 +14,11 @@ static constexpr char kActivateSystemCursorMethod[] = "activateSystemCursor";
 static constexpr char kSetSystemCursorMethod[] = "setSystemCursor";
 
 static constexpr char kKindKey[] = "kind";
+static constexpr char kCustomCursorBufferKey[] = "buffer";
+static constexpr char kCustomCursorHotxKey[] = "hotx";
+static constexpr char kCustomCursorHotyKey[] = "hoty";
+static constexpr char kCustomCursorWidthKey[] = "width";
+static constexpr char kCustomCursorHeightKey[] = "height";
 
 namespace flutter {
 
@@ -47,18 +52,48 @@ void CursorHandler::HandleMethodCall(
     delegate_->UpdateFlutterCursor(kind);
     result->Success();
   } else if (method.compare(kSetSystemCursorMethod) == 0) {
-    const auto& map = std::get<EncodableMap>(*method_call.arguments());
+    const auto& arguments = std::get<EncodableMap>(*method_call.arguments());
+    auto buffer_iter = arguments.find(EncodableValue(std::string(kCustomCursorBufferKey)));
+    if (buffer_iter == arguments.end()) {
+      result->Error("Argument error",
+                    "Missing argument buffer while trying to customize system cursor");
+      return;
+    }
     auto buffer = std::get<std::vector<uint8_t>>(
-        map.at(flutter::EncodableValue("buffer")));
-    auto width = std::get<int>(map.at(flutter::EncodableValue("width")));
-    auto height = std::get<int>(map.at(flutter::EncodableValue("height")));
-    auto hotx = std::get<double>(map.at(flutter::EncodableValue("hotx")));
-    auto hoty = std::get<double>(map.at(flutter::EncodableValue("hoty")));
+        arguments.at(flutter::EncodableValue("buffer")));
+    auto width_iter = arguments.find(EncodableValue(std::string(kCustomCursorWidthKey)));
+    if (width_iter == arguments.end()) {
+      result->Error("Argument error",
+                    "Missing argument width while trying to customize system cursor");
+      return;
+    }
+    auto width = std::get<int>(arguments.at(flutter::EncodableValue("width")));
+    auto height_iter = arguments.find(EncodableValue(std::string(kCustomCursorHeightKey)));
+    if (height_iter == arguments.end()) {
+      result->Error("Argument error",
+                    "Missing argument height while trying to customize system cursor");
+      return;
+    }
+    auto height = std::get<int>(arguments.at(flutter::EncodableValue("height")));
+    auto hotx_iter = arguments.find(EncodableValue(std::string(kCustomCursorHotxKey)));
+    if (hotx_iter == arguments.end()) {
+      result->Error("Argument error",
+                    "Missing argument hotx while trying to customize system cursor");
+      return;
+    }
+    auto hotx = std::get<double>(arguments.at(flutter::EncodableValue("hotx")));
+    auto hoty_iter = arguments.find(EncodableValue(std::string(kCustomCursorHotyKey)));
+    if (hoty_iter == arguments.end()) {
+      result->Error("Argument error",
+                    "Missing argument hoty while trying to customize system cursor");
+      return;
+    }
+    auto hoty = std::get<double>(arguments.at(flutter::EncodableValue("hoty")));
     HCURSOR cursor = nullptr;
     // Flutter returns rawRgba, which has 8bits*4channels
     auto bitmap = CreateBitmap(width, height, 1, 32, &buffer[0]);
     if (bitmap == nullptr) {
-      result->Error("Argument error", "Invalid rawRgba bitmap from flutter");
+      result->Error("Argument error", "Argument buffer must contain valid rawRgba bitmap");
       return;
     }
     ICONINFO icon_info;
@@ -69,6 +104,10 @@ void CursorHandler::HandleMethodCall(
     icon_info.hbmColor = bitmap;
     cursor = CreateIconIndirect(&icon_info);
     DeleteObject(bitmap);
+    if (cursor == nullptr) {
+      result->Error("Argument error", "Create Icon failed, argument buffer must contain valid rawRgba bitmap");
+      return;
+    }
     delegate_->SetFlutterCursor(cursor);
     result->Success();
   } else {
