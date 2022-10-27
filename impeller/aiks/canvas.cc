068bc8ca076183b0724004d2bfd85bf7ee7c01fd
Brandon DeRosier <bdero@google.com>
2022-10-27 14:32:19 -0700

    Add comment about fixing up to respect the color source

--- impeller/aiks/canvas.cc
+++ impeller/aiks/canvas.cc
@@ -160,6 +160,8 @@ void Canvas::DrawPaint(const Paint& paint) {
 bool Canvas::AttemptDrawBlurredRRect(const Rect& rect,
                                      Scalar corner_radius,
                                      const Paint& paint) {
+  // TODO(114184): This should return false when the paint's ColorSource is not
+  //               solid color.
   if (!paint.mask_blur_descriptor.has_value() ||
       paint.mask_blur_descriptor->style != FilterContents::BlurStyle::kNormal ||
       paint.style != Paint::Style::kFill) {
