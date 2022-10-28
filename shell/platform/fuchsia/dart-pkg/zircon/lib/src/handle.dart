84f97cfdedfb8852ba809b723b8b39f4397446e7
Martin Kustermann <kustermann@google.com>
2022-10-28 10:40:45 +0200

    revert formatting of operator==

--- shell/platform/fuchsia/dart-pkg/zircon/lib/src/handle.dart
+++ shell/platform/fuchsia/dart-pkg/zircon/lib/src/handle.dart
@@ -31,8 +31,8 @@ class Handle extends NativeFieldWrapperClass1 {
 
   @override
   bool operator ==(Object other) {
-    return other is Handle &&
-        other.handle == handle;
+    return other is Handle
+        && other.handle == handle;
   }
 
   @override
