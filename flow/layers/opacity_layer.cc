4fc53e254c94e778c1c98b48ec3b67ce730240f9
Jim Graham <flar@google.com>
2022-10-28 16:59:37 -0700

    formatting

--- flow/layers/opacity_layer.cc
+++ flow/layers/opacity_layer.cc
@@ -81,7 +81,8 @@ void OpacityLayer::Paint(PaintContext& context) const {
 
   if (!children_can_accept_opacity()) {
     SkPaint paint;
-    if (layer_raster_cache_item_->Draw(context, context.state_stack.fill(paint))) {
+    if (layer_raster_cache_item_->Draw(context,
+                                       context.state_stack.fill(paint))) {
       return;
     }
   }
