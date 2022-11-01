a0dcb5ca4095785d245f4438aa906f9e064388c9
David Iglesias Teixeira <ditman@gmail.com>
2022-11-01 16:31:16 -0700

    Add test to check init stores config.

---
+++ lib/web_ui/test/canvaskit/initialization/initialization_stores_config.dart
@@ -0,0 +1,26 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:js/js_util.dart' as js_util;
+import 'package:test/bootstrap/browser.dart';
+import 'package:test/test.dart';
+import 'package:ui/src/engine.dart';
+
+void main() {
+  internalBootstrapBrowserTest(() => testMain);
+}
+
+void testMain() {
+  group('initializeEngineServices', () {
+    test('stores runtimeConfiguration', () async {
+      await initializeEngineServices(
+        runtimeConfiguration: js_util.jsify(<String, Object?>{
+          'canvasKitMaximumSurfaces': 32,
+        }) as JsFlutterConfiguration);
+
+      expect(configuration.canvasKitMaximumSurfaces, 32);
+
+    });
+  });
+}
