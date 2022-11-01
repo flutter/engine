a6eb42d5c43ca2eb7b479cf3619f7546baeb7e78
Tong Mu <dkwingsmt@gmail.com>
2022-11-01 13:51:53 -0700

    Correct key

--- third_party/web_keyboard_layouts/test/testing.dart
+++ third_party/web_keyboard_layouts/test/testing.dart
@@ -2,17 +2,29 @@
 import 'package:test/test.dart';
 import 'package:web_keyboard_layouts/web_keyboard_layouts.dart';
 
+final int _kLowerA = 'a'.codeUnitAt(0);
+final int _kUpperA = 'A'.codeUnitAt(0);
+final int _kLowerZ = 'z'.codeUnitAt(0);
+final int _kUpperZ = 'Z'.codeUnitAt(0);
+
+bool _isLetter(String char) {
+  if (char.length != 1) {
+    return false;
+  }
+  final int charCode = char.codeUnitAt(0);
+  return (charCode >= _kLowerA && charCode <= _kLowerZ)
+      || (charCode >= _kUpperA && charCode <= _kUpperZ);
+}
+
 void verifyEntry(LayoutMapping mapping, String eventCode, List<String> eventKeys, int logicalKey) {
-  // If the KeyboardEvent.code is a letter key such as "KeyA", then
-  // KeyboardEvent.keyCode is the upper letter such as "A". Otherwise, this
-  // field must not be used (in reality this field may or may not be platform
-  // independent).
+  // If the first two entry of KeyboardEvent.key are letter keys such as "a" and
+  // "A", then KeyboardEvent.keyCode is the upper letter such as "A". Otherwise,
+  // this field must not be used (in reality this field may or may not be
+  // platform independent).
   String? eventKeyCode;
   {
-    final RegExp regexLetterKey = RegExp(r'^Key([A-Z])$');
-    final RegExpMatch? matchLetterKey = regexLetterKey.firstMatch(eventCode);
-    if (matchLetterKey != null) {
-      eventKeyCode = matchLetterKey.group(1)!.toUpperCase();
+    if (_isLetter(eventKeys[0]) && _isLetter(eventKeys[1])) {
+      eventKeyCode = eventKeys[0].toUpperCase();
     }
   }
 
