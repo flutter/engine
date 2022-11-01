b2a02d2d7ab299afca92e9efdc48af5683ed9faa
Harry Terkelsen <het@google.com>
2022-11-01 10:51:22 -0700

    Move path test

--- lib/web_ui/lib/src/engine/canvaskit/path_metrics.dart
+++ lib/web_ui/lib/src/engine/canvaskit/path_metrics.dart
@@ -20,9 +20,11 @@ class CkPathMetrics extends IterableBase<ui.PathMetric>
 
   /// The [CkPath.isEmpty] case is special-cased to avoid booting the WASM machinery just to find out there are no contours.
   @override
-  Iterator<ui.PathMetric> get iterator => _path.isEmpty
+  Iterator<ui.PathMetric> get iterator => _iterator ??= _path.isEmpty
       ? const CkPathMetricIteratorEmpty._()
       : CkContourMeasureIter(this);
+
+  Iterator<ui.PathMetric>? _iterator;
 }
 
 class CkContourMeasureIter extends ManagedSkiaObject<SkContourMeasureIter>
