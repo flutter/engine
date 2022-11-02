af6fa522cfcb5f27e03c392d816f6fc69ae00612
Erik <erkln@google.com>
2022-11-02 18:29:11 +0000

    Address PR comments

--- shell/platform/fuchsia/flutter/tests/integration/utils/portable_ui_test.cc
+++ shell/platform/fuchsia/flutter/tests/integration/utils/portable_ui_test.cc
@@ -139,7 +139,7 @@ bool PortableUITest::HasViewConnected(zx_koid_t view_ref_koid) {
 void PortableUITest::LaunchClient() {
   scene_provider_ = realm_->Connect<fuchsia::ui::test::scene::Controller>();
   scene_provider_.set_error_handler(
-      [](auto) { FML_LOG(ERROR) << "Error from test scene provider"; });
+      [](auto) { FML_LOG(ERROR) << "Error from test scene provider: " << &zx_status_get_string; });
   fuchsia::ui::test::scene::ControllerAttachClientViewRequest request;
   request.set_view_provider(realm_->Connect<fuchsia::ui::app::ViewProvider>());
   scene_provider_->RegisterViewTreeWatcher(view_tree_watcher_.NewRequest(),
@@ -164,7 +164,7 @@ void PortableUITest::RegisterTouchScreen() {
   FML_LOG(INFO) << "Registering fake touch screen";
   input_registry_ = realm_->Connect<fuchsia::ui::test::input::Registry>();
   input_registry_.set_error_handler(
-      [](auto) { FML_LOG(ERROR) << "Error from input helper"; });
+      [](auto) { FML_LOG(ERROR) << "Error from input helper: " << &zx_status_get_string; });
 
   bool touchscreen_registered = false;
   fuchsia::ui::test::input::RegistryRegisterTouchScreenRequest request;
@@ -181,7 +181,7 @@ void PortableUITest::RegisterMouse() {
   FML_LOG(INFO) << "Registering fake mouse";
   input_registry_ = realm_->Connect<fuchsia::ui::test::input::Registry>();
   input_registry_.set_error_handler(
-      [](auto) { FML_LOG(ERROR) << "Error from input helper"; });
+      [](auto) { FML_LOG(ERROR) << "Error from input helper: " << &zx_status_get_string; });
 
   bool mouse_registered = false;
   fuchsia::ui::test::input::RegistryRegisterMouseRequest request;
