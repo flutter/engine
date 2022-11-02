12d54f9f0b2fff6d2c5e2e43076f32d59e8686ac
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-02 21:39:48 +0000

    Adding tarfile member sanitization to extractall()

--- tools/download_fuchsia_sdk.py
+++ tools/download_fuchsia_sdk.py
@@ -115,7 +115,26 @@ def ExtractGzipArchive(archive, host_os, verbose):
     print('Extracting "%s" to "%s"' % (archive, extract_dest))
 
   with tarfile.open(archive, 'r') as z:
-    z.extractall(extract_dest)
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(z, extract_dest)
 
   shutil.move(extract_dest, sdk_dest)
 
