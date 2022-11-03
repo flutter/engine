124c937ba26f17cced0c44493cdd7b0d825e7d27
Joshua Litt <joshualitt@google.com>
2022-11-03 22:49:43 +0000

    [web] Replace generative for factory constructor in interop classes.

--- lib/web_ui/lib/src/engine/configuration.dart
+++ lib/web_ui/lib/src/engine/configuration.dart
@@ -164,7 +164,9 @@ external JsFlutterConfiguration? get _jsConfiguration;
 /// The JS bindings for the object that's set as `window.flutterConfiguration`.
 @JS()
 @staticInterop
-class JsFlutterConfiguration {}
+class JsFlutterConfiguration {
+  external factory JsFlutterConfiguration();
+}
 
 extension JsFlutterConfigurationExtension on JsFlutterConfiguration {
   external String? get canvasKitBaseUrl;
