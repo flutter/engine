f9b6c213bbf6982e888a1726014f9aa60eeb5e9d
fzyzcjy <ch271828n@outlook.com>
2022-11-05 13:27:36 +0800

    unit tests

--- shell/common/rasterizer_sleep_strategy_unittests.cc
+++ shell/common/rasterizer_sleep_strategy_unittests.cc
@@ -9,8 +9,119 @@
 #include <memory>
 #include <optional>
 
+#include "gtest/gtest.h"
+
 namespace flutter {
+namespace testing {
 
-// TODO
+TEST(RasterizerSleepStrategyTest, WhenConstantlyOneFrameShouldNeverSleep) {
+  const auto frame_budget = fml::TimeDelta::FromMicroseconds(16667);
+  RasterizerSleepStrategy strategy;
 
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16667)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(32000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(33333)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(48000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(50000)),
+                            frame_budget),
+            std::nullopt);
+}
+
+TEST(RasterizerSleepStrategyTest, WhenConstantlyTwoFramesShouldNeverSleep) {
+  const auto frame_budget = fml::TimeDelta::FromMicroseconds(16667);
+  RasterizerSleepStrategy strategy;
+
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(0)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(32000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16667)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(48000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(33333)),
+                            frame_budget),
+            std::nullopt);
+}
+
+TEST(RasterizerSleepStrategyTest, WhenConstantlyThreeFramesShouldNeverSleep) {
+  const auto frame_budget = fml::TimeDelta::FromMicroseconds(16667);
+  RasterizerSleepStrategy strategy;
+
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(-16667)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(32000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(0)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(48000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16667)),
+                            frame_budget),
+            std::nullopt);
+}
+
+TEST(RasterizerSleepStrategyTest, WhenOneLessFrameShouldSleep) {
+  const auto frame_budget = fml::TimeDelta::FromMicroseconds(16667);
+  RasterizerSleepStrategy strategy;
+
+  // normally it is 32ms delay from vsync start to raster end
+  // i.e. 16ms from vsync *target* time to raster end
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(1000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(0)),
+                            frame_budget),
+            std::nullopt);
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(18000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(16667)),
+                            frame_budget),
+            std::nullopt);
+
+  // suddenly this rasterization is too fast
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(33000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(33333)),
+                            frame_budget),
+            std::optional(fml::TimePoint::FromEpochDelta(
+                fml::TimeDelta::FromMicroseconds(33333 + 500))));
+
+  // ... and then it becomes normal delay again
+  EXPECT_EQ(strategy.Handle(fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(52000)),
+                            fml::TimePoint::FromEpochDelta(
+                                fml::TimeDelta::FromMicroseconds(50000)),
+                            frame_budget),
+            std::nullopt);
+}
+
+}  // namespace testing
 }  // namespace flutter
