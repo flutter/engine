5b6c759be19bef901599f402d5fc72d669a48cd6
fzyzcjy <ch271828n@outlook.com>
2022-11-05 13:08:11 +0800

    add blank rasterizer_sleep_strategy

---
+++ shell/common/rasterizer_sleep_strategy.h
@@ -0,0 +1,19 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SHELL_COMMON_RASTERIZER_SLEEP_STRATEGY_H_
+#define SHELL_COMMON_RASTERIZER_SLEEP_STRATEGY_H_
+
+#include <memory>
+#include <optional>
+
+namespace flutter {
+
+class RasterizerSleepStrategy {
+  // TODO
+};
+
+}  // namespace flutter
+
+#endif  // SHELL_COMMON_RASTERIZER_SLEEP_STRATEGY_H_
---
+++ shell/common/rasterizer_sleep_strategy_unittests.cc
@@ -0,0 +1,16 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#define FML_USED_ON_EMBEDDER
+
+#include "flutter/shell/common/rasterizer_sleep_strategy.h"
+
+#include <memory>
+#include <optional>
+
+namespace flutter {
+
+// TODO
+
+}  // namespace flutter
