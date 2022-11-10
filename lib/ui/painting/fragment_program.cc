ce8daa578d19c23839407c641a1755b002a54449
jonahwilliams <jonahwilliams@google.com>
2022-11-10 11:03:39 -0800

    ++

--- lib/ui/painting/fragment_program.cc
+++ lib/ui/painting/fragment_program.cc
@@ -79,7 +79,8 @@ std::string FragmentProgram::initFromAsset(const std::string& asset_name) {
     Dart_PropagateError(ths);
   }
   std::cerr << asset_name << std::endl;
-  std::cerr << "Setting sampled_image_count " << sampled_image_count << std::endl;
+  std::cerr << "Setting sampled_image_count " << sampled_image_count
+            << std::endl;
   Dart_Handle result = Dart_SetField(ths, tonic::ToDart("_samplerCount"),
                                      Dart_NewInteger(sampled_image_count));
   if (Dart_IsError(result)) {
