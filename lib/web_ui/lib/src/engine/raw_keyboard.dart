af10046d3a962d1ea22bf74ca4b0949c0115d54c
Hassan Toor <hassantoor@google.com>
2022-11-22 11:46:15 -0600

    Intercept keydown in raw_keyboard, add tests

--- lib/web_ui/lib/src/engine/raw_keyboard.dart
+++ lib/web_ui/lib/src/engine/raw_keyboard.dart
@@ -96,6 +96,14 @@ class RawKeyboard {
     final FlutterHtmlKeyboardEvent event = FlutterHtmlKeyboardEvent(domEvent as DomKeyboardEvent);
     final String timerKey = event.code!;
 
+    // During IME composition, Tab fires twice (once for composition and once
+    // for regular tabbing behavior), which causes issues. Intercepting the
+    // tab keydown event during composition prevents these issues from occurring.
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/keydown_event#ignoring_keydown_during_ime_composition
+    if (event.type == 'keydown' && event.key == 'Tab' && event.isComposing) {
+      return;
+    }
+
     // Don't handle synthesizing a keyup event for modifier keys
     if (!_isModifierKey(event) && _shouldDoKeyGuard()) {
       _keydownTimers[timerKey]?.cancel();
