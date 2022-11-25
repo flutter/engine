dba4dfbcdf42a4ad3f8c69a7e0db8c2b5dcb1c79
xuty <xty50337@hotmail.com>
2022-11-25 11:55:15 +0800

    Add comments

--- lib/web_ui/lib/src/engine/texture.dart
+++ lib/web_ui/lib/src/engine/texture.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/// A collection of textures available to the engine.
 abstract class TextureRegistry {
   /// Registers a texture.
   int registerTexture(Object source);
