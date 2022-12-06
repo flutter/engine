f2b527336126f95550fe2084f9b53a747a76c96b
Reid Baker <reidbaker@google.com>
2022-12-06 09:56:59 -0800

    formatting java 3

--- shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
+++ shell/platform/android/io/flutter/plugin/platform/PlatformPlugin.java
@@ -205,8 +205,9 @@ public class PlatformPlugin {
     Log.e("Flutter", "hapticFeedbackIsEnabled");
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
       Log.e("Flutter", "Pre R version code");
-      return Settings.System.getInt(activity.getContentResolver(),
-        Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+      return Settings.System.getInt(
+         activity.getContentResolver(), Settings.System.HAPTIC_FEEDBACK_ENABLED, 1)
+         != 0;
     } else {
       // Haptic feedback was replaced by USAGE_TOUCH starting in api 30 and removed in api 33
       // See https://developer.android.com/reference/android/os/VibrationAttributes#USAGE_TOUCH
