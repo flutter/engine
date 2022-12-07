264e3a4cfd9afaa8a270800a02f982fb5ed03f0a
David Iglesias Teixeira <ditman@gmail.com>
2022-12-06 18:22:01 -0800

    Test hot_restart_cache_handler
    Simplify API a little bit, make clear method private.

--- lib/web_ui/lib/src/engine/view_embedder/hot_restart_cache_handler.dart
+++ lib/web_ui/lib/src/engine/view_embedder/hot_restart_cache_handler.dart
@@ -2,22 +2,25 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:meta/meta.dart';
+
 import '../dom.dart';
 import '../safe_browser_api.dart';
 
 /// Handles elements that need to be cleared after a hot-restart.
 class HotRestartCacheHandler {
-  HotRestartCacheHandler([this.storeName = '__flutter_state']) {
+  HotRestartCacheHandler() {
     if (_elements.isNotEmpty) {
       // We are in a post hot-restart world, clear the elements now.
-      clearAllElements();
+      _clearAllElements();
     }
   }
 
   /// This is state persistent across hot restarts that indicates what
   /// to clear.  Delay removal of old visible state to make the
   /// transition appear smooth.
-  final String storeName;
+  @visibleForTesting
+  static const String defaultCacheName = '__flutter_state';
 
   /// The js-interop layer backing [_elements].
   ///
@@ -27,22 +30,23 @@ class HotRestartCacheHandler {
 
   /// The elements that need to be cleaned up after hot-restart.
   List<DomElement?> get _elements {
-    _jsElements = getJsProperty<List<DomElement?>?>(domWindow, storeName);
+    _jsElements =
+        getJsProperty<List<DomElement?>?>(domWindow, defaultCacheName);
     if (_jsElements == null) {
       _jsElements = <DomElement?>[];
-      setJsProperty(domWindow, storeName, _jsElements);
+      setJsProperty(domWindow, defaultCacheName, _jsElements);
     }
     return _jsElements!;
   }
 
-  void registerElement(DomElement element) {
-    _elements.add(element);
-  }
-
-  void clearAllElements() {
+  void _clearAllElements() {
     for (final DomElement? element in _elements) {
       element?.remove();
     }
     _elements.clear();
+  }
+
+  void registerElement(DomElement element) {
+    _elements.add(element);
   }
 }
