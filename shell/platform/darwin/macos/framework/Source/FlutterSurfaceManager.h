117b15c32ce8d17c54c650b683469ba54b7d5696
Matej Knopp <matej.knopp@gmail.com>
2022-12-13 19:33:12 +0100

    Update shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h
    Co-authored-by: Chris Bracken <chris@bracken.jp>

--- shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h
+++ shell/platform/darwin/macos/framework/Source/FlutterSurfaceManager.h
@@ -21,9 +21,9 @@
 @protocol FlutterSurfaceManagerDelegate <NSObject>
 
 /*
- * Schedules the block on platform thread and waits until the block is executed.
- * Provided `frameSize` is used to unblock platform thread if it waits for
- * certain frame size during resizing.
+ * Schedules the block on the platform thread and blocks until the block is executed.
+ * Provided `frameSize` is used to unblock the platform thread if it waits for
+ * a certain frame size during resizing.
  */
 - (void)onPresent:(CGSize)frameSize withBlock:(nonnull dispatch_block_t)block;
 
