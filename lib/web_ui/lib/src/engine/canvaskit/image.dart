4f8587f8e60b4698c3c207335f91a895b9b4a1f5
alanwutang11 <alanwumail11@gmail.com>
2022-12-16 13:23:44 -0800

    use canvaskit toByteData for unsupported videoFrame formats

--- lib/web_ui/lib/src/engine/canvaskit/image.dart
+++ lib/web_ui/lib/src/engine/canvaskit/image.dart
@@ -376,7 +376,8 @@ class CkImage implements ui.Image, StackTraceDebugger {
     ui.ImageByteFormat format = ui.ImageByteFormat.rawRgba,
   }) {
     assert(_debugCheckIsNotDisposed());
-    if (videoFrame != null) {
+    // Web codecs do not support I420, I444, I422 videoFrame formats.
+    if (videoFrame != null && videoFrame!.format != 'I420' && videoFrame!.format != 'I444' && videoFrame!.format != 'I422') {
       return readPixelsFromVideoFrame(videoFrame!, format);
     } else {
       return _readPixelsFromSkImage(format);
