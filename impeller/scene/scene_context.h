44425a60bf2fcd76062e26ddfdfd0de04d062dc7
Brandon DeRosier <bdero@google.com>
2022-12-20 20:48:51 -0800

    [Impeller Scene] Make Geometry/Materials own command binding behavior

--- impeller/scene/scene_context.h
+++ impeller/scene/scene_context.h
@@ -7,15 +7,12 @@
 #include <memory>
 
 #include "impeller/renderer/context.h"
+#include "impeller/renderer/pipeline.h"
 #include "impeller/renderer/pipeline_descriptor.h"
-#include "impeller/scene/shaders/geometry.vert.h"
-#include "impeller/scene/shaders/unlit.frag.h"
+#include "impeller/scene/pipeline_key.h"
 
 namespace impeller {
 namespace scene {
-
-using UnlitPipeline =
-    RenderPipelineT<GeometryVertexShader, UnlitFragmentShader>;
 
 struct SceneContextOptions {
   SampleCount sample_count = SampleCount::kCount1;
@@ -46,14 +43,13 @@ class SceneContext {
 
   bool IsValid() const;
 
+  std::shared_ptr<Pipeline<PipelineDescriptor>> GetPipeline(
+      PipelineKey key,
+      SceneContextOptions opts) const;
+
   std::shared_ptr<Context> GetContext() const;
 
   std::shared_ptr<Texture> GetPlaceholderTexture() const;
-
-  std::shared_ptr<Pipeline<PipelineDescriptor>> GetUnlitPipeline(
-      SceneContextOptions opts) const {
-    return GetPipeline(unlit_pipeline_, opts);
-  }
 
  private:
   template <class T>
@@ -61,37 +57,68 @@ class SceneContext {
                                       std::unique_ptr<T>,
                                       SceneContextOptions::Hash,
                                       SceneContextOptions::Equal>;
-  template <class TypedPipeline>
-  std::shared_ptr<Pipeline<PipelineDescriptor>> GetPipeline(
-      Variants<TypedPipeline>& container,
-      SceneContextOptions opts) const {
-    if (!IsValid()) {
-      return nullptr;
+
+  class PipelineCollection {
+   public:
+    virtual ~PipelineCollection() = default;
+
+    virtual std::shared_ptr<Pipeline<PipelineDescriptor>> GetPipeline(
+        SceneContextOptions opts) = 0;
+  };
+
+  template <class PipelineT>
+  class PipelineCollectionT final : public PipelineCollection {
+   public:
+    explicit PipelineCollectionT(Context& context) {
+      auto desc = PipelineT::Builder::MakeDefaultPipelineDescriptor(context);
+      // Apply default ContentContextOptions to the descriptor.
+      SceneContextOptions{}.ApplyToPipelineDescriptor(*desc);
+      pipelines_[{}] = std::make_unique<PipelineT>(context, desc);
+    };
+
+    // |PipelineCollection|
+    std::shared_ptr<Pipeline<PipelineDescriptor>> GetPipeline(
+        SceneContextOptions opts) {
+      if (auto found = pipelines_.find(opts); found != pipelines_.end()) {
+        return found->second->WaitAndGet();
+      }
+
+      auto prototype = pipelines_.find({});
+
+      // The prototype must always be initialized in the constructor.
+      FML_CHECK(prototype != pipelines_.end());
+
+      auto variant_future = prototype->second->WaitAndGet()->CreateVariant(
+          [&opts,
+           variants_count = pipelines_.size()](PipelineDescriptor& desc) {
+            opts.ApplyToPipelineDescriptor(desc);
+            desc.SetLabel(
+                SPrintF("%s V#%zu", desc.GetLabel().c_str(), variants_count));
+          });
+      auto variant = std::make_unique<PipelineT>(std::move(variant_future));
+      auto variant_pipeline = variant->WaitAndGet();
+      pipelines_[opts] = std::move(variant);
+      return variant_pipeline;
     }
 
-    if (auto found = container.find(opts); found != container.end()) {
-      return found->second->WaitAndGet();
-    }
+   private:
+    Variants<PipelineT> pipelines_;
+  };
 
-    auto prototype = container.find({});
-
-    // The prototype must always be initialized in the constructor.
-    FML_CHECK(prototype != container.end());
-
-    auto variant_future = prototype->second->WaitAndGet()->CreateVariant(
-        [&opts, variants_count = container.size()](PipelineDescriptor& desc) {
-          opts.ApplyToPipelineDescriptor(desc);
-          desc.SetLabel(
-              SPrintF("%s V#%zu", desc.GetLabel().c_str(), variants_count));
-        });
-    auto variant = std::make_unique<TypedPipeline>(std::move(variant_future));
-    auto variant_pipeline = variant->WaitAndGet();
-    container[opts] = std::move(variant);
-    return variant_pipeline;
+  template <typename VertexShader, typename FragmentShader>
+  std::unique_ptr<PipelineCollection> MakePipelineCollection(Context& context) {
+    return std::make_unique<
+        PipelineCollectionT<RenderPipelineT<VertexShader, FragmentShader>>>(
+        context);
   }
 
+  std::unordered_map<PipelineKey,
+                     std::unique_ptr<PipelineCollection>,
+                     PipelineKey::Hash,
+                     PipelineKey::Equal>
+      pipelines_;
+
   std::shared_ptr<Context> context_;
-  mutable Variants<UnlitPipeline> unlit_pipeline_;
 
   bool is_valid_ = false;
   // A 1x1 opaque white texture that can be used as a placeholder binding.
