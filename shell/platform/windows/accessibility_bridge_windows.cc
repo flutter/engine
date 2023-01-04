15f9811ca238354c65263347014c726261589d59
schectman <schectman@google.com>
2023-01-04 10:38:19 -0500

    Limit selection change to focused node on Windows

--- shell/platform/windows/accessibility_bridge_windows.cc
+++ shell/platform/windows/accessibility_bridge_windows.cc
@@ -41,10 +41,16 @@ void AccessibilityBridgeWindows::OnAccessibilityEvent(
       DispatchWinAccessibilityEvent(win_delegate,
                                     ax::mojom::Event::kChildrenChanged);
       break;
-    case ui::AXEventGenerator::Event::DOCUMENT_SELECTION_CHANGED:
+    case ui::AXEventGenerator::Event::DOCUMENT_SELECTION_CHANGED: {
+      ui::AXNode::AXID focus_id = GetAXTreeData().sel_focus_object_id;
+      auto focus_delegate =
+          GetFlutterPlatformNodeDelegateFromID(focus_id).lock();
       DispatchWinAccessibilityEvent(
-          win_delegate, ax::mojom::Event::kDocumentSelectionChanged);
+          std::static_pointer_cast<FlutterPlatformNodeDelegateWindows>(
+              focus_delegate),
+          ax::mojom::Event::kDocumentSelectionChanged);
       break;
+    }
     case ui::AXEventGenerator::Event::FOCUS_CHANGED:
       DispatchWinAccessibilityEvent(win_delegate, ax::mojom::Event::kFocus);
       SetFocus(win_delegate);
