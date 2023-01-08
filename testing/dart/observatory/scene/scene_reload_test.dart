16c0451c1a1feec11de6f5cad4f887529fb957e2
Brandon DeRosier <bdero@google.com>
2023-01-07 22:19:26 -0800

    Ignore lints in observatory test

--- testing/dart/observatory/scene/scene_reload_test.dart
+++ testing/dart/observatory/scene/scene_reload_test.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// ignore_for_file: type=lint
+
 import 'dart:developer' as developer;
 import 'dart:typed_data';
 import 'dart:ui';
