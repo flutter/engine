ce23f7e718d16d3f95debff1a1fcb8e0d0355974
Jackson Gardner <jacksongardner@google.com>
2023-01-13 16:14:46 -0800

    A few more fixups.

--- lib/web_ui/test/raw_keyboard_test.dart
+++ lib/web_ui/test/raw_keyboard_test.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:js_util' as js_util;
 import 'dart:typed_data';
 
 import 'package:quiver/testing/async.dart';
