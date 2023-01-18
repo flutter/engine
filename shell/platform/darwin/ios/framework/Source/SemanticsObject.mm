bd14815313d721584765318f6b8fc900351b0e58
hangyu <108393416+hangyujin@users.noreply.github.com>
2023-01-17 16:28:22 -0800

    Update SemanticsObject.mm

--- shell/platform/darwin/ios/framework/Source/SemanticsObject.mm
+++ shell/platform/darwin/ios/framework/Source/SemanticsObject.mm
@@ -534,8 +534,7 @@ CGRect ConvertRectToGlobal(SemanticsObject* reference, CGRect local_rect) {
 - (SemanticsObject*)search:(SemanticsObject*)semanticsObject withPoint:(CGPoint)point {
   if ([semanticsObject children].count == 0) {
     // Check if the current semantic object should be returned.
-    if ([semanticsObject containsPoint:point] &&
-        [self isFocusable:semanticsObject]) {
+    if ([semanticsObject containsPoint:point] && [self isFocusable:semanticsObject]) {
       return semanticsObject;
     }
     return nil;
@@ -556,8 +555,7 @@ CGRect ConvertRectToGlobal(SemanticsObject* reference, CGRect local_rect) {
   }
 
   // Check if the current semantic object should be returned.
-  if ([semanticsObject containsPoint:point] &&
-      [self isFocusable:semanticsObject]) {
+  if ([semanticsObject containsPoint:point] && [self isFocusable:semanticsObject]) {
     return semanticsObject;
   }
   return nil;
