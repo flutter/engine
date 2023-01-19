e8a062faaef9d6d3a8317dfa8d01cfeb3b476074
luckysmg <2539699336@qq.com>
2023-01-19 23:38:12 +0800

    ++

--- third_party/spring_animation/spring_animation.h
+++ third_party/spring_animation/spring_animation.h
@@ -14,23 +14,30 @@
 #include <Foundation/NSObject.h>
 
 // This simplified spring model is based off of a damped harmonic oscillator.
-// See: https://en.wikipedia.org/wiki/Harmonic_oscillator#Damped_harmonic_oscillator
+// See:
+// https://en.wikipedia.org/wiki/Harmonic_oscillator#Damped_harmonic_oscillator
 //
-// This models the closed form of the second order differential equation which happens to match the
-// algorithm used by CASpringAnimation, a QuartzCore (iOS) API that creates spring animations.
+// This models the closed form of the second order differential equation which
+// happens to match the algorithm used by CASpringAnimation, a QuartzCore (iOS)
+// API that creates spring animations.
 @interface SpringAnimation : NSObject
 
 - (instancetype)initWithStiffness:(double)stiffness
                           damping:(double)damping
                              mass:(double)mass
-                  initialVelocity:(double)initialVelocity;
+                  initialVelocity:(double)initialVelocity
+                        fromValue:(double)fromValue
+                          toValue:(double)toValue;
 
 - (double)curveFunction:(double)t;
 
-@property(nonatomic, assign) double initialVelocity;
-@property(nonatomic, assign) double zeta;
-@property(nonatomic, assign) double omega0;
-@property(nonatomic, assign) double omega1;
+@property(nonatomic, assign, readonly) double stiffness;
+@property(nonatomic, assign, readonly) double damping;
+@property(nonatomic, assign, readonly) double mass;
+@property(nonatomic, assign, readonly) double initialVelocity;
+@property(nonatomic, assign, readonly) double fromValue;
+@property(nonatomic, assign, readonly) double toValue;
+
 @end
 
 #endif  // FLUTTER_SHELL_PLATFORM_DARWIN_IOS_FRAMEWORK_SOURCE_SPRING_ANIMATION_H_
