6ad04032990dba95ddfa868c45898b5ab226194e
schectman <schectman@google.com>
2023-01-27 15:00:33 -0500

    Formatting

--- shell/platform/windows/settings_plugin_unittests.cc
+++ shell/platform/windows/settings_plugin_unittests.cc
@@ -21,14 +21,12 @@ class MockSettingsPlugin : public SettingsPlugin {
 
   virtual ~MockSettingsPlugin() = default;
 
+  bool is_high_contrast() { return is_high_contrast_; }
+
   // |SettingsPlugin|
   MOCK_METHOD0(GetAlwaysUse24HourFormat, bool());
   MOCK_METHOD0(GetTextScaleFactor, float());
   MOCK_METHOD0(GetPreferredBrightness, PlatformBrightness());
-
-  bool is_high_contrast() {
-    return is_high_contrast_;
-  }
 
   MOCK_METHOD0(WatchPreferredBrightnessChanged, void());
   MOCK_METHOD0(WatchTextScaleFactorChanged, void());
