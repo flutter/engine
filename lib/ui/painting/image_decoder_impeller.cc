d3a59883296f9e86bef75792435f28c0e04c2cd0
Aaron Clarke <aaclarke@google.com>
2023-01-27 11:14:49 -0800

    fixed stored srgb gamut variable

--- lib/ui/painting/image_decoder_impeller.cc
+++ lib/ui/painting/image_decoder_impeller.cc
@@ -49,7 +49,7 @@ float CalculateArea(SkPoint abc[3]) {
                       b.fX * a.fY);
 }
 
-constexpr float kSRGB_D50_GamutArea = 0.084f;
+static constexpr float kSrgbD50GamutArea = 0.084f;
 
 // Source:
 // https://source.chromium.org/chromium/_/skia/skia.git/+/393fb1ec80f41d8ad7d104921b6920e69749fda1:src/codec/SkAndroidCodec.cpp;l=67;drc=46572b4d445f41943059d0e377afc6d6748cd5ca;bpv=1;bpt=0
@@ -58,7 +58,7 @@ bool IsWideGamut(const SkColorSpace& color_space) {
   color_space.toXYZD50(&xyzd50);
   SkPoint rgb[3];
   LoadGamut(rgb, xyzd50);
-  return CalculateArea(rgb) > kSRGB_D50_GamutArea;
+  return CalculateArea(rgb) > kSrgbD50GamutArea;
 }
 }  // namespace
 
