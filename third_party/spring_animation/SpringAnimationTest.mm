c682e72c28f1daa89deac967b6a2a3127e01a18b
luckysmg <2539699336@qq.com>
2023-02-01 13:16:07 +0800

    ++

--- third_party/spring_animation/SpringAnimationTest.mm
+++ third_party/spring_animation/SpringAnimationTest.mm
@@ -2,33 +2,26 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <OCMock/OCMock.h>
-#import <XCTest/XCTest.h>
-
+#include <cmath>
 #import "flutter/third_party/spring_animation/spring_animation.h"
+#include "gtest/gtest.h"
 
-@interface SpringAnimationTest : XCTestCase
-
-@end
-
-@implementation SpringAnimationTest
-
-- (void)testSpringAnimationInitializeCorrectly {
+TEST(SpringAnimationTest, CanInitializeCorrectly) {
   SpringAnimation* animation = [[SpringAnimation alloc] initWithStiffness:1000
                                                                   damping:500
                                                                      mass:3
                                                           initialVelocity:0
                                                                 fromValue:0
                                                                   toValue:1000];
-  XCTAssert(animation.stiffness == 1000);
-  XCTAssert(animation.damping == 500);
-  XCTAssert(animation.mass == 3);
-  XCTAssert(animation.initialVelocity == 0);
-  XCTAssert(animation.fromValue == 0);
-  XCTAssert(animation.toValue == 1000);
+  ASSERT_TRUE(animation.stiffness == 1000);
+  ASSERT_TRUE(animation.damping == 500);
+  ASSERT_TRUE(animation.mass == 3);
+  ASSERT_TRUE(animation.initialVelocity == 0);
+  ASSERT_TRUE(animation.fromValue == 0);
+  ASSERT_TRUE(animation.toValue == 1000);
 }
 
-- (void)testSpringAnimationCurveFunctionWorksCorrectly {
+TEST(SpringAnimationTest, CurveFunctionCanWorkCorrectly) {
   // Here is the keyboard curve config on iOS platform.
   SpringAnimation* animation = [[SpringAnimation alloc] initWithStiffness:1000
                                                                   damping:500
@@ -41,9 +34,7 @@
   const double endTime = 0.6;
 
   const double startValue = [animation curveFunction:startTime];
-  XCTAssertEqualWithAccuracy(startValue, animation.fromValue, accuracy);
+  ASSERT_TRUE(fabs(startValue - animation.fromValue) < accuracy);
   const double toValue = [animation curveFunction:endTime];
-  XCTAssertEqualWithAccuracy(toValue, animation.toValue, accuracy);
+  ASSERT_TRUE(fabs(toValue - animation.toValue) < accuracy);
 }
-
-@end
