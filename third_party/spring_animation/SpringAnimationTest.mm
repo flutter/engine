29b8ace99daa49e44e3f80eb88d622cebb2c475a
Jake Schafer <pickle.plaza@gmail.com>
2023-02-03 00:51:12 -0600

    updated test

--- third_party/spring_animation/SpringAnimationTest.mm
+++ third_party/spring_animation/SpringAnimationTest.mm
@@ -39,23 +39,27 @@ TEST(SpringAnimationTest, CurveFunctionCanWorkCorrectly) {
   ASSERT_TRUE(fabs(toValue - animation.toValue) < accuracy);
 }
 
-TEST(SpringAnimationTest, CanUpdatePositionValuesCorrectly) {
+TEST(SpringAnimationTest, CanUpdatePositionValuesAndCalculateCorrectly) {
   SpringAnimation* animation = [[SpringAnimation alloc] initWithStiffness:1000
                                                                   damping:500
                                                                      mass:3
                                                           initialVelocity:0
                                                                 fromValue:0
                                                                   toValue:1000];
-  ASSERT_TRUE(animation.stiffness == 1000);
-  ASSERT_TRUE(animation.damping == 500);
-  ASSERT_TRUE(animation.mass == 3);
-  ASSERT_TRUE(animation.initialVelocity == 0);
-  ASSERT_TRUE(animation.fromValue == 0);
-  ASSERT_TRUE(animation.toValue == 1000);
+  const double startTime = 0;
+  const double endTime = 0.6;
+
+  const double startValue1 = [animation curveFunction:startTime];
+  const double toValue1 = [animation curveFunction:endTime];
 
   animation.fromValue = 10;
   animation.toValue = 800;
 
   ASSERT_TRUE(animation.fromValue == 10);
   ASSERT_TRUE(animation.toValue == 800);
+
+  const double startValue2 = [animation curveFunction:startTime];
+  const double toValue2 = [animation curveFunction:endTime];
+  ASSERT_TRUE(startValue2 > startValue1);
+  ASSERT_TRUE(toValue2 < toValue1);
 }
