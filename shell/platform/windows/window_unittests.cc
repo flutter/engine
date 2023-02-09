fa8b39cf2ed9866d0965886d6bc8a31bcce16da8
schectman <schectman@google.com>
2023-02-09 11:52:04 -0500

    Formatting

--- shell/platform/windows/window_unittests.cc
+++ shell/platform/windows/window_unittests.cc
@@ -364,12 +364,13 @@ TEST(MockWindow, UnknownPointerTypeSkipsDirectManipulation) {
 TEST(MockWindow, GetObjectUia) {
   MockWindow window;
   bool uia_called = false;
-  ON_CALL(window, OnGetObject).WillByDefault(Invoke([&uia_called](UINT msg, WPARAM wpar, LPARAM lpar){
+  ON_CALL(window, OnGetObject)
+      .WillByDefault(Invoke([&uia_called](UINT msg, WPARAM wpar, LPARAM lpar) {
 #ifdef FLUTTER_ENGINE_USE_UIA
-    uia_called = true;
+        uia_called = true;
 #endif  // FLUTTER_ENGINE_USE_UIA
-    return static_cast<LRESULT>(0);
-  }));
+        return static_cast<LRESULT>(0);
+      }));
   EXPECT_CALL(window, OnGetObject).Times(1);
 
   window.InjectWindowMessage(WM_GETOBJECT, 0, UiaRootObjectId);
