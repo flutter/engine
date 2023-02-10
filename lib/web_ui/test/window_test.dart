1e1d41a9af1fbe833d6f912ed1f3e6e22f1e2031
Loic Sharma <sharma.loic@gmail.com>
2023-02-09 17:17:40 -0800

    [dart:ui] Introduce `PlatformDispatcher.implicitView`
    Part of https://github.com/flutter/flutter/issues/120306

--- lib/web_ui/test/window_test.dart
+++ lib/web_ui/test/window_test.dart
@@ -41,6 +41,12 @@ void testMain() {
     await window.resetHistory();
   });
 
+  test('EnginePlatformDispatcher.instance.implicitView should be non-null', () async {
+    expect(EnginePlatformDispatcher.instance.implicitView, isNotNull);
+    expect(EnginePlatformDispatcher.instance.implicitView?.viewId, 0);
+    expect(window.viewId, 0);
+  });
+
   test('window.defaultRouteName should work with JsUrlStrategy', () async {
     dynamic state = <dynamic, dynamic>{};
     final JsUrlStrategy jsUrlStrategy = JsUrlStrategy(
