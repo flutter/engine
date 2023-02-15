cfccaa7fcbab382d98d1f010d9de7790d1261406
Harry Terkelsen <het@google.com>
2023-02-15 13:46:03 -0800

    WIP add screenshot goldens to ui_tests

--- lib/web_ui/test/ui/utils.dart
+++ lib/web_ui/test/ui/utils.dart
@@ -2,15 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
+import 'package:ui/ui.dart';
+import 'package:web_engine_tester/golden_tester.dart';
 
 import '../canvaskit/common.dart';
 
+// TODO(hterkelsen): Support skwasm.
+
 /// Initializes the renderer for this test.
 void setUpUiTest() {
-  if (renderer is CanvasKitRenderer) {
+  setUpAll(() async {
+    await webOnlyInitializePlatform();
+    await renderer.fontCollection.debugDownloadTestFonts();
+    renderer.fontCollection.registerDownloadedFonts();
+  });
+  if (isCanvasKit) {
     setUpCanvasKitTest();
   }
+}
+
+/// Draws the [Picture]. This is in preparation for a golden test.
+void drawPictureUsingCurrentRenderer(Picture picture) {
+  final SceneBuilder sb = SceneBuilder();
+  sb.pushOffset(0, 0);
+  sb.addPicture(Offset.zero, picture);
+  renderer.renderScene(sb.build());
+}
+
+Future<void> matchGolden(String basename, {Rect? region}) async {
+  await matchGoldenFile('canvas_lines_thickness.png', region: region);
 }
 
 /// Returns [true] if this test is running in the CanvasKit renderer.
