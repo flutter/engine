019c72cf8d95422949b2010ff81b46bb80c252f1
Paul Blasi <pdblasi@google.com>
2023-02-23 11:20:25 -0700

    Removed test that required adding `analyzer` as a dependency.

--- testing/dart/platform_dispatcher_test.dart
+++ testing/dart/platform_dispatcher_test.dart
@@ -2,13 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:io';
 import 'dart:ui';
 
-import 'package:analyzer/dart/analysis/features.dart';
-import 'package:analyzer/dart/analysis/results.dart';
-import 'package:analyzer/dart/analysis/utilities.dart';
-import 'package:analyzer/dart/ast/ast.dart';
 import 'package:litetest/litetest.dart';
 
 void main() {
@@ -43,61 +38,5 @@ void main() {
       // ignore: deprecated_member_use
       return ViewConfiguration(view: view)..copyWith(view: view, window: view);
     });
-  });
-
-  test("PlatformDispatcher and PlatformConfiguration have matching getters", () {
-      final List<Pattern> exclusionCriteria = <Pattern>[
-        RegExp(r'^_.*'), // exclude any private members
-        RegExp(r'^on[A-Z].*'), // exclude any callbacks
-        RegExp(r'locale$'), // locale is a convenience getter for interacting with `locales` which _is_ backed by the config
-        'instance', // exclude the static instance of PlatformDispatcher
-        'configuration', // the configuration should not reference itself
-        'views', // views are backed by their own config and don't need to be referenced in the platform config
-        'initialLifecycleState', // default route is stored/retrieved from the platform, not the engine
-        'frameData', // framed data updates too often and would kick off too many config changes
-      ];
-
-      final String flutterDir = Platform.environment['FLUTTER_DIR']!;
-      final List<ClassDeclaration> classes = parseFile(
-        path: '$flutterDir/lib/ui/platform_dispatcher.dart',
-        featureSet: FeatureSet.latestLanguageVersion(),
-        throwIfDiagnostics: false,
-      ).unit.declarations.whereType<ClassDeclaration>().toList();
-      final ClassDeclaration dispatcherClass = classes.singleWhere((ClassDeclaration c) => c.name.toString() == (PlatformDispatcher).toString());
-      final ClassDeclaration configurationClass = classes.singleWhere((ClassDeclaration c) => c.name.toString() == (PlatformConfiguration).toString());
-
-      Iterable<String> getNameOfClassMember(ClassMember member) {
-        if (member is MethodDeclaration) {
-          return <String>[member.name.toString()];
-        }else if (member is FieldDeclaration) {
-          return member.fields.variables.map((VariableDeclaration variable) => variable.name.toString());
-        } else {
-          return [];
-        }
-      }
-
-      final Set<String> dispatcherMembers = dispatcherClass.members
-        .where((ClassMember member) =>
-          member is FieldDeclaration ||
-          member is MethodDeclaration && member.isGetter)
-        .expand<String>(getNameOfClassMember)
-        .where((String name) =>
-          exclusionCriteria.every((criteria) =>
-            criteria.allMatches(name).isEmpty))
-        .toSet();
-      final Set<String> configurationMembers = configurationClass.members
-        .where((ClassMember member) =>
-          member is FieldDeclaration ||
-          member is MethodDeclaration && member.isGetter)
-        .expand(getNameOfClassMember)
-        .toSet();
-
-      final Set<String> missingMembers = configurationMembers.difference(dispatcherMembers);
-      final Set<String> extraMembers = dispatcherMembers.difference(configurationMembers);
-      expect(missingMembers.isEmpty && extraMembers.isEmpty, isTrue,
-        reason:
-          'PlatformDispatcher is out of sync with PlatformConfiguration.\n'
-          '  Missing Members: $missingMembers\n'
-          '  Members not backed by PlatformConfiguration: $extraMembers');
   });
 }
