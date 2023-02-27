e9d5543e9d2de7650498e3c472a9dfef24ca1de3
Godofredo Contreras <godofredoc@google.com>
2023-02-27 15:16:36 -0800

    Update.

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -4,7 +4,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
 """
 A top level harness to run all unit-tests in a specific engine build.
 """
