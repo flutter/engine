575105f62aefd7fd1ead1e866f03b099cead401a
Jenn Magder <magder@google.com>
2023-03-08 16:32:25 -0800

    Build iOS and macOS Obj-C with -fapplication-extension flag

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -814,9 +814,14 @@ static void SendFakeTouchEvent(FlutterEngine* engine,
   if ([_engine.get() viewController] == self) {
     [self onUserSettingsChanged:nil];
     [self onAccessibilityStatusChanged:nil];
+
+#if !APPLICATION_EXTENSION_API_ONLY
     if (UIApplication.sharedApplication.applicationState == UIApplicationStateActive) {
+#endif
       [[_engine.get() lifecycleChannel] sendMessage:@"AppLifecycleState.resumed"];
+#if !APPLICATION_EXTENSION_API_ONLY
     }
+#endif
   }
   [super viewDidAppear:animated];
 }
@@ -1304,8 +1309,13 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
   // There is no guarantee that UIKit will layout subviews when the application is active. Creating
   // the surface when inactive will cause GPU accesses from the background. Only wait for the first
   // frame to render when the application is actually active.
-  bool applicationIsActive =
+  BOOL applicationIsActive =
+
+#if APPLICATION_EXTENSION_API_ONLY
+      YES;
+#else
       [UIApplication sharedApplication].applicationState == UIApplicationStateActive;
+#endif
 
   // This must run after updateViewportMetrics so that the surface creation tasks are queued after
   // the viewport metrics update tasks.
@@ -1808,24 +1818,19 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
     _orientationPreferences = new_preferences;
 
     if (@available(iOS 16.0, *)) {
+#if APPLICATION_EXTENSION_API_ONLY
+      UIWindowScene* windowScene = self.viewIfLoaded.window.windowScene;
+      [self performOrientationUpdateOnWindowScene:windowScene];
+#else
       for (UIScene* scene in UIApplication.sharedApplication.connectedScenes) {
         if (![scene isKindOfClass:[UIWindowScene class]]) {
           continue;
         }
-        UIWindowScene* windowScene = (UIWindowScene*)scene;
-        UIWindowSceneGeometryPreferencesIOS* preference =
-            [[[UIWindowSceneGeometryPreferencesIOS alloc]
-                initWithInterfaceOrientations:_orientationPreferences] autorelease];
-        [windowScene
-            requestGeometryUpdateWithPreferences:preference
-                                    errorHandler:^(NSError* error) {
-                                      os_log_error(OS_LOG_DEFAULT,
-                                                   "Failed to change device orientation: %@",
-                                                   error);
-                                    }];
-        [self setNeedsUpdateOfSupportedInterfaceOrientations];
+        [self performOrientationUpdateOnWindowScene:(UIWindowScene*)scene];
       }
+#endif
     } else {
+#if !APPLICATION_EXTENSION_API_ONLY
       UIInterfaceOrientationMask currentInterfaceOrientation =
           1 << [[UIApplication sharedApplication] statusBarOrientation];
       if (!(_orientationPreferences & currentInterfaceOrientation)) {
@@ -1848,8 +1853,26 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
                                       forKey:@"orientation"];
         }
       }
+#endif
     }
   }
+}
+
+- (void)performOrientationUpdateOnWindowScene:(UIWindowScene*)windowScene API_AVAILABLE(ios(16.0)) {
+  if (windowScene == nil) {
+    return;
+  }
+
+  UIWindowSceneGeometryPreferencesIOS* preference = [[UIWindowSceneGeometryPreferencesIOS alloc]
+      initWithInterfaceOrientations:_orientationPreferences];
+  [windowScene
+      requestGeometryUpdateWithPreferences:preference
+                              errorHandler:^(NSError* error) {
+                                os_log_error(OS_LOG_DEFAULT,
+                                             "Failed to change device orientation: %@", error);
+                              }];
+  [self setNeedsUpdateOfSupportedInterfaceOrientations];
+  [preference release];
 }
 
 - (void)onHideHomeIndicatorNotification:(NSNotification*)notification {
@@ -1951,7 +1974,12 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
 }
 
 - (CGFloat)textScaleFactor {
+#if APPLICATION_EXTENSION_API_ONLY
+  UIContentSizeCategory category =
+      self.mainScreenIfViewLoaded.traitCollection.preferredContentSizeCategory;
+#else
   UIContentSizeCategory category = [UIApplication sharedApplication].preferredContentSizeCategory;
+#endif
   // The delta is computed by approximating Apple's typography guidelines:
   // https://developer.apple.com/ios/human-interface-guidelines/visual-design/typography/
   //
