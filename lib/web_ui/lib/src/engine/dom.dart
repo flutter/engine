646271e0956a7a72d23c01d79713cda4a0666a05
maRci002 <matuzmarci@gmail.com>
2023-03-17 00:56:51 +0100

    readd createDomBlob

--- lib/web_ui/lib/src/engine/dom.dart
+++ lib/web_ui/lib/src/engine/dom.dart
@@ -1420,6 +1420,9 @@ extension DomURLExtension on DomURL {
 @staticInterop
 class DomBlob {}
 
+DomBlob createDomBlob(List<Object?> parts) =>
+    domCallConstructorString('Blob', <Object>[parts])! as DomBlob;
+
 extension DomBlobExtension on DomBlob {
   Future<dynamic> arrayBuffer() => js_util
       .promiseToFuture(js_util.callMethod(this, 'arrayBuffer', <Object>[]));
