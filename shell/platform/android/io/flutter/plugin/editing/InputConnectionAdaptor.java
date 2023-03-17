5a3695818ac7575cbfc94f51adeb8c9409d1119f
Ann Mossman <233583+mossmana@users.noreply.github.com>
2023-03-17 10:27:18 -0700

    trying to figure out issue with formatter

--- shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
+++ shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
@@ -287,13 +287,6 @@ public class InputConnectionAdaptor extends BaseInputConnection
   // occur, and need a chance to be handled by the framework.
   @Override
   public boolean sendKeyEvent(KeyEvent event) {
-    // Prevent Samsung IME keyboards from incorrectly handling Japanese
-    // suggestions.
-    // https://github.com/flutter/flutter/issues/120351
-    if (event.getAction() == KeyEvent.ACTION_DOWN &&
-            event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
-      return handleKeyEvent(event);
-    }
     return keyboardDelegate.handleEvent(event);
   }
 
