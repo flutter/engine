b8a36f55a69f2fbcd38e19095dbf06e494cc7e76
Ann Mossman <233583+mossmana@users.noreply.github.com>
2023-03-17 10:39:56 -0700

    take ?

--- shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
+++ shell/platform/android/io/flutter/plugin/editing/InputConnectionAdaptor.java
@@ -287,10 +287,9 @@ public class InputConnectionAdaptor extends BaseInputConnection
   // occur, and need a chance to be handled by the framework.
   @Override
   public boolean sendKeyEvent(KeyEvent event) {
-    if (event.getAction() == KeyEvent.ACTION_DOWN) {
-      if (event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
+    if (event.getAction() == KeyEvent.ACTION_DOWN
+        && event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
         return handleKeyEvent(event);
-      }
     }
     return keyboardDelegate.handleEvent(event);
   }
