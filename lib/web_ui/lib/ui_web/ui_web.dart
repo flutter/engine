8801c1e6330a6f9927c58e9ae27d01dfbf5d326a
Jackson Gardner <jacksongardner@google.com>
2023-03-23 15:20:12 -0700

    Started making `ui_web`

---
+++ lib/web_ui/lib/ui_web/ui_web.dart
@@ -0,0 +1,17 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// This library defines the web-specific additions that go along with web:ui
+library ui_web;
+
+import 'dart:async';
+import 'dart:collection' as collection;
+import 'dart:convert';
+import 'dart:math' as math;
+import 'dart:typed_data';
+
+import 'dart:_engine' as engine;
+import 'dart:ui';
+
+part 'url_strategy.dart';
