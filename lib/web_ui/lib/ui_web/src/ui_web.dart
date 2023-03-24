4500675e4df45742e6d78a7bb3d7c72c3dc2b11d
Jackson Gardner <jacksongardner@google.com>
2023-03-24 14:20:12 -0700

    Update lib/web_ui/lib/ui_web/src/ui_web.dart
    Co-authored-by: Mouad Debbar <mdebbar@google.com>

--- lib/web_ui/lib/ui_web/src/ui_web.dart
+++ lib/web_ui/lib/ui_web/src/ui_web.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// This library defines the web-specific additions that go along with web:ui
+/// This library defines the web-specific additions that go along with dart:ui
 library ui_web;
 
 export 'ui_web/url_strategy.dart';
