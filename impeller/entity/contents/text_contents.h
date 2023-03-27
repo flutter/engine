98081f1c047e85c12ad52e2a26a49ca936b9e4bf
Jonah Williams <jonahwilliams@google.com>
2023-03-27 12:53:58 -0700

    Update text_contents.h

--- impeller/entity/contents/text_contents.h
+++ impeller/entity/contents/text_contents.h
@@ -56,7 +56,7 @@ class TextContents final : public Contents {
  private:
   TextFrame frame_;
   Color color_;
-  Scalar inherited_opacity_ = 1.0;
+  Scalar inherited_opacity_;
   mutable std::shared_ptr<LazyGlyphAtlas> lazy_atlas_;
   Matrix inverse_matrix_;
 
