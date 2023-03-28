b228ebf9156393f2a922578dfcb28e010bcccc85
Jackson Gardner <jacksongardner@google.com>
2023-03-28 13:52:38 -0700

    Keep library directive with a comment.

--- lib/web_ui/lib/ui_web/src/ui_web.dart
+++ lib/web_ui/lib/ui_web/src/ui_web.dart
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/// This library defines the web-specific additions that go along with dart:ui
+// This library defines the web-specific additions that go along with dart:ui
+//
+// The web_sdk/sdk_rewriter.dart uses this directive.
+// ignore: unnecessary_library_directive
 library ui_web;
 
 export 'ui_web/url_strategy.dart';
