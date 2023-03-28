64bb9e5d6d3bc69f3de4dbac93a622c1d07cf3c6
Chinmay Garde <chinmaygarde@google.com>
2023-03-28 15:25:32 -0700

    [Impeller] Enable playgrounds using a runtime instead of a build time flag.
    Specify `--enable_plauground` to `impeller_unittests` or any playground
    enabled test harness.

--- impeller/playground/playground.cc
+++ impeller/playground/playground.cc
@@ -180,7 +180,7 @@ void Playground::SetCursorPosition(Point pos) {
 
 bool Playground::OpenPlaygroundHere(
     const Renderer::RenderCallback& render_callback) {
-  if (!is_enabled()) {
+  if (!switches_.enable_playground) {
     return true;
   }
 
