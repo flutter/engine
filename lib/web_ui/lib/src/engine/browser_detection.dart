0555cd08a072fbc31b8f7e2c06b0695e8d7178c9
Mouad Debbar <mdebbar@google.com>
2023-03-30 13:16:47 -0400

    remove unused import

--- lib/web_ui/lib/src/engine/browser_detection.dart
+++ lib/web_ui/lib/src/engine/browser_detection.dart
@@ -5,7 +5,6 @@
 import 'package:meta/meta.dart';
 
 import 'dom.dart';
-import 'safe_browser_api.dart';
 
 // iOS 15 launched WebGL 2.0, but there's something broken about it, which
 // leads to apps failing to load. For now, we're forcing WebGL 1 on iOS.
