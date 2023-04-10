b5623e4c7f7f0f797970dec607c95c640ad09e05
Jackson Gardner <jacksongardner@google.com>
2023-04-10 16:08:09 -0700

    Fix formatting issue.

--- lib/web_ui/dev/chrome.dart
+++ lib/web_ui/dev/chrome.dart
@@ -159,7 +159,7 @@ class Chrome extends Browser {
       return process;
     }), remoteDebuggerCompleter.future);
   }
-  
+
   Chrome._(this._process, this.remoteDebuggerUrl);
 
   static Future<String> generateUserDirectory(
