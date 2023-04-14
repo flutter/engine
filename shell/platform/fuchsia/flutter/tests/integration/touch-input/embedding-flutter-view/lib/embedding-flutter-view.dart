9f7d2c613580a7e8e2689402685914a58692c0bc
Caroline Liu <10456171+caroqliu@users.noreply.github.com>
2023-04-14 17:29:41 -0400

    Remove indentation

--- shell/platform/fuchsia/flutter/tests/integration/touch-input/embedding-flutter-view/lib/embedding-flutter-view.dart
+++ shell/platform/fuchsia/flutter/tests/integration/touch-input/embedding-flutter-view/lib/embedding-flutter-view.dart
@@ -33,11 +33,11 @@ void main(List<String> args) {
 
   // TODO(fxbug.dev/125514): Support Flatland Child View.
   TestApp app = TestApp(
-      ChildView.gfx(_launchGfxChildView()),
-      showOverlay: arguments['showOverlay'],
-      hitTestable: arguments['hitTestable'],
-      focusable: arguments['focusable'],
-    );
+    ChildView.gfx(_launchGfxChildView()),
+    showOverlay: arguments['showOverlay'],
+    hitTestable: arguments['hitTestable'],
+    focusable: arguments['focusable'],
+  );
 
   app.run();
 }
