553e23fce3ee6ac076ad1738a09f19a4375132e2
Jackson Gardner <jacksongardner@google.com>
2023-04-15 10:49:34 -0700

    Fix typo in function name.

--- lib/web_ui/skwasm/data.cpp
+++ lib/web_ui/skwasm/data.cpp
@@ -10,7 +10,7 @@ SKWASM_EXPORT SkData* skData_create(size_t size) {
   return SkData::MakeUninitialized(size).release();
 }
 
-SKWASM_EXPORT void* skDataata_getPointer(SkData* data) {
+SKWASM_EXPORT void* skData_getPointer(SkData* data) {
   return data->writable_data();
 }
 
