e06b8853a35b4ce4ee2b655e6e8cb62197405c06
Jackson Gardner <jacksongardner@google.com>
2023-04-23 10:56:35 -0700

    Use a cast that works on all backends.

--- lib/web_ui/lib/src/engine/js_interop/js_typed_data.dart
+++ lib/web_ui/lib/src/engine/js_interop/js_typed_data.dart
@@ -23,5 +23,5 @@ class Uint8Array extends TypedArray {
   external factory Uint8Array._(JSAny bufferOrLength);
 }
 
-Uint8Array createUint8ArrayFromBuffer(ArrayBuffer buffer) => Uint8Array._(buffer as JSAny);
+Uint8Array createUint8ArrayFromBuffer(ArrayBuffer buffer) => Uint8Array._(buffer as JSObject);
 Uint8Array createUint8ArrayFromLength(int length) => Uint8Array._(length.toJS);
