flutter/flutter

commit 6db93ea20e8a864bdd2c33a0c16c2a92673fcfb7

Add a `Display` API to dart:ui that reports the physical size, DPR, and refresh rate of the main display (#41685)
Fixes https://github.com/flutter/flutter/issues/123307 - for Android, iOS, and Web, for the main display only (https://github.com/flutter/flutter/issues/125938 tracks supporting multiple displays, https://github.com/flutter/flutter/issues/125939 for desktop).

Desktop will need to be implemented for this, but given priority for a couple of our customers targetting foldable devices on Android I'm inclined to get this in before desktop can be finished.

The main concern for this right now is that on some Android foldable devices, setting a preferred orientation will cause letterboxing and the `MediaQuery` will _never_ get the full screen size when unfolded. This causes apps to think the screen is smaller than it is, as they've mainly been using `MediaQueryData.size` to figure this out. Android's recommendation is to not set a preferred orientation, and if you must to use the new method introduced in `ViewUtil.java` to calculate the maximal window size.

lib/ui/window.dart
@@ -3,6 +3,40 @@
 // found in the LICENSE file.
 part of dart.ui;
 
+/// A configurable display that a [FlutterView] renders on.
+///
+/// Use [FlutterView.display] to get the current display for that view.
+class Display {
+  const Display._({
+    required this.id,
+    required this.devicePixelRatio,
+    required this.size,
+    required this.refreshRate,
+  });
+
+  /// A unique identifier for this display.
+  ///
+  /// This identifier is unique among a list of displays the Flutter framework
+  /// is aware of, and is not derived from any platform specific identifiers for
+  /// displays.
+  final int id;
+
+  /// The device pixel ratio of this display.
+  ///
+  /// This value is the same as the value of [FlutterView.devicePixelRatio] for
+  /// all view objects attached to this display.
+  final double devicePixelRatio;
+
+  /// The physical size of this display.
+  final Size size;
+
+  /// The refresh rate in FPS of this display.
+  final double refreshRate;
+
+  @override
+  String toString() => 'Display(id: $id, size: $size, devicePixelRatio: $devicePixelRatio, refreshRate: $refreshRate)';
+}
+
 /// A view into which a Flutter [Scene] is drawn.
 ///
 /// Each [FlutterView] has its own layer tree that is rendered
@@ -67,6 +101,12 @@ class FlutterView {
     return platformDispatcher._viewConfigurations[viewId]!;
   }
 
+  /// The [Display] this view is drawn in.
+  Display get display {
+    assert(platformDispatcher._displays.containsKey(_viewConfiguration.displayId));
+    return platformDispatcher._displays[_viewConfiguration.displayId]!;
+  }
+
   /// The number of device pixels for each logical pixel for the screen this
   /// view is displayed on.
   ///
@@ -92,6 +132,8 @@ class FlutterView {
   ///
   ///  * [WidgetsBindingObserver], for a mechanism at the widgets layer to
   ///    observe when this value changes.
+  ///  * [Display.devicePixelRatio], which reports the DPR of the display.
+  ///    The value here is equal to the value on the [display.devicePixelRatio].
   double get devicePixelRatio => _viewConfiguration.devicePixelRatio;
 
   /// The dimensions and location of the rectangle into which the scene rendered

lib/web_ui/lib/src/engine/window.dart
@@ -45,6 +45,24 @@ set customUrlStrategy(ui_web.UrlStrategy? strategy) {
   _customUrlStrategy = strategy;
 }
 
+class EngineFlutterDisplay extends ui.Display {
+  EngineFlutterDisplay({
+    required this.id,
+    required this.devicePixelRatio,
+    required this.size,
+    required this.refreshRate,
+  });
+
+  @override
+  final int id;
+  @override
+  final double devicePixelRatio;
+  @override
+  final ui.Size size;
+  @override
+  final double refreshRate;
+}
+
 /// The Web implementation of [ui.SingletonFlutterWindow].
 class EngineFlutterWindow extends ui.SingletonFlutterWindow {
   EngineFlutterWindow(this.viewId, this.platformDispatcher) {
@@ -60,6 +78,16 @@ class EngineFlutterWindow extends ui.SingletonFlutterWindow {
       renderer.clearFragmentProgramCache();
       _dimensionsProvider.close();
     });
+  }
+
+  @override
+  ui.Display get display {
+    return EngineFlutterDisplay(
+      id: 0,
+      size: ui.Size(domWindow.screen?.width ?? 0, domWindow.screen?.height ?? 0),
+      devicePixelRatio: domWindow.devicePixelRatio,
+      refreshRate: 60,
+    );
   }
 
   @override

lib/web_ui/lib/window.dart
@@ -4,6 +4,13 @@
 
 part of ui;
 
+abstract class Display {
+  int get id;
+  double get devicePixelRatio;
+  Size get size;
+  double get refreshRate;
+}
+
 abstract class FlutterView {
   PlatformDispatcher get platformDispatcher;
   Object get viewId;
@@ -16,6 +23,7 @@ abstract class FlutterView {
   ViewPadding get padding;
   GestureSettings get gestureSettings;
   List<DisplayFeature> get displayFeatures;
+  Display get display;
   void render(Scene scene) => platformDispatcher.render(scene, this);
   void updateSemantics(SemanticsUpdate update) => platformDispatcher.updateSemantics(update);
 }

lib/ui/BUILD.gn
@@ -163,6 +163,7 @@ source_set("ui") {
 
   public_deps = [
     "//flutter/flow",
+    "//flutter/shell/common:display",
     "//flutter/shell/common:platform_message_handler",
     "//flutter/third_party/txt",
   ]

lib/ui/fixtures/ui_test.dart
@@ -6,7 +6,7 @@ import 'dart:async';
 import 'dart:typed_data';
 import 'dart:ui';
 import 'dart:isolate';
-import 'dart:ffi';
+import 'dart:ffi' hide Size;
 
 void main() {}
 
@@ -444,7 +444,7 @@ void hooksTests() async {
     window.onMetricsChanged!();
     _callHook(
       '_updateWindowMetrics',
-      20,
+      21,
       0, // window Id
       0.1234, // device pixel ratio
       0.0,    // width
@@ -465,6 +465,7 @@ void hooksTests() async {
       <double>[],  // display features bounds
       <int>[],     // display features types
       <int>[],     // display features states
+      0, // Display ID
     );
 
     expectIdentical(originalZone, callbackZone);
@@ -540,7 +541,7 @@ void hooksTests() async {
   await test('View padding/insets/viewPadding/systemGestureInsets', () {
     _callHook(
       '_updateWindowMetrics',
-      20,
+      21,
       0, // window Id
       1.0, // devicePixelRatio
       800.0, // width
@@ -561,6 +562,7 @@ void hooksTests() async {
       <double>[],  // display features bounds
       <int>[],     // display features types
       <int>[],     // display features states
+      0, // Display ID
     );
 
     expectEquals(window.viewInsets.bottom, 0.0);
@@ -570,7 +572,7 @@ void hooksTests() async {
 
     _callHook(
       '_updateWindowMetrics',
-      20,
+      21,
       0, // window Id
       1.0, // devicePixelRatio
       800.0, // width
@@ -591,6 +593,7 @@ void hooksTests() async {
       <double>[],  // display features bounds
       <int>[],     // display features types
       <int>[],     // display features states
+      0, // Display ID
     );
 
     expectEquals(window.viewInsets.bottom, 400.0);
@@ -602,7 +605,7 @@ void hooksTests() async {
    await test('Window physical touch slop', () {
     _callHook(
       '_updateWindowMetrics',
-      20,
+      21,
       0, // window Id
       1.0, // devicePixelRatio
       800.0, // width
@@ -623,6 +626,7 @@ void hooksTests() async {
       <double>[],  // display features bounds
       <int>[],     // display features types
       <int>[],     // display features states
+      0, // Display ID
     );
 
     expectEquals(window.gestureSettings,
@@ -630,7 +634,7 @@ void hooksTests() async {
 
     _callHook(
       '_updateWindowMetrics',
-      20,
+      21,
       0, // window Id
       1.0, // devicePixelRatio
       800.0, // width
@@ -651,6 +655,7 @@ void hooksTests() async {
       <double>[],  // display features bounds
       <int>[],     // display features types
       <int>[],     // display features states
+      0, // Display ID
     );
 
     expectEquals(window.gestureSettings,
@@ -658,7 +663,7 @@ void hooksTests() async {
 
     _callHook(
       '_updateWindowMetrics',
-      20,
+      21,
       0, // window Id
       1.0, // devicePixelRatio
       800.0, // width
@@ -679,6 +684,7 @@ void hooksTests() async {
       <double>[],  // display features bounds
       <int>[],     // display features types
       <int>[],     // display features states
+      0, // Display ID
     );
 
     expectEquals(window.gestureSettings,
@@ -881,6 +887,28 @@ void hooksTests() async {
     expectEquals(frameNumber, 2);
   });
 
+  await test('_updateDisplays preserves callback zone', () {
+    late Zone innerZone;
+    late Zone runZone;
+    late Display display;
+
+    runZoned(() {
+      innerZone = Zone.current;
+      window.onMetricsChanged = () {
+        runZone = Zone.current;
+        display = PlatformDispatcher.instance.displays.first;
+      };
+    });
+
+    _callHook('_updateDisplays', 5, <int>[0], <double>[800], <double>[600], <double>[1.5], <double>[65]);
+    expectNotEquals(runZone, null);
+    expectIdentical(runZone, innerZone);
+    expectEquals(display.id, 0);
+    expectEquals(display.size, const Size(800, 600));
+    expectEquals(display.devicePixelRatio, 1.5);
+    expectEquals(display.refreshRate, 65);
+  });
+
   await test('_futureize handles callbacker sync error', () async {
     String? callbacker(void Function(Object? arg) cb) {
       return 'failure';
@@ -1043,4 +1071,5 @@ external void _callHook(
   Object? arg18,
   Object? arg19,
   Object? arg20,
+  Object? arg21,
 ]);

lib/ui/hooks.dart
@@ -5,6 +5,32 @@
 part of dart.ui;
 
 @pragma('vm:entry-point')
+void _updateDisplays(
+  List<int> ids,
+  List<double> widths,
+  List<double> heights,
+  List<double> devicePixelRatios,
+  List<double> refreshRates,
+) {
+  assert(ids.length == widths.length);
+  assert(ids.length == heights.length);
+  assert(ids.length == devicePixelRatios.length);
+  assert(ids.length == refreshRates.length);
+  final List<Display> displays = <Display>[];
+  for (int index = 0; index < ids.length; index += 1) {
+    final int displayId = ids[index];
+    displays.add(Display._(
+      id: displayId,
+      size: Size(widths[index], heights[index]),
+      devicePixelRatio: devicePixelRatios[index],
+      refreshRate: refreshRates[index],
+    ));
+  }
+
+  PlatformDispatcher.instance._updateDisplays(displays);
+}
+
+@pragma('vm:entry-point')
 void _updateWindowMetrics(
   Object id,
   double devicePixelRatio,
@@ -26,6 +52,7 @@ void _updateWindowMetrics(
   List<double> displayFeaturesBounds,
   List<int> displayFeaturesType,
   List<int> displayFeaturesState,
+  int displayId,
 ) {
   PlatformDispatcher.instance._updateWindowMetrics(
     id,
@@ -48,6 +75,7 @@ void _updateWindowMetrics(
     displayFeaturesBounds,
     displayFeaturesType,
     displayFeaturesState,
+    displayId,
   );
 }
 

lib/ui/platform_dispatcher.dart
@@ -149,6 +149,26 @@ class PlatformDispatcher {
     _onPlatformConfigurationChangedZone = Zone.current;
   }
 
+  /// The current list of displays.
+  ///
+  /// If any of their configurations change, [onMetricsChanged] will be called.
+  ///
+  /// To get the display for a [FlutterView], use [FlutterView.display].
+  ///
+  /// Platforms may limit what information is available to the application with
+  /// regard to secondary displays and/or displays that do not have an active
+  /// application window.
+  ///
+  /// Presently, on Android and Web this collection will only contain the
+  /// display that the current window is on. On iOS, it will only contains the
+  /// main display on the phone or tablet. On Desktop, it will contain only
+  /// a main display with a valid refresh rate but invalid size and device
+  /// pixel ratio values.
+  // TODO(dnfield): Update these docs when https://github.com/flutter/flutter/issues/125939
+  // and https://github.com/flutter/flutter/issues/125938 are resolved.
+  Iterable<Display> get displays => _displays.values;
+  final Map<int, Display> _displays = <int, Display>{};
+
   /// The current list of views, including top level platform windows used by
   /// the application.
   ///
@@ -215,6 +235,17 @@ class PlatformDispatcher {
     _onMetricsChangedZone = Zone.current;
   }
 
+  // Called from the engine, via hooks.dart.
+  //
+  // Updates the available displays.
+  void _updateDisplays(List<Display> displays) {
+    _displays.clear();
+    for (final Display display in displays) {
+      _displays[display.id] = display;
+    }
+    _invoke(onMetricsChanged, _onMetricsChangedZone);
+  }
+
   // Called from the engine, via hooks.dart
   //
   // Updates the metrics of the window with the given id.
@@ -239,6 +270,7 @@ class PlatformDispatcher {
     List<double> displayFeaturesBounds,
     List<int> displayFeaturesType,
     List<int> displayFeaturesState,
+    int displayId,
   ) {
     final _ViewConfiguration previousConfiguration =
         _viewConfigurations[id] ?? const _ViewConfiguration();
@@ -283,6 +315,7 @@ class PlatformDispatcher {
         state: displayFeaturesState,
         devicePixelRatio: devicePixelRatio,
       ),
+      displayId: displayId,
     );
     _invoke(onMetricsChanged, _onMetricsChangedZone);
   }
@@ -1318,6 +1351,7 @@ class _ViewConfiguration {
     this.padding = ViewPadding.zero,
     this.gestureSettings = const GestureSettings(),
     this.displayFeatures = const <DisplayFeature>[],
+    this.displayId = 0,
   });
 
   /// Copy this configuration with some fields replaced.
@@ -1332,6 +1366,7 @@ class _ViewConfiguration {
     ViewPadding? padding,
     GestureSettings? gestureSettings,
     List<DisplayFeature>? displayFeatures,
+    int? displayId,
   }) {
     return _ViewConfiguration(
       view: view ?? this.view,
@@ -1344,10 +1379,15 @@ class _ViewConfiguration {
       padding: padding ?? this.padding,
       gestureSettings: gestureSettings ?? this.gestureSettings,
       displayFeatures: displayFeatures ?? this.displayFeatures,
+      displayId: displayId ?? this.displayId,
     );
   }
 
   final FlutterView?  view;
+
+  /// The identifier for a display for this view, in
+  /// [PlatformDispatcher._displays].
+  final int displayId;
 
   /// The pixel density of the output surface.
   final double devicePixelRatio;

lib/ui/window/platform_configuration.cc
@@ -43,6 +43,9 @@ void PlatformConfiguration::DidCreateIsolate() {
 
   on_error_.Set(tonic::DartState::Current(),
                 Dart_GetField(library, tonic::ToDart("_onError")));
+  update_displays_.Set(
+      tonic::DartState::Current(),
+      Dart_GetField(library, tonic::ToDart("_updateDisplays")));
   update_locales_.Set(tonic::DartState::Current(),
                       Dart_GetField(library, tonic::ToDart("_updateLocales")));
   update_user_settings_data_.Set(
@@ -78,7 +81,38 @@ void PlatformConfiguration::DidCreateIsolate() {
   // See: https://github.com/flutter/flutter/issues/120306
   windows_.emplace(kImplicitViewId,
                    std::make_unique<Window>(
-                       kImplicitViewId, ViewportMetrics{1.0, 0.0, 0.0, -1}));
+                       kImplicitViewId, ViewportMetrics{1.0, 0.0, 0.0, -1, 0}));
+}
+
+void PlatformConfiguration::UpdateDisplays(
+    const std::vector<DisplayData>& displays) {
+  std::vector<DisplayId> ids;
+  std::vector<double> widths;
+  std::vector<double> heights;
+  std::vector<double> device_pixel_ratios;
+  std::vector<double> refresh_rates;
+  for (const auto& display : displays) {
+    ids.push_back(display.id);
+    widths.push_back(display.width);
+    heights.push_back(display.height);
+    device_pixel_ratios.push_back(display.pixel_ratio);
+    refresh_rates.push_back(display.refresh_rate);
+  }
+  std::shared_ptr<tonic::DartState> dart_state =
+      update_displays_.dart_state().lock();
+  if (!dart_state) {
+    return;
+  }
+  tonic::DartState::Scope scope(dart_state);
+  tonic::CheckAndHandleError(tonic::DartInvoke(
+      update_displays_.Get(),
+      {
+          tonic::ToDart<std::vector<DisplayId>>(ids),
+          tonic::ToDart<std::vector<double>>(widths),
+          tonic::ToDart<std::vector<double>>(heights),
+          tonic::ToDart<std::vector<double>>(device_pixel_ratios),
+          tonic::ToDart<std::vector<double>>(refresh_rates),
+      }));
 }
 
 void PlatformConfiguration::UpdateLocales(

flutter/engine

commit cd9c24a70f972b45cde72d75161c43968b269a5f

Fix doc to fix engine roll
The engine roll is currently failing (https://cirrus-ci.com/task/5475027185827840?logs=main#L172) because of a doc issue introduced by https://github.com/flutter/engine/pull/41685. This fixes the doc issue.

lib/ui/window.dart
@@ -133,7 +133,7 @@ class FlutterView {
   ///  * [WidgetsBindingObserver], for a mechanism at the widgets layer to
   ///    observe when this value changes.
   ///  * [Display.devicePixelRatio], which reports the DPR of the display.
-  ///    The value here is equal to the value on the [display.devicePixelRatio].
+  ///    The value here is equal to the value exposed on [display].
   double get devicePixelRatio => _viewConfiguration.devicePixelRatio;
 
   /// The dimensions and location of the rectangle into which the scene rendered