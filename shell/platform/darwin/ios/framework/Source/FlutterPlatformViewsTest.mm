8722a94f235bd6b0c9c7e0fb6cf47de9c7da4a69
Chris Yang <ychris@google.com>
2023-05-09 10:17:32 -0700

    make unittest success

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformViewsTest.mm
@@ -219,7 +219,7 @@ fml::RefPtr<fml::TaskRunner> CreateNewThread(std::string name) {
                          arguments:@{@"id" : @2, @"viewType" : @"MockFlutterPlatformView"}],
       result);
 
-  XCTAssertNil(gMockPlatformView);
+  XCTAsserNottNil(gMockPlatformView);
 }
 
 - (void)testChildClippingViewHitTests {
