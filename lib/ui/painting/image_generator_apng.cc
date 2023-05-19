323f2824cdc5d72b1c77f2241a0207d05f9b8259
曾锦和(智鹰) <jinhe.zjh@alibaba-inc.com>
2023-05-19 18:49:45 +0800

    Support DisposalMethod::kRestorePrevious in MultiFrameCodec and fix the apng problem.

--- lib/ui/painting/image_generator_apng.cc
+++ lib/ui/painting/image_generator_apng.cc
@@ -536,6 +536,13 @@ bool APNGImageGenerator::DemuxNextImageInternal() {
           SkCodecAnimation::DisposalMethod::kKeep) {
     // Mark the required frame as the previous frame in all cases.
     image->frame_info->required_frame = images_.size() - 1;
+  } else if (images_.size() > (first_frame_index_ + 1) &&
+      images_.back().frame_info->disposal_method ==
+          SkCodecAnimation::DisposalMethod::kRestorePrevious) {
+    // Mark the required frame as the last previous frame
+    // It is not valid if there are 2 or above frames set |disposal_method| to
+    // |kRestorePrevious|. But it also works in MultiFrameCodec.
+    image->frame_info->required_frame = images_.size() - 2;
   }
 
   // Calling SkCodec::getInfo at least once prior to decoding is mandatory.
