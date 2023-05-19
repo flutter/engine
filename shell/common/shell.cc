e528bc07b40e853ec8660b68bd7560ba3430f6ce
Chun-Heng Tai <chtai@google.com>
2023-05-19 16:37:52 -0700

    Removes unnecesary setDefultFontManager

--- shell/common/shell.cc
+++ shell/common/shell.cc
@@ -2063,7 +2063,6 @@ bool Shell::ReloadSystemFonts() {
   if (!engine_) {
     return false;
   }
-  engine_->SetupDefaultFontManager();
   engine_->GetFontCollection().GetFontCollection()->ClearFontFamilyCache();
   // After system fonts are reloaded, we send a system channel message
   // to notify flutter framework.
