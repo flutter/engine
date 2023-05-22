150f1982fd5af6dac3c62c6c2957af732305442a
Jonah Williams <jonahwilliams@google.com>
2023-05-22 10:02:46 -0700

    Revert "[Impeller] use host image upload path on simulator (#42161)"
    This reverts commit 2b14f8a1f21ce34acd20dbd0b7d010c50fb7c8f7.

--- lib/ui/painting/image_decoder_impeller.cc
+++ lib/ui/painting/image_decoder_impeller.cc
@@ -417,7 +417,7 @@ void ImageDecoderImpeller::Decode(fml::RefPtr<ImageDescriptor> descriptor,
                                                      bitmap_result.value()]() {
 // TODO(jonahwilliams): remove ifdef once blit from buffer to texture is
 // implemented on other platforms.
-#if (FML_OS_IOS && !TARGET_IPHONE_SIMULATOR)
+#ifdef FML_OS_IOS
           result(UploadTextureToPrivate(context, bitmap_result.device_buffer,
                                         bitmap_result.image_info));
 #else
