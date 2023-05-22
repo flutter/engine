282193201b9cd270d5a21abeaa169550266d30d6
jonahwilliams <jonahwilliams@google.com>
2023-05-22 10:56:51 -0700

    ++

--- shell/common/shell_test.cc
+++ shell/common/shell_test.cc
@@ -352,13 +352,14 @@ std::unique_ptr<Shell> ShellTest::CreateShell(
                                      shell_test_external_view_embedder,  //
                                      rendering_backend                   //
     ](Shell& shell) {
-      return ShellTestPlatformView::Create(shell,                                //
-                                           shell.GetTaskRunners(),               //
-                                           vsync_clock,                          //
-                                           create_vsync_waiter,                  //
-                                           rendering_backend,                    //
-                                           shell_test_external_view_embedder,    //
-                                           shell.GetConcurrentWorkerTaskRunner() //
+      return ShellTestPlatformView::Create(
+          shell,                                 //
+          shell.GetTaskRunners(),                //
+          vsync_clock,                           //
+          create_vsync_waiter,                   //
+          rendering_backend,                     //
+          shell_test_external_view_embedder,     //
+          shell.GetConcurrentWorkerTaskRunner()  //
       );
     };
   }
