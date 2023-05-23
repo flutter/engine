35026ebcd683b86a809874acecdcd079420528cc
Aaron Clarke <aaclarke@google.com>
2023-05-23 15:29:20 -0700

    Added comment

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -472,6 +472,8 @@ def run_cc_tests(build_dir, executable_filter, coverage, capture_core_dump):
     # correctly by gtest-parallel.
     # https://github.com/flutter/flutter/issues/104789
     if not os.path.basename(build_dir).startswith('host_debug'):
+      # Test is disabled for flaking in debug runs:
+      # https://github.com/flutter/flutter/issues/127441
       run_engine_executable(
           build_dir,
           'flutter_desktop_darwin_unittests',
