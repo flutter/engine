29f919730d2eaf5e63e9883f449832e9945d4497
Kevin Lubick <kjlubick@google.com>
2023-05-25 13:03:15 -0400

    Fix copypasta

--- shell/common/shell.cc
+++ shell/common/shell.cc
@@ -30,6 +30,7 @@
 #include "rapidjson/writer.h"
 #include "third_party/dart/runtime/include/dart_tools_api.h"
 #include "third_party/skia/include/codec/SkBmpDecoder.h"
+#include "third_party/skia/include/codec/SkCodec.h"
 #include "third_party/skia/include/codec/SkGifDecoder.h"
 #include "third_party/skia/include/codec/SkIcoDecoder.h"
 #include "third_party/skia/include/codec/SkJpegDecoder.h"
@@ -76,18 +77,18 @@ std::unique_ptr<Engine> CreateEngine(
                                   volatile_path_tracker);
 }
 
-void InitSkiaEventTracer() {
+void RegisterCodecsWithSkia() {
   // These are in the order they will be attempted to be decoded from.
   // If we have data to back it up, we can order these by "frequency used in
   // the wild" for a very small performance bump, but for now we mirror the
   // order Skia had them in.
-  decoders->push_back(SkPngDecoder::Decoder());
-  decoders->push_back(SkJpegDecoder::Decoder());
-  decoders->push_back(SkWebpDecoder::Decoder());
-  decoders->push_back(SkGifDecoder::Decoder());
-  decoders->push_back(SkBmpDecoder::Decoder());
-  decoders->push_back(SkWbmpDecoder::Decoder());
-  decoders->push_back(SkIcoDecoder::Decoder());
+  SkCodecs::Register(SkPngDecoder::Decoder());
+  SkCodecs::Register(SkJpegDecoder::Decoder());
+  SkCodecs::Register(SkWebpDecoder::Decoder());
+  SkCodecs::Register(SkGifDecoder::Decoder());
+  SkCodecs::Register(SkBmpDecoder::Decoder());
+  SkCodecs::Register(SkWbmpDecoder::Decoder());
+  SkCodecs::Register(SkIcoDecoder::Decoder());
 }
 
 // Though there can be multiple shells, some settings apply to all components in
