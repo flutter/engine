be708c6926afb4581e3ff7caf89a03697fbd54c1
Kevin Lubick <kjlubick@google.com>
2023-05-25 11:54:48 -0400

    Register codecs with Skia explicitly

--- shell/common/shell.cc
+++ shell/common/shell.cc
@@ -29,6 +29,13 @@
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
 #include "third_party/dart/runtime/include/dart_tools_api.h"
+#include "third_party/skia/include/codec/SkBmpDecoder.h"
+#include "third_party/skia/include/codec/SkGifDecoder.h"
+#include "third_party/skia/include/codec/SkIcoDecoder.h"
+#include "third_party/skia/include/codec/SkJpegDecoder.h"
+#include "third_party/skia/include/codec/SkPngDecoder.h"
+#include "third_party/skia/include/codec/SkWbmpDecoder.h"
+#include "third_party/skia/include/codec/SkWebpDecoder.h"
 #include "third_party/skia/include/core/SkGraphics.h"
 #include "third_party/skia/include/utils/SkBase64.h"
 #include "third_party/tonic/common/log.h"
@@ -69,6 +76,20 @@ std::unique_ptr<Engine> CreateEngine(
                                   volatile_path_tracker);
 }
 
+void InitSkiaEventTracer() {
+  // These are in the order they will be attempted to be decoded from.
+  // If we have data to back it up, we can order these by "frequency used in
+  // the wild" for a very small performance bump, but for now we mirror the
+  // order Skia had them in.
+  decoders->push_back(SkPngDecoder::Decoder());
+  decoders->push_back(SkJpegDecoder::Decoder());
+  decoders->push_back(SkWebpDecoder::Decoder());
+  decoders->push_back(SkGifDecoder::Decoder());
+  decoders->push_back(SkBmpDecoder::Decoder());
+  decoders->push_back(SkWbmpDecoder::Decoder());
+  decoders->push_back(SkIcoDecoder::Decoder());
+}
+
 // Though there can be multiple shells, some settings apply to all components in
 // the process. These have to be set up before the shell or any of its
 // sub-components can be initialized. In a perfect world, this would be empty.
@@ -101,6 +122,7 @@ void PerformInitializationTasks(Settings& settings) {
     } else {
       FML_DLOG(INFO) << "Skia deterministic rendering is enabled.";
     }
+    RegisterCodecsWithSkia();
 
     if (settings.icu_initialization_required) {
       if (!settings.icu_data_path.empty()) {
