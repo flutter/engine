a4311b68ff3d11aeec780b8f9620a0cd9c388118
Renzo Olivares <roliv@google.com>
2023-06-07 16:53:11 -0700

    fix trailing spaces

--- shell/platform/linux/fl_text_input_plugin.cc
+++ shell/platform/linux/fl_text_input_plugin.cc
@@ -305,7 +305,7 @@ static void im_commit_cb(FlTextInputPlugin* self, const gchar* text) {
   if (priv->enable_delta_model) {
     flutter::TextEditingDelta* delta;
     if (wasComposing) {
-        delta = new flutter::TextEditingDelta(text_before_change, composing_before_change, text);  
+        delta = new flutter::TextEditingDelta(text_before_change, composing_before_change, text);
     } else {
         delta = new flutter::TextEditingDelta(text_before_change, selection_before_change, text);
     }
