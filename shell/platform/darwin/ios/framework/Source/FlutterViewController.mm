4fecb346449739cbb4303caf576f356157035517
Chris Yang <ychris@google.com>
2023-06-14 10:18:22 -0700

    use interfaceOrientation on window scene for iOS 13 and above

--- shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterViewController.mm
@@ -1857,8 +1857,13 @@ static flutter::PointerData::DeviceKind DeviceKindFromTouchType(UITouch* touch) 
         [self setNeedsUpdateOfSupportedInterfaceOrientations];
       }
     } else {
-      UIInterfaceOrientationMask currentInterfaceOrientation =
+      UIInterfaceOrientationMask currentInterfaceOrientation;
+      if (@available(iOS 13.0, *)) {
+        currentInterfaceOrientation = 1 << self.view.window.windowScene.interfaceOrientation;
+      } else {
+        currentInterfaceOrientation =
           1 << [[UIApplication sharedApplication] statusBarOrientation];
+      }
       if (!(_orientationPreferences & currentInterfaceOrientation)) {
         [UIViewController attemptRotationToDeviceOrientation];
         // Force orientation switch if the current orientation is not allowed
