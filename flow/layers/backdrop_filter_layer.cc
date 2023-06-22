601b35afe730f27d150f5a17ffd97968352f0b1e
Chris Yang <ychris@google.com>
2023-06-22 15:14:55 -0700

    forma

--- flow/layers/backdrop_filter_layer.cc
+++ flow/layers/backdrop_filter_layer.cc
@@ -43,10 +43,10 @@ void BackdropFilterLayer::Preroll(PrerollContext* context) {
   Layer::AutoPrerollSaveLayerState save =
       Layer::AutoPrerollSaveLayerState::Create(context, true, bool(filter_));
   // if (filter_ && context->view_embedder != nullptr) {
-    if (context->view_embedder != nullptr) {
-      context->view_embedder->PushFilterToVisitedPlatformViews(
+  if (context->view_embedder != nullptr) {
+    context->view_embedder->PushFilterToVisitedPlatformViews(
         filter_, context->state_stack.device_cull_rect());
-    }
+  }
   // }
   SkRect child_paint_bounds = SkRect::MakeEmpty();
   PrerollChildren(context, &child_paint_bounds);
