aebfcb88456c6160866a0c7f6e27ac53475b7c89
Louise Hsu <louisehsu@google.com>
2023-06-28 10:30:09 -0700

    revert accidental changes

--- shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterEngine.mm
@@ -136,7 +136,6 @@ static constexpr int kNumProfilerSamplesPerSec = 5;
   fml::scoped_nsobject<FlutterMethodChannel> _undoManagerChannel;
   fml::scoped_nsobject<FlutterMethodChannel> _scribbleChannel;
   fml::scoped_nsobject<FlutterMethodChannel> _spellCheckChannel;
-  fml::scoped_nsobject<FlutterMethodChannel> _lookupChannel;
   fml::scoped_nsobject<FlutterBasicMessageChannel> _lifecycleChannel;
   fml::scoped_nsobject<FlutterBasicMessageChannel> _systemChannel;
   fml::scoped_nsobject<FlutterBasicMessageChannel> _settingsChannel;
@@ -501,9 +500,6 @@ static constexpr int kNumProfilerSamplesPerSec = 5;
 - (FlutterMethodChannel*)spellCheckChannel {
   return _spellCheckChannel.get();
 }
-- (FlutterMethodChannel*)lookUpChannel {
-  return _lookupChannel.get();
-}
 - (FlutterBasicMessageChannel*)lifecycleChannel {
   return _lifecycleChannel.get();
 }
@@ -539,7 +535,6 @@ static constexpr int kNumProfilerSamplesPerSec = 5;
   _settingsChannel.reset();
   _keyEventChannel.reset();
   _spellCheckChannel.reset();
-  _lookupChannel.reset();
 }
 
 - (void)startProfiler {
@@ -615,11 +610,6 @@ static constexpr int kNumProfilerSamplesPerSec = 5;
 
   _spellCheckChannel.reset([[FlutterMethodChannel alloc]
          initWithName:@"flutter/spellcheck"
-      binaryMessenger:self.binaryMessenger
-                codec:[FlutterStandardMethodCodec sharedInstance]]);
-
-  _lookupChannel.reset([[FlutterMethodChannel alloc]
-         initWithName:@"flutter/lookup"
       binaryMessenger:self.binaryMessenger
                 codec:[FlutterStandardMethodCodec sharedInstance]]);
 
