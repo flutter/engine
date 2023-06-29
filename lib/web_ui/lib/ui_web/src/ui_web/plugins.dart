63b1b7c8f02dbc726781772c937fc2f35a86cfc4
Mouad Debbar <mdebbar@google.com>
2023-06-29 12:04:04 -0400

    remove duplicate typedef

--- lib/web_ui/lib/ui_web/src/ui_web/plugins.dart
+++ lib/web_ui/lib/ui_web/src/ui_web/plugins.dart
@@ -2,21 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:typed_data';
-
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
-
-typedef PluginMessageHandler = Future<void> Function(
-  String,
-  ByteData?,
-  ui.PlatformMessageResponseCallback?,
-);
 
 /// Sets the handler that forwards platform messages to web plugins.
 ///
 /// This function exists because unlike mobile, on the web plugins are also
 /// implemented using Dart code, and that code needs a way to receive messages.
-void setPluginHandler(PluginMessageHandler handler) {
+void setPluginHandler(ui.PlatformMessageCallback handler) {
   pluginMessageCallHandler = handler;
 }
