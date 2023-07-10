74af468968814d0b1bb714ba36fbf69f87e9c82b
Dan Field <dfield@gmail.com>
2023-07-10 11:25:47 -0700

    Update display_list_unittests.cc

--- impeller/display_list/display_list_unittests.cc
+++ impeller/display_list/display_list_unittests.cc
@@ -48,7 +48,7 @@ INSTANTIATE_PLAYGROUND_SUITE(DisplayListTest);
 TEST_P(DisplayListTest, DrawPictureWithAClip) {
   flutter::DisplayListBuilder sub_builder;
   sub_builder.ClipRect(SkRect::MakeXYWH(0, 0, 24, 24),
-                       flutter::ClipOp::kIntersect,
+                       flutter::DlCanvas::ClipOp::kIntersect,
                        /*is_aa=*/false);
   sub_builder.DrawPaint(flutter::DlPaint(flutter::DlColor::kBlue()));
 
