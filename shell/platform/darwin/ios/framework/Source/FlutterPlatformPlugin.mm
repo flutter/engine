cadd44b34067752f71ebd7a9bd551f26d9b6815a
Huan Lin <hellohuanlin@gmail.com>
2023-08-02 15:30:38 -0700

    [ios]make the screenIfViewLoaded and windowSceneIfLoaded helpers reusable

--- shell/platform/darwin/ios/framework/Source/FlutterPlatformPlugin.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterPlatformPlugin.mm
@@ -11,6 +11,7 @@
 
 #include "flutter/fml/logging.h"
 #import "flutter/shell/platform/darwin/ios/framework/Source/FlutterViewController_Internal.h"
+#import "flutter/shell/platform/darwin/ios/framework/Source/UIViewController+FlutterScreenAndSceneIfLoaded.h"
 
 namespace {
 
@@ -278,7 +279,7 @@ using namespace flutter;
 #if APPLICATION_EXTENSION_API_ONLY
     if (@available(iOS 15.0, *)) {
       rootViewController =
-          [engineViewController windowSceneIfViewLoaded].keyWindow.rootViewController;
+          [engineViewController flutterWindowSceneIfViewLoaded].keyWindow.rootViewController;
     } else {
       FML_LOG(WARNING)
           << "rootViewController is not available in application extension prior to iOS 15.0.";
