0ab8781325c9b3f24cabeff24283d8c49fb60d2f
Huan Lin <hellohuanlin@gmail.com>
2023-08-03 15:11:17 -0700

    [ios][autocorrection]disable autocorrection for ios 17

--- shell/platform/darwin/ios/framework/Source/FlutterTextInputPluginTest.mm
+++ shell/platform/darwin/ios/framework/Source/FlutterTextInputPluginTest.mm
@@ -299,15 +299,22 @@ FLUTTER_ASSERT_ARC
   XCTAssertNil(textInputPlugin.activeView.inputViewController);
 }
 
-- (void)testAutocorrectionPromptRectAppears {
+- (void)testAutocorrectionPromptRectAppearsBeforeIOS17AndDoesNotAppearAfterIOS17 {
   FlutterTextInputView* inputView = [[FlutterTextInputView alloc] initWithOwner:textInputPlugin];
   [inputView firstRectForRange:[FlutterTextRange rangeWithNSRange:NSMakeRange(0, 1)]];
 
-  // Verify behavior.
-  OCMVerify([engine flutterTextInputView:inputView
-      showAutocorrectionPromptRectForStart:0
-                                       end:1
-                                withClient:0]);
+  if (@available(iOS 17.0, *)) {
+    // Auto-correction prompt is disabled in iOS 17+.
+    OCMVerify(never(), [engine flutterTextInputView:inputView
+                           showAutocorrectionPromptRectForStart:0
+                                                            end:1
+                                                     withClient:0]);
+  } else {
+    OCMVerify([engine flutterTextInputView:inputView
+        showAutocorrectionPromptRectForStart:0
+                                         end:1
+                                  withClient:0]);
+  }
 }
 
 - (void)testIgnoresSelectionChangeIfSelectionIsDisabled {
@@ -339,6 +346,11 @@ FLUTTER_ASSERT_ARC
 }
 
 - (void)testAutocorrectionPromptRectDoesNotAppearDuringScribble {
+  // Auto-correction prompt is disabled in iOS 17+.
+  if (@available(iOS 17.0, *)) {
+    return;
+  }
+
   if (@available(iOS 14.0, *)) {
     FlutterTextInputView* inputView = [[FlutterTextInputView alloc] initWithOwner:textInputPlugin];
 
