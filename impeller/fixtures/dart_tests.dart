3dc31823645a18297c2d2c60b042a3781e9d0692
Brandon DeRosier <bdero@google.com>
2023-08-09 17:59:27 -0700

    [Impeller] Flutter GPU: Add context override.

--- impeller/fixtures/dart_tests.dart
+++ impeller/fixtures/dart_tests.dart
@@ -3,11 +3,12 @@
 // found in the LICENSE file.
 
 import 'dart:ui' as ui;
-//import 'dart:gpu';
+import '../../lib/gpu/lib/gpu.dart' as gpu;
 
 void main() {}
 
 @pragma('vm:entry-point')
-void sayHi() {
-  print('Hi');
+void instantiateDefaultContext() {
+  // ignore: unused_local_variable
+  final gpu.GpuContext context = gpu.gpuContext;
 }
