57ad22d87f21d9e966146f893b1602860915b091
Matan Lurey <matanlurey@users.noreply.github.com>
2023-08-21 16:51:13 -0700

    Add a regression test.

--- display_list/skia/dl_sk_paint_dispatcher_unittests.cc
+++ display_list/skia/dl_sk_paint_dispatcher_unittests.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "display_list/effects/dl_color_source.h"
 #include "flutter/display_list/skia/dl_sk_paint_dispatcher.h"
 
 #include "flutter/display_list/utils/dl_receiver_utils.h"
@@ -44,6 +45,24 @@ TEST(DisplayListUtils, SetColorSourceClearsDitherIfNotGradient) {
   helper.setDither(true);
   helper.setColorSource(nullptr);
   EXPECT_FALSE(helper.paint().isDither());
+}
+
+// https://github.com/flutter/flutter/issues/132860.
+TEST(DisplayListUtils, SetDitherTrueThenSetColorSourceDithersIfGradient) {
+  // Create a simple linear gradient.
+  const DlColor colors[2] = {0xFF000000, 0xFFFFFFFF};
+  const float stops[2] = {0.0f, 1.0f};
+  const auto linear_gradient = DlColorSource::MakeLinear(
+      SkPoint::Make(0.0f, 0.0f), SkPoint::Make(100.0f, 100.0f), 2, colors,
+      stops, DlTileMode::kClamp, nullptr);
+
+  MockDispatchHelper helper;
+
+  // A naive implementation would ignore the dither flag here since the current
+  // color source is not a gradient.
+  helper.setDither(true);
+  helper.setColorSource(linear_gradient.get());
+  EXPECT_TRUE(helper.paint().isDither());
 }
 
 }  // namespace testing
