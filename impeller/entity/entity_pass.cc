f1f68b46ead5ddf9378072a244d626a34a41899a
Dan Field <dnfield@google.com>
2023-08-25 14:40:00 -0700

    [Impeller] DlAiksCanvas as a DlCanvas wrapper for impeller::Canvas

--- impeller/entity/entity_pass.cc
+++ impeller/entity/entity_pass.cc
@@ -125,6 +125,9 @@ std::optional<Rect> EntityPass::GetElementsCoverage(
     if (!coverage.has_value()) {
       continue;
     }
+    if (coverage->IsMaximum()) {
+      return coverage;
+    }
     result = result->Union(coverage.value());
   }
   return result;
