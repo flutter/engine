894a57ff7e677815ac297a08af1be3c5d02df1c8
Xuan Tung <findyou795@gmail.com>
2023-08-25 19:11:48 +0700

    Update FlutterAndroidComponentTest.java

--- shell/platform/android/test/io/flutter/embedding/android/FlutterAndroidComponentTest.java
+++ shell/platform/android/test/io/flutter/embedding/android/FlutterAndroidComponentTest.java
@@ -404,6 +404,9 @@ public class FlutterAndroidComponentTest {
     public void onFlutterTextureViewCreated(@NonNull FlutterTextureView flutterTextureView) {}
 
     @Override
+    public void onFlutterImageViewCreated(@NonNull FlutterImageView flutterImageView) {}
+
+    @Override
     public void onFlutterUiDisplayed() {}
 
     @Override
