b8e4657724bb8247ed37f95a1a8b3ca75712fca3
Aaron Clarke <aaclarke@google.com>
2023-08-30 09:47:55 -0700

    added ci step to verify the fix

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -492,12 +492,31 @@ def run_cc_tests(build_dir, executable_filter, coverage, capture_core_dump):
           'METAL_DEBUG_ERROR_MODE': '0',  # Enables metal validation.
           'METAL_DEVICE_WRAPPER_TYPE': '1',  # Enables metal validation.
       })
-    # Impeller tests are only supported on macOS for now.
+
     run_engine_executable(
         build_dir,
         'impeller_unittests',
         executable_filter,
         shuffle_flags + ['--enable_vulkan_validation'],
+        coverage=coverage,
+        extra_env=extra_env,
+        # TODO(117122): Remove this allowlist.
+        # https://github.com/flutter/flutter/issues/114872
+        allowed_failure_output=[
+            '[MTLCompiler createVertexStageAndLinkPipelineWithFragment:',
+            '[MTLCompiler pipelineStateWithVariant:',
+        ]
+    )
+
+    # Run one interactive Vulkan tests with validation on.
+    run_engine_executable(
+        build_dir,
+        'impeller_unittests',
+        executable_filter,
+        [
+            '--enable_vulkan_validation', '--playground_timeout_ms=4000',
+            '--enable_playground', '--gtest_filter="*ColorWheel/Vulkan"'
+        ],
         coverage=coverage,
         extra_env=extra_env,
         # TODO(117122): Remove this allowlist.
