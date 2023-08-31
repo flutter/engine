68931d1f86b4bfaa6efc54cbf7da3eb9b1e1954f
jonahwilliams <jonahwilliams@google.com>
2023-08-30 22:51:54 -0700

    ++

--- impeller/entity/entity_pass.cc
+++ impeller/entity/entity_pass.cc
@@ -1023,13 +1023,16 @@ std::unique_ptr<EntityPass> EntityPass::Clone() const {
 
   auto pass = std::make_unique<EntityPass>();
   pass->SetElements(std::move(new_elements));
-  pass->backdrop_filter_reads_from_pass_texture_ = backdrop_filter_reads_from_pass_texture_;
-  pass->advanced_blend_reads_from_pass_texture_ = advanced_blend_reads_from_pass_texture_;
+  pass->backdrop_filter_reads_from_pass_texture_ =
+      backdrop_filter_reads_from_pass_texture_;
+  pass->advanced_blend_reads_from_pass_texture_ =
+      advanced_blend_reads_from_pass_texture_;
   pass->backdrop_filter_proc_ = backdrop_filter_proc_;
   pass->blend_mode_ = blend_mode_;
-  // pass->flood_clip_ = flood_clip_;
-  // pass->bounds_limit_ = bounds_limit_;
   pass->delegate_ = delegate_;
+  // Note: I tried also adding flood clip and bounds limit but one of the
+  // two caused rendering in wonderous to break. It's 10:51 PM, and I'm
+  // ready to move on.
   return pass;
 }
 
