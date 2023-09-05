da84500c4c8c5f089d72b85f11aac80e75c60b16
Aaron Clarke <aaclarke@google.com>
2023-09-05 13:54:36 -0700

    switched from blocking plte images to asserting they work after the skia patch lands

--- lib/ui/painting/image_decoder_no_gl_unittests.cc
+++ lib/ui/painting/image_decoder_no_gl_unittests.cc
@@ -112,6 +112,31 @@ float HalfToFloat(uint16_t half) {
   return (negative ? -1.f : 1.f) * pow_value * (1.0f + fFraction);
 }
 
+bool IsPngWithPLTE(const uint8_t* bytes, size_t size) {
+  if (size < 8) {
+    return false;
+  }
+
+  if (memcmp(bytes, "\x89PNG\x0d\x0a\x1a\x0a", 8) != 0) {
+    return false;
+  }
+
+  const uint8_t* end = bytes + size;
+  const uint8_t* loc = bytes + 8;
+  while (loc + 8 <= end) {
+    uint32_t chunk_length =
+        (loc[0] << 24) | (loc[1] << 16) | (loc[2] << 8) | loc[3];
+
+    if (memcmp(loc + 4, "PLTE", 4) == 0) {
+      return true;
+    }
+
+    loc += /*length*/ 4 + /*type*/ 4 + chunk_length + /*crc*/ 4;
+  }
+
+  return false;
+}
+
 sk_sp<SkData> OpenFixtureAsSkData(const char* name) {
   auto fixtures_directory =
       fml::OpenDirectory(GetFixturesPath(), false, fml::FilePermission::kRead);
@@ -157,6 +182,9 @@ TEST(ImageDecoderNoGLTest, ImpellerWideGamutDisplayP3) {
 
   auto descriptor = fml::MakeRefCounted<ImageDescriptor>(std::move(data),
                                                          std::move(generator));
+
+  ASSERT_FALSE(
+      IsPngWithPLTE(descriptor->data()->bytes(), descriptor->data()->size()));
 
 #if IMPELLER_SUPPORTS_RENDERING
   std::shared_ptr<impeller::Allocator> allocator =
@@ -209,6 +237,9 @@ TEST(ImageDecoderNoGLTest, ImpellerWideGamutIndexedPng) {
   auto descriptor = fml::MakeRefCounted<ImageDescriptor>(std::move(data),
                                                          std::move(generator));
 
+  ASSERT_TRUE(
+      IsPngWithPLTE(descriptor->data()->bytes(), descriptor->data()->size()));
+
 #if IMPELLER_SUPPORTS_RENDERING
   std::shared_ptr<impeller::Allocator> allocator =
       std::make_shared<impeller::TestImpellerAllocator>();
@@ -216,8 +247,32 @@ TEST(ImageDecoderNoGLTest, ImpellerWideGamutIndexedPng) {
       ImageDecoderImpeller::DecompressTexture(
           descriptor.get(), SkISize::Make(100, 100), {100, 100},
           /*supports_wide_gamut=*/true, allocator);
-  ASSERT_TRUE(wide_result.has_value());
-  ASSERT_EQ(wide_result->image_info.colorType(), kRGBA_8888_SkColorType);
+  ASSERT_EQ(wide_result->image_info.colorType(), kRGBA_F16_SkColorType);
+  ASSERT_TRUE(wide_result->image_info.colorSpace()->isSRGB());
+
+  const SkPixmap& wide_pixmap = wide_result->sk_bitmap->pixmap();
+  const uint16_t* half_ptr = static_cast<const uint16_t*>(wide_pixmap.addr());
+  bool found_deep_red = false;
+  for (int i = 0; i < wide_pixmap.width() * wide_pixmap.height(); ++i) {
+    float red = HalfToFloat(*half_ptr++);
+    float green = HalfToFloat(*half_ptr++);
+    float blue = HalfToFloat(*half_ptr++);
+    half_ptr++;  // alpha
+    if (fabsf(red - 1.0931f) < 0.01f && fabsf(green - -0.2268f) < 0.01f &&
+        fabsf(blue - -0.1501f) < 0.01f) {
+      found_deep_red = true;
+      break;
+    }
+  }
+
+  ASSERT_TRUE(found_deep_red);
+  std::optional<DecompressResult> narrow_result =
+      ImageDecoderImpeller::DecompressTexture(
+          descriptor.get(), SkISize::Make(100, 100), {100, 100},
+          /*supports_wide_gamut=*/false, allocator);
+
+  ASSERT_TRUE(narrow_result.has_value());
+  ASSERT_EQ(narrow_result->image_info.colorType(), kRGBA_8888_SkColorType);
 #endif  // IMPELLER_SUPPORTS_RENDERING
 }
 
