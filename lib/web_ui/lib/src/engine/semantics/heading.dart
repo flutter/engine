6298a5fe956558f62eaddcc7971749ea11ab227c
Víctor Galo <vtrgalo@gmail.com>
2023-09-10 02:21:08 +0200

    Remove unused import

--- lib/web_ui/lib/src/engine/semantics/heading.dart
+++ lib/web_ui/lib/src/engine/semantics/heading.dart
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:ui/ui.dart' as ui;
-
 import '../dom.dart';
 import 'semantics.dart';
 
