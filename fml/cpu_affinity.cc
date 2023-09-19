baaa7008ff7ae89d23b6f88006d585fb1500e9bf
jonahwilliams <jonahwilliams@google.com>
2023-09-19 15:21:03 -0700

    ++

--- fml/cpu_affinity.cc
+++ fml/cpu_affinity.cc
@@ -23,14 +23,6 @@ std::optional<size_t> EfficiencyCoreCount() {
 #endif
 }
 
-/// @brief Request the given affinity for the current thread.
-///
-///        Returns true if successfull, or if it was a no-op. This function is
-///        only supported on Android devices.
-///
-///        Affinity requests are based on documented CPU speed. This speed data
-///        is parsed from cpuinfo_max_freq files, see also:
-///        https://www.kernel.org/doc/Documentation/cpu-freq/user-guide.txt
 bool RequestAffinity(CpuAffinity affinity) {
 #ifdef FML_OS_ANDROID
   return AndroidRequestAffinity(affinity);
