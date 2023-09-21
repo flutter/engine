24ba4e0ed363ea8204a29dd9977d726a51ef894e
Matan Lurey <matanlurey@users.noreply.github.com>
2023-09-21 08:57:06 -0700

    Remove LOG_LEVEL.*

--- fml/log_level.h
+++ fml/log_level.h
@@ -16,21 +16,6 @@ constexpr LogSeverity kLogError = 2;
 constexpr LogSeverity kLogFatal = 3;
 constexpr LogSeverity kLogNumSeverities = 4;
 
-// DEPRECATED: Use |kLogInfo|.
-constexpr LogSeverity LOG_INFO = kLogInfo;
-
-// DEPRECATED: Use |kLogWarning|.
-constexpr LogSeverity LOG_WARNING = kLogWarning;
-
-// DEPRECATED: Use |kLogError|.
-constexpr LogSeverity LOG_ERROR = kLogError;
-
-// DEPRECATED: Use |kLogFatal|.
-constexpr LogSeverity LOG_FATAL = kLogFatal;
-
-// DEPRECATED: Use |kLogNumSeverities|.
-constexpr LogSeverity LOG_NUM_SEVERITIES = kLogNumSeverities;
-
 // One of the Windows headers defines ERROR to 0. This makes the token
 // concatenation in FML_LOG(ERROR) to resolve to LOG_0. We define this back to
 // the appropriate log level.
@@ -44,9 +29,6 @@ const LogSeverity kLogDFatal = kLogError;
 #else
 const LogSeverity kLogDFatal = kLogFatal;
 #endif
-
-// DEPRECATED: Use |kLogDFatal|.
-const LogSeverity LOG_DFATAL = kLogDFatal;
 
 }  // namespace fml
 
