5768deddd31e9af34aef368840a8b6581ddc6f90
Aaron Clarke <aaclarke@google.com>
2023-09-28 11:35:15 -0700

    added missing file

---
+++ impeller/aiks/canvas_type.h
@@ -0,0 +1,20 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#pragma once
+
+#include "impeller/aiks/canvas.h"
+#include "impeller/aiks/canvas_recorder.h"
+#include "impeller/aiks/trace_serializer.h"
+
+namespace impeller {
+
+#ifdef IMPELLER_TRACE_CANVAS
+using CanvasType = CanvasRecorder<TraceSerializer>;
+#else
+#error hi
+using CanvasType = Canvas;
+#endif
+
+}  // namespace impeller
