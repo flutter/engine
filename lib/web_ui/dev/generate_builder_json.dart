642b0416daad88006388fbb7ca589cd67fe8a0e5
Jackson Gardner <jacksongardner@google.com>
2023-10-10 16:14:30 -0700

    Bump to Chrome 118

--- lib/web_ui/dev/generate_builder_json.dart
+++ lib/web_ui/dev/generate_builder_json.dart
@@ -154,7 +154,7 @@ Iterable<dynamic> _getTestStepsForPlatform(
           if (suite.runConfig.browser == BrowserName.chrome)
             <String, dynamic>{
               'dependency': 'chrome_and_driver',
-              'version': '117.0.5938.149',
+              'version': '118.0.5993.70',
             },
           if (suite.runConfig.browser == BrowserName.firefox)
             <String, dynamic>{
