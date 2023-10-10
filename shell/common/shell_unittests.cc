181a94b8e451e3b04f5cc7e470e4b92d570b1d01
Tong Mu <dkwingsmt@gmail.com>
2023-10-10 16:03:30 -0700

    Fix lint

--- shell/common/shell_unittests.cc
+++ shell/common/shell_unittests.cc
@@ -4689,17 +4689,15 @@ TEST_F(ShellTest, AnimatorAcceptsMultipleRenders) {
   EXPECT_CALL(mock_shell, GetPlatformMessageHandler)
       .WillOnce(ReturnRef(platform_message_handler));
   fml::AutoResetWaitableEvent draw_latch;
-  std::function<void(std::shared_ptr<FramePipeline> pipeline)>
-      mock_on_animator_draw = [&draw_latch](
-                                  std::shared_ptr<FramePipeline> pipeline) {
+  EXPECT_CALL(mock_shell, OnAnimatorDraw)
+      .WillOnce(Invoke([&draw_latch](
+                           const std::shared_ptr<FramePipeline>& pipeline) {
         auto status = pipeline->Consume([&](std::unique_ptr<FrameItem> item) {
           EXPECT_EQ(item->layer_tree_tasks.size(), 2u);
         });
         EXPECT_EQ(status, PipelineConsumeResult::Done);
         draw_latch.Signal();
-      };
-  EXPECT_CALL(mock_shell, OnAnimatorDraw)
-      .WillOnce(Invoke(mock_on_animator_draw));
+      }));
   EXPECT_CALL(mock_shell, OnAnimatorBeginFrame)
       .WillOnce(Invoke(
           [&engine](fml::TimePoint frame_target_time, uint64_t frame_number) {
