316c1ea088113e4fcf13516e9c5d21150a409a18
Chris Yang <ychris@google.com>
2023-10-12 15:13:50 -0700

    prototype
    update font cutoffs
    
    format
    
    remove unnecessary import
    
    unittests
    
    tests
    
    format

---
+++ testing/scenario_app/lib/src/darwin_system_font.dart
@@ -0,0 +1,62 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:ui';
+
+import 'channel_util.dart';
+import 'scenario.dart';
+
+/// Tries to draw darwin system font: Cupertino-System-Pro, Cupertino-System-Text
+class DarwinSystemFont extends Scenario {
+  /// Creates the DarwinSystemFont scenario.
+  DarwinSystemFont(super.view);
+
+  // Semi-arbitrary.
+  final double _screenWidth = 700;
+
+  @override
+  void onBeginFrame(Duration duration) {
+    final SceneBuilder builder = SceneBuilder();
+    final PictureRecorder recorder = PictureRecorder();
+    final Canvas canvas = Canvas(recorder);
+
+    final ParagraphBuilder paragraphBuilderPro =
+        ParagraphBuilder(ParagraphStyle(fontFamily: "Cupertino-System-Display"))
+          ..pushStyle(TextStyle(fontSize: 50, fontFamily: "Cupertino-System-Display"))
+          ..addText('Cupertino System Display\n')
+          ..pop();
+    final ParagraphBuilder paragraphBuilderText =
+        ParagraphBuilder(ParagraphStyle(fontFamily: "Cupertino-System-Text"))
+          ..pushStyle(TextStyle(fontSize: 50, fontFamily: "Cupertino-System-Text"))
+          ..addText('Cupertino System Text\n')
+          ..pop();
+
+    final Paragraph paragraphPro = paragraphBuilderPro.build();
+    paragraphPro.layout(ParagraphConstraints(width: _screenWidth));
+    canvas.drawParagraph(paragraphPro, const Offset(50, 80));
+
+    final Paragraph paragraphText = paragraphBuilderText.build();
+    paragraphText.layout(ParagraphConstraints(width: _screenWidth));
+    canvas.drawParagraph(paragraphText, const Offset(50, 200));
+
+    final Picture picture = recorder.endRecording();
+
+    builder.addPicture(
+      Offset.zero,
+      picture,
+      willChangeHint: true,
+    );
+    final Scene scene = builder.build();
+    view.render(scene);
+    scene.dispose();
+
+    sendJsonMessage(
+      dispatcher: view.platformDispatcher,
+      channel: 'display_data',
+      json: <String, dynamic>{
+        'data': 'ready',
+      },
+    );
+  }
+}
--- third_party/txt/src/txt/platform_mac.mm
+++ third_party/txt/src/txt/platform_mac.mm
@@ -5,7 +5,9 @@
 #include <TargetConditionals.h>
 
 #include "flutter/fml/platform/darwin/platform_version.h"
+#include "third_party/skia/include/ports/SkTypeface_mac.h"
 #include "txt/platform.h"
+#include "txt/platform_mac.h"
 
 #if TARGET_OS_EMBEDDED || TARGET_OS_SIMULATOR
 #include <UIKit/UIKit.h>
@@ -14,6 +16,15 @@
 #include <AppKit/AppKit.h>
 #define FONT_CLASS NSFont
 #endif  // TARGET_OS_EMBEDDED
+
+// Apple system font larger than size 29 returns SFProText typeface.
+static const CGFloat kSFProDisplayBreakPoint = 29;
+// Apple system font smaller than size 16 returns SFProText typeface.
+static const CGFloat kSFProTextBreakPoint = 16;
+// Font name represents the "SF Pro Display" system font on Apple platforms.
+static const std::string kSFProDisplayName = "Cupertino-System-Display";
+// Font name represents the "SF Pro Text" system font on Apple platforms.
+static const std::string kSFProTextName = "Cupertino-System-Text";
 
 namespace txt {
 
@@ -27,6 +38,33 @@ std::vector<std::string> GetDefaultFontFamilies() {
 
 sk_sp<SkFontMgr> GetDefaultFontManager(uint32_t font_initialization_data) {
   return SkFontMgr::RefDefault();
+}
+
+void RegisterSystemFonts(const DynamicFontManager& dynamic_font_manager) {
+  // iOS loads different system fonts when size is greater than 28 or lower
+  // than 17. The "font family" property returned from CoreText stays the same
+  // despite the typeface is different.
+  //
+  // Below code manually loads and registers them as two different fonts
+  // so Flutter app can access them on macOS and iOS.
+  // System fonts from 17 to 28 also have dynamic spacing based on size.
+  // These two fonts will not match the spacing when sizes are from 17 to 28.
+  // The framework will handle the spacing. See:
+  // https://www.wwdcnotes.com/notes/wwdc20/10175/
+  sk_sp<SkTypeface> large_system_font =
+      SkMakeTypefaceFromCTFont(CTFontCreateUIFontForLanguage(
+          kCTFontUIFontSystem, kSFProDisplayBreakPoint, nullptr));
+  if (large_system_font) {
+    dynamic_font_manager.font_provider().RegisterTypeface(large_system_font,
+                                                          kSFProDisplayName);
+  }
+  sk_sp<SkTypeface> regular_system_font =
+      SkMakeTypefaceFromCTFont(CTFontCreateUIFontForLanguage(
+          kCTFontUIFontSystem, kSFProTextBreakPoint, nullptr));
+  if (regular_system_font) {
+    dynamic_font_manager.font_provider().RegisterTypeface(regular_system_font,
+                                                          kSFProTextName);
+  }
 }
 
 }  // namespace txt
