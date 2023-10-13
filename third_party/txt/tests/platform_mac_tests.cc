8fce06b4f6404ee450695103323666c7ed37c00d
Chris Yang <ychris@google.com>
2023-10-13 11:16:43 -0700

    add ignore files

---
+++ third_party/txt/tests/platform_mac_tests.cc
@@ -0,0 +1,30 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gtest/gtest.h"
+
+#include <sstream>
+
+#include "txt/platform_mac.h"
+
+namespace txt {
+namespace testing {
+
+class PlatformMacTests : public ::testing::Test {
+ public:
+  PlatformMacTests() {}
+
+  void SetUp() override {}
+};
+
+TEST_F(PlatformMacTests, RegisterSystemFonts) {
+  DynamicFontManager dynamic_font_manager;
+  RegisterSystemFonts(dynamic_font_manager);
+  ASSERT_EQ(dynamic_font_manager.font_provider().GetFamilyCount(), 2ul);
+  ASSERT_NE(dynamic_font_manager.font_provider().MatchFamily("Cupertino-System-Display"), nullptr);
+  ASSERT_NE(dynamic_font_manager.font_provider().MatchFamily("Cupertino-System-Text"), nullptr);
+}
+
+}  // namespace testing
+}  // namespace txt
