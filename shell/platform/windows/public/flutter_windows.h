f03eb6b9b3f2f89f8b61e6503e2c40c132189bda
Loic Sharma <sharma.loic@gmail.com>
2023-10-25 13:22:48 -0700

    Tweak comment

--- shell/platform/windows/public/flutter_windows.h
+++ shell/platform/windows/public/flutter_windows.h
@@ -20,7 +20,7 @@ extern "C" {
 
 typedef void (*VoidCallback)(void* /* user data */);
 
-// Opaque reference to a Flutter window controller.
+// Opaque reference to a Flutter view controller.
 struct FlutterDesktopViewController;
 typedef struct FlutterDesktopViewController* FlutterDesktopViewControllerRef;
 
