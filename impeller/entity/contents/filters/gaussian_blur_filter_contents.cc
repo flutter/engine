9554a957ad4c3dc77e1023e9747b84bdaf211a7a
Aaron Clarke <aaclarke@google.com>
2023-11-01 09:56:53 -0700

    [Impeller] added new gaussian blur

---
+++ impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
@@ -0,0 +1,52 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "impeller/entity/contents/filters/gaussian_blur_filter_contents.h"
+
+namespace impeller {
+
+GaussianBlurFilterContents::GaussianBlurFilterContents(Scalar sigma)
+    : sigma_(sigma) {}
+
+std::optional<Rect> GaussianBlurFilterContents::GetFilterSourceCoverage(
+    const Matrix& effect_transform,
+    const Rect& output_limit) const {
+  return {};
+}
+
+std::optional<Rect> GaussianBlurFilterContents::GetFilterCoverage(
+    const FilterInput::Vector& inputs,
+    const Entity& entity,
+    const Matrix& effect_transform) const {
+  if (inputs.empty()) {
+    return {};
+  }
+
+  std::optional<Rect> input_coverage = inputs[0]->GetCoverage(entity);
+  if (!input_coverage.has_value()) {
+    return {};
+  }
+
+  Scalar blur_radius = CalculateBlurRadius(sigma_);
+  return Rect::MakeLTRB(input_coverage.value().GetLeft() - blur_radius,
+                        input_coverage.value().GetTop() - blur_radius,
+                        input_coverage.value().GetRight() + blur_radius,
+                        input_coverage.value().GetBottom() + blur_radius);
+}
+
+std::optional<Entity> GaussianBlurFilterContents::RenderFilter(
+    const FilterInput::Vector& input_textures,
+    const ContentContext& renderer,
+    const Entity& entity,
+    const Matrix& effect_transform,
+    const Rect& coverage,
+    const std::optional<Rect>& coverage_hint) const {
+  return {};
+}
+
+Scalar GaussianBlurFilterContents::CalculateBlurRadius(Scalar sigma) {
+  return static_cast<Radius>(Sigma(sigma)).radius;
+}
+
+}  // namespace impeller
