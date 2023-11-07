9e94ea00cf39e1887e12f11fa8549bac0d8e2e5f
camsim99 <camillesimon90@gmail.com>
2023-11-07 13:34:33 -0800

    Fix shadows

--- shell/platform/android/test/io/flutter/plugin/platform/PlatformViewWrapperTest.java
+++ shell/platform/android/test/io/flutter/plugin/platform/PlatformViewWrapperTest.java
@@ -19,6 +19,7 @@ import android.view.View.OnFocusChangeListener;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.FrameLayout;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
@@ -28,24 +29,21 @@ import org.robolectric.annotation.Config;
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 
-import android.widget.FrameLayout;
-
-
 @TargetApi(33)
 @RunWith(AndroidJUnit4.class)
 public class PlatformViewWrapperTest {
   private final Context ctx = ApplicationProvider.getApplicationContext();
 
-  // @Test
-  // public void invalidateChildInParent_callsInvalidate() {
-  //   final PlatformViewWrapper wrapper = spy(new PlatformViewWrapper(ctx));
+  @Test
+  public void invalidateChildInParent_callsInvalidate() {
+    final PlatformViewWrapper wrapper = spy(new PlatformViewWrapper(ctx));
 
-  //   // Mock Android framework calls.
-  //   wrapper.invalidateChildInParent(null, null);
+    // Mock Android framework calls.
+    wrapper.invalidateChildInParent(null, null);
 
-  //   // Verify.
-  //   verify(wrapper, times(1)).invalidate();
-  // }
+    // Verify.
+    verify(wrapper, times(1)).invalidate();
+  }
 
   @Test
   public void draw_withoutSurface() {
@@ -64,136 +62,137 @@ public class PlatformViewWrapperTest {
     verify(canvas, times(1)).drawColor(Color.RED);
   }
 
-  // @Test
-  // public void focusChangeListener_hasFocus() {
-  //   final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
-  //   when(viewTreeObserver.isAlive()).thenReturn(true);
+  @Test
+  public void focusChangeListener_hasFocus() {
+    final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
+    when(viewTreeObserver.isAlive()).thenReturn(true);
 
-  //   final PlatformViewWrapper view =
-  //       new PlatformViewWrapper(ctx) {
-  //         @Override
-  //         public ViewTreeObserver getViewTreeObserver() {
-  //           return viewTreeObserver;
-  //         }
+    final PlatformViewWrapper view =
+        new PlatformViewWrapper(ctx) {
+          @Override
+          public ViewTreeObserver getViewTreeObserver() {
+            return viewTreeObserver;
+          }
 
-  //         @Override
-  //         public boolean hasFocus() {
-  //           return true;
-  //         }
-  //       };
+          @Override
+          public boolean hasFocus() {
+            return true;
+          }
+        };
 
-  //   final OnFocusChangeListener focusListener = mock(OnFocusChangeListener.class);
-  //   view.setOnDescendantFocusChangeListener(focusListener);
+    final OnFocusChangeListener focusListener = mock(OnFocusChangeListener.class);
+    view.setOnDescendantFocusChangeListener(focusListener);
 
-  //   final ArgumentCaptor<ViewTreeObserver.OnGlobalFocusChangeListener> focusListenerCaptor =
-  //       ArgumentCaptor.forClass(ViewTreeObserver.OnGlobalFocusChangeListener.class);
-  //   verify(viewTreeObserver).addOnGlobalFocusChangeListener(focusListenerCaptor.capture());
+    final ArgumentCaptor<ViewTreeObserver.OnGlobalFocusChangeListener> focusListenerCaptor =
+        ArgumentCaptor.forClass(ViewTreeObserver.OnGlobalFocusChangeListener.class);
+    verify(viewTreeObserver).addOnGlobalFocusChangeListener(focusListenerCaptor.capture());
 
-  //   focusListenerCaptor.getValue().onGlobalFocusChanged(null, null);
-  //   verify(focusListener).onFocusChange(view, true);
-  // }
+    focusListenerCaptor.getValue().onGlobalFocusChanged(null, null);
+    verify(focusListener).onFocusChange(view, true);
+  }
 
-  // @Test
-  // public void focusChangeListener_doesNotHaveFocus() {
-  //   final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
-  //   when(viewTreeObserver.isAlive()).thenReturn(true);
+  @Test
+  public void focusChangeListener_doesNotHaveFocus() {
+    final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
+    when(viewTreeObserver.isAlive()).thenReturn(true);
 
-  //   final PlatformViewWrapper view =
-  //       new PlatformViewWrapper(ctx) {
-  //         @Override
-  //         public ViewTreeObserver getViewTreeObserver() {
-  //           return viewTreeObserver;
-  //         }
+    final PlatformViewWrapper view =
+        new PlatformViewWrapper(ctx) {
+          @Override
+          public ViewTreeObserver getViewTreeObserver() {
+            return viewTreeObserver;
+          }
 
-  //         @Override
-  //         public boolean hasFocus() {
-  //           return false;
-  //         }
-  //       };
+          @Override
+          public boolean hasFocus() {
+            return false;
+          }
+        };
 
-  //   final OnFocusChangeListener focusListener = mock(OnFocusChangeListener.class);
-  //   view.setOnDescendantFocusChangeListener(focusListener);
+    final OnFocusChangeListener focusListener = mock(OnFocusChangeListener.class);
+    view.setOnDescendantFocusChangeListener(focusListener);
 
-  //   final ArgumentCaptor<ViewTreeObserver.OnGlobalFocusChangeListener> focusListenerCaptor =
-  //       ArgumentCaptor.forClass(ViewTreeObserver.OnGlobalFocusChangeListener.class);
-  //   verify(viewTreeObserver).addOnGlobalFocusChangeListener(focusListenerCaptor.capture());
+    final ArgumentCaptor<ViewTreeObserver.OnGlobalFocusChangeListener> focusListenerCaptor =
+        ArgumentCaptor.forClass(ViewTreeObserver.OnGlobalFocusChangeListener.class);
+    verify(viewTreeObserver).addOnGlobalFocusChangeListener(focusListenerCaptor.capture());
 
-  //   focusListenerCaptor.getValue().onGlobalFocusChanged(null, null);
-  //   verify(focusListener).onFocusChange(view, false);
-  // }
+    focusListenerCaptor.getValue().onGlobalFocusChanged(null, null);
+    verify(focusListener).onFocusChange(view, false);
+  }
 
-  // @Test
-  // public void focusChangeListener_viewTreeObserverIsAliveFalseDoesNotThrow() {
-  //   final PlatformViewWrapper view =
-  //       new PlatformViewWrapper(ctx) {
-  //         @Override
-  //         public ViewTreeObserver getViewTreeObserver() {
-  //           final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
-  //           when(viewTreeObserver.isAlive()).thenReturn(false);
-  //           return viewTreeObserver;
-  //         }
-  //       };
-  //   view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
-  // }
+  @Test
+  public void focusChangeListener_viewTreeObserverIsAliveFalseDoesNotThrow() {
+    final PlatformViewWrapper view =
+        new PlatformViewWrapper(ctx) {
+          @Override
+          public ViewTreeObserver getViewTreeObserver() {
+            final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
+            when(viewTreeObserver.isAlive()).thenReturn(false);
+            return viewTreeObserver;
+          }
+        };
+    view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
+  }
 
-  // @Test
-  // public void setOnDescendantFocusChangeListener_keepsSingleListener() {
-  //   final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
-  //   when(viewTreeObserver.isAlive()).thenReturn(true);
+  @Test
+  public void setOnDescendantFocusChangeListener_keepsSingleListener() {
+    final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
+    when(viewTreeObserver.isAlive()).thenReturn(true);
 
-  //   final PlatformViewWrapper view =
-  //       new PlatformViewWrapper(ctx) {
-  //         @Override
-  //         public ViewTreeObserver getViewTreeObserver() {
-  //           return viewTreeObserver;
-  //         }
-  //       };
+    final PlatformViewWrapper view =
+        new PlatformViewWrapper(ctx) {
+          @Override
+          public ViewTreeObserver getViewTreeObserver() {
+            return viewTreeObserver;
+          }
+        };
 
-  //   assertNull(view.getActiveFocusListener());
+    assertNull(view.getActiveFocusListener());
 
-  //   view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
-  //   assertNotNull(view.getActiveFocusListener());
+    view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
+    assertNotNull(view.getActiveFocusListener());
 
-  //   final ViewTreeObserver.OnGlobalFocusChangeListener activeFocusListener =
-  //       view.getActiveFocusListener();
+    final ViewTreeObserver.OnGlobalFocusChangeListener activeFocusListener =
+        view.getActiveFocusListener();
 
-  //   view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
-  //   assertNotNull(view.getActiveFocusListener());
+    view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
+    assertNotNull(view.getActiveFocusListener());
 
-  //   verify(viewTreeObserver, times(1)).removeOnGlobalFocusChangeListener(activeFocusListener);
-  // }
+    verify(viewTreeObserver, times(1)).removeOnGlobalFocusChangeListener(activeFocusListener);
+  }
 
-  // @Test
-  // public void unsetOnDescendantFocusChangeListener_removesActiveListener() {
-  //   final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
-  //   when(viewTreeObserver.isAlive()).thenReturn(true);
+  @Test
+  public void unsetOnDescendantFocusChangeListener_removesActiveListener() {
+    final ViewTreeObserver viewTreeObserver = mock(ViewTreeObserver.class);
+    when(viewTreeObserver.isAlive()).thenReturn(true);
 
-  //   final PlatformViewWrapper view =
-  //       new PlatformViewWrapper(ctx) {
-  //         @Override
-  //         public ViewTreeObserver getViewTreeObserver() {
-  //           return viewTreeObserver;
-  //         }
-  //       };
+    final PlatformViewWrapper view =
+        new PlatformViewWrapper(ctx) {
+          @Override
+          public ViewTreeObserver getViewTreeObserver() {
+            return viewTreeObserver;
+          }
+        };
 
-  //   assertNull(view.getActiveFocusListener());
+    assertNull(view.getActiveFocusListener());
 
-  //   view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
-  //   assertNotNull(view.getActiveFocusListener());
+    view.setOnDescendantFocusChangeListener(mock(OnFocusChangeListener.class));
+    assertNotNull(view.getActiveFocusListener());
 
-  //   final ViewTreeObserver.OnGlobalFocusChangeListener activeFocusListener =
-  //       view.getActiveFocusListener();
+    final ViewTreeObserver.OnGlobalFocusChangeListener activeFocusListener =
+        view.getActiveFocusListener();
 
-  //   view.unsetOnDescendantFocusChangeListener();
-  //   assertNull(view.getActiveFocusListener());
+    view.unsetOnDescendantFocusChangeListener();
+    assertNull(view.getActiveFocusListener());
 
-  //   view.unsetOnDescendantFocusChangeListener();
-  //   verify(viewTreeObserver, times(1)).removeOnGlobalFocusChangeListener(activeFocusListener);
-  // }
+    view.unsetOnDescendantFocusChangeListener();
+    verify(viewTreeObserver, times(1)).removeOnGlobalFocusChangeListener(activeFocusListener);
+  }
 
   @Test
   @Config(
       shadows = {
+        ShadowFrameLayout.class,
         ShadowViewGroup.class,
       })
   public void ignoreAccessibilityEvents() {
@@ -212,6 +211,7 @@ public class PlatformViewWrapperTest {
   @Test
   @Config(
       shadows = {
+        ShadowFrameLayout.class,
         ShadowViewGroup.class,
       })
   public void sendAccessibilityEvents() {
@@ -234,10 +234,13 @@ public class PlatformViewWrapperTest {
   }
 
   @Implements(ViewGroup.class)
-  public static class ShadowViewGroup extends  org.robolectric.shadows.ShadowViewGroup {
+  public static class ShadowViewGroup extends org.robolectric.shadows.ShadowViewGroup {
     @Implementation
-    public boolean requestSendAccessibilityEvent(View child, AccessibilityEvent event) {
+    protected boolean requestSendAccessibilityEvent(View child, AccessibilityEvent event) {
       return true;
     }
   }
+
+  @Implements(FrameLayout.class)
+  public static class ShadowFrameLayout extends io.flutter.plugin.platform.PlatformViewWrapperTest.ShadowViewGroup {}
 }
