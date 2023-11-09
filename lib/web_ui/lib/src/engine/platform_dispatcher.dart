3bd18a7457a3e700f8a566fd252217e78b38a4d1
Michael Goderbauer <goderbauer@google.com>
2023-11-08 17:39:14 -0800

    Update platform_dispatcher.dart

--- lib/web_ui/lib/src/engine/platform_dispatcher.dart
+++ lib/web_ui/lib/src/engine/platform_dispatcher.dart
@@ -1340,7 +1340,6 @@ class ViewConfiguration {
   const ViewConfiguration({
     this.view,
     this.devicePixelRatio = 1.0,
-    this.size = ui.Size.zero,
     this.visible = false,
     this.viewInsets = ui.ViewPadding.zero as ViewPadding,
     this.viewPadding = ui.ViewPadding.zero as ViewPadding,
@@ -1353,7 +1352,6 @@ class ViewConfiguration {
   ViewConfiguration copyWith({
     EngineFlutterView? view,
     double? devicePixelRatio,
-    ui.Size? size,
     bool? visible,
     ViewPadding? viewInsets,
     ViewPadding? viewPadding,
@@ -1365,7 +1363,6 @@ class ViewConfiguration {
     return ViewConfiguration(
       view: view ?? this.view,
       devicePixelRatio: devicePixelRatio ?? this.devicePixelRatio,
-      size: size ?? this.size,
       visible: visible ?? this.visible,
       viewInsets: viewInsets ?? this.viewInsets,
       viewPadding: viewPadding ?? this.viewPadding,
@@ -1378,7 +1375,6 @@ class ViewConfiguration {
 
   final EngineFlutterView? view;
   final double devicePixelRatio;
-  final ui.Size size;
   final bool visible;
   final ViewPadding viewInsets;
   final ViewPadding viewPadding;
@@ -1389,7 +1385,7 @@ class ViewConfiguration {
 
   @override
   String toString() {
-    return '$runtimeType[view: $view, size: $size]';
+    return '$runtimeType[view: $view]';
   }
 }
 
