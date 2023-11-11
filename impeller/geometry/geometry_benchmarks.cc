6ea174b453d3ae4a3fb7c6f6e88e69cae7927501
jonahwilliams <jonahwilliams@google.com>
2023-11-11 09:00:35 -0800

    ++

--- impeller/geometry/geometry_benchmarks.cc
+++ impeller/geometry/geometry_benchmarks.cc
@@ -92,8 +92,8 @@ namespace {
 
 Path CreateRRect() {
   return PathBuilder{}
-    .AddRoundedRect(Rect::MakeLTRB(0, 0, 400, 400), 16)
-    .TakePath();
+      .AddRoundedRect(Rect::MakeLTRB(0, 0, 400, 400), 16)
+      .TakePath();
 }
 
 Path CreateCubic() {
