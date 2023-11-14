cca0ca9f9c18dc4aa12b5c124ac4d41056f45133
Greg Spencer <gspencer@google.com>
2023-11-14 15:48:09 -0800

    Add spot tests

---
+++ shell/platform/darwin/macos/framework/Source/KeyCodeMapTest.mm
@@ -0,0 +1,16 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "KeyCodeMap_Internal.h"
+
+#import "flutter/testing/testing.h"
+#include "third_party/googletest/googletest/include/gtest/gtest.h"
+
+namespace flutter::testing {
+
+TEST(KeyCodeMapTest, HasExpectedValues) {
+  EXPECT_NE([kLayoutGoals ]);
+}
+
+}  // namespace flutter::testing
