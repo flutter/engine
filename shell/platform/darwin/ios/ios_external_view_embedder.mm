6906e77342f16674b1a275149e3ea387d0159669
Matan Lurey <matan@lurey.org>
2023-11-15 15:51:42 -0800

    ++

--- shell/platform/darwin/ios/ios_external_view_embedder.mm
+++ shell/platform/darwin/ios/ios_external_view_embedder.mm
@@ -34,7 +34,7 @@ void IOSExternalViewEmbedder::BeginFrame(
     SkISize frame_size,
     GrDirectContext* context,
     double device_pixel_ratio,
-    fml::RefPtr<fml::RasterThreadMerger> raster_thread_merger) {
+    const fml::RefPtr<fml::RasterThreadMerger>& raster_thread_merger) {
   TRACE_EVENT0("flutter", "IOSExternalViewEmbedder::BeginFrame");
   FML_CHECK(platform_views_controller_);
   platform_views_controller_->BeginFrame(frame_size);
@@ -51,7 +51,7 @@ void IOSExternalViewEmbedder::PrerollCompositeEmbeddedView(
 
 // |ExternalViewEmbedder|
 PostPrerollResult IOSExternalViewEmbedder::PostPrerollAction(
-    fml::RefPtr<fml::RasterThreadMerger> raster_thread_merger) {
+    const fml::RefPtr<fml::RasterThreadMerger>& raster_thread_merger) {
   TRACE_EVENT0("flutter", "IOSExternalViewEmbedder::PostPrerollAction");
   FML_CHECK(platform_views_controller_);
   PostPrerollResult result = platform_views_controller_->PostPrerollAction(raster_thread_merger);
@@ -78,7 +78,7 @@ void IOSExternalViewEmbedder::SubmitFrame(
 
 // |ExternalViewEmbedder|
 void IOSExternalViewEmbedder::EndFrame(bool should_resubmit_frame,
-                                       fml::RefPtr<fml::RasterThreadMerger> raster_thread_merger) {
+                                       const fml::RefPtr<fml::RasterThreadMerger>& raster_thread_merger) {
   TRACE_EVENT0("flutter", "IOSExternalViewEmbedder::EndFrame");
   platform_views_controller_->EndFrame(should_resubmit_frame, raster_thread_merger);
 }
@@ -90,7 +90,7 @@ bool IOSExternalViewEmbedder::SupportsDynamicThreadMerging() {
 
 // |ExternalViewEmbedder|
 void IOSExternalViewEmbedder::PushFilterToVisitedPlatformViews(
-    std::shared_ptr<const DlImageFilter> filter,
+    const std::shared_ptr<const DlImageFilter>& filter,
     const SkRect& filter_rect) {
   platform_views_controller_->PushFilterToVisitedPlatformViews(filter, filter_rect);
 }
