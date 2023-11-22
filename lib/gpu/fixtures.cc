eb9e0b7ddd18ea73d922228ebcb536f40eda1850
Brandon DeRosier <bdero@google.com>
2023-11-22 14:09:34 -0800

    license header

--- lib/gpu/fixtures.cc
+++ lib/gpu/fixtures.cc
@@ -1,4 +1,12 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "flutter/lib/gpu/fixtures.h"
+
+// These blobs are generated using xxd -i [myfile.iplr].
+// This is a temporary hack to provision the shader library before the importer
+// is done.
 
 unsigned char kFlutterGPUUnlitVertIPLR[] = {
     0x18, 0x00, 0x00, 0x00, 0x49, 0x50, 0x4c, 0x52, 0x00, 0x00, 0x0e, 0x00,
