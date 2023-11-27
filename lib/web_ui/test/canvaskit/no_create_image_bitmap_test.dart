4aab2687f5457ad9e722452ff2b2e5512029e6fd
Harry Terkelsen <het@google.com>
2023-11-27 15:27:24 -0800

    Remove unused import in test

--- lib/web_ui/test/canvaskit/no_create_image_bitmap_test.dart
+++ lib/web_ui/test/canvaskit/no_create_image_bitmap_test.dart
@@ -1,8 +1,6 @@
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'dart:js_interop';
-
 import 'package:test/bootstrap/browser.dart';
 import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
