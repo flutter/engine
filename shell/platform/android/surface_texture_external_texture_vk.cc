725a70cda85b4a524151bcc2d88ab4ac0dcb00bc
Matan Lurey <matan@lurey.org>
2023-11-28 17:29:12 -0800

    Revert because I need EGL set up first.

--- shell/platform/android/surface_texture_external_texture_vk.cc
+++ shell/platform/android/surface_texture_external_texture_vk.cc
@@ -29,49 +29,11 @@ SurfaceTextureExternalTextureVK::~SurfaceTextureExternalTextureVK() {}
 void SurfaceTextureExternalTextureVK::ProcessFrame(PaintContext& context,
                                                    const SkRect& bounds) {
   if (state_ == AttachmentState::kUninitialized) {
-    FML_LOG(ERROR) << "Initializing SurfaceTextureExternalTextureVK.";
-    AImageReader* image_reader;
-    media_status_t status = NDKHelpers::AImageReader_new(
-        /*width=*/bounds.width(),
-        /*height=*/bounds.height(),
-        /*format=*/AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM,
-        /*maxImages=*/1,
-        /*reader=*/&image_reader);
-    if (status != AMEDIA_OK) {
-      FML_LOG(ERROR) << "Failed to create ImageReader.";
-      return;
-    }
-    FML_DCHECK(image_reader != nullptr);
-
-    // Configure the ImageReader to use the SurfaceTexture as its source.
-    auto native_window = NDKHelpers::AImageReader_getWindow(image_reader);
-    if (native_window == nullptr) {
-      FML_LOG(ERROR) << "Failed to get native window from ImageReader.";
-      return;
-    }
-
-    // Create a callback to be notified when a new image is available.
-    AImageReader_ImageListener image_listener;
-    image_listener.context = this;
-    image_listener.onImageAvailable = [](void* context, AImageReader* reader) {
-      FML_LOG(ERROR) << "OnImageAvailable";
-      static_cast<SurfaceTextureExternalTextureVK*>(context)->OnImageAvailable(
-          reader);
-    };
-    status = NDKHelpers::AImageReader_setImageListener(image_reader,
-                                                       &image_listener);
-    if (status != AMEDIA_OK) {
-      FML_LOG(ERROR) << "Failed to set image listener.";
-      return;
-    }
-    FML_LOG(ERROR) << "Image listener set.";
-
     // TODO: Remove. This is just to prevent assertion errors.
     state_ = AttachmentState::kAttached;
   }
 
   // TODO: Blit the image from the SurfaceTexture to the ImageReader.
-
   // TODO: Take the hardware buffer from the ImageReader annd render as DlImage.
 }
 
