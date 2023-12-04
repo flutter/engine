bde7992a069f90482bbc1f67005f1b07a289bc1c
jonahwilliams <jonahwilliams@google.com>
2023-12-04 08:41:25 -0800

    ++

--- impeller/aiks/aiks_unittests.cc
+++ impeller/aiks/aiks_unittests.cc
@@ -1984,7 +1984,7 @@ TEST_P(AiksTest, SolidStrokesRenderCorrectly) {
       paint.stroke_join = join;
       for (auto cap : {Cap::kButt, Cap::kSquare, Cap::kRound}) {
         paint.stroke_cap = cap;
-        canvas.DrawPath(std::move(path), paint);
+        canvas.DrawPath(path.Clone(), paint);
         canvas.Translate({80, 0});
       }
       canvas.Translate({-240, 60});
