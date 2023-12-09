32a34a17bb5792d5bcab74bf3dc446550c15a770
Bartek Pacia <barpac02@gmail.com>
2023-12-09 03:16:54 +0100

    SemanticsObjectTest: add tests for accessibilityIdentifier

--- shell/platform/darwin/ios/framework/Source/SemanticsObjectTest.mm
+++ shell/platform/darwin/ios/framework/Source/SemanticsObjectTest.mm
@@ -508,7 +508,22 @@ FLUTTER_ASSERT_ARC
   XCTAssertFalse(scrollView.isAccessibilityElement);
 }
 
-- (void)testFlutterScrollableSemanticsObjectWithLabelValueHintIsNotHiddenWhenVoiceOverIsRunning {
+- (void)testFlutterSemanticsObjectHasIdentifier {
+  flutter::testing::MockAccessibilityBridge* mock = new flutter::testing::MockAccessibilityBridge();
+  mock->isVoiceOverRunningValue = true;
+  fml::WeakPtrFactory<flutter::AccessibilityBridgeIos> factory(mock);
+  fml::WeakPtr<flutter::AccessibilityBridgeIos> bridge = factory.GetWeakPtr();
+
+  flutter::SemanticsNode node;
+  node.identifier = "identifier";
+
+  FlutterSemanticsObject* object = [[FlutterSemanticsObject alloc] initWithBridge:bridge uid:0];
+  [object setSemanticsNode:&node];
+  XCTAssertTrue([object.accessibilityIdentifier isEqualToString:@"identifier"]);
+}
+
+- (void)
+    testFlutterScrollableSemanticsObjectWithIdentifierLabelValueHintIsNotHiddenWhenVoiceOverIsRunning {
   flutter::testing::MockAccessibilityBridge* mock = new flutter::testing::MockAccessibilityBridge();
   mock->isVoiceOverRunningValue = true;
   fml::WeakPtrFactory<flutter::AccessibilityBridgeIos> factory(mock);
@@ -518,6 +533,7 @@ FLUTTER_ASSERT_ARC
   node.flags = static_cast<int32_t>(flutter::SemanticsFlags::kHasImplicitScrolling);
   node.actions = flutter::kHorizontalScrollSemanticsActions;
   node.rect = SkRect::MakeXYWH(0, 0, 100, 200);
+  node.identifier = "identifier";
   node.label = "label";
   node.value = "value";
   node.hint = "hint";
@@ -530,6 +546,8 @@ FLUTTER_ASSERT_ARC
   [scrollable accessibilityBridgeDidFinishUpdate];
   UIScrollView* scrollView = [scrollable nativeAccessibility];
   XCTAssertTrue(scrollView.isAccessibilityElement);
+  FML_LOG(INFO) << "scrollView.accessibilityIdentifier" << scrollView.accessibilityIdentifier;
+  XCTAssertTrue([scrollView.accessibilityIdentifier isEqualToString:@"identifier"]);
   XCTAssertTrue(
       [scrollView.accessibilityLabel isEqualToString:NSLocalizedString(@"label", @"test")]);
   XCTAssertTrue(
