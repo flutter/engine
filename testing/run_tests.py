716cdfdbdf76278e5c67762d21d7d256e5c14dbe
Zachary Anderson <zanderso@users.noreply.github.com>
2023-12-11 11:51:55 -0800

    Fix _availability_version_check for iOS 11 and 12 (#48624)
    This PR ports more of the implementation of availability checking from
    clang-rt into the Engine. In particular, when the call to look up the
    symbol `_availability_version_check` fails, this PR falls back on
    reading the platform version information out of a plist file at a
    well-known location, as is done
    [here](https://github.com/llvm/llvm-project/blob/2fd66e6eb659701b9d4c88708d55d5854a246815/compiler-rt/lib/builtins/os_version_check.c#L163).
    
    This change fixes a mistake in
    https://github.com/flutter/engine/pull/44711, which didn't account for
    `_availability_version_check` not being available on iOS 11 and 12.
    
    Fixes https://github.com/flutter/flutter/issues/138711

--- testing/run_tests.py
+++ testing/run_tests.py
@@ -434,6 +434,7 @@ def run_cc_tests(build_dir, executable_filter, coverage, capture_core_dump):
     unittests += [
         # The accessibility library only supports Mac and Windows.
         make_test('accessibility_unittests'),
+        make_test('availability_version_check_unittests'),
         make_test('framework_common_unittests'),
         make_test('spring_animation_unittests'),
     ]
