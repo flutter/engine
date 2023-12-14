053326a93f93734389939c90fe5d51391186743f
Brandon DeRosier <bdero@google.com>
2023-12-14 14:27:12 -0800

    Fix gn check typo

--- shell/platform/android/android_context_gl_impeller.cc
+++ shell/platform/android/android_context_gl_impeller.cc
@@ -13,7 +13,7 @@
 #include "impeller/entity/gles/framebuffer_blend_shaders_gles.h"
 
 #if IMPELLER_ENABLE_3D
-#include "impeller/scene/shaders/gles/scene_shaders_gles.h"  // nogcncheck
+#include "impeller/scene/shaders/gles/scene_shaders_gles.h"  // nogncheck
 #endif  // IMPELLER_ENABLE_3D
 namespace flutter {
 
