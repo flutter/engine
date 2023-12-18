jason-simmons/flutter_engine

[Impeller] disable entity culling by default. (#48717)
From local testing across both flutter galleries, wonderous, some other test apps, the only time this code successfully culls is during the stretch overscroll (and we cull 1 or so entries). The cost of this culling is approximately 20% of entity rendering time, and about 5% of overall raster time.

![image](https://github.com/flutter/engine/assets/8975114/ef85629c-48a8-457f-8385-0c8136404571)

flutter/impeller/entity/contents/clip_contents.cc
@@ -62,9 +62,8 @@ Contents::ClipCoverage ClipContents::GetClipCoverage(
   FML_UNREACHABLE();
 }
 
-bool ClipContents::ShouldRender(
-    const Entity& entity,
-    const std::optional<Rect>& clip_coverage) const {
+bool ClipContents::ShouldRender(const Entity& entity,
+                                const std::optional<Rect> clip_coverage) const {
   return true;
 }
 
@@ -161,7 +160,7 @@ Contents::ClipCoverage ClipRestoreContents::GetClipCoverage(
 
 bool ClipRestoreContents::ShouldRender(
     const Entity& entity,
-    const std::optional<Rect>& clip_coverage) const {
+    const std::optional<Rect> clip_coverage) const {
   return true;
 }
 

flutter/impeller/entity/contents/clip_contents.h
@@ -35,7 +35,7 @@ class ClipContents final : public Contents {
 
   // |Contents|
   bool ShouldRender(const Entity& entity,
-                    const std::optional<Rect>& clip_coverage) const override;
+                    const std::optional<Rect> clip_coverage) const override;
 
   // |Contents|
   bool Render(const ContentContext& renderer,
@@ -78,7 +78,7 @@ class ClipRestoreContents final : public Contents {
 
   // |Contents|
   bool ShouldRender(const Entity& entity,
-                    const std::optional<Rect>& clip_coverage) const override;
+                    const std::optional<Rect> clip_coverage) const override;
 
   // |Contents|
   bool Render(const ContentContext& renderer,

flutter/impeller/entity/contents/contents.cc
@@ -131,11 +131,10 @@ bool Contents::ApplyColorFilter(
 }
 
 bool Contents::ShouldRender(const Entity& entity,
-                            const std::optional<Rect>& clip_coverage) const {
+                            const std::optional<Rect> clip_coverage) const {
   if (!clip_coverage.has_value()) {
     return false;
   }
-
   auto coverage = GetCoverage(entity);
   if (!coverage.has_value()) {
     return false;

flutter/impeller/entity/contents/contents.h
@@ -123,7 +123,7 @@ class Contents {
       const std::string& label = "Snapshot") const;
 
   virtual bool ShouldRender(const Entity& entity,
-                            const std::optional<Rect>& clip_coverage) const;
+                            const std::optional<Rect> clip_coverage) const;
 
   //----------------------------------------------------------------------------
   /// @brief  Return the color source's intrinsic size, if available.

flutter/engine

impeller/entity/contents/clip_contents.cc
@@ -66,7 +66,7 @@ Contents::StencilCoverage ClipContents::GetStencilCoverage(
 
 bool ClipContents::ShouldRender(
     const Entity& entity,
-    const std::optional<Rect>& stencil_coverage) const {
+    const std::optional<Rect> stencil_coverage) const {
   return true;
 }
 
@@ -163,7 +163,7 @@ Contents::StencilCoverage ClipRestoreContents::GetStencilCoverage(
 
 bool ClipRestoreContents::ShouldRender(
     const Entity& entity,
-    const std::optional<Rect>& stencil_coverage) const {
+    const std::optional<Rect> stencil_coverage) const {
   return true;
 }
 

impeller/entity/contents/clip_contents.h
@@ -35,7 +35,7 @@ class ClipContents final : public Contents {
 
   // |Contents|
   bool ShouldRender(const Entity& entity,
-                    const std::optional<Rect>& stencil_coverage) const override;
+                    const std::optional<Rect> stencil_coverage) const override;
 
   // |Contents|
   bool Render(const ContentContext& renderer,
@@ -76,7 +76,7 @@ class ClipRestoreContents final : public Contents {
 
   // |Contents|
   bool ShouldRender(const Entity& entity,
-                    const std::optional<Rect>& stencil_coverage) const override;
+                    const std::optional<Rect> stencil_coverage) const override;
 
   // |Contents|
   bool Render(const ContentContext& renderer,

impeller/entity/contents/contents.cc
@@ -133,11 +133,10 @@ bool Contents::ApplyColorFilter(
 }
 
 bool Contents::ShouldRender(const Entity& entity,
-                            const std::optional<Rect>& stencil_coverage) const {
+                            const std::optional<Rect> stencil_coverage) const {
   if (!stencil_coverage.has_value()) {
     return false;
   }
-
   auto coverage = GetCoverage(entity);
   if (!coverage.has_value()) {
     return false;

impeller/entity/contents/contents.h
@@ -113,7 +113,7 @@ class Contents {
       const std::string& label = "Snapshot") const;
 
   virtual bool ShouldRender(const Entity& entity,
-                            const std::optional<Rect>& stencil_coverage) const;
+                            const std::optional<Rect> stencil_coverage) const;
 
   //----------------------------------------------------------------------------
   /// @brief  Return the color source's intrinsic size, if available.