eda8a02b099be47fe1e335ace0d5b3bbf7b073ec
Matan Lurey <matan@lurey.org>
2024-01-08 15:45:27 -0800

    Add documentation for editing the Android embedder.

---
+++ shell/platform/android/io/flutter/BuildConfig.java
@@ -0,0 +1,15 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// THIS FILE IS REPLACED WITH AN AUTO_GENERATED FILE (AND EXISTS JUST FOR IDE SUPPORT).
+// DO NOT EDIT THE VALUES HERE - SEE $flutter_root/tools/gen_android_buildconfig.py
+package io.flutter;
+
+public final class BuildConfig {
+  private BuildConfig() {{}}
+  public final static boolean DEBUG = false;
+  public final static boolean PROFILE = false;
+  public final static boolean RELEASE = false;
+  public final static boolean JIT_RELEASE = false;
+}
