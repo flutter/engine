c3160b1987fe61bfac39eef59d81dcac88ca79e7
Matan Lurey <matan@lurey.org>
2024-01-10 11:58:01 -0800

    Add a minimal test.

--- shell/platform/android/test/io/flutter/embedding/engine/renderer/SurfaceTextureSurfaceProducerTest.java
+++ shell/platform/android/test/io/flutter/embedding/engine/renderer/SurfaceTextureSurfaceProducerTest.java
@@ -1,11 +1,18 @@
 package io.flutter.embedding.engine.renderer;
 
+import static org.junit.Assert.assertEquals;
+import static org.robolectric.Shadows.shadowOf;
+
 import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Looper;
 import android.view.Surface;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RunWith(AndroidJUnit4.class)
 public final class SurfaceTextureSurfaceProducerTest {
@@ -16,8 +23,31 @@ public final class SurfaceTextureSurfaceProducerTest {
     // Create a surface and set the initial size.
     final SurfaceTextureSurfaceProducer producer = new SurfaceTextureSurfaceProducer(0);
     final Surface surface = producer.getSurface();
+    AtomicInteger frames = new AtomicInteger();
+    producer.getSurfaceTexture().setOnFrameAvailableListener((texture) -> {
+      if (texture.isReleased()) {
+        return;
+      }
+      frames.getAndIncrement();
+    });
     producer.setSize(100, 200);
 
-    // TODO: Finish implementing.
+    // Draw.
+    Canvas canvas = surface.lockHardwareCanvas();
+    canvas.drawARGB(255, 255, 0, 0);
+    surface.unlockCanvasAndPost(canvas);
+    shadowOf(Looper.getMainLooper()).idle();
+    assertEquals(frames.get(), 1);
+
+    // Resize and redraw.
+    producer.setSize(400, 800);
+    canvas = surface.lockHardwareCanvas();
+    canvas.drawARGB(255, 255, 0, 0);
+    surface.unlockCanvasAndPost(canvas);
+    shadowOf(Looper.getMainLooper()).idle();
+    assertEquals(frames.get(), 2);
+
+    // Done.
+    producer.release();
   }
 }
