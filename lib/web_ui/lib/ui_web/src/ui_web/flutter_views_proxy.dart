436e813dfb73e93bc7d5e0a6f21822ac55109d73
David Iglesias Teixeira <ditman@gmail.com>
2024-01-12 16:10:33 -0800

    Bring back web_ui getters.

--- lib/web_ui/lib/ui_web/src/ui_web/flutter_views_proxy.dart
+++ lib/web_ui/lib/ui_web/src/ui_web/flutter_views_proxy.dart
@@ -16,6 +16,16 @@ class FlutterViewManagerProxy {
     }) : _viewManager = viewManager;
   // The proxied viewManager instance.
   final FlutterViewManager _viewManager;
+
+  /// Returns the `hostElement` configuration value passed from JS when `viewId` was added.
+  ///
+  /// This is useful for plugins and apps to have a safe DOM Element where they
+  /// can add their own custom HTML elements (for example: file inputs for the
+  /// file_selector plugin).
+  JSAny? getHostElement(int viewId) {
+    return _viewManager.getOptions(viewId)?.hostElement as JSAny?;
+  }
+
   /// Returns the `initialData` configuration value passed from JS when `viewId` was added.
   ///
   /// Developers can access the initial data from Dart in two ways:
@@ -23,7 +33,7 @@ class FlutterViewManagerProxy {
   ///    passing to your views, to retain type safety on Dart (preferred).
   ///  * Calling [NullableUndefineableJSAnyExtension.dartify] and accessing the
   ///    returned object as if it were a [Map] (not recommended).
-  Object? getInitialData(int viewId) {
+  JSAny? getInitialData(int viewId) {
     return _viewManager.getOptions(viewId)?.initialData;
   }
 }
