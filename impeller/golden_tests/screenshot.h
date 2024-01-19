19ffc9deb28e59fa7a22ebb759985c6f6deac2e9
Aaron Clarke <aaclarke@google.com>
2024-01-19 13:52:20 -0800

    added docstrings

--- impeller/golden_tests/screenshot.h
+++ impeller/golden_tests/screenshot.h
@@ -17,14 +17,20 @@ class Screenshot {
  public:
   virtual ~Screenshot() = default;
 
+  /// Access raw data of the screenshot.
   virtual const uint8_t* GetBytes() const = 0;
 
+  /// Returns the height of the image in pixels.
   virtual size_t GetHeight() const = 0;
 
+  /// Returns the width of the image in pixels.
   virtual size_t GetWidth() const = 0;
 
+  /// Returns number of bytes required to represent one row of the raw image.
   virtual size_t GetBytesPerRow() const = 0;
 
+  /// Synchronously write the screenshot to disk as a PNG at `path`.  Returns
+  /// `true` if it succeeded.
   virtual bool WriteToPNG(const std::string& path) const = 0;
 };
 
