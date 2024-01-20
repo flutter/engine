9941c87886e6e414d3334bebfc8b32513aaa9719
Matan Lurey <matan@lurey.org>
2024-01-19 16:43:40 -0800

    Add hack to run on CI.

--- impeller/entity/geometry/geometry.cc
+++ impeller/entity/geometry/geometry.cc
@@ -122,8 +122,9 @@ GeometryResult ComputeUVGeometryForRect(Rect source_rect,
   // Calculate UV-specific transform based on texture coverage and effect.
   // For example, if the texture is 100x100 and the effect transform is
   // scaling by 2, then the UV transform should be scaling by 0.5.
-  auto uv_transform =
-      texture_coverage.GetNormalizingTransform() * effect_transform;
+  auto uv_transform = texture_coverage.GetNormalizingTransform() *
+                      // FIXME: This is a hack, and should not be submitted.
+                      effect_transform * entity.GetTransform().Basis();
 
   // Allocate space for vertex and UV data (4 vertices)
   // 0: position
