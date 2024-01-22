377f55a2da147a864c4b967c66be3ea5cf9d2fdc
jonahwilliams <jonahwilliams@google.com>
2024-01-22 12:19:49 -0800

    move pass bindings cache, fix state reset.

--- impeller/renderer/backend/vulkan/compute_pass_vk.cc
+++ impeller/renderer/backend/vulkan/compute_pass_vk.cc
@@ -9,6 +9,7 @@
 #include "impeller/renderer/backend/vulkan/formats_vk.h"
 #include "impeller/renderer/backend/vulkan/sampler_vk.h"
 #include "impeller/renderer/backend/vulkan/texture_vk.h"
+#include "vulkan/vulkan_structs.hpp"
 
 namespace impeller {
 
@@ -69,6 +70,11 @@ void ComputePassVK::SetPipeline(
 // |ComputePass|
 fml::Status ComputePassVK::Compute(const ISize& grid_size) {
   if (grid_size.IsEmpty() || !pipeline_valid_) {
+    bound_image_offset_ = 0u;
+    bound_buffer_offset_ = 0u;
+    descriptor_write_offset_ = 0u;
+    has_label_ = false;
+    pipeline_valid_ = false;
     return fml::Status(fml::StatusCode::kCancelled,
                        "Invalid pipeline or empty grid.");
   }
@@ -120,11 +126,11 @@ fml::Status ComputePassVK::Compute(const ISize& grid_size) {
   has_label_ = false;
 #endif  // IMPELLER_DEBUG
 
-  size_t bound_image_offset_ = 0u;
-  size_t bound_buffer_offset_ = 0u;
-  size_t descriptor_write_offset_ = 0u;
-  bool has_label_ = false;
-  bool pipeline_valid_ = false;
+  bound_image_offset_ = 0u;
+  bound_buffer_offset_ = 0u;
+  descriptor_write_offset_ = 0u;
+  has_label_ = false;
+  pipeline_valid_ = false;
 
   return fml::Status();
 }
@@ -145,7 +151,7 @@ bool ComputePassVK::BindResource(ShaderStage stage,
                                  const ShaderMetadata& metadata,
                                  std::shared_ptr<const Texture> texture,
                                  std::shared_ptr<const Sampler> sampler) {
-  if (bound_buffer_offset_ >= kMaxBindings) {
+  if (bound_image_offset_ >= kMaxBindings) {
     return false;
   }
   const TextureVK& texture_vk = TextureVK::Cast(*texture);
