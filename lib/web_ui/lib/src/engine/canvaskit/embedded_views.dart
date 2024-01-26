771d01b5b06798c3436231ed4e676edf758a84fe
David Iglesias Teixeira <ditman@gmail.com>
2024-01-26 11:42:45 -0800

    Remove unneeded toList

--- lib/web_ui/lib/src/engine/canvaskit/embedded_views.dart
+++ lib/web_ui/lib/src/engine/canvaskit/embedded_views.dart
@@ -658,7 +658,7 @@ class HtmlViewEmbedder {
 
   /// Disposes the state of this view embedder.
   void dispose() {
-    disposeViews(_viewClipChains.keys.toList());
+    disposeViews(_viewClipChains.keys);
     _context = EmbedderFrameContext();
     _currentCompositionParams.clear();
     debugCleanupSvgClipPaths();
