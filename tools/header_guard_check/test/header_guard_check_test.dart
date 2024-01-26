160f031f4bf7a339af4725e69dbe030735fca7a6
Matan Lurey <matan@lurey.org>
2024-01-26 15:10:22 -0800

    Remove prototype, fix TODOs.

--- tools/header_guard_check/test/header_guard_check_test.dart
+++ tools/header_guard_check/test/header_guard_check_test.dart
@@ -75,7 +75,7 @@ Future<int> main(List<String> args) async {
         expect(stdOut.toString(), contains('foo.h'));
         expect(stdOut.toString(), contains('baz.h'));
 
-        // TODO(https://github.com/flutter/flutter/issues/133569).
+        // TODO(matanlurey): https://github.com/flutter/flutter/issues/133569).
         if (stdOut.toString().contains('bar.h')) {
           // There is no not(contains(...)) matcher.
           fail('bar.h should not be checked. Output: $stdOut');
@@ -111,7 +111,7 @@ Future<int> main(List<String> args) async {
         expect(check.run(), completion(1));
         expect(stdOut.toString(), contains('bar.h'));
 
-        // TODO(https://github.com/flutter/flutter/issues/133569).
+        // TODO(matanlurey): https://github.com/flutter/flutter/issues/133569).
         if (stdOut.toString().contains('foo.h')) {
           // There is no not(contains(...)) matcher.
           fail('foo.h should not be checked. Output: $stdOut');
