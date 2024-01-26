2c920d8c39569d244fa702e2bd2216d994abee76
jonahwilliams <jonahwilliams@google.com>
2024-01-25 22:31:17 -0800

    remove unused field.

--- shell/platform/android/image_external_texture.h
+++ shell/platform/android/image_external_texture.h
@@ -62,7 +62,6 @@ class ImageExternalTexture : public flutter::Texture {
 
   enum class AttachmentState { kUninitialized, kAttached, kDetached };
   AttachmentState state_ = AttachmentState::kUninitialized;
-  bool new_frame_ready_ = false;
   sk_sp<flutter::DlImage> dl_image_;
   ImageLRU image_lru_ = ImageLRU();
 
