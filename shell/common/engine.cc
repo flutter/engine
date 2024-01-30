b7c0b58ae97b0aabd712609c1d65ecb06932fc5d
Tong Mu <dkwingsmt@gmail.com>
2024-01-29 18:53:32 -0800

    Everything but the extra view ID

--- shell/common/engine.cc
+++ shell/common/engine.cc
@@ -463,8 +463,7 @@ void Engine::ScheduleFrame(bool regenerate_layer_trees) {
   animator_->RequestFrame(regenerate_layer_trees);
 }
 
-void Engine::Render(int64_t view_id,
-                    std::unique_ptr<flutter::LayerTree> layer_tree,
+void Engine::Render(std::unique_ptr<flutter::LayerTree> layer_tree,
                     float device_pixel_ratio) {
   if (!layer_tree) {
     return;
@@ -475,6 +474,8 @@ void Engine::Render(int64_t view_id,
     return;
   }
 
+  // TODO(dkwingsmt): Currently only supports a single window.
+  int64_t view_id = 0;
   animator_->Render(view_id, std::move(layer_tree), device_pixel_ratio);
   rendered_views_during_frame_.insert(view_id);
   SubmitFrameIfEndOfFrame();
