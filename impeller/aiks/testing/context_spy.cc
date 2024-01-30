b4d81fb76a838a4ad83d0651bd71ff429d56d9ca
jonahwilliams <jonahwilliams@google.com>
2024-01-29 21:40:37 -0800

    maybe this works on CI.

--- impeller/aiks/testing/context_spy.cc
+++ impeller/aiks/testing/context_spy.cc
@@ -3,11 +3,22 @@
 // found in the LICENSE file.
 
 #include <memory>
+#include "impeller/renderer/command_buffer.h"
+#include "impeller/renderer/command_queue.h"
 
 #include "impeller/aiks/testing/context_spy.h"
 
 namespace impeller {
 namespace testing {
+
+fml::Status NoopCommandQueue::Submit(
+    const std::vector<std::shared_ptr<CommandBuffer>>& buffers,
+    const CompletionCallback& completion_callback) {
+  if (completion_callback) {
+    completion_callback(CommandBuffer::Status::kCompleted);
+  }
+  return fml::Status();
+}
 
 std::shared_ptr<ContextSpy> ContextSpy::Make() {
   return std::shared_ptr<ContextSpy>(new ContextSpy());
@@ -50,8 +61,8 @@ std::shared_ptr<ContextMock> ContextSpy::MakeContext(
     return real_context->GetPipelineLibrary();
   });
 
-  ON_CALL(*mock_context, GetCommandQueue).WillByDefault([real_context]() {
-    return real_context->GetCommandQueue();
+  ON_CALL(*mock_context, GetCommandQueue).WillByDefault([shared_this]() {
+    return shared_this->command_queue_;
   });
 
   ON_CALL(*mock_context, CreateCommandBuffer)
