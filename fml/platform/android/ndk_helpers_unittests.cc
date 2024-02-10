b02f974bb162ff6335901b1f260e7f37eb334960
Dan Field <dnfield@google.com>
2024-02-10 14:09:43 -0800

    missing files

---
+++ fml/platform/android/ndk_helpers_unittests.cc
@@ -0,0 +1,89 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "fml/message_loop.h"
+#include "fml/platform/android/ndk_helpers.h"
+
+#include "gtest/gtest.h"
+
+namespace flutter {
+namespace testing {
+namespace android {
+
+class NdkHelpersTest : public ::testing::Test {
+ public:
+  void SetUp() override { NDKHelpers::Init(); }
+
+  static void OnVsync(int64_t frame_nanos, void* data) {}
+};
+
+TEST_F(NdkHelpersTest, ATrace) {
+  // Test is going to expect to find symbols only present in 31 and above.
+  ASSERT_GT(android_get_device_api_level(), 22);
+}
+TEST_F(NdkHelpersTest, AChoreographer) {
+  // Test is going to expect to find symbols only present in 31 and above.
+  ASSERT_GT(android_get_device_api_level(), 30);
+
+  EXPECT_EQ(NDKHelpers::ChoreographerSupported(),
+            ChoreographerSupportStatus::kSupported64);
+
+  EXPECT_FALSE(NDKHelpers::AChoreographer_getInstance());
+
+  fml::MessageLoop::EnsureInitializedForCurrentThread();
+
+  EXPECT_FALSE(NDKHelpers::ATrace_isEnabled());
+
+  EXPECT_TRUE(NDKHelpers::AChoreographer_getInstance());
+
+  NDKHelpers::AChoreographer_postFrameCallback64(
+      NDKHelpers::AChoreographer_getInstance(), &OnVsync, nullptr);
+}
+
+TEST_F(NdkHelpersTest, HardwareBuffer) {
+  ASSERT_GT(android_get_device_api_level(), 31);
+
+  ASSERT_TRUE(NDKHelpers::HardwareBufferSupported());
+
+  AHardwareBuffer_Desc desc{
+      .width = 4,
+      .height = 4,
+      .layers = 1,
+      .format = AHardwareBuffer_Format::AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM,
+  };
+  EXPECT_TRUE(NDKHelpers::AHardwareBuffer_isSupported(&desc));
+
+  AHardwareBuffer* buffer = nullptr;
+  // AHardwareBuffer_allocate returns 0 on success.
+  EXPECT_EQ(NDKHelpers::AHardwareBuffer_allocate(&desc, &buffer), 0);
+  EXPECT_TRUE(buffer);
+
+  AHardwareBuffer_Desc out_desc = {};
+  NDKHelpers::AHardwareBuffer_describe(buffer, &out_desc);
+  EXPECT_EQ(desc.width, out_desc.width);
+  EXPECT_EQ(desc.height, out_desc.height);
+  EXPECT_EQ(desc.layers, out_desc.layers);
+  EXPECT_EQ(desc.format, out_desc.format);
+
+  EXPECT_TRUE(NDKHelpers::AHardwareBuffer_getId(buffer).has_value());
+
+  NDKHelpers::AHardwareBuffer_release(buffer);
+}
+
+TEST_F(NdkHelpersTest, SurfaceTransaction) {
+  ASSERT_GT(android_get_device_api_level(), 30);
+  EXPECT_TRUE(NDKHelpers::SurfaceControlAndTransactionSupported());
+
+  // Need ANativeWindow to create ASurfaceControl and set a buffer to the
+  // transaction. Just create/apply/delete as a smoke test.
+
+  ASurfaceTransaction* transaction = NDKHelpers::ASurfaceTransaction_create();
+  EXPECT_TRUE(transaction);
+  NDKHelpers::ASurfaceTransaction_apply(transaction);
+  NDKHelpers::ASurfaceTransaction_delete(transaction);
+}
+
+}  // namespace android
+}  // namespace testing
+}  // namespace flutter
