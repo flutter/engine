d622d87353fb8b35cd0ab37baa6f416db3279964
Matan Lurey <matan@lurey.org>
2024-02-14 13:55:15 -0800

    ++

--- testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/TestableFlutterActivity.java
+++ testing/scenario_app/android/app/src/main/java/dev/flutter/scenarios/TestableFlutterActivity.java
@@ -6,10 +6,8 @@ package dev.flutter.scenarios;
 
 import android.os.Bundle;
 import android.view.WindowManager;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import java.util.concurrent.atomic.AtomicBoolean;
