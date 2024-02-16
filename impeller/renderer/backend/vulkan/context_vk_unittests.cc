e79023b01d0491fc6d631581939f99b05caf8a05
jonahwilliams <jonahwilliams@google.com>
2024-02-16 11:50:09 -0800

    actually fix test.

--- impeller/renderer/backend/vulkan/context_vk_unittests.cc
+++ impeller/renderer/backend/vulkan/context_vk_unittests.cc
@@ -13,16 +13,16 @@ namespace impeller {
 namespace testing {
 
 TEST(ContextVKTest, CommonHardwareConcurrencyConfigurations) {
-  EXPECT_EQ(ChooseThreadCountForWorkers(100u), 4u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(9u), 4u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(8u), 4u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(7u), 3u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(6u), 3u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(5u), 2u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(4u), 2u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(3u), 1u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(2u), 1u);
-  EXPECT_EQ(ChooseThreadCountForWorkers(1u), 1u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(100u), 4u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(9u), 4u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(8u), 4u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(7u), 3u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(6u), 3u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(5u), 2u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(4u), 2u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(3u), 1u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(2u), 1u);
+  EXPECT_EQ(ContextVK::ChooseThreadCountForWorkers(1u), 1u);
 }
 
 TEST(ContextVKTest, DeletesCommandPools) {
