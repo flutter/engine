7014f7c89a943d75fa955461e8a1ba43067f205c
Jackson Gardner <jacksongardner@google.com>
2024-02-28 15:32:05 -0800

    Fix a few issues rendering text with Skwasm.
    This fixes https://github.com/flutter/flutter/issues/141001
    This also fixes https://github.com/flutter/flutter/issues/143743
    
    * We need to always call `setStrutEnabled(true)` on `StrutStyle`.
    * `getLineMetricsAt` had reversed ternary logic.
    * Ported unit tests from CanvasKit over to UI to cover Skwasm and HTML
    * The HTML renderer should return 0 line metrics for an empty paragraph.

--- lib/web_ui/lib/src/engine/text/layout_fragmenter.dart
+++ lib/web_ui/lib/src/engine/text/layout_fragmenter.dart
@@ -25,6 +25,9 @@ class LayoutFragmenter extends TextFragmenter {
 
   @override
   List<LayoutFragment> fragment() {
+    if (text.isEmpty) {
+      return <LayoutFragment>[];
+    }
     final List<LayoutFragment> fragments = <LayoutFragment>[];
 
     int fragmentStart = 0;
