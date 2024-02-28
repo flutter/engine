0cb3613f2b6b4493365ece8da5265c3b024783c5
Dan Field <dnfield@google.com>
2024-02-28 07:03:36 -0800

    missing symbols

--- shell/platform/android/test/io/flutter/plugin/platform/SingleViewPresentationTest.java
+++ shell/platform/android/test/io/flutter/plugin/platform/SingleViewPresentationTest.java
@@ -27,7 +27,7 @@ import org.robolectric.annotation.Config;
 @TargetApi(P)
 public class SingleViewPresentationTest {
   @Test
-  @Config(minSdk = KITKAT, maxSdk = R)
+  @Config(minSdk = 21, maxSdk = R)
   public void returnsOuterContextInputMethodManager() {
     // There's a bug in Android Q caused by the IMM being instanced per display.
     // https://github.com/flutter/flutter/issues/38375. We need the context returned by
@@ -58,7 +58,7 @@ public class SingleViewPresentationTest {
   }
 
   @Test
-  @Config(minSdk = KITKAT, maxSdk = R)
+  @Config(minSdk = 21, maxSdk = R)
   public void returnsOuterContextInputMethodManager_createDisplayContext() {
     // The IMM should also persist across display contexts created from the base context.
 
