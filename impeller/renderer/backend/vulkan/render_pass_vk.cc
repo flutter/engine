a557e0236a9944c62c58428be95b461b701c1851
jonahwilliams <jonahwilliams@google.com>
2024-02-28 11:08:49 -0800

    more fixes

--- impeller/renderer/backend/vulkan/render_pass_vk.cc
+++ impeller/renderer/backend/vulkan/render_pass_vk.cc
@@ -68,8 +68,7 @@ static std::vector<vk::ClearValue> GetVKClearValues(
   if (depth.has_value()) {
     clears.emplace_back(VKClearValueFromDepthStencil(
         stencil ? stencil->clear_stencil : 0u, depth->clear_depth));
-  }
-  if (stencil.has_value()) {
+  } else if (stencil.has_value()) {
     clears.emplace_back(VKClearValueFromDepthStencil(
         stencil->clear_stencil, depth ? depth->clear_depth : 0.0f));
   }
