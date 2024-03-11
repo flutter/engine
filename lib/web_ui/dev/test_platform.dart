3794546f53004f08725e85a31a4de8a6e25ba60c
David Iglesias Teixeira <ditman@gmail.com>
2024-03-11 10:36:12 -0700

    Revert "Ensure flutter.js can be defined after the _flutter namespace exists."
    This reverts commit 7af7c2cb1d0416a7db8d52bd3356a379f286197c.

--- lib/web_ui/dev/test_platform.dart
+++ lib/web_ui/dev/test_platform.dart
@@ -554,19 +554,13 @@ class BrowserPlatform extends PlatformPlugin {
         (CompileConfiguration config) => _makeBuildConfigString(scriptBase, config)
       ).join(',\n');
       final String bootstrapScript = '''
+<script src="/flutter_js/flutter.js"></script>
 <script>
-  // Define this before flutter.js to test PR flutter/engine#51294
-  if (_flutter == null) {
-    _flutter = {};
-  }
   _flutter.buildConfig = {
     builds: [
       $buildConfigsString
     ]
   };
-</script>
-<script src="/flutter_js/flutter.js"></script>
-<script>
   _flutter.loader.load({
     config: {
       canvasKitBaseUrl: "/canvaskit/",
