1b26d91f474ccfcd07920ae20f2ee72ddda58521
Jim Graham <flar@google.com>
2024-03-21 12:26:25 -0700

    expand list of planned geometry type replacements

--- display_list/geometry/dl_geometry_types_skia_aliases.h
+++ display_list/geometry/dl_geometry_types_skia_aliases.h
@@ -5,16 +5,29 @@
 #ifndef FLUTTER_DISPLAY_LIST_GEOMETRY_DL_GEOMETRY_TYPES_SKIA_ALIASES_H_
 #define FLUTTER_DISPLAY_LIST_GEOMETRY_DL_GEOMETRY_TYPES_SKIA_ALIASES_H_
 
+#include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkM44.h"
 #include "third_party/skia/include/core/SkMatrix.h"
+#include "third_party/skia/include/core/SkRRect.h"
 #include "third_party/skia/include/core/SkRect.h"
 
 using DlScalar = SkScalar;
+
 using DlIPoint = SkIPoint;
 using DlPoint = SkPoint;
+using DlPoint3 = SkPoint3;
+using DlVector2 = SkVector;
+
+using DlSize = SkSize;
+using DlISize = SkISize;
+
 using DlIRect = SkIRect;
 using DlRect = SkRect;
+
+using DlRRect = SkRRect;
+
 using DlTransform3x3 = SkMatrix;
 using DlTransform4x4 = SkM44;
+using DlRSTransform = SkRSXform;
 
 #endif  // FLUTTER_DISPLAY_LIST_GEOMETRY_DL_GEOMETRY_TYPES_SKIA_ALIASES_H_
