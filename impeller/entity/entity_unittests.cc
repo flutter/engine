87ba7a31b561392cf2df4cc9c3f4993a92b8f81d
jonahwilliams <jonahwilliams@google.com>
2024-03-28 14:22:50 -0700

    move for golden.

--- impeller/entity/entity_unittests.cc
+++ impeller/entity/entity_unittests.cc
@@ -2858,69 +2858,6 @@ TEST_P(EntityTest, CanRenderEmptyPathsWithoutCrashing) {
   ASSERT_TRUE(OpenPlaygroundHere(std::move(entity)));
 }
 
-TEST_P(EntityTest, BlitPassMipmapGenerationWorksCorrectly) {
-  TextureDescriptor texture_descriptor;
-  texture_descriptor.size = ISize{1024, 1024};
-  texture_descriptor.format = PixelFormat::kR8G8B8A8UNormInt;
-  texture_descriptor.storage_mode = StorageMode::kHostVisible;
-  texture_descriptor.mip_count = texture_descriptor.size.MipCount();
-
-  std::vector<uint8_t> bytes(4194304);
-  bool alternate = false;
-  for (auto i = 0u; i < 4194304; i += 4) {
-    if (alternate) {
-      bytes[i] = 255;
-      bytes[i + 1] = 0;
-      bytes[i + 2] = 0;
-      bytes[i + 3] = 255;
-    } else {
-      bytes[i] = 0;
-      bytes[i + 1] = 255;
-      bytes[i + 2] = 0;
-      bytes[i + 3] = 255;
-    }
-    alternate = !alternate;
-  }
-
-  ASSERT_EQ(texture_descriptor.GetByteSizeOfBaseMipLevel(), bytes.size());
-  auto mapping = std::make_shared<fml::NonOwnedMapping>(
-      bytes.data(),                                   // data
-      texture_descriptor.GetByteSizeOfBaseMipLevel()  // size
-  );
-  auto texture =
-      GetContext()->GetResourceAllocator()->CreateTexture(texture_descriptor);
-
-  ASSERT_TRUE(!!texture);
-  ASSERT_TRUE(texture->SetContents(mapping));
-
-  auto command_buffer = GetContext()->CreateCommandBuffer();
-  auto blit_pass = command_buffer->CreateBlitPass();
-
-  blit_pass->GenerateMipmap(texture);
-  EXPECT_TRUE(blit_pass->EncodeCommands(GetContext()->GetResourceAllocator()));
-  EXPECT_TRUE(GetContext()->GetCommandQueue()->Submit({command_buffer}).ok());
-
-  SamplerDescriptor descriptor;
-  descriptor.label = "Test Sampler";
-  descriptor.mag_filter = MinMagFilter::kLinear;
-  descriptor.min_filter = MinMagFilter::kLinear;
-
-  auto contents = std::make_shared<TextureContents>();
-  contents->SetTexture(texture);
-  contents->SetSourceRect(Rect::MakeSize(texture->GetSize()));
-  contents->SetDestinationRect(Rect::MakeLTRB(0, 0, 100, 100));
-  contents->SetStrictSourceRect(false);
-  contents->SetSamplerDescriptor(descriptor);
-  contents->SetOpacity(1.0);
-  contents->SetDeferApplyingOpacity(false);
-
-  Entity entity;
-  entity.SetClipDepth(0);
-  entity.SetContents(contents);
-
-  ASSERT_TRUE(OpenPlaygroundHere(std::move(entity)));
-}
-
 }  // namespace testing
 }  // namespace impeller
 
