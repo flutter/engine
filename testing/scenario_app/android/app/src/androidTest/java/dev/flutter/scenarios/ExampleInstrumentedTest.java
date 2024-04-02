9239ed9e9069b19c860fc6df10badc4c967af5e8
Matan Lurey <matan@lurey.org>
2024-04-02 15:30:19 -0700

    ++

--- testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenarios/ExampleInstrumentedTest.java
+++ testing/scenario_app/android/app/src/androidTest/java/dev/flutter/scenarios/ExampleInstrumentedTest.java
@@ -7,8 +7,8 @@ package dev.flutter.scenarios;
 import static org.junit.Assert.*;
 
 import android.content.Context;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
