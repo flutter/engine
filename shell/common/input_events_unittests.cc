ec0fd5b7b9619a5d191943d421d7e663778039c5
Tong Mu <dkwingsmt@gmail.com>
2024-04-05 17:36:16 -0700

    Fix tests

--- shell/common/input_events_unittests.cc
+++ shell/common/input_events_unittests.cc
@@ -124,7 +124,9 @@ static void TestSimulatedInputEvents(
     for (int i = 0, j = 0; i < num_events; j += 1) {
       double t = j * frame_time;
       while (i < num_events && delivery_time(i) <= t) {
-        ShellTest::DispatchFakePointerData(shell.get());
+        // Use a different x every time for the pointer data converter to
+        // generate non-empty events.
+        ShellTest::DispatchFakePointerData(shell.get(), /*x=*/i);
         i += 1;
       }
       ShellTest::VSyncFlush(shell.get(), &will_draw_new_frame);
@@ -146,6 +148,7 @@ static void TestSimulatedInputEvents(
 
   // Make sure that all events have been consumed so
   // https://github.com/flutter/flutter/issues/40863 won't happen again.
+  ASSERT_GT(events_consumed_at_frame.size(), 0u);
   ASSERT_EQ(events_consumed_at_frame.back(), num_events);
 }
 
