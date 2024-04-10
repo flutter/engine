d553fb02a2077c214aae3bc8ed2b08eaa4ef1291
Jonah Williams <jonahwilliams@google.com>
2024-04-10 13:08:57 -0700

    Update switches_unittests.cc

--- shell/common/switches_unittests.cc
+++ shell/common/switches_unittests.cc
@@ -123,17 +123,15 @@ TEST(SwitchesTest, NoEnableImpeller) {
   }
 }
 
+#if !FLUTTER_RELEASE
 TEST(SwitchesTest, EnableAsserts) {
   fml::CommandLine command_line = fml::CommandLineFromInitializerList(
       {"command", "--dart-flags=--enable-asserts"});
   Settings settings = SettingsFromCommandLine(command_line);
-#if !FLUTTER_RELEASE
   ASSERT_EQ(settings.dart_flags.size(), 1ul);
   EXPECT_EQ(settings.dart_flags[0], "--enable-asserts");
-#else
-  EXPECT_TRUE(settings.dart_flags.empty());
-#endif
 }
+#endif 
 
 }  // namespace testing
 }  // namespace flutter
