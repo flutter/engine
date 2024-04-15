206abba9e0f34990fe06654effa2f2b5c05eeb43
jonahwilliams <jonahwilliams@google.com>
2024-04-15 13:00:06 -0700

    revert unrelated change

--- shell/platform/android/io/flutter/plugin/platform/PlatformViewWrapper.java
+++ shell/platform/android/io/flutter/plugin/platform/PlatformViewWrapper.java
@@ -71,8 +71,6 @@ public class PlatformViewWrapper extends FrameLayout {
       } finally {
         surface.unlockCanvasAndPost(canvas);
       }
-    } else {
-      Log.e(TAG, "DISABLED SURFACE CLEAR");
     }
   }
 
