a0a9c62165d48496eb6d001117c2dff37faba2fd
jonahwilliams <jonahwilliams@google.com>
2024-04-20 12:36:05 -0700

    [Impeller] only use framebuffer advanced blends if available.

--- impeller/entity/contents/filters/blend_filter_contents.h
+++ impeller/entity/contents/filters/blend_filter_contents.h
@@ -78,6 +78,20 @@ class BlendFilterContents : public ColorFilterContents {
       std::optional<Scalar> alpha,
       ColorFilterContents::AbsorbOpacity absorb_opacity) const;
 
+  /// @brief Implements the advanced blends filters in terms of the framebuffer
+  /// blend filters.
+  ///
+  /// This allows a substantial reduction in the number of bootstrapped filters.
+  std::optional<Entity> CreateFramebufferAdvancedBlend(
+      const FilterInput::Vector& inputs,
+      const ContentContext& renderer,
+      const Entity& entity,
+      const Rect& coverage,
+      std::optional<Color> foreground_color,
+      BlendMode blend_mode,
+      std::optional<Scalar> alpha,
+      ColorFilterContents::AbsorbOpacity absorb_opacity) const;
+
   /// @brief Optimized porter-duff blend that avoids a second subpass when there
   ///        is only a single input and a foreground color.
   ///
