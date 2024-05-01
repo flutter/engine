flutter/flutter

commit 2f763b7a9317b176b83217cde486e7e0f6224f6a

[et] Prepare local_engine.json for CI, teach et to understand local build names (#51803)
This is steps (2) and (3) of
https://github.com/flutter/flutter/issues/145263.

The next steps after this are to:
1. Fix any issues that come up when running `local_engine.json` in CI.
1. Step (4) of https://github.com/flutter/flutter/issues/145263
1. Fill in some missing builds in `local_engine.json`.

tools/engine_tool/lib/src/commands/run_command.dart
@@ -23,8 +23,10 @@ final class RunCommand extends CommandBase {
     debugCheckBuilds(builds);
     // We default to nothing in order to automatically detect attached devices
     // and select an appropriate target from them.
-    addConfigOption(argParser, runnableBuilds(environment, configs),
-        defaultsTo: '');
+    addConfigOption(
+      environment, argParser, runnableBuilds(environment, configs),
+      defaultsTo: '',
+    );
     argParser.addFlag(
       rbeFlag,
       defaultsTo: true,
@@ -47,26 +49,27 @@ final class RunCommand extends CommandBase {
       'See `flutter run --help` for a listing';
 
   Build? _lookup(String configName) {
-    return builds.where((Build build) => build.name == configName).firstOrNull;
+    final String demangledName = demangleConfigName(environment, configName);
+    return builds.where((Build build) => build.name == demangledName).firstOrNull;
   }
 
   Build? _findHostBuild(Build? targetBuild) {
     if (targetBuild == null) {
       return null;
     }
-
-    final String name = targetBuild.name;
-    if (name.startsWith('host_')) {
+    final String mangledName = mangleConfigName(environment, targetBuild.name);
+    if (mangledName.contains('host_')) {
       return targetBuild;
     }
     // TODO(johnmccutchan): This is brittle, it would be better if we encoded
     // the host config name in the target config.
-    if (name.contains('_debug')) {
-      return _lookup('host_debug');
-    } else if (name.contains('_profile')) {
-      return _lookup('host_profile');
-    } else if (name.contains('_release')) {
-      return _lookup('host_release');
+    final String ci = mangledName.startsWith('ci') ? mangledName.substring(0, 3) : '';
+    if (mangledName.contains('_debug')) {
+      return _lookup('${ci}host_debug');
+    } else if (mangledName.contains('_profile')) {
+      return _lookup('${ci}host_profile');
+    } else if (mangledName.contains('_release')) {
+      return _lookup('${ci}host_release');
     }
     return null;
   }
@@ -101,13 +104,13 @@ final class RunCommand extends CommandBase {
   Future<String?> _selectTargetConfig() async {
     final String configName = argResults![configFlag] as String;
     if (configName.isNotEmpty) {
-      return configName;
+      return demangleConfigName(environment, configName);
     }
     final String deviceId = _getDeviceId();
     final RunTarget? target =
         await detectAndSelectRunTarget(environment, deviceId);
     if (target == null) {
-      return 'host_debug';
+      return demangleConfigName(environment, 'host_debug');
     }
     environment.logger.status(
         'Building to run on "${target.name}" running ${target.targetPlatform}');

tools/engine_tool/test/run_command_test.dart
@@ -31,19 +31,19 @@ void main() {
 
   final BuilderConfig linuxTestConfig = BuilderConfig.fromJson(
     path: 'ci/builders/linux_test_config.json',
-    map: convert.jsonDecode(fixtures.testConfig('Linux'))
+    map: convert.jsonDecode(fixtures.testConfig('Linux', Platform.linux))
         as Map<String, Object?>,
   );
 
   final BuilderConfig macTestConfig = BuilderConfig.fromJson(
     path: 'ci/builders/mac_test_config.json',
-    map: convert.jsonDecode(fixtures.testConfig('Mac-12'))
+    map: convert.jsonDecode(fixtures.testConfig('Mac-12', Platform.macOS))
         as Map<String, Object?>,
   );
 
   final BuilderConfig winTestConfig = BuilderConfig.fromJson(
     path: 'ci/builders/win_test_config.json',
-    map: convert.jsonDecode(fixtures.testConfig('Windows-11'))
+    map: convert.jsonDecode(fixtures.testConfig('Windows-11', Platform.windows))
         as Map<String, Object?>,
   );
 
@@ -62,7 +62,8 @@ void main() {
         engine: engine,
         platform: FakePlatform(
             operatingSystem: Platform.linux,
-            resolvedExecutable: io.Platform.resolvedExecutable),
+            resolvedExecutable: io.Platform.resolvedExecutable,
+            pathSeparator: '/'),
         processRunner: ProcessRunner(
           processManager: FakeProcessManager(onStart: (List<String> command) {
             runHistory.add(command);
@@ -152,9 +153,9 @@ void main() {
           'flutter',
           'run',
           '--local-engine',
-          'android_debug_arm64',
+          'linux/android_debug_arm64',
           '--local-engine-host',
-          'host_debug',
+          'linux/host_debug',
           '-d',
           'emulator'
         ]));

.ci.yaml
@@ -49,6 +49,14 @@ platform_properties:
 # https://chromium.googlesource.com/chromium/src.git/+/HEAD/tools/android/avd/proto
 # You may use those names for the android_virtual_device version.
 targets:
+  - name: Linux local_engine_builds
+    bringup: true
+    enabled_branches:
+      - main
+    recipe: engine_v2/engine_v2
+    properties:
+      config_name: local_engine
+
   - name: Linux linux_android_emulator_tests
     enabled_branches:
       - main

tools/engine_tool/lib/src/build_utils.dart
@@ -65,6 +65,57 @@ void debugCheckBuilds(List<Build> builds) {
   }
 }
 
+String _ciPrefix(Environment env) => 'ci${env.platform.pathSeparator}';
+String _osPrefix(Environment env) => '${env.platform.operatingSystem}/';
+const String _webTestsPrefix = 'web_tests/';
+
+bool _doNotMangle(Environment env, String name) {
+  return name.startsWith(_ciPrefix(env)) || name.startsWith(_webTestsPrefix);
+}
+
+/// Transform the name of a build into the name presented and accepted by the
+/// CLI
+///
+/// If a name starts with '$OS/', it is a local development build, and the
+/// mangled name has the '$OS/' part stripped off, where $OS is the value of
+/// `platform.operatingSystem` in the passed-in `environment`.
+///
+/// If the name does not start with '$OS/', then it must start with 'ci/',
+/// 'ci\', or 'web_tests/' in which case the name is returned unchanged.
+///
+/// Examples:
+///   macos/host_debug -> host_debug
+///   ci/ios_release -> ci/ios_release
+///   ci\host_profile -> ci\host_profile
+///   web_tests/artifacts -> web_tests/artifacts
+String mangleConfigName(Environment env, String name) {
+  final String osPrefix = _osPrefix(env);
+  if (name.startsWith(osPrefix)) {
+    return name.substring(osPrefix.length);
+  }
+  if (_doNotMangle(env, name)) {
+    return name;
+  }
+  throw ArgumentError(
+    'name argument "$name" must start with a valid platform name or "ci"',
+  );
+}
+
+/// Transform the mangled (CLI) name of a build into its true name in the build
+/// config json file.
+///
+/// This does the reverse of [mangleConfigName] taking the operating system
+/// name from `environment`.
+///
+/// Examples:
+///   host_debug -> macos/host_debug
+///   ci/ios_release -> ci/ios_release
+///   ci\host_profile -> ci\host_profile
+///   web_tests/artifacts -> web_tests/artifacts
+String demangleConfigName(Environment env, String name) {
+  return _doNotMangle(env, name) ? name : '${_osPrefix(env)}$name';
+}
+
 /// Build the build target in the environment.
 Future<int> runBuild(Environment environment, Build build,
     {List<String> extraGnArgs = const <String>[],

tools/engine_tool/lib/src/commands/build_command.dart
@@ -17,7 +17,9 @@ final class BuildCommand extends CommandBase {
   }) {
     builds = runnableBuilds(environment, configs);
     debugCheckBuilds(builds);
-    addConfigOption(argParser, runnableBuilds(environment, configs));
+    addConfigOption(
+      environment, argParser, runnableBuilds(environment, configs),
+    );
     argParser.addFlag(
       rbeFlag,
       defaultsTo: true,
@@ -39,8 +41,9 @@ final class BuildCommand extends CommandBase {
   Future<int> run() async {
     final String configName = argResults![configFlag] as String;
     final bool useRbe = argResults![rbeFlag] as bool;
+    final String demangledName = demangleConfigName(environment, configName);
     final Build? build =
-        builds.where((Build build) => build.name == configName).firstOrNull;
+        builds.where((Build build) => build.name == demangledName).firstOrNull;
     if (build == null) {
       environment.logger.error('Could not find config $configName');
       return 1;

tools/engine_tool/lib/src/commands/command.dart
@@ -6,6 +6,7 @@ import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:engine_build_configs/engine_build_configs.dart';
 
+import '../build_utils.dart';
 import '../environment.dart';
 import 'flags.dart';
 
@@ -19,18 +20,24 @@ abstract base class CommandBase extends Command<int> {
 }
 
 /// Adds the -c (--config) option to the parser.
-void addConfigOption(ArgParser parser, List<Build> builds,
-    {String defaultsTo = 'host_debug'}) {
+void addConfigOption(
+  Environment environment,
+  ArgParser parser,
+  List<Build> builds, {
+  String defaultsTo = 'host_debug',
+}) {
   parser.addOption(
     configFlag,
     abbr: 'c',
     defaultsTo: defaultsTo,
     help: 'Specify the build config to use',
     allowed: <String>[
-      for (final Build config in builds) config.name,
+      for (final Build config in builds)
+        mangleConfigName(environment, config.name),
     ],
     allowedHelp: <String, String>{
-      for (final Build config in builds) config.name: config.gn.join(' '),
+      for (final Build config in builds)
+        mangleConfigName(environment, config.name): config.gn.join(' '),
     },
   );
 }

tools/engine_tool/lib/src/commands/command_runner.dart
@@ -2,10 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:engine_build_configs/engine_build_configs.dart';
 
 import '../environment.dart';
+import '../logger.dart';
 import 'build_command.dart';
 import 'fetch_command.dart';
 import 'flags.dart';
@@ -61,8 +63,13 @@ final class ToolCommandRunner extends CommandRunner<int> {
 
   @override
   Future<int> run(Iterable<String> args) async {
+    final ArgResults argResults = parse(args);
+    final bool verbose = argResults[verboseFlag]! as bool;
+    if (verbose) {
+      environment.logger.level = Logger.infoLevel;
+    }
     try {
-      return await runCommand(parse(args)) ?? 0;
+      return await runCommand(argResults) ?? 0;
     } on FormatException catch (e) {
       environment.logger.error(e);
       return 1;

tools/engine_tool/lib/src/commands/query_command.dart
@@ -140,7 +140,9 @@ final class QueryTestsCommand extends CommandBase {
   }) {
     builds = runnableBuilds(environment, configs);
     debugCheckBuilds(builds);
-    addConfigOption(argParser, runnableBuilds(environment, configs));
+    addConfigOption(
+      environment, argParser, runnableBuilds(environment, configs),
+    );
   }
 
   /// Build configurations loaded from the engine from under ci/builders.
@@ -158,8 +160,9 @@ final class QueryTestsCommand extends CommandBase {
   @override
   Future<int> run() async {
     final String configName = argResults![configFlag] as String;
+    final String demangledName = demangleConfigName(environment, configName);
     final Build? build =
-        builds.where((Build build) => build.name == configName).firstOrNull;
+        builds.where((Build build) => build.name == demangledName).firstOrNull;
     if (build == null) {
       environment.logger.error('Could not find config $configName');
       return 1;

flutter/engine

commit b2664439e91da1183c80c76128a3aecc41ae71fc

Fix et run
et run was broken in https://github.com/flutter/engine/pull/51803

this PR adds the missing calls to mangledConfigName before invoking flutter run

tools/engine_tool/lib/src/commands/run_command.dart
@@ -30,7 +30,9 @@ final class RunCommand extends CommandBase {
     // We default to nothing in order to automatically detect attached devices
     // and select an appropriate target from them.
     addConfigOption(
-      environment, argParser, builds,
+      environment,
+      argParser,
+      builds,
       defaultsTo: '',
     );
     argParser.addFlag(
@@ -58,7 +60,9 @@ See `flutter run --help` for a listing
 
   Build? _lookup(String configName) {
     final String demangledName = demangleConfigName(environment, configName);
-    return builds.where((Build build) => build.name == demangledName).firstOrNull;
+    return builds
+        .where((Build build) => build.name == demangledName)
+        .firstOrNull;
   }
 
   Build? _findHostBuild(Build? targetBuild) {
@@ -71,7 +75,8 @@ See `flutter run --help` for a listing
     }
     // TODO(johnmccutchan): This is brittle, it would be better if we encoded
     // the host config name in the target config.
-    final String ci = mangledName.startsWith('ci') ? mangledName.substring(0, 3) : '';
+    final String ci =
+        mangledName.startsWith('ci') ? mangledName.substring(0, 3) : '';
     if (mangledName.contains('_debug')) {
       return _lookup('${ci}host_debug');
     } else if (mangledName.contains('_profile')) {
@@ -166,21 +171,28 @@ See `flutter run --help` for a listing
       }
     }
 
+    final String mangledBuildName = mangleConfigName(environment, build.name);
+
+    final String mangledHostBuildName =
+        mangleConfigName(environment, hostBuild.name);
+
+    final List<String> command = <String>[
+      'flutter',
+      'run',
+      '--local-engine-src-path',
+      environment.engine.srcDir.path,
+      '--local-engine',
+      mangledBuildName,
+      '--local-engine-host',
+      mangledHostBuildName,
+      ...argResults!.rest
+    ];
+
     // TODO(johnmccutchan): Be smart and if the user requested a profile
     // config, add the '--profile' flag when invoking flutter run.
     final ProcessRunnerResult result =
         await environment.processRunner.runProcess(
-      <String>[
-        'flutter',
-        'run',
-        '--local-engine-src-path',
-        environment.engine.srcDir.path,
-        '--local-engine',
-        build.name,
-        '--local-engine-host',
-        hostBuild.name,
-        ...argResults!.rest,
-      ],
+      command,
       runInShell: true,
       startMode: ProcessStartMode.inheritStdio,
     );

tools/engine_tool/test/run_command_test.dart
@@ -153,9 +153,9 @@ void main() {
           'flutter',
           'run',
           '--local-engine',
-          'linux/android_debug_arm64',
+          'android_debug_arm64',
           '--local-engine-host',
-          'linux/host_debug',
+          'host_debug',
           '-d',
           'emulator'
         ]));