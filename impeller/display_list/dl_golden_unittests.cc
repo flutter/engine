8cdfe115448d4490c630fd51496d217970bc41b6
Aaron Clarke <aaclarke@google.com>
2024-05-08 09:05:42 -0700

    [Impeller] added display list golden test

---
+++ impeller/display_list/dl_golden_unittests.cc
@@ -0,0 +1,30 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "impeller/display_list/dl_golden_unittests.h"
+
+#include "flutter/display_list/dl_builder.h"
+#include "flutter/testing/testing.h"
+#include "gtest/gtest.h"
+#include "impeller/display_list/dl_dispatcher.h"
+
+namespace impeller {
+namespace testing {
+
+INSTANTIATE_PLAYGROUND_SUITE(DlGoldenTest);
+
+TEST_P(DlGoldenTest, CanDrawPaint) {
+  flutter::DisplayListBuilder builder;
+  builder.Scale(0.2, 0.2);
+  flutter::DlPaint paint;
+  paint.setColor(flutter::DlColor::kCyan());
+  builder.DrawPaint(paint);
+
+  DlDispatcher dispatcher;
+  builder.Build()->Dispatch(dispatcher);
+  ASSERT_TRUE(OpenPlaygroundHere(dispatcher.EndRecordingAsPicture()));
+}
+
+}  // namespace testing
+}  // namespace impeller
