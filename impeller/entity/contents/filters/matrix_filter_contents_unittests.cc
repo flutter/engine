7100e5c43c6004a2ea44439fbd3914d8004b1d64
Aaron Clarke <aaclarke@google.com>
2024-05-15 15:51:12 -0700

    expanded tests

--- impeller/entity/contents/filters/matrix_filter_contents_unittests.cc
+++ impeller/entity/contents/filters/matrix_filter_contents_unittests.cc
@@ -6,17 +6,43 @@
 #include "gmock/gmock.h"
 #include "impeller/entity/contents/filters/matrix_filter_contents.h"
 
-#if FML_OS_MACOSX
-#define IMPELLER_RAND arc4random
-#else
-#define IMPELLER_RAND rand
-#endif
-
 namespace impeller {
 namespace testing {
 TEST(MatrixFilterContentsTest, Create) {
   MatrixFilterContents contents;
   EXPECT_TRUE(contents.IsTranslationOnly());
+}
+
+TEST(MatrixFilterContentsTest, CoverageEmpty) {
+  MatrixFilterContents contents;
+  FilterInput::Vector inputs = {};
+  Entity entity;
+  std::optional<Rect> coverage =
+      contents.GetFilterCoverage(inputs, entity, /*effect_transform=*/Matrix());
+  ASSERT_FALSE(coverage.has_value());
+}
+
+TEST(MatrixFilterContentsTest, CoverageSimple) {
+  MatrixFilterContents contents;
+  FilterInput::Vector inputs = {
+      FilterInput::Make(Rect::MakeLTRB(10, 10, 110, 110))};
+  Entity entity;
+  std::optional<Rect> coverage =
+      contents.GetFilterCoverage(inputs, entity, /*effect_transform=*/Matrix());
+
+  ASSERT_EQ(coverage, Rect::MakeLTRB(10, 10, 110, 110));
+}
+
+TEST(MatrixFilterContentsTest, Coverage2x) {
+  MatrixFilterContents contents;
+  contents.SetMatrix(Matrix::MakeScale({2.0, 2.0, 1.0}));
+  FilterInput::Vector inputs = {
+      FilterInput::Make(Rect::MakeXYWH(10, 10, 100, 100))};
+  Entity entity;
+  std::optional<Rect> coverage =
+      contents.GetFilterCoverage(inputs, entity, /*effect_transform=*/Matrix());
+
+  ASSERT_EQ(coverage, Rect::MakeXYWH(20, 20, 200, 200));
 }
 
 }  // namespace testing
