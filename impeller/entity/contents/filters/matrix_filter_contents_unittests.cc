0f18297b81f345bdae075812bc864eda09f8fd74
Aaron Clarke <aaclarke@google.com>
2024-05-16 08:52:33 -0700

    added test

--- impeller/entity/contents/filters/matrix_filter_contents_unittests.cc
+++ impeller/entity/contents/filters/matrix_filter_contents_unittests.cc
@@ -45,5 +45,16 @@ TEST(MatrixFilterContentsTest, Coverage2x) {
   ASSERT_EQ(coverage, Rect::MakeXYWH(20, 20, 200, 200));
 }
 
+TEST(MatrixFilterContentsTest, Coverage2xEffect) {
+  MatrixFilterContents contents;
+  FilterInput::Vector inputs = {
+      FilterInput::Make(Rect::MakeXYWH(10, 10, 100, 100))};
+  Entity entity;
+  std::optional<Rect> coverage = contents.GetFilterCoverage(
+      inputs, entity, /*effect_transform=*/Matrix::MakeScale({2.0, 2.0, 1.0}));
+
+  ASSERT_EQ(coverage, Rect::MakeXYWH(10, 10, 100, 100));
+}
+
 }  // namespace testing
 }  // namespace impeller
