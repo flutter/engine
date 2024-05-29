flutter/flutter

commit 6b857def404083a709bf3a6b5a4aea7f442a73d0

Refactor GL rendering to prepare for GTK4 (#50754)
Refactor GL rendering to prepare for GTK4    
- Remove FlGLArea - we can just use GtkGLArea directly
- Stop using gdk_cairo_draw_from_gl, it's not available in GTK4.
- Rename FlRendererGL to FlRendererGdk, this was implying that
FlRenderer could be something other than OpenGL which is not the case
currently.
- Remove unnecessary virtual methods in FlRenderer, just implement them
as standard methods.

Related: https://github.com/flutter/flutter/issues/143408

shell/platform/linux/fl_renderer_gdk.cc
@@ -1,0 +1,89 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "flutter/shell/platform/linux/fl_renderer_gdk.h"
+
+struct _FlRendererGdk {
+  FlRenderer parent_instance;
+
+  // Window being rendered on.
+  GdkWindow* window;
+
+  // Main OpenGL rendering context.
+  GdkGLContext* main_context;
+
+  // Secondary OpenGL rendering context.
+  GdkGLContext* resource_context;
+};
+
+G_DEFINE_TYPE(FlRendererGdk, fl_renderer_gdk, fl_renderer_get_type())
+
+// Implements FlRenderer::make_current.
+static void fl_renderer_gdk_make_current(FlRenderer* renderer) {
+  FlRendererGdk* self = FL_RENDERER_GDK(renderer);
+  gdk_gl_context_make_current(self->main_context);
+}
+
+// Implements FlRenderer::make_resource_current.
+static void fl_renderer_gdk_make_resource_current(FlRenderer* renderer) {
+  FlRendererGdk* self = FL_RENDERER_GDK(renderer);
+  gdk_gl_context_make_current(self->resource_context);
+}
+
+// Implements FlRenderer::clear_current.
+static void fl_renderer_gdk_clear_current(FlRenderer* renderer) {
+  gdk_gl_context_clear_current();
+}
+
+static void fl_renderer_gdk_dispose(GObject* object) {
+  FlRendererGdk* self = FL_RENDERER_GDK(object);
+
+  g_clear_object(&self->main_context);
+  g_clear_object(&self->resource_context);
+
+  G_OBJECT_CLASS(fl_renderer_gdk_parent_class)->dispose(object);
+}
+
+static void fl_renderer_gdk_class_init(FlRendererGdkClass* klass) {
+  G_OBJECT_CLASS(klass)->dispose = fl_renderer_gdk_dispose;
+
+  FL_RENDERER_CLASS(klass)->make_current = fl_renderer_gdk_make_current;
+  FL_RENDERER_CLASS(klass)->make_resource_current =
+      fl_renderer_gdk_make_resource_current;
+  FL_RENDERER_CLASS(klass)->clear_current = fl_renderer_gdk_clear_current;
+}
+
+static void fl_renderer_gdk_init(FlRendererGdk* self) {}
+
+FlRendererGdk* fl_renderer_gdk_new(GdkWindow* window) {
+  FlRendererGdk* self =
+      FL_RENDERER_GDK(g_object_new(fl_renderer_gdk_get_type(), nullptr));
+  self->window = window;
+  return self;
+}
+
+gboolean fl_renderer_gdk_create_contexts(FlRendererGdk* self, GError** error) {
+  self->main_context = gdk_window_create_gl_context(self->window, error);
+  if (self->main_context == nullptr) {
+    return FALSE;
+  }
+  if (!gdk_gl_context_realize(self->main_context, error)) {
+    return FALSE;
+  }
+
+  self->resource_context = gdk_window_create_gl_context(self->window, error);
+  if (self->resource_context == nullptr) {
+    return FALSE;
+  }
+  if (!gdk_gl_context_realize(self->resource_context, error)) {
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+GdkGLContext* fl_renderer_gdk_get_context(FlRendererGdk* self) {
+  g_return_val_if_fail(FL_IS_RENDERER_GDK(self), nullptr);
+  return self->main_context;
+}

shell/platform/linux/BUILD.gn
@@ -105,7 +105,6 @@ source_set("flutter_linux_sources") {
     "fl_dart_project.cc",
     "fl_engine.cc",
     "fl_event_channel.cc",
-    "fl_gl_area.cc",
     "fl_gnome_settings.cc",
     "fl_json_message_codec.cc",
     "fl_json_method_codec.cc",
@@ -126,7 +125,7 @@ source_set("flutter_linux_sources") {
     "fl_plugin_registrar.cc",
     "fl_plugin_registry.cc",
     "fl_renderer.cc",
-    "fl_renderer_gl.cc",
+    "fl_renderer_gdk.cc",
     "fl_renderer_headless.cc",
     "fl_scrolling_manager.cc",
     "fl_scrolling_view_delegate.cc",
@@ -246,7 +245,10 @@ executable("flutter_linux_unittests") {
 
   public_configs = [ "//flutter:config" ]
 
-  configs += [ "//flutter/shell/platform/linux/config:gtk" ]
+  configs += [
+    "//flutter/shell/platform/linux/config:gtk",
+    "//flutter/shell/platform/linux/config:epoxy",
+  ]
 
   defines = [
     "FLUTTER_ENGINE_NO_PROTOTYPES",

shell/platform/linux/fl_engine.cc
@@ -217,22 +217,14 @@ static void* fl_engine_gl_proc_resolver(void* user_data, const char* name) {
 
 static bool fl_engine_gl_make_current(void* user_data) {
   FlEngine* self = static_cast<FlEngine*>(user_data);
-  g_autoptr(GError) error = nullptr;
-  gboolean result = fl_renderer_make_current(self->renderer, &error);
-  if (!result) {
-    g_warning("%s", error->message);
-  }
-  return result;
+  fl_renderer_make_current(self->renderer);
+  return true;
 }
 
 static bool fl_engine_gl_clear_current(void* user_data) {
   FlEngine* self = static_cast<FlEngine*>(user_data);
-  g_autoptr(GError) error = nullptr;
-  gboolean result = fl_renderer_clear_current(self->renderer, &error);
-  if (!result) {
-    g_warning("%s", error->message);
-  }
-  return result;
+  fl_renderer_clear_current(self->renderer);
+  return true;
 }
 
 static uint32_t fl_engine_gl_get_fbo(void* user_data) {
@@ -248,12 +240,8 @@ static bool fl_engine_gl_present(void* user_data) {
 
 static bool fl_engine_gl_make_resource_current(void* user_data) {
   FlEngine* self = static_cast<FlEngine*>(user_data);
-  g_autoptr(GError) error = nullptr;
-  gboolean result = fl_renderer_make_resource_current(self->renderer, &error);
-  if (!result) {
-    g_warning("%s", error->message);
-  }
-  return result;
+  fl_renderer_make_resource_current(self->renderer);
+  return true;
 }
 
 // Called by the engine to retrieve an external texture.

shell/platform/linux/fl_gl_area.cc
@@ -1,136 +1,0 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "flutter/shell/platform/linux/fl_gl_area.h"
-
-#include <epoxy/gl.h>
-
-struct _FlGLArea {
-  GtkWidget parent_instance;
-
-  GdkGLContext* context;
-
-  GPtrArray* textures;
-};
-
-G_DEFINE_TYPE(FlGLArea, fl_gl_area, GTK_TYPE_WIDGET)
-
-static void fl_gl_area_dispose(GObject* gobject) {
-  FlGLArea* self = FL_GL_AREA(gobject);
-
-  g_clear_object(&self->context);
-  g_clear_pointer(&self->textures, g_ptr_array_unref);
-
-  G_OBJECT_CLASS(fl_gl_area_parent_class)->dispose(gobject);
-}
-
-// Implements GtkWidget::realize.
-static void fl_gl_area_realize(GtkWidget* widget) {
-  GtkAllocation allocation;
-  gtk_widget_get_allocation(widget, &allocation);
-
-  gtk_widget_set_realized(widget, TRUE);
-
-  GdkWindowAttr attributes;
-  attributes.window_type = GDK_WINDOW_CHILD;
-  attributes.x = allocation.x;
-  attributes.y = allocation.y;
-  attributes.width = allocation.width;
-  attributes.height = allocation.height;
-  attributes.wclass = GDK_INPUT_OUTPUT;
-  attributes.visual = gtk_widget_get_visual(widget);
-  attributes.event_mask = gtk_widget_get_events(widget);
-  gint attributes_mask = GDK_WA_X | GDK_WA_Y | GDK_WA_VISUAL;
-  GdkWindow* window = gdk_window_new(gtk_widget_get_parent_window(widget),
-                                     &attributes, attributes_mask);
-  gtk_widget_set_window(widget, window);
-  gtk_widget_register_window(widget, window);
-}
-
-// Implements GtkWidget::unrealize.
-static void fl_gl_area_unrealize(GtkWidget* widget) {
-  FlGLArea* self = FL_GL_AREA(widget);
-  gdk_gl_context_make_current(self->context);
-  g_clear_pointer(&self->textures, g_ptr_array_unref);
-
-  /* Make sure to unset the context if current */
-  if (self->context == gdk_gl_context_get_current()) {
-    gdk_gl_context_clear_current();
-  }
-
-  GTK_WIDGET_CLASS(fl_gl_area_parent_class)->unrealize(widget);
-}
-
-// Implements GtkWidget::size_allocate.
-static void fl_gl_area_size_allocate(GtkWidget* widget,
-                                     GtkAllocation* allocation) {
-  gtk_widget_set_allocation(widget, allocation);
-
-  if (gtk_widget_get_has_window(widget)) {
-    if (gtk_widget_get_realized(widget)) {
-      gdk_window_move_resize(gtk_widget_get_window(widget), allocation->x,
-                             allocation->y, allocation->width,
-                             allocation->height);
-    }
-  }
-}
-
-// Implements GtkWidget::draw.
-static gboolean fl_gl_area_draw(GtkWidget* widget, cairo_t* cr) {
-  FlGLArea* self = FL_GL_AREA(widget);
-
-  gdk_gl_context_make_current(self->context);
-
-  gint scale = gtk_widget_get_scale_factor(widget);
-
-  if (self->textures == nullptr) {
-    return TRUE;
-  }
-
-  for (guint i = 0; i < self->textures->len; i++) {
-    FlBackingStoreProvider* texture =
-        FL_BACKING_STORE_PROVIDER(g_ptr_array_index(self->textures, i));
-    uint32_t texture_id = fl_backing_store_provider_get_gl_texture_id(texture);
-    GdkRectangle geometry = fl_backing_store_provider_get_geometry(texture);
-    gdk_cairo_draw_from_gl(cr, gtk_widget_get_window(widget), texture_id,
-                           GL_TEXTURE, scale, geometry.x, geometry.y,
-                           geometry.width, geometry.height);
-  }
-
-  return TRUE;
-}
-
-static void fl_gl_area_class_init(FlGLAreaClass* klass) {
-  GObjectClass* gobject_class = G_OBJECT_CLASS(klass);
-  gobject_class->dispose = fl_gl_area_dispose;
-
-  GtkWidgetClass* widget_class = GTK_WIDGET_CLASS(klass);
-  widget_class->realize = fl_gl_area_realize;
-  widget_class->unrealize = fl_gl_area_unrealize;
-  widget_class->size_allocate = fl_gl_area_size_allocate;
-  widget_class->draw = fl_gl_area_draw;
-
-  gtk_widget_class_set_accessible_role(widget_class, ATK_ROLE_DRAWING_AREA);
-}
-
-static void fl_gl_area_init(FlGLArea* self) {
-  gtk_widget_set_app_paintable(GTK_WIDGET(self), TRUE);
-}
-
-GtkWidget* fl_gl_area_new(GdkGLContext* context) {
-  g_return_val_if_fail(GDK_IS_GL_CONTEXT(context), nullptr);
-  FlGLArea* area =
-      reinterpret_cast<FlGLArea*>(g_object_new(fl_gl_area_get_type(), nullptr));
-  area->context = GDK_GL_CONTEXT(g_object_ref(context));
-  return GTK_WIDGET(area);
-}
-
-void fl_gl_area_queue_render(FlGLArea* self, GPtrArray* textures) {
-  g_return_if_fail(FL_IS_GL_AREA(self));
-
-  g_clear_pointer(&self->textures, g_ptr_array_unref);
-  self->textures = g_ptr_array_ref(textures);
-
-  gtk_widget_queue_draw(GTK_WIDGET(self));
-}

shell/platform/linux/fl_gl_area.h
@@ -1,44 +1,0 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#ifndef FLUTTER_SHELL_PLATFORM_LINUX_FL_GL_AREA_H_
-#define FLUTTER_SHELL_PLATFORM_LINUX_FL_GL_AREA_H_
-
-#include <gtk/gtk.h>
-
-#include "flutter/shell/platform/linux/fl_backing_store_provider.h"
-
-G_BEGIN_DECLS
-
-G_DECLARE_FINAL_TYPE(FlGLArea, fl_gl_area, FL, GL_AREA, GtkWidget)
-
-/**
- * FlGLArea:
- *
- * #FlGLArea is a OpenGL drawing area that shows Flutter backing store Layer.
- */
-
-/**
- * fl_gl_area_new:
- * @context: an #GdkGLContext.
- *
- * Creates a new #FlGLArea widget.
- *
- * Returns: the newly created #FlGLArea widget.
- */
-GtkWidget* fl_gl_area_new(GdkGLContext* context);
-
-/**
- * fl_gl_area_queue_render:
- * @area: an #FlGLArea.
- * @textures: (transfer none) (element-type FlBackingStoreProvider): a list of
- * #FlBackingStoreProvider.
- *
- * Queues textures to be drawn later.
- */
-void fl_gl_area_queue_render(FlGLArea* area, GPtrArray* textures);
-
-G_END_DECLS
-
-#endif  // FLUTTER_SHELL_PLATFORM_LINUX_FL_GL_AREA_H_

shell/platform/linux/fl_renderer.cc
@@ -10,6 +10,27 @@
 #include "flutter/shell/platform/embedder/embedder.h"
 #include "flutter/shell/platform/linux/fl_backing_store_provider.h"
 #include "flutter/shell/platform/linux/fl_engine_private.h"
+#include "flutter/shell/platform/linux/fl_view_private.h"
+
+// Vertex shader to draw Flutter window contents.
+static const char* vertex_shader_src =
+    "attribute vec2 position;\n"
+    "attribute vec2 in_texcoord;\n"
+    "varying vec2 texcoord;\n"
+    "\n"
+    "void main() {\n"
+    "  gl_Position = vec4(position, 0, 1);\n"
+    "  texcoord = in_texcoord;\n"
+    "}\n";
+
+// Fragment shader to draw Flutter window contents.
+static const char* fragment_shader_src =
+    "uniform sampler2D texture;\n"
+    "varying vec2 texcoord;\n"
+    "\n"
+    "void main() {\n"
+    "  gl_FragColor = texture2D(texture, texcoord);\n"
+    "}\n";
 
 G_DEFINE_QUARK(fl_renderer_error_quark, fl_renderer_error)
 
@@ -27,11 +48,45 @@ typedef struct {
   // was rendered
   bool had_first_frame;
 
-  GdkGLContext* main_context;
-  GdkGLContext* resource_context;
+  // Shader program.
+  GLuint program;
+
+  // Textures to render.
+  GPtrArray* textures;
 } FlRendererPrivate;
 
 G_DEFINE_TYPE_WITH_PRIVATE(FlRenderer, fl_renderer, G_TYPE_OBJECT)
+
+// Returns the log for the given OpenGL shader. Must be freed by the caller.
+static gchar* get_shader_log(GLuint shader) {
+  int log_length;
+  gchar* log;
+
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+
+  log = static_cast<gchar*>(g_malloc(log_length + 1));
+  glGetShaderInfoLog(shader, log_length, nullptr, log);
+
+  return log;
+}
+
+// Returns the log for the given OpenGL program. Must be freed by the caller.
+static gchar* get_program_log(GLuint program) {
+  int log_length;
+  gchar* log;
+
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length);
+
+  log = static_cast<gchar*>(g_malloc(log_length + 1));
+  glGetProgramInfoLog(program, log_length, nullptr, log);
+
+  return log;
+}
+
+/// Converts a pixel co-ordinate from 0..pixels to OpenGL -1..1.
+static GLfloat pixels_to_gl_coords(GLfloat position, GLfloat pixels) {
+  return (2.0 * position / pixels) - 1.0;
+}
 
 static void fl_renderer_unblock_main_thread(FlRenderer* self) {
   FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
@@ -45,96 +100,103 @@ static void fl_renderer_unblock_main_thread(FlRenderer* self) {
   }
 }
 
-static void fl_renderer_dispose(GObject* self) {
-  fl_renderer_unblock_main_thread(FL_RENDERER(self));
-  G_OBJECT_CLASS(fl_renderer_parent_class)->dispose(self);
+static void fl_renderer_dispose(GObject* object) {
+  FlRenderer* self = FL_RENDERER(object);
+  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
+      fl_renderer_get_instance_private(self));
+
+  fl_renderer_unblock_main_thread(self);
+
+  g_clear_pointer(&priv->textures, g_ptr_array_unref);
+
+  G_OBJECT_CLASS(fl_renderer_parent_class)->dispose(object);
 }
 
 static void fl_renderer_class_init(FlRendererClass* klass) {
   G_OBJECT_CLASS(klass)->dispose = fl_renderer_dispose;
 }
 
-static void fl_renderer_init(FlRenderer* self) {}
+static void fl_renderer_init(FlRenderer* self) {
+  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
+      fl_renderer_get_instance_private(self));
+  priv->textures = g_ptr_array_new_with_free_func(g_object_unref);
+}
 
-gboolean fl_renderer_start(FlRenderer* self, FlView* view, GError** error) {
+gboolean fl_renderer_start(FlRenderer* self, FlView* view) {
+  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
+      fl_renderer_get_instance_private(self));
+
   g_return_val_if_fail(FL_IS_RENDERER(self), FALSE);
-  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
-      fl_renderer_get_instance_private(self));
+
   priv->view = view;
-  gboolean result = FL_RENDERER_GET_CLASS(self)->create_contexts(
-      self, GTK_WIDGET(view), &priv->main_context, &priv->resource_context,
-      error);
-
-  if (result) {
-    gdk_gl_context_realize(priv->main_context, error);
-    gdk_gl_context_realize(priv->resource_context, error);
-  }
-
-  if (*error != nullptr) {
-    return FALSE;
-  }
   return TRUE;
-}
-
-FlView* fl_renderer_get_view(FlRenderer* self) {
-  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
-      fl_renderer_get_instance_private(self));
-  return priv->view;
-}
-
-GdkGLContext* fl_renderer_get_context(FlRenderer* self) {
-  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
-      fl_renderer_get_instance_private(self));
-  return priv->main_context;
 }
 
 void* fl_renderer_get_proc_address(FlRenderer* self, const char* name) {
+  g_return_val_if_fail(FL_IS_RENDERER(self), NULL);
+
   return reinterpret_cast<void*>(eglGetProcAddress(name));
 }
 
-gboolean fl_renderer_make_current(FlRenderer* self, GError** error) {
-  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
-      fl_renderer_get_instance_private(self));
-  if (priv->main_context) {
-    gdk_gl_context_make_current(priv->main_context);
-  }
-
-  return TRUE;
+void fl_renderer_make_current(FlRenderer* self) {
+  g_return_if_fail(FL_IS_RENDERER(self));
+  FL_RENDERER_GET_CLASS(self)->make_current(self);
 }
 
-gboolean fl_renderer_make_resource_current(FlRenderer* self, GError** error) {
-  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
-      fl_renderer_get_instance_private(self));
-  if (priv->resource_context) {
-    gdk_gl_context_make_current(priv->resource_context);
-  }
-
-  return TRUE;
+void fl_renderer_make_resource_current(FlRenderer* self) {
+  g_return_if_fail(FL_IS_RENDERER(self));
+  FL_RENDERER_GET_CLASS(self)->make_resource_current(self);
 }
 
-gboolean fl_renderer_clear_current(FlRenderer* self, GError** error) {
-  gdk_gl_context_clear_current();
-  return TRUE;
+void fl_renderer_clear_current(FlRenderer* self) {
+  g_return_if_fail(FL_IS_RENDERER(self));
+  FL_RENDERER_GET_CLASS(self)->clear_current(self);
 }
 
 guint32 fl_renderer_get_fbo(FlRenderer* self) {
+  g_return_val_if_fail(FL_IS_RENDERER(self), 0);
+
   // There is only one frame buffer object - always return that.
   return 0;
 }
 
 gboolean fl_renderer_create_backing_store(
-    FlRenderer* self,
+    FlRenderer* renderer,
     const FlutterBackingStoreConfig* config,
     FlutterBackingStore* backing_store_out) {
-  return FL_RENDERER_GET_CLASS(self)->create_backing_store(self, config,
-                                                           backing_store_out);
+  fl_renderer_make_current(renderer);
+
+  FlBackingStoreProvider* provider =
+      fl_backing_store_provider_new(config->size.width, config->size.height);
+  if (!provider) {
+    g_warning("Failed to create backing store");
+    return FALSE;
+  }
+
+  uint32_t name = fl_backing_store_provider_get_gl_framebuffer_id(provider);
+  uint32_t format = fl_backing_store_provider_get_gl_format(provider);
+
+  backing_store_out->type = kFlutterBackingStoreTypeOpenGL;
+  backing_store_out->open_gl.type = kFlutterOpenGLTargetTypeFramebuffer;
+  backing_store_out->open_gl.framebuffer.user_data = provider;
+  backing_store_out->open_gl.framebuffer.name = name;
+  backing_store_out->open_gl.framebuffer.target = format;
+  backing_store_out->open_gl.framebuffer.destruction_callback = [](void* p) {
+    // Backing store destroyed in fl_renderer_collect_backing_store(), set
+    // on FlutterCompositor.collect_backing_store_callback during engine start.
+  };
+
+  return TRUE;
 }
 
 gboolean fl_renderer_collect_backing_store(
     FlRenderer* self,
     const FlutterBackingStore* backing_store) {
-  return FL_RENDERER_GET_CLASS(self)->collect_backing_store(self,
-                                                            backing_store);
+  fl_renderer_make_current(self);
+
+  // OpenGL context is required when destroying #FlBackingStoreProvider.
+  g_object_unref(backing_store->open_gl.framebuffer.user_data);
+  return TRUE;
 }
 
 void fl_renderer_wait_for_frame(FlRenderer* self,
@@ -142,6 +204,8 @@ void fl_renderer_wait_for_frame(FlRenderer* self,
                                 int target_height) {
   FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
       fl_renderer_get_instance_private(self));
+
+  g_return_if_fail(FL_IS_RENDERER(self));
 
   priv->target_width = target_width;
   priv->target_height = target_height;
@@ -160,6 +224,8 @@ gboolean fl_renderer_present_layers(FlRenderer* self,
   FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
       fl_renderer_get_instance_private(self));
 
+  g_return_val_if_fail(FL_IS_RENDERER(self), FALSE);
+
   // ignore incoming frame with wrong dimensions in trivial case with just one
   // layer
   if (priv->blocking_main_thread && layers_count == 1 &&
@@ -173,6 +239,138 @@ gboolean fl_renderer_present_layers(FlRenderer* self,
 
   fl_renderer_unblock_main_thread(self);
 
-  return FL_RENDERER_GET_CLASS(self)->present_layers(self, layers,
-                                                     layers_count);
+  if (!priv->view) {
+    return FALSE;
+  }
+
+  g_ptr_array_set_size(priv->textures, 0);
+  for (size_t i = 0; i < layers_count; ++i) {
+    const FlutterLayer* layer = layers[i];
+    switch (layer->type) {
+      case kFlutterLayerContentTypeBackingStore: {
+        const FlutterBackingStore* backing_store = layer->backing_store;
+        auto framebuffer = &backing_store->open_gl.framebuffer;
+        FlBackingStoreProvider* provider =
+            FL_BACKING_STORE_PROVIDER(framebuffer->user_data);
+        g_ptr_array_add(priv->textures, g_object_ref(provider));
+      } break;
+      case kFlutterLayerContentTypePlatformView: {
+        // TODO(robert-ancell) Not implemented -
+        // https://github.com/flutter/flutter/issues/41724
+      } break;
+    }
+  }
+
+  fl_view_redraw(priv->view);
+
+  return TRUE;
+}
+
+void fl_renderer_setup(FlRenderer* self) {
+  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
+      fl_renderer_get_instance_private(self));
+
+  g_return_if_fail(FL_IS_RENDERER(self));
+
+  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_src, nullptr);
+  glCompileShader(vertex_shader);
+  int vertex_compile_status;
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &vertex_compile_status);
+  if (vertex_compile_status == GL_FALSE) {
+    g_autofree gchar* shader_log = get_shader_log(vertex_shader);
+    g_warning("Failed to compile vertex shader: %s", shader_log);
+  }
+
+  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_src, nullptr);
+  glCompileShader(fragment_shader);
+  int fragment_compile_status;
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &fragment_compile_status);
+  if (fragment_compile_status == GL_FALSE) {
+    g_autofree gchar* shader_log = get_shader_log(fragment_shader);
+    g_warning("Failed to compile fragment shader: %s", shader_log);
+  }
+
+  priv->program = glCreateProgram();
+  glAttachShader(priv->program, vertex_shader);
+  glAttachShader(priv->program, fragment_shader);
+  glLinkProgram(priv->program);
+
+  int link_status;
+  glGetProgramiv(priv->program, GL_LINK_STATUS, &link_status);
+  if (link_status == GL_FALSE) {
+    g_autofree gchar* program_log = get_program_log(priv->program);
+    g_warning("Failed to link program: %s", program_log);
+  }
+
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+}
+
+void fl_renderer_render(FlRenderer* self, int width, int height) {
+  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
+      fl_renderer_get_instance_private(self));
+
+  g_return_if_fail(FL_IS_RENDERER(self));
+
+  glClearColor(0.0, 0.0, 0.0, 1.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glUseProgram(priv->program);
+
+  for (guint i = 0; i < priv->textures->len; i++) {
+    FlBackingStoreProvider* texture =
+        FL_BACKING_STORE_PROVIDER(g_ptr_array_index(priv->textures, i));
+
+    uint32_t texture_id = fl_backing_store_provider_get_gl_texture_id(texture);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+
+    // Translate into OpenGL co-ordinates
+    GdkRectangle texture_geometry =
+        fl_backing_store_provider_get_geometry(texture);
+    GLfloat texture_x = texture_geometry.x;
+    GLfloat texture_y = texture_geometry.y;
+    GLfloat texture_width = texture_geometry.width;
+    GLfloat texture_height = texture_geometry.height;
+    GLfloat x0 = pixels_to_gl_coords(texture_x, width);
+    GLfloat y0 =
+        pixels_to_gl_coords(height - (texture_y + texture_height), height);
+    GLfloat x1 = pixels_to_gl_coords(texture_x + texture_width, width);
+    GLfloat y1 = pixels_to_gl_coords(height - texture_y, height);
+    GLfloat vertex_data[] = {x0, y0, 0, 0, x1, y1, 1, 1, x0, y1, 0, 1,
+                             x0, y0, 0, 0, x1, y0, 1, 0, x1, y1, 1, 1};
+
+    GLuint vao, vertex_buffer;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data,
+                 GL_STATIC_DRAW);
+    GLint position_index = glGetAttribLocation(priv->program, "position");
+    glEnableVertexAttribArray(position_index);
+    glVertexAttribPointer(position_index, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(GLfloat) * 4, 0);
+    GLint texcoord_index = glGetAttribLocation(priv->program, "in_texcoord");
+    glEnableVertexAttribArray(texcoord_index);
+    glVertexAttribPointer(texcoord_index, 2, GL_FLOAT, GL_FALSE,
+                          sizeof(GLfloat) * 4, (void*)(sizeof(GLfloat) * 2));
+
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vertex_buffer);
+  }
+
+  glFlush();
+}
+
+void fl_renderer_cleanup(FlRenderer* self) {
+  FlRendererPrivate* priv = reinterpret_cast<FlRendererPrivate*>(
+      fl_renderer_get_instance_private(self));
+
+  g_return_if_fail(FL_IS_RENDERER(self));
+
+  glDeleteProgram(priv->program);
 }

flutter/engine

commit 407bae5d7f96e9abcaf84c7bff810e5194ae5224

Fix rendering corruption by Flutter and GDK sharing the same OpenGL context
Solved by having three contexts - one for GDK and two for Flutter.

Regression introduced in https://github.com/flutter/engine/pull/50754

Fixes https://github.com/flutter/flutter/issues/148653

shell/platform/linux/fl_renderer_gdk.cc
@@ -10,10 +10,13 @@ struct _FlRendererGdk {
   // Window being rendered on.
   GdkWindow* window;
 
-  // Main OpenGL rendering context.
+  // OpenGL rendering context used by GDK.
+  GdkGLContext* gdk_context;
+
+  // Main OpenGL rendering context used by Flutter.
   GdkGLContext* main_context;
 
-  // Secondary OpenGL rendering context.
+  // Secondary OpenGL rendering context used by Flutter.
   GdkGLContext* resource_context;
 };
 
@@ -56,6 +59,7 @@ static gdouble fl_renderer_gdk_get_refresh_rate(FlRenderer* renderer) {
 static void fl_renderer_gdk_dispose(GObject* object) {
   FlRendererGdk* self = FL_RENDERER_GDK(object);
 
+  g_clear_object(&self->gdk_context);
   g_clear_object(&self->main_context);
   g_clear_object(&self->resource_context);
 
@@ -82,6 +86,14 @@ FlRendererGdk* fl_renderer_gdk_new(GdkWindow* window) {
 }
 
 gboolean fl_renderer_gdk_create_contexts(FlRendererGdk* self, GError** error) {
+  self->gdk_context = gdk_window_create_gl_context(self->window, error);
+  if (self->gdk_context == nullptr) {
+    return FALSE;
+  }
+  if (!gdk_gl_context_realize(self->gdk_context, error)) {
+    return FALSE;
+  }
+
   self->main_context = gdk_window_create_gl_context(self->window, error);
   if (self->main_context == nullptr) {
     return FALSE;
@@ -103,5 +115,5 @@ gboolean fl_renderer_gdk_create_contexts(FlRendererGdk* self, GError** error) {
 
 GdkGLContext* fl_renderer_gdk_get_context(FlRendererGdk* self) {
   g_return_val_if_fail(FL_IS_RENDERER_GDK(self), nullptr);
-  return self->main_context;
+  return self->gdk_context;
 }