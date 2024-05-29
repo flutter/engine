39a4c8ec4dc1e6530f5372a54caeee0049dd6150
Justin McCandless <jmccandless@google.com>
2024-05-29 12:02:18 -0700

    Version gate

--- shell/platform/android/io/flutter/embedding/android/FlutterView.java
+++ shell/platform/android/io/flutter/embedding/android/FlutterView.java
@@ -1136,8 +1136,10 @@ public class FlutterView extends FrameLayout
       Log.e(TAG, "TextServicesManager not supported by device, spell check disabled.");
     }
 
+    if (Build.VERSION.SDK_INT >= API_LEVELS.API_34) {
       scribePlugin =
           new ScribePlugin(this, textInputPlugin.getInputMethodManager(), this.flutterEngine.getScribeChannel());
+    }
 
     localizationPlugin = this.flutterEngine.getLocalizationPlugin();
 
