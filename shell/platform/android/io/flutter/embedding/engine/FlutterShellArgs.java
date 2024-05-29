e9d80f4a44b7bb2b90ca35af4e49d3df03bf6f3c
jonahwilliams <jonahwilliams@google.com>
2024-05-29 16:15:40 -0700

    allow opting out of impeller.

--- shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
@@ -46,6 +46,7 @@ public class FlutterShellArgs {
   public static final String ARG_TRACE_TO_FILE = "--trace-to-file";
   public static final String ARG_KEY_ENABLE_IMPELLER = "enable-impeller";
   public static final String ARG_ENABLE_IMPELLER = "--enable-impeller";
+  public static final String ARG_DISABLE_IMPELLER = "--enable-impeller";
   public static final String ARG_KEY_ENABLE_VULKAN_VALIDATION = "enable-vulkan-validation";
   public static final String ARG_ENABLE_VULKAN_VALIDATION = "--enable-vulkan-validation";
   public static final String ARG_KEY_DUMP_SHADER_SKP_ON_SHADER_COMPILATION =
@@ -125,6 +126,8 @@ public class FlutterShellArgs {
     }
     if (intent.getBooleanExtra(ARG_KEY_ENABLE_IMPELLER, false)) {
       args.add(ARG_ENABLE_IMPELLER);
+    } else {
+      args.add(ARG_DISABLE_IMPELLER);
     }
     if (intent.getBooleanExtra(ARG_KEY_ENABLE_VULKAN_VALIDATION, false)) {
       args.add(ARG_ENABLE_VULKAN_VALIDATION);
