a7f84b1f5c8451e414a21dd4b00cc5a38a5bf3b3
jonahwilliams <jonahwilliams@google.com>
2024-05-29 16:20:54 -0700

    ++

--- shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
@@ -46,7 +46,7 @@ public class FlutterShellArgs {
   public static final String ARG_TRACE_TO_FILE = "--trace-to-file";
   public static final String ARG_KEY_ENABLE_IMPELLER = "enable-impeller";
   public static final String ARG_ENABLE_IMPELLER = "--enable-impeller";
-  public static final String ARG_DISABLE_IMPELLER = "--enable-impeller";
+  public static final String ARG_DISABLE_IMPELLER = "--no-enable-impeller";
   public static final String ARG_KEY_ENABLE_VULKAN_VALIDATION = "enable-vulkan-validation";
   public static final String ARG_ENABLE_VULKAN_VALIDATION = "--enable-vulkan-validation";
   public static final String ARG_KEY_DUMP_SHADER_SKP_ON_SHADER_COMPILATION =
