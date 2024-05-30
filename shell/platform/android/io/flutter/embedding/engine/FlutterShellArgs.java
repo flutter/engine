c23104fef98fa5637bb0f5601227bbf2994849ed
jonahwilliams <jonahwilliams@google.com>
2024-05-29 17:06:31 -0700

    default to --enable-impeller

--- shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
+++ shell/platform/android/io/flutter/embedding/engine/FlutterShellArgs.java
@@ -45,7 +45,6 @@ public class FlutterShellArgs {
   public static final String ARG_KEY_TRACE_TO_FILE = "trace-to-file";
   public static final String ARG_TRACE_TO_FILE = "--trace-to-file";
   public static final String ARG_KEY_ENABLE_IMPELLER = "enable-impeller";
-  public static final String ARG_ENABLE_IMPELLER = "--enable-impeller=true";
   public static final String ARG_DISABLE_IMPELLER = "--enable-impeller=false";
   public static final String ARG_KEY_ENABLE_VULKAN_VALIDATION = "enable-vulkan-validation";
   public static final String ARG_ENABLE_VULKAN_VALIDATION = "--enable-vulkan-validation";
@@ -124,9 +123,7 @@ public class FlutterShellArgs {
     if (intent.hasExtra(ARG_KEY_TRACE_TO_FILE)) {
       args.add(ARG_TRACE_TO_FILE + "=" + intent.getStringExtra(ARG_KEY_TRACE_TO_FILE));
     }
-    if (intent.getBooleanExtra(ARG_KEY_ENABLE_IMPELLER, false)) {
-      args.add(ARG_ENABLE_IMPELLER);
-    } else {
+    if (!intent.getBooleanExtra(ARG_KEY_ENABLE_IMPELLER, true)) {
       args.add(ARG_DISABLE_IMPELLER);
     }
     if (intent.getBooleanExtra(ARG_KEY_ENABLE_VULKAN_VALIDATION, false)) {
