663dafa58db8fb2c13ddb919a0bb33754dd168dc
David Iglesias Teixeira <ditman@gmail.com>
2024-06-05 15:41:11 -0700

    Add info on what happens when NOT calling the method.

--- lib/ui/pointer.dart
+++ lib/ui/pointer.dart
@@ -397,10 +397,17 @@ class PointerData {
   /// This method can be called any number of times, but once `allowPlatformDefault`
   /// is set to `true`, it can't be set to `false` again.
   ///
+  /// If `allowPlatformDefault` is never set to `true`, the Flutter engine will
+  /// consume the event, so it won't be seen by the platform. In the web, this
+  /// means that `preventDefault` will be called in the DOM event that triggered
+  /// the `PointerData`. See [Event: preventDefault() method in MDN][EpDmiMDN].
+  ///
   /// The implementation of this method is configured through the `onRespond`
   /// parameter of the [PointerData] constructor.
   ///
   /// See also [PointerDataRespondCallback].
+  ///
+  /// [EpDmiMDN]: https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
   void respond({required bool allowPlatformDefault}) {
     if (_onRespond != null) {
       _onRespond(allowPlatformDefault: allowPlatformDefault);
