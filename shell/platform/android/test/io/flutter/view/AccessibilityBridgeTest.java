e2714b0d86db753972323adaecbdbb568045bfc6
Haakon Bertheussen <haakon@bertheussen.com>
2024-06-13 11:36:24 +0200

    Parse Settings.Global.TRANSITION_ANIMATION_SCALE as float.

--- shell/platform/android/test/io/flutter/view/AccessibilityBridgeTest.java
+++ shell/platform/android/test/io/flutter/view/AccessibilityBridgeTest.java
@@ -28,8 +28,10 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.database.ContentObserver;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.SpannableString;
 import android.text.SpannedString;
 import android.text.style.LocaleSpan;
@@ -2010,6 +2012,45 @@ public class AccessibilityBridgeTest {
 
     accessibilityBridge.createAccessibilityNodeInfo(0);
     verify(accessibilityViewEmbedder).getRootNode(eq(embeddedView), eq(0), any(Rect.class));
+  }
+
+  @Test
+  public void testItSetsDisableAnimationsFlagBasedOnTransitionAnimationScale() {
+    AccessibilityChannel mockChannel = mock(AccessibilityChannel.class);
+    ContentResolver mockContentResolver = mock(ContentResolver.class);
+
+    AccessibilityBridge accessibilityBridge =
+        setUpBridge(
+            /*rootAccessibilityView=*/ null,
+            /*accessibilityChannel=*/ mockChannel,
+            /*accessibilityManager=*/ null,
+            /*contentResolver=*/ mockContentResolver,
+            /*accessibilityViewEmbedder=*/ null,
+            /*platformViewsAccessibilityDelegate=*/ null);
+
+    // Capture the observer registered for Settings.Global.TRANSITION_ANIMATION_SCALE
+    ArgumentCaptor<ContentObserver> observerCaptor = ArgumentCaptor.forClass(ContentObserver.class);
+    verify(mockContentResolver)
+        .registerContentObserver(
+            eq(Settings.Global.getUriFor(Settings.Global.TRANSITION_ANIMATION_SCALE)),
+            eq(false),
+            observerCaptor.capture());
+    ContentObserver observer = observerCaptor.getValue();
+
+    // Initial state
+    verify(mockChannel).setAccessibilityFeatures(0);
+    reset(mockChannel);
+
+    // Animations are disabled
+    Settings.Global.putFloat(mockContentResolver, "transition_animation_scale", 0.0f);
+    observer.onChange(false);
+    verify(mockChannel).setAccessibilityFeatures(1 << 2);
+    reset(mockChannel);
+
+    // Animations are enabled
+    Settings.Global.putFloat(mockContentResolver, "transition_animation_scale", 1.0f);
+    observer.onChange(false);
+    verify(mockChannel).setAccessibilityFeatures(0);
   }
 
   @Test
