b9c996bbdf2d516203327e4997b21df7c28eae94
Mouad Debbar <mdebbar@google.com>
2024-06-24 15:29:48 -0400

    Delete commented import

--- lib/web_ui/test/engine/platform_dispatcher/app_lifecycle_state_test.dart
+++ lib/web_ui/test/engine/platform_dispatcher/app_lifecycle_state_test.dart
@@ -7,8 +7,6 @@ import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
 
-// import '../../common/test_initialization.dart';
-
 void main() {
   internalBootstrapBrowserTest(() => testMain);
 }
