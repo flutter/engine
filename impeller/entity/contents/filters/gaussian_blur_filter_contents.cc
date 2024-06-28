dfafc28117ea6a7d69d32abd01b7f836a87dd252
jonahwilliams <jonahwilliams@google.com>
2024-06-28 09:43:05 -0700

    [Impeller] skip mip generation if blur downsample is factor is 0.5 or greater.

--- impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
+++ impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
@@ -5,13 +5,16 @@
 #include "impeller/entity/contents/filters/gaussian_blur_filter_contents.h"
 
 #include <cmath>
+#include <optional>
 
 #include "flutter/fml/make_copyable.h"
+#include "impeller/core/formats.h"
 #include "impeller/entity/contents/clip_contents.h"
 #include "impeller/entity/contents/content_context.h"
 #include "impeller/entity/texture_fill.frag.h"
 #include "impeller/entity/texture_fill.vert.h"
 #include "impeller/renderer/render_pass.h"
+#include "impeller/renderer/texture_mipmap.h"
 #include "impeller/renderer/vertex_buffer_builder.h"
 
 namespace impeller {
@@ -146,12 +149,6 @@ std::optional<Snapshot> GetSnapshot(const std::shared_ptr<FilterInput>& input,
   if (!input_snapshot.has_value()) {
     return std::nullopt;
   }
-
-  // In order to avoid shimmering in downsampling step, we should have mips.
-  if (input_snapshot->texture->GetMipCount() <= 1) {
-    FML_DLOG(ERROR) << GaussianBlurFilterContents::kNoMipsError;
-  }
-  FML_DCHECK(!input_snapshot->texture->NeedsMipmapGeneration());
 
   return input_snapshot;
 }
@@ -326,7 +323,8 @@ fml::StatusOr<RenderTarget> MakeDownsampleSubpass(
     std::shared_ptr<Texture> input_texture,
     const SamplerDescriptor& sampler_descriptor,
     const DownsamplePassArgs& pass_args,
-    Entity::TileMode tile_mode) {
+    Entity::TileMode tile_mode,
+    bool min_lod_clamp) {
   ContentContext::SubpassCallback subpass_callback =
       [&](const ContentContext& renderer, RenderPass& pass) {
         HostBuffer& host_buffer = renderer.GetTransientsBuffer();
@@ -356,6 +354,9 @@ fml::StatusOr<RenderTarget> MakeDownsampleSubpass(
         SetTileMode(&linear_sampler_descriptor, renderer, tile_mode);
         linear_sampler_descriptor.mag_filter = MinMagFilter::kLinear;
         linear_sampler_descriptor.min_filter = MinMagFilter::kLinear;
+        if (min_lod_clamp) {
+          linear_sampler_descriptor.mip_filter = MipFilter::kBase;
+        }
         TextureFillVertexShader::BindFrameInfo(
             pass, host_buffer.EmplaceUniform(frame_info));
         TextureFillFragmentShader::BindFragInfo(
@@ -679,9 +680,23 @@ std::optional<Entity> GaussianBlurFilterContents::RenderFilter(
       blur_info.scaled_sigma, blur_info.padding, input_snapshot.value(),
       source_expanded_coverage_hint, inputs[0], snapshot_entity);
 
-  fml::StatusOr<RenderTarget> pass1_out = MakeDownsampleSubpass(
-      renderer, command_buffer, input_snapshot->texture,
-      input_snapshot->sampler_descriptor, downsample_pass_args, tile_mode_);
+  // If the blur is scaled by more than 0.5, generate mipmaps. Otherwise, the
+  // bilinear filtering is sufficient to preserve quality.
+  bool generated_mips = false;
+  if (downsample_pass_args.effective_scalar.x < 0.5 ||
+      downsample_pass_args.effective_scalar.y < 0.5) {
+    if (!AddMipmapGeneration(command_buffer, renderer.GetContext(),
+                             input_snapshot->texture)
+             .ok()) {
+      return std::nullopt;
+    }
+    generated_mips = true;
+  }
+
+  fml::StatusOr<RenderTarget> pass1_out =
+      MakeDownsampleSubpass(renderer, command_buffer, input_snapshot->texture,
+                            input_snapshot->sampler_descriptor,
+                            downsample_pass_args, tile_mode_, !generated_mips);
 
   if (!pass1_out.ok()) {
     return std::nullopt;
