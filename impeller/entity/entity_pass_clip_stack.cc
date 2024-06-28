3b5e58f4854bb4e70e143edee3d74c4b2f023619
Jonah Williams <jonahwilliams@google.com>
2024-06-28 12:30:05 -0700

    Update entity_pass_clip_stack.cc

--- impeller/entity/entity_pass_clip_stack.cc
+++ impeller/entity/entity_pass_clip_stack.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include "impeller/entity/entity_pass_clip_stack.h"
-#include "fml/logging.h"
 #include "impeller/entity/contents/clip_contents.h"
 #include "impeller/entity/entity.h"
 
@@ -22,12 +21,6 @@ EntityPassClipStack::EntityPassClipStack(const Rect& initial_coverage_rect) {
 }
 
 std::optional<Rect> EntityPassClipStack::CurrentClipCoverage() const {
-  if (subpass_state_.empty()) {
-    return std::nullopt;
-  }
-  if (subpass_state_.back().clip_coverage.empty()) {
-    return std::nullopt;
-  }
   return subpass_state_.back().clip_coverage.back().coverage;
 }
 
@@ -48,7 +41,6 @@ void EntityPassClipStack::PushSubpass(std::optional<Rect> subpass_coverage,
 
 void EntityPassClipStack::PopSubpass() {
   subpass_state_.pop_back();
-  FML_CHECK(!subpass_state_.empty());
 }
 
 const std::vector<ClipCoverageLayer>
