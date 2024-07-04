4e97f4907a749e547fe291695d67c8d310c14131
jonahwilliams <jonahwilliams@google.com>
2024-07-03 17:17:38 -0700

    [Impeller] dont recycle host buffers in between platform view compositing on iOS.

--- shell/gpu/gpu_surface_vulkan_impeller.cc
+++ shell/gpu/gpu_surface_vulkan_impeller.cc
@@ -117,9 +117,8 @@ std::unique_ptr<SurfaceFrame> GPUSurfaceVulkanImpeller::AcquireFrame(
                   impeller_dispatcher,
                   SkIRect::MakeWH(cull_rect.width, cull_rect.height));
               auto picture = impeller_dispatcher.EndRecordingAsPicture();
-
-              return aiks_context->Render(picture, render_target,
-                                          /*reset_host_buffer=*/true);
+              const bool reset_host_buffer = surface_frame.submit_info().frame_boundary;
+              return aiks_context->Render(picture, render_target, reset_host_buffer);
 #endif
             }));
       });
