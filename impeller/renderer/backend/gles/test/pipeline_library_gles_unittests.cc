3b3e4bee853b9435dd5510ab16dbbae0055233b0
Chinmay Garde <chinmaygarde@google.com>
2024-07-08 14:22:38 -0700

    Spec. constant test.

--- impeller/renderer/backend/gles/test/pipeline_library_gles_unittests.cc
+++ impeller/renderer/backend/gles/test/pipeline_library_gles_unittests.cc
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "impeller/fixtures/box_fade.frag.h"
-#include "impeller/fixtures/box_fade.vert.h"
+#include "impeller/fixtures/spec_constant.frag.h"
+#include "impeller/fixtures/spec_constant.vert.h"
 #include "impeller/playground/playground_test.h"
 #include "impeller/renderer/backend/gles/pipeline_gles.h"
 #include "impeller/renderer/backend/gles/pipeline_library_gles.h"
@@ -15,8 +15,8 @@ using PipelineLibraryGLESTest = PlaygroundTest;
 INSTANTIATE_OPENGLES_PLAYGROUND_SUITE(PipelineLibraryGLESTest);
 
 TEST_P(PipelineLibraryGLESTest, ProgramHandlesAreReused) {
-  using VS = BoxFadeVertexShader;
-  using FS = BoxFadeFragmentShader;
+  using VS = SpecConstantVertexShader;
+  using FS = SpecConstantFragmentShader;
   auto context = GetContext();
   ASSERT_TRUE(context);
   auto desc = PipelineBuilder<VS, FS>::MakeDefaultPipelineDescriptor(*context);
@@ -39,6 +39,31 @@ TEST_P(PipelineLibraryGLESTest, ProgramHandlesAreReused) {
   ASSERT_FALSE(new_pipeline_gles.GetProgramHandle().IsDead());
   ASSERT_EQ(pipeline_gles.GetProgramHandle().name.value(),
             new_pipeline_gles.GetProgramHandle().name.value());
+}
+
+TEST_P(PipelineLibraryGLESTest, ChangingSpecConstantsCausesNewProgramObject) {
+  using VS = SpecConstantVertexShader;
+  using FS = SpecConstantFragmentShader;
+  auto context = GetContext();
+  ASSERT_TRUE(context);
+  auto desc = PipelineBuilder<VS, FS>::MakeDefaultPipelineDescriptor(*context);
+  ASSERT_TRUE(desc.has_value());
+  desc->SetSpecializationConstants({2.0f});
+  auto pipeline = context->GetPipelineLibrary()->GetPipeline(desc).Get();
+  ASSERT_TRUE(pipeline && pipeline->IsValid());
+  auto new_desc = desc;
+  // Changing the spec. constants should result in a new program object.
+  new_desc->SetSpecializationConstants({4.0f});
+  auto new_pipeline =
+      context->GetPipelineLibrary()->GetPipeline(new_desc).Get();
+  ASSERT_TRUE(new_pipeline && new_pipeline->IsValid());
+  const auto& pipeline_gles = PipelineGLES::Cast(*pipeline);
+  const auto& new_pipeline_gles = PipelineGLES::Cast(*new_pipeline);
+  // The program handles should be live and equal.
+  ASSERT_FALSE(pipeline_gles.GetProgramHandle().IsDead());
+  ASSERT_FALSE(new_pipeline_gles.GetProgramHandle().IsDead());
+  ASSERT_FALSE(pipeline_gles.GetProgramHandle().name.value() ==
+               new_pipeline_gles.GetProgramHandle().name.value());
 }
 
 }  // namespace impeller::testing
