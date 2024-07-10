99a2e4e26fcd7f88919e1fa3e537d4cc2d119943
Jenn Magder <magder@google.com>
2024-07-10 16:23:04 -0700

    Delete bundle between runs

--- testing/scenario_app/bin/run_ios_tests.dart
+++ testing/scenario_app/bin/run_ios_tests.dart
@@ -144,7 +144,7 @@ Future<void> _run(
     if (await process.exitCode != 0) {
       final String outputPath = _zipAndStoreFailedTestResults(
         iosEngineVariant: iosEngineVariant,
-        resultBundlePath: resultBundle.path,
+        resultBundle: resultBundle,
         storePath: dumpXcresultOnFailure,
       );
       io.stderr.writeln('Failed test results are stored at $outputPath');
@@ -171,7 +171,7 @@ Future<void> _run(
     if (await process.exitCode != 0) {
       final String outputPath = _zipAndStoreFailedTestResults(
         iosEngineVariant: iosEngineVariant,
-        resultBundlePath: resultBundle.path,
+        resultBundle: resultBundle,
         storePath: dumpXcresultOnFailure,
       );
       io.stderr.writeln('Failed test results are stored at $outputPath');
@@ -399,7 +399,7 @@ String _infoPlistFPathForImpeller(Engine engine) {
 @useResult
 String _zipAndStoreFailedTestResults({
   required String iosEngineVariant,
-  required String resultBundlePath,
+  required io.Directory resultBundle,
   required String storePath,
 }) {
   final outputPath = path.join(storePath, '$iosEngineVariant.zip');
@@ -409,11 +409,12 @@ String _zipAndStoreFailedTestResults({
       '-q',
       '-r',
       outputPath,
-      resultBundlePath,
+      resultBundle.path,
     ],
   );
   if (result.exitCode != 0) {
     throw Exception('Failed to zip the test results: ${result.stderr}');
   }
+  resultBundle.deleteSync(recursive: true);
   return outputPath;
 }
