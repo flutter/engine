d62caf7bf0534d67740ac7cd85e1ffddeb465a76
Robert Ancell <robert.ancell@canonical.com>
2024-07-23 10:41:37 +0100

    Reorder headers

--- shell/platform/linux/fl_window_state_monitor.cc
+++ shell/platform/linux/fl_window_state_monitor.cc
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <gtk/gtk.h>
-
 #include "flutter/shell/platform/linux/fl_window_state_monitor.h"
+
+#include <gtk/gtk.h>
 
 #include "flutter/shell/platform/linux/public/flutter_linux/fl_string_codec.h"
 
