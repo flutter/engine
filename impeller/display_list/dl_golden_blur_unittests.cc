da0deb7eda144bf546e520601c97579ab0495592
Aaron Clarke <aaclarke@google.com>
2024-07-24 16:02:31 -0700

    got the rmse working

--- impeller/display_list/dl_golden_blur_unittests.cc
+++ impeller/display_list/dl_golden_blur_unittests.cc
@@ -111,6 +111,41 @@ TEST_P(DlGoldenTest, TextBlurMaskFilterDisrespectCTM) {
   ASSERT_TRUE(OpenPlaygroundHere(builder.Build()));
 }
 
+namespace {
+double CalculateDistance(const uint8_t* left, const uint8_t* right) {
+  double diff[4] = {
+      static_cast<double>(left[0] - right[0]),  //
+      static_cast<double>(left[1] - right[1]),  //
+      static_cast<double>(left[2] - right[2]),  //
+      static_cast<double>(left[3] - right[3])   //
+  };
+  return sqrt((diff[0] * diff[0]) +  //
+              (diff[1] * diff[1]) +  //
+              (diff[2] * diff[2]) +  //
+              (diff[3] * diff[3]));
+}
+
+double RMSE(impeller::testing::Screenshot* left,
+            impeller::testing::Screenshot* right) {
+  FML_CHECK(left);
+  FML_CHECK(right);
+  FML_CHECK(left->GetWidth() == right->GetWidth());
+  FML_CHECK(left->GetHeight() == right->GetHeight());
+
+  int64_t samples = left->GetWidth() * left->GetHeight();
+  double tally = 0;
+
+  const uint8_t* left_ptr = left->GetBytes();
+  const uint8_t* right_ptr = right->GetBytes();
+  for (int64_t i = 0; i < samples; ++i, left_ptr += 4, right_ptr += 4) {
+    double distance = CalculateDistance(left_ptr, right_ptr);
+    tally += distance * distance;
+  }
+
+  return sqrt(tally / static_cast<double>(samples));
+}
+}  // namespace
+
 TEST_P(DlGoldenTest, ShimmerTest) {
   impeller::Point content_scale = GetContentScale();
   auto draw = [&](DlCanvas* canvas, const std::vector<sk_sp<DlImage>>& images,
@@ -145,10 +180,12 @@ TEST_P(DlGoldenTest, ShimmerTest) {
     GTEST_SKIP() << "making screenshots not supported.";
   }
 
-  for (int i = 1; i < 500; ++i) {
+  for (int i = 1; i <= 500; ++i) {
     float sigma = i;
     std::unique_ptr<impeller::testing::Screenshot> right =
         make_screenshot(sigma);
+    double rmse = RMSE(left.get(), right.get());
+    EXPECT_TRUE(rmse < 1.0) << "sigma:" << sigma << " rmse: " << rmse;
     left = std::move(right);
   }
 }
