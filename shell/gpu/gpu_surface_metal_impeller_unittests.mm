3a355557d23fc600f41c0189aec807360e5d4f8b
Jonah Williams <jonahwilliams@google.com>
2024-07-25 09:43:43 -0700

    Update gpu_surface_metal_impeller_unittests.mm

--- shell/gpu/gpu_surface_metal_impeller_unittests.mm
+++ shell/gpu/gpu_surface_metal_impeller_unittests.mm
@@ -125,35 +125,35 @@ TEST(GPUSurfaceMetalImpeller, AcquireFrameFromCAMetalLayerDoesNotRetainThis) {
 //   EXPECT_EQ(host_buffer.GetStateForTest().current_frame, 1u);
 // }
 
-#ifdef IMPELLER_DEBUG
-TEST(GPUSurfaceMetalImpeller, CreatesImpellerCaptureScope) {
-  auto delegate = std::make_shared<TestGPUSurfaceMetalDelegate>();
-  delegate->SetDevice();
+// #ifdef IMPELLER_DEBUG
+// TEST(GPUSurfaceMetalImpeller, CreatesImpellerCaptureScope) {
+//   auto delegate = std::make_shared<TestGPUSurfaceMetalDelegate>();
+//   delegate->SetDevice();
 
-  auto context = CreateImpellerContext();
+//   auto context = CreateImpellerContext();
 
-  EXPECT_FALSE(context->GetCaptureManager()->CaptureScopeActive());
+//   EXPECT_FALSE(context->GetCaptureManager()->CaptureScopeActive());
 
-  std::unique_ptr<Surface> surface =
-      std::make_unique<GPUSurfaceMetalImpeller>(delegate.get(), context);
-  auto frame_1 = surface->AcquireFrame(SkISize::Make(100, 100));
-  frame_1->set_submit_info({.frame_boundary = false});
+//   std::unique_ptr<Surface> surface =
+//       std::make_unique<GPUSurfaceMetalImpeller>(delegate.get(), context);
+//   auto frame_1 = surface->AcquireFrame(SkISize::Make(100, 100));
+//   frame_1->set_submit_info({.frame_boundary = false});
 
-  EXPECT_TRUE(context->GetCaptureManager()->CaptureScopeActive());
+//   EXPECT_TRUE(context->GetCaptureManager()->CaptureScopeActive());
 
-  std::unique_ptr<Surface> surface_2 =
-      std::make_unique<GPUSurfaceMetalImpeller>(delegate.get(), context);
-  auto frame_2 = surface->AcquireFrame(SkISize::Make(100, 100));
-  frame_2->set_submit_info({.frame_boundary = true});
+//   std::unique_ptr<Surface> surface_2 =
+//       std::make_unique<GPUSurfaceMetalImpeller>(delegate.get(), context);
+//   auto frame_2 = surface->AcquireFrame(SkISize::Make(100, 100));
+//   frame_2->set_submit_info({.frame_boundary = true});
 
-  EXPECT_TRUE(context->GetCaptureManager()->CaptureScopeActive());
+//   EXPECT_TRUE(context->GetCaptureManager()->CaptureScopeActive());
 
-  ASSERT_TRUE(frame_1->Submit());
-  EXPECT_TRUE(context->GetCaptureManager()->CaptureScopeActive());
-  ASSERT_TRUE(frame_2->Submit());
-  EXPECT_FALSE(context->GetCaptureManager()->CaptureScopeActive());
-}
-#endif  // IMPELLER_DEBUG
+//   ASSERT_TRUE(frame_1->Submit());
+//   EXPECT_TRUE(context->GetCaptureManager()->CaptureScopeActive());
+//   ASSERT_TRUE(frame_2->Submit());
+//   EXPECT_FALSE(context->GetCaptureManager()->CaptureScopeActive());
+// }
+// #endif  // IMPELLER_DEBUG
 
 }  // namespace testing
 }  // namespace flutter
