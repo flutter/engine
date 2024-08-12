cca8913418971dcdb83a5c38ce8d164d81ac96a4
Chris Bracken <chris@bracken.jp>
2024-08-12 13:19:51 -0700

    Prefixed usages

--- sky/tools/create_ios_framework.py
+++ sky/tools/create_ios_framework.py
@@ -12,10 +12,7 @@ import os
 import sys
 
 from create_xcframework import create_xcframework  # pylint: disable=import-error
-from sky_utils import (  # pylint: disable=import-error
-    assert_directory, assert_file, buildroot_relative_path, copy_binary, copy_tree, create_zip,
-    extract_dsym, lipo, strip_binary, write_codesign_config
-)
+import sky_utils  # pylint: disable=import-error
 
 
 def main():
@@ -36,25 +33,25 @@ def main():
 
   args = parser.parse_args()
 
-  dst = (args.dst if os.path.isabs(args.dst) else buildroot_relative_path(args.dst))
+  dst = (args.dst if os.path.isabs(args.dst) else sky_utils.buildroot_relative_path(args.dst))
 
   arm64_out_dir = (
-      args.arm64_out_dir
-      if os.path.isabs(args.arm64_out_dir) else buildroot_relative_path(args.arm64_out_dir)
+      args.arm64_out_dir if os.path.isabs(args.arm64_out_dir) else
+      sky_utils.buildroot_relative_path(args.arm64_out_dir)
   )
 
   x64_out_dir = None
   if args.x64_out_dir:
     x64_out_dir = (
         args.x64_out_dir
-        if os.path.isabs(args.x64_out_dir) else buildroot_relative_path(args.x64_out_dir)
+        if os.path.isabs(args.x64_out_dir) else sky_utils.buildroot_relative_path(args.x64_out_dir)
     )
 
   simulator_x64_out_dir = None
   if args.simulator_x64_out_dir:
     simulator_x64_out_dir = (
         args.simulator_x64_out_dir if os.path.isabs(args.simulator_x64_out_dir) else
-        buildroot_relative_path(args.simulator_x64_out_dir)
+        sky_utils.buildroot_relative_path(args.simulator_x64_out_dir)
     )
 
   framework = os.path.join(dst, 'Flutter.framework')
@@ -66,15 +63,15 @@ def main():
   if args.simulator_arm64_out_dir:
     simulator_arm64_out_dir = (
         args.simulator_arm64_out_dir if os.path.isabs(args.simulator_arm64_out_dir) else
-        buildroot_relative_path(args.simulator_arm64_out_dir)
+        sky_utils.buildroot_relative_path(args.simulator_arm64_out_dir)
     )
 
   if args.simulator_arm64_out_dir is not None:
     simulator_arm64_framework = os.path.join(simulator_arm64_out_dir, 'Flutter.framework')
 
-  assert_directory(arm64_framework, 'iOS arm64 framework')
-  assert_directory(simulator_arm64_framework, 'iOS arm64 simulator framework')
-  assert_directory(simulator_x64_framework, 'iOS x64 simulator framework')
+  sky_utils.assert_directory(arm64_framework, 'iOS arm64 framework')
+  sky_utils.assert_directory(simulator_arm64_framework, 'iOS arm64 simulator framework')
+  sky_utils.assert_directory(simulator_x64_framework, 'iOS x64 simulator framework')
   create_framework(
       args, dst, framework, arm64_framework, simulator_framework, simulator_x64_framework,
       simulator_arm64_framework
@@ -89,10 +86,10 @@ def main():
   # Copy gen_snapshot binary to destination directory.
   if arm64_out_dir:
     gen_snapshot = os.path.join(arm64_out_dir, 'gen_snapshot_arm64')
-    copy_binary(gen_snapshot, os.path.join(dst, 'gen_snapshot_arm64'))
+    sky_utils.copy_binary(gen_snapshot, os.path.join(dst, 'gen_snapshot_arm64'))
   if x64_out_dir:
     gen_snapshot = os.path.join(x64_out_dir, 'gen_snapshot_x64')
-    copy_binary(gen_snapshot, os.path.join(dst, 'gen_snapshot_x64'))
+    sky_utils.copy_binary(gen_snapshot, os.path.join(dst, 'gen_snapshot_x64'))
 
   zip_archive(dst)
   return 0
@@ -129,9 +126,9 @@ def create_framework(  # pylint: disable=too-many-arguments
   arm64_dylib = os.path.join(arm64_framework, 'Flutter')
   simulator_x64_dylib = os.path.join(simulator_x64_framework, 'Flutter')
   simulator_arm64_dylib = os.path.join(simulator_arm64_framework, 'Flutter')
-  assert_file(arm64_dylib, 'iOS arm64 dylib')
-  assert_file(simulator_arm64_dylib, 'iOS simulator arm64 dylib')
-  assert_file(simulator_x64_dylib, 'iOS simulator x64 dylib')
+  sky_utils.assert_file(arm64_dylib, 'iOS arm64 dylib')
+  sky_utils.assert_file(simulator_arm64_dylib, 'iOS simulator arm64 dylib')
+  sky_utils.assert_file(simulator_x64_dylib, 'iOS simulator x64 dylib')
 
   # Compute dsym output paths, if enabled.
   framework_dsym = None
@@ -141,15 +138,15 @@ def create_framework(  # pylint: disable=too-many-arguments
     simulator_dsym = simulator_framework + '.dSYM'
 
   # Emit the framework for physical devices.
-  copy_tree(arm64_framework, framework)
+  sky_utils.copy_tree(arm64_framework, framework)
   framework_binary = os.path.join(framework, 'Flutter')
   process_framework(args, dst, framework_binary, framework_dsym)
 
   # Emit the framework for simulators.
   if args.simulator_arm64_out_dir is not None:
-    copy_tree(simulator_arm64_framework, simulator_framework)
+    sky_utils.copy_tree(simulator_arm64_framework, simulator_framework)
     simulator_framework_binary = os.path.join(simulator_framework, 'Flutter')
-    lipo([simulator_x64_dylib, simulator_arm64_dylib], simulator_framework_binary)
+    sky_utils.lipo([simulator_x64_dylib, simulator_arm64_dylib], simulator_framework_binary)
     process_framework(args, dst, simulator_framework_binary, simulator_dsym)
   else:
     simulator_framework = simulator_x64_framework
@@ -161,15 +158,15 @@ def create_framework(  # pylint: disable=too-many-arguments
   dsyms = [simulator_dsym, framework_dsym] if args.dsym else None
   create_xcframework(location=dst, name='Flutter', frameworks=xcframeworks, dsyms=dsyms)
 
-  lipo([arm64_dylib, simulator_x64_dylib], framework_binary)
+  sky_utils.lipo([arm64_dylib, simulator_x64_dylib], framework_binary)
   process_framework(args, dst, framework_binary, framework_dsym)
   return 0
 
 
 def zip_archive(dst):
-  write_codesign_config(os.path.join(dst, 'entitlements.txt'), ['gen_snapshot_arm64'])
+  sky_utils.write_codesign_config(os.path.join(dst, 'entitlements.txt'), ['gen_snapshot_arm64'])
 
-  write_codesign_config(
+  sky_utils.write_codesign_config(
       os.path.join(dst, 'without_entitlements.txt'), [
           'Flutter.xcframework/ios-arm64/Flutter.framework/Flutter',
           'Flutter.xcframework/ios-arm64_x86_64-simulator/Flutter.framework/Flutter',
@@ -178,7 +175,7 @@ def zip_archive(dst):
       ]
   )
 
-  create_zip(
+  sky_utils.create_zip(
       dst, 'artifacts.zip', [
           'gen_snapshot_arm64',
           'Flutter.xcframework',
@@ -191,11 +188,11 @@ def zip_archive(dst):
 
 def process_framework(args, dst, framework_binary, dsym):
   if dsym:
-    extract_dsym(framework_binary, dsym)
+    sky_utils.extract_dsym(framework_binary, dsym)
 
   if args.strip:
     unstripped_out = os.path.join(dst, 'Flutter.unstripped')
-    strip_binary(framework_binary, unstripped_copy_path=unstripped_out)
+    sky_utils.strip_binary(framework_binary, unstripped_copy_path=unstripped_out)
 
 
 if __name__ == '__main__':
