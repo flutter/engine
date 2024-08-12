74d40fcf9a3ac83e5c16070a030f2d612a237868
Valentin Hăloiu <valentin.haloiu@gmail.com>
2024-08-12 22:31:21 +0100

    Add `format` field to embedder EGL surface backing store
    Replace hard-coded `BGRA8` value with `FlutterOpenGLSurface.format` field

--- shell/platform/embedder/embedder.cc
+++ shell/platform/embedder/embedder.cc
@@ -868,7 +868,7 @@ static sk_sp<SkSurface> MakeSkSurfaceFromBackingStore(
     const FlutterOpenGLSurface* surface) {
 #ifdef SHELL_ENABLE_GL
   GrGLFramebufferInfo framebuffer_info = {};
-  framebuffer_info.fFormat = GL_BGRA8_EXT;
+  framebuffer_info.fFormat = SAFE_ACCESS(surface, format, GL_BGRA8_EXT);
   framebuffer_info.fFBOID = 0;
 
   auto backend_render_target =
