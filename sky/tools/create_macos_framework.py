04ce39942a60b6762d540adf45609c87095af310
Chris Bracken <chris@bracken.jp>
2024-08-16 12:04:56 -0700

    Extract create_fat_macos_framework

--- sky/tools/create_macos_framework.py
+++ sky/tools/create_macos_framework.py
@@ -59,30 +59,8 @@ def main():
     return 1
 
   fat_framework = os.path.join(dst, 'FlutterMacOS.framework')
-  sky_utils.copy_tree(arm64_framework, fat_framework, symlinks=True)
-  regenerate_symlinks(fat_framework)
-
-  fat_framework_binary = os.path.join(fat_framework, 'Versions', 'A', 'FlutterMacOS')
-
-  # Create the arm64/x64 fat framework.
-  sky_utils.lipo([arm64_dylib, x64_dylib], fat_framework_binary)
-
-  # Make the framework readable and executable: u=rwx,go=rx.
-  subprocess.check_call(['chmod', '755', fat_framework])
-
-  # Add group and other readability to all files.
-  versions_path = os.path.join(fat_framework, 'Versions')
-  subprocess.check_call(['chmod', '-R', 'og+r', versions_path])
-  # Find all the files below the target dir with owner execute permission
-  find_subprocess = subprocess.Popen(['find', versions_path, '-perm', '-100', '-print0'],
-                                     stdout=subprocess.PIPE)
-  # Add execute permission for other and group for all files that had it for owner.
-  xargs_subprocess = subprocess.Popen(['xargs', '-0', 'chmod', 'og+x'],
-                                      stdin=find_subprocess.stdout)
-  find_subprocess.wait()
-  xargs_subprocess.wait()
-
-  process_framework(dst, args, fat_framework, fat_framework_binary)
+  create_fat_macos_framework(fat_framework, arm64_framework, x64_framework)
+  process_framework(dst, args, fat_framework)
 
   # Create XCFramework from the arm64 and x64 fat framework.
   xcframeworks = [fat_framework]
@@ -94,38 +72,69 @@ def main():
   return 0
 
 
-def regenerate_symlinks(fat_framework):
+def create_fat_macos_framework(fat_framework, arm64_framework, x64_framework):
+  sky_utils.copy_tree(arm64_framework, fat_framework, symlinks=True)
+  regenerate_symlinks(fat_framework)
+
+  fat_framework_binary = os.path.join(fat_framework, 'Versions', 'A', 'FlutterMacOS')
+
+  # Create the arm64/x64 fat framework.
+  arm64_dylib = os.path.join(arm64_framework, 'FlutterMacOS')
+  x64_dylib = os.path.join(x64_framework, 'FlutterMacOS')
+  sky_utils.lipo([arm64_dylib, x64_dylib], fat_framework_binary)
+  set_framework_permissions(fat_framework)
+
+
+def set_framework_permissions(framework_dir):
+  # Make the framework readable and executable: u=rwx,go=rx.
+  subprocess.check_call(['chmod', '755', framework_dir])
+
+  # Add group and other readability to all files.
+  versions_path = os.path.join(framework_dir, 'Versions')
+  subprocess.check_call(['chmod', '-R', 'og+r', versions_path])
+  # Find all the files below the target dir with owner execute permission
+  find_subprocess = subprocess.Popen(['find', versions_path, '-perm', '-100', '-print0'],
+                                     stdout=subprocess.PIPE)
+  # Add execute permission for other and group for all files that had it for owner.
+  xargs_subprocess = subprocess.Popen(['xargs', '-0', 'chmod', 'og+x'],
+                                      stdin=find_subprocess.stdout)
+  find_subprocess.wait()
+  xargs_subprocess.wait()
+
+
+def regenerate_symlinks(framework_dir):
   """Regenerates the symlinks structure.
 
   Recipes V2 upload artifacts in CAS before integration and CAS follows symlinks.
   This logic regenerates the symlinks in the expected structure.
   """
-  if os.path.islink(os.path.join(fat_framework, 'FlutterMacOS')):
+  if os.path.islink(os.path.join(framework_dir, 'FlutterMacOS')):
     return
-  os.remove(os.path.join(fat_framework, 'FlutterMacOS'))
-  shutil.rmtree(os.path.join(fat_framework, 'Headers'), True)
-  shutil.rmtree(os.path.join(fat_framework, 'Modules'), True)
-  shutil.rmtree(os.path.join(fat_framework, 'Resources'), True)
-  current_version_path = os.path.join(fat_framework, 'Versions', 'Current')
+  os.remove(os.path.join(framework_dir, 'FlutterMacOS'))
+  shutil.rmtree(os.path.join(framework_dir, 'Headers'), True)
+  shutil.rmtree(os.path.join(framework_dir, 'Modules'), True)
+  shutil.rmtree(os.path.join(framework_dir, 'Resources'), True)
+  current_version_path = os.path.join(framework_dir, 'Versions', 'Current')
   shutil.rmtree(current_version_path, True)
   os.symlink('A', current_version_path)
   os.symlink(
       os.path.join('Versions', 'Current', 'FlutterMacOS'),
-      os.path.join(fat_framework, 'FlutterMacOS')
+      os.path.join(framework_dir, 'FlutterMacOS')
   )
   os.symlink(
-      os.path.join('Versions', 'Current', 'Headers'), os.path.join(fat_framework, 'Headers')
+      os.path.join('Versions', 'Current', 'Headers'), os.path.join(framework_dir, 'Headers')
   )
   os.symlink(
-      os.path.join('Versions', 'Current', 'Modules'), os.path.join(fat_framework, 'Modules')
+      os.path.join('Versions', 'Current', 'Modules'), os.path.join(framework_dir, 'Modules')
   )
   os.symlink(
       os.path.join('Versions', 'Current', 'Resources'),
-      os.path.join(fat_framework, 'Resources')
+      os.path.join(framework_dir, 'Resources')
   )
 
 
-def process_framework(dst, args, fat_framework, fat_framework_binary):
+def process_framework(dst, args, fat_framework):
+  fat_framework_binary = os.path.join(fat_framework, 'Versions', 'A', 'FlutterMacOS')
   if args.dsym:
     dsym_out = os.path.splitext(fat_framework)[0] + '.dSYM'
     sky_utils.extract_dsym(fat_framework_binary, dsym_out)
