234783fbbad66cb6e2f1bc25f11df6a62076e82e
Yegor Jbanov <yjbanov@google.com>
2024-08-20 16:33:53 -0700

    add an explainer in a comment

--- lib/web_ui/lib/src/engine/semantics/text_field.dart
+++ lib/web_ui/lib/src/engine/semantics/text_field.dart
@@ -231,6 +231,18 @@ class SemanticTextField extends SemanticRole {
   }
 
   void _initializeEditableElement() {
+    // Technically, a field can be both multi-line and obscured. However, there's
+    // no standard way on the web to do it (there's a proprietary Safari method
+    // but unclear how well that works, and practice shows - remember role="text"?
+    // - that Safari can yank proprietary features suddenly).
+    //
+    // The best choice in this situation is to use a non-obscured multiline
+    // <textarea> because:
+    //   - Multiline fields need to be able to grow vertically, matching the
+    //     framework's layout, but <input type="password"> cannot do it. Its
+    //     internal text layout with go wildly out of sync.
+    //   - Obscuring semantic fields is not strictly necessary, as the entire
+    //     semantic tree is already obscured by being invisible.
     editableElement = semanticsObject.hasFlag(ui.SemanticsFlag.isMultiline)
         ? createDomHTMLTextAreaElement()
         : _createSingleLineField();
