2201c625ef01ef5012fc9b0522b33684ec57e3b1
jonahwilliams <jonahwilliams@google.com>
2024-08-21 17:04:43 -0700

    ++

--- shell/gpu/gpu_surface_noop.mm
+++ shell/gpu/gpu_surface_noop.mm
@@ -12,8 +12,6 @@
 #include "flutter/common/settings.h"
 #include "flutter/fml/mapping.h"
 #include "flutter/fml/trace_event.h"
-#include "impeller/display_list/dl_dispatcher.h"
-#include "impeller/renderer/backend/metal/surface_mtl.h"
 
 static_assert(!__has_feature(objc_arc), "ARC must be disabled.");
 
