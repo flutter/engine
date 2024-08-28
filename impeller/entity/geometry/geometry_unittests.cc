ffc49ed18fbad64a4750c5ffb378ef082b12eed0
jonahwilliams <jonahwilliams@google.com>
2024-08-27 21:01:06 -0700

    one more expect near.

--- impeller/entity/geometry/geometry_unittests.cc
+++ impeller/entity/geometry/geometry_unittests.cc
@@ -139,8 +139,8 @@ TEST(EntityGeometryTest, GeometryResultHasReasonableDefaults) {
 TEST(EntityGeometryTest, AlphaCoverageStrokePaths) {
   auto matrix = Matrix::MakeScale(Vector2{3.0, 3.0});
   EXPECT_EQ(Geometry::MakeStrokePath({}, 0.5)->ComputeAlphaCoverage(matrix), 1);
-  EXPECT_EQ(Geometry::MakeStrokePath({}, 0.1)->ComputeAlphaCoverage(matrix),
-            0.6);
+  EXPECT_NEAR(Geometry::MakeStrokePath({}, 0.1)->ComputeAlphaCoverage(matrix),
+              0.6, 0.05);
   EXPECT_NEAR(Geometry::MakeStrokePath({}, 0.05)->ComputeAlphaCoverage(matrix),
               0.3, 0.05);
   EXPECT_NEAR(Geometry::MakeStrokePath({}, 0.01)->ComputeAlphaCoverage(matrix),
