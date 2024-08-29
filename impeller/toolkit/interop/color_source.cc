adfb2e4e4a220e0b2c3bd1377556d6565cffa992
Chinmay Garde <chinmaygarde@google.com>
2024-08-29 12:23:45 -0700

    [Impeller] Expose a single-header C API to Impellers Display-List layer.
    This is a versioned, single-header, dependency-free, C API that exposes Display-Lists in almost their entirety.
    
    The anticipated users of this API do not expect ABI or API stability guarantees at this time but those can be added between specific versions as the API matures.
    
    Testing this API can be done via a playgrounds harness. One has been setup with rudimentary tests that will be filled out.
    
    A simple C example using GLFW with OpenGL ES has been setup. Only OpenGL ES has been exposed at this time but additional backend will be added.
    
    This API is meant to be easy to bind to using automated tools for access in different languages and runtimes. Consequently, the API follows a strict convention for object naming, creating, and reference-counting.
    
    All typedefs and method names have the “Impeller” prefix.
    Most objects are reference counted. Methods ending with “New” return a new instance of the object with reference count of 1.
    Each object has a “Retain” and “Release” method to modify reference counts.
    
    For instance, with Rust bindgen, the following invocation generates usable bindings:
    
    ```sh
    bindgen impeller.h -o impeller/rs
      --no-layout-tests
      --rustified-enum “Impeller.*”
      --allowlist-item “Impeller.*”
    ```
    
    It is expected that wrapped over these generated bindings will be written to make the usage more idiomatic. The C API itself is fairly verbose as well.

---
+++ impeller/toolkit/interop/color_source.cc
@@ -0,0 +1,123 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "impeller/toolkit/interop/color_source.h"
+
+namespace impeller::interop {
+
+ScopedObject<ColorSource> ColorSource::MakeLinearGradient(
+    const Point& start_point,
+    const Point& end_point,
+    const std::vector<Color>& colors,
+    const std::vector<Scalar>& stops,
+    flutter::DlTileMode tile_mode,
+    const Matrix& transformation) {
+  const auto dl_colors = ToDisplayListType<flutter::DlColor, Color>(colors);
+  const auto sk_transformation = ToSkMatrix(transformation);
+  auto dl_filter =
+      flutter::DlColorSource::MakeLinear(ToSkiaType(start_point),  //
+                                         ToSkiaType(end_point),    //
+                                         stops.size(),             //
+                                         dl_colors.data(),         //
+                                         stops.data(),             //
+                                         tile_mode,                //
+                                         &sk_transformation        //
+      );
+  if (!dl_filter) {
+    return nullptr;
+  }
+  return Create<ColorSource>(std::move(dl_filter));
+}
+
+ScopedObject<ColorSource> ColorSource::MakeRadialGradient(
+    const Point& center,
+    Scalar radius,
+    const std::vector<Color>& colors,
+    const std::vector<Scalar>& stops,
+    flutter::DlTileMode tile_mode,
+    const Matrix& transformation) {
+  const auto dl_colors = ToDisplayListType<flutter::DlColor, Color>(colors);
+  const auto sk_transformation = ToSkMatrix(transformation);
+  auto dl_filter = flutter::DlColorSource::MakeRadial(ToSkiaType(center),  //
+                                                      radius,              //
+                                                      stops.size(),        //
+                                                      dl_colors.data(),    //
+                                                      stops.data(),        //
+                                                      tile_mode,           //
+                                                      &sk_transformation   //
+  );
+  if (!dl_filter) {
+    return nullptr;
+  }
+  return Create<ColorSource>(std::move(dl_filter));
+}
+
+ScopedObject<ColorSource> ColorSource::MakeConicalGradient(
+    const Point& start_center,
+    Scalar start_radius,
+    const Point& end_center,
+    Scalar end_radius,
+    const std::vector<Color>& colors,
+    const std::vector<Scalar>& stops,
+    flutter::DlTileMode tile_mode,
+    const Matrix& transformation) {
+  const auto dl_colors = ToDisplayListType<flutter::DlColor, Color>(colors);
+  const auto sk_transformation = ToSkMatrix(transformation);
+  auto dl_filter =
+      flutter::DlColorSource::MakeConical(ToSkiaType(start_center),  //
+                                          start_radius,              //
+                                          ToSkiaType(end_center),    //
+                                          end_radius,                //
+                                          stops.size(),              //
+                                          dl_colors.data(),          //
+                                          stops.data(),              //
+                                          tile_mode,                 //
+                                          &sk_transformation         //
+      );
+  if (!dl_filter) {
+    return nullptr;
+  }
+  return Create<ColorSource>(std::move(dl_filter));
+}
+
+ScopedObject<ColorSource> ColorSource::MakeSweepGradient(
+    const Point& center,
+    Scalar start,
+    Scalar end,
+    const std::vector<Color>& colors,
+    const std::vector<Scalar>& stops,
+    flutter::DlTileMode tile_mode,
+    const Matrix& transformation) {
+  const auto dl_colors = ToDisplayListType<flutter::DlColor, Color>(colors);
+  const auto sk_transformation = ToSkMatrix(transformation);
+  auto dl_filter = flutter::DlColorSource::MakeSweep(ToSkiaType(center),  //
+                                                     start,               //
+                                                     end,                 //
+                                                     stops.size(),        //
+                                                     dl_colors.data(),    //
+                                                     stops.data(),        //
+                                                     tile_mode,           //
+                                                     &sk_transformation   //
+  );
+  if (!dl_filter) {
+    return nullptr;
+  }
+  return Create<ColorSource>(std::move(dl_filter));
+}
+
+ColorSource::ColorSource(std::shared_ptr<flutter::DlColorSource> source)
+    : color_source_(std::move(source)) {}
+
+ColorSource::~ColorSource() = default;
+
+bool ColorSource::IsValid() const {
+  return !!color_source_;
+}
+
+const std::shared_ptr<flutter::DlColorSource>& ColorSource::GetColorSource()
+    const {
+  return color_source_;
+}
+
+}  // namespace impeller::interop
