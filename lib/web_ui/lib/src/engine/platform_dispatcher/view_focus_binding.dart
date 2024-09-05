b74d9b5fa4e2d1d819b584b0f0e8ddd3d4736e33
Juan Tugores <tugorez@google.com>
2024-09-04 20:11:41 -0700

    Clean up the view focus enabler flag

--- lib/web_ui/lib/src/engine/platform_dispatcher/view_focus_binding.dart
+++ lib/web_ui/lib/src/engine/platform_dispatcher/view_focus_binding.dart
@@ -3,20 +3,12 @@
 // found in the LICENSE file.
 
 import 'dart:async';
-import 'package:meta/meta.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
 
 /// Tracks the [FlutterView]s focus changes.
 final class ViewFocusBinding {
   ViewFocusBinding(this._viewManager, this._onViewFocusChange);
-
-
-  /// Wether [FlutterView] focus changes will be reported and performed.
-  ///
-  /// DO NOT rely on this bit as it will go away soon. You're warned :)!
-  @visibleForTesting
-  static bool isEnabled = false;
 
   final FlutterViewManager _viewManager;
   final ui.ViewFocusChangeCallback _onViewFocusChange;
@@ -43,9 +35,6 @@ final class ViewFocusBinding {
   }
 
   void changeViewFocus(int viewId, ui.ViewFocusState state) {
-    if (!isEnabled) {
-      return;
-    }
     final DomElement? viewElement = _viewManager[viewId]?.dom.rootElement;
 
     if (state == ui.ViewFocusState.focused) {
@@ -82,10 +71,6 @@ final class ViewFocusBinding {
   });
 
   void _handleFocusChange(DomElement? focusedElement) {
-    if (!isEnabled) {
-      return;
-    }
-
     final int? viewId = _viewId(focusedElement);
     if (viewId == _lastViewId) {
       return;
