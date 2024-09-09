b9e4578125307c6aeca26d53d079395ff5ec7757
jonahwilliams <jonahwilliams@google.com>
2024-09-08 21:59:22 -0700

    duh.

--- shell/gpu/gpu_surface_metal_impeller.mm
+++ shell/gpu/gpu_surface_metal_impeller.mm
@@ -166,7 +166,6 @@ std::unique_ptr<SurfaceFrame> GPUSurfaceMetalImpeller::AcquireFrameFromCAMetalLa
 
         impeller::IRect cull_rect = surface->coverage();
         SkIRect sk_cull_rect = SkIRect::MakeWH(cull_rect.GetWidth(), cull_rect.GetHeight());
-        const bool reset_host_buffer = surface_frame.submit_info().frame_boundary;
 
         impeller::RenderTarget render_target = surface->GetTargetRenderPassDescriptor();
         surface->SetFrameBoundary(surface_frame.submit_info().frame_boundary);
@@ -183,9 +182,7 @@ std::unique_ptr<SurfaceFrame> GPUSurfaceMetalImpeller::AcquireFrameFromCAMetalLa
         display_list->Dispatch(impeller_dispatcher, sk_cull_rect);
         impeller_dispatcher.FinishRecording();
         aiks_context->GetContentContext().GetLazyGlyphAtlas()->ResetTextFrames();
-        if (reset_host_buffer) {
-          aiks_context->GetContentContext().GetTransientsBuffer().Reset();
-        }
+        aiks_context->GetContentContext().GetTransientsBuffer().Reset();
 
         if (!surface->PreparePresent()) {
           return false;
