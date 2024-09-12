418fb9a883f5be09d3057ac63c49b5decc30e086
Gray Mackall <mackall@google.com>
2024-09-12 13:02:15 -0700

    synthesize remove events

--- shell/platform/android/io/flutter/embedding/android/AndroidTouchProcessor.java
+++ shell/platform/android/io/flutter/embedding/android/AndroidTouchProcessor.java
@@ -175,7 +175,11 @@ public class AndroidTouchProcessor {
       // to correctly batch everything back into the original Android event if needed.
       addPointerForIndex(event, event.getActionIndex(), pointerChange, 0, transformMatrix, packet);
 
-      //addPointerForIndex(event, event.getActionIndex(), PointerChange.REMOVE, 0, transformMatrix, packet);
+      // Flutter's pointer handling synthesizes an add event for each new pointer, but does not
+      // synthesize remove events. Remove each pointer on it's corresponding ACTION_UP or
+      // ACTION_POINTER_UP.
+      // See https://github.com/flutter/flutter/issues/154842.
+      addPointerForIndex(event, event.getActionIndex(), PointerChange.REMOVE, 0, transformMatrix, packet);
     } else {
       // ACTION_MOVE may not actually mean all pointers have moved
       // but it's the responsibility of a later part of the system to
