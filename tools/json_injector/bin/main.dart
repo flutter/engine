c37888cdb805308a887654f195248cad2624d25c
Aaron Clarke <aaclarke@google.com>
2024-09-19 13:53:25 -0700

    implemented main

--- tools/json_injector/bin/main.dart
+++ tools/json_injector/bin/main.dart
@@ -2,8 +2,49 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:convert' show jsonDecode, jsonEncode;
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:json_injector/json_injector.dart';
 
-void main() {
+void main(List<String> arguments) {
+  final parser = ArgParser()
+    ..addOption('input',
+        abbr: 'i', help: 'Path to the input JSON file', mandatory: true)
+    ..addOption('injector',
+        abbr: 'j', help: 'Path to the injector JSON file', mandatory: true)
+    ..addOption('output',
+        abbr: 'o', help: 'Path to the output JSON file', mandatory: true)
+    ..addOption('templates',
+        abbr: 't', help: 'Path to the templates JSON file', mandatory: false)
+    ..addOption('name-key',
+        abbr: 'n', help: 'Name key for processing', mandatory: false);
 
+  ArgResults argResults;
+  try {
+    argResults = parser.parse(arguments);
+  } catch (e) {
+    print('Error: $e\n');
+    print('Usage:\n${parser.usage}');
+    return;
+  }
+
+  final String inputJson = File(argResults['input'] as String).readAsStringSync();
+  final String injectorJson = File(argResults['injector'] as String).readAsStringSync();
+  final String? nameKey = argResults['name-key'] as String?;
+  final String outputPath = argResults['output'] as String;
+  
+  Map<String, Map<dynamic, dynamic>>? templates;
+  final String? templatesPath = argResults['templates'] as String?;
+  if (templatesPath != null) {
+    final templateJson = File(templatesPath).readAsStringSync();
+    templates = jsonDecode(templateJson) as Map<String, Map<dynamic, dynamic>>?;
+  }
+
+  final dynamic input = jsonDecode(inputJson);
+  final dynamic injector = jsonDecode(injectorJson);
+
+  final dynamic result = inject(input, injector, nameKey: nameKey, templates: templates);
+  File(outputPath).writeAsStringSync(jsonEncode(result));
 }
