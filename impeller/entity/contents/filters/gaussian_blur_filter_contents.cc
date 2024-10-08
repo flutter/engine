a2d9ab223d20d8ea14758e7d8f31091802273884
Aaron Clarke <aaclarke@google.com>
2024-10-08 10:02:58 -0700

    tidied up naming

--- impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
+++ impeller/entity/contents/filters/gaussian_blur_filter_contents.cc
@@ -74,6 +74,9 @@ Vector2 ExtractScale(const Matrix& matrix) {
 struct BlurInfo {
   /// The scalar that is used to get from source space to unrotated local space.
   Vector2 source_space_scalar;
+  /// The translation that is used to get from  source space to unrotated local
+  /// space.
+  Vector2 source_space_offset;
   /// Sigma when considering an entity's scale and the effect transform.
   Vector2 scaled_sigma;
   /// Blur radius in source pixels based on scaled_sigma.
@@ -95,6 +98,8 @@ BlurInfo CalculateBlurInfo(const Entity& entity,
   // applied to the result of the blur as part of the result's transform.
   const Vector2 source_space_scalar =
       ExtractScale(entity.GetTransform().Basis());
+  const Vector2 source_space_offset =
+      Vector2(entity.GetTransform().m[12], entity.GetTransform().m[13]);
 
   Vector2 scaled_sigma =
       (effect_transform.Basis() * Matrix::MakeScale(source_space_scalar) *  //
@@ -110,6 +115,7 @@ BlurInfo CalculateBlurInfo(const Entity& entity,
       (Matrix::MakeScale(source_space_scalar) * padding).Abs();
   return {
       .source_space_scalar = source_space_scalar,
+      .source_space_offset = source_space_offset,
       .scaled_sigma = scaled_sigma,
       .blur_radius = blur_radius,
       .padding = padding,
@@ -696,11 +702,8 @@ std::optional<Entity> GaussianBlurFilterContents::RenderFilter(
   }
 
   Entity snapshot_entity = entity.Clone();
-  Scalar foox = entity.GetTransform().m[12];
-  Scalar fooy = entity.GetTransform().m[13];
-
   snapshot_entity.SetTransform(
-      Matrix::MakeTranslation({foox, fooy, 0}) *
+      Matrix::MakeTranslation(blur_info.source_space_offset) *
       Matrix::MakeScale(blur_info.source_space_scalar));
 
   std::optional<Rect> source_expanded_coverage_hint;
@@ -721,10 +724,11 @@ std::optional<Entity> GaussianBlurFilterContents::RenderFilter(
     Entity result =
         Entity::FromSnapshot(input_snapshot.value(),
                              entity.GetBlendMode());  // No blur to render.
-    result.SetTransform(entity.GetTransform() *
-                        Matrix::MakeScale(1.f / blur_info.source_space_scalar) *
-                        Matrix::MakeTranslation({-foox, -fooy, 0}) *
-                        input_snapshot->transform);
+    result.SetTransform(
+        entity.GetTransform() *
+        Matrix::MakeScale(1.f / blur_info.source_space_scalar) *
+        Matrix::MakeTranslation(-1 * blur_info.source_space_offset) *
+        input_snapshot->transform);
     return result;
   }
 
@@ -832,8 +836,8 @@ std::optional<Entity> GaussianBlurFilterContents::RenderFilter(
                .transform =
                    entity.GetTransform() *                                   //
                    Matrix::MakeScale(1.f / blur_info.source_space_scalar) *  //
-                   Matrix::MakeTranslation({-foox, -fooy, 0}) *
-                   downsample_pass_args.transform *                          //
+                   Matrix::MakeTranslation(-1 * blur_info.source_space_offset) *
+                   downsample_pass_args.transform *  //
                    Matrix::MakeScale(1 / downsample_pass_args.effective_scalar),
                .sampler_descriptor = sampler_desc,
                .opacity = input_snapshot->opacity},
