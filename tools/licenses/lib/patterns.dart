2acad1325f4755073fb68114a5bc3cb04c7d09e6
Sam Rawlins <srawlins@google.com>
2024-10-15 15:04:34 -0700

    Ignore unnecessary_parenthesis in patterns.dart
    These parens seem like they are explicitly included to improve readability.

--- tools/licenses/lib/patterns.dart
+++ tools/licenses/lib/patterns.dart
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ignore_for_file: use_raw_strings, prefer_interpolation_to_compose_strings
+// ignore_for_file: use_raw_strings, prefer_interpolation_to_compose_strings, unnecessary_parenthesis
 
 import 'dart:core' hide RegExp;
 
