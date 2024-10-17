024f714e0dafe9e3e1624f284141ca1758560c3b
John McDole <codefu@google.com>
2024-10-17 10:45:25 -0700

    Move meat out to library

---
+++ tools/engine_tool/lib/engine_hash.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart';
+
+/// Returns the hash signature for the engine source code.
+///
+/// By default, [useMergeBase] will find the base commit hash for local development.
+/// To generate a hash for the local HEAD, set useMergeBase to false, e.g. when building
+/// a release from a branch with cherry picks.
+Future<({String result, String? error})> engineHash(
+  Future<ProcessResult> Function(List<String> command) runProccess, {
+  bool useMergeBase = true,
+}) async {
+  final String base;
+
+  if (useMergeBase) {
+    final processResult = await runProccess(
+      <String>[
+        'git',
+        'merge-base',
+        'upstream/main',
+        'HEAD',
+      ],
+    );
+
+    if (processResult.exitCode != 0) {
+      return (
+        result: '',
+        error: '''
+  Error getting base hash of the repositor: ${processResult.exitCode}
+  ${processResult.stderr}''',
+      );
+    }
+
+    final regex = RegExp(r'^([a-fA-F0-9]+)');
+    final baseHash = regex.matchAsPrefix(processResult.stdout as String);
+    if (baseHash == null || baseHash.groupCount != 1) {
+      return (
+        result: '',
+        error: '''
+  Error getting base hash of the repositor: ${processResult.exitCode}
+  ${processResult.stdout}''',
+      );
+    }
+    base = baseHash[1]!;
+  } else {
+    base = 'HEAD';
+  }
+
+  // List the tree (not the working tree) recusrively for the merge-base.
+  // This is importnat for future filtering of files, but also do not include
+  // the developer's changes / in flight PRs.
+  final processResult = await runProccess(
+    <String>[
+      'git',
+      'ls-tree',
+      '-r',
+      base,
+      // Uncomment the following lines after merge
+      // 'engine', 'DEPS'
+    ],
+  );
+
+  if (processResult.exitCode != 0) {
+    return (
+      result: '',
+      error: '''
+Error listing tree: ${processResult.exitCode}
+${processResult.stderr}''',
+    );
+  }
+
+  // Ensure stable line endings so our hash calculation is stable
+  final treeLines = LineSplitter.split(processResult.stdout as String);
+
+  // We could call `git hash-object --stdin` which would just take the input, calculate the size,
+  // and then sha1sum it like: `blob $size\0$string'. However, that deals with newlines.
+  // Instead this is equivilant to:
+  //     git ls-tree -r $(git merge-base upstream/main HEAD) | <only newlines> | sha1sum
+  final output = AccumulatorSink<Digest>();
+  final sink = sha1.startChunkedConversion(output);
+  for (final line in treeLines) {
+    sink.add(utf8.encode(line));
+    sink.add([0x0a]);
+  }
+  sink.close();
+  final digest = output.events.first;
+
+  return (result: '$digest', error: null);
+}
--- tools/engine_tool/lib/src/commands/hash_command.dart
+++ tools/engine_tool/lib/src/commands/hash_command.dart
@@ -4,8 +4,7 @@
 
 import 'dart:async';
 import 'dart:convert';
-import 'package:convert/convert.dart';
-import 'package:crypto/crypto.dart';
+import '../../engine_hash.dart';
 import 'command.dart';
 
 /// Returns the hash signature of the engine sources used for downloading assets.
@@ -24,70 +23,20 @@ final class HashCommand extends CommandBase {
 
   @override
   Future<int> run() async {
-    var processResult = await environment.processRunner.processManager.run(
-      <String>[
-        'git',
-        'merge-base',
-        'upstream/main',
-        'HEAD',
-      ],
-      workingDirectory: environment.engine.flutterDir.path,
+    final result = await engineHash(
+      (List<String> command) async =>
+          environment.processRunner.processManager.run(
+        command,
+        workingDirectory: environment.engine.flutterDir.path,
+        stdoutEncoding: utf8,
+      ),
     );
 
-    if (processResult.exitCode != 0) {
-      environment.logger.fatal('''
-Error getting base hash of the repositor: ${processResult.exitCode}
-${processResult.stderr}''');
+    if (result.error != null) {
+      environment.logger.fatal(result.error);
     }
 
-    final regex = RegExp(r'^([a-fA-F0-9]+)');
-    final baseHash = regex.matchAsPrefix(processResult.stdout as String);
-    if (baseHash == null || baseHash.groupCount != 1) {
-      environment.logger.fatal('''
-Error getting base hash of the repositor: ${processResult.exitCode}
-${processResult.stdout}''');
-    }
-
-    // List the tree (not the working tree) recusrively for the merge-base.
-    // This is importnat for future filtering of files, but also do not include
-    // the developer's changes / in flight PRs.
-    processResult = await environment.processRunner.processManager.run(
-      <String>[
-        'git',
-        'ls-tree',
-        '-r',
-        baseHash[1]!,
-        // Uncomment the following lines after merge
-        // 'engine', 'DEPS'
-      ],
-      workingDirectory: environment.engine.flutterDir.path,
-      stdoutEncoding: utf8,
-    );
-
-    if (processResult.exitCode != 0) {
-      environment.logger.fatal('''
-Error listing tree: ${processResult.exitCode}
-${processResult.stderr}''');
-    }
-
-    // Ensure stable line endings so our hash calculation is stable
-    final treeLines =
-        LineSplitter.split(processResult.stdout as String);
-
-    // We could call `git hash-object --stdin` which would just take the input, calculate the size,
-    // and then sha1sum it like: `blob $size\0$string'. However, that deals with newlines.
-    // Instead this is equivilant to:
-    //     git ls-tree -r $(git merge-base upstream/main HEAD) | <only newlines> | sha1sum
-    final output = AccumulatorSink<Digest>();
-    final sink = sha1.startChunkedConversion(output);
-    for (final line in treeLines) {
-      sink.add(utf8.encode(line));
-      sink.add([0x0a]);
-    }
-    sink.close();
-    final digest = output.events.first;
-
-    environment.stdout.write('$digest');
+    environment.stdout.write(result.result);
     return 0;
   }
 }
