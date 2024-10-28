c0550c918a7a1b15b3349ff231bf1e6afad45e18
jonahwilliams <jonahwilliams@google.com>
2024-10-28 16:18:51 -0700

    ++

--- impeller/geometry/path_unittests.cc
+++ impeller/geometry/path_unittests.cc
@@ -576,11 +576,49 @@ TEST(PathTest, FanTessellation) {
   EXPECT_EQ(point_storage[0], Point(10, 0));
 }
 
+TEST(PathTest, FanTessellationMultiContour) {
+  PathBuilder builder{};
+  for (auto i = 0; i < 10; i++) {
+    builder.AddRoundRect(
+        RoundRect::MakeRectRadius(Rect::MakeLTRB(0 + i, 0 + i, 100, 100), 10));
+  }
+  auto path = builder.TakePath();
+  auto [points, contours] = path.CountStorage(1.0);
+
+  std::vector<Point> point_storage(points);
+  std::vector<uint16_t> index_storage(points + (contours - 1));
+
+  FanVertexWriter writer(point_storage.data(), index_storage.data());
+  path.WritePolyline(1.0, writer);
+
+  EXPECT_LE(writer.GetIndexCount(), index_storage.size());
+  EXPECT_EQ(point_storage[0], Point(10, 0));
+}
+
 TEST(PathTest, StripTessellation) {
   Path path = PathBuilder{}
                   .AddRoundRect(RoundRect::MakeRectRadius(
                       Rect::MakeLTRB(0, 0, 100, 100), 10))
                   .TakePath();
+  auto [points, contours] = path.CountStorage(1.0);
+
+  std::vector<Point> point_storage(points);
+  std::vector<uint16_t> index_storage(points + (contours - 1));
+
+  StripVertexWriter writer(point_storage.data(), index_storage.data());
+  path.WritePolyline(1.0, writer);
+
+  EXPECT_LE(writer.GetIndexCount(), index_storage.size());
+  EXPECT_EQ(point_storage[0], Point(10, 0));
+}
+
+TEST(PathTest, StripTessellationMultiContour) {
+  PathBuilder builder{};
+  for (auto i = 0; i < 10; i++) {
+    builder.AddRoundRect(
+        RoundRect::MakeRectRadius(Rect::MakeLTRB(0 + i, 0 + i, 100, 100), 10));
+  }
+  auto path = builder.TakePath();
   auto [points, contours] = path.CountStorage(1.0);
 
   std::vector<Point> point_storage(points);
