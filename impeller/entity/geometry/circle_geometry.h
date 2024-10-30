b7152ee1aba8fb78235b2e4525c0b3971f3cf21f
jonahwilliams <jonahwilliams@google.com>
2024-10-30 09:34:35 -0700

    ++

--- impeller/entity/geometry/circle_geometry.h
+++ impeller/entity/geometry/circle_geometry.h
@@ -9,7 +9,7 @@
 
 namespace impeller {
 
-/// @brief Generator for vertices or either filled or stroked circles
+/// @brief Vertex generator for either filled or stroked circles.
 class CircleGeometry final : public Geometry {
  public:
   explicit CircleGeometry(const Point& center, Scalar radius);
