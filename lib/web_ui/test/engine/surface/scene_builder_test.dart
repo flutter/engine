93a6ee6f3ccb16ef50583064444a12a8e662afb3
David Iglesias Teixeira <ditman@gmail.com>
2024-10-31 17:43:37 -0700

    Remove unused import.

--- lib/web_ui/test/engine/surface/scene_builder_test.dart
+++ lib/web_ui/test/engine/surface/scene_builder_test.dart
@@ -13,7 +13,6 @@ import 'package:test/bootstrap/browser.dart';
 import 'package:test/test.dart';
 import 'package:ui/src/engine.dart';
 import 'package:ui/ui.dart' as ui;
-import 'package:ui/ui_web/src/ui_web.dart' as ui_web;
 
 import '../../common/matchers.dart';
 import '../../common/rendering.dart';
