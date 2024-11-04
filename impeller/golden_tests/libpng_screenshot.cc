ffa2f4c3471191fd3108d7d68c1adb8567ec4064
Aaron Clarke <aaclarke@google.com>
2024-11-04 21:09:58 +0000

    started calling libpng

--- impeller/golden_tests/libpng_screenshot.cc
+++ impeller/golden_tests/libpng_screenshot.cc
@@ -4,13 +4,18 @@
 
 #include "impeller/golden_tests/libpng_screenshot.h"
 
+#include <fstream>
+#include "flutter/third_party/libpng/png.h"
+
 namespace impeller {
 namespace testing {
 
 LibPNGScreenshot::LibPNGScreenshot(const uint8_t* bytes,
                                    size_t width,
                                    size_t height)
-    : width_(width), height_(height) {}
+    : bytes_(bytes, bytes + 4 * width * height),
+      width_(width),
+      height_(height) {}
 
 LibPNGScreenshot::~LibPNGScreenshot() = default;
 
@@ -31,7 +36,53 @@ size_t LibPNGScreenshot::GetBytesPerRow() const {
 }
 
 bool LibPNGScreenshot::WriteToPNG(const std::string& path) const {
-  return false;
+  std::ofstream file(path, std::ios::binary);
+  if (!file.is_open()) {
+    return false;
+  }
+
+  png_structp png_ptr =
+      png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  if (!png_ptr) {
+    return false;
+  }
+
+  png_infop info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr) {
+    png_destroy_write_struct(&png_ptr, (png_infopp)nullptr);
+    return false;
+  }
+
+  if (setjmp(png_jmpbuf(png_ptr))) {
+    png_destroy_write_struct(&png_ptr, &info_ptr);
+    return false;
+  }
+
+  png_set_write_fn(
+      png_ptr, (png_voidp)&file,
+      [](png_structp png_ptr, png_bytep data, png_size_t length) {
+        std::ofstream* file = (std::ofstream*)png_get_io_ptr(png_ptr);
+        file->write(reinterpret_cast<char*>(data), length);
+      },
+      NULL);
+
+  png_set_IHDR(png_ptr, info_ptr, width_, height_, 8, PNG_COLOR_TYPE_RGBA,
+               PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT,
+               PNG_FILTER_TYPE_DEFAULT);
+
+  png_write_info(png_ptr, info_ptr);
+
+  png_set_bgr(png_ptr);
+
+  for (size_t y = 0; y < height_; ++y) {
+    png_write_row(png_ptr, &bytes_[y * width_ * 4]);
+  }
+
+  png_write_end(png_ptr, nullptr);
+
+  png_destroy_write_struct(&png_ptr, &info_ptr);
+
+  return true;
 }
 
 }  // namespace testing
