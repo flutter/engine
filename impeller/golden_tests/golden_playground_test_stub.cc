ed4d106be8f64394d108ef1465975ddc4bafcbb7
Aaron Clarke <aaclarke@google.com>
2024-11-04 18:45:25 +0000

    got the exec compiling

--- impeller/golden_tests/golden_playground_test_stub.cc
+++ impeller/golden_tests/golden_playground_test_stub.cc
@@ -4,6 +4,9 @@
 
 #include "flutter/impeller/golden_tests/golden_playground_test.h"
 
+#include <filesystem>
+
+#include "flutter/impeller/golden_tests/golden_digest.h"
 #include "flutter/impeller/playground/playground_impl.h"
 #include "flutter/third_party/abseil-cpp/absl/base/no_destructor.h"
 #include "flutter/impeller/golden_tests/vulkan_screenshotter.h"
@@ -14,6 +17,19 @@
 namespace impeller {
 
 namespace {
+std::string GetTestName() {
+  std::string suite_name =
+      ::testing::UnitTest::GetInstance()->current_test_suite()->name();
+  std::string test_name =
+      ::testing::UnitTest::GetInstance()->current_test_info()->name();
+  std::stringstream ss;
+  ss << "impeller_" << suite_name << "_" << test_name;
+  std::string result = ss.str();
+  // Make sure there are no slashes in the test name.
+  std::replace(result.begin(), result.end(), '/', '_');
+  return result;
+}
+
 std::unique_ptr<PlaygroundImpl> MakeVulkanPlayground(bool enable_validations) {
   FML_CHECK(::glfwInit() == GLFW_TRUE);
   PlaygroundSwitches playground_switches;
@@ -65,7 +81,56 @@ void GoldenPlaygroundTest::SetTypographerContext(
 void GoldenPlaygroundTest::TearDown() {}
 
 void GoldenPlaygroundTest::SetUp() {
-  GTEST_SKIP() << "GoldenPlaygroundTest doesn't support this backend type.";
+  std::filesystem::path testing_assets_path =
+      flutter::testing::GetTestingAssetsPath();
+  std::filesystem::path target_path = testing_assets_path.parent_path()
+                                          .parent_path()
+                                          .parent_path()
+                                          .parent_path();
+  std::filesystem::path icd_path = target_path / "vk_swiftshader_icd.json";
+  setenv("VK_ICD_FILENAMES", icd_path.c_str(), 1);
+
+  std::string test_name = GetTestName();
+  bool enable_wide_gamut = test_name.find("WideGamut_") != std::string::npos;
+  switch (GetParam()) {
+    case PlaygroundBackend::kMetal:
+      GTEST_SKIP()
+        << "Platform doesn't support metal.";
+      break;
+    case PlaygroundBackend::kVulkan: {
+      if (enable_wide_gamut) {
+        GTEST_SKIP() << "Vulkan doesn't support wide gamut golden tests.";
+      }
+      const std::unique_ptr<PlaygroundImpl>& playground =
+          GetSharedVulkanPlayground(/*enable_validations=*/true);
+      pimpl_->screenshotter =
+          std::make_unique<testing::VulkanScreenshotter>(playground);
+      break;
+    }
+    case PlaygroundBackend::kOpenGLES: {
+      if (enable_wide_gamut) {
+        GTEST_SKIP() << "OpenGLES doesn't support wide gamut golden tests.";
+      }
+      FML_CHECK(::glfwInit() == GLFW_TRUE);
+      PlaygroundSwitches playground_switches;
+      playground_switches.use_angle = true;
+      pimpl_->test_opengl_playground = PlaygroundImpl::Create(
+          PlaygroundBackend::kOpenGLES, playground_switches);
+      pimpl_->screenshotter = std::make_unique<testing::VulkanScreenshotter>(
+          pimpl_->test_opengl_playground);
+      break;
+    }
+  }
+
+  // if (std::find(kSkipTests.begin(), kSkipTests.end(), test_name) !=
+  //     kSkipTests.end()) {
+  //   GTEST_SKIP()
+  //       << "GoldenPlaygroundTest doesn't support interactive playground tests "
+  //          "yet.";
+  // }
+
+  testing::GoldenDigest::Instance()->AddDimension(
+      "gpu_string", GetContext()->DescribeGpuModel());
 }
 
 PlaygroundBackend GoldenPlaygroundTest::GetBackend() const {
@@ -138,6 +203,7 @@ bool GoldenPlaygroundTest::ImGuiBegin(const char* name,
 std::shared_ptr<Context> GoldenPlaygroundTest::MakeContext() const {
   if (GetParam() == PlaygroundBackend::kMetal) {
     FML_CHECK(false) << "not supported.";
+    return nullptr;
   } else if (GetParam() == PlaygroundBackend::kVulkan) {
     bool enable_vulkan_validations = true;
     FML_CHECK(!pimpl_->test_vulkan_playground)
