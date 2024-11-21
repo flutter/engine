4bd94d6129a91cc81b9661ce9f37c43db5adca28
Gray Mackall <mackall@google.com>
2024-11-21 14:12:24 -0800

    remove deprecated class - test still passes?

--- shell/platform/android/test/io/flutter/embedding/engine/systemchannels/KeyEventChannelTest.java
+++ shell/platform/android/test/io/flutter/embedding/engine/systemchannels/KeyEventChannelTest.java
@@ -11,11 +11,10 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import android.annotation.TargetApi;
-import android.view.InputDevice;
 import android.view.KeyEvent;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.JSONMessageCodec;
@@ -23,7 +22,6 @@ import io.flutter.util.FakeKeyEvent;
 import java.nio.ByteBuffer;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,11 +29,10 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.robolectric.annotation.Config;
-import org.robolectric.shadows.InputDeviceBuilder;
 
 @Config(manifest = Config.NONE)
 @RunWith(AndroidJUnit4.class)
-@TargetApi(API_LEVELS.API_24)
+@TargetApi(API_LEVELS.API_35)
 public class KeyEventChannelTest {
 
   KeyEvent keyEvent;
@@ -61,59 +58,36 @@ public class KeyEventChannelTest {
     keyEventChannel = new KeyEventChannel(fakeMessenger);
   }
 
-  @After
-  public void tearDown() {
-    // nothing
-  }
+    @Test
+    public void keyDownEventIsSentToFramework() throws JSONException {
+      KeyEventChannel.FlutterKeyEvent flutterKeyEvent =
+          new KeyEventChannel.FlutterKeyEvent(keyEvent, null);
+      keyEventChannel.sendFlutterKeyEvent(
+          flutterKeyEvent,
+          false,
+          (isHandled) -> {
+            handled[0] = isHandled;
+          });
 
-  //  @Test
-  //  public void keyDownEventIsSentToFramework() throws JSONException {
-  //    final InputDevice device = mock(InputDevice.class);
-  //    when(device.isVirtual()).thenReturn(false);
-  //    when(device.getName()).thenReturn("keyboard");
-  //    ShadowInputDevice.sDeviceIds = new int[] {0};
-  //    ShadowInputDevice.addDevice(0, device);
-  //
-  //    KeyEventChannel.FlutterKeyEvent flutterKeyEvent =
-  //        new KeyEventChannel.FlutterKeyEvent(keyEvent, null);
-  //    keyEventChannel.sendFlutterKeyEvent(
-  //        flutterKeyEvent,
-  //        false,
-  //        (isHandled) -> {
-  //          handled[0] = isHandled;
-  //        });
-  //
-  //    ArgumentCaptor<ByteBuffer> byteBufferArgumentCaptor =
-  // ArgumentCaptor.forClass(ByteBuffer.class);
-  //    ArgumentCaptor<BinaryMessenger.BinaryReply> replyArgumentCaptor =
-  //        ArgumentCaptor.forClass(BinaryMessenger.BinaryReply.class);
-  //    verify(fakeMessenger, times(1))
-  //        .send(any(), byteBufferArgumentCaptor.capture(), replyArgumentCaptor.capture());
-  //    ByteBuffer capturedMessage = byteBufferArgumentCaptor.getValue();
-  //    capturedMessage.rewind();
-  //    JSONObject message = (JSONObject) JSONMessageCodec.INSTANCE.decodeMessage(capturedMessage);
-  //    assertNotNull(message);
-  //    assertEquals("keydown", message.get("type"));
-  //
-  //    // Simulate a reply, and see that it is handled.
-  //    sendReply(true, replyArgumentCaptor.getValue());
-  //    assertTrue(handled[0]);
-  //  }
+      ArgumentCaptor<ByteBuffer> byteBufferArgumentCaptor =
+   ArgumentCaptor.forClass(ByteBuffer.class);
+      ArgumentCaptor<BinaryMessenger.BinaryReply> replyArgumentCaptor =
+          ArgumentCaptor.forClass(BinaryMessenger.BinaryReply.class);
+      verify(fakeMessenger, times(1))
+          .send(any(), byteBufferArgumentCaptor.capture(), replyArgumentCaptor.capture());
+      ByteBuffer capturedMessage = byteBufferArgumentCaptor.getValue();
+      capturedMessage.rewind();
+      JSONObject message = (JSONObject) JSONMessageCodec.INSTANCE.decodeMessage(capturedMessage);
+      assertNotNull(message);
+      assertEquals("keydown", message.get("type"));
+
+      // Simulate a reply, and see that it is handled.
+      sendReply(true, replyArgumentCaptor.getValue());
+      assertTrue(handled[0]);
+    }
 
   @Test
   public void keyUpEventIsSentToFramework() throws JSONException {
-    InputDevice device =
-        InputDeviceBuilder.newBuilder()
-            .setId(0)
-            .setName("keyboard")
-            .setKeyboardType(InputDevice.SOURCE_KEYBOARD)
-            .build();
-    // final InputDevice device = mock(InputDevice.class);
-    when(device.isVirtual()).thenReturn(false);
-    when(device.getName()).thenReturn("keyboard");
-    // ShadowInputDevice.sDeviceIds = new int[] {0};
-    // ShadowInputDevice.addDevice(0, device);
-
     keyEvent = new FakeKeyEvent(KeyEvent.ACTION_UP, 65);
     KeyEventChannel.FlutterKeyEvent flutterKeyEvent =
         new KeyEventChannel.FlutterKeyEvent(keyEvent, null);
