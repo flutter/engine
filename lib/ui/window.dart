6a00c6836964e9707dfccc26815902ca3ac60131
Chun-Heng Tai <chtai@google.com>
2024-12-09 14:28:22 -0800

    update doc

--- lib/ui/window.dart
+++ lib/ui/window.dart
@@ -382,9 +382,8 @@ class FlutterView {
 
   /// Change the retained semantics data about this [FlutterView].
   ///
-  /// If [PlatformDispatcher.semanticsEnabled] is true, the user has requested that this function
-  /// be called whenever the semantic content of this [FlutterView]
-  /// changes.
+  /// This method should only be called after [setSemanticsTreeEnabled] is called with
+  /// true.
   ///
   /// This function disposes the given update, which means the semantics update
   /// cannot be used further.
