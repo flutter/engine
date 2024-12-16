2d6e25b9c50796071beeb7cb8da98aca65e4ead6
Camille Simon <camillesimon@google.com>
2024-12-16 15:47:17 -0800

    logs

--- shell/platform/android/io/flutter/plugin/view/SensitiveContentPlugin.java
+++ shell/platform/android/io/flutter/plugin/view/SensitiveContentPlugin.java
@@ -11,6 +11,9 @@ import io.flutter.embedding.engine.systemchannels.SensitiveContentChannel;
 import io.flutter.plugin.common.MethodChannel;
 
 import io.flutter.Log;
+import android.os.IBinder;
+import android.view.Window;
+import android.view.WindowId;
 
 /**
  * {@link SensitiveContentPlugin} is the implementation of all functionality needed to set content
@@ -34,6 +37,7 @@ public class SensitiveContentPlugin
     mSensitiveContentChannel.setSensitiveContentMethodHandler(this);
   }
 
+  int i = 0;
   /**
    * Sets content sensitivity level of the Android {@code View} with the specified {@code
    * flutterViewId} to the level specified by {@contentSensitivity}.
@@ -44,14 +48,32 @@ public class SensitiveContentPlugin
       @NonNull int contentSensitivity,
       @NonNull MethodChannel.Result result) {
     final View flutterView = mflutterActivity.findViewById(flutterViewId);
-    Log.e("CAMILLE", "flutter view ID: " + Integer.toString(flutterViewId));
+    // final Window flutterViewWindow = flutterView.getWindow();
+    WindowId flutterWindowId = flutterView.getWindowId();
+    IBinder windowId = flutterView.getWindowToken();
+
+    Log.e("CAMILLE", Integer.toString(i) + ": flutter view ID: " + Integer.toString(flutterViewId));
+    Log.e("CAMILLE", Integer.toString(i) + ": flutter view: " + flutterView.toString());
+    Log.e("CAMILLE", Integer.toString(i) + ": flutter view is visible: " + Integer.toString(flutterView.getVisibility()));
+    Log.e("CAMILLE", Integer.toString(i) + ": flutter window ID : " + flutterWindowId);
+    Log.e("CAMILLE", Integer.toString(i) + ": flutter view window token: " + windowId);
+
+
     if (flutterView == null) {
       result.error("error", "Requested Flutter View to set content sensitivty of not found.", null);
     }
+    
+    int currentSensitivity = flutterView.getContentSensitivity();
+    Log.e("CAMILLE", Integer.toString(i) + ": current content sensitivity is: " + Integer.toString(currentSensitivity));
+
 
     flutterView.setContentSensitivity(contentSensitivity);
-    Log.e("CAMILLE", "set content sensitivity to: " + Integer.toString(contentSensitivity));
+    int newSensitivity = flutterView.getContentSensitivity();
+
+    Log.e("CAMILLE", Integer.toString(i) + ": set content sensitivity to: " + Integer.toString(contentSensitivity));
+    Log.e("CAMILLE", Integer.toString(i) + ": acutal new content sensitivity is: " + Integer.toString(newSensitivity));
     result.success(null);
+    i++;
   }
 
   /**
